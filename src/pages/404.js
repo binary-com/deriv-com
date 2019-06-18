4720a24d1a020e56d09184dc128cdc580f312d70
OskarAhl <oskar@binary.com>
2019-06-18 14:32:50 +0800

    404 path

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Layout from '../components/layout/layout'
-import SEO from '../components/seo'
+import SEO from '../components/containers/seo'
 
 const NotFoundPage = () => (
 	<Layout>

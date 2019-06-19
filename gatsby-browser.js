c2700d318127ec671e0d48df05c0cf6b0c2b5842
OskarAhl <oskar@binary.com>
2019-06-19 10:09:18 +0800

    add React.context

---
+++ gatsby-browser.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { ThemeProvider } from './src/context/ThemeContext'
+
+export const wrapRootElement = ({ element }) => (
+    <ThemeProvider>{element}</ThemeProvider>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ThemeContext from '../context/ThemeContext'
 
 import Layout from '../components/layout/layout'
 import Image from '../components/elements/image'
 import SEO from '../components/containers/seo'
 
-const Home = () => (
-    <Layout>
-        <SEO title="Home" />
-        <h1>Home</h1>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-            <Image />
-        </div>
-    </Layout>
-)
-
+class Home extends Component {
+    render() {
+        return (
+            <ThemeContext.Consumer>
+                {theme => (
+                    <Layout>
+                        <SEO title="Home" />
+                        <h1>Home {theme.dark}</h1>
+                        <div
+                            style={{
+                                maxWidth: `300px`,
+                                marginBottom: `1.45rem`,
+                            }}
+                        >
+                            <Image />
+                        </div>
+                    </Layout>
+                )}
+            </ThemeContext.Consumer>
+        )
+    }
+}
 export default Home

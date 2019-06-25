21c7cea1c67541c6c908cdc27c97ff7c6099614b
OskarAhl <oskar@binary.com>
2019-06-25 13:32:33 +0800

    add remaining eslint rules

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,45 +1,46 @@
 module.exports = {
-  pathPrefix: process.env.PATH_PREFIX || "/deriv-com/", // For non CNAME GH-pages deployment
-  siteMetadata: {
-    title: `Deriv.com`,
-    description: `Deriv.com gives everyone an easy way to participate in the financial markets. Trade with as little as $1 USD on major currencies, stocks, indices, and commodities.`,
-    author: `Deriv.com`
-  },
-  plugins: [
-    "gatsby-plugin-eslint",
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
+    pathPrefix: process.env.PATH_PREFIX || '/deriv-com/', // For non CNAME GH-pages deployment
+    siteMetadata: {
+        title: 'Deriv.com',
+        description:
+            'Deriv.com gives everyone an easy way to participate in the financial markets. Trade with as little as $1 USD on major currencies, stocks, indices, and commodities.',
+        author: 'Deriv.com',
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/common/logo.png` // This path is relative to the root of the site.
-      }
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /svg/ // See below to configure properly
-        }
-      }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
-  ]
-};
+    plugins: [
+        'gatsby-plugin-eslint',
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-styled-components',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: '${__dirname}/src/images',
+            },
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: 'gatsby-starter-default',
+                short_name: 'starter',
+                start_url: '/',
+                background_color: '#663399',
+                theme_color: '#663399',
+                display: 'minimal-ui',
+                icon: 'src/images/common/logo.png', // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-react-svg',
+            options: {
+                rule: {
+                    include: /svg/, // See below to configure properly
+                },
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // 'gatsby-plugin-offline',
+    ],
+}

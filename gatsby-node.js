a574ab09c175a85c6175603a977038aee4ca6e20
Amam Mustofa <mustofa@binary.com>
2019-06-28 12:19:07 +0800

    add breakpoints and absolute path

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,16 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions }) => {
+    actions.setWebpackConfig({
+        resolve: {
+            modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+            // alias: {
+            //     common: path.resolve(__dirname, 'src/common'),
+            //     components: path.resolve(__dirname, 'src/components'),
+            //     images: path.resolve(__dirname, 'src/images'),
+            //     pages: path.resolve(__dirname, 'src/pages'),
+            //     themes: path.resolve(__dirname, 'src/themes'),
+            // },
+        },
+    })
+}
--- src/components/containers/container.js
+++ src/components/containers/container.js
@@ -1,11 +1,19 @@
 import styled from 'styled-components'
+import device from 'themes/device'
 
 const Container = styled.div`
-    max-width: 1024px;
     margin: 0 auto;
     display: flex;
     align-items: center;
     padding: 2rem 1rem;
+
+    @media ${device.laptop} {
+        max-width: 1024px;
+    }
+
+    @media ${device.desktop} {
+        max-width: 1400px;
+    }
 `
 
 export default Container

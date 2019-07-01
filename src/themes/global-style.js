b140987ab6f13d728103e8c80b6567c8caba0336
Amam Mustofa <mustofa@binary.com>
2019-07-01 15:04:28 +0800

    add font size

--- src/themes/global-style.js
+++ src/themes/global-style.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 import Reset from './reset'
 import Variables from './variables'
+import device from './device'
 
 const GlobalStyle = createGlobalStyle`
     ${Reset}
@@ -11,10 +12,31 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
     }
     * {
-        box-sizing: border-box;
         margin: 0;
         padding: 0;
-        font-size: 62.5%; /* 1 rem = 10px */
+    }
+    html {
+        font-size: 62.5%; /* 1rem = 10px */
+        box-sizing: border-box;
+
+        @media ${device.desktopL} {
+            font-size: 75.5%; /* 1rem = 12px */
+        }
+        @media ${device.laptop} {
+            font-size: 50%; /* 1rem = 8px */
+        }
+        @media ${device.tablet} {
+            font-size: 47%; /* 1rem = 7.5px */
+        }
+        @media ${device.mobileL} {
+            font-size: 38%; /* 1rem = 6px */
+        }
+        @media ${device.mobileM} {
+            font-size: 35%; /* 1rem = 5.5px */
+        }
+        @media ${device.mobileS} {
+            font-size: 32%; /* 1rem = 5px */
+        }
     }
 `
 

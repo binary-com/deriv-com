66104397fabb1927c077b42af73a9e5973622e25
Amam Mustofa <mustofa@binary.com>
2019-07-02 12:41:49 +0800

    remove unnecessary comments

--- src/components/containers/portal.js
+++ src/components/containers/portal.js
@@ -2,7 +2,6 @@ import { Component } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-// Check these element is not null before accessing them
 const has_document = typeof document !== 'undefined'
 const portalRoot = has_document ? document.getElementById('modal') : null
 const gatsbyRoot = has_document ? document.getElementById('___gatsby') : null
@@ -24,10 +23,10 @@ class Portal extends Component {
 
     render() {
         const { children, is_open, is_blurred } = this.props
+
         if (gatsbyRoot && is_blurred) {
             gatsbyRoot.style.filter = is_open ? 'blur(2px)' : 'none'
         }
-        // Check that this.el is not null before using ReactDOM.createPortal
         if (this.el) {
             return ReactDOM.createPortal(children, this.el)
         } else {

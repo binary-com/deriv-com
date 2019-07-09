e3996e98e62ffc0acd0387099807d33346c210c4
Sash <sash@binary.com>
2019-07-09 08:48:14 +0300

    update footer

--- src/themes/variables.js
+++ src/themes/variables.js
@@ -10,6 +10,7 @@ const Variables = css`
         --color-red: #ff444f;
         --color-grey-1: #f4f4f4;
         --color-grey-2: #e8e8e8;
+        --color-grey-3: #646464;
         --color-grey: #9b9b9b;
         --color-green: #85acb0;
         --color-blue: #4c76be; 

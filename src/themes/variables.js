d9a232e6d700cffc4f2afb2f9c8df7adbcc600ea
Amam Mustofa <mustofa@binary.com>
2019-07-11 13:36:52 +0800

    remove keyframes

--- src/themes/variables.js
+++ src/themes/variables.js
@@ -13,7 +13,7 @@ const Variables = css`
         --color-grey-2: #e8e8e8;
         --color-grey: #9b9b9b;
         --color-green: #85acb0;
-        --color-blue: #4c76be; 
+        --color-blue: #4c76be;
 
         /* sizes */
         --text-size-s: 1.6rem;
@@ -21,52 +21,6 @@ const Variables = css`
         --text-size-l: 3.2rem;
         --text-size-xl: 4.8rem;
         --text-size-xxl: 6.4rem;
-
-        /* keyframes */
-        @keyframes rotates {
-            from {
-                transform: rotate(0deg);
-            }
-            to {
-                transform: rotate(360deg);
-            }
-        }
-
-        @keyframes sweep {
-            0% {
-                clip-path: polygon(
-                    0% 0%,
-                    0% 0%,
-                    0% 0%,
-                    50% 50%,
-                    0% 0%,
-                    0% 0%,
-                    0% 0%
-                );
-            }
-            50% {
-                clip-path: polygon(
-                    0% 0%,
-                    0% 100%,
-                    0% 100%,
-                    50% 50%,
-                    100% 0%,
-                    100% 0%,
-                    0% 0%
-                );
-            }
-            100% {
-                clip-path: polygon(
-                    0% 0%,
-                    0% 100%,
-                    100% 100%,
-                    50% 50%,
-                    100% 100%,
-                    100% 0%,
-                    0% 0%
-                );
-            }
-        }
     }
 `
 

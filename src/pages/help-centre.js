4059b26db7f57b51da461b311260baac06ef6dca
OskarAhl <oskar@binary.com>
2019-07-15 15:00:02 +0800

    use matchSort for searching

--- src/pages/help-centre.js
+++ src/pages/help-centre.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 import SEO from '../components/containers/seo'
 import Layout from '../components/layout/layout'
@@ -6,6 +6,7 @@ import { localize, WithIntl } from '../components/localization'
 import Container from '../components/containers/container'
 import SearchSVG from 'images/svg/search.svg'
 import { Header, Text } from '../components/elements/topography.js'
+import matchSorter from 'match-sorter'
 
 const SearchIcon = styled(SearchSVG)`
     position: absolute;
@@ -52,76 +53,172 @@ const ResultWrapper = styled.div`
         margin-bottom: 1.6rem;
     }
 `
-const suggested_topics = [
-    'Who can open an account?',
-    'Opening an account',
-    'Opening an MT5 account',
+
+const HelpArticle = ({ header, paragraphs, children }) => (
+    <>
+        <Header as="h3">{header}</Header>
+        {paragraphs.map((text, idx) => (
+            <Text key={idx}>{text}</Text>
+        ))}
+        {children}
+    </>
+)
+
+const CanITrade = () => (
+    <HelpArticle
+        paragraphs={[
+            'Yes if you are a non-EU citizen and sign up as a expert trader.',
+        ]}
+    />
+)
+
+const OpeningAnAccount = () => (
+    <HelpArticle paragraphs={['This is how you open an account']} />
+)
+
+const WhoCanOpenAnAccount = () => (
+    <HelpArticle
+        paragraphs={[
+            'You may only open an account with us on these conditions:',
+        ]}
+    />
+)
+
+const MakingADeposit = () => (
+    <HelpArticle paragraphs={['Select deposit method and deposit']} />
+)
+
+const MakingAWithDrawal = () => (
+    <HelpArticle paragraphs={['Select deposit method and withdraw']} />
+)
+
+const articles = [
+    {
+        category: 'Account',
+        articles: [
+            {
+                title: 'Who can open an account?',
+                keywords: ['account', 'how', 'who'],
+                sub_category: 'Opening an account',
+                content: <WhoCanOpenAnAccount />,
+            },
+            {
+                title: 'Opening an account',
+                keywords: ['account', 'how', 'who'],
+                sub_category: 'Opening an account',
+                content: <OpeningAnAccount />,
+            },
+        ],
+    },
+    {
+        category: 'Deposits and withdrawals',
+        articles: [
+            {
+                title: 'Making a deposit',
+                keywords: ['deposit', 'make'],
+                sub_category: 'Deposits',
+                content: <MakingADeposit />,
+            },
+            {
+                title: 'Withdrawal processing time',
+                keywords: ['Withdrawal', 'take', 'money'],
+                sub_category: 'Withdrawals',
+                content: <MakingAWithDrawal />,
+            },
+        ],
+    },
 ]
 
-const HelpCentre = () => {
-    const has_results = true
-    return (
-        <Layout>
-            <SEO title={localize('Help centre')} />
-            <Backdrop>
+class HelpCentre extends Component {
+    state = {
+        search: '',
+        selected_article: null,
+    }
+
+    handleInputChange = e => {
+        e.preventDefault()
+        const { name, value } = e.target
+        this.setState({ [name]: value.trim() })
+    }
+
+    handleSelectArticle = article => {
+        this.setState({ selected_article: article })
+    }
+
+    render() {
+        const all_articles = articles
+            .map(category =>
+                category.articles.map(article => ({
+                    ...article,
+                    category: category.category,
+                })),
+            )
+            .flat()
+
+        const { search, selected_article } = this.state
+        const filtered_articles = matchSorter(all_articles, search, {
+            keys: ['title', 'sub_category', 'keywords'],
+        })
+        const has_results = !!filtered_articles.length
+        console.log('search: ', this.state.search)
+        console.log('all_articles: ', all_articles)
+        console.log('filtered: ', filtered_articles)
+
+        return (
+            <Layout>
+                <SEO title={localize('Help centre')} />
+                <Backdrop>
+                    <Container alignItems="normal" flexDirection="column">
+                        <SearchForm>
+                            <SearchIcon />
+                            <Search
+                                name="search"
+                                onChange={this.handleInputChange}
+                                placeholder={localize('How can we help?')}
+                            />
+                        </SearchForm>
+                        <ResultWrapper>
+                            {has_results && (
+                                <SearchSuccess
+                                    suggested_topics={filtered_articles}
+                                    onClick={this.handleSelectArticle}
+                                />
+                            )}
+                            {!has_results && <SearchError search={search} />}
+                        </ResultWrapper>
+                    </Container>
+                </Backdrop>
                 <Container alignItems="normal" flexDirection="column">
-                    <SearchForm>
-                        <SearchIcon />
-                        <Search placeholder={localize('How can we help?')} />
-                    </SearchForm>
-                    <ResultWrapper>
-                        {has_results && <SearchSuccess />}
-                        {!has_results && <SearchError />}
-                    </ResultWrapper>
+                    {!selected_article && (
+                        <TableOfContents>
+                            <ArticleList
+                                articles={articles}
+                                onClick={this.handleSelectArticle}
+                            />
+                        </TableOfContents>
+                    )}
+                    {selected_article && <Article article={selected_article} />}
                 </Container>
-            </Backdrop>
-            <Container alignItems="normal" flexDirection="column">
-                <TableOfContents>
-                    <TextLink.Group>
-                        <TextLink.List
-                            header="Account"
-                            links={[
-                                'Who can open an account?',
-                                'Opening an account',
-                                'Changing your personal details',
-                                'Forgetting your password',
-                                'Resetting your MT5 password',
-                                'Authenticating your account',
-                            ]}
-                        />
-                    </TextLink.Group>
-                    <TextLink.Group>
-                        <TextLink.List
-                            header="Deposit and withdrawals"
-                            links={[
-                                'Who can open an account?',
-                                'Opening an account',
-                                'Changing your personal details',
-                            ]}
-                        />
-                        <TextLink.List
-                            header="Trading"
-                            links={[
-                                'Who can open an account?',
-                                'Opening an account',
-                            ]}
-                        />
-                    </TextLink.Group>
-                </TableOfContents>
-            </Container>
-        </Layout>
-    )
+            </Layout>
+        )
+    }
 }
 
-const SearchSuccess = () => (
+const Article = ({ article }) => article.content
+
+const SearchSuccess = ({ suggested_topics, onClick }) => (
     <>
         <Header as="h3" color="white">
-            {'Topic Suggestions'}
+            Topic Suggestions
         </Header>
         <ListNoBullets>
-            {suggested_topics.map((topic, idx) => (
-                <ListLink key={idx} color="white">
-                    {topic}
+            {suggested_topics.slice(0, 3).map((topic, idx) => (
+                <ListLink
+                    key={idx}
+                    color="white"
+                    onClick={() => onClick(topic)}
+                >
+                    {topic.title}
                 </ListLink>
             ))}
         </ListNoBullets>
@@ -144,10 +241,11 @@ const ErrorHeader = styled(Header)`
     font-size: 2rem;
     padding-bottom: 0.8rem;
 `
-const SearchError = () => (
+
+const SearchError = ({ search }) => (
     <>
         <ErrorHeader as="h5">
-            Sorry, we couldn’t find any results matching “asdfghj”.
+            {`Sorry, we couldn’t find any results matching “${search}”.`}
         </ErrorHeader>
         <Text color="green">Search tips:</Text>
         <BulletList>
@@ -199,7 +297,7 @@ const ListNoBullets = styled.ul`
     }
 `
 
-TextLink.List = function List({ header, links }) {
+TextLink.List = function List({ header, links, onClick }) {
     return (
         <>
             <HeaderWrapper>
@@ -208,10 +306,30 @@ TextLink.List = function List({ header, links }) {
             <ListNoBullets>
                 {links.map((link, idx) => (
                     <li key={idx}>
-                        <ListLink>{link} </ListLink>
+                        <ListLink onClick={() => onClick(link)}>
+                            {link.title}
+                        </ListLink>
                     </li>
                 ))}
             </ListNoBullets>
+        </>
+    )
+}
+
+const ArticleList = ({ articles, onClick }) => {
+    return (
+        <>
+            {articles.map((category, idx) => {
+                return (
+                    <TextLink.Group key={idx}>
+                        <TextLink.List
+                            header={category.category}
+                            links={category.articles}
+                            onClick={onClick}
+                        />
+                    </TextLink.Group>
+                )
+            })}
         </>
     )
 }

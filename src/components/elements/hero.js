bfe302703a830b2f271fc04e0cf23c1c8b9826d8
OskarAhl <oskar@binary.com>
2019-07-16 16:18:51 +0800

    reset hero changes

--- src/components/elements/hero.js
+++ src/components/elements/hero.js
@@ -14,45 +14,36 @@ const NoImageHero = styled(Container)`
     flex-direction: column;
     align-content: space-around;
     padding: 7rem 0 7.8rem 0;
-`
-const ContentWrapper = styled.div`
-    padding: 5rem 0;
 
-    @media ${device.laptop} {
-        text-align: center;
-        padding: 3rem 1rem;
+    div {
+        padding: 5rem 0;
+
+        @media ${device.laptop} {
+            text-align: center;
+            padding: 3rem 1rem;
+        }
     }
 `
-const Hero = ({ paragraph, header_part_1, header_part_2, children }) => (
+const Hero = ({ paragraph, header_part_1, header_part_2 }) => (
     <Wrapper>
         <NoImageHero>
-            {children ? (
-                children
-            ) : (
-                <>
-                    <ContentWrapper>
-                        <Header as="h4" color="white" weight="500">
-                            {paragraph}
-                        </Header>
-                    </ContentWrapper>
-                    <ContentWrapper>
-                        <Header as="h1" color="red" align="center">
-                            {header_part_1}
-                            <br />
-                            {header_part_2}
-                        </Header>
-                    </ContentWrapper>
-                </>
-            )}
+            <div>
+                <Header as="h4" color="white" weight="500">
+                    {paragraph}
+                </Header>
+            </div>
+            <div>
+                <Header as="h1" color="red" align="center">
+                    {header_part_1}
+                    <br />
+                    {header_part_2}
+                </Header>
+            </div>
         </NoImageHero>
     </Wrapper>
 )
 
 Hero.propTypes = {
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node,
-    ]),
     header_part_1: PropTypes.string,
     header_part_2: PropTypes.string,
     paragraph: PropTypes.string,

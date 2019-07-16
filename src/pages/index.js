0da2684246ad2016eac2dffb06c2a4e143741f91
Sash <sash@binary.com>
2019-07-16 09:02:43 +0300

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,9 @@ import Container from '../components/containers/container'
 import Modal from '../components/elements/modal'
 import SignupModal from '../components/elements/signup-modal'
 import { localize, WithIntl } from '../components/localization'
-import HomeHero from '../components/elements/home-hero.js'
-import StyledCard from '../components/elements/styled-card.js'
-import GridCard from '../components/elements/grid-card.js'
-import { Header } from '../components/elements/typography.js'
+import Signup from '../components/form/signup'
+import img from 'images/common/header-trade.png'
+import { Header, Text } from '../components/elements/typography.js'
 import Logo from 'images/svg/deriv.svg'
 import SuperiorPlatform from 'images/svg/superior-trading-platform.svg'
 import PatentedTech from 'images/svg/patented-technology.svg'
@@ -21,6 +20,122 @@ import Trade from 'images/svg/trade.svg'
 import Withdraw from 'images/svg/withdraw.svg'
 import PaymentMethods from '../components/elements/payment-methods.js'
 
+const StyledCard = styled.article`
+    box-sizing: border-box;
+    border-radius: 6px;
+    box-shadow: 0 16px 20px 0 rgba(0, 0, 0, 0.1);
+    background-color: var(--color-white);
+    padding: 4.8rem 5.6rem 2.2rem;
+
+    * {
+        max-width: 100%;
+    }
+    .card-title {
+        margin-top: 2.4rem;
+        margin-bottom: 1rem;
+    }
+    @media ${device.sm} {
+        text-align: center;
+    }
+`
+const Card = styled.article`
+    padding: 1rem;
+
+    * {
+        max-width: 100%;
+    }
+    div {
+        margin-top: 2.4rem;
+    }
+    @media ${device.tabletS} {
+        text-align: center;
+    }
+`
+const HeroWrapper = styled.section`
+    width: 100%;
+    padding: 2rem 0;
+    min-height: 55rem;
+    background: var(--color-black) url(${img});
+    background-repeat: no-repeat;
+    background-position: 23% 100%;
+    background-size: auto;
+
+    @media ${device.lg} {
+        background-position: -10rem 100%;
+    }
+    @media ${device.md} {
+        background-position: -20rem 100%;
+    }
+    @media ${device.sm} {
+        background-position: -40rem 100%;
+    }
+`
+const HeroGrid = styled.section`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    grid-column-gap: 2rem;
+    max-width: 100%;
+
+    article {
+        padding: 0;
+
+        * {
+            max-width: 100%;
+        }
+    }
+    .hero-left {
+        text-align: left;
+
+        .subheader {
+            margin-top: 1rem;
+        }
+    }
+    .signup {
+        justify-self: end;
+        min-width: 33rem;
+
+        .signup-box {
+            max-width: 33rem;
+            background-color: var(--color-grey-1);
+            border-radius: 6px;
+            box-sizing: border-box;
+            text-align: center;
+            padding: 0.5rem 0;
+
+            form {
+                margin-top: -7rem;
+                margin-bottom: -1rem;
+            }
+            h3 {
+                font-size: var(--text-size-m);
+                font-weight: 500;
+            }
+            .already {
+                display: none;
+            }
+            .social-signup {
+                margin-top: -2rem;
+            }
+        }
+    }
+    @media ${device.lg} {
+        grid-template-columns: 1fr;
+        grid-row-gap: 5rem;
+
+        .hero-left * {
+            text-align: center;
+        }
+        .signup {
+            justify-self: center;
+        }
+    }
+    @media ${device.sm} {
+        grid-template-columns: 1fr;
+        grid-row-gap: 8rem;
+        text-align: center;
+    }
+`
 const SectionHeader = styled.div`
     margin-top: 8rem;
     margin-bottom: 2rem;
@@ -135,6 +250,27 @@ const WhyDeriv = styled.div`
         text-align: center;
     }
 `
+const Hero = () => (
+    <HeroWrapper>
+        <Container>
+            <HeroGrid>
+                <article className='hero-left'>
+                    <Header as="h1" color="white" lh="1.2">
+                        {localize('This is your ultimate trading experience')}
+                    </Header>
+                    <Header as="h4" color="white" weight="500" className='subheader'>
+                        {localize('The world’s markets at your fingertips anytime, anywhere.')}
+                    </Header>
+                </article>
+                <article className='signup'>
+                    <div className='signup-box'>
+                        <Signup />
+                    </div>
+                </article>
+            </HeroGrid>
+        </Container>
+    </HeroWrapper>
+)
 
 class Home extends Component {
     state = {
@@ -158,10 +294,7 @@ class Home extends Component {
         return (
             <Layout>
                 <SEO title={localize('Home')} />
-                <HomeHero
-                    header_part_1={localize('This is your ultimate trading experience')}
-                    header_part_2={localize('The world’s markets at your fingertips anytime, anywhere.')}
-                />
+                <Hero />
                 <DtraderSection>
                     <SectionHeader>
                         <h2><Logo /> {localize('DTrader')}</h2>
@@ -169,42 +302,63 @@ class Home extends Component {
                     </SectionHeader>
                     <Container>
                         <Dtrader>
-                            <StyledCard
-                                Icon={SuperiorPlatform}
-                                title={localize('Superior trading platform')}
-                                content_1={localize(
-                                    'A revolutionary platform for all traders.',
-                                )}
-                                content_2={localize(
-                                    'Access the world’s most traded markets and assets.',
-                                )}
-                                content_3={localize(
-                                    '100+ instruments.',
-                                )}
-                            />
-                            <StyledCard
-                                Icon={PatentedTech}
-                                title={localize('Patented pricing technology')}
-                                content_1={localize(
-                                    'Real-time, two-way pricing.',
-                                )}
-                                content_2={localize(
-                                    'Powered by our patented algorithms.',
-                                )}
-                                content_3={localize(
-                                    'Transparency guaranteed.',
-                                )}
-                            />
-                            <StyledCard
-                                Icon={Intuitive}
-                                title={localize('Intuitive and customisable')}
-                                content_1={localize(
-                                    'Easy-to-use and powerful trading environment.',
-                                )}
-                                content_2={localize(
-                                    'Trade the way you want with customisable charts, analytic tools, and themes.',
-                                )}
-                            />
+                            <StyledCard>
+                                <SuperiorPlatform />
+                                <div className='card-title'>
+                                    <Header as="h4" weight="500">
+                                        {localize('Superior trading platform')}
+                                    </Header>
+                                </div>
+                                <div className='card-content'>
+                                    <Header as="h5" color="black-3" lh="1.55" weight="normal">
+                                        {localize('A revolutionary platform for all traders.')}
+                                    </Header>
+                                    <Header as="h5" color="black-3" lh="1.55" weight="normal">
+                                        {localize('Access the world’s most traded markets and assets.')}
+                                    </Header>
+                                    <Header as="h5" color="black-3" lh="1.55" weight="normal">
+                                        {localize('100+ instruments.')}
+                                    </Header>
+                                </div>
+                            </StyledCard>
+                            <StyledCard>
+                                <PatentedTech />
+                                <div className='card-title'>
+                                    <Header as="h4" weight="500">
+                                        {localize('Superior trading platform')}
+                                    </Header>
+                                </div>
+                                <div className='card-content'>
+                                    <Header as="h5" color="black-3" lh="1.55" weight="normal">
+                                        {localize('A revolutionary platform for all traders.')}
+                                    </Header>
+                                    <Header as="h5" color="black-3" lh="1.55" weight="normal">
+                                        {localize('Access the world’s most traded markets and assets.')}
+                                    </Header>
+                                    <Header as="h5" color="black-3" lh="1.55" weight="normal">
+                                        {localize('100+ instruments.')}
+                                    </Header>
+                                </div>
+                            </StyledCard>
+                            <StyledCard>
+                                <Intuitive />
+                                <div className='card-title'>
+                                    <Header as="h4" weight="500">
+                                        {localize('Superior trading platform')}
+                                    </Header>
+                                </div>
+                                <div className='card-content'>
+                                    <Header as="h5" color="black-3" lh="1.55" weight="normal">
+                                        {localize('A revolutionary platform for all traders.')}
+                                    </Header>
+                                    <Header as="h5" color="black-3" lh="1.55" weight="normal">
+                                        {localize('Access the world’s most traded markets and assets.')}
+                                    </Header>
+                                    <Header as="h5" color="black-3" lh="1.55" weight="normal">
+                                        {localize('100+ instruments.')}
+                                    </Header>
+                                </div>
+                            </StyledCard>
                         </Dtrader>
                     </Container>
                     <Cta>
@@ -220,27 +374,39 @@ class Home extends Component {
                 </SectionHeader>
                 <Container>
                     <Works>
-                        <GridCard
-                            Icon={Practice}
-                            title={localize('Practice')}
-                            content={localize(
-                                'Open a demo account with unlimited funds. Start trading for free and practice to hone your skills.',
-                            )}
-                        />
-                        <GridCard
-                            Icon={Trade}
-                            title={localize('Trade')}
-                            content={localize(
-                                'Open a real account and add funds. Trade forex, indices, commodities, and other derivatives.',
-                            )}
-                        />
-                        <GridCard
-                            Icon={Withdraw}
-                            title={localize('Withdraw')}
-                            content={localize(
-                                'Get funds quickly and easily. We support many deposit and withdrawal options.',
-                            )}
-                        />
+                        <Card>
+                            <Practice />
+                            <div>
+                                <Header as="h4" weight="500" lh="2.2">
+                                    {localize('Practice')}
+                                </Header>
+                                <Text color="black-3" lh="1.55">
+                                    {localize('Open a demo account with unlimited funds. Start trading for free and practice to hone your skills.')}
+                                </Text>
+                            </div>
+                        </Card>
+                        <Card>
+                            <Trade />
+                            <div>
+                                <Header as="h4" weight="500" lh="2.2">
+                                    {localize('Trade')}
+                                </Header>
+                                <Text color="black-3" lh="1.55">
+                                    {localize('Open a real account and add funds. Trade forex, indices, commodities, and other derivatives.')}
+                                </Text>
+                            </div>
+                        </Card>
+                        <Card>
+                            <Withdraw />
+                            <div>
+                                <Header as="h4" weight="500" lh="2.2">
+                                    {localize('Withdraw')}
+                                </Header>
+                                <Text color="black-3" lh="1.55">
+                                {localize('Get funds quickly and easily. We support many deposit and withdrawal options.')}
+                                </Text>
+                            </div>
+                        </Card>
                     </Works>
                 </Container>
                 <GreySection>

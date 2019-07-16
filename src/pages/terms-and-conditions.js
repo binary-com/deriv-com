685ad1c108aaff93581b19f0fe3ab09ba65b2e65
Amam Mustofa <mustofa@binary.com>
2019-07-16 15:52:38 +0800

    data feed finish

--- src/pages/terms-and-conditions.js
+++ src/pages/terms-and-conditions.js
@@ -211,7 +211,44 @@ const General = () => (
     </StyledSection>
 )
 
-const DataFeed = () => <StyledSection>DataFeed</StyledSection>
+const DataFeed = () => (
+    <StyledSection>
+        <Header as="h2" color="black">
+            {localize('Data feed')}
+        </Header>
+        <Text margin={{ vertical: '2.4rem' }}>
+            {localize(
+                'Here is some information about the data feeds used by the Company.',
+            )}
+        </Text>
+        <Header as="h4">{localize('Differences with other data feeds')}</Header>
+        <Text margin={{ vertical: '1.8rem' }}>
+            {localize(
+                "The Company's data feed may at times differ slightly from other data feeds found on the Internet (who may themselves differ from each other). Reasons for differences in feed providers include:",
+            )}
+        </Text>
+        <Text margin={{ vertical: '1.8rem' }}>
+            {localize(
+                "For forex: The forex market is an OTC (over the counter) market, where banks and other major financial institutions trade currencies amongst themselves without there being any central clearing house. Accordingly, there is no 'official' price source for forex quotes. Different data feeds will contain quotes from a different sub-set of international banks. Accordingly, prices may differ among providers, depending on which bank(s) they obtain prices from.",
+            )}
+        </Text>
+        <Text margin={{ vertical: '1.8rem' }}>
+            {localize(
+                "Market-closing times: The client is asked to refer to Trading times page for the exact time of settlement for all contracts. Other websites may adopt a different convention (for example, some websites choose 4 o'clock NY time or 5 o'clock London time). As a result, the open, high, low, and closing prices displayed on the Company's website may differ from those on other websites, due to the market-closing time convention.",
+            )}
+        </Text>
+        <Text margin={{ vertical: '1.8rem' }}>
+            {localize(
+                "Use of bid/ask prices: When the market is illiquid, the data feed may contain many bid and ask prices, without there actually being any traded price for quite a while. By taking an average of the bid/ask price (i.e. bid + ask, divided by 2), a market quote is generated that reflects the current market, without it actually being a traded price. The Company's system will generate prices from these bid and ask prices, whereas other websites might not. As a result, the Company's website might display ticks that do not appear in the data feeds of other websites.",
+            )}
+        </Text>
+        <Text margin={{ vertical: '1.8rem' }}>
+            {localize(
+                "The Company's data feed is designed to be one of the best and most robust available for a trading environment.",
+            )}
+        </Text>
+    </StyledSection>
+)
 
 const SecurityAndPrivacy = () => (
     <StyledSection>SecurityAndPrivacy</StyledSection>

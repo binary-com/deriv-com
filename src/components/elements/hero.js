28892d88020bb9b7a3e308bbff7e85af3014b52e
Amam Mustofa <mustofa@binary.com>
2019-07-17 11:49:25 +0800

    change style hero

--- src/components/elements/hero.js
+++ src/components/elements/hero.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import Container from '../containers/container'
-import device from 'themes/device'
 import { Header } from './typography.js'
 
 const Wrapper = styled.section`
@@ -10,28 +9,24 @@ const Wrapper = styled.section`
     background-color: var(--color-black);
 `
 
+const StyledHeader = styled(Header)`
+    padding: 0.8rem 0;
+`
+
 const NoImageHero = styled(Container)`
     flex-direction: column;
     align-content: space-around;
-    padding: 7rem 0 7.8rem 0;
-
-    & > * {
-        padding: 5rem 0;
-
-        @media ${device.laptop} {
-            padding: 3rem 1rem;
-        }
-    }
+    padding: 8rem 0 10rem 0;
 `
 const Hero = ({ paragraph, header }) => (
     <Wrapper>
         <NoImageHero>
-            <Header as="h1" color="red" align="center">
+            <StyledHeader as="h1" color="red" align="center">
                 {header}
-            </Header>
-            <Header as="h4" color="white" weight="500" align="center">
+            </StyledHeader>
+            <StyledHeader as="h4" color="white" weight="500" align="center">
                 {paragraph}
-            </Header>
+            </StyledHeader>
         </NoImageHero>
     </Wrapper>
 )

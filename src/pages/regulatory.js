6ebc0d422a05cb8b0dd822859d20935d46a12de1
Amam Mustofa <mustofa@binary.com>
2019-07-17 13:48:40 +0800

    add flexwrapper

--- src/pages/regulatory.js
+++ src/pages/regulatory.js
@@ -5,13 +5,33 @@ import { localize, WithIntl } from '../components/localization'
 import Hero from 'components/elements/hero'
 import Container from 'components/containers/container'
 import styled from 'styled-components'
+import { Header, Text } from 'components/elements/typography'
 
-const StyledContainer = styled(Container)`
+const RegulatoryWrapper = styled.section`
     background-image: linear-gradient(
         to bottom,
         var(--color-grey-2),
         var(--color-white)
     );
+    width: 100%;
+`
+const StyledContainer = styled(Container)`
+    flex-direction: column;
+    padding: 8rem 0;
+`
+
+const StyledText = styled(Text)`
+    margin: 0.8rem 0;
+    font-size: 2rem;
+`
+
+const FlexWrapper = styled(Container)`
+    flex-wrap: wrap;
+    padding: 1.6rem 0;
+
+    & > * {
+        flex: 0 0 50%;
+    }
 `
 
 const Regulatory = () => (
@@ -23,7 +43,25 @@ const Regulatory = () => (
                 'Deriv operates under the jurisdiction of Binary.com.',
             )}
         />
-        <StyledContainer></StyledContainer>
+        <RegulatoryWrapper>
+            <StyledContainer>
+                <Header as="h2" color="black-2" align="center">
+                    {localize('Binary Limited')}
+                </Header>
+                <StyledText align="center" lh="1.5">
+                    {localize(
+                        'Binary Limited, with a registered office at 47 Esplanade, St Helier, Jersey JE1 0BD, Channel Islands, is the holding company for the following subsidiaries.',
+                    )}
+                </StyledText>
+                <FlexWrapper>
+                    <Text align="center">1</Text>
+                    <Text align="center">2</Text>
+                    <Text align="center">3</Text>
+                    <Text align="center">4</Text>
+                    <Text align="center">5</Text>
+                </FlexWrapper>
+            </StyledContainer>
+        </RegulatoryWrapper>
     </Layout>
 )
 

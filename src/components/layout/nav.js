e6bcaabf828f72898c73c620f62e0694572fc5da
arash <arash@firstsource.io>
2019-07-24 10:51:36 +0800

    covert nav to functional component

--- src/components/layout/nav.js
+++ src/components/layout/nav.js
@@ -1,12 +1,13 @@
 import { LocalizedLink, localize } from '../localization'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import LogoHeader from '../../images/svg/logo-header.svg'
 import Button from '../form/button'
 import Container from '../containers/container'
-import Modal from '../elements/modal'
+import Modal, { useModal } from '../elements/modal'
 import SignupModal from '../elements/signup-modal'
 import { SharedLinkStyle } from '../localization/localized-link'
+import { DERIV_APP_URL } from '../../common/utility'
 
 const StyledNav = styled.nav`
     background-color: var(--color-black);
@@ -79,102 +80,96 @@ const NavRightContainer = styled.div`
         }
     }}
 `
+const handleScroll = (show, hide) => {
+    const show_height = 400
+    window.scrollY > show_height ? show() : hide()
+}
 
-class Nav extends React.Component {
-    state = {
-        enable_move: false,
-        show_modal: false,
-        active_tab: '',
-    }
+const Nav = () => {
+    const [show_modal, toggleModal, closeModal] = useModal()
+    const [show_button, showButton, hideButton] = moveButton()
 
-    toggleModal = e => {
-        e.stopPropagation()
-        this.setState({
-            show_modal: !this.state.show_modal,
-        })
-    }
-
-    closeModal = () => {
-        this.setState({
-            show_modal: false,
-        })
-    }
-    handleScroll = () => {
-        window.scrollY > 400
-            ? this.setState({ enable_move: true })
-            : this.setState({ enable_move: false })
-    }
-    componentDidMount() {
-        document.addEventListener('scroll', this.handleScroll, false)
-    }
-    componentWillUnmount() {
-        document.removeEventListener('scroll', this.handleScroll, false)
-    }
-    render() {
-        return (
-            <StyledNav>
-                <Wrapper>
-                    <NavLeft>
-                        <LocalizedLink to="/" aria-label={localize('Home')}>
-                            <LogoHeader />
-                        </LocalizedLink>
-                    </NavLeft>
-                    <NavCenter>
-                        <NavLink>
-                            <StyledLink
-                                activeClassName="active"
-                                to="https://deriv.app"
-                                external_link
-                                aria-label={localize('Trade')}
-                            >
-                                {localize('Trade')}
-                            </StyledLink>
-                        </NavLink>
-                        <NavLink margin>
-                            <StyledLink
-                                activeClassName="active"
-                                to="/about/"
-                                aria-label={localize('About us')}
-                            >
-                                {localize('About us')}
-                            </StyledLink>
-                        </NavLink>
-                        <NavLink>
-                            <StyledLink
-                                activeClassName="active"
-                                to="/help-centre/"
-                                aria-label={localize('Help centre')}
-                            >
-                                {localize('Help centre')}
-                            </StyledLink>
-                        </NavLink>
-                    </NavCenter>
-                    <NavRight>
-                        <NavRightContainer enable_move={this.state.enable_move}>
-                            <NavButton primary>
-                                <span>{localize('Login')}</span>
-                            </NavButton>
-                            <NavButton
-                                secondary
-                                movable_button
-                                onClick={this.toggleModal}
-                            >
-                                <span>{localize('Try for free')}</span>
-                            </NavButton>
-                        </NavRightContainer>
-                    </NavRight>
-                </Wrapper>
-                <Modal
-                    toggle={this.toggleModal}
-                    is_open={this.state.show_modal}
-                    is_blurred={true}
-                    closeModal={this.closeModal}
-                >
-                    <SignupModal />
-                </Modal>
-            </StyledNav>
+    useEffect(() => {
+        document.addEventListener('scroll', () =>
+            handleScroll(showButton, hideButton),
         )
-    }
+        return () => {
+            document.removeEventListener('scroll', () =>
+                handleScroll(showButton, hideButton),
+            )
+        }
+    }, [])
+    return (
+        <StyledNav>
+            <Wrapper>
+                <NavLeft>
+                    <LocalizedLink to="/" aria-label={localize('Home')}>
+                        <LogoHeader />
+                    </LocalizedLink>
+                </NavLeft>
+                <NavCenter>
+                    <NavLink>
+                        <StyledLink
+                            activeClassName="active"
+                            to={DERIV_APP_URL}
+                            external_link
+                            aria-label={localize('Trade')}
+                        >
+                            {localize('Trade')}
+                        </StyledLink>
+                    </NavLink>
+                    <NavLink margin>
+                        <StyledLink
+                            activeClassName="active"
+                            to="/about/"
+                            aria-label={localize('About us')}
+                        >
+                            {localize('About us')}
+                        </StyledLink>
+                    </NavLink>
+                    <NavLink>
+                        <StyledLink
+                            activeClassName="active"
+                            to="/help-centre/"
+                            aria-label={localize('Help centre')}
+                        >
+                            {localize('Help centre')}
+                        </StyledLink>
+                    </NavLink>
+                </NavCenter>
+                <NavRight>
+                    <NavRightContainer enable_move={show_button}>
+                        <NavButton primary>
+                            <span>{localize('Login')}</span>
+                        </NavButton>
+                        <NavButton
+                            secondary
+                            movable_button
+                            onClick={toggleModal}
+                        >
+                            <span>{localize('Try for free')}</span>
+                        </NavButton>
+                    </NavRightContainer>
+                </NavRight>
+            </Wrapper>
+            <Modal
+                toggle={toggleModal}
+                is_open={show_modal}
+                is_blurred={true}
+                closeModal={closeModal}
+            >
+                <SignupModal />
+            </Modal>
+        </StyledNav>
+    )
 }
 
 export default Nav
+
+function moveButton(is_visible = false) {
+    const [show_button, setShowButton] = useState(is_visible)
+    const showButton = () => setShowButton(!show_button)
+    const hideButton = () => setShowButton(false)
+
+    return [show_button, showButton, hideButton]
+}

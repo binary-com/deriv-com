246146423b5ee19c045ea86582f596e0e4542583
arash <arash@firstsource.io>
2019-07-30 15:23:08 +0430

    about us hero resoponsive

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -7,16 +7,27 @@ import Hero from 'components/elements/hero.js'
 import { OurLocations } from './_our-locations'
 import { OurNumbers } from './_our-numbers'
 import { OurGoals } from './_our-goals'
+import { MobileOnly, LaptopOnly } from '../../components/containers/device-only'
 
 const About = () => (
     <Layout>
         <SEO title={localize('About us')} />
-        <Hero
-            header={localize('About us')}
-            paragraph={localize(
-                'Deriv is a new trading platform created by the Binary Group, a multi-award winning pioneer in online trading. It’s built upon 20 years of experience, customer focus, and technical innovation. With our powerful yet simple trading experience and tools, new and professional traders alike can understand risk and make better trading decisions.',
-            )}
-        />
+        <LaptopOnly>
+            <Hero
+                header={localize('About us')}
+                paragraph={localize(
+                    'Deriv is a new trading platform created by the Binary Group, a multi-award winning pioneer in online trading. It’s built upon 20 years of experience, customer focus, and technical innovation. With our powerful yet simple trading experience and tools, new and professional traders alike can understand risk and make better trading decisions.',
+                )}
+            />
+        </LaptopOnly>
+        <MobileOnly>
+            <Hero
+                header={localize('About us')}
+                paragraph={localize(
+                    'Deriv is the latest innovation by the Binary Group, powered by 20 years of experience, customer focus, and technical innovation.',
+                )}
+            />
+        </MobileOnly>
         <OurGoals />
         <OurNumbers />
         <OurLocations />

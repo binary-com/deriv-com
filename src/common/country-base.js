86a0f53d6d538adbebc6177ae7598ab5868962d6
mustofa-binary <mustofa@regentmarkets.com>
2019-08-05 10:26:50 +0800

    use cookies instead

--- src/common/country-base.js
+++ src/common/country-base.js
@@ -1,20 +1,33 @@
-import { State } from './storage'
+const eu_countries = [
+    'it',
+    'de',
+    'fr',
+    'lu',
+    'gr',
+    'mf',
+    'es',
+    'sk',
+    'lt',
+    'nl',
+    'at',
+    'bg',
+    'si',
+    'cy',
+    'be',
+    'ro',
+    'hr',
+    'pt',
+    'pl',
+    'lv',
+    'ee',
+    'cz',
+    'fi',
+    'hu',
+    'dk',
+    'se',
+    'ie',
+    'im',
+    'gb',
+]
 
-// needs to wait for website_status before being called
-// 'mt' is part of EU but account opening is not offered so the landing company response won't include the expected shortcode.
-export const isEuCountry = () => {
-    const eu_shortcode_regex = new RegExp('^(maltainvest|malta|iom)$')
-    const eu_excluded_regex = new RegExp('^mt$')
-    const financial_shortcode = State.getResponse(
-        'landing_company.financial_company.shortcode',
-    )
-    const gaming_shortcode = State.getResponse(
-        'landing_company.gaming_company.shortcode',
-    )
-    const clients_country = State.getResponse('website_status.clients_country')
-
-    return financial_shortcode || gaming_shortcode
-        ? eu_shortcode_regex.test(financial_shortcode) ||
-              eu_shortcode_regex.test(gaming_shortcode)
-        : eu_excluded_regex.test(clients_country)
-}
+export const isEuCountry = country_code => eu_countries.includes(country_code)
--- src/components/containers/show.js
+++ src/components/containers/show.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+import Cookies from 'js-cookie'
 import { isEuCountry } from 'common/country-base'
 import { BinarySocketBase } from 'common/websocket/socket_base'
 
@@ -7,27 +8,46 @@ const Show = ({ children, to, device }) => {
     const [invisible, setInvisible] = useState(true)
 
     useEffect(() => {
-        BinarySocketBase.wait('website_status', 'landing_company').then(() => {
-            switch (to) {
-                case 'eu':
-                    setInvisible(isEuCountry())
+        if (to) {
+            const country_code = Cookies.get('country_code')
+            let is_eu_country = false
+
+            if (country_code) {
+                is_eu_country = isEuCountry(country_code)
+                switchCaseEu(is_eu_country)
+            } else {
+                BinarySocketBase.wait('website_status').then(response => {
+                    is_eu_country = isEuCountry(
+                        response.website_status.country_code,
+                    )
+                    switchCaseEu(is_eu_country)
+                })
+            }
+
+            const switchCaseEu = is_eu_country => {
+                switch (to) {
+                    case 'eu':
+                        setInvisible(is_eu_country)
+                        break
+                    case 'non-eu':
+                        setInvisible(!is_eu_country)
+                        break
+                    default:
+                        break
+                }
+            }
+        }
+        if (device) {
+            switch (device) {
+                case 'laptop':
+                    //TODO: add breakpoints for laptop > window.innerwidth here, and update setInvisible memoized state
                     break
-                case 'non-eu':
-                    setInvisible(!isEuCountry())
+                case 'mobile':
+                    //TODO: add breakpoints for mobile > window.innerwidth here, and update setInvisible memoized state
                     break
                 default:
                     break
             }
-        })
-        switch (device) {
-            case 'laptop':
-                //TODO: add breakpoints for laptop > window.innerwidth here, and update setInvisible memoized state
-                break
-            case 'mobile':
-                //TODO: add breakpoints for mobile > window.innerwidth here, and update setInvisible memoized state
-                break
-            default:
-                break
         }
     })
     return !invisible ? <>{children}</> : null
@@ -38,7 +58,7 @@ Show.propTypes = {
         PropTypes.arrayOf(PropTypes.node),
         PropTypes.node,
     ]).isRequired,
-    device: PropTypes.oneOf(['mobile', 'laptop'])
+    device: PropTypes.oneOf(['mobile', 'laptop']),
     to: PropTypes.oneOf(['eu', 'non-eu']),
 }
 export default Show

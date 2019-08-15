64c181055dafa3544504b0a0195b33fcc1e996db
arash <arash@firstsource.io>
2019-08-15 11:43:26 +0430

    remove eu and none eu

--- src/components/containers/show.js
+++ src/components/containers/show.js
@@ -1,49 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import Cookies from 'js-cookie'
-import { isEuCountry } from 'common/country-base'
-import { BinarySocketBase } from 'common/websocket/socket_base'
 import { size } from 'themes/device.js'
 
-const handleEu = (setVisible, to) => is_eu_country => {
-    switch (to) {
-        case 'eu':
-            setVisible(is_eu_country)
-            break
-        case 'non-eu':
-            setVisible(!is_eu_country)
-            break
-        case 'global':
-            setVisible(true)
-            break
-        default:
-            break
-    }
-}
-
-const Show = ({ children, to = 'global', device }) => {
-    const [visible, setVisible] = useState(false)
-    const [deviceVisibility, setdeviceVisibility] = useState(true)
+const Show = ({ children, device }) => {
+    const [deviceVisibility, setdeviceVisibility] = useState(false)
     useEffect(() => {
-        if (to) {
-            const clients_country = Cookies.get('clients_country')
-            const showEu = handleEu(setVisible, to)
-            if (clients_country) {
-                isEuCountry(clients_country)
-                showEu(isEuCountry(clients_country))
-            } else {
-                BinarySocketBase.wait('website_status').then(response => {
-                    showEu(isEuCountry(response.website_status.clients_country))
-
-                    /* country_code cookies will be valid for 1 month */
-                    Cookies.set(
-                        'clients_country',
-                        response.website_status.clients_country,
-                        { expires: 30 },
-                    )
-                })
-            }
-        }
         if (device) {
             switch (device) {
                 case 'laptop':
@@ -61,7 +22,7 @@ const Show = ({ children, to = 'global', device }) => {
             }
         }
     })
-    return visible && deviceVisibility ? <>{children}</> : null
+    return deviceVisibility ? <>{children}</> : null
 }
 
 Show.propTypes = {

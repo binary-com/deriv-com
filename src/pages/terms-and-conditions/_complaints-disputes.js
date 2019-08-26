8bdc7fe7258d8a4b25c751fcbce0dd7a413726fc
mustofa-binary <mustofa@regentmarkets.com>
2019-08-26 14:12:27 +0800

    add Localize

--- src/pages/terms-and-conditions/_complaints-disputes.js
+++ src/pages/terms-and-conditions/_complaints-disputes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyledSection, StyledOrder, TncListText } from './_styles'
 import { Header } from 'components/elements/typography'
-import { localize } from 'components/localization'
+import { localize, Localize } from 'components/localization'
 
 export const ComplaintsAndDisputes = () => (
     <StyledSection>
@@ -11,9 +11,17 @@ export const ComplaintsAndDisputes = () => (
         <StyledOrder type="decimal">
             <li>
                 <TncListText>
-                    {localize(
-                        'If you would like to file a complaint with regards to Our services, you can contact Us at complaints@deriv.com. You need to provide any relevant details relating to your complaint. We will investigate your enquiry and respond to you. We usually provide a final response within 15 working days from the day that We receive all relevant details.',
-                    )}
+                    <Localize
+                        translate_text="If you would like to file a complaint with regards to Our services, you can contact Us at <0>complaints@deriv.com</0>. You need to provide any relevant details relating to your complaint. We will investigate your enquiry and respond to you. We usually provide a final response within 15 working days from the day that We receive all relevant details."
+                        component={[
+                            <a
+                                key={0}
+                                href="mailto:complaints@deriv.com"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            />,
+                        ]}
+                    />
                 </TncListText>
             </li>
         </StyledOrder>

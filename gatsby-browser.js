08e3058dd2842c0f507a88572ba2c5cac801db48
mustofa-binary <mustofa@regentmarkets.com>
2019-08-27 11:20:45 +0800

    track update service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -26,4 +26,14 @@ export const onInitialClientRender = () => {
     }
 }
 
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+        `This application has been updated. ` +
+            `Reload to display the latest version?`,
+    )
+    if (answer === true) {
+        window.location.reload()
+    }
+}
+
 export const wrapPageElement = WrapPagesWithLocaleContext

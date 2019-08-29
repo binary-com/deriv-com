1a0c98772370ce716ed9f147cbc3f8a349927c61
mustofa-binary <mustofa@regentmarkets.com>
2019-08-29 15:21:54 +0800

    add directives plugin csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,8 +95,9 @@ module.exports = {
                 mergeStyleHashes: true, // you can disable styles sha256 hashes
                 mergeDefaultDirectives: true,
                 directives: {
-                    'script-src': "'self' www.google-analytics.com",
-                    'style-src': "'self' 'unsafe-inline'",
+                    'script-src':
+                        "'self' 'unsafe-inline' www.google-analytics.com d33wubrfki0l68.cloudfront.net cdn.trackjs.com www.googletagmanager.com cdn.crowdin.com",
+                    'style-src': "'self' 'unsafe-inline' cdn.crowdin.com ",
                     'img-src': "'self' data: www.google-analytics.com",
                     // you can add your directives or override defaults
                 },

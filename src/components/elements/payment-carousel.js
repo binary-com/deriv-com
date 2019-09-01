c41079e445a80fd52c78c9711c21e5bce95ee9a5
arash <arash@firstsource.io>
2019-09-01 13:12:27 +0430

    add new payment icons

---
+++ src/components/elements/payment-carousel.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+class PaymentCarousel extends React.Component {
+    render() {
+        return(
+            <div>
+                this is payment carousel
+            </div>
+        )
+    }
+}
+
+export default PaymentCarousel

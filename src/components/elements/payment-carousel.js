0be98d2cc85444d6429b8520b4e31c0b47a857ee
arash <arash@firstsource.io>
2019-09-01 15:14:30 +0430

    add aria-labelledby to paymen icons

--- src/components/elements/payment-carousel.js
+++ src/components/elements/payment-carousel.js
@@ -1,5 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import WebMoney from 'images/svg/payment-webmoney.svg'
+import Sticpay from 'images/svg/payment-sticpay.svg'
+import Banktransfer from 'images/svg/payment-bank-transfer.svg'
+import Qiwi from 'images/svg/payment-qiwi.svg'
+import Yandex from 'images/svg/payment-yandex.svg'
+import Mastercard from 'images/svg/payment-mastercard.svg'
+import Skrill from 'images/svg/payment-skrill.svg'
+import PerfectMoney from 'images/svg/payment-perfect-money.svg'
+import Fasapay from 'images/svg/payment-fasapay.svg'
+import Payscale from 'images/svg/payment-payscale.svg'
+import Netller from 'images/svg/payment-neteller.svg'
+import Maestro from 'images/svg/payment-maestro.svg'
+import InternetBankTransfer from 'images/svg/payment-internet-bank-transfer.svg'
+import Visa from 'images/svg/payment-visa.svg'
+import IWallet from 'images/svg/payment-i-wallet.svg'
+import Paytrust from 'images/svg/payment-paytrust.svg'
+import Jeton from 'images/svg/payment-jeton.svg'
 
 class PaymentCarousel extends React.Component {
     render() {

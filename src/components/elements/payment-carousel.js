f09e2fd8ed7af2145148fbd4711b1feb99f209f2
arash <arash@firstsource.io>
2019-09-03 10:15:52 +0430

    clean up payment icons

--- src/components/elements/payment-carousel.js
+++ src/components/elements/payment-carousel.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import Container, { SectionContainer } from 'components/containers/container'
 import WebMoney from 'images/svg/payment-webmoney.svg'
 import Sticpay from 'images/svg/payment-sticpay.svg'
 import Banktransfer from 'images/svg/payment-bank-transfer.svg'
@@ -18,14 +19,47 @@ import IWallet from 'images/svg/payment-i-wallet.svg'
 import Paytrust from 'images/svg/payment-paytrust.svg'
 import Jeton from 'images/svg/payment-jeton.svg'
 
-class PaymentCarousel extends React.Component {
-    render() {
-        return(
-            <div>
-                this is payment carousel
-            </div>
-        )
-    }
+const PaymentSection = styled(SectionContainer)``
+const CarouselContainer = styled(Container)`
+    overflow: hidden;
+    justify-content: flex-start;
+`
+const PaymentWrapper = styled.div`
+    width: 96px;
+    margin-right: 3.2rem;
+`
+const PaymentCarousel = () => {
+    const paymentArray = [
+        IWallet,
+        Jeton,
+        InternetBankTransfer,
+        WebMoney,
+        Mastercard,
+        Visa,
+        Paytrust,
+        Banktransfer,
+        Netller,
+        Skrill,
+        Qiwi,
+        Yandex,
+        PerfectMoney,
+        Maestro,
+        Sticpay,
+        Fasapay,
+        Payscale,
+    ]
+
+    return (
+        <PaymentSection>
+            <CarouselContainer>
+                {paymentArray.map((Payment, index) => (
+                    <PaymentWrapper key={index}>
+                        <Payment />
+                    </PaymentWrapper>
+                ))}
+            </CarouselContainer>
+        </PaymentSection>
+    )
 }
 
 export default PaymentCarousel

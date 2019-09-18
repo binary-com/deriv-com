9d8877caf45c344189165b53632e73d891651926
arash <arash@firstsource.io>
2019-09-18 18:33:03 +0430

    format style

--- src/components/elements/dot_loader.js
+++ src/components/elements/dot_loader.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
 
-const dots = keyframes` 
-0%, 20% {
-    color: rgba(0,0,0,0);
-    text-shadow:
-      .25em 0 0 rgba(0,0,0,0),
-      .5em 0 0 rgba(0,0,0,0);}
-  40% {
-    color: grey;
-    text-shadow:
-      .25em 0 0 rgba(0,0,0,0),
-      .5em 0 0 rgba(0,0,0,0);}
-  60% {
-    text-shadow:
-      .25em 0 0 grey,
-      .5em 0 0 rgba(0,0,0,0);}
-  80%, 100% {
-    text-shadow:
-      .25em 0 0 grey,
-      .5em 0 0 grey;}`
+const dots = keyframes`
+    0%, 20% {
+        color: rgba(0,0,0,0);
+        text-shadow:
+            0.25em 0 0 rgba(0,0,0,0),
+            0.5em 0 0 rgba(0,0,0,0);
+    }
+    40% {
+        color: grey;
+        text-shadow:
+            0.25em 0 0 rgba(0,0,0,0),
+            0.5em 0 0 rgba(0,0,0,0);
+    }
+    60% {
+        text-shadow:
+            0.25em 0 0 grey,
+            0.5em 0 0 rgba(0,0,0,0);
+    }
+    80%, 100% {
+        text-shadow:
+            0.25em 0 0 grey,
+            0.5em 0 0 grey;
+    }
+`
 const InitialLoader = styled.span`
     text-align: center;
+
     ::after {
         color: grey;
         content: ' .';

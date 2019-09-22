0fe5ee15724717c945baac22004ca185a7819a92
arash <arash@firstsource.io>
2019-09-22 09:51:57 +0330

    add translate to parent of items in auto carousel

--- src/components/elements/auto-carousel.js
+++ src/components/elements/auto-carousel.js
@@ -4,24 +4,32 @@ import PropTypes from 'prop-types'
 
 const AutoCarouselSection = styled.section`
     width: ${props => props.width};
-    display: flex;
-    justify-content: flex-start;
     overflow: hidden;
     margin: 0 auto;
+    display: flex;
+    justify-content: flex-start;
 `
-const move_items = count_child => keyframes`
+const move_items = total_translate => keyframes`
     0% {
         transform: translateX(0);
     }
     100% {
-        transform: translateX(-${count_child}%);
+        transform: translateX(-${total_translate}px);
     }
 `
 const ItemContainer = styled.div`
     padding: 0 ${props => props.padding / 2}px;
+`
+const ItemsWrapper = styled.div`
+    width: ${props => props.total_translate};
+    display: flex;
+    justify-content: flex-start;
+    margin: 0 auto;
     animation-name: ${props =>
-        props.should_carousel_move ? move_items(props.count_child * 100) : ''};
-    animation-duration: ${props => props.transition_duration}ms;
+        props.total_translate !== 0 && props.should_carousel_move
+            ? move_items(props.total_translate)
+            : ''};
+    animation-duration: 12000ms;
     animation-fill-mode: both;
     animation-iteration-count: infinite;
     animation-timing-function: linear;
@@ -38,6 +46,7 @@ class AutoCarousel extends React.PureComponent {
             items: [],
             carousel_width: 0,
             should_carousel_move: false,
+            total_translate: 0,
         }
     }
     // every time you observe this, the carousel will restart from first child component
@@ -56,17 +65,22 @@ class AutoCarousel extends React.PureComponent {
         }
     }
     componentDidMount() {
+        let total_translate = 0
+        let child
+        for (child of this.my_ref.current.children[0].children) {
+            total_translate = total_translate + child.offsetWidth
+        }
+        this.setState({ total_translate: total_translate })
         let observer = new IntersectionObserver(this.handler)
         observer.observe(this.my_ref.current)
     }
+
     static getDerivedStateFromProps(props, state) {
         if (state.items.length === 0) {
-            const newItems = [...props.children, ...props.children].map(
-                (item, index) => ({
-                    Component: item,
-                    key: index,
-                }),
-            )
+            const newItems = props.children.map((item, index) => ({
+                Component: item,
+                key: index,
+            }))
             return { items: newItems }
         }
         return null
@@ -78,26 +92,36 @@ class AutoCarousel extends React.PureComponent {
                     width={this.props.carousel_width}
                     ref={this.my_ref}
                 >
-                    {this.state.items !== []
-                        ? this.state.items.map(({ Component, key }) => {
-                              return (
-                                  <ItemContainer
-                                      should_carousel_move={
-                                          this.state.should_carousel_move
-                                      }
-                                      key={key}
-                                      padding={this.props.items_padding}
-                                      transition={this.state.transition}
-                                      transition_duration={
-                                          this.props.transition_duration
-                                      }
-                                      count_child={this.props.children.length}
-                                  >
-                                      {Component}
-                                  </ItemContainer>
-                              )
-                          })
-                        : null}
+                    {/* We need to render this wrapper two times to fill the empty space at the end of the Carousel's section */}
+                    {[0, 1].map(i => (
+                        <ItemsWrapper
+                            key={i}
+                            total_translate={this.state.total_translate}
+                            should_carousel_move={
+                                this.state.should_carousel_move
+                            }
+                        >
+                            {this.state.items !== []
+                                ? this.state.items.map(({ Component, key }) => {
+                                      return (
+                                          <ItemContainer
+                                              key={key}
+                                              padding={this.props.items_padding}
+                                              transition={this.state.transition}
+                                              transition_duration={
+                                                  this.props.transition_duration
+                                              }
+                                              count_child={
+                                                  this.props.children.length
+                                              }
+                                          >
+                                              {Component}
+                                          </ItemContainer>
+                                      )
+                                  })
+                                : null}
+                        </ItemsWrapper>
+                    ))}
                 </AutoCarouselSection>
             </>
         )

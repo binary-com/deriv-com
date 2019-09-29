cd79955cd80d7d8c732e6ec04bcaa7eb083759e0
arash <arash@firstsource.io>
2019-09-29 19:12:16 +0330

    make dhero

---
+++ src/components/elements/dhero.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import styled from 'styled-components'
+import { localize } from 'components/localization'
+import Container from 'components/containers/container.js'
+import device from 'themes/device.js'
+import { Header } from 'components/elements/typography.js'
+import Show from 'components/containers/show'
+import Modal, { useModal } from 'components/elements/modal'
+import SignupModal from 'components/elements/signup-modal'
+import Image from 'components/elements/image'
+import Button from 'components/form/button.js'
+import dtrader_logo from 'images/svg/d-trader.svg'
+
+const HeroContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    max-width: 40rem;
+
+    ${Header} {
+        color: var(--color-white);
+        display: flex;
+        margin-top: 0.8rem;
+    }
+    h4:first-child {
+        margin-top: 0;
+    }
+`
+const Wrapper = styled.div`
+    background-color: var(--color-black);
+    width: 100%;
+    padding: 7.2rem 0;
+    display: flex;
+    flex-direction: column;
+`
+const LogoWrapper = styled.div`
+    margin-right: 1.6rem;
+`
+const TryForFree = styled(Button)`
+    border: 2px solid var(--color-red);
+    width: 12rem;
+    font-weight: bold;
+    line-height: 1.43;
+    margin-top: 1.22rem;
+
+    @media ${device.tabletL} {
+        width: 100%;
+        margin-top: 18.5rem;
+    }
+`
+const openBot = () => {
+    window.open('https://www.binary.bot/', '_blank')
+}
+const DHero = ({
+    Logo,
+    title,
+    message,
+    img_name,
+    img_alt,
+    button_text,
+    clickRedirect,
+}) => {
+    const [show_modal, toggleModal, closeModal] = useModal()
+    return (
+        <Wrapper>
+            <Container justify="space-between">
+                <HeroContent>
+                    <Header as="h4" weight={500}>
+                        <LogoWrapper>
+                            <Logo />
+                        </LogoWrapper>
+                        {title}
+                    </Header>
+                    <Header as="h1">{message}</Header>
+                </HeroContent>
+                <Show.Desktop>
+                    <Image img_name={img_name} alt={img_alt} width="58.9rem" />
+                </Show.Desktop>
+            </Container>
+            <Container justify="flex-start">
+                {clickRedirect === 'signup modal' ? (
+                    <TryForFree secondary onClick={toggleModal}>
+                        {button_text}
+                    </TryForFree>
+                ) : (
+                    <TryForFree secondary onClick={openBot}>
+                        {button_text}
+                    </TryForFree>
+                )}
+            </Container>
+            <Modal
+                toggle={toggleModal}
+                is_open={show_modal}
+                closeModal={closeModal}
+            >
+                <SignupModal autofocus />
+            </Modal>
+        </Wrapper>
+    )
+}
+
+export default DHero
--- src/pages/dtrader/index.js
+++ src/pages/dtrader/index.js
@@ -1,20 +1,29 @@
 import React from 'react'
-import Hero from './_hero.js'
 import Numbers from './_numbers.js'
 import HowItWorks from './_how-it-works.js'
 import Trading from './_trading.js'
+import DHero from 'components/elements/DHero.js'
 import Experience from './_experience'
 import OtherPlatform from './_other-platform.js'
 import Show from 'components/containers/show'
 import SEO from 'components/containers/seo'
 import Layout from 'components/layout/layout'
 import { localize, WithIntl } from 'components/localization'
+import dtrader_logo from 'images/svg/d-trader.svg'
 
 const Dtrader = () => {
     return (
         <Layout>
             <SEO title={localize('Dtrader')} />
-            <Hero />
+            <DHero
+                Logo={dtrader_logo}
+                title={localize('Dtrader')}
+                message={localize('Everything you need and more')}
+                img_name="platform.png"
+                img_alt={localize('Platform')}
+                button_text={localize('Try for free')}
+                clickRedirect="signup modal"
+            />
             <Show.Desktop>
                 <Numbers />
             </Show.Desktop>

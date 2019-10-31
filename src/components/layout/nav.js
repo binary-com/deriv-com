bd887aa15de98b6b9d1074fa6136afc653fb46ef
oskar-binary <oskar@binary.com>
2019-10-31 11:27:24 +0800

    fix navbutton

--- src/components/layout/nav.js
+++ src/components/layout/nav.js
@@ -97,7 +97,7 @@ const NavRight = styled.div`
                     const calculation = props.button_ref.current.offsetWidth + 2
                     return `${calculation}px`
                 }
-                return '20rem'
+                return '250px'
             }
         }}
     );

837c637f333f8ce85beb80acf7584c178623b529
arash <arash@firstsource.io>
2019-12-01 13:25:42 +0330

    change dmt5 hero layout

--- src/pages/dmt5/_hero.js
+++ src/pages/dmt5/_hero.js
@@ -31,7 +31,7 @@ const StyledHeader = styled(Header)`
 const Wrapper = styled.div`
     background-color: var(--color-black);
     width: 100%;
-    padding: 7.2rem 0;
+    padding: 10rem 0;
     display: flex;
     flex-direction: column;
 `
@@ -42,39 +42,50 @@ const TryForFree = styled(Button)`
     border: 2px solid var(--color-red);
     font-weight: bold;
     line-height: 1.43;
-    margin-top: 3.2rem;
+    margin-top: 3.3rem;
 
     @media ${device.tabletL} {
         width: 100%;
         margin-top: 18.5rem;
     }
 `
-
+const StyledContainer = styled(Container)`
+    width: 55%;
+    justify-content: space-between;
+    align-items: flex-start;
+    flex-direction: column;
+`
+const ContentHolder = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+`
 const Hero = () => {
     const [show_modal, toggleModal, closeModal] = useModal()
     return (
         <Wrapper>
-            <Container align="flex-start">
-                <StyledHeader as="h4" weight={500}>
-                    <DMT5Logo />
-                    {localize('DMT5')}
-                </StyledHeader>
-            </Container>
-            <Container justify="space-between" align="flex-start">
-                <HeroContent>
-                    <Header as="h1">
-                        {localize('The all-in-one FX and CFD trading platform')}
-                    </Header>
-                </HeroContent>
-                <Show.Desktop>
-                    <Image img_name="dmt5-platform.png" alt="DMT5 platform" width="58.9rem" />
-                </Show.Desktop>
-            </Container>
-            <Container justify="flex-start">
+            <StyledContainer>
+                <div>
+                    <StyledHeader as="h4" weight={500}>
+                        <DMT5Logo />
+                        {localize('DMT5')}
+                    </StyledHeader>
+                </div>
+                <ContentHolder>
+                    <HeroContent>
+                        <Header as="h1">
+                            {localize('The all-in-one FX and CFD trading platform')}
+                        </Header>
+                    </HeroContent>
+                    <Show.Desktop>
+                        <Image img_name="dmt5-platform.png" alt="DMT5 platform" width="58.9rem" />
+                    </Show.Desktop>
+                </ContentHolder>
                 <TryForFree secondary onClick={toggleModal}>
-                    {localize('Try for free')}
+                    {localize('Join us for free')}
                 </TryForFree>
-            </Container>
+            </StyledContainer>
             <Modal toggle={toggleModal} is_open={show_modal} closeModal={closeModal}>
                 <SignupModal autofocus />
             </Modal>

b67f57ff45b88412b604bd43afe5336aa39c3e7d
arash <arash@firstsource.io>
2019-12-08 18:00:57 +0330

    replace dmt5 hero with dhero

--- src/pages/dmt5/index.js
+++ src/pages/dmt5/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Hero from './_hero.js'
+import DMT5Animation from './dmt5-animation.json'
 import Numbers from './_numbers.js'
 import HowItWorks from './_how-it-works.js'
 import Experience from './_experience.js'
 import Trading from './_trading.js'
+import DHero from 'components/custom/_dhero.js'
 import Layout from 'components/layout/layout'
+import dmt5_logo from 'images/svg/dmt5-icon.svg'
 import { OtherPlatform } from 'components/custom/other-platforms.js'
 import { SEO, Show } from 'components/containers'
 import { localize, WithIntl } from 'components/localization'
@@ -13,7 +15,13 @@ const DMT5 = () => {
     return (
         <Layout>
             <SEO title={localize('DMT5')} />
-            <Hero />
+            <DHero
+                title={localize('DMT5')}
+                content={localize('The all-in-one FX and CFD trading platform')}
+                join_us_for_free
+                Logo={dmt5_logo}
+                animation={DMT5Animation}
+            />
             <Show.Desktop>
                 <Numbers />
             </Show.Desktop>

a30d275593a6556485eaf84c7a90f85896979b13
sara-fs <sara.khosropour@regentmarkets.com>
2019-12-12 10:31:18 +0330

    Changed the order of imports

--- src/components/elements/hero.js
+++ src/components/elements/hero.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 import { Header } from './typography.js'
 import { Container } from 'components/containers'
 

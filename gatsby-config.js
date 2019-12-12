07145507b4aa604cd02f2d65e5b06135af81d0b1
sara-fs <sara.khosropour@regentmarkets.com>
2019-12-12 11:44:15 +0330

    Changed the styled-component config

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
             resolve: 'gatsby-source-filesystem',
             options: {
                 name: 'images',
-                path: `${__dirname}/src/images/common`,
+                path: `${__dirname}/src/images`,
             },
         },
         'gatsby-transformer-sharp',

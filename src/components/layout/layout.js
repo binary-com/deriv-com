1887571cd4c3aa86295d2b2d84bf5002a0cc13be
sara-fs <sara.khosropour@regentmarkets.com>
2019-12-12 16:33:57 +0330

    Removed globalstyle from layout

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,42 +1,42 @@
 import React from 'react'
-import styled, { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import Variables from '../../themes/variables'
-import Reset from '../../themes/reset'
-import device from '../../themes/device'
+// import Variables from '../../themes/variables'
+// import Reset from '../../themes/reset'
+// import device from '../../themes/device'
 import Footer from './footer'
 import { Nav, NavStatic } from './nav'
 
-const GlobalStyle = createGlobalStyle`
-    ${Reset}
-    ${Variables}
+// const GlobalStyle = createGlobalStyle`
+//     ${Reset}
+//     ${Variables}
 
-    * {
-        &, &::before, &::after {
-            box-sizing: border-box;
-        }
-    }
-    html {
-        font-size: 62.5%; /* 1rem = 10px */
-        box-sizing: border-box;
+//     * {
+//         &, &::before, &::after {
+//             box-sizing: border-box;
+//         }
+//     }
+//     html {
+//         font-size: 62.5%; /* 1rem = 10px */
+//         box-sizing: border-box;
 
-        @media ${device.desktopL} {
-            font-size: 75.5%; /* 1rem = 12px */
-        }
-        @media ${device.laptop} {
-            font-size: 58%; /* 1rem = 9px */
-        }
-        @media ${device.tabletL} {
-            font-size: 50%; /* 1rem = 8px */
-        }
-    }
-    body {
-        font-family: 'IBM Plex Sans', sans-serif;
-        margin: 0;
-        padding: 0;
-        background: var(--color-green);
-    }
-`
+//         @media ${device.desktopL} {
+//             font-size: 75.5%; /* 1rem = 12px */
+//         }
+//         @media ${device.laptop} {
+//             font-size: 58%; /* 1rem = 9px */
+//         }
+//         @media ${device.tabletL} {
+//             font-size: 50%; /* 1rem = 8px */
+//         }
+//     }
+//     body {
+//         font-family: 'IBM Plex Sans', sans-serif;
+//         margin: 0;
+//         padding: 0;
+//         background: var(--color-green);
+//     }
+// `
 
 const Main = styled.main`
     padding-top: 7rem;
@@ -48,7 +48,7 @@ const Layout = ({ children, is_static }) => (
     <>
         {is_static ? <NavStatic /> : <Nav />}
         <Main is_static={is_static}>
-            <GlobalStyle />
+            {/* <GlobalStyle /> */}
             {children}
         </Main>
         {!is_static && <Footer />}

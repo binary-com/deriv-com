872c97613d65d5c396301f000748cc1444275cbe
Sash <sash@binary.com>
2019-12-17 13:37:33 +0200

    refactor keepsafe icon grid

--- src/pages/keep-safe/_icon-grid.js
+++ src/pages/keep-safe/_icon-grid.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import device from 'themes/device'
+import { GridContainer, CssGrid, CssGridColumn } from 'components/containers'
 import { Text } from 'components/elements'
 import { localize } from 'components/localization'
 // Icons
@@ -12,35 +13,12 @@ import Configure from 'images/svg/cloudfare-dns.svg'
 import DontShare from 'images/svg/don-t-share-payment-methods.svg'
 import Monitor from 'images/svg/recent-logins.svg'
 import Antivirus from 'images/svg/install-latest-antivirus.svg'
-import Container from 'components/containers/container'
 
-const IconGrid = styled(Container)`
-    margin-top: 1rem;
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-column-gap: 3rem;
-    grid-row-gap: 4rem;
-
-    @media ${device.tabletL} {
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: auto;
-        grid-column-gap: 3rem;
-        grid-row-gap: 3rem;
-    }
-    @media ${device.tabletS} {
-        grid-template-columns: repeat(1, 1fr);
-        grid-template-rows: auto;
-        grid-column-gap: 3rem;
-        grid-row-gap: 6rem;
-        text-align: center;
-    }
-`
-const GridCol = styled.article`
-    height: 100%;
+const GridCol = styled(CssGridColumn)`
+    width: 100%;
 
     ${Text} {
         margin-top: 1.8rem;
-        font-size: 1.6rem;
 
         @media ${device.tabletL} {
             font-size: 2rem;
@@ -48,6 +26,9 @@ const GridCol = styled.article`
         @media ${device.tabletS} {
             text-align: center;
         }
+    }
+    @media ${device.tabletS} {
+        text-align: center;
     }
 `
 
@@ -65,7 +46,16 @@ Col.propTypes = {
 }
 
 export const SecurityIconGrid = () => (
-    <IconGrid>
+    <GridContainer>
+        <CssGrid
+            columns="repeat(4, 1fr)"
+            columngap="3rem"
+            rowgap="4rem"
+            tabletcolumns="repeat(2, 1fr)"
+            mobilecolumns="1fr"
+            mobilerowgap="6rem"
+            margin="1rem 0 0"
+        >
         <Col
             Icon={Two}
             content={localize('Enable two-factor authentication on your account.')}
@@ -106,5 +96,6 @@ export const SecurityIconGrid = () => (
                 'Install antivirus software (such as Avast Free Antivirus for Windows and Mac) on your computer.',
             )}
         />
-    </IconGrid>
+        </CssGrid>
+    </GridContainer>
 )

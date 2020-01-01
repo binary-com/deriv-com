949c48a6c69bab0c677e2943a7b4425e7e3b028d
arash <arash@firstsource.io>
2020-01-01 14:49:03 +0330

    adjust footer layout

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -6,21 +6,24 @@ import { Container, CssGrid } from '../containers'
 import { Text, StyledLink } from '../elements'
 import { isProduction } from 'common/websocket/config'
 // Icons
-import Logo from 'images/svg/deriv.svg'
+import Logo from 'images/svg/deriv-footer.svg'
 import Twitter from 'images/svg/twitter.svg'
 import Telegram from 'images/svg/telegram.svg'
 import Facebook from 'images/svg/social-facebook.svg'
 
-const FooterWrapper = styled.section`
+const StyledFooter = styled.section`
     background-color: var(--color-white);
     width: 100%;
     margin: 0 auto;
 `
-const StyledContainer = styled.div`
+const Wrapper = styled.div`
     background-color: var(--color-grey-8);
     max-width: 1440px;
     padding: 4rem 0;
     margin: 0 auto;
+`
+const StyledContainer = styled(Container)`
+    width: 100%;
 `
 const StyledGrid = styled(CssGrid)`
     grid-template-areas: 'info info info . . items items items items items items items';
@@ -30,10 +33,10 @@ const InfoSection = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
+    width: 100%;
 
     ${Text} {
         margin-top: 2.2rem;
-        max-width: 22.9rem;
     }
 `
 const Items = styled.div`
@@ -44,8 +47,7 @@ const Items = styled.div`
     justify-content: space-between;
 `
 const Col = styled.div`
-    width: 100%;
-    max-width: 20.4rem;
+    width: ${props => props.width};
     ${props => props.margin_top ? 'margin-top: 3.9rem;' : ''}
 
     div {
@@ -77,12 +79,12 @@ const Link = styled(StyledLink)`
     line-height: 1.5;
 `
 const Footer = () => (
-    <FooterWrapper>
-        <StyledContainer>
-            <Container>
+    <StyledFooter>
+        <Wrapper>
+            <StyledContainer>
                 <StyledGrid columns='repeat(12, 1fr)' columngap='2.4rem' rowgap='3.9rem'>
                     <InfoSection>
-                        <Logo />
+                        <Logo width='14.5rem' />
                         <Text>{localize('Deriv is a new trading platform created by the Binary Group, a multi-award winning pioneer in online trading.')}</Text>
                         <SocialMeida>
                             <Text>{localize('CONNECT WITH US')}</Text>
@@ -94,41 +96,41 @@ const Footer = () => (
                         </SocialMeida>
                     </InfoSection>
                     <Items>
-                        <Col>
+                        <Col width='20%'>
                             <div><Title>{localize('TRADE')}</Title></div>
                             <div><Link to='/dtrader'>{localize('DTrader')}</Link></div>
                             <div><Link to='/dbot'>{localize('DBot')}</Link></div>
                             <div><Link to='/dmt5'>{localize('DMT5')}</Link></div>
                         </Col>
-                        <Col>
+                        <Col width='40%'>
                             <div><Title>{localize('LEGAL')}</Title></div>
                             <div><Link to='/regulatory'>{localize('Regulatory information')}</Link></div>
                             <div><Link to='/terms-and-conditions'>{localize('Terms and conditions')}</Link></div>
                             <div><Link to='/responsible-trading'>{localize('Responsible trading')}</Link></div>
                         </Col>
-                        <Col>
+                        <Col width='25%'>
                             <div><Title>{localize('SUPPORT')}</Title></div>
                             <div><Link to='/help-centre'>{localize('Help centre')}</Link></div>
                             <div><Link to='/payment'>{localize('Payment methods')}</Link></div>
                             <div><Link to='/keep-safe'>{localize('Keep safe')}</Link></div>
                             <div><Link to='/why-choose-us'>{localize('Why choose us')}</Link></div>
                         </Col>
-                        <Col margin_top>
+                        <Col margin_top width='20%'>
                             <div><Title>{localize('COMPANY')}</Title></div>
                             <div><Link to='/about-us'>{localize('About us')}</Link></div>
                         </Col>
-                        <Col margin_top>
+                        <Col margin_top width='40%'>
                             <div><Title>{localize('PARTNER WITH US')}</Title></div>
                             <div><Link to='/affiliate'>{localize('Affiliate and IB programmes')}</Link></div>
                         </Col>
-                        <Col margin_top>
+                        <Col margin_top width='25%'>
                             {!isProduction() && <LanguageSwitcher />}
                         </Col>
                     </Items>
                 </StyledGrid>
-            </Container>
-        </StyledContainer>
-    </FooterWrapper>
+            </StyledContainer>
+        </Wrapper>
+    </StyledFooter>
 )
 
 export default Footer

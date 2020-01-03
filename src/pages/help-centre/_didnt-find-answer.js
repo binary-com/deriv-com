7f5ab7fb1353eefc01a3244465760c31a1ef7e08
oskar-binary <oskar@binary.com>
2020-01-03 16:40:25 +0800

    make contact us banner responsive for mobile

--- src/pages/help-centre/_didnt-find-answer.js
+++ src/pages/help-centre/_didnt-find-answer.js
@@ -4,6 +4,7 @@ import { StyledLink } from 'components/elements'
 import { localize } from 'components/localization'
 import { FlexGridContainer } from 'components/containers'
 import ContactUsIcon from 'images/svg/contact_us.svg'
+import device from 'themes/device'
 
 const DFYAWrapper = styled.section`
     background-color: var(--color-grey-1);
@@ -16,12 +17,24 @@ const DFYASection = styled(FlexGridContainer)`
     & > * {
         width: auto;
     }
+
+    @media ${device.tabletL} {
+        ${StyledLink} {
+            font-size: var(--text-size-s);
+        }
+    }
+`
+const StyledIcon = styled(ContactUsIcon)`
+    @media ${device.tabletL} {
+        width: 30px;
+        height: 30px;
+    }
 `
 
 export const DidntFindYourAnswerBanner = () => (
     <DFYAWrapper>
         <DFYASection>
-            <ContactUsIcon />
+            <StyledIcon />
             <StyledLink
                 size="var(--text-size-header-1)"
                 to="/contact-us"

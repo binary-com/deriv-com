313485da56796316c7f1686b480a3c6a952fe3f6
mustofa binary <mustofa@regentmarkets.com>
2020-01-06 16:21:03 +0800

    responsive desktop

--- src/pages/about/_our-history.js
+++ src/pages/about/_our-history.js
@@ -5,22 +5,23 @@ import 'pure-react-carousel/dist/react-carousel.es.css'
 import { Wrapper, Container } from 'components/containers'
 import { Header, Image, Text } from 'components/elements'
 import { localize } from 'components/localization'
-import Chevron from 'images/svg/fill-chevron.svg'
+import ChevronRight from 'images/svg/fill-chevron.svg'
 import Binary from 'images/svg/binary-blur.svg'
 import Deriv from 'images/svg/deriv-blur.svg'
+import device from 'themes/device'
 
 const SliderWrapper = styled.div`
     width: 100%;
     position: relative;
     padding-bottom: 6rem;
+
+    @media ${device.laptopLC} {
+        padding-bottom: 0;
+    }
 `
 
 const StyledContainer = styled(Container)`
     position: relative;
-`
-
-const ChevronRight = styled(Chevron)`
-    cursor: pointer;
 `
 
 const ChevronLeft = styled(ChevronRight)`
@@ -76,10 +77,14 @@ const StyledDotsWrapper = styled.ul`
         right: 0;
         bottom: 0.8rem;
     }
+
+    @media ${device.laptopLC} {
+        display: none;
+    }
 `
 
 const StyledDot = styled.li`
-    margin: 0 3rem;
+    margin: 0 2.9rem;
     position: relative;
 
     & .carousel__dot {
@@ -124,33 +129,192 @@ const ButtonWrapper = styled.div`
         position: absolute;
         top: 40%;
         left: 0;
+        cursor: pointer;
 
         &[disabled] {
             opacity: 0.32;
+            cursor: default;
         }
     }
     & .carousel__next-button {
         position: absolute;
         top: 40%;
         right: 0;
+        cursor: pointer;
 
         &[disabled] {
             opacity: 0.32;
+            cursor: default;
         }
     }
 `
 
 const StyledBinary = styled(Binary)`
     position: absolute;
-    left: -12.6rem;
+    left: -15.6rem;
     bottom: 4rem;
+
+    @media ${device.laptopLC} {
+        display: none;
+    }
 `
 
 const StyledDeriv = styled(Deriv)`
     position: absolute;
-    right: -12.6rem;
+    right: -15.6rem;
     bottom: 4rem;
+
+    @media ${device.laptopLC} {
+        display: none;
+    }
 `
+
+const history_data = [
+    {
+        year: '1990',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '1993',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '1999',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2000',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2001',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2004',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2007',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2008',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2009',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2011',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2012',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2013',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2014',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2015',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2016',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2017',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2018',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+    {
+        year: '2019',
+        title: localize('An award-winning young entrepreneur'),
+        description: localize(
+            'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
+        ),
+        image: 'jean-yves.png',
+    },
+]
 
 const OurHistory = () => {
     return (
@@ -163,45 +327,22 @@ const OurHistory = () => {
                     <CarouselProvider
                         naturalSlideWidth={100}
                         naturalSlideHeight={50}
-                        totalSlides={2}
+                        totalSlides={history_data.length}
                     >
                         <Slider style={{ width: '80%', margin: '0 auto' }}>
-                            <Slide index={0}>
-                                <StyledSlide>
-                                    <Wrapper width="90rem" margin={{ right: '2.4rem' }}>
-                                        <Image img_name="jean-yves.png" />
-                                    </Wrapper>
-
-                                    <StyledSlideText>
-                                        <StyledHeader as="h3">
-                                            {localize('An award-winning young entrepreneur')}
-                                        </StyledHeader>
-                                        <Text>
-                                            {localize(
-                                                'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
-                                            )}
-                                        </Text>
-                                    </StyledSlideText>
-                                </StyledSlide>
-                            </Slide>
-                            <Slide index={1}>
-                                <StyledSlide>
-                                    <Wrapper width="90rem" margin={{ right: '2.4rem' }}>
-                                        <Image img_name="jean-yves.png" />
-                                    </Wrapper>
-
-                                    <StyledSlideText>
-                                        <StyledHeader as="h3">
-                                            {localize('An award-winning young entrepreneur')}
-                                        </StyledHeader>
-                                        <Text>
-                                            {localize(
-                                                'It all started with the man behind Binary.com and Deriv: founder and CEO Jean-Yves Sireau. At 19, he created his own technical analysis software that won him the Jacques Douce Prize for young entrepreneurs by the French Prime Minister. Inspired by this, he decided to pursue his dream of starting his own company.',
-                                            )}
-                                        </Text>
-                                    </StyledSlideText>
-                                </StyledSlide>
-                            </Slide>
+                            {history_data.map((history, index) => (
+                                <Slide key={index} index={index}>
+                                    <StyledSlide>
+                                        <Wrapper width="90rem" margin={{ right: '2.4rem' }}>
+                                            <Image img_name={history.image} />
+                                        </Wrapper>
+                                        <StyledSlideText>
+                                            <StyledHeader as="h3">{history.title}</StyledHeader>
+                                            <Text>{history.description}</Text>
+                                        </StyledSlideText>
+                                    </StyledSlide>
+                                </Slide>
+                            ))}
                         </Slider>
                         <ButtonWrapper>
                             <ButtonBack>
@@ -212,16 +353,13 @@ const OurHistory = () => {
                             </ButtonNext>
                         </ButtonWrapper>
                         <StyledDotsWrapper>
-                            <StyledDot>
-                                <Dot slide={0}>
-                                    <StyledYear>1993</StyledYear>
-                                </Dot>
-                            </StyledDot>
-                            <StyledDot>
-                                <Dot slide={1}>
-                                    <StyledYear>1994</StyledYear>
-                                </Dot>
-                            </StyledDot>
+                            {history_data.map((history, index) => (
+                                <StyledDot key={index}>
+                                    <Dot slide={index}>
+                                        <StyledYear>{history.year}</StyledYear>
+                                    </Dot>
+                                </StyledDot>
+                            ))}
                         </StyledDotsWrapper>
                     </CarouselProvider>
                 </SliderWrapper>

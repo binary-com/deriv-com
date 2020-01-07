d4fda208aadfc04d2ebc8f5b0fddab52e7e14e79
oskar-binary <oskar@binary.com>
2020-01-07 14:57:55 +0800

    add remaining regulatory content (excluding pdfs)

--- src/components/containers/flex.js
+++ src/components/containers/flex.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 const Flex = styled.div`
     display: flex;
     flex-wrap: wrap;
-    width: 100%;
     width: ${props => props.width || '100%'};
     justify-content: ${props => (props.jc ? props.jc : 'center')};
     align-items: ${props => (props.ai ? props.ai : '')};
--- src/pages/regulatory/_document_accordion.js
+++ src/pages/regulatory/_document_accordion.js
@@ -1,8 +1,85 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Text, Accordion, AccordionItem, LinkText } from 'components/elements'
-import { CssGrid, Flex } from 'components/containers'
+import { Flex } from 'components/containers'
 import { localize } from 'components/localization'
+import PDFIcon from 'images/svg/pdf-icon.svg'
+
+const IconContainer = styled(Flex)`
+    justify-content: flex-start;
+    align-items: center;
+    min-width: ${props => props.minWidth || ''};
+
+    svg {
+        margin-right: 0.8rem;
+    }
+`
+
+// TODO: pass pdf ref as prop once it's ready
+const UpDown = () => (
+    <Flex mt="1.8rem" spacing="8rem" jc="space-evenly">
+        <IconContainer width="unset">
+            <PDFIcon />
+            <LinkText color="red" weight="bold">
+                {localize('Open Up/Down document')}
+            </LinkText>
+        </IconContainer>
+        <IconContainer width="unset">
+            <PDFIcon />
+            <LinkText color="red" weight="bold">
+                {localize('Open Touch/No Touch document')}
+            </LinkText>
+        </IconContainer>
+    </Flex>
+)
+
+const QuarterHeader = styled(Text)`
+    padding-bottom: 1.6rem;
+`
+
+// TODO: pass pdf ref as prop once it's ready
+const QuarterTable = ({ quarters }) => (
+    <div style={{ padding: '0.8rem' }}>
+        {quarters.map((quarter, idx) => (
+            <div
+                key={quarter}
+                style={{ paddingBottom: idx === quarters.length - 1 ? '' : '1.6rem' }}
+            >
+                <QuarterHeader align="center">{quarter}</QuarterHeader>
+                <Flex jc="flex-start">
+                    <IconContainer width="unset" minWidth="46%">
+                        <PDFIcon />
+                        <LinkText color="red" weight="bold">
+                            {localize('Table 1 - Type of execution venue')}
+                        </LinkText>
+                    </IconContainer>
+                    <IconContainer width="unset">
+                        <PDFIcon />
+                        <LinkText color="red" weight="bold">
+                            {localize('Table 4 - Daily Price information')}
+                        </LinkText>
+                    </IconContainer>
+                    <IconContainer width="unset" minWidth="46%">
+                        <PDFIcon />
+                        <LinkText color="red" weight="bold">
+                            {localize('Table 3 - Intra-Day Price information')}
+                        </LinkText>
+                    </IconContainer>
+                    <IconContainer width="unset">
+                        <PDFIcon />
+                        <LinkText color="red" weight="bold">
+                            {localize('Table 6 - Likelihood of execution information')}
+                        </LinkText>
+                    </IconContainer>
+                </Flex>
+            </div>
+        ))}
+    </div>
+)
+QuarterTable.propTypes = {
+    quarters: PropTypes.array,
+}
 
 const DocumentAccordion = () => {
     return (
@@ -15,9 +92,12 @@ const DocumentAccordion = () => {
                         )}
                     </Text>
                     <Flex mt="1.8rem">
-                        <LinkText color="red" weight="bold">
-                            {localize('Open pillar 3 disclosure report')}
-                        </LinkText>
+                        <IconContainer width="unset">
+                            <PDFIcon />
+                            <LinkText color="red" weight="bold">
+                                {localize('Open pillar 3 disclosure report')}
+                            </LinkText>
+                        </IconContainer>
                     </Flex>
                 </AccordionItem>
                 <AccordionItem header={localize('Key information documents')}>
@@ -27,15 +107,24 @@ const DocumentAccordion = () => {
                         )}
                     </Text>
                     <Flex mt="1.8rem" spacing="8rem" jc="space-evenly">
-                        <LinkText color="red" weight="bold">
-                            {localize('Commodities')}
-                        </LinkText>
-                        <LinkText color="red" weight="bold">
-                            {localize('Forex')}
-                        </LinkText>
-                        <LinkText color="red" weight="bold">
-                            {localize('Cryptocurrency')}
-                        </LinkText>
+                        <IconContainer width="unset">
+                            <PDFIcon />
+                            <LinkText color="red" weight="bold">
+                                {localize('Commodities')}
+                            </LinkText>
+                        </IconContainer>
+                        <IconContainer width="unset">
+                            <PDFIcon />
+                            <LinkText color="red" weight="bold">
+                                {localize('Forex')}
+                            </LinkText>
+                        </IconContainer>
+                        <IconContainer width="unset">
+                            <PDFIcon />
+                            <LinkText color="red" weight="bold">
+                                {localize('Cryptocurrency')}
+                            </LinkText>
+                        </IconContainer>
                     </Flex>
                 </AccordionItem>
                 <AccordionItem header="RTS 28">
@@ -44,69 +133,10 @@ const DocumentAccordion = () => {
                             'These documents provide you with key information about our investment products. This information is required by law to help you understand the nature, risks, costs, potential gains and losses of these products and to help you compare them with other products.',
                         )}
                     </Text>
-                    <Flex mt="1.8rem" spacing="8rem" jc="space-evenly">
-                        <LinkText color="red" weight="bold">
-                            {localize('Open Up/Down document')}
-                        </LinkText>
-                        <LinkText color="red" weight="bold">
-                            {localize('Open Touch/No Touch document')}
-                        </LinkText>
-                    </Flex>
+                    <UpDown />
                 </AccordionItem>
                 <AccordionItem header="RTS 27">
-                    <Text align="center">{localize('Q1 2019')}</Text>
-                    <div style={{ padding: '16px' }}>
-                        <CssGrid columns="repeat(2, 1fr)">
-                            <LinkText color="red" weight="bold">
-                                {localize('Table 1 - Type of execution venue')}
-                            </LinkText>
-                            <LinkText color="red" weight="bold">
-                                {localize('Table 4 - Daily Price information')}
-                            </LinkText>
-                        </CssGrid>
-                        <CssGrid columns="repeat(2, 1fr)">
-                            <LinkText color="red" weight="bold">
-                                {localize('Table 3 - Intra-Day Price information')}
-                            </LinkText>
-                            <LinkText color="red" weight="bold">
-                                {localize('Table 6 - Likelihood of execution information')}
-                            </LinkText>
-                        </CssGrid>
-                        <Text align="center">{localize('Q2 2019')}</Text>
-                        <CssGrid columns="repeat(2, 1fr)">
-                            <LinkText color="red" weight="bold">
-                                {localize('Table 1 - Type of execution venue')}
-                            </LinkText>
-                            <LinkText color="red" weight="bold">
-                                {localize('Table 4 - Daily Price information')}
-                            </LinkText>
-                        </CssGrid>
-                        <CssGrid columns="repeat(2, 1fr)">
-                            <LinkText color="red" weight="bold">
-                                {localize('Table 3 - Intra-Day Price information')}
-                            </LinkText>
-                            <LinkText color="red" weight="bold">
-                                {localize('Table 6 - Likelihood of execution information')}
-                            </LinkText>
-                        </CssGrid>
-                        <Text align="center">{localize('Q3 2019')}</Text>
-                        <CssGrid columns="repeat(2, 1fr)">
-                            <LinkText color="red" weight="bold">
-                                {localize('Table 1 - Type of execution venue')}
-                            </LinkText>
-                            <LinkText color="red" weight="bold">
-                                {localize('Table 4 - Daily Price information')}
-                            </LinkText>
-                        </CssGrid>
-                        <CssGrid columns="repeat(2, 1fr)">
-                            <LinkText color="red" weight="bold">
-                                {localize('Table 3 - Intra-Day Price information')}
-                            </LinkText>
-                            <LinkText color="red" weight="bold">
-                                {localize('Table 6 - Likelihood of execution information')}
-                            </LinkText>
-                        </CssGrid>
-                    </div>
+                    <QuarterTable quarters={['Q1 2019', 'Q2 2019', 'Q3 2019']} />
                 </AccordionItem>
                 <AccordionItem header="RTS 28">
                     <Text>
@@ -114,46 +144,10 @@ const DocumentAccordion = () => {
                             'These documents provide you with key information about our investment products. This information is required by law to help you understand the nature, risks, costs, potential gains and losses of these products and to help you compare them with other products.',
                         )}
                     </Text>
-                    <Flex mt="1.8rem" spacing="8rem" jc="space-evenly">
-                        <LinkText color="red" weight="bold">
-                            {localize('Open Up/Down document')}
-                        </LinkText>
-                        <LinkText color="red" weight="bold">
-                            {localize('Open Touch/No Touch document')}
-                        </LinkText>
-                    </Flex>
+                    <UpDown />
                 </AccordionItem>
                 <AccordionItem header="RTS 27 2018">
-                    <Text align="center">{localize('Q3 2019')}</Text>
-                    <Flex mt="1.8rem" spacing="8rem" jc="space-evenly">
-                        <LinkText color="red" weight="bold">
-                            {localize('Table 1 - Type of execution venue')}
-                        </LinkText>
-                        <LinkText color="red" weight="bold">
-                            {localize('Table 4 - Daily Price information')}
-                        </LinkText>
-                        <LinkText color="red" weight="bold">
-                            {localize('Table 3 - Intra-Day Price information')}
-                        </LinkText>
-                        <LinkText color="red" weight="bold">
-                            {localize('Table 6 - Likelihood of execution information')}
-                        </LinkText>
-                    </Flex>
-                    <Text align="center">{localize('Q4 2019')}</Text>
-                    <Flex mt="1.8rem" spacing="8rem" jc="space-evenly">
-                        <LinkText color="red" weight="bold">
-                            {localize('Table 1 - Type of execution venue')}
-                        </LinkText>
-                        <LinkText color="red" weight="bold">
-                            {localize('Table 4 - Daily Price information')}
-                        </LinkText>
-                        <LinkText color="red" weight="bold">
-                            {localize('Table 3 - Intra-Day Price information')}
-                        </LinkText>
-                        <LinkText color="red" weight="bold">
-                            {localize('Table 6 - Likelihood of execution information')}
-                        </LinkText>
-                    </Flex>
+                    <QuarterTable quarters={['Q3 2018', 'Q4 2019']} />
                 </AccordionItem>
             </Accordion>
         </div>

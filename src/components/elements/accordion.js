db56a12becd5deab8f4eb9a28dd2993628a424d9
oskar-binary <oskar@binary.com>
2020-01-09 12:14:51 +0800

    fix style

--- src/components/elements/accordion.js
+++ src/components/elements/accordion.js
@@ -29,11 +29,11 @@ const AccordionContentWrapper = styled.div`
 `
 
 const AccordionWrapper = styled.div`
-    margin-bottom: 16px;
+    margin-bottom: 1.6rem;
     width: 100%;
     border-radius: 6px;
     box-shadow: 0 16px 20px 0 rgba(0, 0, 0, 0.1);
-    background-color: #ffffff;
+    background-color: var(--color-white);
 `
 const TRANSITION_DURATION = 400
 

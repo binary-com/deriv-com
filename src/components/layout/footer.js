f102874ecba4553f6d357a26ee86e44dfd4b5979
Mohammadreza Ghorbani <reza@firstsource.io>
2020-01-14 16:34:34 +0330

    Update copyright

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -336,7 +336,7 @@ const Footer = () => (
             <StyledContainer justify="flex-start">
                 <StyledText>
                     <Copyright width="1.6rem" />
-                    {localize('2019 Deriv | All right reserved')}
+                    {localize('2020 Deriv | All right reserved')}
                 </StyledText>
             </StyledContainer>
         </BlackNav>

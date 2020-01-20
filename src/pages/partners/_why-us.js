de48813daca23c12fae5e01a05be9a7ee1120b48
arash <arash@firstsource.io>
2020-01-20 11:08:09 +0330

    add why us

---
+++ src/pages/partners/_why-us.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { SectionContainer } from 'components/containers'
+import { Header, Text, Image } from 'components/elements'
+import { localize } from 'components/localization'
+import device from 'themes/device'
+
+const Row = styled(SectionContainer)`
+    display: flex;
+    flex-direction: ${props => props.flex_direction};
+    justify-content: space-between;
+    padding: 4rem 0;
+
+    div {
+        width: 100%;
+        max-width: 58.8rem;
+    }
+
+    @media ${device.tabletL} {
+        flex-direction: column;
+
+        div {
+            max-width: unset;
+        }
+    }
+`
+const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 4rem;
+
+    @media ${device.tabletL} {
+        ${Text, Header} {
+            text-align: center;
+        }
+    }
+`
+const WhyUs = ({ items }) => {
+    return (
+        <SectionContainer>
+            <Header font_size='3.6rem' align='center'>
+                {localize('Why partner with us')}
+            </Header>
+            {items.map((item, index) => {
+                let is_even = index % 2
+                return (
+                    <Row flex_direction={is_even ? 'row-reverse' : 'row'} key={index}>
+                        <Content>
+                            <Header font_size='2.8rem'>{item.title}</Header>
+                            <Text>{item.subtitle}</Text>
+                        </Content>
+                        <Image
+                            img_name={item.image_name}
+                            alt={item.image_alt}
+                            width="100%"
+                        />
+                    </Row>
+                )
+            })}
+        </SectionContainer>
+    )
+}
+WhyUs.propTypes = {
+    items: PropTypes.array
+}
+export default WhyUs
--- src/pages/partners/index.js
+++ src/pages/partners/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import WhyUs from './_why-us.js'
 import { Header } from 'components/elements/typography'
 import Layout from 'components/layout/layout'
 import { SectionContainer, Container } from 'components/containers'
@@ -12,6 +13,32 @@ const items = [
     { title: '$1M+', subtitle: localize('clients') },
 ]
 
+const why_partner_with_us_items = [
+    {
+        title: localize('Multiple income opportunities and generous commissions'),
+        subtitle: localize(
+            'Start off as an affiliate and get ensuing access to our IB programme. Earn up to 45% commission for as long as your referred clients keep trading.',
+        ),
+        image_name: 'multiple-income-opportunities.png',
+        image_alt: localize('Multiple income opportunities'),
+    },
+    {
+        title: localize('No charges. No hidden fees. Prompt monthly and daily payment.'),
+        subtitle: localize(
+            'All Deriv partnership programmes are free and always will be. Get your affiliate commissions paid to your method of choice every month and IB commissions paid to your DMT5 account daily.',
+        ),
+        image_name: 'daily-ib-commission.png',
+        image_alt: localize('Daily IB commission'),
+    },
+    {
+        title: localize('Optimise conversions with great user experience and creative support'),
+        subtitle: localize(
+            'We’ve designed the most customer-centric and intuitive trading experience for Deriv that’s optimised to convert visitors into clients. We’ll also provide you with all the tools and creative materials you need to drive traffic to Deriv.',
+        ),
+        image_name: 'marketing-material.png',
+        image_alt: localize('Marketing material'),
+    },
+]
 const Partners = () => {
     return (
         <Layout>
@@ -21,6 +48,7 @@ const Partners = () => {
                         {localize('Earn up to 45% lifetime commission with an online trading provider that enables anyone to trade on any financial market with the utmost convenience. Binary Group –– the owner of Binary.com and Deriv –– has a proven track record of running successful referral programmes with prompt payouts.')}
                     </Header>
                     <DNumber items={items} justify="space-around" />
+                    <WhyUs items={why_partner_with_us_items} />
                 </Container>
             </SectionContainer>
         </Layout>

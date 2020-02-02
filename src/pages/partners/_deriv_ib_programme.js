0cf11d86389b8549296f34cdb28c64e6ed18b6f2
arash <arash@firstsource.io>
2020-02-02 10:46:54 +0330

    add deriv ib section

--- src/pages/partners/_deriv_ib_programme.js
+++ src/pages/partners/_deriv_ib_programme.js
@@ -1,13 +1,44 @@
 import React from 'react'
-import { SectionContainer } from 'components/containers'
+import styled from 'styled-components'
+import { Card, CardWrapper } from './_partner-card.js'
+import { SectionContainer, Container } from 'components/containers'
 import { Header } from 'components/elements/typography'
 import { localize } from 'components/localization'
+import device from 'themes/device'
 
+const TitleWrapper = styled.div`
+    max-width: 104.5rem;
+    margin: 0 auto;
+`
+const StyledSection = styled(SectionContainer)`
+    padding-bottom: 0;    
+    background-color: var(--color-grey-8);
+`
+const StyledHeader = styled(Header)`
+    margin-bottom: 1.6rem;
+
+    @media ${device.tabletL} {
+        text-align: center;
+    }
+`
 const DerivIBProgramme = () => {
     return (
-        <SectionContainer>
-            <Header font_size='3.6rem' align='center'>{localize('Deriv IB Programme')}</Header>
-        </SectionContainer>
+        <StyledSection>
+            <Container direction='column'>
+                <TitleWrapper>
+                    <Header font_size='3.6rem' align='center'>{localize('Deriv IB Programme')}</Header>
+                    <Header as='h4' align='center' weight='500'>{localize('Our IB programme is available to all Deriv affiliates. Earn commission from your clients’ trades on DMT5.')}</Header>
+                </TitleWrapper>
+                <SectionContainer>
+                    <StyledHeader as='h4'>{localize('Choose a commission plan:')}</StyledHeader>
+                    <CardWrapper mt='-2rem' wrap='wrap'>
+                        <Card height='42rem'>
+                            test
+                        </Card>
+                    </CardWrapper>
+                </SectionContainer>
+            </Container>
+        </StyledSection>
     )
 }
 

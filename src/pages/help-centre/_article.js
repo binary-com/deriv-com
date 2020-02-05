0a8be9b77b31e0551205ba4c8b8ab21757fe4fae
sara-fs <sara.khosropour@regentmarkets.com>
2020-02-05 11:36:06 +0800

    Applied new design for contact-us section

--- src/pages/help-centre/_article.js
+++ src/pages/help-centre/_article.js
@@ -1,17 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import { DidntFindYourAnswerBanner } from './_didnt-find-answer'
 import Layout from 'components/layout/layout'
 import { localize } from 'components/localization'
-import { Header, SideTab, StyledLink } from 'components/elements'
+import { SideTab, StyledLink } from 'components/elements'
 import { Container } from 'components/containers'
 
-const ArticleSection = styled.section`
-    padding: 12.8rem 0 8rem 0;
-    margin-bottom: 4rem;
-    border-bottom: 1px solid var(--color-grey-8);
-`
 const Content = styled.div`
     display: flex;
     flex-direction: row;
@@ -26,18 +21,8 @@ const ContactContainer = styled.div`
     margin-top: 8rem;
 `
 export const Article = ({ children, header }) => {
-    const [active_tab_title, setactiveTabTitle] = useState('')
-
-    function setTabTitle(tab) {
-        if (tab) setactiveTabTitle(tab.props.text)
-    }
     return (
         <Layout>
-            {/* <ArticleSection>
-                <Container align="left" justify="flex-start" direction="column">
-                    <Header as="h1">{localize(active_tab_title)}</Header>
-                </Container>
-            </ArticleSection> */}
             <Container align="left" justify="flex-start" direction="column">
                 <StyledLink
                     to="/help-centre/"
@@ -52,7 +37,7 @@ export const Article = ({ children, header }) => {
                 </StyledLink>
                 <Content>
                     <TabWrapper>
-                        <SideTab has_hash_routing onTabChange={setTabTitle} tab_header={header}>
+                        <SideTab has_hash_routing tab_header={header}>
                             {children}
                         </SideTab>
                     </TabWrapper>

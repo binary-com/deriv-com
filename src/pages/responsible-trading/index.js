907bab33be600e9f466295c0a318ad540cd762e2
mustofa-binary <mustofa@binary.com>
2020-02-11 18:05:18 +0800

    add trading limits

--- src/pages/responsible-trading/index.js
+++ src/pages/responsible-trading/index.js
@@ -12,15 +12,6 @@ const Section = styled(SectionContainer)`
     background-color: ${props => props.bgcolor || 'transparent'};
 `
 const StyledHeader = styled(Header)`
-    a {
-        color: var(--color-red);
-        text-decoration: none;
-
-        :hover {
-            text-decoration: underline;
-        }
-    }
-
     max-width: ${props => props.maxwidth || '100%'};
     padding: ${props => props.padding || '0'};
 
@@ -31,39 +22,6 @@ const StyledHeader = styled(Header)`
 
 const WarningIcon = styled(Warning)`
     margin-bottom: 2.4rem;
-`
-
-const Ol = styled.ol`
-    list-style-type: decimal;
-    font-weight: bold;
-    padding-left: 2rem;
-
-    li {
-        padding-left: 0.3rem;
-    }
-    span {
-        font-weight: normal;
-    }
-    > *:not(:last-child) {
-        padding-bottom: 1rem;
-    }
-`
-
-const UL = styled.ul`
-    list-style-type: disc;
-    padding-left: 2rem;
-
-    li {
-        max-width: 56.8rem;
-    }
-    > *:not(:last-child) {
-        padding-bottom: 1.6rem;
-    }
-    @media ${device.laptop} {
-        li {
-            padding-bottom: 1.6rem;
-        }
-    }
 `
 
 const ListWrapper = styled.div`
@@ -152,96 +110,22 @@ const ResponsibleTrading = () => {
                     />
                 </ResponsibleText>
             </Section>
-            <Section>
-                <StyledHeader as="h2" font_size="3.6rem" lh="4.5rem" align="center">
-                    {localize('Written limits and self-exclusion')}
-                </StyledHeader>
+            <Section bgcolor="var(--color-grey-8)">
                 <Container direction="column">
                     <Flex m="4rem 0" jc="space-between" wrap="wrap">
                         <ListWrapper laptop_mb="4rem">
                             <StyledHeader
                                 maxwidth="48.6rem"
-                                font_size="var(--text-size-sm)"
-                                weight="bold"
-                                lh="1.5"
+                                font_size="var(--text-size-header-1)"
+                                align="center"
                             >
-                                <Localize
-                                    translate_text="With Deriv, you can self-exclude yourself or set limits on your trading activities through our site. You may also <0>contact us</0> to state the limits you wish to set."
-                                    components={[<LocalizedLink key={0} to="/contact-us/" />]}
-                                />
+                                <Localize translate_text="Trading limits and self-exclusion" />
                             </StyledHeader>
                         </ListWrapper>
-                        <ListWrapper pl="1rem">
-                            <Ol>
-                                <Text as="li" weight="bold">
-                                    <span>
-                                        {localize(
-                                            'Limit the amount of money you may trade within a specified period.',
-                                        )}
-                                    </span>
-                                </Text>
-                                <Text as="li" weight="bold">
-                                    <span>
-                                        {localize(
-                                            'Limit the losses you may incur within a specified period.',
-                                        )}
-                                    </span>
-                                </Text>
-                                <Text as="li" weight="bold">
-                                    <span>
-                                        {localize(
-                                            'Limit the amount of time you may trade in a session.',
-                                        )}
-                                    </span>
-                                </Text>
-                                <Text as="li" weight="bold">
-                                    <span>
-                                        {localize(
-                                            'Exclude yourself from trading on our website for a definite or indefinite period.',
-                                        )}
-                                    </span>
-                                </Text>
-                            </Ol>
-                        </ListWrapper>
-                    </Flex>
-                </Container>
-                <Container direction="column">
-                    <StyledHeader font_size="2.8rem" as="h3" align="center">
-                        {localize('How it works')}
-                    </StyledHeader>
-                    <Flex jc="space-between" wrap="wrap" mt="4rem">
                         <ListWrapper>
-                            <UL>
-                                <Text as="li">
-                                    {localize(
-                                        'The first three limits may only be removed or increased after 24 hours of receiving the notice. You must confirm that you wish to amend the limit.',
-                                    )}
-                                </Text>
-                                <Text as="li">
-                                    {localize(
-                                        'There’s a minimum period of six months for self-exclusion. You have the option to extend it to a total of five years immediately without any cooling-off period.',
-                                    )}
-                                </Text>
-                                <Text as="li">
-                                    {localize(
-                                        'When you’ve set your self-exclusion period, we will refund your account balance to you.',
-                                    )}
-                                </Text>
-                            </UL>
-                        </ListWrapper>
-                        <ListWrapper pl="1rem">
-                            <UL>
-                                <Text as="li">
-                                    {localize(
-                                        'At the end of the self-exclusion period, the self-exclusion will remain in place until you take action.',
-                                    )}
-                                </Text>
-                                <Text as="li">
-                                    {localize(
-                                        'If you do not wish to renew the self-exclusion and you make a request to trade again, there will be a cooling-off period of one day before you are allowed access to our site. Please note that email is insufficient and your request must be made by phone.',
-                                    )}
-                                </Text>
-                            </UL>
+                            <StyledHeader maxwidth="58.8rem" font_size="var(--text-size-sm)">
+                                <Localize translate_text="Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world " />
+                            </StyledHeader>
                         </ListWrapper>
                     </Flex>
                 </Container>

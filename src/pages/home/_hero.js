aaab45e49f3a4bd228121c824f61a9e8a488dc97
arash <arash@firstsource.io>
2020-02-19 18:11:32 +0330

    merge to dev

--- src/pages/home/_hero.js
+++ src/pages/home/_hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import PlatformVideoMp4 from './Deriv_platform_tour.mp4'
 import device from 'themes/device'
@@ -6,7 +7,12 @@ import { LinkButton, Button } from 'components/form'
 import { Container, Show, Flex } from 'components/containers'
 import { Header, Text } from 'components/elements'
 import { localize } from 'components/localization'
-import Checklist from 'images/svg/checklist.svg'
+import CheckMarkIcon from 'images/svg/checklist.svg'
+
+const CheckMark = styled(CheckMarkIcon)`
+    width: 18px;
+    height: 18px;
+`
 
 const HeroWrapper = styled.section`
     width: 100%;
@@ -25,8 +31,6 @@ const HeroWrapper = styled.section`
         background: unset;
         background-position: -20rem 100%;
         background-color: var(--color-black);
-        min-height: 58.4rem;
-        padding: 13.3rem 0 5rem 0;
     }
     @media ${device.tablet} {
         background-position: -40rem 100%;
@@ -35,21 +39,32 @@ const HeroWrapper = styled.section`
 
 const HeroHeader = styled(Header)`
     margin-top: ${props => (props.secondary ? '1.6rem' : '')};
+    margin-bottom: 3.2rem;
+    font-size: 4.8rem;
 
-    @media ${device.laptop} {
-        text-align: center;
-    }
     @media ${device.tabletL} {
         text-align: left;
+    }
+    @media ${device.desktopL} {
+        font-size: 6.72rem;
+    }
+`
+
+const HeroSubHeader = styled(Header)`
+    margin-bottom: 1.4rem;
+
+    @media ${device.desktopL} {
+        font-size: 3.36rem;
     }
 `
 
 const StyledArticle = styled.article`
     position: relative;
     z-index: 2;
+    margin-left: 18%;
 
-    ${HeroHeader} {
-        width: 100%;
+    @media ${device.tabletL} {
+        margin: 0 2rem;
     }
 `
 
@@ -67,7 +82,7 @@ const HeroGrid = styled.section`
 `
 
 const ButtonWrapper = styled(Flex)`
-    margin-top: 4rem;
+    margin-top: 6rem;
     height: 40px;
 
     ${Button} {
@@ -88,7 +103,7 @@ const StyledVideo = styled.video`
     left: 50%;
     transform: translate(-50%, -50%);
     width: 100%;
-    height: auto;
+    height: 100%;
 
     /* for edge */
     @supports (object-fit: fill) {
@@ -103,64 +118,78 @@ const HeroButton = styled(LinkButton)`
     display: flex;
     align-items: center;
 `
-const MobileItems = styled(Flex)`
-    div {
-        display: flex;
-        align-items: center;
 
-        svg:first-child {
-            margin-right: 1.25rem;
-        }
-    }
-    p {
-        color: var(--color-white);
-    }
-`
-const MobileArticle = styled(Flex)`
-    padding: 0 2rem;
-    
-    h1 {
-        margin: 0 auto;
-        color: var(--color-white);
-    }
-    div {
-        margin-top: 2rem;
-    }
-    div:first-child {
-        margin-top: 0
+const CheckBoxText = styled(Text)`
+    margin-left: 16px;
+    font-size: 1.8rem;
+    font-weight: 500;
+    line-height: 2;
+
+    @media ${device.desktopL} {
+        font-size: 2.52rem;
     }
 `
-const ChecklistSVG = styled(Checklist)`
-    height: 2.25rem;
-    width: 2.25rem;
+const CheckMarkBullet = ({ children }) => (
+    <li>
+        <Flex jc="unset" ai="center">
+            <CheckMark />
+            <CheckBoxText secondary="true" color="white">
+                {children}
+            </CheckBoxText>
+        </Flex>
+    </li>
+)
+CheckMarkBullet.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+}
+
+const UList = styled.ul`
+    list-style-type: none;
 `
-const SignupButton = styled(LinkButton)`
-    margin: 22.5rem auto 0 auto;
-    width: 100%;
-    max-width: 27rem;
-    font-size: var(--text-size-sm);
-`
+
+const CheckMarkList = ({ children }) => <UList>{children}</UList>
+CheckMarkList.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+}
+
 export const Hero = () => {
     return (
         <HeroWrapper>
+            <Container>
+                <HeroGrid>
+                    <StyledArticle>
+                        <HeroHeader as="h1" color="white" lh="1.15">
+                            {localize('Simple. Flexible. Reliable.')}
+                        </HeroHeader>
+                        <HeroSubHeader
+                            as="h2"
+                            color="white"
+                            lh="1.5"
+                            font_size="var(--text-size-m)"
+                            weight="500"
+                        >
+                            {localize('Trade forex, commodities, stock and synthetic indices')}
+                        </HeroSubHeader>
+                        <CheckMarkList>
+                            <CheckMarkBullet>
+                                {localize('Built upon over 20 years of experience')}
+                            </CheckMarkBullet>
+                            <CheckMarkBullet>
+                                {localize('Over 100+ tradable instruments')}
+                            </CheckMarkBullet>
+                            <CheckMarkBullet>
+                                {localize('24x7 trading, tight prices, low spreads')}
+                            </CheckMarkBullet>
+                        </CheckMarkList>
+                        <ButtonWrapper jc="unset">
+                            <HeroButton type="submit" secondary="true" to="/signup/">
+                                {localize('Join us for free')}
+                            </HeroButton>
+                        </ButtonWrapper>
+                    </StyledArticle>
+                </HeroGrid>
+            </Container>
             <Show.Desktop>
-                <Container>
-                    <HeroGrid>
-                        <StyledArticle>
-                            <HeroHeader align="center" as="h1" color="white" lh="1.2">
-                                {localize('This is your ultimate trading experience')}
-                            </HeroHeader>
-                            <HeroHeader as="h4" align="center" color="white" weight="500" secondary>
-                                {localize('The next evolution of online trading by Binary.com')}
-                            </HeroHeader>
-                            <ButtonWrapper>
-                                <HeroButton type="submit" secondary to="/signup/">
-                                    {localize('Join us for free')}
-                                </HeroButton>
-                            </ButtonWrapper>
-                        </StyledArticle>
-                    </HeroGrid>
-                </Container>
                 <StyledVideo
                     title={localize('deriv.app platform video')}
                     width="100%"
@@ -173,33 +202,6 @@ export const Hero = () => {
                     <source src={PlatformVideoMp4} type="video/mp4" />
                 </StyledVideo>
             </Show.Desktop>
-            <Show.Mobile>
-                <MobileArticle direction='column'>
-                    <div>
-                        <Header font_size='var(--text-size-l)'>{localize('Simple. Flexible. Reliable.')}</Header>
-                    </div>
-                    <div>
-                        <Header font_size='2.25rem' weight='normal'>{localize('Trade forex, commodities, synthetic indices, and cryptocurrencies')}</Header>
-                    </div>
-                    <MobileItems direction='column'>
-                        <div>
-                            <ChecklistSVG />
-                            <Text secondary>{localize('Built upon over 20 years of experience')}</Text>
-                        </div>
-                        <div>
-                            <ChecklistSVG />
-                            <Text secondary>{localize('Over 100+ tradable instruments')}</Text>
-                        </div>
-                        <div>
-                            <ChecklistSVG />
-                            <Text secondary>{localize('24x7 trading, tight prices, low spreads')}</Text>
-                        </div>
-                    </MobileItems>
-                    <SignupButton secondary to="/signup/">
-                        {localize('Join us for free')}
-                    </SignupButton>
-                </MobileArticle>
-            </Show.Mobile>
         </HeroWrapper>
     )
 }

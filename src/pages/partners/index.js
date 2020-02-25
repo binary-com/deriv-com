915ffbe42aca269d4897357d22a9970cd3b2ff56
mustofa-binary <mustofa@binary.com>
2020-02-25 16:40:18 +0800

    change to deriv.com

--- src/pages/partners/index.js
+++ src/pages/partners/index.js
@@ -54,7 +54,7 @@ const Partners = () => {
                 <Container direction="column">
                     <Header as="h4" weight="500" align="center" lh="1.5">
                         {localize(
-                            'Earn up to 45% lifetime commission with an online trading provider that enables anyone to trade on several popular financial markets with the utmost convenience. Binary Group – the owner of Binary.com and Deriv – has a proven track record of running successful referral programmes with prompt payouts.',
+                            'Earn up to 45% lifetime commission with an online trading provider that enables anyone to trade on several popular financial markets with the utmost convenience. Binary Group – the owner of Binary.com and Deriv.com – has a proven track record of running successful referral programmes with prompt payouts.',
                         )}
                     </Header>
                     <DNumber items={items} justify="space-around" />

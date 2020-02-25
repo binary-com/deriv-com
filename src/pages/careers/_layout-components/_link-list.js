85774a26526aae2fb796531e55b062bf125d7c75
oskar-binary <oskar@binary.com>
2020-02-25 16:53:47 +0800

    update structure

---
+++ src/pages/careers/_layout-components/_link-list.js
@@ -0,0 +1,4 @@
+import React from 'react'
+// import styled from 'styled-components'
+
+export const LinkList = () => <div>LinkList</div>
--- src/pages/careers/_open-positions/_position-data.js
+++ src/pages/careers/_open-positions/_position-data.js
@@ -1,3 +1,3 @@
 import { accounts_and_payments_positions } from './_accounts-and-payments'
 
-export const positions = [accounts_and_payments_positions]
+export const positions = [...accounts_and_payments_positions]

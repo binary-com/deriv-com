0e7a9edbae84c1a5c893737aaf54948d3e9b913d
oskar-binary <oskar@binary.com>
2020-02-25 16:31:41 +0800

    refine structure

---
+++ src/pages/careers/_layout-components/_banner.js
@@ -0,0 +1,4 @@
+import React from 'react'
+// import styled from 'styled-components'
+
+export const Banner = () => <div>Banner</div>
---
+++ src/pages/careers/_open-positions/_position-data.js
@@ -0,0 +1,3 @@
+import { accounts_and_payments_positions } from './_accounts-and-payments'
+
+export const positions = [accounts_and_payments_positions]

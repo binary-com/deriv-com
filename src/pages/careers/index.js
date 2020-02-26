ad0e634a17955250f6aea5bfcea3477c5cb788aa
mustofa-binary <mustofa@binary.com>
2020-02-26 14:00:03 +0800

    hero complete

--- src/pages/careers/index.js
+++ src/pages/careers/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Hero from './_hero'
 import { SEO } from 'components/containers'
 import Layout from 'components/layout/layout'
 import { localize, WithIntl } from 'components/localization'
 
 const Careers = () => (
-    <Layout type="careers">
+    <Layout type="careers" padding_top="10rem">
         <SEO title={localize('Careers')} />
+        <Hero />
         <h1>Careers</h1>
         <h1>Careers</h1>
         <h1>Careers</h1>
--- src/pages/careers/jobs/index.js
+++ src/pages/careers/jobs/index.js
@@ -5,7 +5,7 @@ import Layout from 'components/layout/layout'
 import { localize, WithIntl } from 'components/localization'
 
 const Jobs = () => (
-    <Layout type="careers">
+    <Layout type="careers" padding_top="10rem">
         <SEO title={localize('Jobs')} />
         <h1>Jobs</h1>
         <Banner />

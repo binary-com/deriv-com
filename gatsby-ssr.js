4721216dee4b30e6f91eea10790ed1e340b16143
sara-fs <sara.khosropour@regentmarkets.com>
2020-02-26 09:32:20 +0330

    Added language links to header of all html pages

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,16 @@
+import React from 'react'
+const language_config = require(`./i18n-config.js`)
 import { WrapPagesWithLocaleContext } from './src/components/localization'
 import './src/components/localization/config'
 
 export const wrapPageElement = WrapPagesWithLocaleContext
+
+export function onRenderBody({ setHeadComponents }) {
+    const languageLinks = []
+
+    Object.keys(language_config).map(lang => {
+        languageLinks.push(<link rel="alternate" href="http://deriv.com/about" hrefLang={lang} />)
+    })
+   
+    setHeadComponents(languageLinks)
+}

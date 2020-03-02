f8ad578f936d91f63bb425a9f11556b842341176
sara-fs <sara.khosropour@regentmarkets.com>
2020-03-02 11:11:20 +0330

    Fixed dbot responsive issue

--- src/pages/help-centre/dbot.js
+++ src/pages/help-centre/dbot.js
@@ -16,7 +16,7 @@ const ArticleWrapper = styled.div`
     margin-left: 12.6rem;
 
     @media ${device.laptopL} {
-        margin-left: 8rem;
+        margin-left: 2rem;
     }
 `
 const StyledText = styled(Text)`
@@ -24,12 +24,13 @@ const StyledText = styled(Text)`
 `
 const ImageWrapper = styled.div`
     padding: 2.4rem 10.2rem;
+    max-width: 60rem;
+    width: 100%;
 
     @media ${device.laptopL} {
-        padding: 2.4rem 5rem;
-    }
-    @media ${device.laptop} {
+        margin-left: 0;
         padding: 2.4rem 0;
+        width: auto;
     }
 `
 

acebc6a435a93cb18f33036e82e9babb8d078bac
mustofa-binary <mustofa@binary.com>
2020-03-10 17:09:47 +0800

    fix text size

--- src/pages/careers/_hero.js
+++ src/pages/careers/_hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { graphql, useStaticQuery } from 'gatsby'
 import Container from './_layout-components/_container'
-import { Text, Header, BackgroundImage } from 'components/elements'
+import { Header, BackgroundImage } from 'components/elements'
 import { localize, Localize } from 'components/localization'
 import { LinkButton } from 'components/form'
 import device from 'themes/device'
@@ -23,13 +23,13 @@ const StyledHeader = styled(Header)`
     color: var(--color-white);
 `
 
-const Subheadline = styled(Text)`
-    font-size: var(--text-size-m);
+const Subheadline = styled(Header)`
     color: var(--color-white);
     max-width: 99.6rem;
 `
 
 const JoinHeader = styled(Header)`
+    font-size: var(--text-size-header-1);
     margin-bottom: 4.8rem;
     margin-top: 3.2rem;
     color: var(--color-white);
@@ -68,7 +68,7 @@ const Hero = () => {
                         components={[<RedBanner key={0} />]}
                     />
                 </StyledHeader>
-                <Subheadline>
+                <Subheadline as="h3">
                     {localize(
                         'We have a huge mission, an incredible team, and rapid growth. But it’s not just about our success — it’s about yours.',
                     )}

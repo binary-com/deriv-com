4b294397b280d87611fa125d7fa279e3d1e1784d
sara-fs <sara.khosropour@regentmarkets.com>
2020-03-15 09:40:03 +0330

    Removed extra codes from gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,5 @@
-import React from 'react'
-const language_config = require(`./i18n-config.js`)
+
 import { WrapPagesWithLocaleContext } from './src/components/localization'
 import './src/components/localization/config'
 
 export const wrapPageElement = WrapPagesWithLocaleContext
-
-// export function onRenderBody({ setHeadComponents }) {
-//     const languageLinks = []
-
-//     Object.keys(language_config).map(lang => {
-//         languageLinks.push(<link rel="alternate" href="https://deriv.com/about" hrefLang={lang} />)
-//     })
-   
-//     setHeadComponents(languageLinks)
-// }

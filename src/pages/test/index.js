cde2ba097196219f13aaa45a6dca46b56301c1ce
sara-fs <sara.khosropour@regentmarkets.com>
2020-03-16 15:07:50 +0330

    Added netlify cache

---
+++ src/pages/test/index.js
@@ -0,0 +1,15 @@
+import React from './node_modules/react'
+import Layout from './node_modules/components/layout/layout'
+import { WithIntl } from './node_modules/components/localization'
+
+const About = () => {
+
+    return (
+        <Layout>
+           <div>sdad</div>
+       
+        </Layout>
+    )
+}
+
+export default WithIntl()(About)

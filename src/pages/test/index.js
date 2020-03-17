dd21063c88733a85d82806f6b137ccbed67dbe03
sara-fs <sara.khosropour@regentmarkets.com>
2020-03-17 14:30:01 +0330

    test

---
+++ src/pages/test/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from 'components/layout/layout'
+import { WithIntl } from 'components/localization'
+
+const About = () => {
+
+    return (
+        <Layout>
+           <div>asd</div>
+       
+        </Layout>
+    )
+}
+
+export default WithIntl()(About)

ce0b887e88ae5c9826ab025a9d08b367505ff40c
arash <arash@firstsource.io>
2020-03-24 16:37:17 +0430

    add tab section

--- src/pages/markets/index.js
+++ src/pages/markets/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { getLocationHash } from 'common/utility'
 // import styled from 'styled-components'
 // import { Forex } from './_forex'
 // import { Commodities } from './_commodities'
@@ -7,13 +8,30 @@ import React from 'react'
 import { Hero } from './_hero'
 import Layout from 'components/layout/layout'
 import { localize, WithIntl } from 'components/localization'
-import { SEO } from 'components/containers'
+import { SEO, Flex } from 'components/containers'
+
+const useTabState = tab => {
+    const [active_tab, setActiveTab] = useState(tab)
+    const setTab = tab => {
+        if (tab === active_tab) return
+        setActiveTab(tab)
+        navigate(`#${tab}`)
+    }
+    return [active_tab, setTab]
+}
+const Tabs = styled(Flex)``
 
 const Markets = () => {
+    const [active_tab, setTab] = useTabState('forex')
+    useEffect(() => {
+        const new_tab = getLocationHash() || 'forex'
+        setTab(new_tab)
+    })
     return (
         <Layout>
             <SEO description={localize('')} title={localize('')} />
             <Hero />
+            <Tabs />
         </Layout>
     )
 }

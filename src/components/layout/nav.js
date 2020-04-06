df9f2e90480d9052cf1ad3a25a04e3c7e613308e
sara-fs <sara.khosropour@regentmarkets.com>
2020-04-06 10:04:58 +0430

    Changed logo for sign up page

--- src/components/layout/nav.js
+++ src/components/layout/nav.js
@@ -15,7 +15,8 @@ import Partner from 'common/partner'
 import device from 'themes/device'
 import { binary_url } from 'common/utility'
 // Icons
-import Logo from 'images/svg/logo_deriv.svg'
+import Logo from 'images/svg/logo-deriv.svg'
+import LogoSignup from 'images/svg/logo_deriv.svg'
 import LogoPartner from 'images/svg/logo-partners.svg'
 import LogoCareers from 'images/svg/logo-careers.svg'
 import Hamburger from 'images/svg/hamburger_menu.svg'
@@ -424,7 +425,7 @@ export const Nav = () => {
 export const NavStatic = () => (
     <StaticWrapper>
         <StyledLink to="/">
-            <Logo />
+            <LogoSignup />
         </StyledLink>
     </StaticWrapper>
 )

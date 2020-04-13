084f3197960f981ec3f2b147e05b5f8f644c508b
arash <arash@firstsource.io>
2020-04-13 14:07:40 +0430

    add synthetic/multipliers

--- src/pages/markets/synthetic/_multipliers.js
+++ src/pages/markets/synthetic/_multipliers.js
@@ -1,105 +1,64 @@
 import React from 'react'
 import styled from 'styled-components'
-import Scrollbar from 'react-perfect-scrollbar'
 import { ContinuousIndices, DailyResetIndices } from '../sub-markets/_submarkets.js'
-import { Accordion, AccordionItem, Text } from 'components/elements'
-import { SectionContainer, CssGrid, Box } from 'components/containers'
+import { Text } from 'components/elements'
+import { SectionContainer, Flex, CssGrid } from 'components/containers'
 import { localize } from 'components/localization'
-import 'react-perfect-scrollbar/dist/css/styles.css'
+import Dtrader from 'images/svg/dtrader-icon.svg'
 
-const header_style = {
-    padding: '2.4rem 10.2rem',
-    height: 'auto',
-    borderBottom: 'unset',
-    borderTop: '1px solid var(--color-grey-2)',
-    boxShadow: 'unset',
-}
-const first_item_style = {
-    padding: '2.4rem 10.2rem',
-    height: 'auto',
-    borderBottom: 'unset',
-    borderTop: 'unset',
-    boxShadow: 'unset',
-}
-const ContentWrapper = styled.div`
-    padding-top: 2.5rem;
+const Descriptions = styled.div`
     padding-bottom: 4rem;
-    max-width: 99.6rem;
-    margin: 0 auto;
+    border-bottom: 1px solid var(--color-grey-21);
 `
-const Row = styled(Box)`
-    border-radius: 4px;
-    border: 1px solid var(--color-grey-22);
-    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.04);
+const Col = styled(Flex)`
+    ${(props) => props.border_left && 'border-left: 1px solid #e3e4e5;'}
+`
+const Row = styled(Flex)`
+    border: ${(props) => (props.remove_border ? '' : '1px solid #e3e4e5')};
+    ${(props) => props.romve_border_top && 'border-top: unset;'}
+`
 
-    div:last-child {
-        border-right: unset;
-    }
-`
-const Col = styled(Box)`
-    border-right: 1px solid var(--color-grey-22);
-    height: 100%;
-
-    ${Text} {
-        text-align: center;
-    }
-`
 const Multipliers = () => {
     return (
-        <SectionContainer padding="0">
-            <Accordion>
-                <AccordionItem
-                    header_style={first_item_style}
-                    header={localize('How does multiplier trading work?')}
-                >
-                    <ContentWrapper>
-                        <Text>
-                            {localize(
-                                'Multipliers give you additional trading power so that you can control a position that is larger than the amount of capital at your disposal.',
-                            )}
+        <SectionContainer padding="4rem 0 8rem 0">
+            <Flex max_width="79.2rem" m="0 auto" direction="column">
+                <Descriptions>
+                    <Text>
+                        {localize(
+                            'Multipliers allow you to trade on leverage while limiting downside risk to your investment. You can maximise your potential profit by several multiples of any market movement without risking more than your initial investment.',
+                        )}
+                    </Text>
+                    <Flex jc="flex-end" ai="center" mt="2rem" pr="8rem">
+                        <Text mr="0.8rem">{localize('Available on')}</Text>
+                        <Dtrader />
+                    </Flex>
+                </Descriptions>
+                <Text weight="bold" mt="2.4rem">
+                    {localize('Instruments available for multipliers')}
+                </Text>
+                <Row jc="flex-start" ai="center" mt="1.6rem" background="rgba(242, 243, 244, 0.3)">
+                    <Col max_width="13.2rem">
+                        <Text weight="bold" align="center">
+                            {localize('Continuous indices')}
                         </Text>
-                        <Text mt="1.8rem">
-                            {localize(
-                                'Traditionally, the higher the multiplier, the higher your trading power, and the higher your potential profit or loss',
-                            )}
+                    </Col>
+                    <Col wrap="wrap" jc="flex-start" p="2rem 0 2.4rem 1.6rem" border_left>
+                        <CssGrid columns="1fr 1fr 1fr" row_gap="1.6rem">
+                            <ContinuousIndices />
+                        </CssGrid>
+                    </Col>
+                </Row>
+                <Row jc="flex-start" ai="center" romve_border_top>
+                    <Col max_width="13.2rem">
+                        <Text weight="bold" align="center">
+                            {localize('Daily reset indices')}
                         </Text>
-                        <Text mt="1.8rem">
-                            {localize(
-                                'The unique benefit of trading on our platform is that your loss never exceeds the amount you paid to buy the contract',
-                            )}
-                        </Text>
-                    </ContentWrapper>
-                </AccordionItem>
-                <AccordionItem
-                    header_style={header_style}
-                    header={localize('View Synthetic Indices available on Deriv')}
-                >
-                    <ContentWrapper>
-                        <Row>
-                            <CssGrid columns="repeat(2, 1fr)">
-                                <Col p="2.4rem 0">
-                                    <Text weight="bold">{localize('Continuous indices')}</Text>
-                                </Col>
-                                <Col p="2.4rem 0">
-                                    <Text weight="bold">{localize('Daily reset indices')}</Text>
-                                </Col>
-                            </CssGrid>
-                        </Row>
-                        <Row>
-                            <Scrollbar style={{ maxHeight: '55.2rem' }}>
-                                <CssGrid columns="repeat(2, 1fr)">
-                                    <Col>
-                                        <ContinuousIndices />
-                                    </Col>
-                                    <Col>
-                                        <DailyResetIndices />
-                                    </Col>
-                                </CssGrid>
-                            </Scrollbar>
-                        </Row>
-                    </ContentWrapper>
-                </AccordionItem>
-            </Accordion>
+                    </Col>
+                    <Col wrap="wrap" jc="flex-start" p="3.2rem 0 3.2rem 1.6rem" border_left>
+                        <DailyResetIndices />
+                    </Col>
+                </Row>
+            </Flex>
         </SectionContainer>
     )
 }

ce76bbe126ae9876a0fdcf6958ae9500b087cc17
mustofa-binary <mustofa@binary.com>
2020-04-15 16:49:38 +0800

    test amp only

--- src/pages/amp/index.js
+++ src/pages/amp/index.js
@@ -1,37 +1,8 @@
 import React from 'react'
-import { Hero } from '../home/_hero'
-import { Trade } from '../home/_trade'
-import TradeTheWayYouLike from '../home/_trade-the-way-you-like'
-import Markets from '../home/_markets'
-import SimpleSteps from '../home/_simple-steps'
-import WhatOurClientsSay from '../home/_what-our-clients-say'
-import { SEO, Show } from 'components/containers'
-import Layout from 'components/layout/layout'
-import { localize, WithIntl } from 'components/localization'
-import Signup, { Appearances } from 'components/custom/signup'
+import { WithIntl } from 'components/localization'
 
 const Home = () => {
-    return (
-        <Layout>
-            <SEO
-                title={localize('Online trading platform | Forex, commodities and indices')}
-                description={localize(
-                    'Deriv - An online trading platform that offers a wide selection of derivatives to trade on 24/7.',
-                )}
-            />
-            <Hero />
-            <Show.Mobile>
-                <TradeTheWayYouLike />
-            </Show.Mobile>
-            <Show.Desktop>
-                <Trade />
-            </Show.Desktop>
-            <Markets />
-            <SimpleSteps />
-            <WhatOurClientsSay />
-            <Signup appearance={Appearances.public} />
-        </Layout>
-    )
+    return <div>Test AMP</div>
 }
 
 export default WithIntl()(Home)

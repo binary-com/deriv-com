0e1c6e21c9ce4eb2108c9f67bb4d9372630279f3
mustofa-binary <mustofa@binary.com>
2020-04-15 16:50:53 +0800

    color white

--- src/pages/amp/index.js
+++ src/pages/amp/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { WithIntl } from 'components/localization'
 
 const Home = () => {
-    return <div>Test AMP</div>
+    return <div style={{ color: 'white' }}>Test AMP</div>
 }
 
 export default WithIntl()(Home)

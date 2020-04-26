07e1717d459f076f3d4a0942afc527a0475f4c99
arash <arash@firstsource.io>
2020-04-26 15:35:54 +0430

    why trade responsive

--- src/pages/markets/_why-trade.js
+++ src/pages/markets/_why-trade.js
@@ -8,44 +8,80 @@ import { SectionContainer, Flex, Box } from 'components/containers'
 import device from 'themes/device'
 
 const Item = styled(Flex)`
+    max-width: 18rem;
+    width: 100%;
+
     svg {
         width: 48px;
         height: 48px;
     }
+    @media ${device.tabletL} {
+        max-width: 27rem;
+        margin-top: 5rem;
+
+        ${Text} {
+            font-size: 2rem;
+        }
+    }
 `
 const ItemContainer = styled(Box)`
     display: flex;
+    margin: 3.2rem 0;
     flex-direction: row;
     justify-content: space-between;
     max-width: 140.4rem;
+
+    @media ${device.tabletL} {
+        flex-direction: column;
+        align-items: center;
+        margin: 1rem 0 5rem 0;
+    }
 `
 const StyledHeader = styled(Header)`
+    font-size: var(--text-size-header-1);
     @media ${device.tablet} {
         text-align: center;
         max-width: unset;
+        font-size: var(--text-size-header-4);
+    }
+`
+const StyledText = styled(Text)`
+    padding: 0 1px;
+    @media ${device.tabletL} {
+        font-size: 2rem;
+        padding: 0;
+    }
+`
+const StyledSection = styled(SectionContainer)`
+    @media ${device.tabletL} {
+        padding: 5rem 2rem;
+    }
+`
+const StyledButton = styled(LinkButton)`
+    @media ${device.tabletL} {
+        font-size: 1.75rem;
+        padding: 1.25rem 4.75rem;
     }
 `
 export const WhyTrade = ({ children, header, text }) => {
     return (
-        <SectionContainer background="var(--color-grey-23)">
+        <StyledSection background="var(--color-grey-23)">
             <Flex direction="column" max_width="99.6rem" m="0 auto" jc="space-between" ai="center">
                 <div>
-                    <StyledHeader align="center" mb="1.6rem" size="3.6rem" lh="1.25">
-                        {localize(header)}
+                    <StyledHeader align="center" mb="1.6rem" lh="1.25">
+                        {header}
                     </StyledHeader>
-                    <Text align="center" p="0 1px">
-                        {localize(text)}
-                    </Text>
+                    <StyledText align="center">{text}</StyledText>
                 </div>
-                <ItemContainer max_width="48.6rem" width="100%" mt="3.2rem" mb="3.2rem">
+                <ItemContainer max_width="48.6rem" width="100%">
                     {children.map((child, idx) => {
                         {
                             const { text, icon } = child.props
                             return (
-                                <Item key={idx} ai="center" direction="column" width="18rem">
+                                <Item key={idx} ai="center" direction="column">
                                     {icon}
                                     {
-                                        <Text align="center" mt="1.6rem" max_width="18rem">
+                                        <Text align="center" mt="1.6rem">
                                             {text}
                                         </Text>
                                     }
@@ -54,11 +90,11 @@ export const WhyTrade = ({ children, header, text }) => {
                         }
                     })}
                 </ItemContainer>
-                <LinkButton to="/signup/" secondary="true">
+                <StyledButton to="/signup/" secondary="true">
                     {localize('Create free demo account')}
-                </LinkButton>
+                </StyledButton>
             </Flex>
-        </SectionContainer>
+        </StyledSection>
     )
 }
 WhyTrade.propTypes = {

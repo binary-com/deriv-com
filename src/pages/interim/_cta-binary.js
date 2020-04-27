e02edaf94648d9b22e9c6e778f84f45e79b4f095
mustofa-binary <mustofa@binary.com>
2020-04-27 12:50:15 +0800

    fix qa issues

--- src/pages/interim/_cta-binary.js
+++ src/pages/interim/_cta-binary.js
@@ -12,7 +12,7 @@ const CtaBinary = ({ is_white }) => {
     return (
         <Flex mt="1.6rem" ai="center">
             {is_white ? <BinaryWhite /> : <BinaryBlue />}
-            <Text ml="4px" max_width="20rem" color={color} size="var(--text-size-xs)">
+            <Text ml="4px" max_width="20rem" lh="16px" color={color} size="var(--text-size-xs)">
                 <Localize
                     translate_text="No sign up needed. Log in with your <0>Binary.com</0> credentials."
                     components={[

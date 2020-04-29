a7024c1b8e9734663310e517b7f7beb3900b2acf
arash <arash@firstsource.io>
2020-04-29 17:52:14 +0430

    add new style to Forex/multipliers

--- src/pages/markets/forex/_multipliers.js
+++ src/pages/markets/forex/_multipliers.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { MajorPairs } from '../sub-markets/_submarkets.js'
 import AvailablePlatforms from '../_available-platforms.js'
 import { Text } from 'components/elements'
-import { SectionContainer, Flex } from 'components/containers'
+import { SectionContainer, Flex, CssGrid } from 'components/containers'
 import { localize } from 'components/localization'
 import device from 'themes/device'
 const Descriptions = styled.div`
@@ -11,16 +11,49 @@ const Descriptions = styled.div`
     border-bottom: 1px solid var(--color-grey-21);
 `
 const Col = styled(Flex)`
-    ${(props) => props.border_left && 'border-left: 1px solid #e3e4e5;'}
+    max-width: 13.2rem;
+
+    @media ${device.tabletL} {
+        max-width: 7rem;
+    }
 `
 const Row = styled(Flex)`
-    border: 1px solid #e3e4e5;
-    ${(props) => props.romve_border_top && 'border-top: unset;'}
+    border: 1px solid var(--color-grey-22);
+    margin-top: 2.4rem;
+    border-radius: 8px;
 `
 const StyledText = styled(Text)`
     @media ${device.tabletL} {
         font-size: 2rem;
         text-align: left;
+    }
+`
+const MarketsList = styled(CssGrid)`
+    border-left: 1px solid var(--color-grey-22);
+    grid-template-columns: repeat(5, 1fr);
+    width: 100%;
+    padding: 2.4rem;
+    grid-row-gap: 1.6rem;
+
+    @media ${device.tabletL} {
+        grid-template-columns: ${(props) =>
+            props.mobile_col_template ? props.mobile_col_template : 'repeat(3, 1fr)'};
+
+        svg {
+            width: 16px;
+            height: 16px;
+            margin-right: 4px;
+        }
+        ${Text} {
+            font-size: 1.5rem;
+            line-height: 1.5;
+        }
+    }
+`
+const Title = styled(Text)`
+    @media ${device.tabletL} {
+        text-align: center;
+        max-width: 4rem;
     }
 `
 const Multipliers = () => {
@@ -40,11 +73,11 @@ const Multipliers = () => {
                 </StyledText>
                 <Row jc="flex-start" ai="center" mt="1.6rem" background="rgba(242, 243, 244, 0.3)">
                     <Col max_width="13.2rem">
-                        <Text weight="bold">{localize('Major pairs')}</Text>
+                        <Title weight="bold">{localize('Major pairs')}</Title>
                     </Col>
-                    <Col wrap="wrap" jc="flex-start" p="2.4rem 0 0.8rem 1.6rem" border_left>
+                    <MarketsList>
                         <MajorPairs />
-                    </Col>
+                    </MarketsList>
                 </Row>
             </Flex>
         </SectionContainer>

7e24080f363ad832bd354df70da1ede9ddf4b681
mustofa-binary <mustofa@binary.com>
2020-05-22 14:02:31 +0800

    prepare for future faq interim text

---
+++ src/pages/interim/_future-faq-text.js
@@ -0,0 +1,48 @@
+/*
+    <Localize translate_text='<0>Binary.com</0> has rebranded to <1>Deriv.com</1> and here is everything you need to know' />
+    <Localize translate_text='You may have heard about the exciting improvements we’ve made recently to <0>Binary.com</0>. Yes, we’ve rebranded to <1>Deriv.com</1> as part of our renewed commitment to enhance your online trading experience.'
+    localize('We know you may have some questions about these changes, so here are answers to shed light on our transformation. In this FAQ, we explain to you what the rebranding is about, how the rebranding transforms our service delivery, and what it means to you.')
+    localize('What is the rebranding about?')
+    localize('We may have a new name and look, but be assured that we’re still the same team working hard since 1999 to deliver an exceptional online trading solution to our clients.')
+    <Localize translate_text='How will <0>Deriv.com</0> transform my trading experience?'
+    localize('After 20 years of service to you, we feel that we understand you better. The rebranding is an opportunity for us to implement an improved online trading solution to meet all your needs.')
+    localize('What does it mean to me?')
+    <Localize translate_text='We understand that change can be intimidating. That’s why we’ve been working very hard to make sure that your transition from <0>Binary.com</0> to <1>Deriv.com</1> is as seamless as possible.'
+    localize('What do I do next?')
+    <Localize translate_text='We hope these answers have helped you to know more about <0>Deriv.com</0> and our motivation for the rebranding.'
+    <Localize translate_text='To be clear, this rebranding has been inspired by you. We are thrilled to upgrade you to a new and improved trading experience on <0>Deriv.com</0> where you can trade what you want, when you want, and how you want.' />
+
+    localize('Why are you rebranding?')
+    localize('After 20 years in the market, we are stepping into the third decade of operations with a renewed commitment to you, our clients. Our new name and platforms are in line with our vision to provide a broader range of products and an enhanced user experience. With the rebranding, our intention is to continue serving you with simple, flexible, and reliable online trading solutions.')
+    
+    localize('What changes will I see?')
+    localize('The rebranding involves a new name, logo, corporate colours, and website which you may have already seen in our online presence. You will also enjoy a wider range of instruments to trade with, besides the availability of 4 advanced platforms on a single trading space. Over time, we will roll out more new solutions, all geared towards improving your trading experience.')
+    
+    <Localize translate_text='Is <0>Binary.com</0> being bought over by another company?' />
+    <Localize translate_text='No, <0>Deriv.com</0> is built, owned, and managed by the same team that brought you <1>Binary.com</1>. We like to think of <0>Deriv.com</0> as the next step in the evolution of our company. We have taken lessons from 20 years of experience to produce a diverse range of products that give clients the flexibility and ease to trade according to their preference with confidence.' />
+
+    localize('What are the new products and services offered as a result of this rebranding?')
+    <Localize translate_text='<0>Deriv.com</0> gives you multiple trading options in an environment that is simple, flexible, and reliable. Three core products — DTrader, DBot, and DMT5 — are offered alongside SmartTrader, the popular trading platform from <1>Binary.com</1>.' />
+    localize('While you can continue to enjoy trading on MetaTrader 5 as before, we’ve listened to your feedback and upgraded the experience for you. You’ll find that the MT5 account sign up and dashboard experience is far more efficient on Deriv.com, allowing you to manage your account and focus on your trades with much ease.')
+
+    <Localize translate_text='What else can we expect from Deriv.com?' />
+    localize('As we roll out new features in phases, you can look forward to such things as:')
+    localize('Multiplier options that potentially offer traders higher profits while limiting risks')
+    localize('P2P Cashier which facilitates seamless financial transactions')
+    localize('Light and dark mode features for a more enjoyable experience')
+
+    <Localize translate_text='Is <0>Deriv.com</0> regulated/registered/licensed?' />
+    <Localize translate_text='Yes. <0>Deriv.com</0> and <1>Binary.com</1> are owned by the same group, which is licensed and regulated in many jurisdictions around the world.' />
+
+    <Localize translate_text='Are you going to close down <0>Binary.com</0>?' />
+    <Localize translate_text='The <0>Binary.com</0> website will still be around for a while. In due time, we will phase out <0>Binary.com</0>. However, your accounts, funds, and everything you have on <0>Binary.com</0> will be available and accessible by you on <1>Deriv.com</1>.' />
+
+    <Localize translate_text='Why should I switch to <0>Deriv.com</0>?' />
+    <Localize translate_text='We’ve planned this rebranding process carefully and over many months with you, our clients, in mind. Based on the experience of serving you for the past 20 years, we’ve taken all your valuable feedback to deliver an improved experience to support your online trading journey. To guide you in this transition, we’ve prepared educational resources that will help you easily navigate around the new trading space. The demo account on <0>Deriv.com</0> is also available for you to explore and build confidence for the new environment.' />
+
+    <Localize translate_text='Can I still trade on <0>Binary.com</0>?' />
+    <Localize translate_text='Your funds will be reflected in both your <0>Binary.com</0> and <1>Deriv.com</1> accounts because they are essentially the same account. But we recommend that you try out <1>Deriv.com</1> now because we will gradually be phasing out <0>Binary.com</0>. We assure you that your favourite features are available on <1>Deriv.com</1>; plus, your account details, funds, and custom settings on <0>Binary.com</0> will be available on <1>Deriv.com</1> once you log in with your <0>Binary.com</0> credentials.' />
+
+    <Localize translate_text='How do I get started on <0>Deriv.com</0>?' />
+    <Localize translate_text='It’s simple. Just go to <0>Deriv.com</0> and enter your existing <1>Binary.com</1> credentials. You’ll immediately be able to explore the new look, feel, and added functionality of <0>Deriv.com</0>.' />
+*/

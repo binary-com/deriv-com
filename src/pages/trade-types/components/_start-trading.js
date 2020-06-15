1ad231598aaa3fa6df2657433a7aff1e7520b983
sara-fs <sara.khosropour@regentmarkets.com>
2020-06-15 13:22:54 +0430

    Finalized strat-trading component

--- src/pages/trade-types/components/_start-trading.js
+++ src/pages/trade-types/components/_start-trading.js
@@ -1,48 +1,51 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { Header } from 'components/elements'
+import { Text, Header } from 'components/elements'
+import Checklist from 'images/svg/checklist-red.svg'
 
-const Item = styled.div`
+const StyledItem = styled.div`
     display: flex;
-    border-left: var(--color-red) dash 1px;
+    border-left: var(--color-red) dashed 1px;
     position: relative;
+    height: 11rem;
+
+    &:last-child {
+        border-left: unset;
+    }
 `
 const Container = styled.div`
-    margin-top: 6px;
-    margin-left: 20px;
+    margin-left: 40px;
 `
-const Content = styled.div`
-    margin: 16px 0;
-`
-const Oval = styled.div`
-    width: 24px;
-    height: 24px;
-    line-height: 24px;
-    background-color: var(--brand-red-coral);
-    border-radius: 50%;
-    text-align: center;
-    margin-right: 8px;
+const Oval = styled(Checklist)`
+    margin-right: 2.4rem;
     position: absolute;
     left: -12px;
 `
-
 const StartTrading = ({ children, ...props }) => {
     return (
         <div {...props}>
             {children.map((child, idx) => (
-                <Item key={idx}>
+                <StyledItem key={idx}>
                     <Oval />
                     <Container>
-                        <Header>{child.props.title}</Header>
-                        <Content>{child}</Content>
+                        <Header mb="0.8rem" mt="-5px" as="h4">
+                            {child.props.title}
+                        </Header>
+                        <Text>{child}</Text>
                     </Container>
-                </Item>
+                </StyledItem>
             ))}
         </div>
     )
 }
+const Item = ({ children, ...props }) => <div {...props}>{children}</div>
 
+StartTrading.Item = Item
+
+Item.propTypes = {
+    children: PropTypes.node,
+}
 StartTrading.propTypes = {
     children: PropTypes.node,
 }
---
+++ src/pages/trade-types/margin/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { SmallContainer, Hero } from '../components/_style'
+import StartTrading from '../components/_start-trading'
+import { SEO } from 'components/containers'
+import { Header } from 'components/elements'
+import Layout from 'components/layout/layout'
+import { localize, WithIntl } from 'components/localization'
+
+const Options = () => {
+    return (
+        <Layout>
+            <SEO
+                title={localize('Trade types | Margin')}
+                description={localize(
+                    'Options are products that allow for payouts from predicting market movements, without needing to buy the underlying asset.',
+                )}
+            />
+            <Hero jc="cneter" ai="center">
+                <SmallContainer>
+                    <Header as="h1" color="white" align="center">
+                        {localize('Margin')}
+                    </Header>
+                </SmallContainer>
+            </Hero>
+            <StartTrading>
+                <StartTrading.Item title="Practise">
+                    {localize(
+                        'Open a demo MetaTrader 5 account on Deriv and practise with an unlimited amount of virtual funds.',
+                    )}
+                </StartTrading.Item>
+                <StartTrading.Item title="Trade">
+                    {localize(
+                        'Trade with a real Deriv MT5 (DMT5) account and get access to high leverage to trade positions larger than your existing capital.',
+                    )}
+                </StartTrading.Item>
+                <StartTrading.Item title="Withdraw">
+                    {localize(
+                        'Conveniently withdraw your funds through any of our supported withdrawal methods.',
+                    )}
+                </StartTrading.Item>
+            </StartTrading>
+        </Layout>
+    )
+}
+
+export default WithIntl()(Options)

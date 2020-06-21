d93931bd69e58a78dcef6d7afb5538d34807acdb
sara-fs <sara.khosropour@regentmarkets.com>
2020-06-21 13:14:09 +0430

    Added our values section

--- src/pages/about/_our-numbers.js
+++ src/pages/about/_our-numbers.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Show from 'components/containers/show'
-import { Container, Flex } from 'components/containers'
+import { Container, Flex, CssGrid } from 'components/containers'
 import { Header, Text } from 'components/elements'
 import { localize } from 'components/localization'
 import device from 'themes/device'
@@ -9,7 +9,6 @@ import Experience from 'images/svg/experience.svg'
 
 const OurNumbersWrapper = styled.section`
     width: 100%;
-    background-color: var(--color-grey-8);
     margin: auto;
 
     @media ${device.tablet} {
@@ -25,12 +24,20 @@ const StyledText = styled(Text)`
     max-width: 79.2rem;
 `
 
-const Card = styled.div`
+const Card = styled(Flex)`
     max-width: 48.6rem;
-    max-height: 48.8rem;
-    width: 100%;
-    height: 100%;
+    height: 48.8rem;
     box-shadow: 0 4px 7px 0 rgba(14, 14, 14, 0.1);
+    padding: 4rem 0;
+    justify-content: center;
+    flex-wrap: wrap;
+`
+const Span = styled.span`
+    color: ${(props) => props.color};
+    margin-right: ${(props) => props.mr || 'unset'};
+    font-size: var(--text-size-l);
+    font-weight: bold;
+    width: auto;
 `
 
 const OurNumbersContainer = styled(Container)`
@@ -58,32 +65,62 @@ const OurNumbers = () => (
                 </StyledHeader>
             </Show.Mobile>
 
-            <Header mb="1.2rem" as="h2" color="black-2" align="center">
-                {localize('Our numbers')}
-            </Header>
-            <StyledText align="center" lh="2.4rem">
-                {localize(
-                    'Deriv.com is the next step in the evolution of Binary.com. With Binary.com, we have a proven record of delivering market-leading products that are trusted around the world. ',
-                )}
-            </StyledText>
-            <Flex ai="center" mt="5.4rem">
+            <Show.Desktop>
+                <Header mb="1.2rem" as="h2" color="black-2" align="center">
+                    {localize('Our numbers')}
+                </Header>
+                <StyledText align="center" lh="2.4rem">
+                    {localize(
+                        'Deriv.com is the next step in the evolution of Binary.com. With Binary.com, we have a proven record of delivering market-leading products that are trusted around the world. ',
+                    )}
+                </StyledText>
+            </Show.Desktop>
+            <Flex mb="4rem" ai="center" mt="5.4rem">
                 <Experience />
                 <Text size="var(--text-size-m)" weight="bold" ml="1.6rem" color="orange-2">
                     {localize('20 years of industry experience')}
                 </Text>
             </Flex>
-            <Flex>
+
+            <CssGrid
+                columns="repeat(2, 1fr)"
+                column_gap="2.4rem"
+                row_gap="2.4rem"
+                tablet_columns="repeat(2, 1fr)"
+                mobile_columns="1fr"
+                mobile_row_gap="10rem"
+            >
                 <Card>
-                    <Header align="center" as="h3">
-                        {localize('175K+ active trading clients')}
-                    </Header>
+                    <Span mr="8px" color="var(--color-orange-2)">
+                        {localize('175K+')}
+                    </Span>
+                    <Span>{localize('active trading')}</Span>
+                    <Flex mt="1.2rem">
+                        <Span>{localize('clients')}</Span>
+                    </Flex>
                 </Card>
                 <Card>
-                    <Header as="h3" align="center">
-                        {localize('377M+ transactions')}
-                    </Header>
+                    <Span mr="8px" color="var(--color-orange-2)">
+                        {localize('377M+')}
+                    </Span>
+                    <Span>{localize('transactions')}</Span>
                 </Card>
-            </Flex>
+                <Card>
+                    <Span mr="8px" color="var(--color-orange-2)">
+                        {localize('6M+ USD')}
+                    </Span>
+                    <Span>{localize('total withdrawal ')}</Span>
+                    <Flex mt="1.2rem">
+                        <Span>{localize('per month')}</Span>
+                    </Flex>
+                </Card>
+                <Card>
+                    <Span mr="8px" color="var(--color-orange-2)">
+                        {localize('2B+ USD ')}
+                    </Span>
+                    <Span>{localize('trade turnover')}</Span>
+                </Card>
+            </CssGrid>
         </OurNumbersContainer>
     </OurNumbersWrapper>
 )

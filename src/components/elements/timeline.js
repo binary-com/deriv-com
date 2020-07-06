ddf5eacf9c6ef733627999fc15399248256eab76
Suthesan Kanagasabai <45410833+suthesan-binary@users.noreply.github.com>
2020-07-06 15:03:36 +0800

    Update src/components/elements/timeline.js
    Co-authored-by: Mustofa Ghaleb Amami (Front End) <52783019+mustofa-binary@users.noreply.github.com>

--- src/components/elements/timeline.js
+++ src/components/elements/timeline.js
@@ -13,7 +13,7 @@ const Checkmark = styled.span`
         position: absolute;
         width: 2px;
         height: 9px;
-        background-color: ${(props) => (props.color ? props.color : 'white')};
+        background-color: ${(props) => (props.color ? props.color : var(--color-white))};
         left: 11px;
         top: 6px;
     }

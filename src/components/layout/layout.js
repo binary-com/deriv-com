4f164a5fc684ae05da4ef3cd1a0d82dbef7b8638
mustofa-binary <mustofa@binary.com>
2020-07-10 12:02:09 +0800

    update to use two box shadows

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -20,7 +20,7 @@ const LiveChat = styled.div`
     bottom: 1.6rem;
     right: 1.6rem;
     background-color: var(--color-white);
-    box-shadow: 0 16px 20px 0 rgba(0, 0, 0, 0.05);
+    box-shadow: 0 16px 20px 0 rgba(0, 0, 0, 0.05), 0 0 20px 0 rgba(0, 0, 0, 0.05);
     padding: 1.6rem;
     display: flex;
     cursor: pointer;

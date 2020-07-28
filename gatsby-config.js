744a0057b7c1c63fceeeefccfe22e5c79a1a0d8c
mustofa-binary <mustofa@binary.com>
2020-07-28 13:55:15 +0800

    remove spinner

--- gatsby-config.js
+++ gatsby-config.js
@@ -218,7 +218,7 @@ module.exports = {
             resolve: `gatsby-plugin-nprogress`,
             options: {
                 color: `#ff444f`,
-                showSpinner: true,
+                showSpinner: false,
                 minimum: 0.4,
             },
         },

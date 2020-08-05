94a530a9bccc2ee14f21bfaec1cb086bfb434e7c
mustofa-binary <mustofa@binary.com>
2020-08-05 13:28:38 +0800

    add styling

--- src/pages/recruit/index.js
+++ src/pages/recruit/index.js
@@ -1,6 +1,53 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Helmet } from 'react-helmet'
+import Layout from 'components/layout/layout'
+import { WithIntl } from 'components/localization'
 import { isBrowser } from 'common/utility'
+import device from 'themes/device'
+
+const RecruitWrapper = styled.div`
+    .embed_jobs_head2 {
+        width: 100%;
+    }
+    .rec-grp-heading {
+        font-size: var(--text-size-m);
+        margin: 1.6rem 0;
+    }
+    .embed_jobs_with_style.embed_jobs_with_style_1 {
+        ul.rec-group {
+            grid-template-columns: 1fr 1fr 1fr;
+
+            @media ${device.tabletL} {
+                grid-template-columns: 1fr 1fr;
+            }
+            @media ${device.tablet} {
+                grid-template-columns: 1fr;
+            }
+        }
+        ul.rec-job-info {
+            height: auto;
+            width: 100%;
+            box-shadow: rgba(0, 0, 0, 0.05) 0 16px 20px 0, rgba(0, 0, 0, 0.05) 0 0 20px 0;
+            margin-bottom: 2.4rem;
+            border-radius: 4px;
+            padding: 3.2rem;
+            border: none;
+
+            &:hover {
+                background: var(--color-white);
+            }
+            .rec-job-title a {
+                font-size: var(--text-size-s);
+                color: var(--color-red);
+                margin-bottom: 1.6rem;
+            }
+            li > span {
+                font-size: var(--text-size-xs);
+            }
+        }
+    }
+`
 
 const Recruit = () => {
     const loadZohoScript = (callback) => {
@@ -29,22 +76,24 @@ const Recruit = () => {
         })
     }, [])
     return (
-        <div>
+        <Layout>
             <Helmet>
                 <link
                     rel="stylesheet"
                     href="https://css.zohostatic.com/recruit/embed_careers_site/css/v1.0/embed_jobs.css"
                 />
             </Helmet>
-            <div className="embed_jobs_head embed_jobs_with_style_1 embed_jobs_with_style">
-                <div className="embed_jobs_head2">
-                    <div className="embed_jobs_head3">
-                        <div id="rec_job_listing_div"> </div>
+            <RecruitWrapper>
+                <div className="embed_jobs_head embed_jobs_with_style_1 embed_jobs_with_style">
+                    <div className="embed_jobs_head2">
+                        <div className="embed_jobs_head3">
+                            <div id="rec_job_listing_div"> </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
+            </RecruitWrapper>
+        </Layout>
     )
 }
 
-export default Recruit
+export default WithIntl()(Recruit)

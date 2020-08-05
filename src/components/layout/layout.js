44d05a5382104d00a35bc4369bbfd052e300e49a
mustofa-binary <mustofa@binary.com>
2020-08-05 14:18:02 +0800

    change gtm livechat from id to classname

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -155,7 +155,7 @@ const Layout = ({ children, type, interim_type, padding_top, no_login_signup }) 
             )}
             {is_livechat_interactive && (
                 <LiveChat
-                    id="gtm-deriv-livechat"
+                    className="gtm-deriv-livechat"
                     onClick={() => {
                         LC_API.open_chat_window()
                     }}

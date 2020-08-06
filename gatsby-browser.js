0f9476da8ef7305cf1fdb57e439c454b5438d917
oskar-binary <oskar@binary.com>
2020-08-06 14:50:51 +0800

    Fix: move GTM to onClientEntry

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -43,7 +43,27 @@ export const onInitialClientRender = () => {
 
 export const onClientEntry = () => {
     LocalStore.set('window_loaded', '')
-    window.onload = () => LocalStore.set('window_loaded', 'true')
+    window.onload = () => {
+        LocalStore.set('window_loaded', 'true')
+    }
+
+    const gtm_test_domain = 'deriv-com.binary.sx'
+    if (window.location.hostname === gtm_test_domain) {
+        const gtm = document.createElement('script')
+        gtm.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-TNX2ZKH'
+        gtm.id = 'gtm-test-container'
+        document.body.appendChild(gtm)
+
+        const datalayer = document.createElement('script')
+        datalayer.text = `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TNX2ZKH');
+        `
+        document.body.appendChild(datalayer)
+    }
 }
 
 export const wrapPageElement = WrapPagesWithLocaleContext

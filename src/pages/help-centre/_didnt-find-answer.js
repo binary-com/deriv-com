dabb33789a6d1c0f328ec9705d83301bb2ada9b2
mustofa-binary <mustofa@binary.com>
2020-08-11 14:01:36 +0800

    feat: add helpcenter chat link both desktop and mobile

--- src/pages/help-centre/_didnt-find-answer.js
+++ src/pages/help-centre/_didnt-find-answer.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Text } from 'components/elements'
-import { LinkButton } from 'components/form'
+import { Button } from 'components/form'
 import { localize } from 'components/localization'
 import { Container } from 'components/containers'
-import ContactUsIcon from 'images/svg/contact-us.svg'
 import device from 'themes/device'
+import { isBrowser } from 'common/utility'
+import ContactUsIcon from 'images/svg/livechat-red.svg'
 
 const DFYAWrapper = styled.section`
     background-color: var(--color-black-3);
 `
 const DFYASection = styled(Container)`
     padding: 3.5rem 0;
-    justify-content: flex-start;
+    justify-content: center;
     align-items: center;
     @media ${device.mobileL} {
         flex-wrap: wrap;
         justify-content: center;
         align-items: center;
+
+        ${Button} {
+            font-size: 14px;
+            padding: 10px 16px;
+        }
     }
 
     & > * {
@@ -26,8 +32,8 @@ const DFYASection = styled(Container)`
 `
 const StyledIcon = styled(ContactUsIcon)`
     @media ${device.tabletL} {
-        width: 30px;
-        height: 30px;
+        width: 48px;
+        height: 48px;
         margin-right: 1.6rem;
     }
 `
@@ -38,16 +44,38 @@ const MiddleText = styled(Text)`
     }
 `
 
-export const DidntFindYourAnswerBanner = () => (
-    <DFYAWrapper>
-        <DFYASection>
-            <StyledIcon />
-            <MiddleText size="var(--text-size-l)" color="white" weight="bold" m="0 2.4rem">
-                {localize('Didn’t find your answer?')}
-            </MiddleText>
-            <LinkButton secondary="true" to="/contact-us" weight="bold" color="black">
-                {localize('Contact us')}
-            </LinkButton>
-        </DFYASection>
-    </DFYAWrapper>
-)
+export const DidntFindYourAnswerBanner = () => {
+    const LC_API = (isBrowser() && window.LC_API) || {}
+    const [is_livechat_interactive, setLiveChatInteractive] = React.useState(false)
+
+    React.useEffect(() => {
+        if (isBrowser()) {
+            window.scrollTo(0, 0)
+            window.LiveChatWidget.on('ready', () => {
+                setLiveChatInteractive(true)
+            })
+        }
+    }, [])
+    return (
+        <DFYAWrapper>
+            <DFYASection>
+                <StyledIcon />
+                <MiddleText size="var(--text-size-l)" color="white" m="0 2.4rem">
+                    {localize('Didn’t find your answer? We can help.')}
+                </MiddleText>
+                {is_livechat_interactive && (
+                    <Button
+                        secondary="true"
+                        onClick={() => {
+                            LC_API.open_chat_window()
+                        }}
+                        weight="bold"
+                        color="black"
+                    >
+                        {localize('Chat')}
+                    </Button>
+                )}
+            </DFYASection>
+        </DFYAWrapper>
+    )
+}

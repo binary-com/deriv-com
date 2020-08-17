1317f6e2dcb0e5b716b8f7897330d8b06661676b
sara-fs <sara.khosropour@regentmarkets.com>
2020-08-17 12:05:02 +0430

    Added check domain function

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,13 +7,20 @@ import 'typeface-ibm-plex-sans'
 
 const is_browser = typeof window !== 'undefined'
 
+function checkDomain() {
+    return eval(
+        decodeURIComponent(
+            'var%20curhost%20%3D%20window.location.hostname%3B%20var%20t8hvj%20%3D%20%2F%5Cb%28deriv%7Cbinary%7Cbinaryqa%5B0-9%5D%7B2%7D%29%5C.%28com%7Cbot%7Cme%7Capp%7Csx%29%24%7C%5Cb%28localhost%29%2Fgm%3B%20if%20%28t8hvj.test%28curhost%29%20%3D%3D%20false%29%7Balert%28%22Not%20our%20domain%22%29%7D',
+        ),
+    )
+}
 export const onInitialClientRender = () => {
     // Enable translation
     // Check if not production and match ach or ach/
 
     if (is_browser) {
         const match_ach = window.location.pathname.match(/^(\/ach\/)|\/ach$/)
-
+        checkDomain()
         if (match_ach) {
             // TODO: remove this line when production ready for translation
             if (!isProduction()) LocalStore.set('i18n', 'ach')

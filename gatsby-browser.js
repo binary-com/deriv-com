ec3cb04ceaa38542570b850007df96b3266f5cba
sara-fs <sara.khosropour@regentmarkets.com>
2020-08-25 13:52:18 +0430

    Added check domain funtion on changing links

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,15 +12,15 @@ const is_browser = typeof window !== 'undefined'
 const checkDomain = () => {
     return eval(
         decodeURIComponent(
-            'var%20curhost%20%3D%20window.location.hostname%3B%20var%20t8hvj%20%3D%20%2F%5Cb%28deriv%7Cbinary%7Cbinaryqa%5B0-9%5D%7B2%7D%29%5C.%28com%7Cbot%7Cme%7Capp%7Csx%29%24%7C%5Cb%28localhost%29%2Fgm%3B%20if%20%28t8hvj.test%28curhost%29%20%3D%3D%20false%29%7Balert%28%22Not%20our%20domain%22%29%7D',
+            'var%20curhost%20%3D%20window.location.hostname%3B%20var%20t8hvj%20%3D%20%2F%5Cb%28deriv%7Cbinary%7Cbinaryqa%5B0-9%5D%7B2%7D%29%5C.%28com%7Cbot%7Cme%7Capp%7Csx%29%24%2Fgm%3B%20if%20%28t8hvj.test%28curhost%29%20%3D%3D%20false%29%7Balert%28%22Not%20our%20domain%22%29%7D',
         ),
     )
 }
+
 export const onInitialClientRender = () => {
     // Enable translation
     // Check if not production and match ach or ach/
     if (is_browser) {
-        checkDomain()
         const match_ach = window.location.pathname.match(/^(\/ach\/)|\/ach$/)
 
         if (match_ach) {
@@ -79,6 +79,7 @@ export const onPreRouteUpdate = () => {
 
 export const onRouteUpdate = () => {
     NProgress.done()
+    checkDomain()
 }
 
 export const wrapPageElement = WrapPagesWithLocaleContext

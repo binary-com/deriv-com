0c06f58b68336f7dcb67f46b88e4a6dc2848e31e
mustofa-binary <mustofa@binary.com>
2020-09-01 11:14:00 +0800

    update: remove pl

--- src/components/localization/language-switcher.js
+++ src/components/localization/language-switcher.js
@@ -8,7 +8,7 @@ import { isProduction } from 'common/websocket/config'
 
 const languages = Object.keys(language_config)
 
-const disabled_lang = ['ach']
+const disabled_lang = ['ach', 'pl']
 
 class LanguageSwitch extends Component {
     constructor(props) {

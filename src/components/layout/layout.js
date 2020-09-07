c62ec153f19a05d5f02e23fea006fb8c59555d8a
Kevin <kevin@Kevins-MacBook-Pro.local>
2020-09-07 16:28:44 +0800

    refactor: revert unecessary changes

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -28,10 +28,9 @@ const clients_country_cookie = new CookieStorage(CLIENTS_COUNTRY_KEY)
 const tracking_status_cookie = new CookieStorage(TRACKING_STATUS_KEY)
 
 const Layout = ({ children, type, interim_type, padding_top, no_login_signup }) => {
-    let [clients_country, setClientCountry] = React.useState(
+    const [clients_country, setClientCountry] = React.useState(
         clients_country_cookie.get(CLIENTS_COUNTRY_KEY),
     )
-    clients_country = 'mt'
     const [show_cookie_banner, setShowCookieBanner] = React.useState(false)
     const [show_modal, toggleModal, closeModal] = useModal()
     const [modal_payload, setModalPayload] = useState({})

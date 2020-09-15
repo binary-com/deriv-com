95b26f96e34a828f91c92954cb2edde67d9dc9ab
mustofa-binary <mustofa@binary.com>
2020-09-15 16:36:57 +0800

    fix: remove noindex in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,7 +49,6 @@ const Home = () => {
                 description={localize(
                     'Deriv - An online trading platform that offers a wide selection of derivatives to trade on 24/7.',
                 )}
-                no_index
             />
             <Hero />
 

2999961d532c8c90a2f59503492eb6dbecd0b74b
mustofa-binary <mustofa@binary.com>
2020-09-22 13:46:11 +0800

    feat: add language detector from browser, add faq schema, and org schema json

--- src/components/localization/config.js
+++ src/components/localization/config.js
@@ -1,28 +1,31 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import { str as crc32 } from 'crc-32'
+import detector from 'i18next-browser-languagedetector'
 import language_config from '../../../i18n-config'
 
-i18n.use(initReactI18next).init({
-    fallbackLng: 'en',
-    ns: ['translations'],
-    defaultNS: 'translations',
+i18n.use(detector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'en',
+        ns: ['translations'],
+        defaultNS: 'translations',
 
-    // To investigate react-i18next translation issues enable this
-    debug: false,
+        // To investigate react-i18next translation issues enable this
+        debug: false,
 
-    interpolation: {
-        escapeValue: false,
-    },
-
-    react: {
-        wait: true,
-        useSuspense: false,
-        hashTransKey(defaultValue) {
-            return crc32(defaultValue)
+        interpolation: {
+            escapeValue: false,
         },
-    },
-})
+
+        react: {
+            wait: true,
+            useSuspense: false,
+            hashTransKey(defaultValue) {
+                return crc32(defaultValue)
+            },
+        },
+    })
 
 i18n.languages = Object.keys(language_config)
 

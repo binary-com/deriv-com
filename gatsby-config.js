96f2cf7af0a235ca8c145118564834ebfd470b74
mustofa-binary <mustofa@binary.com>
2020-10-13 13:45:25 +0800

    feat: new favicons 192 and mas icons in manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -140,6 +140,19 @@ module.exports = {
                 theme_color: '#ff444f',
                 display: 'standalone',
                 icon: './favicons/favicon-512x512.png',
+                icons: [
+                    {
+                        src: `./favicons/favicon-192x192.png`,
+                        sizes: `192x192`,
+                        type: `image/png`,
+                        purpose: 'any maskable',
+                    },
+                    {
+                        src: `./favicons/favicon-512x512.png`,
+                        sizes: `512x512`,
+                        type: `image/png`,
+                    },
+                ],
                 // TODO: add translations and support for language routes e.g:
                 // localize: [
                 //     {

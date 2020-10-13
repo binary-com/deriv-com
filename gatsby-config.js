66e45839cd76b11ab46295650d6b8099c3cf9868
Kevin <kevin@binary.com>
2020-10-13 17:45:51 +0800

    remove:unused package

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,36 +10,6 @@ module.exports = {
         siteUrl: 'https://deriv.com',
     },
     plugins: [
-        // TODO: AMP will be disabled until further notice
-        // {
-        //     resolve: `gatsby-plugin-amp`,
-        //     options: {
-        //         analytics: {
-        //             type: 'gtag',
-        //             dataCredentials: 'include',
-        //             config: {
-        //                 vars: {
-        //                     gtag_id: 'UA-139927388-1',
-        //                     config: {
-        //                         'UA-139927388-1': {
-        //                             page_location: '{{pathname}}',
-        //                         },
-        //                     },
-        //                 },
-        //             },
-        //         },
-        //         canonicalBaseUrl: 'https://deriv.com/',
-        //         components: [
-        //             'amp-animation',
-        //             'amp-position-observer',
-        //             'amp-carousel',
-        //             'amp-iframe',
-        //         ],
-        //         pathIdentifier: '/amp/',
-        //         relAmpHtmlPattern: '{{canonicalBaseUrl}}{{pathname}}{{pathIdentifier}}',
-        //         useAmpClientIdApi: true,
-        //     },
-        // },
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-styled-components',
         {
@@ -128,7 +98,6 @@ module.exports = {
                     }),
             },
         },
-        'gatsby-plugin-remove-serviceworker',
         {
             resolve: 'gatsby-plugin-manifest',
             options: {

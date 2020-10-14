ad98ed906795e8d9f3dca6bb4008fdfaba6552eb
Mustofa Ghaleb Amami (Front End) <52783019+mustofa-binary@users.noreply.github.com>
2020-10-14 15:11:45 +0800

    amam/enable service worker (#1131)
    * chore: add plugin offline and remove remove sw
    
    * chore: re-order gatsby config
    
    * feat: new favicons 192 and mas icons in manifest
    
    * test: change text
    
    * test: change image
    
    * revert: favicons
    
    * feat: new favicons
    
    Co-authored-by: mustofa-binary <mustofa@binary.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,7 +128,6 @@ module.exports = {
                     }),
             },
         },
-        'gatsby-plugin-remove-serviceworker',
         {
             resolve: 'gatsby-plugin-manifest',
             options: {
@@ -141,6 +140,19 @@ module.exports = {
                 theme_color: '#ff444f',
                 display: 'standalone',
                 icon: './favicons/favicon-512x512.png',
+                icons: [
+                    {
+                        src: `./favicons/favicon-192x192.png`,
+                        sizes: `192x192`,
+                        type: `image/png`,
+                        purpose: 'any maskable',
+                    },
+                    {
+                        src: `./favicons/favicon-512x512.png`,
+                        sizes: `512x512`,
+                        type: `image/png`,
+                    },
+                ],
                 // TODO: add translations and support for language routes e.g:
                 // localize: [
                 //     {
@@ -223,5 +235,6 @@ module.exports = {
                 includeInDevelopment: false,
             },
         },
+        'gatsby-plugin-offline',
     ],
 }

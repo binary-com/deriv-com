a0188eec2c15345dd1f27671bc03788934619976
mustofa-binary <mustofa@binary.com>
2020-10-15 16:58:21 +0800

    revert: test hero

--- src/pages/home/_vertical-carousel.js
+++ src/pages/home/_vertical-carousel.js
@@ -38,31 +38,36 @@ const VerticalCarousel = ({ contents }) => {
     // We have 5 items which height of each of them is 36px.
     // In each step we should +36px to the last item (coordinate of the last item is -72px)
     // This is list of item's coordinate [-72, -36, 0, 36, 72]
-    // const [active_header, setActiveHeader] = React.useState(contents.length - 1)
-    // const [transform, setTransform] = React.useState(-72)
-    // const [visibility, setVisibility] = React.useState('hidden')
+    const [active_header, setActiveHeader] = React.useState(contents.length - 1)
+    const [transform, setTransform] = React.useState(-72)
+    const [visibility, setVisibility] = React.useState('hidden')
 
-    // const startVerticalCarousel = (active_header = 5, transform = -72) => {
-    //     if (active_header > 0) {
-    //         setActiveHeader(active_header - 1)
-    //         setTransform(transform + 36)
-    //         setTimeout(() => startVerticalCarousel(active_header - 1, transform + 36), 3000)
-    //     } else {
-    //         // here we should restart the carousel with the first item (which is the last item in the array)
-    //         startVerticalCarousel(5, -108)
-    //     }
-    // }
+    const startVerticalCarousel = (active_header = 5, transform = -72) => {
+        if (active_header > 0) {
+            setActiveHeader(active_header - 1)
+            setTransform(transform + 36)
+            setTimeout(() => startVerticalCarousel(active_header - 1, transform + 36), 3000)
+        } else {
+            // here we should restart the carousel with the first item (which is the last item in the array)
+            startVerticalCarousel(5, -108)
+        }
+    }
 
-    // React.useEffect(() => {
-    //     setTimeout(() => setVisibility('inherit'), 500)
-    //     setTimeout(() => startVerticalCarousel(active_header, transform), 3000)
-    // }, [])
+    React.useEffect(() => {
+        setTimeout(() => setVisibility('inherit'), 500)
+        setTimeout(() => startVerticalCarousel(active_header, transform), 3000)
+    }, [])
 
     return (
         <VerticalCarouselWrapper direction="column">
-            <VerticalCarouselContainer>
+            <VerticalCarouselContainer transform={transform} visibility={visibility}>
                 {contents.map((content, index) => (
-                    <StyledHeader color="white" index={index} key={index}>
+                    <StyledHeader
+                        color="white"
+                        active_header={active_header}
+                        index={index}
+                        key={index}
+                    >
                         {content}
                     </StyledHeader>
                 ))}

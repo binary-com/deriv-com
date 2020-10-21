9429182095ce90147ae0f4c7c46ada12954e6643
mustofa-binary <mustofa@binary.com>
2020-10-21 16:47:39 +0800

    chore: remove clients country cookies

--- src/components/hooks/eu-country-hooks.js
+++ src/components/hooks/eu-country-hooks.js
@@ -1,22 +1,15 @@
 import React from 'react'
 import { useWebsiteStatus } from './website-status-hooks'
-import { CookieStorage } from 'common/storage'
 import { isEuCountry } from 'common/country-base'
 
-const CLIENTS_COUNTRY_KEY = 'clients_country'
-const clients_country_cookie = new CookieStorage(CLIENTS_COUNTRY_KEY)
-
 export const useEuCountry = () => {
-    const [clients_country, setClientCountry] = React.useState(
-        clients_country_cookie.get(CLIENTS_COUNTRY_KEY),
-    )
-    const [is_eu_country, setEuCountry] = React.useState(isEuCountry(clients_country))
     const [website_status] = useWebsiteStatus()
+    const [is_eu_country, setEuCountry] = React.useState(
+        isEuCountry(website_status?.clients_country),
+    )
 
     React.useEffect(() => {
-        if (!clients_country && website_status) {
-            setClientCountry(website_status.clients_country)
-            clients_country_cookie.set(CLIENTS_COUNTRY_KEY, website_status.clients_country)
+        if (website_status) {
             setEuCountry(isEuCountry(website_status.clients_country))
         }
     }, [website_status])

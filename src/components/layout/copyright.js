537a72894cb41e3081ed22a95c7923a3560365c8
sean-binary <sean@deriv.com>
2020-11-04 16:32:12 +0800

    fix: white copyright svg

--- src/components/layout/copyright.js
+++ src/components/layout/copyright.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Box, Container } from 'components/containers'
 import { Text } from 'components/elements'
 import { localize } from 'components/localization'
-import CopyrightIc from 'images/svg/copyright.svg'
+import { CopyrightIcon } from 'components/svgs/copyright'
 
 const Copyright = () => {
     return (
         <Box bg="var(--color-black)">
             <Container jc="flex-start" p="1.5rem 0" ai="center">
-                <CopyrightIc width="1.6rem" />
+                <CopyrightIcon width="1.6rem" color="white" />
                 <Text color="white" ml="0.4rem">
                     {localize('2020 Deriv | All rights reserved')}
                 </Text>

b9e9244d9c93f82d7e790f554020d0a04ee17a92
Suthesan <suthesan@Suthesans-MacBook-Pro.local>
2020-11-11 15:12:33 +0800

    add listener on cookie and end chat when client log out

--- src/components/layout/livechat.js
+++ src/components/layout/livechat.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Cookies from 'js-cookie'
 import styled from 'styled-components'
+import * as CustomerSDK from '@livechat/customer-sdk'
 import { isBrowser } from 'common/utility'
 import LiveChatIC from 'images/svg/livechat.svg'
 import LiveChatHover from 'images/svg/livechat-hover.svg'
@@ -26,6 +27,16 @@ const StyledLiveChat = styled.div`
 
 const LiveChat = ({ LC_API, is_livechat_interactive, setLiveChatInteractive }) => {
     const [is_livechat_hover, setLivechatHover] = React.useState(false)
+    const [is_logged_in, setLoggedIn] = React.useState(false)
+    const domain = (window.location.hostname.includes('deriv.com')
+        ? 'deriv.com'
+        : 'binary.sx'
+    )
+
+    const customerSDK = CustomerSDK.init({
+        licenseId: 12049137,
+        clientId: '66aa088aad5a414484c1fd1fa8a5ace7',
+    })
 
     const loadLiveChatScript = (callback) => {
         const livechat_script = document.createElement('script')
@@ -38,87 +49,122 @@ const LiveChat = ({ LC_API, is_livechat_interactive, setLiveChatInteractive }) =
         if (callback) callback()
     }
 
+    const checkCookie = function () {
+
+        let lastCookie = document.cookie; // 'static' memory between function calls
+
+        return function () {
+
+            const currentCookie = document.cookie;
+
+            if (currentCookie != lastCookie) {
+
+                const client_information = Cookies.get('client_information', {
+                    domain,
+                })
+
+                if (client_information) {
+                    setLoggedIn(true)
+                }
+                else {
+                    setLoggedIn(false)
+                }
+
+                // something useful like parse cookie, run a callback fn, etc.
+
+                lastCookie = currentCookie; // store latest cookie
+
+            }
+        };
+    }();
+
     React.useEffect(() => {
+
+        const cookie_interval = setInterval(checkCookie, 500);
         if (isBrowser()) {
             // The purpose is to load the script after everything is load but not async or defer. Therefore, it will be ignored in the rendering timeline
             setTimeout(() => {
                 loadLiveChatScript(() => {
                     window.LiveChatWidget.on('ready', () => {
                         setLiveChatInteractive(true)
-
-                        window.LiveChatWidget.on('visibility_changed', ({ visibility }) => {
-                            const domain = window.location.hostname.includes('deriv.com')
-                                ? 'deriv.com'
-                                : 'binary.sx'
-                            const client_information = Cookies.get('client_information', {
-                                domain,
-                            })
-                            // only visible to CS
-                            let session_variables = {
-                                loginid: '',
-                                landing_company_shortcode: '',
-                                currency: '',
-                                residence: '',
-                                email: '',
-                            }
-
-                            if (client_information) {
-                                const {
-                                    loginid,
-                                    email,
-                                    landing_company_shortcode,
-                                    currency,
-                                    residence,
-                                    first_name,
-                                    last_name,
-                                } = JSON.parse(client_information)
-
-                                if (visibility === 'maximized' && client_information) {
-                                    session_variables = {
-                                        ...(loginid && { loginid }),
-                                        ...(landing_company_shortcode && {
-                                            landing_company_shortcode,
-                                        }),
-                                        ...(currency && { currency }),
-                                        ...(residence && { residence }),
-                                        ...(email && { email }),
-                                    }
-
-                                    window.LiveChatWidget.call(
-                                        'set_session_variables',
-                                        session_variables,
-                                    )
-                                    if (email) {
-                                        window.LiveChatWidget.call('set_customer_email', email)
-                                    }
-                                    if (first_name && last_name) {
-                                        window.LiveChatWidget.call(
-                                            'set_customer_name',
-                                            `${first_name} ${last_name}`,
-                                        )
-                                    }
-                                }
-                            }
-
-                            if (visibility === 'maximized' && !client_information) {
-                                if (
-                                    window.LiveChatWidget.get('customer_data').status !==
-                                    'chatting'
-                                ) {
-                                    window.LiveChatWidget.call('set_customer_email', ' ')
-                                    window.LiveChatWidget.call('set_customer_name', ' ')
-                                }
-                                window.LiveChatWidget.call(
-                                    'set_session_variables',
-                                    session_variables,
-                                )
-                            }
-                        })
                     })
                 })
+
+                return () => {
+                    clearInterval(cookie_interval)
+                }
             }, 2000)
         }
     }, [])
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            loadLiveChatScript(() => {
+                window.LiveChatWidget.on('ready', () => {
+                    let session_variables = {
+                        loginid: '',
+                        landing_company_shortcode: '',
+                        currency: '',
+                        residence: '',
+                        email: '',
+                    }
+                    if (is_logged_in) {
+                        const client_information = Cookies.get('client_information', {
+                            domain,
+                        })
+                        const {
+                            loginid,
+                            email,
+                            landing_company_shortcode,
+                            currency,
+                            residence,
+                            first_name,
+                            last_name,
+                        } = JSON.parse(client_information)
+
+                        session_variables = {
+                            ...(loginid && { loginid }),
+                            ...(landing_company_shortcode && {
+                                landing_company_shortcode,
+                            }),
+                            ...(currency && { currency }),
+                            ...(residence && { residence }),
+                            ...(email && { email }),
+                        }
+
+                        window.LiveChatWidget.call(
+                            'set_session_variables',
+                            session_variables,
+                        )
+                        if (email) {
+                            window.LiveChatWidget.call('set_customer_email', email)
+                        }
+                        if (first_name && last_name) {
+                            window.LiveChatWidget.call(
+                                'set_customer_name',
+                                `${first_name} ${last_name}`,
+                            )
+                        }
+                    } else {
+                        if (window.LiveChatWidget.get('chat_data')) {
+                            const chatID = window.LiveChatWidget.get('chat_data').chatId;
+                            customerSDK
+                                .deactivateChat({ chatId: chatID })
+                                .then(response => {
+                                    console.log(response) //eslint-disable-line
+                                })
+                                .catch(error => {
+                                    console.log(error) //eslint-disable-line
+                                })
+                        }
+                        window.LiveChatWidget.call('set_customer_email', ' ')
+                        window.LiveChatWidget.call('set_customer_name', ' ')
+                    }
+                })
+            })
+        }, 2000)
+
+    }, [is_logged_in])
 
     return (
         <>

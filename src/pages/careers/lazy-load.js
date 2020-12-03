24f89a9d6f8c91a8cc6239975991ddb5f0cf4895
sean-binary <sean@deriv.com>
2020-12-03 15:06:57 +0800

    lazy loading non-crucial components

---
+++ src/pages/careers/lazy-load.js
@@ -0,0 +1,7 @@
+import Loadable from '@loadable/component'
+
+export const WhoWeLookFor = Loadable(() => import('./_who-we-look-for'))
+export const LifeAtDerivCarousel = Loadable(() => import('./_life-at-deriv-carousel'))
+export const OurValues = Loadable(() => import('./_our-values'))
+export const EmployeeTestimonial = Loadable(() => import('./_employee-testimonial-carousel'))
+export const OurHiringProcess = Loadable(() => import('./_our-hiring-process'))

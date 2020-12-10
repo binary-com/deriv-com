74e922fb7a55527e1812fa7c24fe9679a646071a
yashim-deriv <yashim@deriv.com>
2020-12-10 11:38:12 +0800

    Hide address from other pages as well

--- src/pages/careers/_model/_locations/_contact-details.js
+++ src/pages/careers/_model/_locations/_contact-details.js
@@ -23,7 +23,7 @@ const address_cyprus = ['Level 5, 42 Agias Filaxeos', 'Limassol 3025']
 
 const address_rwanda = ['Level 2 East Wing, Kigali Heights, KG7', 'Avenue, Kigali']
 
-const address_belarus = ['Duis 6,', '73 aute irure dolor in reprehenderit,', 'Minsk 3417']
+const address_belarus = ['', '', '']
 
 export {
     address_cyberjaya,

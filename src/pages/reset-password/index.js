ceb31fec099f5e94c27a83dd2d81e815f1465127
Mohammadreza Ghorbani <reza@firstsource.email>
2020-12-17 12:53:58 +0330

    Update Reset password design with the universal flow

--- src/pages/reset-password/index.js
+++ src/pages/reset-password/index.js
@@ -14,20 +14,28 @@ import Login from 'common/login'
 const StyledContainer = styled(Container)`
     text-align: center;
     height: 72rem;
-    padding: auto 0;
+    padding: 7rem 0 0;
 `
 
 const ButtonContainer = styled.div`
-    margin-top: 2rem;
+    margin-top: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 1rem;
 `
 
 const InputGroup = styled.div`
     width: 40rem;
-    margin: 0 auto 3.4rem;
+    margin: 0 auto 2rem;
 `
 
 const StyledButton = styled(Button)`
+    &:first-child {
+        justify-self: end;
+    }
+
     margin: 0.8rem 0.4rem;
+    width: max-content;
 `
 
 const resetValidation = (values) => {
@@ -70,75 +78,117 @@ const resetSubmission = (values, actions) => {
     }
 }
 
-const ResetPassword = () => (
-    <Layout type="static" margin_top="0">
-        <SEO
-            title={localize('Reset password | Deriv')}
-            description={localize(
-                'Forgot your Deriv password? Want to reset your password? Send us your email address and we’ll email you the instructions.',
-            )}
-            no_index
-        />
-        <StyledContainer justify="center" align="center" direction="column">
-            <Header as="h2" align="center">
-                {localize('Reset password')}
-            </Header>
-            <Header as="h4" align="center" weight="500" mt="0.5rem" mb="3.8rem">
-                {localize("We'll email you instructions to reset your password.")}
-            </Header>
-            <Formik
-                initialValues={{ email: '' }}
-                initialStatus={{}}
-                validate={resetValidation}
-                onSubmit={resetSubmission}
-            >
-                {({
-                    values,
-                    errors,
-                    handleChange,
-                    handleBlur,
-                    isSubmitting,
-                    resetForm,
-                    status,
-                }) => (
-                    <Form noValidate>
-                        <InputGroup>
-                            <Input
-                                id="email"
-                                name="email"
-                                error={errors.email}
-                                value={trimSpaces(values.email)}
-                                handleError={resetForm}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                autoComplete="off"
-                                type="text"
-                                label={localize('Email')}
-                                background="white"
-                                placeholder={'example@mail.com'}
-                                data-lpignore="true"
-                                required
-                            />
-                        </InputGroup>
-                        <Text align="center" color="red">
-                            {status.error && status.error}
-                        </Text>
-                        <Text align="center" color="green">
-                            {status.success && status.success}
-                        </Text>
-                        <ButtonContainer>
-                            <StyledButton tertiary onClick={Login.redirectToLogin} type="button">
-                                {localize('Return to log in')}
-                            </StyledButton>
-                            <StyledButton secondary="true" disabled={isSubmitting} type="submit">
-                                {localize('Reset my password')}
-                            </StyledButton>
-                        </ButtonContainer>
-                    </Form>
+const ResetPassword = () => {
+    const [has_prompt, setOnePasswordPrompt] = React.useState(true)
+    let body = null
+    if (!has_prompt) {
+        body = (
+            <StyledContainer mt="7rem" justify="flex-start" align="center" direction="column">
+                <Header as="h3" align="center">
+                    {localize('Reset password')}
+                </Header>
+                <Text align="center" mb="2rem">
+                    {localize("We'll email you instructions to reset your password.")}
+                </Text>
+                <Formik
+                    initialValues={{ email: '' }}
+                    initialStatus={{}}
+                    validate={resetValidation}
+                    onSubmit={resetSubmission}
+                >
+                    {({
+                        values,
+                        errors,
+                        handleChange,
+                        handleBlur,
+                        isSubmitting,
+                        resetForm,
+                        status,
+                    }) => (
+                        <Form noValidate>
+                            <InputGroup>
+                                <Input
+                                    id="email"
+                                    name="email"
+                                    error={errors.email}
+                                    value={trimSpaces(values.email)}
+                                    handleError={resetForm}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    autoComplete="off"
+                                    type="text"
+                                    label={localize('Email')}
+                                    background="white"
+                                    placeholder={'example@mail.com'}
+                                    data-lpignore="true"
+                                    required
+                                />
+                            </InputGroup>
+                            <Text align="center" color="red">
+                                {status.error && status.error}
+                            </Text>
+                            <Text align="center" color="green">
+                                {status.success && status.success}
+                            </Text>
+                            <ButtonContainer>
+                                <StyledButton
+                                    tertiary
+                                    onClick={Login.redirectToLogin}
+                                    type="button"
+                                >
+                                    {localize('Return to log in')}
+                                </StyledButton>
+                                <StyledButton
+                                    secondary="true"
+                                    disabled={isSubmitting}
+                                    type="submit"
+                                >
+                                    {localize('Reset my password')}
+                                </StyledButton>
+                            </ButtonContainer>
+                        </Form>
+                    )}
+                </Formik>
+            </StyledContainer>
+        )
+    } else {
+        body = (
+            <StyledContainer mt="7rem" justify="flex-start" align="center" direction="column">
+                <Header as="h3" align="center" mb="2rem">
+                    {localize('All you’ll need from now is one password')}
+                </Header>
+                <Text align="center" mb="2rem">
+                    {localize(
+                        'We’ve upgraded our system to support a single, more secure password across all of Deriv/Binary.com. Once you’ve set a new password, you can use it to log into all your Deriv/Binary.com, and DMT5/MT5 accounts.',
+                    )}
+                </Text>
+                <ButtonContainer>
+                    <StyledButton tertiary onClick={Login.redirectToLogin} type="button">
+                        {localize('Return to log in')}
+                    </StyledButton>
+                    <StyledButton
+                        secondary="true"
+                        type="button"
+                        onClick={() => setOnePasswordPrompt(false)}
+                    >
+                        {localize('Reset my password')}
+                    </StyledButton>
+                </ButtonContainer>
+            </StyledContainer>
+        )
+    }
+    return (
+        <Layout type="static" margin_top="0">
+            <SEO
+                title={localize('Reset password | Deriv')}
+                description={localize(
+                    'Forgot your Deriv password? Want to reset your password? Send us your email address and we’ll email you the instructions.',
                 )}
-            </Formik>
-        </StyledContainer>
-    </Layout>
-)
+                no_index
+            />
+            {body}
+        </Layout>
+    )
+}
 
 export default WithIntl()(ResetPassword)

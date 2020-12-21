e035fe069a319338d73912d6aa3e6fe1545cdae7
Kevin <kevin@regentmarkets.com>
2020-12-21 13:42:35 +0800

    fix:translate text

--- src/pages/trader-tools/_validation.js
+++ src/pages/trader-tools/_validation.js
@@ -1,4 +1,4 @@
-import { localize } from 'components/localization'
+import { localize, Localize } from 'components/localization'
 
 const validation_regex = {
     number: /^\d*\.?\d+$/,
@@ -25,7 +25,8 @@ const validation_is_not_zero = (input) => {
 
 const numberValidation = (input, fieldName, maxDigit) => {
     if (!input) {
-        return localize(`${fieldName} is required`)
+        // eslint-disable-next-line react/react-in-jsx-scope
+        return <Localize translate_text="{{fieldName}} is required" values={{ fieldName }} />
     } else if (!validation_regex.number.test(input)) {
         return localize('Should be a valid number')
     } else if (!validation_is_exceed_number(input, maxDigit)) {
@@ -39,7 +40,8 @@ const numberValidation = (input, fieldName, maxDigit) => {
 
 const numberWithNegativeValidation = (input, fieldName, maxDigit) => {
     if (!input) {
-        return localize(`${fieldName} is required`)
+        // eslint-disable-next-line react/react-in-jsx-scope
+        return <Localize translate_text="{{fieldName}} is required" values={{ fieldName }} />
     } else if (!validation_regex.numberWithNegative.test(input)) {
         return localize('Should be a valid number')
     } else if (!validation_is_exceed_number(input, maxDigit)) {

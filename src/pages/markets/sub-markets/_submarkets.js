9db9077edcf83aadd3d7f7456ba64656dd7e3cff
AbdallaMourad <abdullah.f98m@gmail.com>
2020-12-28 11:39:15 +0800

    Add: new instruments to Markets->Forex->margin

--- src/pages/markets/sub-markets/_submarkets.js
+++ src/pages/markets/sub-markets/_submarkets.js
@@ -1,33 +1,37 @@
-import MajorPairs from './_major-pairs.js'
-import MinorPairs from './_minor-pairs.js'
-import SmartFX from './_smartfx.js'
-import ExoticPairs from './_exotic-pairs.js'
-import VolatilityIndices from './_volatility-indices.js'
-import DailyResetIndices from './_daily-reset-indices.js'
-import CrashBoom from './_crash-boom.js'
-import RangeBreak from './_range-break.js'
-import StepIndices from './_step-indices.js'
-import ContinuousIndices from './_continuous-indices.js'
 import Americas from './_americas.js'
 import AsiaOceania from './_asia-oceania.js'
-import Europe from './_europe.js'
-import Metals from './_metals.js'
+import ContinuousIndices from './_continuous-indices.js'
+import CrashBoom from './_crash-boom.js'
+import DailyResetIndices from './_daily-reset-indices.js'
 import Energy from './_energy.js'
+import Europe from './_europe.js'
+import ExoticPairs from './_exotic-pairs.js'
+import MajorPairs from './_major-pairs.js'
+import Metals from './_metals.js'
+import MinorPairs from './_minor-pairs.js'
+import RangeBreak from './_range-break.js'
+import SmartFX from './_smartfx.js'
+import SmartGoldIndex from './_smart-gold-index'
+import StepFX from './_stepfx'
+import StepIndices from './_step-indices.js'
+import VolatilityIndices from './_volatility-indices.js'
 
 export {
-    MajorPairs,
-    MinorPairs,
-    SmartFX,
-    ExoticPairs,
-    VolatilityIndices,
-    DailyResetIndices,
-    CrashBoom,
-    StepIndices,
-    ContinuousIndices,
     Americas,
     AsiaOceania,
-    Europe,
-    Metals,
+    ContinuousIndices,
+    CrashBoom,
+    DailyResetIndices,
     Energy,
+    Europe,
+    ExoticPairs,
+    MajorPairs,
+    Metals,
+    MinorPairs,
     RangeBreak,
+    SmartFX,
+    SmartGoldIndex,
+    StepFX,
+    StepIndices,
+    VolatilityIndices,
 }

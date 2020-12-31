13d486c71471ac58f69c609427d706b7c9a042bb
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2020-12-31 18:31:05 +0600

    feat: add CFD warning banner on top of responsible trading page

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -69,6 +69,7 @@ const Layout = ({
     no_login_signup,
     no_live_chat,
     nav_type,
+    CompotentAbove,
 }) => {
     const { is_eu_country } = React.useContext(DerivStore)
     const [has_mounted, setMounted] = React.useState(false)
@@ -154,7 +155,7 @@ const Layout = ({
             FooterNav = <Copyright />
             break
         default:
-            Navigation = <Nav />
+            Navigation = <Nav CompotentAbove={CompotentAbove} />
             FooterNav = <Footer />
             break
     }
@@ -203,6 +204,7 @@ const Layout = ({
 
 Layout.propTypes = {
     children: PropTypes.node.isRequired,
+    CompotentAbove: PropTypes.elementType,
     interim_type: PropTypes.string,
     margin_top: PropTypes.number,
     nav_type: PropTypes.string,

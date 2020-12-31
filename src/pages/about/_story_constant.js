5e9f3eb70e9ec16b526ed25e42e1b63284c47f48
Abdalla Mourad <abdullah.f98m@gmail.com>
2020-12-31 13:40:19 +0800

    Update: about - why-choose-us (#1301)

--- src/pages/about/_story_constant.js
+++ src/pages/about/_story_constant.js
@@ -162,7 +162,7 @@ const Stories = [
                 texts: [
                     {
                         text: (
-                            <Localize translate_text="The Malta Financial Services Authority grants Binary.com a Category 3 Investment Services licence. The licence authorises Binary.com to offer our service to clients in the European Union as a financial product." />
+                            <Localize translate_text="The Malta Financial Services Authority (MFSA) grants Binary.com a Category 3 Investment Services licence. This licence authorises Binary.com to offer investment services to clients in the European Union." />
                         ),
                     },
                 ],

d4d3bc57dce08ca28b1937efd270b277773fea5b
Mahdi Aryayi <mahdi.aryayi@firstsource.io>
2020-12-31 13:27:33 +0330

    Replace normal IBM plex font with variable font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,7 +9,7 @@ import { isMobile } from './src/common/os-detect'
 import { gtm_test_domain } from './src/common/utility'
 import { MediaContextProvider } from './src/themes/media'
 import { DerivProvider } from './src/store'
-import 'typeface-ibm-plex-sans'
+import './static/css/ibm-plex-sans-var.css';
 
 const is_browser = typeof window !== 'undefined'
 

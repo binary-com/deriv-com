a0966127021cb481961c29952d3557c4534b8f00
AbdallaMourad <abdullah.f98m@gmail.com>
2021-01-07 17:13:52 +0800

    Finish: designing why deriv section

--- src/pages/be-square/static/components/_why-deriv.js
+++ src/pages/be-square/static/components/_why-deriv.js
@@ -9,13 +9,13 @@ const WhyDeriv = () => (
             <ImageWrapper src={WhyDerivImage} alt="deriv employee" grid_area="employee-image" />
             <div>
                 <Title>{localize('Why Deriv?')}</Title>
-                <TextWrapper grid_area="text-1" margin_m="8px 0 24px">
+                <TextWrapper grid_area="text-1" margin_m="8px 0 24px" margin="8px 0 0">
                     {localize(
                         'A tech powerhouse, Deriv has succeeded in bringing together technology and finance to meet the needs of a global client base for over 20 years. Our products include derivative trading platforms and synthetic markets, delivered to our clients via trading apps and websites. Our teams consist of talented professionals based in 7 countries: Malaysia (Cyberjaya, Labuan, Melaka, and Ipoh), Malta, the United Arab Emirates, Paraguay, Rwanda, Cyprus, and Belarus.',
                     )}
                 </TextWrapper>
             </div>
-            <TextWrapper grid_area="text-2" margin_m="40px 0 24px">
+            <TextWrapper grid_area="text-2" margin_m="40px 0 24px" margin="auto 0">
                 {localize(
                     'At Deriv, you can learn from our talented IT teams of dedicated innovators, receive international exposure by collaborating with the overseas offices on projects that will challenge and excite you, work with state-of-the-art electronic devices, and enjoy the facilities of Deriv’s prestigious new headquarters at Quill 5 in Cyberjaya, where BeSquare candidates will have a whole floor at their disposal.',
                 )}

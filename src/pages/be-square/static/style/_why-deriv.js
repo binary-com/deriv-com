55af4b2cfa2cc8e809d7ab1cd6b73106c068ab9a
AbdallaMourad <abdullah.f98m@gmail.com>
2021-01-07 17:13:52 +0800

    Finish: designing why deriv section

--- src/pages/be-square/static/style/_why-deriv.js
+++ src/pages/be-square/static/style/_why-deriv.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import { Flex } from 'components/containers'
 import { Header, Text } from 'components/elements'
 import device from 'themes/device'
 
@@ -7,16 +6,18 @@ export const Container = styled.div`
     box-shadow: inset 0 1px 0 rgba(51, 51, 51, 0.16);
 `
 
-export const ContentContainer = styled(Flex)`
+export const ContentContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 486px);
+    grid-row-gap: 40px;
+    grid-column-gap: 24px;
     justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
     padding: 80px 0;
 
     @media ${device.tablet} {
-        display: grid;
         grid-template-columns: auto;
         grid-template-areas: 'title' 'text-1' 'employee-image' 'text-2' 'quil5-image';
+        gap: 0;
         padding: 40px 0;
     }
 `
@@ -26,9 +27,9 @@ export const Title = styled(Header)`
     font-weight: 700;
     line-height: 60px;
     color: var(--color-black-3);
-    grid-area: title;
 
     @media ${device.tablet} {
+        grid-area: title;
         font-size: 28px;
         line-height: 34px;
     }
@@ -41,23 +42,24 @@ export const TextWrapper = styled(Text)`
     font-weight: 400;
     line-height: 30px;
     color: var(--color-black-3);
-    grid-area: ${({ grid_area }) => grid_area};
-    margin: ${({ margin_m }) => margin_m ?? 0};
+    margin: ${({ margin }) => margin ?? 0};
 
     @media ${device.tablet} {
         max-width: 328px;
         font-size: 14px;
         line-height: 20px;
+        margin: ${({ margin_m }) => margin_m ?? 0};
+        grid-area: ${({ grid_area }) => grid_area};
     }
 `
 
 export const ImageWrapper = styled.img`
     max-width: 486px;
     max-height: 384px;
-    grid-area: ${({ grid_area }) => grid_area};
 
     @media ${device.tablet} {
         max-width: 320px;
         max-height: 259px;
+        grid-area: ${({ grid_area }) => grid_area};
     }
 `

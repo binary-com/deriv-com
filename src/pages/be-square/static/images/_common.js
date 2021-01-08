839630115a04b003a3b19f4578ffddedcc1e52a9
AbdallaMourad <abdullah.f98m@gmail.com>
2021-01-08 13:45:03 +0800

    Add: images directory

---
+++ src/pages/be-square/static/images/_common.js
@@ -0,0 +1,3 @@
+import MarkIcon from 'images/svg/be-square/mark-icon.svg'
+
+export { MarkIcon }
---
+++ src/pages/be-square/static/images/_why-deriv.js
@@ -0,0 +1,4 @@
+import WhyDerivImage from 'images/common/be-square/why-deriv.png'
+import Quil5Image from 'images/common/be-square/quil5.png'
+
+export { Quil5Image, WhyDerivImage }

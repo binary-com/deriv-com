337ae67a6fd9388c09864f4cfb95d9a4ccb5ab80
AbdallaMourad <abdullah.f98m@gmail.com>
2021-01-14 17:10:19 +0800

    Amend: What lies ahead section

--- src/pages/be-square/static/components/_what-lies-ahead.js
+++ src/pages/be-square/static/components/_what-lies-ahead.js
@@ -1,57 +1,51 @@
 import React from 'react'
 import {
     CardContainer,
-    CardWrapper,
     Container,
     ContentContainer,
     TextWrapper,
     Title,
     TitleWrapper,
 } from '../style/_what-lies-ahead'
+import { what_lies_ahead_content } from '../helper/_card-content'
+import Card from './_card'
 import { localize } from 'components/localization'
 
-const WhatLiesAhead = () => (
-    <Container>
-        <ContentContainer>
-            <TitleWrapper>
-                <Title>{localize('What lies ahead')}</Title>
+const WhatLiesAhead = () => {
+    return (
+        <Container>
+            <ContentContainer>
+                <TitleWrapper>
+                    <Title>{localize('What lies ahead')}</Title>
+                    <TextWrapper
+                        max_width={['792px', '328px']}
+                        font_size={['24px', '18px']}
+                        line_height={['36px', '26px']}
+                        text_align={'center'}
+                    >
+                        {localize(
+                            "Over the course of 6 months, you'll go through 10 modules and face exciting challenges",
+                        )}
+                    </TextWrapper>
+                </TitleWrapper>
+                <CardContainer>
+                    {what_lies_ahead_content.map((card, index) => (
+                        <Card key={index} card_content={card} />
+                    ))}
+                </CardContainer>
                 <TextWrapper
-                    max_width={['792px', '328px']}
-                    font_size={['24px', '18px']}
-                    line_height={['36px', '26px']}
+                    max_width={['791px', '328px']}
+                    font_size={['16px', '14px']}
+                    line_height={['24px', '20px']}
                     text_align={'center'}
                 >
                     {localize(
-                        "Over the course of 6 months, you'll go through 10 modules and face exciting challenges",
+                        'Besides these hard skills, you’ll learn essential soft skills that are based on Harvard Business Review principles. You’ll also get the chance to extend your professional network and connect with veteran techies as they share their professional experience.',
                     )}
                 </TextWrapper>
-            </TitleWrapper>
-            <CardContainer>
-                <CardWrapper>
-                    <img width="48" height="48" />
-                    <TextWrapper
-                        max_width={['266px', '200px']}
-                        font_size={['24px', '18px']}
-                        line_height={['36px', '26px']}
-                    >
-                        {localize('Front-end development')}
-                    </TextWrapper>
-                    <img width="32" height="32" />
-                </CardWrapper>
-            </CardContainer>
-
-            <TextWrapper
-                max_width={['791px', '328px']}
-                font_size={['16px', '14px']}
-                line_height={['24px', '20px']}
-                text_align={'center'}
-            >
-                {localize(
-                    'Besides these hard skills, you’ll learn essential soft skills that are based on Harvard Business Review principles. You’ll also get the chance to extend your professional network and connect with veteran techies as they share their professional experience.',
-                )}
-            </TextWrapper>
-        </ContentContainer>
-    </Container>
-)
+            </ContentContainer>
+        </Container>
+    )
+}
 
 export default WhatLiesAhead
--- src/pages/be-square/static/style/_what-lies-ahead.js
+++ src/pages/be-square/static/style/_what-lies-ahead.js
@@ -53,7 +53,7 @@ export const TextWrapper = styled(Text)`
     max-width: ${({ max_width }) => (max_width ? max_width[0] : 'unset')};
     width: fit-content;
     font-size: ${({ font_size }) => (font_size ? font_size[0] : 'unset')};
-    font-weight: 400;
+    font-weight: ${({ font_weight }) => font_weight ?? 400};
     line-height: ${({ line_height }) => (line_height ? line_height[0] : 'unset')};
     text-align: ${({ text_align }) => text_align ?? 'unset'};
     color: var(--color-black-3);
@@ -70,17 +70,59 @@ export const CardContainer = styled.div`
     display: grid;
     align-items: center;
     justify-content: center;
+    grid-row-gap: 22px;
+
+    @media ${device.tablet} {
+        grid-row-gap: 17px;
+    }
 `
 
 export const CardWrapper = styled.div`
     display: grid;
-    grid-template-columns: auto auto auto;
+    width: 800px;
+    grid-template-columns: 48px 84% 32px;
     grid-column-gap: 22px;
-    background-color: var(--color-grey-30);
     align-items: center;
-    box-shadow: inset 0 1px 0 #d6d7d8;
+    box-shadow: inset 0 -1px 0 var(--color-grey-34);
+    padding-bottom: 22px;
+
+    :last-child {
+        margin-bottom: 32px;
+    }
 
     @media ${device.tablet} {
-        grid-template-columns: 32px auto 24px;
+        width: 328px;
+        background-color: var(--color-grey-33);
+        grid-template-columns: 32px 77.9% 24px;
+        grid-column-gap: 8px;
+        padding-bottom: 17px;
+
+        :last-child {
+            margin-bottom: 17px;
+        }
+    }
+`
+
+export const DropdownWrapper = styled.img`
+    width: 32px;
+    height: 32px;
+
+    :hover {
+        cursor: pointer;
+    }
+
+    @media ${device.tablet} {
+        width: 24px;
+        height: 24px;
+    }
+`
+
+export const IconWrapper = styled.img`
+    width: 48px;
+    height: 48px;
+
+    @media ${device.tablet} {
+        width: 32px;
+        height: 32px;
     }
 `

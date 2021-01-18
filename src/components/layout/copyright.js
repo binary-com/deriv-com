8b2d771d4d5c0f828c57a15a0d52a94aabb38d71
Kevin <kevin@regentmarkets.com>
2021-01-18 13:04:13 +0800

    update footer copy 2021

--- src/components/layout/copyright.js
+++ src/components/layout/copyright.js
@@ -5,12 +5,14 @@ import { localize } from 'components/localization'
 import CopyrightIc from 'images/svg/copyright-white.svg'
 
 const Copyright = () => {
+    const current_year = new Date().getFullYear()
+
     return (
         <Box bg="var(--color-black)">
             <Container jc="flex-start" p="1.5rem 0" ai="center">
                 <img src={CopyrightIc} alt="copyright ic" width="16" height="16" />
                 <Text color="white" ml="0.4rem">
-                    {localize('2020 Deriv | All rights reserved')}
+                    {current_year} {localize(' Deriv | All rights reserved')}
                 </Text>
             </Container>
         </Box>

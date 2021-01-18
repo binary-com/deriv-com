eb464da2798749a3249ee2a8e0b181118d85e400
fiona-deriv <76169421+fiona-deriv@users.noreply.github.com>
2021-01-18 05:25:51 +0200

    remove help center questions obsolete design (#1352)
    Co-authored-by: FionaK <katefiona121@gmail.com>

--- src/components/elements/side-tab.js
+++ src/components/elements/side-tab.js
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
 import { Text, Header } from './typography'
 import device, { size } from 'themes/device'
 import { getWindowWidth } from 'common/utility'
 import { Box } from 'components/containers'
-import { Desktop, Mobile } from 'components/containers/show'
+import { Desktop } from 'components/containers/show'
 import { useTabState } from 'components/hooks/use-tab-state'
-import { ReactComponent as Chevron } from 'images/svg/chevron.svg'
 
 const StyledSideTab = styled(Box)`
     padding: 0;
@@ -67,9 +66,7 @@ const TabsText = css`
     font-size: var(--text-size-m);
     color: var(--color-red);
 `
-const StyledActiveTabText = styled(Text)`
-    ${TabsText}
-`
+
 const StyledDropDown = styled.li`
     padding: 1rem 0;
     border-bottom: 1px solid var(--color-red);
@@ -81,12 +78,7 @@ const StyledDropDown = styled.li`
         ${TabsText}
     }
 `
-const ChevronWrapper = styled(Chevron)`
-    min-width: 16px;
-    width: 16px;
-    height: 16px;
-    transform: ${(props) => (props.active_tab === '-' ? 'rotate(0deg)' : 'rotate(180deg)')};
-`
+
 const ItemWrapper = styled.div`
     display: flex;
     flex-direction: row;
@@ -114,17 +106,10 @@ const Tab = ({ active_tab, label, onClick, text, mobile, font_size }) => {
 }
 
 const getTabs = (children) => children.map((child) => child.props.label)
-const findCurrentTab = (children, active_tab) =>
-    children.find((child) => child.props.label === active_tab)
 
 const SideTab = ({ children, is_sticky, tab_header, font_size }) => {
     const [active_tab, setActiveTab] = useTabState(getTabs(children))
-    const [current_active_tab, setCurrentActiveTab] = useState(findCurrentTab(children, active_tab))
     const [is_menu, setMenu] = useState(false)
-
-    useEffect(() => {
-        setCurrentActiveTab(!is_menu ? findCurrentTab(children, active_tab) : null)
-    }, [active_tab, is_menu])
 
     const Tabs = (props) => {
         return children.map((child, idx) => {
@@ -162,17 +147,6 @@ const SideTab = ({ children, is_sticky, tab_header, font_size }) => {
                     )}
                     <Tabs />
                 </Desktop>
-                <Mobile>
-                    <StyledDropDown onClick={() => setMenu(!is_menu)}>
-                        {current_active_tab && (
-                            <StyledActiveTabText>
-                                {current_active_tab.props.text}
-                            </StyledActiveTabText>
-                        )}
-                        <ChevronWrapper active_tab={active_tab} />
-                    </StyledDropDown>
-                    {is_menu && <Tabs is_mobile={true} />}
-                </Mobile>
             </TabList>
             <TabContent>
                 {(!is_menu || getWindowWidth() >= size.tabletL) &&

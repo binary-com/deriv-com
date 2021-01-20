9346b717cdc4de5398406cecf2cb31b506826cc9
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2021-01-20 14:41:34 +0600

    fix: link text become black in career page as a side effect of #15248

--- src/pages/regulatory/index.js
+++ src/pages/regulatory/index.js
@@ -239,6 +239,10 @@ const Regulatory = () => {
                                                 target="_blank"
                                                 to="https://beta.gamblingcommission.gov.uk/public-register/business/detail/39495"
                                                 rel="noopener noreferrer"
+                                                style={{
+                                                    color: `var(--color-black-3)`,
+                                                    textDecoration: `none`,
+                                                }}
                                             />,
                                         ]}
                                     />
@@ -282,6 +286,10 @@ const Regulatory = () => {
                                                 target="_blank"
                                                 to="https://beta.gamblingcommission.gov.uk/public-register/business/detail/39172"
                                                 rel="noopener noreferrer"
+                                                style={{
+                                                    color: `var(--color-black-3)`,
+                                                    textDecoration: `none`,
+                                                }}
                                             />,
                                         ]}
                                     />

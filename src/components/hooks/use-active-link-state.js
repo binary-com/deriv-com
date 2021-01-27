b661fa7482e235afc33228ba99e4b692cc024c7c
yashim-deriv <yashim@deriv.com>
2021-01-27 12:56:23 +0800

    Format fix: snake case
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>

--- src/components/hooks/use-active-link-state.js
+++ src/components/hooks/use-active-link-state.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { getLocationPathname } from 'common/utility'
 
 // Maps navigation main elements (keys) to root pages (values)
-const navigationMap = {
+const navigation_map = {
     trade: ['dtrader', 'dbot', 'dmt5', 'trade-types'],
     markets: ['markets'],
     about: ['about', 'regulatory', 'why-choose-us', 'partners', 'contact-us', 'careers'],
@@ -15,8 +15,8 @@ export const useActiveLinkState = () => {
     useEffect(() => {
         let current_root_page = getLocationPathname().match(/\/([a-zA-Z0-9-]*)/)
         if (!current_root_page) return
-        Object.keys(navigationMap).forEach((key) => {
-            if (navigationMap[key].includes(current_root_page[1])) {
+        Object.keys(navigation_map).forEach((key) => {
+            if (navigation_map[key].includes(current_root_page[1])) {
                 setCurrentPage(key)
             }
         })

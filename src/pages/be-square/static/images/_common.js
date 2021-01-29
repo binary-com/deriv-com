5c3332cc1acf558249077cd9b97d442ee8c2ba43
AbdallaMourad <abdullah.f98m@gmail.com>
2021-01-29 10:33:42 +0800

    Change: Directory be-square -> besquare

--- src/pages/be-square/static/images/_common.js
+++
@@ -1,4 +0,0 @@
-import GreenMarkIcon from 'images/svg/be-square/green-mark-icon.svg'
-import RedMarkIcon from 'images/svg/be-square/red-mark-icon.svg'
-
-export { GreenMarkIcon, RedMarkIcon }
--- src/pages/be-square/static/images/_join-us.js
+++
@@ -1,6 +0,0 @@
-import ExtensiveNetworkIcon from 'images/svg/be-square/extensive-network.svg'
-import TechExperienceIcon from 'images/svg/be-square/tech-experience.svg'
-import ResumeIcon from 'images/svg/be-square/resume.svg'
-import ProfessionalGuidanceIcon from 'images/svg/be-square/professional-guidance.svg'
-
-export { ExtensiveNetworkIcon, ProfessionalGuidanceIcon, ResumeIcon, TechExperienceIcon }

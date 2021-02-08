f1d526e99fc34739e62a07ef6cb4d4fd624bd602
Kevin <kevin@regentmarkets.com>
2021-02-08 16:55:25 +0800

    remove: unused id

--- src/pages/regulatory/data/_kid_data.js
+++ src/pages/regulatory/data/_kid_data.js
@@ -5,22 +5,18 @@ const kid_data = [
     {
         title: <Localize translate_text="Commodities" />,
         ref: 'kid_deriv_commodities.pdf',
-        id: 1,
     },
     {
         title: <Localize translate_text="Forex" />,
         ref: 'kid_deriv_forex.pdf',
-        id: 2,
     },
     {
         title: <Localize translate_text="Cryptocurrency" />,
         ref: 'kid_deriv_crypto.pdf',
-        id: 3,
     },
     {
         title: <Localize translate_text="Multipliers" />,
         ref: 'kid_deriv_multipliers.pdf',
-        id: 4,
     },
 ]
 

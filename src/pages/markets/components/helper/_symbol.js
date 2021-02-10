76fcd94000b9aba9b6a965f9e963826ec7627fb2
Abdalla Mourad <abdullah.f98m@gmail.com>
2021-02-10 19:12:34 +0800

    Add: instruments to forex -> margin (#1432)

--- src/pages/markets/components/helper/_symbol.js
+++ src/pages/markets/components/helper/_symbol.js
@@ -3,14 +3,29 @@ import PropTypes from 'prop-types'
 import { SymbolContainer } from '../../static/style/_markets-style'
 import { Text } from 'components/elements'
 
-const Symbol = ({ src, text }) => (
-    <SymbolContainer>
-        <img src={src} />
-        <Text>{text}</Text>
-    </SymbolContainer>
+//TODO: refactor this component to always use instruments_type
+const Symbol = ({ instruments_type, src, text }) => (
+    <React.Fragment>
+        {instruments_type ? (
+            <React.Fragment>
+                {instruments_type.map((symbol, index) => (
+                    <SymbolContainer key={index}>
+                        <img src={symbol.src} />
+                        <Text>{symbol.text}</Text>
+                    </SymbolContainer>
+                ))}
+            </React.Fragment>
+        ) : (
+            <SymbolContainer>
+                <img src={src} />
+                <Text>{text}</Text>
+            </SymbolContainer>
+        )}
+    </React.Fragment>
 )
 
 Symbol.propTypes = {
+    instruments_type: PropTypes.object,
     src: PropTypes.string,
     text: PropTypes.string,
 }

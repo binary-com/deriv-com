cd6030e6628ae9067dcc931acd67a5f3ee93622b
fiona-deriv <fiona@deriv.com>
2021-02-11 20:58:47 +0200

    add deriv-go page

---
+++ src/pages/deriv-go/components/_dgo-hero.js
@@ -0,0 +1,315 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { graphql, useStaticQuery } from 'gatsby'
+import { localize } from 'components/localization'
+import { Flex } from 'components/containers'
+import { Header, QueryImage } from 'components/elements'
+import { LinkButton } from 'components/form'
+import device from 'themes/device.js'
+import Dgoogle_play_Logo from '/home/deriv/Desktop/deriv-com/src/images/common/derivgo/derivgo_gogleplay.png'
+
+const Wrapper = styled.div`
+    position: relative;
+    background-color: var(--color-blue);
+    width: 100%;
+    display: flex;
+    height: 575px;
+    padding: 12rem 12rem 9rem 12rem;
+
+    @media ${device.laptopM} {
+        max-height: 429px;
+        padding: 8rem 12rem 8rem 4rem;
+    }
+    @media ${device.tabletL} {
+        min-height: 304px;
+        flex-direction: column;
+    }
+    @media ${device.tablet} {
+        flex-direction: column-reverse;
+        max-height: 640px;
+        padding: 0 16px 40px 16px;
+    }
+`
+
+const HeroContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+
+    ${Header} {
+        color: var(--color-white);
+        display: flex;
+        margin-top: 1.5rem;
+        line-height: 1.25;
+    }
+
+    @media ${device.laptopM} {
+        ${Header} {
+            font-size: 32px;
+        }
+    }
+`
+const StyledHeader = styled(Header)`
+    color: var(--color-white);
+    display: flex;
+    margin-top: 0;
+    font-size: 2.4rem;
+
+    @media ${device.laptopM} {
+        font-size: 24px;
+    }
+`
+
+const LottieWrapper = styled.div`
+    width: 597px;
+    max-width: 384px;
+    position: absolute;
+    top: 10.8rem;
+    right: 12rem;
+
+    @media ${device.laptopM} {
+        max-width: 500px;
+        top: 8rem;
+        right: 8rem;
+    }
+    @media ${device.laptop} {
+        max-width: 450px;
+        right: 10rem;
+    }
+    @media ${device.tabletL} {
+        max-width: 390px;
+        top: 5rem;
+        right: 2rem;
+    }
+    @media ${device.tabletS} {
+        max-width: 380px;
+        top: 5rem;
+        right: 2rem;
+    }
+    @media ${device.mobileL} {
+        max-width: 328px;
+        top: 34px;
+        left: 50%;
+        right: unset;
+        transform: translateX(-50%);
+    }
+    @media ${device.mobileM} {
+        max-width: 280px;
+        top: 34px;
+        left: 50%;
+        right: unset;
+        transform: translateX(-50%);
+    }
+`
+
+const LinkWrapper = styled.div`
+    display: flex;
+    margin-top: 3.2rem;
+
+    @media (max-width: 1420px) {
+        top: 480px;
+    }
+    @media ${device.laptopM} {
+        top: 350px;
+    }
+    @media ${device.tabletL} {
+        top: 236px;
+    }
+    @media ${device.tablet} {
+        position: unset;
+        top: unset;
+        justify-content: start;
+    }
+    @media (max-width: 360px) {
+        flex-wrap: wrap;
+    }
+`
+
+const IosComingSoon = styled(LinkButton)`
+    color: var(--color-white);
+    width: auto;
+
+    @media ${device.mobileL} {
+        max-width: 100%;
+        white-space: nowrap;
+        margin-left: 0;
+    }
+
+    @media (max-width: 360px) {
+        white-space: nowrap;
+    }
+`
+
+const StyledContent = styled(Header)`
+    font-size: 6.4rem;
+
+    @media (max-width: 1322px) {
+        font-size: 4.2rem;
+    }
+`
+const NormalContent = styled(Header)`
+    font-size: 2.4rem;
+
+    @media (max-width: 1322px) {
+        font-size: 1.2rem;
+    }
+`
+const InformationWrapper = styled(Flex)`
+    width: 100%;
+    max-width: 562px;
+
+    @media (max-width: 1320px) {
+        max-width: 450px;
+    }
+    @media ${device.laptop} {
+        max-width: 390px;
+    }
+    @media ${device.laptopM} {
+        max-width: 410px;
+    }
+    @media ${device.tabletL} {
+        width: 46%;
+        max-width: 400px;
+        top: 60px;
+    }
+    @media ${device.tablet} {
+        width: 100%;
+        top: 350px;
+    }
+    @media ${device.mobileL} {
+        top: 280px;
+        max-width: 328px;
+    }
+    @media ${device.mobileM} {
+        top: 230px;
+    }
+`
+
+const query = graphql`
+    query {
+        mobile_float: file(relativePath: { eq: "mobile_float.png" }) {
+            childImageSharp {
+                fluid(maxWidth: 1024, srcSetBreakpoints: [1440]) {
+                    ...GatsbyImageSharpFluid_withWebp_noBase64
+                    originalName
+                }
+            }
+        }
+    }
+`
+
+const DGoHero = ({
+    background_alt,
+    content,
+    description,
+    google_play,
+    image_name,
+    is_mobile,
+    ios_coming_soon,
+    Logo,
+}) => {
+    const data = useStaticQuery(query)
+    // const getRedirectLink = () => {
+    //     const path = image_name === 'dbot' ? '/bot' : '/'
+    //     return path
+    // }
+
+    // const BackgroundSVG = styled.img`
+    //     position: absolute;
+    //     top: 0;
+    //     right: 0;
+    //     height: 100%;
+
+    //     @media ${device.laptopM} {
+    //         width: 100%;
+    //         max-width: 1440px;
+    //         height: initial;
+    //     }
+    //     @media ${device.laptop} {
+    //         width: 100%;
+    //     }
+    //     @media ${device.tabletL} {
+    //         width: 100%;
+    //         max-width: 350px;
+    //     }
+    //     @media ${device.tablet} {
+    //         width: 100%;
+    //     }
+    //     @media ${device.tabletS} {
+    //         width: 80%;
+    //         max-width: 337px;
+    //     }
+    //     @media ${device.mobileL} {
+    //         max-width: 250px;
+    //         min-height: 244px;
+    //     }
+    //     @media ${device.mobileM} {
+    //         max-width: 205px;
+    //         min-height: 0;
+    //     }
+    // `
+    const DLogo = styled.img`
+        width: 213px !important;
+        height: 28px !important;
+        margin-right: 1.6rem;
+    `
+    const Dgoogle_play = styled.img`
+        width: 138px !important;
+        height: 40px !important;
+        margin-right: 1.6rem;
+    `
+    return (
+        <Wrapper>
+            <InformationWrapper height="unset" direction="column">
+                <StyledHeader as="h4" weight="normal">
+                    <DLogo src={Logo} alt="logo" width="213" height="27" />
+                </StyledHeader>
+                <HeroContent>
+                    <StyledContent as="h1">{content}</StyledContent>
+                    <NormalContent>{description}</NormalContent>
+                </HeroContent>
+                <LinkWrapper>
+                    {google_play && (
+                        <Dgoogle_play
+                            src={Dgoogle_play_Logo}
+                            alt="Get it on Google Play"
+                            width="138"
+                            height="40"
+                        />
+                    )}
+                    {ios_coming_soon && (
+                        <IosComingSoon>{localize('( iOS coming soon )')}</IosComingSoon>
+                    )}
+                </LinkWrapper>
+            </InformationWrapper>
+
+            <LottieWrapper>
+                {image_name === 'mobile_float' ? (
+                    <QueryImage data={data['mobile_float']} alt={background_alt} />
+                ) : (
+                    <QueryImage
+                        data={data[is_mobile ? image_name + '_mobile' : image_name]}
+                        alt={background_alt}
+                    />
+                )}
+            </LottieWrapper>
+        </Wrapper>
+    )
+}
+
+DGoHero.propTypes = {
+    background_alt: PropTypes.string,
+    background_image_name: PropTypes.string,
+    background_svg: PropTypes.func,
+    content: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    description: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    google_play: PropTypes.func,
+    image_name: PropTypes.string,
+    ios_coming_soon: PropTypes.bool,
+    is_mobile: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+    Logo: PropTypes.func,
+    title: PropTypes.string,
+}
+
+export default DGoHero

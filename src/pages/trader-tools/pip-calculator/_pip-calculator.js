8399ef415045919bc85feb60830c2769813f58ca
Emmanuel Arome <emmanuel@deriv.com>
2021-02-19 09:05:09 +0200

    Changes on _margin-calculator.js

--- src/pages/trader-tools/pip-calculator/_pip-calculator.js
+++ src/pages/trader-tools/pip-calculator/_pip-calculator.js
@@ -3,7 +3,6 @@ import { Formik, Field } from 'formik'
 import { graphql, useStaticQuery } from 'gatsby'
 import {
     optionItemDefault,
-    leverageItemLists,
     syntheticItemLists,
     financialItemLists,
 } from '../components/_underlying-data'
@@ -32,25 +31,24 @@ import {
     StyledOl,
     LinkWrapper,
     BottomContent,
-    BottomText,
     StyledLinkButton,
 } from '../components/_style'
 import validation from '../components/_validation'
 import { localize, Localize } from 'components/localization'
 import { Flex, Show } from 'components/containers'
-import { Text, QueryImage, Dropdown } from 'components/elements'
+import { Text, QueryImage } from 'components/elements'
 import Input from 'components/form/input'
 
-const MarginCalculator = () => {
+const PipCalculator = () => {
     const query = graphql`
         query {
-            margin_formula: file(relativePath: { eq: "trade-tools/margin-formula.png" }) {
+            pip_formula: file(relativePath: { eq: "trade-tools/pip-value-formula.png" }) {
                 ...fadeIn
             }
-            margin_info: file(relativePath: { eq: "trade-tools/margin-info.png" }) {
+            pip_info: file(relativePath: { eq: "trade-tools/margin-info.png" }) {
                 ...fadeIn
             }
-            margin_formula_mobile: file(
+            pip_formula_mobile: file(
                 relativePath: { eq: "trade-tools/margin-formula-mobile.png" }
             ) {
                 ...fadeIn
@@ -65,24 +63,24 @@ const MarginCalculator = () => {
         setTab(tab)
     }
 
-    const getMargin = (values) => {
-        const { symbol, volume, assetPrice, leverage, contractSize } = values
-        let margin_formula
+    const getPip = (values) => {
+        const { symbol, volume, pointValue, contractSize } = values
+        let pip_formula
         const STEPINDEX_VALUE = 100
         const RANGEBREAK100VALUE = 400
         const RANGEBREAK200VALUE = 800
 
         if (symbol.name === 'Step Index') {
-            margin_formula = volume * STEPINDEX_VALUE
+            pip_formula = volume * STEPINDEX_VALUE
         } else if (symbol.name === 'Range Break 100 Index') {
-            margin_formula = volume * RANGEBREAK100VALUE
+            pip_formula = volume * RANGEBREAK100VALUE
         } else if (symbol.name === 'Range Break 200 Index') {
-            margin_formula = volume * RANGEBREAK200VALUE
+            pip_formula = volume * RANGEBREAK200VALUE
         } else {
-            margin_formula = (volume * contractSize * assetPrice) / leverage.name
+            pip_formula = volume * contractSize * pointValue
         }
 
-        return toFixed(margin_formula)
+        return toFixed(pip_formula)
     }
 
     const toFixed = (val) => {
@@ -93,8 +91,7 @@ const MarginCalculator = () => {
         const errors = {}
         const symbol_error = validation.symbol(values.symbol)
         const volume_error = validation.volume(values.volume)
-        const assetPrice_error = validation.assetPrice(values.assetPrice)
-        const leverage_error = validation.leverage(values.leverage)
+        const pointValue_error = validation.pointValue(values.pointValue)
 
         if (symbol_error) {
             errors.symbol = symbol_error
@@ -102,17 +99,14 @@ const MarginCalculator = () => {
         if (volume_error) {
             errors.volume = volume_error
         }
-        if (assetPrice_error) {
-            errors.assetPrice = assetPrice_error
-        }
-        if (leverage_error) {
-            errors.leverage = leverage_error
+        if (pointValue_error) {
+            errors.pointValue = pointValue_error
         }
 
         return errors
     }
 
-    const getMarginCurrency = (symbol) => {
+    const getPipCurrency = (symbol) => {
         let currency = 'USD'
         if (symbol.market === 'synthetic_indices' || symbol.market === 'commodities') {
             currency = 'USD'
@@ -168,38 +162,38 @@ const MarginCalculator = () => {
         <StyledSection direction="column">
             <Text padding="0rem 0rem 6rem 32rem">
                 <Localize
-                    translate_text="Trader`s tools > <0> Margin Calculator</0>"
+                    translate_text="Trader`s tools > <0> Pip Calculator</0>"
                     components={[<strong key={0} />]}
                 ></Localize>
             </Text>
-            <Text size="var(--text-size-m)" mt="1rem" align="center">
+            <Text size="var(--text-size-m)" align="center">
                 {localize(
-                    'Our margin calculator helps you to estimate the margin required to keep',
+                    'Our pip calculator helps you to estimate the pip value in your trades so',
                 )}
             </Text>
-            <Text size="var(--text-size-m)" align="center">
-                {localize('your positions open overnight on Deriv MetaTrader 5 (DMT5).')}
+            <Text size="var(--text-size-m)" align="center" padding="0rem 0rem 6rem 0rem">
+                {localize('that you can better manage your risk.')}
             </Text>
-            <ContentContainer mb="4.0rem" mt="8rem">
+
+            <ContentContainer mb="4.0rem">
                 <FormWrapper>
                     <Formik
                         enableReinitialize
                         initialValues={{
                             accountType: 'Synthetic',
-                            margin: 0,
-                            marginSymbol: 'USD',
+                            pip: 0,
+                            pipSymbol: 'USD',
                             symbol: '',
                             volume: '',
-                            assetPrice: '',
-                            leverage: '',
+                            pointValue: '',
                             optionList: syntheticItemLists,
                             contractSize: '',
                         }}
                         validate={resetValidation}
                         onSubmit={(values, { setFieldValue }) => {
-                            setFieldValue('margin', getMargin(values))
+                            setFieldValue('pip', getPip(values))
                             setFieldValue('volume', numberSubmitFormat(values.volume))
-                            setFieldValue('assetPrice', numberSubmitFormat(values.assetPrice))
+                            setFieldValue('pointValue', numberSubmitFormat(values.pointValue))
                         }}
                     >
                         {({
@@ -218,14 +212,14 @@ const MarginCalculator = () => {
                             <CalculatorForm>
                                 <CalculatorHeader>
                                     <CalculatorLabel htmlFor="message">
-                                        {localize('Margin required')}
+                                        {localize('Pip required')}
                                     </CalculatorLabel>
                                     <CalculatorOutputContainer>
                                         <CalculatorOutputField>
-                                            {numberWithCommas(values.margin)}
+                                            {numberWithCommas(values.pip)}
                                         </CalculatorOutputField>
                                         <CalculatorOutputSymbol>
-                                            {values.marginSymbol}
+                                            {values.pipSymbol}
                                         </CalculatorOutputSymbol>
                                     </CalculatorOutputContainer>
                                 </CalculatorHeader>
@@ -274,7 +268,7 @@ const MarginCalculator = () => {
                                         selected_option={values.symbol}
                                         id="symbol"
                                         onChange={(value) => {
-                                            setFieldValue('marginSymbol', getMarginCurrency(value))
+                                            setFieldValue('pipSymbol', getPipCurrency(value))
                                             setFieldValue('contractSize', getContractSize(value))
                                             setFieldValue('symbol', value)
                                         }}
@@ -316,25 +310,25 @@ const MarginCalculator = () => {
 
                                     <InputGroup>
                                         <Field
-                                            name="assetPrice"
-                                            value={values.assetPrice}
+                                            name="pointValue"
+                                            value={values.pointValue}
                                             onChange={(value) => {
-                                                setFieldValue('assetPrice', value)
+                                                setFieldValue('pointValue', value)
                                             }}
                                         >
                                             {({ field }) => (
                                                 <Input
                                                     {...field}
-                                                    id="assetPrice"
+                                                    id="pointValue"
                                                     type="text"
-                                                    label={localize('Asset price')}
+                                                    label={localize('Point Value')}
                                                     autoComplete="off"
-                                                    error={touched.assetPrice && errors.assetPrice}
+                                                    error={touched.pointValue && errors.pointValue}
                                                     onBlur={handleBlur}
                                                     handleError={(current_input) => {
-                                                        setFieldValue('assetPrice', '', false)
-                                                        setFieldError('assetPrice', '')
-                                                        setFieldTouched('assetPrice', false, false)
+                                                        setFieldValue('pointValue', '', false)
+                                                        setFieldError('pointValue', '')
+                                                        setFieldTouched('pointValue', false, false)
                                                         current_input.focus()
                                                     }}
                                                     maxLength="15"
@@ -343,20 +337,6 @@ const MarginCalculator = () => {
                                             )}
                                         </Field>
                                     </InputGroup>
-                                    <Dropdown
-                                        option_list={leverageItemLists}
-                                        id="leverage"
-                                        label={localize('Leverage')}
-                                        default_option={optionItemDefault}
-                                        selected_option={values.leverage}
-                                        onChange={(value) => {
-                                            setFieldValue('leverage', value)
-                                        }}
-                                        error={touched.leverage && errors.leverage}
-                                        onBlur={handleBlur}
-                                        autoComplete="off"
-                                        data-lpignore="true"
-                                    />
                                     <ActionSection>
                                         <CalculateButton
                                             secondary
@@ -375,26 +355,21 @@ const MarginCalculator = () => {
                 <RightContent>
                     <TextWrapper>
                         <RightContentHeader as="h3" type="section-title" mb="0.8rem">
-                            {localize('How to calculate margin')}
+                            {localize('How to calculate pip value')}
                         </RightContentHeader>
 
                         <StyledText>
-                            <Localize translate_text="The margin required for a contract on DMT5 is calculated based on the formula:" />
+                            <Localize translate_text="The pip value for a contract on DMT5 is calculated based on this formula:" />
                         </StyledText>
                         <StyledText mb="1.6rem">
                             <Localize
-                                translate_text="<0> Margin = (volume in lots × contract size × asset price)÷leverage </0>"
+                                translate_text="<0> Pip value = point value x volume x contract size</0>"
                                 components={[<strong key={0} />]}
                             />
                         </StyledText>
 
                         <StyledText mb="1.6rem">
-                            <Localize translate_text="This gives you the margin requirement in the quote currency for forex pairs, or in the denomination of the underlying asset for other instruments." />
-                        </StyledText>
-                        <StyledText mb="2.4rem">
-                            {localize(
-                                'For instance, if you are trading the USD/CHF forex pair, the margin requirement will be calculated in Swiss Franc (CHF) which is the quote currency. On the other hand, if you are trading Volatility Index 75,  then the margin requirement will be computed in US Dollar (USD), which is the denomination of the underlying asset – Volatility Index 75.',
-                            )}
+                            <Localize translate_text="The pip value is calculated in USD." />
                         </StyledText>
 
                         <RightContentHeader as="h3" type="section-title" mb="0.8rem">
@@ -402,29 +377,29 @@ const MarginCalculator = () => {
                         </RightContentHeader>
 
                         <StyledText mb="16px">
-                            {localize(
-                                'Let’s say you want to trade two lots of EUR/USD with an asset price of 1.10 USD and leverage of 100.',
-                            )}
+                            {localize('Let’s say you want to trade 1 lot of Volatility 75 Index.')}
                         </StyledText>
                     </TextWrapper>
 
                     <ImageWrapper>
                         <Show.Desktop>
-                            <QueryImage data={data.margin_formula} alt={'Margin formula'} />
+                            <QueryImage data={data.pip_formula} alt={'Pip formula'} />
                         </Show.Desktop>
 
                         <Show.Mobile>
-                            <QueryImage
-                                data={data.margin_formula_mobile}
-                                alt={'Margin formula mobile'}
-                            />
+                            <QueryImage data={data.pip_formula_mobile} alt={'Pip formula mobile'} />
                         </Show.Mobile>
 
                         <FormulaText>
                             <StyledOl>
                                 <li>
                                     <span>
-                                        <Localize translate_text=" One standard lot of forex = 100,000 units" />
+                                        <Localize translate_text=" The point value is derived from the current digits of the asset. In this example, the digit is 2, so the point value is 0.01." />
+                                    </span>
+                                </li>
+                                <li>
+                                    <span>
+                                        <Localize translate_text=" The contract size is one standard lot of Volatility 75 Index = 1" />
                                     </span>
                                 </li>
                             </StyledOl>
@@ -434,38 +409,23 @@ const MarginCalculator = () => {
                     <TextWrapper>
                         <StyledText mb="1.6rem" mt="1.6rem">
                             <Localize
-                                translate_text="So you will require a margin rate of <0>2,200 USD</0> to open the above position."
+                                translate_text="So your pip value is <0>0.01 USD</0>."
                                 components={[<strong key={0} />]}
                             />
-                        </StyledText>
-                        <StyledText>
-                            {localize(
-                                'Note that these are approximate values only and will differ depending on the leverage that is set for your account and the asset you want to trade.',
-                            )}
                         </StyledText>
                     </TextWrapper>
                 </RightContent>
             </ContentContainer>
 
             <BottomContent direction="column">
-                <BottomText mb="2.4rem">
-                    <Localize
-                        translate_text="To view the asset price, go to DMT5, click on the <0>View </0> tab and select<0> Market Watch</0>. Then right-click on the symbol you want to trade and select <0>Specification.</0>"
-                        components={[<strong key={0} />]}
-                    />
-                </BottomText>
-
                 <LinkWrapper>
                     <StyledLinkButton
-                        tertiary="true"
+                        secondary="true"
                         to="https://app.deriv.com/mt5"
                         external="true"
                         target="_blank"
                     >
                         {localize('Go to DMT5 dashboard')}
-                    </StyledLinkButton>
-                    <StyledLinkButton secondary="true" to="/trade-types/margin">
-                        {localize('Learn more about margin')}
                     </StyledLinkButton>
                 </LinkWrapper>
             </BottomContent>
@@ -473,4 +433,4 @@ const MarginCalculator = () => {
     )
 }
 
-export default MarginCalculator
+export default PipCalculator

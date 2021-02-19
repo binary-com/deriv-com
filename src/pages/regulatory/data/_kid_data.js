798d2dd694f697f043aee4a95a29b20be7861390
Kevin <70131533+kevinw-binary@users.noreply.github.com>
2021-02-19 11:03:50 +0800

    Kevin/Update Regulatory - kid pdf with eu language compatibility (#1422)
    * update kid with eu language compatibility
    
    * remove: unused id
    
    * refactor dev comment

---
+++ src/pages/regulatory/data/_kid_data.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Localize } from 'components/localization'
+
+const kid_data = [
+    {
+        title: <Localize translate_text="Commodities" />,
+        ref: 'kid_deriv_commodities.pdf',
+    },
+    {
+        title: <Localize translate_text="Forex" />,
+        ref: 'kid_deriv_forex.pdf',
+    },
+    {
+        title: <Localize translate_text="Cryptocurrency" />,
+        ref: 'kid_deriv_crypto.pdf',
+    },
+    {
+        title: <Localize translate_text="Multipliers" />,
+        ref: 'kid_deriv_multipliers.pdf',
+    },
+]
+
+export default kid_data

dedad1ca538be50e848b2705c35a59ff23cddba0
yashim-deriv <75345074+yashim-deriv@users.noreply.github.com>
2021-02-19 11:20:46 +0800

    yashim/fix/refactor: Interim Pages (#1424)
    * Fix design issues and consolidated LoveTrading component
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Formating fix
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Layout fix
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Fixed QA Remarks
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Add localization for mt5 link
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Applied Dev's Suggestion
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Fixed deriv app link issues
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Fixed localized link for dmt5 and dbot
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Made Love Trading Image more consistent for mobile and desktop view
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Fixed link
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Corrected affiliate code for language
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Fixed mobile layout
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Fixed affiliate layout. Made StyledLeftContainer shared.
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Sorted language config according to id
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    * Reverted sorting
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>
    
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>

--- src/pages/interim/dmt5/_love-trading.js
+++ src/pages/interim/dmt5/_love-trading.js
@@ -1,79 +1,26 @@
 import React from 'react'
-import styled from 'styled-components'
-import { graphql, useStaticQuery } from 'gatsby'
-import {
-    Section,
-    Responsive,
-    AbsoluteWrapper,
-    MarLeft,
-    FitButton,
-    White,
-    MobileWrapper,
-} from '../_love-trading'
-import CtaBinary from '../_cta-binary'
-import { Flex } from 'components/containers'
-import { Header, QueryImage } from 'components/elements'
-import { localize } from 'components/localization'
-import { deriv_app_url } from 'common/utility'
-import PlatformMt5 from 'images/common/interim-bg-dmt5.png'
+import { LoveTradingComponent } from '../_love-trading'
+import { Localize } from 'components/localization'
+import PlatformMt5 from 'images/svg/interim/bg-dmt5.svg'
+import PlatformMt5Mobile from 'images/common/interim-mt5-mobile-bg.png'
 
-const Mt5Section = styled(Section)`
-    background-image: url(${PlatformMt5});
-    background-size: 80% 100%;
-`
-
-const Mt5Wrapper = styled(AbsoluteWrapper)`
-    left: 30%;
-`
-
-const query = graphql`
-    query {
-        dmt5: file(relativePath: { eq: "interim-dmt5.png" }) {
-            ...fadeIn
-        }
-    }
-`
-
-const LoveTrading = () => {
-    const data = useStaticQuery(query)
-    return (
-        <>
-            <Mt5Section p="3.2rem 0">
-                <Mt5Wrapper>
-                    <QueryImage data={data['dmt5']} width="54rem" />
-                </Mt5Wrapper>
-                <Responsive jc="space-between" position="relative">
-                    <Flex fd="column" ai="center" max_width="28.2rem">
-                        <Header as="h3" type="section-title" mb="4rem" align="center">
-                            {localize('Be among the first to try Deriv.com')}
-                        </Header>
-                        <FitButton secondary to="/">
-                            {localize('Visit Deriv.com now')}
-                        </FitButton>
-                        <CtaBinary />
-                    </Flex>
-                    <MobileWrapper>
-                        <QueryImage data={data['dmt5']} width="30rem" />
-                    </MobileWrapper>
-
-                    <MarLeft width="auto" fd="column" ai="center">
-                        <White as="h3" type="section-title" mb="3rem" align="center">
-                            {localize('Love MT5 on Binary.com?')}
-                        </White>
-                        <FitButton
-                            external="true"
-                            white
-                            to={deriv_app_url + '/mt5'}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            {localize('Try MT5 on Deriv.com')}
-                        </FitButton>
-                    </MarLeft>
-                </Responsive>
-            </Mt5Section>
-        </>
-    )
+const content = {
+    left: {
+        header: <Localize translate_text="Be among the first to try Deriv.com" />,
+        button_text: <Localize translate_text="Visit Deriv.com now" />,
+        button_url: '/',
+    },
+    bg_image: PlatformMt5,
+    bg_image_mobile: PlatformMt5Mobile,
+    image: 'dmt5',
+    right: {
+        header: <Localize translate_text="Love MT5 on Binary.com?" />,
+        button_text: <Localize translate_text="Try MT5 on Deriv.com" />,
+        button_props: { is_mt5_link: true },
+        hide_cta: true,
+    },
 }
+
+const LoveTrading = () => <LoveTradingComponent {...content} />
 
 export default LoveTrading

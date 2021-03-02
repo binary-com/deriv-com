1e0dd511be71dafa803cf1234a88e60e8f4ce082
yashim-deriv <yashim@deriv.com>
2021-03-02 17:10:36 +0800

    Fixed config
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,6 @@ module.exports = {
                 useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
                 stripMetadata: true,
                 defaultQuality: 50,
-                webpQuality: 50,
             },
         },
         {

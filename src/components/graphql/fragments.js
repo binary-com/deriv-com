2fc43521a8c0d55491f45ccbbf59680bdd321f9a
yashim-deriv <yashim@deriv.com>
2021-03-03 14:20:15 +0800

    Removed explicity width declarations for images
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>

--- src/components/graphql/fragments.js
+++ src/components/graphql/fragments.js
@@ -4,7 +4,6 @@ export const backGroundBlur = graphql`
     fragment backGroundBlur on File {
         childImageSharp {
             gatsbyImageData(
-                quality: 75
                 breakpoints: [360, 600, 1280, 1920]
                 formats: [AUTO, WEBP, AVIF]
                 layout: FULL_WIDTH
@@ -18,7 +17,6 @@ export const fadeIn = graphql`
     fragment fadeIn on File {
         childImageSharp {
             gatsbyImageData(
-                quality: 45
                 breakpoints: [360, 600, 992]
                 formats: [AUTO, WEBP, AVIF]
                 layout: FULL_WIDTH

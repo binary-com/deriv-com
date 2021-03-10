6dff94da113e0fc76b9a655282513b10c2a35516
sean-binary <seanho96@yahoo.com>
2021-03-10 18:25:13 +0800

    feat(marketing): add marketing data from url or cookies into deriv store

---
+++ src/components/hooks/use-marketing-data.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import Cookies from 'js-cookie'
+import { isBrowser } from 'common/utility'
+
+export const useMarketingData = () => {
+    const [affiliate_token, setAffiliateToken] = useState(null)
+    const [utm_data, setUtmData] = useState(null)
+
+    const affiliate_token_cookie = Cookies.get('affiliate_token')
+    const utm_data_cookie = Cookies.get('utm_data')
+
+    const url_params = new URLSearchParams((isBrowser() && window.location.search) || '')
+    const affiliate_token_url = url_params.get('affiliate_token')
+    const utm_data_url = url_params.get('utm_data')
+
+    useEffect(() => {
+        if (affiliate_token_cookie) {
+            setAffiliateToken(affiliate_token_cookie)
+        } else {
+            setAffiliateToken(affiliate_token_url)
+        }
+        if (utm_data_cookie) {
+            setUtmData(utm_data_cookie)
+        } else {
+            setUtmData(utm_data_url)
+        }
+    }, [url_params])
+
+    return [affiliate_token, utm_data]
+}

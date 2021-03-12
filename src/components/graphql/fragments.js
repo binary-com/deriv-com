73630cd59f213c7273c30ee6847dd6722f8ddcf9
yashimwong <yashimwong@gmail.com>
2021-03-12 22:11:00 +0800

    Removed AVIF for normal images

--- src/components/graphql/fragments.js
+++ src/components/graphql/fragments.js
@@ -6,7 +6,6 @@ export const heroImage = graphql`
             gatsbyImageData(
                 formats: [AUTO, AVIF, WEBP]
                 layout: FULL_WIDTH
-                breakpoints: [360, 1440]
                 placeholder: DOMINANT_COLOR
             )
         }
@@ -17,7 +16,7 @@ export const fadeIn = graphql`
     fragment fadeIn on File {
         childImageSharp {
             gatsbyImageData(
-                formats: [AUTO, AVIF, WEBP]
+                formats: [AUTO, WEBP]
                 layout: CONSTRAINED
                 breakpoints: [360, 992]
                 placeholder: DOMINANT_COLOR

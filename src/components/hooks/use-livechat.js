c5b5e9215b9b7a578a8c0a77e5b4e0c746f5a819
yashim-deriv <yashim@deriv.com>
2021-03-18 17:07:24 +0800

    Fixes
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>

--- src/components/hooks/use-livechat.js
+++ src/components/hooks/use-livechat.js
@@ -64,7 +64,7 @@ export const useLivechat = () => {
                         }
                     })
                 })
-            }, 3000)
+            }, 2000)
         }
 
         return () => {

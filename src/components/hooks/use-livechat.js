e1d3d863c9d795839ddd0895f07dec69cc3d1d10
seanho96 <seanho96@yahoo.com>
2021-03-19 09:54:27 +0800

    fix: unexpected token in JSON track js error

--- src/components/hooks/use-livechat.js
+++ src/components/hooks/use-livechat.js
@@ -92,7 +92,7 @@ export const useLivechat = () => {
             if (is_livechat_interactive) {
                 window.LiveChatWidget.on('ready', () => {
                     if (is_logged_in) {
-                        const client_information = getClientInformation(domain)
+                        const client_information = getClientInformation(domain) || ''
                         const {
                             loginid,
                             email,

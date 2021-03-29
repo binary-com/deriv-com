46db06f4ca893cb35b6a5f4e6e326de6fb1e44aa
suthesan <suthesankanagasabai@gmail.com>
2021-03-29 17:23:59 +0800

    changed localize to Localize

--- src/pages/help-centre/_faq-schema.js
+++ src/pages/help-centre/_faq-schema.js
@@ -1,61 +1,55 @@
-import { localize } from 'components/localization'
+import React from "react"
+import { Localize } from 'components/localization'
 export const faq_schema = {
     '@context': 'https://schema.org',
     '@type': 'FAQPage',
     mainEntity: [
         {
             '@type': 'Question',
-            name: localize('What is DMT5?'),
+            name: <Localize translate_text="What is DMT5?" />,
             acceptedAnswer: {
                 '@type': 'Answer',
-                text:
-                    localize('DMT5 is the MT5 platform on Deriv. It is a multi-asset online platform designed to give new and experienced traders access to a wide range of financial markets.'),
+                text: <Localize translate_text="DMT5 is the MT5 platform on Deriv. It is a multi-asset online platform designed to give new and experienced traders access to a wide range of financial markets." />,
             },
         },
         {
             '@type': 'Question',
-            name: localize('What is DTrader?'),
+            name: <Localize translate_text="What is DTrader?" />,
             acceptedAnswer: {
                 '@type': 'Answer',
-                text:
-                    localize('DTrader is an advanced trading platform that allows you to trade more than 50 assets in the form of digitals, multiplier, and lookback options.'),
+                text: <Localize translate_text="DTrader is an advanced trading platform that allows you to trade more than 50 assets in the form of digitals, multiplier, and lookback options." />,
             },
         },
         {
             '@type': 'Question',
-            name: localize('What is DBot?'),
+            name: <Localize translate_text="What is DBot?" />,
             acceptedAnswer: {
                 '@type': 'Answer',
-                text:
-                    localize("DBot is a web-based strategy builder for trading digital options. It’s a platform where you can build your own trading bot using drag-and-drop 'blocks'."),
+                text: <Localize translate_text="DBot is a web-based strategy builder for trading digital options. It’s a platform where you can build your own trading bot using drag-and-drop 'blocks'." />,
             },
         },
         {
             '@type': 'Question',
-            name: localize('What are the major differences between DTrader and DMT5?'),
+            name: <Localize translate_text="What are the major differences between DTrader and DMT5?" />,
             acceptedAnswer: {
                 '@type': 'Answer',
-                text:
-                    localize('DTrader allows you to trade more than 50 assets in the form of digitals, multiplier, and lookback options.\n\nDMT5 is a multi-asset trading platform that you can use to trade spot forex and contracts for difference (CFDs) with leverage.'),
+                text: <Localize translate_text="DTrader allows you to trade more than 50 assets in the form of digitals, multiplier, and lookback options.\n\nDMT5 is a multi-asset trading platform that you can use to trade spot forex and contracts for difference (CFDs) with leverage." />,
             },
         },
         {
             '@type': 'Question',
-            name:
-                localize('What are the differences between the DMT5 Standard, Advanced, and Synthetic Indices accounts?'),
+            name: <Localize translate_text="What are the differences between the DMT5 Standard, Advanced, and Synthetic Indices accounts?" />,
             acceptedAnswer: {
                 '@type': 'Answer',
-                text:
-                    localize('The DMT5 Standard account offers new and experienced traders high leverage and variable spreads for maximum flexibility.\n\nThe DMT5 Advanced account is a 100% A Book account where your trades are passed straight through to the market, giving you direct access to forex liquidity providers.\n\nThe DMT5 Synthetic Indices account allows you to trade contracts for difference (CFDs) on synthetic indices that mimic real-world movements. It is available for trading 24/7 and audited for fairness by an independent third party.'),
+                text: <Localize translate_text="The DMT5 Standard account offers new and experienced traders high leverage and variable spreads for maximum flexibility.\n\nThe DMT5 Advanced account is a 100% A Book account where your trades are passed straight through to the market, giving you direct access to forex liquidity providers.\n\nThe DMT5 Synthetic Indices account allows you to trade contracts for difference (CFDs) on synthetic indices that mimic real-world movements. It is available for trading 24/7 and audited for fairness by an independent third party." />,
             },
         },
         {
             '@type': 'Question',
-            name: localize('What markets can I trade on DTrader?'),
+            name: <Localize translate_text="What markets can I trade on DTrader?" />,
             acceptedAnswer: {
                 '@type': 'Answer',
-                text:
-                    localize('You can trade forex, stock indices, commodities, and synthetic indices on DTrader.'),
+                text: <Localize translate_text="You can trade forex, stock indices, commodities, and synthetic indices on DTrader." />,
             },
         },
     ],
--- src/pages/partners/affiliate-ib/_faq-schema.js
+++ src/pages/partners/affiliate-ib/_faq-schema.js
@@ -1,71 +1,72 @@
-import { localize } from 'components/localization'
+import React from "react"
+import { Localize } from 'components/localization'
 export const faq_schema = {
     '@context': 'https://schema.org',
     '@type': 'FAQPage',
     mainEntity: [
         {
             '@type': 'Question',
-            name: localize('What is the Deriv IB Programme?'),
+            name: <Localize translate_text='What is the Deriv IB Programme?' />,
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    localize('The Deriv IB Programme is a partnership where you benefit from referring new clients to trade on our DMT5 platform.'),
+                    <Localize translate_text='The Deriv IB Programme is a partnership where you benefit from referring new clients to trade on our DMT5 platform.' />,
             },
         },
         {
             '@type': 'Question',
-            name: localize("How much can I earn from referring new clients to your DMT5 platform?"),
+            name: <Localize translate_text="How much can I earn from referring new clients to your DMT5 platform?" />,
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    localize('You’ll earn a fixed commission based on the volume of individual trades purchased by your referred clients. Please refer to our IB commission plan for more info.'),
+                    <Localize translate_text='You’ll earn a fixed commission based on the volume of individual trades purchased by your referred clients. Please refer to our IB commission plan for more info.' />,
 
             },
         },
         {
             '@type': 'Question',
-            name: localize('How much do I need to pay to join your IB programme?'),
+            name: <Localize translate_text='How much do I need to pay to join your IB programme?' />,
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    localize("The Deriv IB Programme is completely free."),
+                    <Localize translate_text="The Deriv IB Programme is completely free." />,
             },
         },
         {
             '@type': 'Question',
-            name: localize('Are there any minimum client or volume conditions that I need to meet before I can withdraw my commissions?'),
+            name: <Localize translate_text='Are there any minimum client or volume conditions that I need to meet before I can withdraw my commissions?' />,
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    localize('There is absolutely no requirement to withdraw your IB commissions.'),
+                    <Localize translate_text='There is absolutely no requirement to withdraw your IB commissions.' />,
             },
         },
         {
             '@type': 'Question',
             name:
-                localize('How can I sign up as a Deriv IB?'),
+                <Localize translate_text='How can I sign up as a Deriv IB?' />,
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    localize('If you’re interested in becoming an IB, all you need to do is head to the signup page and fill out the application form. We’ll review your application and get in touch if it’s successful. If you’re already registered as a Deriv affiliate, you can email us at affiliates@deriv.com instead.'),
+                    <Localize translate_text='If you’re interested in becoming an IB, all you need to do is head to the signup page and fill out the application form. We’ll review your application and get in touch if it’s successful. If you’re already registered as a Deriv affiliate, you can email us at affiliates@deriv.com instead.' />,
             },
         },
         {
             '@type': 'Question',
-            name: localize('How and when will I receive my Deriv IB commissions?'),
+            name: <Localize translate_text='How and when will I receive my Deriv IB commissions?' />,
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    localize('Your IB commissions are credited directly into your DMT5 account daily. You can transfer the funds to your Deriv account and withdraw them to your preferred payment method.'),
+                    <Localize translate_text='Your IB commissions are credited directly into your DMT5 account daily. You can transfer the funds to your Deriv account and withdraw them to your preferred payment method.' />,
             },
         },
         {
             '@type': 'Question',
-            name: localize('Do you offer any referral tools to your IBs?'),
+            name: <Localize translate_text='Do you offer any referral tools to your IBs?' />,
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    localize('Definitely. We’ll provide you with banners, videos, reviews, links, and text ads that you can use to bring new clients to our DMT5 platform.'),
+                    <Localize translate_text='Definitely. We’ll provide you with banners, videos, reviews, links, and text ads that you can use to bring new clients to our DMT5 platform.' />,
             },
         },
     ],

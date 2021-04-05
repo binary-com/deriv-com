710814d6a2bb76dc55169da014f9dd7f06612602
Sean Ho <seanho96@yahoo.com>
2021-04-05 11:58:02 +0800

    remove media query

--- src/pages/besquare/static/style/_faq.js
+++ src/pages/besquare/static/style/_faq.js
@@ -10,10 +10,6 @@ export const AccordionWrapper = styled.div`
     margin: 0 auto;
     position: relative;
     z-index: 2;
-
-    @media ${device.tablet} {
-        margin: 0 16px;
-    }
 `
 
 export const ContentContainer = styled(Container)`

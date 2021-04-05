05f7a69be829a97eae71ef81656da89bd1c8a547
suthesan <suthesankanagasabai@gmail.com>
2021-04-05 14:34:57 +0800

    replace Localize with localize

--- src/pages/partners/affiliate-ib/_faq-schema.js
+++ src/pages/partners/affiliate-ib/_faq-schema.js
@@ -1,72 +1,71 @@
-import React from "react"
-import { Localize } from 'components/localization'
+import { localize } from 'components/localization'
 export const faq_schema = {
     '@context': 'https://schema.org',
     '@type': 'FAQPage',
     mainEntity: [
         {
             '@type': 'Question',
-            name: <Localize translate_text='What is the Deriv IB Programme?' />,
+            name: localize('What is the Deriv IB Programme?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text='The Deriv IB Programme is a partnership where you benefit from referring new clients to trade on our DMT5 platform.' />,
+                    localize('The Deriv IB Programme is a partnership where you benefit from referring new clients to trade on our DMT5 platform.'),
             },
         },
         {
             '@type': 'Question',
-            name: <Localize translate_text="How much can I earn from referring new clients to your DMT5 platform?" />,
+            name: localize("How much can I earn from referring new clients to your DMT5 platform?"),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text='You’ll earn a fixed commission based on the volume of individual trades purchased by your referred clients. Please refer to our IB commission plan for more info.' />,
+                    localize('You’ll earn a fixed commission based on the volume of individual trades purchased by your referred clients. Please refer to our IB commission plan for more info.'),
 
             },
         },
         {
             '@type': 'Question',
-            name: <Localize translate_text='How much do I need to pay to join your IB programme?' />,
+            name: localize('How much do I need to pay to join your IB programme?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text="The Deriv IB Programme is completely free." />,
+                    localize("The Deriv IB Programme is completely free."),
             },
         },
         {
             '@type': 'Question',
-            name: <Localize translate_text='Are there any minimum client or volume conditions that I need to meet before I can withdraw my commissions?' />,
+            name: localize('Are there any minimum client or volume conditions that I need to meet before I can withdraw my commissions?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text='There is absolutely no requirement to withdraw your IB commissions.' />,
+                    localize('There is absolutely no requirement to withdraw your IB commissions.'),
             },
         },
         {
             '@type': 'Question',
             name:
-                <Localize translate_text='How can I sign up as a Deriv IB?' />,
+                localize('How can I sign up as a Deriv IB?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text='If you’re interested in becoming an IB, all you need to do is head to the signup page and fill out the application form. We’ll review your application and get in touch if it’s successful. If you’re already registered as a Deriv affiliate, you can email us at affiliates@deriv.com instead.' />,
+                    localize('If you’re interested in becoming an IB, all you need to do is head to the signup page and fill out the application form. We’ll review your application and get in touch if it’s successful. If you’re already registered as a Deriv affiliate, you can email us at affiliates@deriv.com instead.'),
             },
         },
         {
             '@type': 'Question',
-            name: <Localize translate_text='How and when will I receive my Deriv IB commissions?' />,
+            name: localize('How and when will I receive my Deriv IB commissions?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text='Your IB commissions are credited directly into your DMT5 account daily. You can transfer the funds to your Deriv account and withdraw them to your preferred payment method.' />,
+                    localize('Your IB commissions are credited directly into your DMT5 account daily. You can transfer the funds to your Deriv account and withdraw them to your preferred payment method.'),
             },
         },
         {
             '@type': 'Question',
-            name: <Localize translate_text='Do you offer any referral tools to your IBs?' />,
+            name: localize('Do you offer any referral tools to your IBs?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text='Definitely. We’ll provide you with banners, videos, reviews, links, and text ads that you can use to bring new clients to our DMT5 platform.' />,
+                    localize('Definitely. We’ll provide you with banners, videos, reviews, links, and text ads that you can use to bring new clients to our DMT5 platform.'),
             },
         },
     ],
--- src/pages/partners/payment-agent/_faq-schema.js
+++ src/pages/partners/payment-agent/_faq-schema.js
@@ -1,98 +1,97 @@
-import React from "react"
-import { Localize } from 'components/localization'
+import { localize } from 'components/localization'
 export const faq_schema = {
     '@context': 'https://schema.org',
     '@type': 'FAQPage',
     mainEntity: [
         {
             '@type': 'Question',
-            name: <Localize translate_text='What is the Deriv Payment Agent Programme?' />,
+            name: localize('What is the Deriv Payment Agent Programme?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text='It is a partnership arrangement where a payment agent is authorised to process deposits and withdrawals for our clients.' />,
+                    localize('It is a partnership arrangement where a payment agent is authorised to process deposits and withdrawals for our clients.'),
             },
         },
         {
             '@type': 'Question',
-            name: <Localize translate_text="Why do clients need a payment agent?" />,
+            name: localize("Why do clients need a payment agent?"),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text="Many of our clients are looking for ways to fund their accounts using payment methods that are not directly available on Deriv. As a payment agent, you'll be able to help them fund their accounts while charging a fixed commission per transaction." />,
+                    localize("Many of our clients are looking for ways to fund their accounts using payment methods that are not directly available on Deriv. As a payment agent, you'll be able to help them fund their accounts while charging a fixed commission per transaction."),
             },
         },
         {
             '@type': 'Question',
-            name: <Localize translate_text='Are there any fees I need to pay to become a payment agent on Deriv?' />,
+            name: localize('Are there any fees I need to pay to become a payment agent on Deriv?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text="Not at all. Joining our payment agent programme is completely free." />,
+                    localize("Not at all. Joining our payment agent programme is completely free."),
             },
         },
         {
             '@type': 'Question',
-            name: <Localize translate_text='Who sets the commission rate per transaction?' />,
+            name: localize('Who sets the commission rate per transaction?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text='As a payment agent, you determine your commission per transaction, subject to our established thresholds.' />,
+                    localize('As a payment agent, you determine your commission per transaction, subject to our established thresholds.'),
             },
         },
         {
             '@type': 'Question',
             name:
-                <Localize translate_text='What happens if a payment agent charges more than the set commission threshold?' />,
+                localize('What happens if a payment agent charges more than the set commission threshold?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text='If we receive justified complaints, the payment agents involved will be banned immediately from our platform.' />,
+                    localize('If we receive justified complaints, the payment agents involved will be banned immediately from our platform.'),
             },
         },
         {
             '@type': 'Question',
-            name: <Localize translate_text='Are payment agents employees or affiliates of Deriv?' />,
+            name: localize('Are payment agents employees or affiliates of Deriv?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text='No. Payment agents operate as independent exchangers and are not affiliates of Deriv.' />,
+                    localize('No. Payment agents operate as independent exchangers and are not affiliates of Deriv.'),
             },
         },
         {
             '@type': 'Question',
-            name: <Localize translate_text='How can I add, remove or change my accepted payment methods?' />,
+            name: localize('How can I add, remove or change my accepted payment methods?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text='To change your payment method, please contact us via livechat.' />,
+                    localize('To change your payment method, please contact us via livechat.'),
             },
         },
         {
             '@type': 'Question',
-            name: <Localize translate_text='As a payment agent, will I receive commissions from Deriv?' />,
+            name: localize('As a payment agent, will I receive commissions from Deriv?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text='We do not pay commissions to payment agents. You set your own commission rate per transaction and our clients will bear the necessary fees.' />,
+                    localize('We do not pay commissions to payment agents. You set your own commission rate per transaction and our clients will bear the necessary fees.'),
             },
         },
         {
             '@type': 'Question',
-            name: <Localize translate_text='Can I advertise my services to Deriv clients?' />,
+            name: localize('Can I advertise my services to Deriv clients?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text="Yes, provided that you follow all the relevant terms and conditions (see the tab entitled 'For business partners' on our Terms and conditions page)." />,
+                    localize("Yes, provided that you follow all the relevant terms and conditions (see the tab entitled 'For business partners' on our Terms and conditions page)."),
             },
         },
         {
             '@type': 'Question',
-            name: <Localize translate_text='Will I still be able to trade with my account after registering as a payment agent?' />,
+            name: localize('Will I still be able to trade with my account after registering as a payment agent?'),
             acceptedAnswer: {
                 '@type': 'Answer',
                 text:
-                    <Localize translate_text="Yes. As a payment agent, you will still be able to trade with your account." />,
+                    localize("Yes. As a payment agent, you will still be able to trade with your account."),
             },
         },
     ],

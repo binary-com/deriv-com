02b2ff0b151432ae24a5a90c5953fc2e34913119
Suthesan Kanagasabai <45410833+suthesan-deriv@users.noreply.github.com>
2021-04-06 16:40:21 +0800

    suthesh/faq-markup-update (#1558)
    * updated help centre schema, added schema for payment agent and affiliate
    
    * updated messages.json
    
    * remove double spacing
    
    * changed localize to Localize
    
    * reverted messages.json
    
    * testing translations
    
    * replace Localize with localize

--- src/pages/help-centre/_faq-schema.js
+++ src/pages/help-centre/_faq-schema.js
@@ -34,7 +34,7 @@ export const faq_schema =
             name: localize("What are the major differences between DTrader and DMT5?"),
             acceptedAnswer: {
                 '@type': 'Answer',
-                text: localize("DTrader allows you to trade more than 50 assets in the form of digitals, multiplier, and lookback options.<br />DMT5 is a multi-asset trading platform that you can use to trade spot forex and contracts for difference (CFDs) with leverage."),
+                text: localize("DTrader allows you to trade more than 50 assets in the form of digitals, multiplier, and lookback options.\n\nDMT5 is a multi-asset trading platform that you can use to trade spot forex and contracts for difference (CFDs) with leverage."),
             },
         },
         {
@@ -42,7 +42,7 @@ export const faq_schema =
             name: localize("What are the differences between the DMT5 Standard, Advanced, and Synthetic Indices accounts?"),
             acceptedAnswer: {
                 '@type': 'Answer',
-                text: localize("The DMT5 Standard account offers new and experienced traders high leverage and variable spreads for maximum flexibility.<br />The DMT5 Advanced account is a 100% A Book account where your trades are passed straight through to the market, giving you direct access to forex liquidity providers.<br />The DMT5 Synthetic Indices account allows you to trade contracts for difference (CFDs) on synthetic indices that mimic real-world movements. It is available for trading 24/7 and audited for fairness by an independent third party."),
+                text: localize("The DMT5 Standard account offers new and experienced traders high leverage and variable spreads for maximum flexibility.\n\nThe DMT5 Advanced account is a 100% A Book account where your trades are passed straight through to the market, giving you direct access to forex liquidity providers.\n\nThe DMT5 Synthetic Indices account allows you to trade contracts for difference (CFDs) on synthetic indices that mimic real-world movements. It is available for trading 24/7 and audited for fairness by an independent third party."),
             },
         },
         {

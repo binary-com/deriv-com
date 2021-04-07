5d0faf904fce56e58fd7f83723e32da87ad1cc4f
Sean Ho <seanho96@yahoo.com>
2021-04-07 14:09:46 +0800

    compliance: quarterly update of the EU footer %

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -656,7 +656,7 @@ const Footer = ({ type, is_ppc, is_ppc_redirect }) => {
                                         <Localize translate_text="CFDs are considered complex derivatives and may not be suitable for retail clients." />
                                     </DisclaimerParagraph>
                                     <DisclaimerParagraph>
-                                        <Localize translate_text="CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. 71% of retail investor accounts lose money when trading CFDs with this provider. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money." />
+                                        <Localize translate_text="CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. 68% of retail investor accounts lose money when trading CFDs with this provider. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money." />
                                     </DisclaimerParagraph>
                                     <DisclaimerParagraph>
                                         <Localize translate_text="The products mentioned here may be affected by changes in currency exchange rates. If you invest in these products, you may lose some or all of your investment and the value of your investment may fluctuate. You should never invest money that you cannot afford to lose and never trade with borrowed money." />
@@ -687,7 +687,7 @@ const Footer = ({ type, is_ppc, is_ppc_redirect }) => {
                                         <Localize translate_text="CFDs are considered complex derivatives and may not be suitable for retail clients." />
                                     </DisclaimerParagraph>
                                     <DisclaimerParagraph>
-                                        <Localize translate_text="CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. 71% of retail investor accounts lose money when trading CFDs with this provider. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money." />
+                                        <Localize translate_text="CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. 68% of retail investor accounts lose money when trading CFDs with this provider. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money." />
                                     </DisclaimerParagraph>
                                     <DisclaimerParagraph>
                                         <Localize translate_text="The products mentioned here may be affected by changes in currency exchange rates. If you invest in these products, you may lose some or all of your investment and the value of your investment may fluctuate. You should never invest money that you cannot afford to lose and never trade with borrowed money." />

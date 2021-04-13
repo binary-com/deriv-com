20d2bc663c80b5ae42fde3c1c2da1a233402a2e1
habib-deriv <shuvohabib@gmail.com>
2021-04-13 09:28:06 +0600

    remove optional chaining

--- src/pages/trade-types/multiplier/_synthetic-indices.js
+++ src/pages/trade-types/multiplier/_synthetic-indices.js
@@ -167,7 +167,7 @@ const SyntheticIndices = () => {
                 </MarketsWrapper>
                 <AvailablePlatforms dtrader />
                 <Show.Eu>
-                    {synthetic_multiplier.eu_content?.map((text, index) => (
+                    {synthetic_multiplier.eu_content.map((text, index) => (
                         <Text key={index} mt="2.5rem" color="grey-5" size="var(--text-size-xs)">
                             {text}
                         </Text>

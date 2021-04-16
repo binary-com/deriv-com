07d955290e9e17a1078e7b8cd32a31f9ba1c8497
kevinw-binary <kevin@regentmarkets.com>
2021-04-16 10:44:23 +0800

    change stock indices to stock & indices in market page

--- src/pages/markets/components/markets/_stock-indices.js
+++ src/pages/markets/components/markets/_stock-indices.js
@@ -14,12 +14,12 @@ const OtherMarkets = Loadable(() => import('../sections/_other-markets.js'))
 
 const StockIndices = ({ simple_step_content }) => {
     simple_step_content[1].text = localize(
-        'Open a real account, make a deposit, and start trading stock indices and other markets.',
+        'Open a real account, make a deposit, and start trading stocks, indices and other markets.',
     )
     return (
         <div>
             <WhyTrade
-                header={<Localize translate_text="Why trade stock indices on Deriv" />}
+                header={<Localize translate_text='Why trade stock & indices on Deriv' />}
                 text={
                     <Localize translate_text="Profit from the price movements in a broad market or sector without owning underlying assets. Sourced from over-the-counter (OTC) markets, our stock indices offer competitively priced asset baskets and allow you to trade outside the regular hours of major stock markets." />
                 }
@@ -35,16 +35,16 @@ const StockIndices = ({ simple_step_content }) => {
             <AvailableTrades
                 Margin={<Margin market_content={stock_margin} />}
                 DigitalOptions={
-                    <DigitalOptions market_name={'stock indices'} options_list={stock_options} />
+                    <DigitalOptions market_name={'stocks & indices'} options_list={stock_options} />
                 }
-                name="Stock indices"
+                name="Stocks & indices"
                 display_title={
-                    <Localize translate_text="Stock indices trades available on Deriv" />
+                    <Localize translate_text="Stocks & indices trades available on Deriv" />
                 }
             />
             <SimpleSteps
                 header={
-                    <Localize translate_text="Start trading stock indices on Deriv in 3 simple steps" />
+                    <Localize translate_text="Start trading stocks & indices on Deriv in 3 simple steps" />
                 }
                 content={simple_step_content}
                 sign_up

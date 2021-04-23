1772de9e134aa52dd39c07574f57c174ad8dd545
habib-deriv <shuvohabib@gmail.com>
2021-04-23 14:01:35 +0600

    add deriv com url for blog and community

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,21 +1,24 @@
 const isBrowser = () => typeof window !== 'undefined'
 
-const supported_domains = ['deriv.com', 'deriv.me']
+const deriv_com_url = 'deriv.com'
+const deriv_me_url = 'deriv.me'
+
+const supported_domains = [deriv_com_url, deriv_me_url]
 const domain_url =
     isBrowser() && supported_domains.includes(window.location.hostname)
     ? window.location.hostname
-    : 'deriv.com'
+    : deriv_com_url
 
 export const deriv_app_url = `https://app.${domain_url}`
 export const deriv_bot_app_url = `https://app.${domain_url}/bot`
 export const deriv_dp2p_app_url = `https://app.${domain_url}/cashier/p2p`
 export const smarttrader_url = `https://smarttrader.${domain_url}`
-export const blog_url = `https://blog.${domain_url}`
+export const blog_url = `https://blog.${deriv_com_url}`
 export const deriv_cookie_domain = domain_url
 export const login_url = `https://login.${domain_url}`
 export const affiliate_signup_url = `https://login.${domain_url}/signup.php`
 export const affiliate_signin_url = `https://login.${domain_url}/signin.php`
-export const community_url = `https://community.${domain_url}`
+export const community_url = `https://community.${deriv_com_url}`
 export const oauth_url = `https://oauth.${domain_url}`
 
 export const application_id = 'f0aef779-d9ec-4517-807e-a84c683c4265'

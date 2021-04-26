6133508fb7856a7a1a5ba6aaf75c829ede4bb4cf
yashim-deriv <yashim@deriv.com>
2021-04-26 11:21:55 +0800

    fix: login deriv app id Co-authored-by: Yashim Wong <yashimwong@gmail.com>

--- src/common/constants.js
+++ src/common/constants.js
@@ -48,6 +48,7 @@ export const dp2p_google_play_url =
 export const gtm_test_domain = 'deriv-com.binary.sx'
 export const livechat_client_id = '66aa088aad5a414484c1fd1fa8a5ace7'
 export const livechat_license_id = 12049137
+export const deriv_app_app_id = domain_url === deriv_me_url ? 1411 : 16929
 export const map_api_key = 'AIzaSyAEha6-HeZuI95L9JWmX3m6o-AxQr_oFqU'
 export const mga_link_url =
     'https://authorisation.mga.org.mt/verification.aspx?lang=EN&company=a5fd1edc-d072-4c26-b0cd-ab3fa0f0cc40&details=1'

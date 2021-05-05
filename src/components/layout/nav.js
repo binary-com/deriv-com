eedc9f199e7dd18ce9fbec285fd9e8d40aab62be
Sean Ho <seanho96@yahoo.com>
2021-05-05 12:00:35 +0800

    revert: changes in nav.js

--- src/components/layout/nav.js
+++ src/components/layout/nav.js
@@ -460,8 +460,6 @@ const NavDesktop = ({ base, is_ppc, is_ppc_redirect, is_logged_in }) => {
 
     const checkActive = (link_name) => link_name === active_dropdown || link_name === current_page
 
-    const closeDropdown = () => useCallback(setActiveDropdown(''))
-
     const handleLinkClick = (dropdown, target) => {
         setActiveDropdown(dropdown)
         if (!target) return
@@ -472,7 +470,7 @@ const NavDesktop = ({ base, is_ppc, is_ppc_redirect, is_logged_in }) => {
 
     const setDropdownRef = (new_ref) => setActiveDropdownRef(new_ref)
 
-    useOutsideClick(navigation_bar_ref, closeDropdown, active_dropdown_ref)
+    useOutsideClick(navigation_bar_ref, () => setActiveDropdown(''), active_dropdown_ref)
 
     useEffect(() => {
         setMounted(true)
@@ -574,9 +572,6 @@ const NavDesktop = ({ base, is_ppc, is_ppc_redirect, is_logged_in }) => {
     )
 }
 
-const MemoizedNavDesktop = React.memo(NavDesktop)
-const MemoizedNavMobile = React.memo(NavMobile)
-
 export const Nav = ({ base, is_ppc_redirect, is_ppc }) => {
     const [is_logged_in, setLoggedIn] = useState(false)
 
@@ -594,7 +589,7 @@ export const Nav = ({ base, is_ppc_redirect, is_ppc }) => {
             <CFDWarning />
             <StyledNavMain>
                 <Show.Desktop max_width="bp1060">
-                    <MemoizedNavDesktop
+                    <NavDesktop
                         base={base}
                         is_ppc={is_ppc}
                         is_ppc_redirect={is_ppc_redirect}
@@ -602,7 +597,7 @@ export const Nav = ({ base, is_ppc_redirect, is_ppc }) => {
                     />
                 </Show.Desktop>
                 <Show.Mobile min_width="bp1060">
-                    <MemoizedNavMobile is_ppc={is_ppc} is_logged_in={is_logged_in} />
+                    <NavMobile is_ppc={is_ppc} is_logged_in={is_logged_in} />
                 </Show.Mobile>
             </StyledNavMain>
         </NavWrapperMain>

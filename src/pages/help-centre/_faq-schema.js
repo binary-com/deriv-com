fcfdc8eb7130ada133c1a96688167f1c23bac3b7
fiona-deriv <fiona@deriv.com>
2021-05-06 11:09:50 +0200

    add Deriv X schema

--- src/pages/help-centre/_faq-schema.js
+++ src/pages/help-centre/_faq-schema.js
@@ -13,6 +13,15 @@ export const faq_schema = {
         },
         {
             '@type': 'Question',
+            name: 'What is DMT5?',
+            acceptedAnswer: {
+                '@type': 'Answer',
+                text:
+                    'DMT5 is the MT5 platform on Deriv. It is a multi-asset online platform designed to give new and experienced traders access to a wide range of financial markets.',
+            },
+        },
+        {
+            '@type': 'Question',
             name: 'What is DTrader?',
             acceptedAnswer: {
                 '@type': 'Answer',
@@ -27,6 +36,15 @@ export const faq_schema = {
                 '@type': 'Answer',
                 text:
                     "DBot is a web-based strategy builder for trading digital options. It’s a platform where you can build your own trading bot using drag-and-drop 'blocks'.",
+            },
+        },
+        {
+            '@type': 'Question',
+            name: 'What is Deriv X?',
+            acceptedAnswer: {
+                '@type': 'Answer',
+                text:
+                    "Deriv X is an easy to-use trading platform where you can trade CFDs on various assets on a platform layout that you can customise according to your preference.",
             },
         },
         {

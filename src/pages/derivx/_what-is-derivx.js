523b9dd46c11c4215a1dfb69638c934ea12a069d
suthesan <suthesankanagasabai@gmail.com>
2021-05-12 13:50:05 +0800

    updated what is derivx description

--- src/pages/derivx/_what-is-derivx.js
+++ src/pages/derivx/_what-is-derivx.js
@@ -22,7 +22,7 @@ const WhatIsDeriv = () => {
                         <Localize translate_text="What is Deriv X" />
                     </Header>
                     <Text size="16px" align="center" mt="12px">
-                        <Localize translate_text="Deriv X, developed by Devexperts, is a flexible, easy-to-use multi-asset trading platform. Offering CFDs on forex, commodities, cryptocurrencies, and synthetics, Deriv X gives you a versatile trading experience that’s fully customisable." />
+                        <Localize translate_text="Deriv X is a customisable multi-asset trading platform. Offering CFDs on forex, commodities, cryptocurrencies, and synthetics, Deriv X gives you a versatile trading experience that lets you customise your trading environment." />
                     </Text>
                 </Flex>
             </Container>

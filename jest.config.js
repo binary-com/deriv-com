6afb7799dbdfe4f875b8ce0f3a3845866adabea9
yashim-deriv <yashim@deriv.com>
2021-05-20 16:24:33 +0800

    feat/chore: configured and added enzyme
    Co-authored-by: Yashim Wong <yashimwong@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,16 @@
 module.exports = {
     transform: {
         '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
+        '^.+\\.svg$': 'jest-svg-transformer',
     },
     moduleNameMapper: {
         '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
         '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+        '^common(.*)$': '<rootDir>/src/common$1',
+        '^components(.*)$': '<rootDir>/src/components$1',
+        '^images(.*)$': '<rootDir>/src/images$1',
+        '^themes(.*)$': '<rootDir>/src/themes$1',
+        '^store(.*)$': '<rootDir>/src/store$1',
     },
     testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
     transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
--- src/__tests__/link.js
+++ src/__tests__/link.js
@@ -1,1 +1,33 @@
-// Add test cases here
+import React from 'react'
+import { configure, shallow } from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import { expect, jest, test } from '@jest/globals'
+import { localized_link_url } from '../common/constants'
+import { LocaleContext } from '../components/localization/locale-context'
+import { LocalizedLink } from '../components/localization/localized-link'
+
+configure({ adapter: new Adapter() })
+
+// Test Case 1 - Correct URL return for different "type" props
+Object.keys(localized_link_url).forEach((type) => {
+    test(`LocalizedLink type ${type} should output ${localized_link_url[type]}`, () => {
+        // Arrange - Setup Context and Props
+        const locale_context = { locale: 'en' }
+        jest.spyOn(LocaleContext, 'useContext').mockImplementation(() => locale_context)
+
+        // Act - Shallow render component passing in props
+        const link_wrapper = shallow(
+            <LocalizedLink
+                to=""
+                type={type}
+                external="true"
+                target="_blank"
+                rel="noopener noreferrer"
+            />,
+        )
+
+        // Assert - Check if the href matches expected values
+        const link_href = link_wrapper.find('<a>').prop('href')
+        expect(link_href).toEqual(localized_link_url[type])
+    })
+})

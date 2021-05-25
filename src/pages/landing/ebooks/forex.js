eaf136e42ff909286865ce39c44dce3d7a9c2437
khalidxx <fxtryio@gmail.com>
2021-05-25 20:39:32 +0800

    added ebook basic layout

--- src/pages/landing/ebooks/forex.js
+++ src/pages/landing/ebooks/forex.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react'
+// import React, { useState, useEffect } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 // import { graphql, useStaticQuery } from 'gatsby'
+// import HeaderSection from 'components/layout/ebooks/header'
 import Layout from 'components/layout/layout'
 import { SEO } from 'components/containers'
 import { localize, WithIntl } from 'components/localization'
-import { size } from 'themes/device'
-import { isBrowser } from 'common/utility'
+// import mainHeaderImage from 'images/common/ebooks/minimal-cover-book-mockup.png'
+// import { size } from 'themes/device'
+// import { isBrowser } from 'common/utility'
 
 const MainSec = styled.section`
     background-color: var(--color-grey-25);
@@ -18,16 +21,6 @@ const MainSec = styled.section`
 `
 
 const ForexEbook = () => {
-    const [is_mobile, setMobile] = useState(false)
-    const handleResizeWindow = () => {
-        setMobile(isBrowser() ? window.screen.width <= size.mobileL : false)
-    }
-
-    useEffect(() => {
-        setMobile(isBrowser() ? window.screen.width <= size.mobileL : false)
-        window.addEventListener('resize', handleResizeWindow)
-    })
-
     return (
         <Layout is_ppc_redirect={true}>
             <SEO
@@ -35,14 +28,9 @@ const ForexEbook = () => {
                 description={localize('Trade Forex CFDs on our Deriv platform.')}
                 no_index
             />
-            {is_mobile && (
-                <div>
-                    <p>This is a mobile page</p>
-                </div>
-            )}
-            <MainSec>
-                <h1>This is the main stuff </h1>
-            </MainSec>
+            {/* <HeaderSection mainHeaderImage={mainHeaderImage}/> */}
+
+            <MainSec></MainSec>
         </Layout>
     )
 }

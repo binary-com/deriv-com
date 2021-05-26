5c8231cfaa9cc2bd12b17ec6b184bc1ee09a72d5
arome-binary <emmanuel@deriv.com>
2021-05-26 12:11:53 +0200

    Careers update

--- src/pages/careers/index.js
+++ src/pages/careers/index.js
@@ -12,9 +12,9 @@ import Layout from 'components/layout/layout'
 import { localize, WithIntl, Localize } from 'components/localization'
 
 const meta_attributes = {
-    og_title: <Localize translate_text="Careers | Join and grow with us | Deriv" />,
-    og_description: (
-        <Localize translate_text="Looking for a great place to work? Deriv is looking for smart, talented, and dedicated people who are up for a challenging and rewarding career." />
+    og_title: localize('Careers | Join and grow with us | Deriv'),
+    og_description: localize(
+        'Looking for a great place to work? Deriv is looking for smart, talented, and dedicated people who are up for a challenging and rewarding career.',
     ),
 }
 

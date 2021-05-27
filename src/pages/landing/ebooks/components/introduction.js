de689e8ebd36a43663e4d308599085b438506e38
habib-deriv <shuvohabib@gmail.com>
2021-05-27 13:56:45 +0600

    add introduction section

---
+++ src/pages/landing/ebooks/components/introduction.js
@@ -0,0 +1,100 @@
+// import React, { useState, useEffect } from 'react'
+import React from 'react'
+import styled from 'styled-components'
+// import { graphql, useStaticQuery } from 'gatsby'
+import introForexEbook from 'images/common/ebooks/introduction-forex-ebook.png'
+import checkIcon from 'images/common/ebooks/check-icon.png'
+import { WithIntl } from 'components/localization'
+// import { size } from 'themes/device'
+// import { isBrowser } from 'common/utility'
+
+const MediaWapper = styled.div`
+    display: flex;
+    align-items: flex-start;
+    max-width: 1022px;
+    margin: 100px auto 0;
+
+    > img {
+        align-self: center;
+        width: 142px;
+        margin-right: 82px;
+        object-fit: contain;
+    }
+    h3.intro-text {
+        font-size: 48px;
+        font-weight: bold;
+        line-height: 1.25;
+        color: #333333;
+        margin-bottom: 20px;
+    }
+`
+const MediaBody = styled.div`
+    flex: 1;
+    font-size: 20px;
+    align-self: center;
+
+    p.intro-desc {
+        margin-bottom: 20px;
+    }
+    p.intro-desc, li {
+        font-size: 16px;
+        font-weight: normal;
+        line-height: 1.5;
+        color: #333333;
+    }
+    li {
+        margin-bottom: 10px;
+    }
+    h5 {
+        font-size: 24px;
+        font-weight: bold;
+        line-height: 1.5;
+        color: #333333;
+        margin-bottom: 20px;
+    }
+    img {
+        width: 16px;
+        margin-right: 10px;
+        position: relative;
+        top: 3px;
+    }
+`
+
+const MediaItemList = styled.ul`
+    font-size: 20px;
+`
+
+const Introduction = () => {
+    return (
+        <MediaWapper>
+            <img src={introForexEbook} alt="Generic placeholder image" />
+            <MediaBody>
+                <h3 className="mt-0 intro-text">Introduction</h3>
+                <p className="intro-desc">
+                    Foreign exchange – forex or the FX market, as it iscommonly known – is one of the biggestmarketplaces in the world, offering endless tradingopportunities. In this 75-page book, experttrader Vince Stanzione will teach you how to spotand take advantage of trading opportunities inthe forex markets, using Deriv’s DTrader and MetaTrader5 platforms.
+                </p>
+                <h5>In this book you’ll learn:</h5>
+                <MediaItemList>
+                    <li>
+                        <img src={checkIcon} alt="Check Icon" />
+                        The simple basics of forex
+                    </li>
+                    <li>
+                        <img src={checkIcon} alt="Check Icon" />
+                        How to make more profitable forex trades and loweryour trading risks
+                    </li>
+                    <li>
+                        <img src={checkIcon} alt="Check Icon" />
+                        How to use digital options to profit from up, down,and even sideways moves
+                    </li>
+                    <li>
+                        <img src={checkIcon} alt="Check Icon" />
+                        How to trade like professionals without spending hourseach day analysing the worldmarket
+                    </li>
+                </MediaItemList>
+            </MediaBody>
+        </MediaWapper>
+    )
+}
+
+export default WithIntl()(Introduction)
--- src/pages/landing/ebooks/forex.js
+++ src/pages/landing/ebooks/forex.js
@@ -1,36 +1,28 @@
 // import React, { useState, useEffect } from 'react'
 import React from 'react'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 // import { graphql, useStaticQuery } from 'gatsby'
-// import HeaderSection from 'components/layout/ebooks/header'
+import Introduction from './components/introduction';
+import HeaderSection from 'components/layout/ebooks/header'
 import Layout from 'components/layout/layout'
 import { SEO } from 'components/containers'
 import { localize, WithIntl } from 'components/localization'
-// import mainHeaderImage from 'images/common/ebooks/minimal-cover-book-mockup.png'
+import forexEbook from 'images/common/ebooks/forex-ebook.png'
 // import { size } from 'themes/device'
 // import { isBrowser } from 'common/utility'
-
-const MainSec = styled.section`
-    background-color: var(--color-grey-25);
-    background-size: cover;
-    height: 100vh;
-    width: 100%;
-    margin: 0;
-    display: flex;
-    align-items: center;
-`
 
 const ForexEbook = () => {
     return (
         <Layout is_ppc_redirect={true}>
             <SEO
-                title={localize('Forex Ebook')}
+                title={localize('Stocks Ebook')}
                 description={localize('Trade Forex CFDs on our Deriv platform.')}
                 no_index
             />
-            {/* <HeaderSection mainHeaderImage={mainHeaderImage}/> */}
+            <HeaderSection mainHeaderImage={forexEbook} />
 
-            <MainSec></MainSec>
+            <Introduction />
+
         </Layout>
     )
 }

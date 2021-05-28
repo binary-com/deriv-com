ee473220866d373795ec55b3381a369ab3a69014
khalidxx <fxtryio@gmail.com>
2021-05-28 12:47:11 +0800

    added content as props

--- src/pages/landing/ebooks/forex.js
+++ src/pages/landing/ebooks/forex.js
@@ -1,7 +1,4 @@
-// import React, { useState, useEffect } from 'react'
 import React from 'react'
-// import styled from 'styled-components'
-// import { graphql, useStaticQuery } from 'gatsby'
 import Introduction from 'components/layout/ebooks/introduction'
 import HeaderSection from 'components/layout/ebooks/header'
 import Topics from 'components/layout/ebooks/topics'
@@ -11,8 +8,24 @@ import { localize, WithIntl } from 'components/localization'
 import introForexEbook from 'images/common/ebooks/introduction-forex-ebook.png'
 import forexEbookImage from 'images/common/ebooks/forex-ebook.png'
 import forexEbookInside from 'images/common/ebooks/forex-ebook-inside.png'
-// import { size } from 'themes/device'
-// import { isBrowser } from 'common/utility'
+
+const introPoints = [
+    'The simple basics of forex',
+    'How to make more profitable forex trades and loweryour trading risks',
+    'How to use digital options to profit from up, down,and even sideways moves',
+    'How to trade like professionals without spending hourseach day analysing the worldmarket',
+]
+
+const topicsCovered = [
+    'The basics of forex',
+    'Who uses the forex market?',
+    'Which currencies are on the forex market?',
+    'Why trade forex on Deriv?',
+    'Advantages of trading forex on DTrader',
+    'Advantages of trading forex on DMT5',
+    'Currency pairs you can trade on Deriv',
+    'Forex in more detail',
+]
 
 const ForexEbook = () => {
     return (
@@ -23,8 +36,15 @@ const ForexEbook = () => {
                 no_index
             />
             <HeaderSection mainHeaderImage={forexEbookImage} />
-            <Introduction introImage={introForexEbook} imageHeight={462} />
-            <Topics topicsImage={forexEbookInside} />
+            <Introduction
+                introImage={introForexEbook}
+                imageWidth={142}
+                introPara={localize(
+                    'Foreign exchange – forex or the FX market, as it iscommonly known – is one of the biggestmarketplaces in the world, offering endless tradingopportunities. In this 75-page book, experttrader Vince Stanzione will teach you how to spotand take advantage of trading opportunities inthe forex markets, using Deriv’s DTrader and MetaTrader5 platforms.',
+                )}
+                introList={introPoints}
+            />
+            <Topics topicsImage={forexEbookInside} topicsList={topicsCovered} />
         </Layout>
     )
 }
--- src/pages/landing/ebooks/stocks.js
+++ src/pages/landing/ebooks/stocks.js
@@ -1,7 +1,4 @@
-// import React, { useState, useEffect } from 'react'
 import React from 'react'
-// import styled from 'styled-components'
-// import { graphql, useStaticQuery } from 'gatsby'
 import HeaderSection from 'components/layout/ebooks/header'
 import Introduction from 'components/layout/ebooks/introduction'
 import Topics from 'components/layout/ebooks/topics'
@@ -11,18 +8,14 @@ import { localize, WithIntl } from 'components/localization'
 import stocksEbookImage from 'images/common/ebooks/stocks-ebook.png'
 import stocksEbookInside from 'images/common/ebooks/stocks-ebook-inside.png'
 import StocksIntroImage from 'images/svg/stock-indices/stocksHeaderBelowSection.svg'
-// import { size } from 'themes/device'
-// import { isBrowser } from 'common/utility'
 
-// const MainSec = styled.section`
-//     background-color: var(--color-grey-25);
-//     background-size: cover;
-//     height: 100vh;
-//     width: 100%;
-//     margin: 0;
-//     display: flex;
-//     align-items: center;
-// `
+const topicsCovered = [
+    'The basics of CFD trading',
+    'Stocks and stock indices: the basics',
+    'CFD trading: order types',
+    'Rise or fall? Assessing stocks',
+    'Stock market seasonality',
+]
 
 const StocksEbook = () => {
     return (
@@ -33,8 +26,17 @@ const StocksEbook = () => {
                 no_index
             />
             <HeaderSection mainHeaderImage={stocksEbookImage} />
-            <Introduction introImage={StocksIntroImage} imageHeight={476} />
-            <Topics topicsImage={stocksEbookInside} />
+            <Introduction
+                introImage={StocksIntroImage}
+                imageWidth={282}
+                introPara={localize(
+                    'In this 57-page e-book by trader Vince Stanzione, you will learn how to diversify your portfolio with commission-free trading on the biggest international stocks and stock market indices. Wall Street 30, US Tech 100, UK 100, Alphabet, Tesla, Amazon plus many more are available on the MetaTrader 5 platform, where you can trade stock derivatives with leverage, allowing you to get started with minimal capital.',
+                )}
+                subPara={localize(
+                    'We’ll even add in a free demo account on Deriv.com right away. This way, you can practice trading stock derivatives completely risk-free on the Deriv MT5 platform, using a free Financial demo account. And once you are ready to get trading for real, you can get started with as little as $5.',
+                )}
+            />
+            <Topics topicsImage={stocksEbookInside} topicsList={topicsCovered} />
         </Layout>
     )
 }

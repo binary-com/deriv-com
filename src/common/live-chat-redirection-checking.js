5167835e3c803d4f8cbc19ca556406873fe4d392
njazuli-deriv <78890281+njazuli-deriv@users.noreply.github.com>
2021-06-04 13:34:55 +0800

    code cleanup

--- src/common/live-chat-redirection-checking.js
+++ src/common/live-chat-redirection-checking.js
@@ -1,9 +1,9 @@
 export const checkLiveChatRedirection = () => {
     if (typeof window !== 'undefined') {
+        const live_chat_key = 'live_chat_redirection'
         const queryString = window.location.search
         const urlParams = new URLSearchParams(queryString)
         const liveChatOpened = urlParams.get('is_livechat_open')
-        const live_chat_key = 'live_chat_redirection'
 
         if (liveChatOpened) {
             localStorage.setItem(live_chat_key, liveChatOpened)

6f6781e5f589d5b2491ec6cc79a277631f6a5d85
kevinw-binary <kevin@regentmarkets.com>
2021-06-10 11:09:22 +0800

    fix deriv career page social media links

--- src/components/layout/footer/bottom-social-wrapper.js
+++ src/components/layout/footer/bottom-social-wrapper.js
@@ -1,22 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import SocialWrapperComponent  from './social-wrapper'
 import { Show } from 'components/containers'
 
-const BottomSocialSection = (type) => {
+const BottomSocialSection = ({ type }) => (
+    <>
+        <Show.NonEU>
+            <SocialWrapperComponent is_career_page={type === 'careers'} />
+        </Show.NonEU>
 
-    return (
-        <>
-            <Show.NonEU>
+        <Show.Eu>
+            <Show.Mobile>
                 <SocialWrapperComponent is_career_page={type === 'careers'} />
-            </Show.NonEU>
+            </Show.Mobile>
+        </Show.Eu>
+    </>
+)
 
-            <Show.Eu>
-                <Show.Mobile>
-                    <SocialWrapperComponent is_career_page={type === 'careers'} />
-                </Show.Mobile>
-            </Show.Eu>
-        </>
-    )
+BottomSocialSection.propTypes = {
+    type: PropTypes.string,
 }
 
 export default BottomSocialSection;
--- src/components/layout/footer/logo.js
+++ src/components/layout/footer/logo.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import SocialWrapperComponent  from './social-wrapper'
 import { DerivLogoWrapper, StyledLogo, } from './common/style.js'
 import { Show } from 'components/containers'
 import DerivLogo from 'images/svg/deriv-footer.svg'
 
-const LogoSection = ( type ) => {
+const LogoSection = ({ type }) => {
     return (
         <DerivLogoWrapper>
             <StyledLogo src={DerivLogo} alt="logo" width="147" height="25" />
@@ -15,6 +16,10 @@ const LogoSection = ( type ) => {
             </Show.Eu>
         </DerivLogoWrapper>
     )
+}
+
+LogoSection.propTypes = {
+    type: PropTypes.string,
 }
 
 export default LogoSection;

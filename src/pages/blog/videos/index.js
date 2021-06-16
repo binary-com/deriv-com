0e01b81d6f5cea5bd468f41959a128822487a738
Sean Ho <seanho96@yahoo.com>
2021-06-16 17:17:07 +0800

    fix: heading sizes

--- src/pages/blog/videos/index.js
+++ src/pages/blog/videos/index.js
@@ -37,20 +37,29 @@ const Hero = styled(Flex)`
     background-size: cover;
 `
 
+const StyledHeader = styled(Header)`
+    @media ${device.tabletL} {
+        margin-top: 16px;
+    }
+    @media ${device.mobileL} {
+        font-size: 28px;
+    }
+`
+
 const VideosPage = () => (
     <Layout>
         <SEO
             title={localize('Videos Page | Blog by Deriv')}
             description={localize('All videos page.')}
         />
-        <Hero jc="cneter" ai="center">
+        <Hero jc="center" ai="center">
             <SmallContainer>
                 <Header as="h2" type="section-title" color="white" weight="400" align="left">
                     {localize('Video tutorials')}
                 </Header>
-                <Header as="h2" type="page-title" color="white" align="left">
+                <StyledHeader as="h2" type="page-title" color="white" align="left">
                     {localize('Our latest videos and webinars')}
-                </Header>
+                </StyledHeader>
             </SmallContainer>
         </Hero>
         <AllVideos video_data={video_data} />

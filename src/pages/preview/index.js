ffce764a34f1416ca4067813b1ed250a6d885131
mustofa-binary <mustofa@binary.com>
2021-06-22 21:59:08 +0800

    fix: preview style

--- src/pages/preview/index.js
+++ src/pages/preview/index.js
@@ -1,8 +1,57 @@
 import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 import Layout from 'components/layout/layout'
 import { localize, WithIntl } from 'components/localization'
-import { SEO } from 'components/containers'
+import { SEO, SectionContainer, SmallContainer } from 'components/containers'
 import { Header } from 'components/elements'
+
+const PreviewContainer = styled.div`
+    font-size: 16px;
+
+    & p {
+        margin-top: 32px;
+        font-weight: 400;
+        line-height: 32px;
+        font-size: 20px;
+    }
+    & blockquote {
+        margin-top: 32px;
+        border-left: 0.25rem solid #ff1a75;
+        padding-left: 2rem;
+        font-style: italic;
+        line-height: 32px;
+    }
+    & hr {
+        margin: 32px 0;
+    }
+    & ul {
+        margin-top: 32px;
+        list-style-type: disc;
+        margin-block-start: 1em;
+        margin-block-end: 1em;
+
+        > li {
+            margin: 10px 0 0 24px;
+            padding: 0 0 0 6px;
+            line-height: 32px;
+            font-size: 20px;
+
+            :first-child {
+                margin: 0 0 0 24px;
+            }
+        }
+    }
+    & a {
+        font-size: 20px;
+        color: var(--color-red);
+        text-decoration: none;
+        cursor: pointer;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+`
 
 const PreviewPage = () => {
     const [id, setId] = useState(null)
@@ -47,12 +96,17 @@ const PreviewPage = () => {
                 title={localize('Markets | Markets to trade | Deriv')}
             />
             {id && (
-                <>
-                    <Header as="h1" type="page-title" align="center">
-                        {data?.data.article_title}
-                    </Header>
-                    <div dangerouslySetInnerHTML={{ __html: data?.data.article_body }}></div>
-                </>
+                <SectionContainer>
+                    <SmallContainer fd="column">
+                        <Header as="h1" type="page-title">
+                            {data?.data.article_title}
+                        </Header>
+
+                        <PreviewContainer
+                            dangerouslySetInnerHTML={{ __html: data?.data.article_body }}
+                        ></PreviewContainer>
+                    </SmallContainer>
+                </SectionContainer>
             )}
         </Layout>
     )

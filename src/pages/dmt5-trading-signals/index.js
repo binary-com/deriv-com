85b515f3f24984e89d1f8168a5a9e13a6f1ac597
Nikolai Berestevich <nikolai.berestevich@regentmarkets.com>
2021-06-23 14:28:19 +0300

    nikolai-berestevich/fix:signal provider hightlight

--- src/pages/dmt5-trading-signals/index.js
+++ src/pages/dmt5-trading-signals/index.js
@@ -9,9 +9,16 @@ import { SEO, Flex, Box } from 'components/containers'
 import Layout from 'components/layout/layout'
 import { localize, Localize, WithIntl } from 'components/localization'
 import { Header } from 'components/elements'
-import { useTabState } from 'components/hooks/use-tab-state'
+// import { useTabState } from 'components/hooks/use-tab-state'
 import device from 'themes/device'
-import { getLocationHash } from 'common/utility'
+import {
+    getLocationHash,
+    checkElemInArray,
+    isBrowser,
+    routeBack,
+    scrollTop,
+    setLocationHash,
+} from 'common/utility'
 
 const signal_content_subscriber = {
     header: (
@@ -102,17 +109,43 @@ const Separator = styled.div`
 `
 
 const DMT5TradingSignals = () => {
-    const [active_tab, setActiveTab] = useTabState(['signal-subscriber', 'signal-provider'])
-    const [, setJust_for_update] = useState()
-    // google chrome has a bug, so i need to use just_for_update for rerender, because setActiveTab doesnt do it
+    const tab_list = ['signal-subscriber', 'signal-provider']
+    const [active_tab, setActiveTab] = useState(
+        getLocationHash() && checkElemInArray(tab_list, getLocationHash())
+            ? getLocationHash()
+            : tab_list[0],
+    )
+
     useEffect(() => {
-        if (getLocationHash() !== "signal-provider") {
+        if (!getLocationHash() || !checkElemInArray(tab_list, getLocationHash())) {
+            setLocationHash(active_tab)
+        } else {
             setActiveTab(getLocationHash())
-            setJust_for_update()
+            scrollTop()
         }
     }, [])
-    // eslint-disable-next-line no-console
-    console.log('rerender!!!')
+
+    useEffect(() => {
+        if (getLocationHash() !== active_tab && isBrowser()) {
+            setLocationHash(active_tab)
+        }
+    }, [active_tab])
+
+    useEffect(() => {
+        if (getLocationHash() !== active_tab && checkElemInArray(tab_list, getLocationHash())) {
+            setActiveTab(getLocationHash())
+            // eslint-disable-next-line no-console
+            console.log({active_tab})
+            scrollTop()
+        } else if (!checkElemInArray(tab_list, getLocationHash())) {
+            routeBack()
+        }
+    }, [getLocationHash(),active_tab])
+
+// eslint-disable-next-line no-console
+console.log({active_tab})
+// eslint-disable-next-line no-console
+console.log(getLocationHash())
     return (
         <Layout>
             <SEO description={localize('Subscribe to Deriv MetaTrader 5 trading signals to copy the trades of experienced traders, or become a signal provider and share your strategies.')} title={localize('Deriv MetaTrader 5 trading signals | Resources | Deriv')} />

80c134f33b47260e55ac2bd3a4c3b507232a359a
suthesan <suthesankanagasabai@gmail.com>
2021-06-25 11:24:18 +0800

    added markets news data

--- src/pages/blog/_markets-news.js
+++ src/pages/blog/_markets-news.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { market_news_data } from './_markets_news_data'
+import { SectionContainer, Container, Flex } from 'components/containers'
+import { Text } from 'components/elements/typography'
 
 const MarketNews = () => {
     return (
-        <>
-            Testing
-        </>
+        <SectionContainer>
+            <Container>
+                {market_news_data.map((data, index) => {
+                    return (
+                        <Flex key={index} fd="column">
+                            <Text>
+                                {data.report_type}
+                            </Text>
+                            <Text>
+                                {data.date}
+                            </Text>
+                            <Text>
+                                {data.read_time}
+                            </Text>
+                        </Flex>
+                    )
+                })}
+            </Container>
+        </SectionContainer>
     )
 }
 
---
+++ src/pages/blog/_markets_news_data.js
@@ -0,0 +1,50 @@
+export const market_news_data = [
+    {
+        report_type: "Weekly report",
+        date: "24 May 2021 - 30 May 2021",
+        read_time: 3,
+        img_source: "https://www.iiste.org/wp-content/uploads/2019/07/Blog-graphic-from-Istock.jpg"
+    },
+    {
+        report_type: "Weekly report",
+        date: "17 May 2021 - 24 May 2021",
+        read_time: 4,
+        img_source: "https://www.iiste.org/wp-content/uploads/2019/07/Blog-graphic-from-Istock.jpg"
+    },
+    {
+        report_type: "Weekly report",
+        date: "10 May 2021 - 17 May 2021",
+        read_time: 3,
+        img_source: "https://www.iiste.org/wp-content/uploads/2019/07/Blog-graphic-from-Istock.jpg"
+    },
+    {
+        report_type: "Weekly report",
+        date: "5 April 2021 - 12 April 2021",
+        read_time: 2,
+        img_source: "https://www.iiste.org/wp-content/uploads/2019/07/Blog-graphic-from-Istock.jpg"
+    },
+    {
+        report_type: "Weekly report",
+        date: "22 March 2021 - 29 March 2021",
+        read_time: 6,
+        img_source: "https://www.iiste.org/wp-content/uploads/2019/07/Blog-graphic-from-Istock.jpg"
+    },
+    {
+        report_type: "Weekly report",
+        date: "22 March 2021 - 29 March 2021",
+        read_time: 2,
+        img_source: "https://www.iiste.org/wp-content/uploads/2019/07/Blog-graphic-from-Istock.jpg"
+    },
+    {
+        report_type: "Weekly report",
+        date: "1 March 2021 - 8 March 2021",
+        read_time: 3,
+        img_source: "https://www.iiste.org/wp-content/uploads/2019/07/Blog-graphic-from-Istock.jpg"
+    },
+    {
+        report_type: "Weekly report",
+        date: "1 March 2021 - 8 March 2021",
+        read_time: 4,
+        img_source: "https://www.iiste.org/wp-content/uploads/2019/07/Blog-graphic-from-Istock.jpg"
+    }
+]

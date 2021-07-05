4f812b48059c74d8bcc05871f778e6c70122d7fe
khalidxx <fxtryio@gmail.com>
2021-07-05 20:31:02 +0800

    added code snippet

--- src/components/containers/seo.js
+++ src/components/containers/seo.js
@@ -157,7 +157,24 @@ const SEO = ({ description, meta, title, no_index, has_organization_schema, meta
             ].concat(meta)}
         >
             {has_organization_schema && (
+                <>
                 <script type="application/ld+json">{JSON.stringify(organization_schema)}</script>
+                <script>
+                    (function(A,s,a,y,e,r){
+                    r=window.asayer=[s,r,e,[y-1]];
+                    s=document.createElement('script');s.src=a;s.async=!A;
+                    document.getElementsByTagName('head')[0].appendChild(s);
+                    r.start=function(v){r.push([0])};
+                    r.stop=function(v){r.push([1])};
+                    r.userID=function(id){r.push([2,id])};
+                    r.userAnonymousID=function(id){r.push([3,id])};
+                    r.metadata=function(k,v){r.push([4,k,v])};
+                    r.event=function(k,p){r.push([5,k,p])};
+                    r.active=function(){return false};
+                    r.sessionID=function(){};
+                    })(0,5142598291963829,'//static.asayer.io/tracker.js',1,29);
+                </script>
+                </>
             )}
 
             {!is_non_localized &&

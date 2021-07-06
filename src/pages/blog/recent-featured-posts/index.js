6021d9a3618f296ab2d62214647e003565be9ee0
kevinw-binary <kevin@regentmarkets.com>
2021-07-06 11:09:16 +0800

    drag 4 items at a time

--- src/pages/blog/recent-featured-posts/index.js
+++ src/pages/blog/recent-featured-posts/index.js
@@ -356,7 +356,7 @@ const settings = {
     options: {
         axis: 'y',
         align: 'start',
-        draggable: true,
+        slidesToScroll:3,
     },
     view_port: {
         height: 'auto',

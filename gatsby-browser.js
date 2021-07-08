55b93762d09d4476bf597236b10b48f7427cfad0
khalidxx <fxtryio@gmail.com>
2021-07-08 16:10:14 +0800

    added cookie test link

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -137,7 +137,7 @@ export const onClientEntry = () => {
     }
 
     addScript({
-        src: 'https://static.deriv.com/scripts/cookie.js',
+        src: 'https://cdn.jsdelivr.net/gh/khalidxx/deriv-static-content/scripts/cookie.js',
         async: true,
     })
 

1c12e0193cac64ca603cab8ac24de4505b67a98c
kevinw-binary <kevin@regentmarkets.com>
2021-07-13 11:17:37 +0800

    edited the data

--- src/pages/blog/recent-featured-posts/_data.js
+++ src/pages/blog/recent-featured-posts/_data.js
@@ -4,7 +4,7 @@ export const recent_article_data = {
     category: 'CFDs',
     title: 'The complete beginner’s guide to cryptocurrency trading',
     description:
-        'Love the idea of leverage, but hate the thought of getting wiped out with one bad trade? We have the ideal trade type for you. One that combines the upside of a leveraged trade with the limited downside of an options trade.',
+        'Love the idea of leverage, but hate the thought of getting wiped out with one bad trade? We have the ideal trade type for you. One that combines the upside of a leveraged trade with the limited downside of an options trade',
     date: 'Nov 5, 2020',
     id: 1,
     link: '/blog/articles/',
@@ -16,7 +16,7 @@ export const featured_article_data = {
     category: 'Finance news',
     title: 'Why it’s the Tesla of the financial industry',
     description:
-        'Tesla, Inc. designs, develops, manufactures and sells electric vehicles and designs, manufactures, installs and sells solar energy generation and energy storage products.',
+        'Tesla, Inc. designs, develops, manufactures and sells electric vehicles and designs, manufactures, installs and sells solar energy generation and energy storage products',
     date: '12 June 2020',
     id: 2,
     link: '/blog/articles/',
@@ -29,7 +29,7 @@ export const article_data = [
         category: 'Multipliers',
         title: 'How to trade multipliers on Deriv',
         description:
-            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com.',
+            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com',
         date: '12 June 2021',
         id: 1,
         link: '/blog/articles/',
@@ -40,7 +40,7 @@ export const article_data = [
         category: 'Multipliers',
         title: 'How to trade multipliers on Deriv this is for really long titles',
         description:
-            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com.',
+            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on deriv.com',
         date: '12 June 2021',
         id: 2,
         link: '/blog/articles/',
@@ -51,7 +51,7 @@ export const article_data = [
         category: 'Multipliers',
         title: 'How to trade multipliers on Deriv',
         description:
-            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes.',
+            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes',
         date: '12 June 2021',
         id: 3,
         link: '/blog/articles/',
@@ -62,7 +62,7 @@ export const article_data = [
         category: 'Multipliers',
         title: 'How to trade multipliers on Deriv',
         description:
-            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes.',
+            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes',
         date: '12 June 2021',
         id: 4,
         link: '/blog/articles/',
@@ -73,7 +73,7 @@ export const article_data = [
         category: 'Multipliers',
         title: 'How to trade multipliers on Deriv',
         description:
-            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes.',
+            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes',
         date: '12 June 2021',
         id: 5,
         link: '/blog/articles/',
@@ -84,7 +84,7 @@ export const article_data = [
         category: 'Multipliers',
         title: 'How to trade multipliers on Deriv',
         description:
-            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes.',
+            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes',
         date: '12 June 2021',
         id: 6,
         link: '/blog/articles/',
@@ -98,7 +98,7 @@ export const article_data_2 = [
         category: 'Multipliers',
         title: 'How to trade multipliers on Deriv',
         description:
-            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com.',
+            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on deriv.com',
         date: '12 June 2021',
         id: 1,
         link: '/blog/articles/',
@@ -109,7 +109,7 @@ export const article_data_2 = [
         category: 'Multipliers',
         title: 'How to trade multipliers on Deriv this is for really long titles',
         description:
-            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com.',
+            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on deriv.com',
         date: '12 June 2021',
         id: 2,
         link: '/blog/articles/',
@@ -120,7 +120,7 @@ export const article_data_2 = [
         category: 'Multipliers',
         title: 'How to trade multipliers on Deriv',
         description:
-            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes.',
+            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes',
         date: '12 June 2021',
         id: 3,
         link: '/blog/articles/',
@@ -131,7 +131,7 @@ export const article_data_2 = [
         category: 'Multipliers',
         title: 'How to trade multipliers on Deriv',
         description:
-            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes.',
+            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes',
         date: '12 June 2021',
         id: 4,
         link: '/blog/articles/',
@@ -142,7 +142,7 @@ export const article_data_2 = [
         category: 'Multipliers',
         title: 'How to trade multipliers on Deriv',
         description:
-            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes.',
+            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes',
         date: '12 June 2021',
         id: 5,
         link: '/blog/articles/',
@@ -153,7 +153,7 @@ export const article_data_2 = [
         category: 'Multipliers',
         title: 'How to trade multipliers on Deriv',
         description:
-            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes.',
+            'To help you begin, we will look at the basics of cryptocurrency: what it is, how it works, and how to start trading cryptos on Deriv.com. This has an extra extra long section of the description for testing purposes',
         date: '12 June 2021',
         id: 6,
         link: '/blog/articles/',

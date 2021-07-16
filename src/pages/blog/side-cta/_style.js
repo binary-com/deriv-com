a2461b06749e0c7f80b0043a80d8effc730ad489
kevinw-binary <kevin@regentmarkets.com>
2021-07-16 11:54:26 +0800

    dev suggestion

--- src/pages/blog/side-cta/_style.js
+++ src/pages/blog/side-cta/_style.js
@@ -6,39 +6,37 @@ import { LinkButton } from 'components/form'
 
 export const SideContainer = styled(Flex)`
     padding: 20px;
-    justify-content:start;
+    justify-content: start;
 
     @media ${device.tablet} {
-        justify-content:center;
+        justify-content: center;
     }
 `
 export const CtaContainer = styled(Flex)`
     width: 282px;
     height: 420px;
     border-radius: 8px;
-    padding-top: 25px;
-    padding-left: 16px;
+    padding: 25px 0 0 16px;
     background-color: var(--color-grey-41);
-    flex-flow:column;
-    justify-content:flex-start;
+    flex-flow: column;
+    justify-content: flex-start;
     background-image: url(${Shape});
-    background-position: right bottom; 
+    background-position: right bottom;
     background-repeat: no-repeat;
 
     @media ${device.tablet} {
-        width: 328px;
+        width: 100%;
+        max-width: 328px;
         height: 420px;
-        background-position: center bottom; 
-        padding-top: 26px;
-        padding-left:39px;
-        padding-right:46px;
+        background-position: center bottom;
+        padding: 26px 46px 0 39px;
     }
 `
 
 export const DownloadPdfIcon = styled.img`
-    width:44px;
-    height:18px;
-    margin-bottom:5px;
+    width: 44px;
+    height: 18px;
+    margin-bottom: 5px;
 `
 
 export const LinkButtonWrapper = styled(Flex)`
@@ -50,7 +48,7 @@ export const LinkButtonWrapper = styled(Flex)`
 
 export const ClaimButton = styled(LinkButton)`
     width: fit-content;
-    size:14px;
+    size: 14px;
 
     &:hover {
         cursor: pointer;
@@ -59,13 +57,13 @@ export const ClaimButton = styled(LinkButton)`
 
 export const ImageWrapper = styled.div`
     position: relative;
-    margin:0 auto;
+    margin: 0 auto;
 `
 
 export const BookImage = styled.img`
     height: 212px;
-    margin-bottom:-2px;
+    margin-bottom: -2px;
     @media ${device.tablet} {
-        margin-right:30px;
+        margin-right: 30px;
     }
 `

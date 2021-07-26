579721669268b642e0bcdfb3b42a3dba62f2cc2d
Kevin <70131533+kevinw-binary@users.noreply.github.com>
2021-07-26 11:50:49 +0800

    Kevin/Deriv blog - Side CTA Banner (#1912)
    * kevin/side-cta-banner
    
    * side cta banner
    
    * empty message
    
    * dev suggestion
    
    * update flex
    
    * dev comment
    
    * dev suggestion
    
    * change the author font weight

---
+++ src/pages/blog/side-cta/_style.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components'
+import Shape from './images/shape.svg'
+import { Flex } from 'components/containers'
+import device from 'themes/device'
+
+export const SideContainer = styled(Flex)`
+    padding: 20px;
+    justify-content: start;
+
+    @media ${device.tablet} {
+        justify-content: center;
+    }
+`
+export const CtaContainer = styled(Flex)`
+    width: 282px;
+    height: 420px;
+    border-radius: 8px;
+    padding: 24px 0 0 16px;
+    background-color: var(--color-grey-41);
+    flex-flow: column;
+    justify-content: flex-start;
+    background-image: url(${Shape});
+    background-position: right bottom;
+    background-repeat: no-repeat;
+
+    @media ${device.tablet} {
+        width: 100%;
+        max-width: 328px;
+        height: 420px;
+        background-position: center bottom;
+        padding: 24px 39px 0;
+    }
+`
+
+export const DownloadPdfIcon = styled.img`
+    width: 44px;
+    height: 18px;
+    margin-bottom: 5px;
+`
+
+export const LinkButtonWrapper = styled(Flex)`
+    margin-top: 16px;
+    margin-bottom: 16px;
+    text-align: center;
+    justify-content: center;
+`
+
+export const ImageWrapper = styled.div`
+    position: relative;
+    margin: 0 auto;
+`
+
+export const BookImage = styled.img`
+    height: 212px;
+    margin-bottom: -2px;
+    @media ${device.tablet} {
+        margin-right: 30px;
+    }
+`

799fc5ca49ea48e16d2417aaf1aa3c929c45e160
kevinw-binary <kevin@regentmarkets.com>
2021-07-27 12:54:02 +0800

    change the web title

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -38,7 +38,9 @@ const Home = () => {
     return (
         <Layout is_ppc_redirect={true} is_ppc={true}>
             <SEO
-                title={localize('Online trading platform | Forex, commodities and indices | Deriv')}
+                title={localize(
+                    'Online trading platform | Forex, commodities, synthetic indices, stocks, and stock indices | Deriv',
+                )}
                 description={localize(
                     'Deriv - An online trading platform that offers a wide selection of derivatives to trade on',
                 )}

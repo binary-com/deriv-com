e53913b7f15b4a57c75ff487ec37dea629918d78
alamin-deriv <alamin@deriv.com>
2021-08-09 07:48:00 +0100

    working on live chat standalone page

--- src/pages/livechat/index.js
+++ src/pages/livechat/index.js
@@ -5,7 +5,6 @@ import InitialLoader from 'components/elements/dot-loader'
 import { localize, WithIntl } from 'components/localization'
 import { SEO, Container } from 'components/containers'
 import { useLivechat } from 'components/hooks/use-livechat'
-import { usePageLoaded } from 'components/hooks/use-page-loaded'
 
 const StyledContainer = styled(Container)`
     text-align: center;
@@ -17,33 +16,19 @@ const StyledContainer = styled(Container)`
 const LiveChat = () => {
     const [is_livechat_interactive, LC_API] = useLivechat()
 
-    const [is_mounted] = usePageLoaded(false)
-
     useEffect(() => {
         if (is_livechat_interactive) {
-            LC_API.open_chat_window()
+            window.LiveChatWidget.on('ready', () => {
+                LC_API.open_chat_window()
+            })
         }
     }, [is_livechat_interactive])
-
-    // const myfunc = () => {
-    //     LC_API.open_chat_window()
-    // };
-
-    // const handleliveChat = () => {
-    //     // eslint-disable-next-line no-console
-    //     console.log('hello');
-
-    // }
 
     return (
         <Layout type="static" margin_top={'0'}>
             <SEO title={localize('Live Chat')} description={localize('Live chat')} no_index />
             <StyledContainer>
-                {is_mounted && (
-                    <React.Fragment>
-                        {is_livechat_interactive ? LC_API.open_chat_window() : <InitialLoader />}
-                    </React.Fragment>
-                )}
+                <InitialLoader />
             </StyledContainer>
         </Layout>
     )

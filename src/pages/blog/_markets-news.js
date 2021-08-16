b130d4e3d7caad030b6925d60ea08920ee6087b5
suthesan <suthesankanagasabai@gmail.com>
2021-08-16 15:07:53 +0800

    updated data source

--- src/pages/blog/_markets-news.js
+++ src/pages/blog/_markets-news.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 import { market_news_data } from './_markets_news_data'
 import { SectionContainer, Container, Flex } from 'components/containers'
 import { Header } from 'components/elements/typography'
@@ -20,13 +21,13 @@ const MarketsNewsWrapper = styled.div`
 
     @media ${device.tabletS} {
         grid-template-columns: auto;
-        margin:0 auto;
+        margin: 0 auto;
         max-height: 668px;
     }
 `
 
 const StyledSpan = styled.span`
-    background-color: var(--color-blue-10); 
+    background-color: var(--color-blue-10);
     padding: 3px 8px 1px;
     border-radius: 8px;
 `
@@ -35,7 +36,8 @@ const StyledFlex = styled(Flex)`
     max-height: 83px;
 `
 
-const MarketNews = () => {
+const MarketNews = ({ data }) => {
+    console.log(data) //eslint-disable-line
     return (
         <SectionContainer>
             <Container>
@@ -44,32 +46,50 @@ const MarketNews = () => {
                         Market News
                     </Header>
                     <MarketsNewsWrapper>
-                        {market_news_data.filter((data) => data.report_type === "Weekly report").slice(0, 6).map((data, index) => {
-                            return (
-                                <StyledFlex key={index}>
-                                    <img src={data.img_source} width="104px" height="78px" />
-                                    <Flex ml="8px" fd="column">
-                                        <Header type="paragraph-2" color="blue-9">
-                                            <StyledSpan>{data.report_type}</StyledSpan>
-                                        </Header>
-                                        <Header mt="8px" type="paragraph-1" weight="bold">
-                                            {data.date}
-                                        </Header>
-                                        <Flex mt="auto" height="fit-content" jc="center" ai="center">
-                                            <img src={EyeIcon} width="16px" height="11px" />
-                                            <Header ml="4px" mt="3px" type="small" weight="normal" color='grey-5'>
-                                                {data.read_time} min read
+                        {market_news_data
+                            .filter((data) => data.report_type === 'Weekly report')
+                            .slice(0, 6)
+                            .map((data, index) => {
+                                return (
+                                    <StyledFlex key={index}>
+                                        <img src={data.img_source} width="104px" height="78px" />
+                                        <Flex ml="8px" fd="column">
+                                            <Header type="paragraph-2" color="blue-9">
+                                                <StyledSpan>{data.report_type}</StyledSpan>
                                             </Header>
+                                            <Header mt="8px" type="paragraph-1" weight="bold">
+                                                {data.date}
+                                            </Header>
+                                            <Flex
+                                                mt="auto"
+                                                height="fit-content"
+                                                jc="center"
+                                                ai="center"
+                                            >
+                                                <img src={EyeIcon} width="16px" height="11px" />
+                                                <Header
+                                                    ml="4px"
+                                                    mt="3px"
+                                                    type="small"
+                                                    weight="normal"
+                                                    color="grey-5"
+                                                >
+                                                    {data.read_time} min read
+                                                </Header>
+                                            </Flex>
                                         </Flex>
-                                    </Flex>
-                                </StyledFlex>
-                            )
-                        })}
+                                    </StyledFlex>
+                                )
+                            })}
                     </MarketsNewsWrapper>
                 </Flex>
             </Container>
-        </SectionContainer >
+        </SectionContainer>
     )
 }
 
 export default MarketNews
+
+MarketNews.propTypes = {
+    data: PropTypes.object,
+}

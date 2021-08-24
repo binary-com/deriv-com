15c8b3a5757f73aef590973acd92fae54f2a5df2
Sean Ho <seanho96@yahoo.com>
2021-08-24 13:03:42 +0800

    remove backticks

--- gatsby-config.js
+++ gatsby-config.js
@@ -223,7 +223,7 @@ module.exports = {
         {
             resolve: '@directus/gatsby-source-directus',
             options: {
-                url: `https://cms.deriv.cloud`,
+                url: 'https://cms.deriv.cloud',
                 dev: {
                     refresh: '5s',
                 },

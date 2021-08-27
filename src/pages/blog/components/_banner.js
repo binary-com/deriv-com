94ecd8b5882865092c1366f2274c47929dde5f92
Sean Ho <72253841+sean-binary@users.noreply.github.com>
2021-08-27 16:11:47 +0800

    Sean/change preview to rest api (#2052)
    * add OG image to published articles
    
    * update social sharing link to use current href
    
    * fetch preview blog post with REST api
    
    * fix: sonar cloud bugs
    
    * apply code suggestions
    
    * update preview container styles
    
    Co-authored-by: Sean Ho <seanho96@yahoo.com>

--- src/pages/blog/components/_banner.js
+++ src/pages/blog/components/_banner.js
@@ -4,11 +4,16 @@ import PropTypes from 'prop-types'
 import { QueryImage } from 'components/elements'
 import { Flex } from 'components/containers'
 import { LocalizedLink } from 'components/localization'
+import { cms_assets_end_point } from 'common/constants'
 import device from 'themes/device'
 
 const ParentWrapper = styled(Flex)`
     max-width: ${(props) => (props.max_w ? props.max_w : '792px')};
     margin: 0 auto;
+
+    img {
+        width: 100%;
+    }
 
     @media ${device.tabletS} {
         max-width: ${(props) => (props.max_w_tablet ? props.max_w_tablet : '100%')};
@@ -27,8 +32,8 @@ const MobileWrapper = styled.div`
     }
 `
 
-const Banner = ({ detailsObj }) => {
-    return (
+const Banner = ({ detailsObj, detailsPreviewObj }) => {
+    return detailsObj ? (
         <ParentWrapper max_w={detailsObj.max_w_value} max_w_tablet={detailsObj.max_w_tablet}>
             <LocalizedLink
                 external={detailsObj.isExternal}
@@ -62,11 +67,49 @@ const Banner = ({ detailsObj }) => {
                 )}
             </LocalizedLink>
         </ParentWrapper>
+    ) : (
+        <ParentWrapper
+            max_w={detailsPreviewObj.max_w_value}
+            max_w_tablet={detailsPreviewObj.max_w_tablet}
+        >
+            <LocalizedLink
+                external={detailsPreviewObj.isExternal}
+                to={detailsPreviewObj.redirectLink}
+                rel="noopener noreferrer"
+                target="_blank"
+            >
+                {detailsPreviewObj.imgSrcDesktop && !detailsPreviewObj.imgSrcMobile && (
+                    <>
+                        <img
+                            src={`${cms_assets_end_point}${detailsPreviewObj.imgSrcDesktop}`}
+                            alt={detailsPreviewObj.imgAltDesktop}
+                        />
+                    </>
+                )}
+                {detailsPreviewObj.imgSrcDesktop && detailsPreviewObj.imgSrcMobile && (
+                    <>
+                        <DesktopWrapper>
+                            <img
+                                src={`${cms_assets_end_point}${detailsPreviewObj.imgSrcDesktop}`}
+                                alt={detailsPreviewObj.imgAltDesktop}
+                            />
+                        </DesktopWrapper>
+                        <MobileWrapper>
+                            <img
+                                src={`${cms_assets_end_point}${detailsPreviewObj.imgSrcMobile}`}
+                                alt={detailsPreviewObj.imgAltMobile}
+                            />
+                        </MobileWrapper>
+                    </>
+                )}
+            </LocalizedLink>
+        </ParentWrapper>
     )
 }
 
 Banner.propTypes = {
     detailsObj: PropTypes.object,
+    detailsPreviewObj: PropTypes.object,
 }
 
 export default Banner

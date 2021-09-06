209c8cc632426be87e3e3484c38901eab3f32f0d
Suthesan <suthesan@Suthesans-MacBook-Pro.local>
2021-09-06 23:01:05 +0800

    fix dots

--- src/common/utility.js
+++ src/common/utility.js
@@ -287,5 +287,12 @@ export const getVideoObject = (video_data) => {
     }
 }
 
+const getLimit = (input, limit) => {
+    if (input[limit - 1] === ' ') {
+        return limit - 1
+    }
+    return limit
+}
+
 export const truncateString = (input, limit) =>
-    input.length > limit ? `${input.substring(0, limit)}...` : input
+    input.length > limit ? `${input.substring(0, getLimit(input, limit))}...` : input

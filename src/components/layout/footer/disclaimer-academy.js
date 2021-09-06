fabdc1885b37d4aedcf677a7f9d83e31fcd026a2
Nikolai Berestevich <nikolai.berestevich@regentmarkets.com>
2021-09-06 14:45:15 +0300

    nikolai.berestevich/fix/blog-uk-regulatory

--- src/components/layout/footer/disclaimer-academy.js
+++ src/components/layout/footer/disclaimer-academy.js
@@ -46,18 +46,12 @@ const DisclaimerSectionAcademy = () => {
                 </DisclaimerParagraph>
                 <DisclaimerParagraph>
                     <Localize
-                        translate_text="Deriv (Europe) Limited, W Business Centre, Level 3, Triq Dun Karm, Birkirkara BKR 9033, Malta, is licensed and regulated for synthetic indices by the Malta Gaming Authority <0>(licence no. MGA/B2C/102/2000)</0>, by the Gambling Commission for clients in Great Britain <1>(account no. 39495)</1>, and by the Revenue Commissioners for clients in Ireland (licence no. 1010285)."
+                        translate_text="Deriv (Europe) Limited, W Business Centre, Level 3, Triq Dun Karm, Birkirkara BKR 9033, Malta, is licensed and regulated for synthetic indices by the Malta Gaming Authority <0>(licence no. MGA/B2C/102/2000)</0> and by the Revenue Commissioners for clients in Ireland (licence no. 1010285)."
                         components={[
                             <StaticAsset
                                 key={0}
                                 target="_blank"
                                 href="/regulatory/Deriv_(Europe)_Limited.pdf"
-                                rel="noopener noreferrer"
-                            />,
-                            <StaticAsset
-                                key={1}
-                                target="_blank"
-                                href="https://www.gamblingcommission.gov.uk/public-register/business/detail/39495"
                                 rel="noopener noreferrer"
                             />,
                         ]}

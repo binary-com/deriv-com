3a31e4a3e51a3a51d6c057e5119ec8b7e23d0f8d
Suthesan <suthesan@Suthesans-MacBook-Pro.local>
2021-09-08 14:48:56 +0800

    split featured and non featured video query

--- src/pages/academy/_video-banner.js
+++ src/pages/academy/_video-banner.js
@@ -3,16 +3,22 @@ import PropTypes from 'prop-types'
 import Dbanner from './components/video-banner/_DBanner'
 import { Flex } from 'components/containers'
 
-const DVideoBanner = ({ video_list_data }) => {
+const DVideoBanner = ({ featured_video_list_data, non_featured_video_list_data }) => {
     return (
         <Flex>
-            {video_list_data && video_list_data.length && <Dbanner video_list={video_list_data} />}
+            {non_featured_video_list_data && non_featured_video_list_data.length && (
+                <Dbanner
+                    featured_video_list={featured_video_list_data}
+                    non_featured_video_list={non_featured_video_list_data}
+                />
+            )}
         </Flex>
     )
 }
 
 DVideoBanner.propTypes = {
-    video_list_data: PropTypes.array,
+    featured_video_list_data: PropTypes.array,
+    non_featured_video_list_data: PropTypes.array,
 }
 
 export default DVideoBanner

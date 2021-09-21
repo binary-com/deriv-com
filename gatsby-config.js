c90d2310bd2f2e81466cd3cb65d91ac7097f29b7
Suthesan <suthesan@Suthesans-MacBook-Pro.local>
2021-09-21 10:05:38 +0800

    add crossorigin attr for manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -148,6 +148,7 @@ module.exports = {
                 ],
                 gcm_sender_id: '370236002280',
                 gcm_user_visible_only: true,
+                crossOrigin: `use-credentials`,
                 // TODO: add translations and support for language routes e.g:
                 // localize: [
                 //     {

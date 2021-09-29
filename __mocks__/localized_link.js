c49cf05446ea0d6038b6225f46bd37e164395dd6
yashim-deriv <yashim@deriv.com>
2021-09-29 14:35:05 +0800

    feat: fixed styled svg mock

--- __mocks__/localized_link.js
+++ __mocks__/localized_link.js
@@ -1,8 +1,18 @@
-import React from 'react'
-import { DerivStore } from 'store'
-import { LocationProvider } from 'components/localization/locale-context'
+import React, { useEffect } from 'react'
+import { LocationProvider } from '../src/components/layout/location-context'
+import { LocaleContextWrapper } from '../src/components/localization/locale-context'
+import EURedirect, { useModal } from '../src/components/custom/_eu-redirect-modal'
+import { DerivStore } from '../src/store/index'
 
-export const MockProvider = ({ is_eu_country, children, locale }) => {
+export const MockProvider = ({ children, is_eu_country = false, locale = 'en' }) => {
+    const [has_mounted, setMounted] = React.useState(false)
+    const [modal_payload, setModalPayload] = React.useState({})
+    const [show_modal, toggleModal, closeModal] = useModal()
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
     return (
         <DerivStore.Provider
             value={{
@@ -14,7 +24,24 @@ export const MockProvider = ({ is_eu_country, children, locale }) => {
                 setWebsiteStatus: {},
             }}
         >
-            <LocationProvider pageContext={{ locale }}>{children}</LocationProvider>
+            <LocationProvider
+                has_mounted={has_mounted}
+                show_cookie_banner={false}
+                toggleModal={toggleModal}
+                setModalPayload={setModalPayload}
+            >
+                <LocaleContextWrapper pageContext={{ locale }}>{children}</LocaleContextWrapper>
+                <EURedirect
+                    toggle={toggleModal}
+                    is_open={show_modal}
+                    closeModal={closeModal}
+                    to={modal_payload.to}
+                    target={modal_payload.target}
+                    rel={modal_payload.rel}
+                    ref={modal_payload.ref}
+                    aria_label={modal_payload.aria_label}
+                />
+            </LocationProvider>
         </DerivStore.Provider>
     )
 }

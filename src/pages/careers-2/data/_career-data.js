e0231a474a513f1e9343e9389bf81bbc0d310be6
kevin <kevin@regentmarkets.com>
2021-10-01 17:09:06 +0800

    push

--- src/pages/careers-2/data/_career-data.js
+++ src/pages/careers-2/data/_career-data.js
@@ -47,33 +47,33 @@ const career_data = {
                 background: 'bg_recruitment.png',
                 title: 'Marketing',
                 subtitle:
-                    'We are the marketing team and we’re passionate about communications. We’re split into various teams from content creation and digital outreach, to paid acquisition and affiliate marketing support; our marketing department creates and distributes all of our external client communications for the business. Spanning across all our offices, our marketing team are here to support, educate and inform clients and prospectsabout trading across the world.',
+                    'We bring in the go-getters; from the inspiring leaders to the ambitious grads. We link the best people with the right opportunities, and support them in every way to help them grow in their professional career at Deriv.',
             },
             intro_section: {
                 image: 'recruitment_1.png',
                 paragraph: [
                     {
-                        text: 'Joining this team means your main focus is our clients, at every point in their journey. Whether they are brand new to trading and want to join, they have just joined, or are a long standing client; we provide the communications to help them along their journey to get the best out of our platforms.',
+                        text: 'We’re the ones who build a creative, diverse, and thriving workforce. Our goals are to foster a great employer-employee relationship, advocate for the employees’ emotional and physical well being, and provide a safe and productive work environment.',
                     },
                     {
-                        text: 'Working within marketing means a fast paced environment where you’ll always find a new challenge. We’re a friendly bunch, and every team is aligned to work together towards a common goal. A normal day could mean distributing a full scope marketing campaign to clients after our morning coffee, working on a webinar for affiliates after lunch and finishing the day onboarding a brand new affiliate. There are so many opportunities to get involved in and with so many different aspects of marketing to learn, the career opportunities are endless.',
+                        text: 'Join our team, and help us support employees from day one of the recruitment process and throughout their career at Deriv. We pride ourselves on our work culture, and work hard to ensure people enjoy coming to work at the beginning of a week. We offer training programmes, further knowledge courses and team building activities when you join our company, and it’s our job to ensure we are always striving for greater things across all our offices.',
                     },
                 ],
             },
             testimony_section: {
-                image: 'marketing_2.png',
-                image_mobile: 'marketing_2_mobile.png',
+                image: 'recruitment_2.png',
+                image_mobile: 'recruitment_2_mobile.png',
                 testimony_headline:
-                    '”I joined Deriv in November 2020, in the middle of a global pandemic! I relocated from Wales to join the company as its Content marketing lead which is part of the Marketing team.',
+                    '“I joined the company in April 2019 and started as a Human Resources Administrator, then switched to the Talent Management team in November 2020.',
                 paragraph: [
                     {
-                        text: 'The marketing team collaborates with the other teams daily across multiple offices using communication tools such as Slack and Zoom.',
+                        text: "Our team handles performance management for all of Deriv's workforce — from searching, onboarding, developing, motivating, and retaining high-performing employees. We design and implement practices and strategies to improve business performance by cultivating every employee's productivity.",
                     },
                     {
-                        text: "We're a close-knit department working together to keep all the marketing aligned across the different regions we operate in. Everyone helps each other out no matter which team they're on, and we have a fast-paced and fun work culture. I'm proud to say that we face our team's challenges together, building each other up to create something incredible.",
+                        text: 'A challenge I faced before was my inexperience when I started my current role. Nonetheless, the company gave me every opportunity to study and learn employee performance management. There is a positive and energetic culture within our team, and everyone has endless opportunities to grow and improve.',
                     },
                     {
-                        text: "The best thing about working at Deriv is that colleagues are more like friends, we're honest and open with one another, and we bounce off each other's strengths and weaknesses.”",
+                        text: 'Diversity is one of my favorite things about working at Deriv. My job allows me to meet, interact, and work with people from all over the world.”',
                     },
                 ],
             },

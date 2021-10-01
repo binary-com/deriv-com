7f0394e650f43491bd49e473425dd6f2bb6e4de7
kevin <kevin@regentmarkets.com>
2021-10-01 15:01:05 +0800

    dummy data

---
+++ src/pages/careers2/data/career-data.js
@@ -0,0 +1,25 @@
+export const career_marketing = [
+    {
+        header: {
+            background: '',
+            title: 'Marketing',
+            intro: 'We are the marketing team and we’re passionate about communications. We’re split into various teams from content creation and digital outreach, to paid acquisition and affiliate marketing support; our marketing department creates and distributes all of our external client communications for the business. Spanning across all our offices, our marketing team are here to support, educate and inform clients and prospectsabout trading across the world.',
+        },
+        intro_section: {
+            image: '',
+            first_paragraph:
+                'Joining this team means your main focus is our clients, at every point in their journey. Whether they are brand new to trading and want to join, they have just joined, or are a long standing client; we provide the communications to help them along their journey to get the best out of our platforms.',
+            second_paragraph:
+                'Working within marketing means a fast paced environment where you’ll always find a new challenge. We’re a friendly bunch, and every team is aligned to work together towards a common goal. A normal day could mean distributing a full scope marketing campaign to clients after our morning coffee, working on a webinar for affiliates after lunch and finishing the day onboarding a brand new affiliate. There are so many opportunities to get involved in and with so many different aspects of marketing to learn, the career opportunities are endless.',
+        },
+        testimony_section: {
+            image: '',
+            name: 'Chloe Glen-Jones',
+            job_title: 'Marketing',
+            testimony_headline:
+                'Joining this team means your main focus is our clients, at every point in their journey. Whether they are brand new to trading and want to join, they have just joined, or are a long standing client; we provide the communications to help them along their journey to get the best out of our platforms.',
+            testimony_paragraph:
+                'Working within marketing means a fast paced environment where you’ll always find a new challenge. We’re a friendly bunch, and every team is aligned to work together towards a common goal. A normal day could mean distributing a full scope marketing campaign to clients after our morning coffee, working on a webinar for affiliates after lunch and finishing the day onboarding a brand new affiliate. There are so many opportunities to get involved in and with so many different aspects of marketing to learn, the career opportunities are endless.',
+        },
+    },
+]

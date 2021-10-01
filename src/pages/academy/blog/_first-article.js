ec33b5e386affecead1ab00313d3180ecadd6730
Sean Ho <seanho96@yahoo.com>
2021-10-01 14:00:18 +0800

    fix: first card border radius

--- src/pages/academy/blog/_first-article.js
+++ src/pages/academy/blog/_first-article.js
@@ -60,7 +60,7 @@ const FirstArticle = ({ item }) => {
     return (
         <RedirectLink to={`/academy/blog/posts/${item.slug}/`}>
             <StyledFlex jc="flex-start" mt="96px">
-                <StandardImgWrapper width="592px" br="6px">
+                <StandardImgWrapper width="592px" br="6px 0 0 6px" tabletL_br="6px 6px 0 0">
                     <QueryImage
                         data={getImage(item.main_image.imageFile)}
                         alt={item.main_image.description || ''}

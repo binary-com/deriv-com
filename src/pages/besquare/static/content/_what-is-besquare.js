1acf204777bed023b894e06017605bb64a07ada5
umithra <umithra@deriv.com>
2021-10-04 11:30:43 +0800

    test

--- src/pages/besquare/static/content/_what-is-besquare.js
+++ src/pages/besquare/static/content/_what-is-besquare.js
@@ -3,7 +3,7 @@ export default {
     subtitle:
         'BeSquare is built on the idea that successful professionals are generalists. Here’s an outline of your BeSquare journey that will carry on far beyond the 6 months of the programme:',
     content: [
-        'Start by gaining a comprehensive view of the IT industry by working with all tech teams. Enhance your career options.',
+        'Start by test gaining a comprehensive view of the IT industry by working with all tech teams. Enhance your career options.',
         'Continue to explore your potentials and passions. Discover challenges that call to you.',
         'Start the IT career of your choice. Your familiarity with all domains of the industry enables you to think outside the box.',
         'Find unique solutions as a generalist who sees the big picture. Solve the problems that defeat everyone else.',

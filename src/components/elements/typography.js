39704c31b0f662a39d15b199ce80bb94d96e5c7a
Sean Ho <72253841+sean-binary@users.noreply.github.com>
2021-10-05 13:51:07 +0800

    sean/feat: add responsive style properties to frequently used components (#2143)
    * feat: add responsive style properties to container components
    
    * feat: add responsive style properties to header and text component
    
    * feat: add min width and min height
    
    Co-authored-by: Sean Ho <seanho96@yahoo.com>

--- src/components/elements/typography.js
+++ src/components/elements/typography.js
@@ -1,7 +1,42 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
+import { generateResponsiveStyles } from '../containers/box'
 import { Margins, Paddings } from 'themes/function'
 import device from 'themes/device'
+
+const baseStyles = ({
+    m,
+    mt,
+    ml,
+    mr,
+    mb,
+    p,
+    pt,
+    pl,
+    pr,
+    pb,
+    align,
+    lh,
+    min_width,
+    max_width,
+    min_height,
+    max_height,
+    width,
+    height,
+}) => css`
+    min-width: ${min_width};
+    max-width: ${max_width};
+    min-height: ${min_height};
+    max-height: ${max_height};
+    width: ${width};
+    height: ${height};
+    text-align: ${align};
+    line-height: ${lh};
+    ${Margins({ m, mt, ml, mr, mb })}
+    ${Paddings({ p, pt, pl, pr, pb })}
+`
+
+const responsiveStyles = generateResponsiveStyles(baseStyles)
 
 const createElement = React.createElement
 
@@ -40,6 +75,8 @@ export const Text = styled.p`
     @media ${device.tabletL} {
         font-size: ${(props) => props.size || '16px'};
     }
+
+    ${responsiveStyles}
 `
 
 export const Header = styled(({ as = 'h1', children, ...props }) =>
@@ -132,6 +169,8 @@ export const Header = styled(({ as = 'h1', children, ...props }) =>
             if (props.type === 'extra-small') return '12px'
         }};
     }
+
+    ${responsiveStyles}
 `
 
 export const LinkText = styled(Text).attrs({ as: 'a' })`

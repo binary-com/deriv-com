b62e46711711c94b6a9acff597316aa5c21c6a28
Suthesan <suthesan@Suthesans-MacBook-Pro.local>
2021-10-06 12:45:15 +0800

    updated bg

--- src/pages/careers-2/_layout-components/_hero.js
+++ src/pages/careers-2/_layout-components/_hero.js
@@ -44,8 +44,50 @@ const StyledContainer = styled(Container)`
 
 const query = graphql`
     query {
-        image: file(relativePath: { eq: "careers-2/marketing-bg.png" }) {
-            ...heroImage
+        back_end_bg: file(relativePath: { eq: "careers-2/back-end-bg.png" }) {
+            ...fadeIn
+        }
+        business_excellence_bg: file(relativePath: { eq: "careers-2/business-excellence-bg.png" }) {
+            ...fadeIn
+        }
+        compliance_bg: file(relativePath: { eq: "careers-2/compliance-bg.png" }) {
+            ...fadeIn
+        }
+        cs_bg: file(relativePath: { eq: "careers-2/cs-bg.png" }) {
+            ...fadeIn
+        }
+        security_bg: file(relativePath: { eq: "careers-2/security-bg.png" }) {
+            ...fadeIn
+        }
+        finance_bg: file(relativePath: { eq: "careers-2/finance-bg.png" }) {
+            ...fadeIn
+        }
+        front_end_bg: file(relativePath: { eq: "careers-2/front-end-bg.png" }) {
+            ...fadeIn
+        }
+        internal_audit_bg: file(relativePath: { eq: "careers-2/internal-audit-bg.png" }) {
+            ...fadeIn
+        }
+        marketing_bg: file(relativePath: { eq: "careers-2/marketing-bg.png" }) {
+            ...fadeIn
+        }
+        payments_bg: file(relativePath: { eq: "careers-2/payments-bg.png" }) {
+            ...fadeIn
+        }
+        people_management_bg: file(relativePath: { eq: "careers-2/people-management-bg.png" }) {
+            ...fadeIn
+        }
+        product_bg: file(relativePath: { eq: "careers-2/product-bg.png" }) {
+            ...fadeIn
+        }
+        pm_bg: file(relativePath: { eq: "careers-2/pm-bg.png" }) {
+            ...fadeIn
+        }
+        software_testing_bg: file(relativePath: { eq: "careers-2/software-testing-bg.png" }) {
+            ...fadeIn
+        }
+        trading_operations_bg: file(relativePath: { eq: "careers-2/trading-operations-bg.png" }) {
+            ...fadeIn
         }
     }
 `
@@ -54,7 +96,7 @@ const Hero = (header_data) => {
     const data = useStaticQuery(query)
     return (
         <StyledBackGroundImage
-            data={data.image}
+            data={data[header_data.data.background]}
             alt={'Deriv careers'}
             style={{
                 backgroundSize: `cover`,

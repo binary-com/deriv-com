c4ea0325ebe93516da3f53364b60c6363ebb624f
“pavel-latyshou-deriv” <pavel.latyshou@regentmarkets.com>
2021-10-18 10:09:40 +0300

    fix PicnPic issue

--- src/pages/academy/components/_video-player.js
+++ src/pages/academy/components/_video-player.js
@@ -106,13 +106,7 @@ const VideoPlayer = ({ video_src, closeVideo }) => {
     }
 
     return (
-        <VideoWrapper
-            onClick={() => {
-                closeVideo()
-                document.exitPictureInPicture()
-            }}
-            show={is_show}
-        >
+        <VideoWrapper onClick={() => closeVideo()} show={is_show}>
             <VidPlayerWrapper>
                 <VidDivWrapper>
                     <Flex direction="column">
@@ -127,7 +121,7 @@ const VideoPlayer = ({ video_src, closeVideo }) => {
                         </StyledFlex>
                         <VidPlayer
                             controls
-                            disablePictureInPicture
+                            // disablePictureInPicture
                             controlsList="nodownload"
                             autoPlay
                             onClick={(event) => event.stopPropagation()}

3e466b035d9dc9bd3d63a8893438e06374096233
amina-deriv <amina@deriv.com>
2021-10-18 17:36:38 +0400

    logos added

--- src/images/svg/payment-methods/index.js
+++ src/images/svg/payment-methods/index.js
@@ -1,30 +1,64 @@
-import InternetBankTransfer from './payment-bank-transfer.svg'
-import Paytrust88 from './payment-paytrust.svg'
-import Help2pay from './payment-help-pay.svg'
-import Dragonphoenix from './payment-dragon-phoenix.svg'
-import NganLuong from './payment-ngan-luong.svg'
-import Zingpay from './payment-zing-pay.svg'
 import Visa from './payment-visa.svg'
 import VisaElectron from './payment-visa-electron.svg'
 import Mastercard from './payment-mastercard.svg'
 import Maestro from './payment-maestro.svg'
 import Diners from './payment-diners.svg'
+//JCB
+import InternetBankTransfer from './payment-bank-transfer.svg'
+import Paytrust88 from './payment-paytrust.svg'
+import Dragonphoenix from './payment-dragon-phoenix.svg'
+import NganLuong from './payment-ngan-luong.svg'
+import Help2pay from './payment-help-pay.svg'
+import Zingpay from './payment-zing-pay.svg'
+import Fasapay from './payment-fasapay.svg'
+import PerfectMoney from './payment-perfect-money.svg'
+import Skrill from './payment-skrill.svg'
+import Neteller from './payment-neteller.svg'
+import Webmoney from './payment-webmoney.svg'
+import Paysafecard from './payment-paysafe.svg'
 import Paylivre from './payment-paylivre.svg'
+import Jeton from './payment-jeton.svg'
+///
+import Airtm from './payment-airtm.svg'
 import Bitcoin from './payment-bitcoin.svg'
 import Banxa from './payment-banxa.svg'
+import Onlinenaira from './payment-onlinenaira.svg'
+import Trustly from './payment-trustly.svg'
+import Ethereum from './payment-ethereum.svg'
+import Litecoin from './payment-litecoin.svg'
+import Tether from './payment-tether.svg'
+import Usdc from './payment-usd-coin.svg'
+import Changelly from './payment-changelly.svg'
+import Xanpool from './payment-xanpool.svg'
 export default {
-    InternetBankTransfer,
-    Paytrust88,
-    Help2pay,
-    Dragonphoenix,
-    NganLuong,
-    Zingpay,
     Visa,
     VisaElectron,
     Mastercard,
     Maestro,
     Diners,
+    InternetBankTransfer,
+    Paytrust88,
+    Dragonphoenix,
+    NganLuong,
+    Help2pay,
+    Zingpay,
+    Fasapay,
+    PerfectMoney,
+    Skrill,
+    Neteller,
+    Webmoney,
+    Paysafecard,
     Paylivre,
+    Jeton,
+    Airtm,
     Bitcoin,
     Banxa,
+    Onlinenaira,
+    Trustly,
+    Ethereum,
+    Litecoin,
+    Tether,
+    Usdc,
+    Changelly,
+    Xanpool,
 }

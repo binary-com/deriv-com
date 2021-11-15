f8dda56ad1e6a03f2125797f3b48e6b38bed8006
behnaz-fs <43137011+behnaz-fs@users.noreply.github.com>
2021-11-15 09:31:01 +0330

    behnaz/fx-lp-v1 (#2246)
    * fx lp v1
    
    * 2 lp
    
    * fix cta url review1
    
    * fix cta typo
    
    * svgo

---
+++ src/pages/landing/forex-trading/components/_hero.js
@@ -0,0 +1,164 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { graphql, useStaticQuery } from 'gatsby'
+import { Flex, Container, Show } from 'components/containers'
+import { Header } from 'components/elements'
+import { localize } from 'components/localization'
+import { Background } from 'components/elements/background-image'
+import { LinkButton } from 'components/form'
+import device from 'themes/device.js'
+
+const BackgroundWrapper = styled(Background)`
+    background-size: cover;
+    background-position: bottom right;
+`
+
+const Wrapper = styled(Container)`
+    @media ${device.tabletS} {
+        height: 70rem;
+        margin-left: 0;
+        padding: 2rem 16px 0;
+        flex-direction: column-reverse;
+        justify-content: center;
+    }
+`
+
+const InformationWrapper = styled(Flex)`
+    width: 100%;
+    max-width: 75rem;
+    z-index: 1;
+
+    @media ${device.tabletL} {
+        max-width: 55rem;
+        top: 280px;
+        padding: 0 16px;
+    }
+    @media ${device.mobileL} {
+        max-width: 390px;
+        padding: 0;
+    }
+`
+
+const StyledHeader = styled(Header)`
+    display: flex;
+
+    @media ${device.tabletL} {
+        margin-top: 1.3rem;
+    }
+    @media ${device.mobileS} {
+        margin-top: 15px;
+    }
+`
+
+const HeroContent = styled(Flex)`
+    height: unset;
+
+    ${Header} {
+        display: flex;
+        margin-top: 1rem;
+    }
+
+    @media ${device.mobileL} {
+        ${Header} {
+            margin: 10px 0 0;
+        }
+    }
+`
+
+const TryButton = styled(LinkButton)`
+    padding: 17px 24px;
+    width: min-content;
+    white-space: nowrap;
+    font-size: 18px;
+    border: unset;
+
+    @media ${device.tablet} {
+        padding: 14px 18px;
+        font-size: 14px;
+        margin-top: 40px;
+        font-weight: 700;
+    }
+    @media ${device.mobileL} {
+        margin-top: 20px;
+    }
+    @media ${device.mobileS} {
+        margin-top: 10px;
+    }
+`
+
+const HeroComponent = ({ title, content, background_data }) => {
+    return (
+        <BackgroundWrapper data={background_data}>
+            <Wrapper p="0" justify="space-between" height="63rem">
+                <InformationWrapper height="unset" direction="column">
+                    <StyledHeader type="hero" color="white">
+                        {title}
+                    </StyledHeader>
+                    <HeroContent m="2rem 0 0" direction="column" justify="flex-start">
+                        <Header color="white" type="subtitle-1" weight="normal">
+                            {content}
+                        </Header>
+                    </HeroContent>
+                    <TryButton
+                        m="4.2rem 0 80px"
+                        target="_blank"
+                        rel="noopener noreferrer nofollow"
+                        type="submit"
+                        secondary="true"
+                        to={'https://deriv.com/signup/'}
+                    >
+                        {localize('Start trading')}
+                    </TryButton>
+                </InformationWrapper>
+            </Wrapper>
+        </BackgroundWrapper>
+    )
+}
+
+const query = graphql`
+    query {
+        p2p_hero_background: file(relativePath: { eq: "landing/trade-fx.jpg" }) {
+            ...fadeIn
+        }
+        p2p_hero_background_mobile: file(relativePath: { eq: "landing/trade-fx-m.jpg" }) {
+            ...fadeIn
+        }
+    }
+`
+
+const Hero = ({ title, content }) => {
+    const data = useStaticQuery(query)
+
+    return (
+        <div>
+            <Show.Desktop min_width="800">
+                <HeroComponent
+                    title={title}
+                    content={content}
+                    background_data={data['p2p_hero_background']}
+                />
+            </Show.Desktop>
+            <Show.Mobile>
+                <HeroComponent
+                    title={title}
+                    content={content}
+                    background_data={data['p2p_hero_background_mobile']}
+                />
+            </Show.Mobile>
+        </div>
+    )
+}
+
+HeroComponent.propTypes = {
+    background_data: PropTypes.any,
+    content: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    title: PropTypes.string,
+}
+
+Hero.propTypes = {
+    content: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    title: PropTypes.string,
+}
+
+export default Hero
---
+++ src/pages/landing/forex-trading/index.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import Loadable from '@loadable/component'
+import { Signup, WhatOurClientsSay } from '../../home/_lazy-load'
+import Hero from './components/_hero'
+import ImageTextSwitching from './components/_image-text-switching'
+import Tablebtn from './components/_table-btn'
+import { usePageLoaded } from 'components/hooks/use-page-loaded'
+import { localize, Localize, WithIntl } from 'components/localization'
+import { Appearances } from 'components/custom/signup'
+import Layout from 'components/layout/layout'
+import { SEO } from 'components/containers'
+const IconTextRow = Loadable(() => import('./components/_icon-text-row'))
+
+const DP2P_CONTENT = [
+    {
+        title: <Localize translate_text="Step 1" />,
+        subtitle1: (
+            <Localize
+                translate_text="Log in or sign up for a Deriv account.<0 />"
+                components={[<br key={0} />]}
+            />
+        ),
+        subtitle_mobile1: (
+            <Localize
+                translate_text="Log in or sign up for a Deriv account.<0 />"
+                components={[<br key={0} />]}
+            />
+        ),
+
+        image_name: 'login',
+        image_alt: localize('Login'),
+    },
+    {
+        title: <Localize translate_text="Step 2" />,
+        subtitle1: (
+            <Localize
+                translate_text="Add a Deriv MT5 Financial real account.<0 />"
+                components={[<br key={0} />]}
+            />
+        ),
+        subtitle_mobile1: (
+            <Localize
+                translate_text="Add a Deriv MT5 Financial real account.<0 />"
+                components={[<br key={0} />]}
+            />
+        ),
+
+        image_name: 'dmt5_acc',
+        image_alt: localize('DMT5 account'),
+    },
+    {
+        title: <Localize translate_text="Step 3" />,
+        subtitle1: (
+            <Localize
+                translate_text="Log in to Deriv MT5, select an asset, and start trading.<0 />"
+                components={[<br key={0} />]}
+            />
+        ),
+        subtitle_mobile1: (
+            <Localize
+                translate_text="Log in to Deriv MT5, select an asset, and start trading.<0 />"
+                components={[<br key={0} />]}
+            />
+        ),
+
+        image_name: 'dmt5_login',
+        image_alt: localize('DMT5 login'),
+    },
+]
+
+const ForexTrading = () => {
+    const [is_mounted] = usePageLoaded(false) // needed to fix the second Hero-component during page's loading
+    return (
+        <Layout type="landing-page" is_ppc_redirect={true}>
+            <SEO
+                title={localize('Weekends')}
+                description={localize('Ride the trends even on weekends')}
+            />
+            {is_mounted && (
+                <>
+                    <Hero
+                        title={localize('Take advantage of the best trading conditions by Deriv')}
+                        content={
+                            <Localize translate_text="Your trusted broker with intuitive forex trading platforms, convenient payment options, and a 24/7 support team." />
+                        }
+                    />
+                    <IconTextRow />
+                    <Tablebtn
+                        btnlabel={localize('Get trading on Deriv')}
+                        text={localize('*available for selected pairs:')}
+                    />
+                    <ImageTextSwitching reverse P2P={DP2P_CONTENT} />
+                    <WhatOurClientsSay />
+                    <Signup appearance={Appearances.public} />
+                </>
+            )}
+        </Layout>
+    )
+}
+
+export default WithIntl()(ForexTrading)

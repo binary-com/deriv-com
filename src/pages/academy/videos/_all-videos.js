3815735f293712d58bde37217afcb58cf9bd9e1f
Sean Ho <seanho96@yahoo.com>
2021-11-16 16:16:51 +0800

    feat: add video params when video is clicked

--- src/pages/academy/videos/_all-videos.js
+++ src/pages/academy/videos/_all-videos.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+import { useQueryParam, StringParam } from 'use-query-params'
 import { StyledImg, Container, VideoGrid } from '../common/_styles'
 import VideoPlayer from '../components/_video-player'
 import VideoCard from './_video-card'
@@ -10,6 +11,7 @@ import RightArrow from 'images/svg/tools/black-right-arrow.svg'
 const AllVideos = ({ video_data }) => {
     const [show, setShow] = useState(false)
     const [play_video_id, setPlayVideoId] = useState('')
+    const [video_params, setVideoParams] = useQueryParam('video', StringParam)
 
     useEffect(() => {
         show ? (document.body.style.overflow = 'hidden') : (document.body.style.overflow = 'unset')
@@ -17,22 +19,22 @@ const AllVideos = ({ video_data }) => {
 
     // opens the video player based on the valid video id passed to url params
     useEffect(() => {
-        const params = new URLSearchParams(location.search)
-        const video = params.get('video')
-        const video_track = video_data.find((item) => item.video_id == video)?.video_file.id
-        if (video_track) openVideo(video_track)
+        const video_track = video_data.find((item) => item.video_id == video_params)?.video_file.id
+        if (video_track) openVideo(video_track, video_params)
     }, [])
 
     const play_video_src = `https://cms.deriv.cloud/assets/${play_video_id}`
 
-    const openVideo = (video_id) => {
-        setPlayVideoId(video_id)
+    const openVideo = (track_id, video_id) => {
+        setPlayVideoId(track_id)
+        setVideoParams(video_id)
         setShow(true)
     }
 
     const closeVideo = () => {
         setShow(false)
         setPlayVideoId('')
+        setVideoParams()
     }
     return (
         <Container m="0 auto" fd="column">
@@ -49,7 +51,7 @@ const AllVideos = ({ video_data }) => {
                         <VideoCard
                             key={item.video_id}
                             item={item}
-                            openVideo={() => openVideo(item.video_file.id)}
+                            openVideo={() => openVideo(item.video_file.id, item.video_id)}
                         />
                     )
                 })}

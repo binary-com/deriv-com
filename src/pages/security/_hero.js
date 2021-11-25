7e857a3c57703715654a84265015bb7b627b8a13
niloo-fs <niloofar.sadeghi@firstsource.tech>
2021-11-25 14:12:21 +0330

    hero section

---
+++ src/pages/security/_hero.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import HeroComponent from './components/_hero_component'
+import { Show } from 'components/containers'
+
+const query = graphql`
+    query {
+        hero_background_desktop: file(
+            relativePath: { eq: "security/security-overlay-desktop.jpg" }
+        ) {
+            ...heroImage
+        }
+        hero_background_mobile: file(relativePath: { eq: "security/security-overlay-mobile.jpg" }) {
+            ...heroImage
+        }
+    }
+`
+
+const Hero = () => {
+    const data = useStaticQuery(query)
+
+    return (
+        <>
+            <Show.Desktop>
+                <HeroComponent
+                    background_data={data['hero_background_desktop']}
+                    background_height="70vh"
+                />
+            </Show.Desktop>
+
+            <Show.Mobile>
+                <HeroComponent
+                    background_data={data['hero_background_mobile']}
+                    background_height="60vh"
+                    background_dark="0.3"
+                />
+            </Show.Mobile>
+        </>
+    )
+}
+
+export default Hero

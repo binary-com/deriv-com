7e00f4631f2fda1c9b6afce089b00d04efbc977e
Niloofar Sadeghi <93518187+niloo-fs@users.noreply.github.com>
2021-11-30 07:40:24 +0330

    Niloofar/security-Hero/ about deriv and scope sections (#50235) (#2268)
    * hero section
    
    * about deriv section
    
    * scope section
    
    * resolve reviews
    
    * add font size to scope list

---
+++ src/pages/security/_lazy-load.js
@@ -0,0 +1,5 @@
+import Loadable from '@loadable/component'
+
+export const AboutDeriv = Loadable(() => import('./_about-deriv'))
+export const Rewards = Loadable(() => import('./rewards'))
+export const Scope = Loadable(() => import('./scope'))

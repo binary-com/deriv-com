4f3e2225b7174745693af545ee7a620a2082ba6d
niloo-fs <niloofar.sadeghi@firstsource.tech>
2021-12-01 12:32:29 +0330

    security form resolve ts-lint error

--- src/pages/security/_submit_a_bug_report.js
+++ src/pages/security/_submit_a_bug_report.js
@@ -1,20 +1,9 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
-import { Container, Flex, SectionContainer } from 'components/containers'
-import { Header, Text, BackgroundImage } from 'components/elements'
+import { Container, SectionContainer } from 'components/containers'
+import { Header, Text } from 'components/elements'
 import { localize } from 'components/localization'
 
-const query = graphql`
-    query {
-        form_background: file(relativePath: { eq: "security/security_form_submission.jpg" }) {
-            ...submissionFormImage
-        }
-    }
-`
-
 const SubmitABugReport = () => {
-    const data = useStaticQuery(query)
-
     return (
         <SectionContainer>
             <Container direction="column">
@@ -26,10 +15,6 @@ const SubmitABugReport = () => {
                         'Send us a report using the form below. We’ll contact you after we’ve reviewed your report.',
                     )}
                 </Text>
-
-                <Flex>
-                    <BackgroundImage data={data}></BackgroundImage>
-                </Flex>
             </Container>
         </SectionContainer>
     )

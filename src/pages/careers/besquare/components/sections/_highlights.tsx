3ac6a26dd39e4b5fa46f664bc37a0f8c8b8b2325
yashim-deriv <yashim@deriv.com>
2021-12-07 16:29:43 +0800

    fix: return type

--- src/pages/careers/besquare/components/sections/_highlights.tsx
+++ src/pages/careers/besquare/components/sections/_highlights.tsx
@@ -1,9 +1,9 @@
-import React, { ReactNode } from 'react'
+import React, { ReactElement } from 'react'
 import HighlightVideo from '../../static/videos/highlights.mp4'
 import { VideoPlayer } from 'components/elements'
 import { Flex } from 'components/containers'
 
-const Highlights = (): ReactNode => {
+const Highlights = (): ReactElement => {
     return (
         <Flex mb="80px">
             <VideoPlayer src={HighlightVideo} max_width="900px" mobile_max_width="90%" controls />

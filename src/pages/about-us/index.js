5ecbcb9b11bd856e69c85f8af8d0e20a1758de96
Nikolai Berestevich <nikolai.berestevich@regentmarkets.com>
2021-12-09 09:15:07 +0300

    nikolai.berestevich/feat/add header and footer

--- src/pages/about-us/index.js
+++ src/pages/about-us/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import { WithIntl } from 'components/localization'
+import { localize, WithIntl } from 'components/localization'
+import { SEO } from 'components/containers'
+import Layout from 'components/layout/layout'
 
 const StyledDiv = styled.div`
     font-size: 330%;
@@ -8,7 +10,18 @@ const StyledDiv = styled.div`
 `
 
 const AboutUs = () => {
-    return <StyledDiv>About Us new page</StyledDiv>
+    return (
+        <Layout>
+            <SEO
+                title={localize('Articles, trading guide and resources | Deriv')}
+                description={localize(
+                    "If you are looking for trading guide or tutorials, visit Deriv's trading academy and learn how to trade online.",
+                )}
+                // meta_attributes={meta_attributes}
+            />
+            <StyledDiv>About Us new page</StyledDiv>{' '}
+        </Layout>
+    )
 }
 
 AboutUs.propTypes = {

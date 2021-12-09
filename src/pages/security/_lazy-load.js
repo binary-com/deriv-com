18fcf73439b72ab8255e34e5c90ea82c80c41317
Niloofar Sadeghi <93518187+niloo-fs@users.noreply.github.com>
2021-12-09 08:46:45 +0330

    Niloofar Sadeghi / Security- TNC update (#2295)
    * hero section
    
    * about deriv section
    
    * scope section
    
    * resolve reviews
    
    * add font size to scope list
    
    * Vulnerabilities types page
    
    * see vulnerability types link
    
    * add security to partner nav
    
    * bug submission form
    
    * security form resolve ts-lint error
    
    * security form resolve ts-lint error
    
    * resolve issues
    
    * clear extra files
    
    * bug bounty program- update tnc
    
    * resolve review comments
    
    * decrease svg images size using svgo
    
    * resolve reviewed comments
    
    * resolve font sizes in vulnerabilities types page

--- src/pages/security/_lazy-load.js
+++ src/pages/security/_lazy-load.js
@@ -1,6 +1,4 @@
 import Loadable from '@loadable/component'
 
-export const AboutDeriv = Loadable(() => import('./_about-deriv'))
 export const Rewards = Loadable(() => import('./rewards'))
-export const Scope = Loadable(() => import('./_scope'))
 export const SubmitABugReport = Loadable(() => import('./_submit_a_bug_report'))

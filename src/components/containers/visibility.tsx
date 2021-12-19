c21c15e42afea0efdab3f29cf8cc1dcbe45294f8
Prince <prince@deriv.com>
2021-12-19 14:06:46 +0400

    Feat: added show desktop/mobile component rules

--- src/components/containers/visibility.tsx
+++ src/components/containers/visibility.tsx
@@ -1,72 +1,52 @@
+/* eslint-disable react/prop-types */
+/* TODO: Remove the above once fully typed the whole codebae */
+
 import React, { ReactNode, useEffect, useState } from 'react'
+import { BrowserView, MobileView, TabletView, isTablet } from 'react-device-detect'
 import styled from 'styled-components'
-import { Breakpoint, setDefaultBreakpoints } from 'react-socks'
-import { size } from 'themes/device'
-import { useBrowserResize } from 'components/hooks/use-browser-resize'
-import { Box } from 'components/containers'
+import device, { size } from 'themes/device'
 
-// const NoMobile = styled(Box)`
-//     background: red;
+const DesktopLayer = styled.div`
+    @media ${device.tabletS} {
+        display: none;
+    }
+`
+const MobileLayer = styled.div`
+    @media (min-width: ${size.tablet}px) {
+        display: none;
+    }
+`
 
-//     // @media ${device.mobile} {
-//     //     display: none;
-//     // }
-// `
+const deviceRenderer = (): boolean => {
+    const [is_loaded, setIsLoaded] = useState<boolean>(false)
 
-// const NoDesktop = styled(Box)`
-//     @media min-width(${device.laptop}) {
-//         display: none;
-//     }
-// `
+    useEffect(() => {
+        setIsLoaded(true)
+    }, [BrowserView, MobileView])
 
-// const pageDetails = (): boolean[] => {
-//     const { mobileL } = size
-//     const [is_ready, setIsReady] = useState<boolean>(false)
-//     const [is_mobile] = useBrowserResize(mobileL)
-
-//     useEffect(() => {
-//         setTimeout(() => {
-//             setIsReady(true)
-//         })
-//     }, [is_mobile])
-
-//     return [is_ready, is_mobile]
-// }
-
-// export const Desktop: ReactNode = ({ children }) => {
-//     const [is_ready, is_mobile] = pageDetails()
-
-//     if (!is_ready) {
-//         return <NoMobile>{children}</NoMobile>
-//     } else {
-//         if (!is_mobile) {
-//             return <NoMobile>{children}</NoMobile>
-//         }
-//     }
-
-//     return null
-// }
-
-// export const Mobile: ReactNode = ({ children }) => {
-//     const [is_ready, is_mobile] = pageDetails()
-
-//     if (!is_ready) {
-//         return <NoDesktop>{children}</NoDesktop>
-//     } else {
-//         if (is_mobile) {
-//             return children
-//         }
-//     }
-
-//     return null
-// }
-
-setDefaultBreakpoints([{ mobile: size.mobileL, desktop: size.laptop }])
+    return is_loaded
+}
 
 export const Desktop: ReactNode = ({ children }) => {
-    return <Breakpoint mobile only></Breakpoint>
+    const is_loaded = deviceRenderer()
+
+    return is_loaded ? (
+        isTablet ? (
+            <TabletView>{children}</TabletView>
+        ) : (
+            <BrowserView>{children}</BrowserView>
+        )
+    ) : (
+        <DesktopLayer>{children}</DesktopLayer>
+    )
 }
 
 export const Mobile: ReactNode = ({ children }) => {
-    return <Breakpoint desktop up></Breakpoint>
+    const is_loaded = deviceRenderer()
+
+    return is_loaded && !isTablet ? (
+        <MobileView>{children}</MobileView>
+    ) : (
+        <MobileLayer>{children}</MobileLayer>
+    )
 }

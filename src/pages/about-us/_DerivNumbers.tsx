f90ee7d253908165b1c8f14e8997d693240d8015
pavel-latyshou-deriv <90393787+pavel-latyshou-deriv@users.noreply.github.com>
2021-12-22 07:46:33 +0300

    Deriv numbers block (#2364)
    * create Principles page
    
    * typescypt
    
    * svg optimize
    
    * update branch
    
    * Deriv numbers block
    
    Co-authored-by: “pavel-latyshou-deriv” <“pavel.latyshou@regentmarkets.com”>

---
+++ src/pages/about-us/_DerivNumbers.tsx
@@ -0,0 +1,150 @@
+import React from 'react'
+import styled from 'styled-components'
+import { localize } from 'components/localization'
+import { SectionContainer, Flex, CssGrid } from 'components/containers'
+import { Header, Text } from 'components/elements'
+import device from 'themes/device'
+
+const StyledSection = styled(SectionContainer)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 24px 16px 120px;
+    background-color: var(--color-white);
+
+    @media ${device.tablet} {
+        padding: 40px 16px;
+    }
+`
+
+const StyledFlex = styled(Flex)`
+    margin: 0 2rem;
+    max-width: 1200px;
+    justify-content: space-around;
+
+    @media ${device.laptop} {
+        flex-direction: column;
+        align-items: center;
+    }
+`
+
+const TitleSection = styled(Flex)`
+    max-width: 46rem;
+    flex-direction: column;
+
+    @media ${device.laptop} {
+        margin-bottom: 8rem;
+        max-width: 90rem;
+        grid-column-gap: 4rem;
+    }
+
+    @media ${device.laptop} {
+        margin-bottom: 24px;
+    }
+`
+
+const NumberSection = styled(CssGrid)`
+    max-width: 588px;
+
+    @media ${device.tabletL} {
+        grid-column-gap: 4rem;
+    }
+
+    @media ${device.tabletS} {
+        grid-template-columns: minmax(auto, 484px);
+        grid-row-gap: 24px;
+        grid-template-rows: 1fr 1fr 1fr 1fr;
+        justify-content: start;
+    }
+`
+
+const TitleHeader = styled(Header)`
+    font-size: 48px;
+    line-height: 60px;
+
+    @media ${device.laptop} {
+        text-align: center;
+    }
+
+    @media ${device.tabletS} {
+        text-align: left;
+    }
+
+    @media ${device.mobileL} {
+        width: auto;
+        text-align: left;
+    }
+`
+
+const StyledHeader = styled(Header)`
+    @media ${device.laptop} {
+        text-align: center;
+    }
+
+    @media ${device.tabletS} {
+        text-align: left;
+    }
+`
+
+const NumberHeader = styled(Text)`
+    font-weight: bold;
+    line-height: 6rem;
+    text-align: left;
+
+    @media ${device.mobileL} {
+        margin-bottom: 8px;
+    }
+`
+
+const NumberText = styled(Text)`
+    font-weight: 400;
+    text-align: left;
+`
+
+const DerivNumbers = () => {
+    return (
+        <StyledSection>
+            <StyledFlex>
+                <TitleSection>
+                    <TitleHeader as="h6" color="black-2" align="left">
+                        {localize('Deriv in numbers')}
+                    </TitleHeader>
+                    <StyledHeader as="h6" size="24px" align="left" weight="400">
+                        {localize(
+                            'We aim to deliver market-leading products that are trusted around the world. ',
+                        )}
+                    </StyledHeader>
+                </TitleSection>
+                <NumberSection columns="1fr 1fr" rows="1fr 1fr" column_gap="40px" row_gap="4rem">
+                    <Flex fd="column">
+                        <NumberHeader size="48px">{localize('2.5M+')}</NumberHeader>
+                        <NumberText size="20px" align="left">
+                            {localize('traders worldwide')}
+                        </NumberText>
+                    </Flex>
+                    <Flex fd="column">
+                        <NumberHeader size="48px">{localize('USD 26M+')}</NumberHeader>
+                        <NumberText size="20px" align="left">
+                            {localize('withdrawals last month')}
+                        </NumberText>
+                    </Flex>
+                    <Flex fd="column">
+                        <NumberHeader size="48px">{localize('114M+')}</NumberHeader>
+                        <NumberText size="20px" align="left">
+                            {localize('trades last month')}
+                        </NumberText>
+                    </Flex>
+                    <Flex fd="column">
+                        <NumberHeader size="48px">{localize('USD 10B+')}</NumberHeader>
+                        <NumberText size="20px" align="left">
+                            {localize('total trade turnover')}
+                        </NumberText>
+                    </Flex>
+                </NumberSection>
+            </StyledFlex>
+        </StyledSection>
+    )
+}
+
+export default DerivNumbers

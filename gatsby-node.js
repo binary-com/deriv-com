e16dfbab4efb03eb36645b47ba8f50276c9bed86
Sean Ho <seanho96@yahoo.com>
2021-12-24 17:25:46 +0800

    add source nodes of videos from Vimeo

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,9 @@
 /* eslint-disable import/order */
 const language_config = require(`./i18n-config.js`)
 const path = require('path')
+const fs = require('fs')
+const crypto = require('crypto')
+const Vimeo = require('vimeo').Vimeo
 
 const translations_cache = {}
 // Based upon https://github.com/gatsbyjs/gatsby/tree/master/examples/using-i18n
@@ -16,8 +19,8 @@ exports.onCreatePage = ({ page, actions }) => {
     const is_story = /story/g.test(page.path)
     const is_market = /markets/g.test(page.path)
     const is_cfds = /cfds/g.test(page.path)
-    const is_landing_ebooks =/landing\/ebooks/g.test(page.path)
-    
+    const is_landing_ebooks = /landing\/ebooks/g.test(page.path)
+
     if (is_landing_ebooks) {
         createRedirect({
             fromPath: `/landing/ebooks/`,
@@ -32,7 +35,7 @@ exports.onCreatePage = ({ page, actions }) => {
             isPermanent: true,
         })
     }
-    
+
     if (is_responsible_trading) {
         createRedirect({
             fromPath: `/responsible-trading/`,
@@ -344,4 +347,98 @@ exports.createPages = async ({ reporter, actions, graphql }) => {
             },
         })
     })
+}
+
+// Create nodes for the videos from Vimeo
+let hasVideoFiles = false
+
+exports.sourceNodes = async (
+    { actions: { createNode, createNodeField, touchNode }, store, cache, createNodeId },
+    pluginOptions,
+) => {
+    // const options = { ...defaultOptions, ...pluginOptions }
+    // const { clientId, clientSecret, accessToken } = options
+
+    const clientId = process.env.VIMEO_CLIENT_ID
+    const clientSecret = process.env.VIMEO_CLIENT_SECRET
+    const accessToken = process.env.VIMEO_ACCESS_TOKEN
+
+    const client = new Vimeo(clientId, clientSecret, accessToken)
+    const videos = await new Promise((resolve, reject) => {
+        client.request(
+            {
+                method: 'GET',
+                path: '/me/videos?per_page=100', // /me/videos/{id}
+            },
+            (error, body, status_code, headers) => {
+                if (error) reject(error)
+                resolve(body.data)
+            },
+        )
+    })
+
+    const videoFiles = videos && videos.filter((video) => video.files)
+    hasVideoFiles = videoFiles.length !== 0
+
+    if (!hasVideoFiles) {
+        console.info(
+            'Can\'t access video files through Vimeo API on this account. Won\'t create "VimeoSrcset" fragment.',
+        )
+        console.info(
+            'Please make sure that you\'re on a Pro plan and that "private" and "video_files" are in the scope of your token.',
+        )
+    }
+
+    videos &&
+        videos.map((video) => {
+            const nodeData = {
+                srcset: hasVideoFiles ? video.files : false,
+                name: video.name,
+                width: video.width,
+                height: video.height,
+                aspectRatio: video.width / video.height,
+                description: video.description,
+                pictures: video.pictures,
+                user: video.user,
+                link: video.link,
+                duration: video.duration,
+            }
+            createNode({
+                // Data for the node.
+                ...nodeData,
+
+                // Required fields.
+                id: video.uri.split('/')[2],
+                parent: '__SOURCE__', // or null if it's a source node without a parent
+                children: [],
+                internal: {
+                    type: `Vimeo`,
+                    contentDigest: crypto
+                        .createHash(`md5`)
+                        .update(JSON.stringify(nodeData))
+                        .digest(`hex`),
+                },
+            })
+        })
+}
+
+exports.onPreExtractQueries = async ({ store, getNodesByType }) => {
+    if (hasVideoFiles) {
+        const program = store.getState().program
+
+        // Check if there are any Vimeo nodes. If so add fragments for Vimeo.
+        // The fragment will cause an error if there are no Vimeo nodes.
+        if (getNodesByType(`Vimeo`).length == 0) {
+            return
+        }
+
+        // We have Vimeo nodes so let's add our fragments to .cache/fragments.
+        await fs.copyFile(
+            require.resolve(`${__dirname}/src/components/graphql/vimeo-fragments.js`),
+            `${program.directory}/.cache/fragments/vimeo-fragments.js`,
+            (err) => {
+                if (err) throw err
+            },
+        )
+    }
 }

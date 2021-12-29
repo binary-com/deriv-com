a1ebe3b5ebff25d4cc72dc6f9c0bfcc695693efd
Suthesan Kanagasabai <45410833+suthesan-deriv@users.noreply.github.com>
2021-12-29 14:29:41 +0800

    suthesh/blog-geo-conditions (#2262)
    * filter contents for uk, eu, row
    
    * fix code smell
    
    * geo-locking for banners redone
    
    * academy query as a fragment
    
    * split and rename fragment files
    
    * added geo locking for videos
    
    * fallback for featured blogs
    
    * feat: CTA button to open in new tab
    
    * fix: cursor pointer outside of card
    
    * feat: add visibility logic to blog & videos page
    
    * fix: add is not uk country check for EU view
    
    * fix: no video cards shown
    
    Co-authored-by: Suthesan <suthesan@Suthesans-MacBook-Pro.local>
    Co-authored-by: Sean Ho <seanho96@yahoo.com>

--- src/pages/academy/index.js
+++ src/pages/academy/index.js
@@ -13,314 +13,16 @@ import { localize, WithIntl } from 'components/localization'
 import { Carousel } from 'components/elements'
 import { DerivStore } from 'store'
 
+export const query = graphql`
+    query {
+        ...academyQuery
+    }
+`
+
 const MainWrapper = styled(Flex)`
     background-color: var(--color-white);
     flex-direction: column;
     overflow: hidden;
-`
-
-export const query = graphql`
-    query HomepageQuery {
-        directus {
-            homepage_banners(filter: { status: { _eq: "published" } }) {
-                order
-                id
-                button_text
-                link
-                heading
-                sub_heading
-                desktop_hero: image {
-                    imageFile {
-                        childImageSharp {
-                            fluid(quality: 100) {
-                                ...GatsbyImageSharpFluid_withWebp
-                            }
-                        }
-                    }
-                    id
-                    description
-                }
-            }
-            market_news: blog(
-                filter: {
-                    tags: { tags_id: { tag_name: { _contains: "Market report" } } }
-                    status: { _eq: "published" }
-                    test_data: { _eq: false }
-                }
-                limit: 6
-                sort: "-published_date"
-            ) {
-                id
-                blog_title
-                slug
-                tags {
-                    tags_id {
-                        tag_name
-                    }
-                }
-                blog_post
-                main_image {
-                    imageFile {
-                        childImageSharp {
-                            gatsbyImageData
-                        }
-                    }
-                    id
-                }
-            }
-            market_news_eu: blog(
-                filter: {
-                    tags: { tags_id: { tag_name: { _contains: "Market report" } } }
-                    status: { _eq: "published" }
-                    hide_for_eu: { _eq: false }
-                    test_data: { _eq: false }
-                }
-                limit: 6
-                sort: "-published_date"
-            ) {
-                id
-                blog_title
-                slug
-                hide_for_eu
-                tags {
-                    tags_id {
-                        tag_name
-                    }
-                }
-                blog_post
-                main_image {
-                    imageFile {
-                        childImageSharp {
-                            gatsbyImageData
-                        }
-                    }
-                    id
-                }
-            }
-            videos(limit: 6, filter: { status: { _eq: "published" } }, sort: "-published_date") {
-                video_title
-                published_date
-                video_description
-                video_duration
-                featured
-                video_thumbnail {
-                    id
-                    title
-                }
-                video_file {
-                    id
-                }
-                tags {
-                    tags_id {
-                        tag_name
-                    }
-                }
-            }
-            videos_eu: videos(
-                limit: 6
-                filter: { status: { _eq: "published" }, hide_for_eu: { _eq: false } }
-                sort: "-published_date"
-            ) {
-                video_title
-                published_date
-                video_description
-                video_duration
-                featured
-                hide_for_eu
-                video_thumbnail {
-                    id
-                    title
-                }
-                video_file {
-                    id
-                }
-                tags {
-                    tags_id {
-                        tag_name
-                    }
-                }
-            }
-            featured_video: videos(
-                filter: { status: { _eq: "published" }, featured: { _eq: true } }
-                sort: "-published_date"
-                limit: 1
-            ) {
-                video_title
-                published_date
-                video_description
-                video_duration
-                featured
-                hide_for_eu
-                video_thumbnail {
-                    id
-                    title
-                }
-                video_file {
-                    id
-                }
-                tags {
-                    tags_id {
-                        tag_name
-                    }
-                }
-            }
-            featured_video_eu: videos(
-                filter: {
-                    status: { _eq: "published" }
-                    featured: { _eq: true }
-                    hide_for_eu: { _eq: false }
-                }
-                sort: "-published_date"
-                limit: 1
-            ) {
-                video_title
-                published_date
-                video_description
-                video_duration
-                featured
-                hide_for_eu
-                video_thumbnail {
-                    id
-                    title
-                }
-                video_file {
-                    id
-                }
-                tags {
-                    tags_id {
-                        tag_name
-                    }
-                }
-            }
-            recent: blog(
-                filter: { status: { _eq: "published" }, test_data: { _eq: false } }
-                sort: "-published_date"
-                limit: 8
-            ) {
-                id
-                main_image {
-                    id
-                    description
-                    imageFile {
-                        childImageSharp {
-                            gatsbyImageData
-                        }
-                    }
-                }
-                slug
-                published_date
-                featured
-                tags {
-                    id
-                    tags_id {
-                        tag_name
-                    }
-                }
-                blog_title
-                blog_description
-                blog_post
-            }
-            recent_eu: blog(
-                filter: {
-                    status: { _eq: "published" }
-                    hide_for_eu: { _eq: false }
-                    test_data: { _eq: false }
-                }
-                sort: "-published_date"
-                limit: 8
-            ) {
-                id
-                main_image {
-                    id
-                    description
-                    imageFile {
-                        childImageSharp {
-                            gatsbyImageData
-                        }
-                    }
-                }
-                slug
-                published_date
-                featured
-                hide_for_eu
-                tags {
-                    id
-                    tags_id {
-                        tag_name
-                    }
-                }
-                blog_title
-                blog_description
-                blog_post
-            }
-            featured: blog(
-                filter: {
-                    status: { _eq: "published" }
-                    featured: { _eq: true }
-                    test_data: { _eq: false }
-                }
-                sort: "-published_date"
-                limit: 8
-            ) {
-                id
-                main_image {
-                    id
-                    description
-                    imageFile {
-                        childImageSharp {
-                            gatsbyImageData
-                        }
-                    }
-                }
-                slug
-                published_date
-                featured
-                tags {
-                    id
-                    tags_id {
-                        tag_name
-                    }
-                }
-                blog_title
-                blog_description
-                blog_post
-            }
-            featured_eu: blog(
-                filter: {
-                    status: { _eq: "published" }
-                    featured: { _eq: true }
-                    hide_for_eu: { _eq: false }
-                    test_data: { _eq: false }
-                }
-                sort: "-published_date"
-                limit: 8
-            ) {
-                id
-                main_image {
-                    id
-                    description
-                    imageFile {
-                        childImageSharp {
-                            gatsbyImageData
-                        }
-                    }
-                }
-                slug
-                published_date
-                featured
-                hide_for_eu
-                tags {
-                    id
-                    tags_id {
-                        tag_name
-                    }
-                }
-                blog_title
-                blog_description
-                blog_post
-            }
-        }
-    }
 `
 
 const DerivBlog = ({ data }) => {
@@ -346,25 +48,42 @@ const DerivBlog = ({ data }) => {
         },
     }
 
-    const { is_eu_country } = React.useContext(DerivStore)
+    const { is_eu_country, is_uk_country } = React.useContext(DerivStore)
 
-    const homepage_banner_data = data.directus.homepage_banners
+    let market_news_data,
+        recent_data,
+        featured_data,
+        homepage_banner_data,
+        non_featured_video_list_data,
+        featured_video_list_data
+
+    // We need to include the !is_uk_country check together with is_eu_country because 'gb'
+    // is a valid country code for both EU and UK in our country base.
+    if (is_eu_country && !is_uk_country) {
+        market_news_data = data.directus.market_news_eu
+        recent_data = data.directus.recent_eu
+        featured_data = data.directus.featured_eu
+        homepage_banner_data = data.directus.homepage_banners_eu
+        non_featured_video_list_data = data.directus.videos_eu
+        featured_video_list_data = data.directus.featured_video_eu
+    } else if (is_uk_country) {
+        market_news_data = data.directus.market_news_uk
+        recent_data = data.directus.recent_uk
+        featured_data = data.directus.featured_uk
+        homepage_banner_data = data.directus.homepage_banners_uk
+        non_featured_video_list_data = data.directus.videos_uk
+        featured_video_list_data = data.directus.featured_video_uk
+    } else {
+        market_news_data = data.directus.market_news
+        recent_data = data.directus.recent
+        featured_data = data.directus.featured
+        homepage_banner_data = data.directus.homepage_banners
+        non_featured_video_list_data = data.directus.videos
+        featured_video_list_data = data.directus.featured_video
+    }
 
     //arranges homepage banners in ascendingly on order value
     homepage_banner_data.sort((a, b) => parseInt(a.order) - parseInt(b.order))
-
-    const market_news_data = is_eu_country
-        ? data.directus.market_news_eu
-        : data.directus.market_news
-
-    const recent_data = is_eu_country ? data.directus.recent_eu : data.directus.recent
-    const featured_data = is_eu_country ? data.directus.featured_eu : data.directus.featured
-    const non_featured_video_list_data = is_eu_country
-        ? data.directus.videos_eu
-        : data.directus.videos
-    const featured_video_list_data = is_eu_country
-        ? data.directus.featured_video_eu
-        : data.directus.featured_video
 
     return (
         <Layout type="academy">

09d1239e28640001dd2c242f2a537990843f82e6
yashimwong <yashimwong@gmail.com>
2021-12-30 22:48:05 +0800

    fix: LCP for slideshow

--- src/components/elements/slideshow.tsx
+++ src/components/elements/slideshow.tsx
@@ -9,7 +9,9 @@ type SlideshowProps = {
     is_eager?: boolean
 }
 
-const StyledImage = styled(QueryImage)`
+const StyledImage = styled(QueryImage)<{ $is_hidden: boolean }>`
+    display: ${({ $is_hidden }) => ($is_hidden ? 'none' : '')};
+
     .gatsby-image-wrapper {
         div {
             transition: opacity ease-in-out 2s;
@@ -35,19 +37,18 @@ const Slideshow = ({ slides = [], interval, is_eager = true }: SlideshowProps) =
     return (
         <div>
             {slides.map((slide, index) => {
-                if (active_index === index) {
-                    const { key, image } = slide
-                    return (
-                        <StyledImage
-                            key={key}
-                            data={image}
-                            alt="platform devices"
-                            width="100%"
-                            height="346"
-                            loading={is_eager ? 'eager' : 'lazy'}
-                        />
-                    )
-                }
+                const { key, image } = slide
+                return (
+                    <StyledImage
+                        key={key}
+                        data={image}
+                        alt="platform devices"
+                        width="100%"
+                        height="346"
+                        loading={is_eager ? 'eager' : 'lazy'}
+                        $is_hidden={active_index !== index}
+                    />
+                )
             })}
         </div>
     )

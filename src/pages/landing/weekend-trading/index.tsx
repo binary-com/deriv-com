f56d69143d1e09d465fa36dc3567d4ddf6a9c8cc
niloo-fs <niloofar.sadeghi@firstsource.tech>
2022-01-05 20:24:35 +0330

    rebuild

--- src/pages/landing/weekend-trading/index.tsx
+++ src/pages/landing/weekend-trading/index.tsx
@@ -29,7 +29,6 @@ const DP2P_CONTENT = [
         subtitle_mobile2: (
             <Localize translate_text="Available to trade on Deriv MT5 and Deriv X (with margin), DTrader (with options and multipliers), and Deriv GO (with multipliers)." />
         ),
-
         image_name: 'buy_sell',
         image_alt: localize('Buy and sell'),
     },

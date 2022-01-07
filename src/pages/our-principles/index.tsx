94db75fbb31e2bda0d7766bd1f630a9a55461196
Kevin <kevin@regentmarkets.com>
2022-01-07 09:47:54 +0800

    solve comment

--- src/pages/our-principles/index.tsx
+++ src/pages/our-principles/index.tsx
@@ -99,7 +99,7 @@ const OurPrinciples = (): React.ReactNode => {
             <SEO title={localize('Our principles | Deriv')} />
             <TitleSection padding="120px 0 24px 10rem">
                 <StyledContainer>
-                    <Header as="h1" type="hero" weight="700" align="left" mb="24px">
+                    <Header as="h1" type="hero" align="left" mb="24px">
                         {localize('Our principles')}
                     </Header>
                     <StyledMainText>

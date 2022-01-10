f0cd2ad2aecba072f8ab09337a54a918f9519c40
Niloofar Sadeghi <93518187+niloo-fs@users.noreply.github.com>
2022-01-10 14:10:37 +0330

    fix security page design issues (#2462)

--- src/pages/security/_about-deriv.tsx
+++ src/pages/security/_about-deriv.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import styled from 'styled-components'
-import { localize } from 'components/localization'
+import { localize, Localize } from 'components/localization'
 import { Container, Flex } from 'components/containers'
 import { Header, QueryImage } from 'components/elements'
+import { useBrowserResize } from 'components/hooks/use-browser-resize'
 import device from 'themes/device'
 
 const query = graphql`
@@ -24,37 +25,64 @@ const ImageWrapper = styled.div`
     }
 `
 
+const WrapContainer = styled(Flex)`
+    align-items: center;
+
+    @media ${device.laptopM} {
+        flex-direction: column;
+    }
+`
+
+const WrapText = styled(Flex)`
+    @media ${device.laptopM} {
+        margin-bottom: 2.5rem;
+    }
+`
+
+const StyledHeader = styled(Header)`
+    @media ${device.laptopM} {
+        text-align: center;
+    }
+`
+
+const SeparatorLine = styled.div`
+    height: 24px;
+    @media ${device.laptopM} {
+        height: 20px;
+    }
+`
+
 const AboutDeriv = () => {
     const data = useStaticQuery(query)
+    const [is_mobile] = useBrowserResize()
+    const image_width = is_mobile ? '328px' : '384px'
+    const image_height = is_mobile ? '187px' : '224px'
 
     return (
         <Container>
-            <Flex ai="center" p="8.2rem 0" laptopM={{ fd: 'column' }}>
-                <Flex
-                    direction="column"
-                    max_width="38.4rem"
-                    tablet={{ max_width: '100%' }}
-                    laptopM={{ mb: '2.5rem' }}
-                >
-                    <Header mb="0.8rem" as="h2" type="heading-2" laptopM={{ align: 'center' }}>
+            <WrapContainer p="8rem 0" tablet={{ p: '40px 0' }}>
+                <WrapText direction="column" max_width="38.4rem" tablet={{ max_width: '100%' }}>
+                    <StyledHeader mb="0.8rem" as="h2" type="heading-2">
                         {localize('About Deriv')}
+                    </StyledHeader>
+                    <Header type="paragraph-1" weight="normal" as="p">
+                        <Localize
+                            translate_text="We provide online trading services to over 2 million clients via desktop and mobile applications across multiple platforms. 
+                            <0/>Security is important to us. We continuously improve our products and services by collaborating with independent security researchers worldwide."
+                            components={[<SeparatorLine key={0} />]}
+                        />
                     </Header>
-                    <Header as="p" type="paragraph-1" weight="normal">
-                        {localize(
-                            'We provide online trading services to over 2 million clients via desktop and mobile applications across multiple platforms.',
-                        )}
-                    </Header>
+                </WrapText>
 
-                    <Header as="p" type="paragraph-1" weight="normal" mt="2.4rem">
-                        {localize(
-                            'Security is important to us. We continuously improve our products and services by collaborating with independent security researchers worldwide.',
-                        )}
-                    </Header>
-                </Flex>
                 <ImageWrapper>
-                    <QueryImage data={data.deriv_platform} alt={'Deriv platform'} />
+                    <QueryImage
+                        data={data.deriv_platform}
+                        alt={'Deriv platform'}
+                        height={image_height}
+                        width={image_width}
+                    />
                 </ImageWrapper>
-            </Flex>
+            </WrapContainer>
         </Container>
     )
 }

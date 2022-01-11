0af602e2e7d61df36263ba803e9c53c0b12eef07
Farrah Mae Ochoa <farrah@deriv.com>
2022-01-11 10:20:03 +0400

    fix rendering of language switcher

--- src/components/hooks/use-client-information.js
+++ src/components/hooks/use-client-information.js
@@ -1,26 +1,28 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { getClientInformation, getDomain } from 'common/utility'
 
 export const useClientInformation = () => {
     const [client_information, setClientInformation] = useState(false)
-    const [current_client_information, setCurrentClientInformation] = useState(false)
+    const callback_ref = useRef()
+
+    const setCurrentClientInformation = () => {
+        const current_client_information = getClientInformation(getDomain())
+        if (JSON.stringify(current_client_information) != JSON.stringify(client_information)) {
+            setClientInformation(current_client_information)
+        }
+    }
+
+    useEffect(() => {
+        callback_ref.current = setCurrentClientInformation
+    })
 
     useEffect(() => {
         const cookie_interval = setInterval(() => {
-            setCurrentClientInformation(getClientInformation(getDomain()))
+            callback_ref.current()
         }, 1000)
+
         return () => clearInterval(cookie_interval)
     }, [])
-
-    useEffect(() => {
-        if (current_client_information) {
-            if (JSON.stringify(current_client_information) != JSON.stringify(client_information)) {
-                setClientInformation(current_client_information)
-            }
-        } else {
-            setClientInformation(false)
-        }
-    }, [current_client_information])
 
     return client_information
 }

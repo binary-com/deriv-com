8ca60530b8c82813c489c6673289e672884f76f3
Kevin <kevin@regentmarkets.com>
2022-01-11 14:39:53 +0800

    eu changes

--- src/pages/home/_trade.js
+++ src/pages/home/_trade.js
@@ -6,6 +6,7 @@ import { TraderCard, BotCard, DMT5Card } from 'components/custom/other-platforms
 import { localize } from 'components/localization'
 import { SectionContainer, Container, Flex, CssGrid } from 'components/containers'
 import { Header, QueryImage } from 'components/elements'
+import { DerivStore } from 'store'
 
 const platforms = Object.freeze({
     trader: 'dtrader',
@@ -31,6 +32,7 @@ const ImageContainer = styled.div`
 `
 const StyledSection = styled(SectionContainer)`
     background-image: linear-gradient(to bottom, var(--color-grey-30), rgba(238, 238, 238, 0));
+    min-height: 69rem;
 `
 const query = graphql`
     query {
@@ -48,6 +50,7 @@ const query = graphql`
 
 const Trade = ({ is_ppc_redirect }) => {
     const data = useStaticQuery(query)
+    const { is_eu_country } = React.useContext(DerivStore)
     // one option always has to be selected
     const [selected, setSelected] = React.useState(null)
     return (
@@ -96,12 +99,14 @@ const Trade = ({ is_ppc_redirect }) => {
                             >
                                 <TraderCard />
                             </div>
-                            <div
-                                onMouseEnter={() => setSelected(platforms.bot)}
-                                onMouseLeave={() => setSelected('')}
-                            >
-                                <BotCard />
-                            </div>
+                            {!is_eu_country && (
+                                <div
+                                    onMouseEnter={() => setSelected(platforms.bot)}
+                                    onMouseLeave={() => setSelected('')}
+                                >
+                                    <BotCard />
+                                </div>
+                            )}
                             <div
                                 onMouseEnter={() => setSelected(platforms.mt5)}
                                 onMouseLeave={() => setSelected('')}

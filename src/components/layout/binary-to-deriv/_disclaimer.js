1a61d536f5d5c80c0352ce5282e65d00c963e393
NikitaK-deriv <nikita.kolesnik@regentmarkets.com>
2022-01-12 14:53:12 +0300

    turn back value

--- src/components/layout/binary-to-deriv/_disclaimer.js
+++ src/components/layout/binary-to-deriv/_disclaimer.js
@@ -51,7 +51,7 @@ const DisclaimerSection = () => {
                         <DisclaimerParagraph>
                             <Localize
                                 translate_text="CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. {{loss_percent}}% of retail investor accounts lose money when trading CFDs with this provider. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money."
-                                values={{ loss_percent: 62 }}
+                                values={{ loss_percent: 66 }}
                             />
                         </DisclaimerParagraph>
                         <DisclaimerParagraph>
@@ -78,7 +78,7 @@ const DisclaimerSection = () => {
                         <DisclaimerParagraph>
                             <Localize
                                 translate_text="CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. {{loss_percent}}% of retail investor accounts lose money when trading CFDs with this provider. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money."
-                                values={{ loss_percent: 62 }}
+                                values={{ loss_percent: 66 }}
                             />
                         </DisclaimerParagraph>
                         <DisclaimerParagraph>

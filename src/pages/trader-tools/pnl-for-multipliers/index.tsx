703b515c03a883c5eb2e79160b129bd145e1e1f7
mohammad-rework <mohammad.hashemi@re-work.dev>
2022-01-12 20:23:38 +0330

    fix: code smells in pnl-multipliers-calculator

--- src/pages/trader-tools/pnl-for-multipliers/index.tsx
+++ src/pages/trader-tools/pnl-for-multipliers/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Hero, StyledHeader } from '../common/_style'
-import PnLMultipliersCalculatorSection from './_pnl-for-multipliers'
+import PnLMultipliersCalculatorSection from './_pnl_multipliers_calculator'
 import { Container, SEO } from 'components/containers'
 import { localize, WithIntl } from 'components/localization'
 import Layout from 'components/layout/layout'

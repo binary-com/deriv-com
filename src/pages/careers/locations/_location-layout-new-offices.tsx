6c2f14f272eef5a8827d0f8b957b7185a575e309
pavel-latyshou-deriv <90393787+pavel-latyshou-deriv@users.noreply.github.com>
2022-01-14 11:45:29 +0300

    Pavel-Latyshov/fixes to locations (#2506)
    * fixes to locations
    
    * fix border radius
    
    * add border radius to hero
    
    Co-authored-by: “pavel-latyshou-deriv” <“pavel.latyshou@regentmarkets.com”>

--- src/pages/careers/locations/_location-layout-new-offices.tsx
+++ src/pages/careers/locations/_location-layout-new-offices.tsx
@@ -36,17 +36,7 @@ const SecondStyledHeader = styled(Header)`
 
     margin-bottom: 0 24px;
 `
-const StyledLinkButton = styled(LinkButton)`
-    font-size: 20px;
-    line-height: 30px;
-    @media ${device.tablet} {
-        font-size: 18px;
-        max-width: 317px;
-    }
-    @media (max-width: 340px) {
-        font-size: 15px;
-    }
-`
+
 type HeroProps = {
     display_name: string
     img_data: string
@@ -57,16 +47,16 @@ const Hero = ({ display_name, img_data }: HeroProps) => {
         <StyledBackground data={img_data} alt={display_name}>
             <StyledContainer>
                 <StyledHeader as="h1">{display_name}</StyledHeader>
-                <StyledLinkButton
+                <LinkButton
+                    hero
                     has_no_end_slash
-                    secondary="true"
                     to={zoho_url}
                     target="_blank"
                     rel="noopener noreferrer"
                     external
                 >
                     View open positions in {display_name}
-                </StyledLinkButton>
+                </LinkButton>
             </StyledContainer>
         </StyledBackground>
     )
@@ -240,11 +230,12 @@ export const NewLocationLayout = ({ location, images }: LocationLayoutProps) => 
                     align="center"
                     as="h2"
                     size="var(--text-size-header-5)"
+                    color="black-6"
                 >
                     Deriv in {display_name}
                 </SecondStyledHeader>
                 <Flex tablet_direction="column">
-                    <Text>{location.first_p}</Text>
+                    <Text color="black-6">{location.first_p}</Text>
                     <StyledImageWrapper>
                         <QueryImage
                             data={images[location.first_img]}
@@ -266,10 +257,10 @@ export const NewLocationLayout = ({ location, images }: LocationLayoutProps) => 
                         />
                         <WorkingInformation p="3.2rem 6rem" direction="column">
                             <StyledDiv>
-                                <Header as="h3" type="subtitle-1">
+                                <Header as="h3" type="subtitle-1" color="black-6">
                                     {`Working at Deriv ${map_office_name}`}
                                 </Header>
-                                <CardText>{location.map_text}</CardText>
+                                <CardText color="black-6">{location.map_text}</CardText>
                             </StyledDiv>
                         </WorkingInformation>
                     </WorkingFlex>

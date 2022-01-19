c65f1b35e02d45ff5e5bf136f6ad1f958c206b9a
Sean Ho <seanho96@yahoo.com>
2022-01-19 14:01:58 +0800

    fix: dmt5 copy change

--- src/pages/dmt5/_margin_calculator.tsx
+++ src/pages/dmt5/_margin_calculator.tsx
@@ -276,7 +276,7 @@ const MarginCalculator = () => {
                                 />
                             ) : (
                                 <Localize
-                                    translate_text="Explore <0>CFDs</0> on Deriv MT5, and enjoy high leverage and low spreads to increase your returns when the market moves in your favour."
+                                    translate_text="Explore <0>CFDs</0> on Deriv MT5 (DMT5) and enjoy high leverage and low spreads to increase your returns when the market moves in your favour."
                                     components={[
                                         <LinkText
                                             color="red"

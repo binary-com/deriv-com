e6bbb7e3bd2d4661e72f945d1b5b7be4160d5db1
Prince <prince@deriv.com>
2022-01-20 12:25:00 +0400

    Initial Commit

---
+++ src/components/hooks/use-academy-data.tsx
@@ -0,0 +1,64 @@
+import { useStaticQuery, graphql } from 'gatsby'
+
+// This hook is used to fetch data from Directus during build time
+export const useAcademyData = (): [unknown] => {
+    let academy_data: any = useStaticQuery(query)
+    academy_data = academy_data?.directus
+
+    return [academy_data]
+}
+
+const query = graphql`
+    query StoreQuery {
+        directus {
+            blog(filter: { status: { _eq: "published" } }, sort: "-published_date") {
+                id
+                main_image {
+                    id
+                    description
+                    imageFile {
+                        childImageSharp {
+                            gatsbyImageData
+                        }
+                    }
+                }
+                slug
+                published_date
+                featured
+                tags {
+                    id
+                    tags_id {
+                        tag_name
+                    }
+                }
+                blog_title
+                blog_description
+            }
+            videos(filter: { status: { _eq: "published" } }, sort: "-published_date") {
+                video_title
+                published_date
+                video_description
+                video_duration
+                featured
+                video_thumbnail {
+                    id
+                    title
+                    imageFile {
+                        id
+                        childImageSharp {
+                            gatsbyImageData
+                        }
+                    }
+                }
+                video_file {
+                    id
+                }
+                tags {
+                    tags_id {
+                        tag_name
+                    }
+                }
+            }
+        }
+    }
+`

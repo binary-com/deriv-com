e05fbef0f3b9061fd95735fc0d8fca498d0bd201
Mohammad Hashemi <mohammad.hashemi@re-work.dev>
2022-01-22 15:37:55 +0330

    fix: revert header image height to it's default

--- src/pages/landing/ebooks/components/_header-section.tsx
+++ src/pages/landing/ebooks/components/_header-section.tsx
@@ -85,7 +85,7 @@ const HeaderImage = styled(QueryImage)<HeaderImageProps>`
 
     @media ${device.tabletS} {
         width: ${(props) => (props.imgWidth < 600 ? '115%' : '105%')};
-        height: ${(props) => (props.imgWidth < 600 ? '115%' : '105%')};
+        height: ${(props) => props.imgWidth < 600 && '105%'};
         margin-left: ${(props) => (props.imgWidth < 600 ? '-110px' : '-50px')};
     }
     @media ${device.mobileS} {

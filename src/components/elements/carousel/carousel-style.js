3836cf7d213cbd0d45736554b42bb0d5b9e4141c
yashim-deriv <yashim@deriv.com>
2022-01-26 11:05:24 +0800

    fix: code smells

--- src/components/elements/carousel/carousel-style.js
+++ src/components/elements/carousel/carousel-style.js
@@ -127,14 +127,14 @@ export const StyledChevron = styled(Chevron)`
 `
 
 export const NavigationContainer = styled(Flex)`
-    ${(props) => {
-        if (props.navigation_css) return props.navigation_css
+    ${({ navigation_css, bottom_offset }) => {
+        if (navigation_css) return navigation_css
         else
             return css`
                 position: relative;
                 width: 100%;
                 height: 8px;
-                bottom: ${(props) => props.bottom_offset ?? '40px'};
+                bottom: ${bottom_offset ?? '40px'};
             `
     }}
 `

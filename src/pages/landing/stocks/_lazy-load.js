589d9d909e0b40a9887fb35fd9933f447ef7fe45
Niloofar Sadeghi <93518187+niloo-fs@users.noreply.github.com>
2022-01-26 11:49:02 +0330

    Niloofar Sadeghi / convert stocks to ts (#2521)
    * convert stockes to ts
    
    * fix stocks_banner type

--- src/pages/landing/stocks/_lazy-load.js
+++
@@ -1,7 +0,0 @@
-import Loadable from '@loadable/component'
-
-export const HeaderSection = Loadable(() => import('./_headerSection.js'))
-export const DTrading = Loadable(() => import('./_dtrading.js'))
-export const Parallelogram = Loadable(() => import('./_parallelogram.js'))
-export const WhyTradeWithUs = Loadable(() => import('./_whyTradeWIthUs.js'))
-export const FooterBanner = Loadable(() => import('./_footerBanner.js'))

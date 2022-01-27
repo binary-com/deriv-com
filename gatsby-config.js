a07d09aba2cb497d2ded88ed915bfa7f04704a8c
habib-binary <shuvohabib@gmail.com>
2022-01-27 12:54:20 +0600

    add new urls

--- gatsby-config.js
+++ gatsby-config.js
@@ -166,7 +166,7 @@ module.exports = {
         {
             resolve: `gatsby-plugin-offline`,
             options: {
-              precachePages: [`s/`, `/markets/*`],
+              precachePages: [`/`, `/markets/*`],
             },
         },
         {

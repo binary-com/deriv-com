31841603db3db511691c412dfaf0289858ac3d75
mitra-fs <mitra@firstsource.tech>
2022-01-31 09:22:24 +0330

    Add deriv limited address

--- src/components/layout/binary-to-deriv/_disclaimer.js
+++ src/components/layout/binary-to-deriv/_disclaimer.js
@@ -27,6 +27,11 @@ const DisclaimerSection = () => {
                 </DisclaimerParagraph>
                 <DisclaimerParagraph>
                     {localize(
+                        'Deriv Limited - 13 Castle Street, St. Helier, JE2 3BT, Jersey - is the holding company for the above subsidiaries.',
+                    )}
+                </DisclaimerParagraph>
+                <DisclaimerParagraph>
+                    {localize(
                         'This website’s services are not available in certain countries, including the USA, Canada, and Hong Kong, or to persons below 18.',
                     )}
                 </DisclaimerParagraph>

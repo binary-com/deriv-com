d2a97b570b952b29c708f9ed45d45abebd48fb93
Mohammad Hashemi <mohammad.hashemi@re-work.dev>
2022-02-15 20:10:04 +0330

    feat: bring all eu uk changes to homepage

--- src/pages/home/platform-carousel/_desktop-platform-carousel.tsx
+++ src/pages/home/platform-carousel/_desktop-platform-carousel.tsx
@@ -14,7 +14,7 @@ const StyledDesktopCarousel = styled(Flex)`
 `
 const DesktopPlatformCarousel = () => {
     const [slide_index, setSlideIndex] = useState(getSlideStartingIndex())
-    const platform_details = getPlatformDetails(no_slide_sets)
+    const platform_details = getPlatformDetails(no_slide_sets())
 
     return (
         <StyledDesktopCarousel ai="start" jc="center">

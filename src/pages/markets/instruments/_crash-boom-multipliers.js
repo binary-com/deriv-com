bc5577ce457389b038363d8abf31c0ceac59d2a2
Mohammad Hashemi <mohammad.hashemi@re-work.dev>
2022-02-22 07:38:57 +0330

    fix: crash boom in eu trader tools shows 300 now

--- src/pages/markets/instruments/_crash-boom-multipliers.js
+++ src/pages/markets/instruments/_crash-boom-multipliers.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import Symbol from '../components/helper/_symbol'
-import { crash_boom_multipliers } from './_market-symbols'
+import { crash_boom_multipliers, crash_boom_eu } from './_market-symbols'
+import { DerivStore } from 'store'
 
 const CrashBoomMultipliers = () => {
+    const { is_eu_country } = React.useContext(DerivStore)
+
     return (
         <>
             <>
-                {crash_boom_multipliers.map((symbol, index) => (
-                    <Symbol key={index} src={symbol.src} text={symbol.text} />
-                ))}
+                {!is_eu_country
+                    ? crash_boom_multipliers.map((symbol, index) => (
+                          <Symbol key={index} src={symbol.src} text={symbol.text} />
+                      ))
+                    : crash_boom_eu.map((symbol, index) => (
+                          <Symbol key={index} src={symbol.src} text={symbol.text} />
+                      ))}
             </>
         </>
     )

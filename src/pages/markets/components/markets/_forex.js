4c26abb7799fb57122f5ee2de9897eae04f4baf5
Kevin <kevin@regentmarkets.com>
2022-02-24 16:40:57 +0800

    fix ocmments

--- src/pages/markets/components/markets/_forex.js
+++ src/pages/markets/components/markets/_forex.js
@@ -19,7 +19,7 @@ const SimpleSteps = Loadable(() => import('components/custom/_simple-steps'))
 const OtherMarkets = Loadable(() => import('../sections/_other-markets.js'))
 
 const Forex = ({ simple_step_content }) => {
-    const { is_row, is_eu, is_eu_uk } = getCountryRule()
+    const { is_row, is_eu_uk } = getCountryRule()
     return (
         <>
             <WhyTrade
@@ -28,7 +28,7 @@ const Forex = ({ simple_step_content }) => {
                     <Localize translate_text="Benefit from round-the-clock trading hours (Monday to Friday), high liquidity, low barriers to entry, a wide range of offerings, and opportunities to trade on world events." />
                 }
             >
-                {(is_eu ? forex_content_eu : forex_content).map((content, index) => (
+                {(is_eu_uk ? forex_content_eu : forex_content).map((content, index) => (
                     <div key={index} text={content.text} icon={<img src={content.src} alt="" />} />
                 ))}
             </WhyTrade>

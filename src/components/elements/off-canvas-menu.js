8d2ae4f6bebd3f87c89c1562ea98755c63c7bbd5
Suthesan Kanagasabai <45410833+suthesan-deriv@users.noreply.github.com>
2022-03-02 17:18:27 +0800

    suthesh/master-hotfixes (#2699)
    * translation, markets filtering
    
    * replace is_uk with is_non_uk
    
    Co-authored-by: Suthesan <suthesan@Suthesans-MacBook-Pro.local>

--- src/components/elements/off-canvas-menu.js
+++ src/components/elements/off-canvas-menu.js
@@ -291,7 +291,7 @@ export const OffCanvasMenuWrapper = (props) => {
                                             <img src={BinaryBot} alt="" width="32" height="32" />
                                         )}
                                         content={
-                                            <Localize translate_text="Our classic &ldquo;drag-and-drop&rdquo; tool for creating trading bots, featuring pop-up trading charts, for advanced users." />
+                                            <Localize translate_text='Our classic "drag-and-drop" tool for creating trading bots, featuring pop-up trading charts, for advanced users.' />
                                         }
                                         title={<Localize translate_text="BinaryBot" />}
                                         onClick={handleArrowClick}

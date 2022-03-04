86305a1fd5f65f4c55dd3fd9fc9869063a13bafd
matin-binary <matin@binary.com>
2022-03-04 10:25:20 +0800

    Chore: px to rem for desktop

--- src/components/custom/_signup-new.js
+++ src/components/custom/_signup-new.js
@@ -297,7 +297,7 @@ const SignupNew = ({
                         <StyledLocalizedLink
                             key={0}
                             to="/tnc/security-and-privacy.pdf"
-                            size="12px"
+                            size="12rem"
                             color="red"
                             external="true"
                             rel="noopener noreferrer"

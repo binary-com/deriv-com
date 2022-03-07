6cd163ce350b12ce08aa1c1aa6a12b0bef6d6041
Kevin <kevin@regentmarkets.com>
2022-03-07 11:03:43 +0800

    add styling

--- src/pages/careers/besquare/components/sections/_hero.tsx
+++ src/pages/careers/besquare/components/sections/_hero.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
 import { HeroContainer, HeaderContainer, ImageWrapper } from '../../static/style/_hero'
 import { TextWrapper, Title } from '../../static/style/_common'
 import HeroImage from 'images/common/be-square/hero.png'
+import device from 'themes/device'
+
+export const StyledHeaderContainer = styled(HeaderContainer)`
+    padding: 120px 0 80px 120;
+
+    @media ${device.laptopL} {
+        padding: 40px 0 0;
+    }
+
+    @media ${device.laptop} {
+        padding: 40px 0 0;
+    }
+`
+
+const StyledImageWrapper = styled(ImageWrapper)`
+    right: 120px;
+    @media ${device.laptopL} {
+        right: 30px;
+    }
+
+    @media ${device.laptop} {
+        right: 0;
+    }
+`
 
 const Hero = () => (
     <HeroContainer>
-        <HeaderContainer>
+        <StyledHeaderContainer>
             <Title
                 as="h1"
                 color={'white'}
@@ -24,8 +49,8 @@ const Hero = () => (
             >
                 Launch your IT career with our graduate programme
             </TextWrapper>
-        </HeaderContainer>
-        <ImageWrapper src={HeroImage} alt="BeSquare students standing with their laptops" />
+        </StyledHeaderContainer>
+        <StyledImageWrapper src={HeroImage} alt="BeSquare students standing with their laptops" />
     </HeroContainer>
 )
 

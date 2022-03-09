54301695da6856a8b2ad0feb5665cc933d00c54a
Nikolai Berestevich <nikolai.berestevich@deriv.com>
2022-03-09 23:18:42 +0300

    nikolai.berestevich/fix/svg loading

--- src/pages/markets/components/helper/_available-trades.tsx
+++ src/pages/markets/components/helper/_available-trades.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react'
 import AvailableTradesMobile from './_available-trades_mobile'
 import AvailableTradesDesktop from './_available-trades_desktop'
 import { getWindowWidth } from 'common/utility'
+import { usePageLoaded } from 'components/hooks/use-page-loaded'
 
 type AvailableTradesProps = {
     CFDs: ReactElement
@@ -16,23 +17,30 @@ const AvailableTrades = ({
     Multipliers,
     display_title,
 }: AvailableTradesProps) => {
-    const is_mobile = getWindowWidth() < 426
+    const [is_mounted] = usePageLoaded(false)
+    const is_mobile = 426 < getWindowWidth()
     return (
         <>
-            {is_mobile ? (
-                <AvailableTradesMobile
-                    CFDs={CFDs}
-                    DigitalOptions={DigitalOptions}
-                    Multipliers={Multipliers}
-                    display_title={display_title}
-                />
+            {is_mounted ? (
+                <>
+                    {!is_mobile ? (
+                        <AvailableTradesMobile
+                            CFDs={CFDs}
+                            DigitalOptions={DigitalOptions}
+                            Multipliers={Multipliers}
+                            display_title={display_title}
+                        />
+                    ) : (
+                        <AvailableTradesDesktop
+                            CFDs={CFDs}
+                            DigitalOptions={DigitalOptions}
+                            Multipliers={Multipliers}
+                            display_title={display_title}
+                        />
+                    )}
+                </>
             ) : (
-                <AvailableTradesDesktop
-                    CFDs={CFDs}
-                    DigitalOptions={DigitalOptions}
-                    Multipliers={Multipliers}
-                    display_title={display_title}
-                />
+                <></>
             )}
         </>
     )

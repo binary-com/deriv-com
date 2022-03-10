7a0b7b50a3b25aee367796999f227afa6d96ba6f
fiona-deriv <fiona@deriv.com>
2022-03-10 13:10:56 +0200

    revert change

--- src/pages/careers/locations/_location-layout-new-offices.tsx
+++ src/pages/careers/locations/_location-layout-new-offices.tsx
@@ -252,7 +252,7 @@ export const NewLocationLayout = ({ location, images }: LocationLayoutProps) => 
                     {location.has_map ? (
                         <WorkingFlex jc="cover">
                             <Iframe
-                                src={`https://www.google.com/maps/embed/v1/place?q=${location.map}&key=${map_api_key}`}
+                                src={`https://www.google.com/maps/embed/v1/place?q=place_id:${location.map}&key=${map_api_key}`}
                             />
 
                             <WorkingInformation p="3.2rem 6rem" direction="column">
@@ -264,7 +264,7 @@ export const NewLocationLayout = ({ location, images }: LocationLayoutProps) => 
                                     <Flex jc="unset" mt="37px">
                                         <Pin src={MapPin} alt="map pin" />
                                         <LinkText
-                                            rel="noopener noreferrer"
+                                            rel="noopenernoreferrer"
                                             target="_blank"
                                             href={location.google_map_link}
                                         >

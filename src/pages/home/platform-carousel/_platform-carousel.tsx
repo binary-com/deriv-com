d0af7f796b39df30384f0019b307c7ad971ad947
Mohammad Hashemi <mohammad.hashemi@re-work.dev>
2022-03-16 15:12:16 +0330

    chore: remove mobile wrapper from platform-carousel

--- src/pages/home/platform-carousel/_platform-carousel.tsx
+++ src/pages/home/platform-carousel/_platform-carousel.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import DesktopPlatformCarousel from './_desktop-platform-carousel'
-import MobilePlatformCarousel from './_mobile-platform-carousel'
-import { DesktopWrapper, MobileWrapper } from 'components/containers/wrapper'
+// import MobilePlatformCarousel from './_mobile-platform-carousel'
+import { DesktopWrapper } from 'components/containers/wrapper'
 
 const PlatformCarousel = () => {
     return (
@@ -9,9 +9,6 @@ const PlatformCarousel = () => {
             <DesktopWrapper>
                 <DesktopPlatformCarousel />
             </DesktopWrapper>
-            <MobileWrapper>
-                <MobilePlatformCarousel />
-            </MobileWrapper>
         </>
     )
 }

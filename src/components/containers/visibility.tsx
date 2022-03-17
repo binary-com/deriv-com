cfa3f5dddf888b0314cf117c31b6b9abf1da09a8
Prince <prince@deriv.com>
2022-03-17 13:42:46 +0400

    Refactor: country based rule with hooks

--- src/components/containers/visibility.tsx
+++ src/components/containers/visibility.tsx
@@ -74,24 +74,42 @@ const deviceRenderer = (): boolean => {
 
 export const getCountryRule = () => {
     const [website_status] = useWebsiteStatus()
-    const user_ip_country = website_status?.clients_country || ''
-    const { is_eu_domain, is_uk_domain } = domainBasedCheck()
     const { residence } = getClientInformation(getDomain()) || {
         residence: '',
     }
-    const eu_countries_uk_excluded = eu_countries.filter((country: string) => country !== 'gb')
 
-    const is_eu_country = eu_countries_uk_excluded.includes(user_ip_country)
-    const is_uk_country = user_ip_country === 'gb'
-    const is_eu_residence = eu_countries_uk_excluded.includes(residence)
-    const is_uk_residence = residence === 'gb'
+    const [is_eu, setEu] = useState(false)
+    const [is_uk, setUk] = useState(false)
+    const [is_non_uk, setNonUk] = useState(false)
+    const [is_non_eu, setNonEu] = useState(false)
+    const [is_uk_eu, setUkEu] = useState(false)
+    const [is_row, setRow] = useState(false)
+    const { is_eu_domain, is_uk_domain } = domainBasedCheck()
 
-    const is_eu = is_eu_residence || (!residence && is_eu_country) || is_eu_domain
-    const is_uk = is_uk_residence || (!residence && is_uk_country) || is_uk_domain
-    const is_non_uk = !is_uk
-    const is_non_eu = !is_eu
-    const is_uk_eu = !(!is_eu && !is_uk)
-    const is_row = !is_uk_eu
+    useLayoutEffect(() => {
+        const user_ip_country = website_status?.clients_country || ''
+
+        const eu_countries_uk_excluded = eu_countries.filter((country: string) => country !== 'gb')
+
+        const is_eu_country = eu_countries_uk_excluded.includes(user_ip_country)
+        const is_uk_country = user_ip_country === 'gb'
+        const is_eu_residence = eu_countries_uk_excluded.includes(residence)
+        const is_uk_residence = residence === 'gb'
+
+        const eu = is_eu_residence || (!residence && is_eu_country) || is_eu_domain
+        const uk = is_uk_residence || (!residence && is_uk_country) || is_uk_domain
+        const non_uk = !uk
+        const non_eu = !eu
+        const uk_eu = !(!eu && !uk)
+        const row = !uk_eu
+
+        setEu(eu)
+        setUk(uk)
+        setNonUk(non_uk)
+        setNonEu(non_eu)
+        setUkEu(uk_eu)
+        setRow(row)
+    }, [website_status, residence])
 
     return { is_eu, is_uk, is_non_uk, is_non_eu, is_uk_eu, is_row }
 }
@@ -142,7 +160,7 @@ const CountryBasedContent = ({ country_rule, children }: CountryBasedContentProp
 
     const condition = rules[country_rule]
 
-    return is_loaded && condition ? <>{children}</> : null
+    return is_loaded && condition ? <>{children}</> : <></>
 }
 
 export const EU = ({ children }: ResponsiveContainerProps) => (

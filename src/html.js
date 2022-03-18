ceaba6d67b8a6c2fd39dfe6900b8f5b7f46a50b1
Sean Ho <seanho96@yahoo.com>
2022-03-18 13:07:15 +0800

    feat: don't load gtm through partytown when flag is present

--- src/html.js
+++ src/html.js
@@ -12,29 +12,24 @@ export default function HTML(props) {
                     name="viewport"
                     content="width=device-width, initial-scale=1, shrink-to-fit=no"
                 />
-
-                <Partytown
-                    forward={['dataLayer.push', '__lc']}
-                    resolveUrl={(url) => {
-                        if (url.pathname.includes('debug/bootstrap')) {
-                            var proxyUrl = new URL(
-                                'https://deriv-com-git-fork-sean-binary-offload-third-party-scripts.binary.sx/party-proxy',
-                            )
-                            proxyUrl.searchParams.append('url', url)
-                            return proxyUrl
-                        }
-                        return url
-                    }}
-                />
+                <Partytown forward={['dataLayer.push']} />
                 <script
                     dangerouslySetInnerHTML={{
-                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
-        f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.type="text/partytown";j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NF7884S');</script>`,
+                        __html: `
+                            if(typeof window !== 'undefined' && window.location.href.includes('no_partytown=1')) {
+                                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
+                                f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.type="text/javascript";j.src=
+                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                                })(window,document,'script','dataLayer','GTM-NF7884S');
+                            } else {
+                                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
+                                f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.type="text/partytown";j.src=
+                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                                })(window,document,'script','dataLayer','GTM-NF7884S');
+                            }
+                        `,
                     }}
                 />
-
                 {props.headComponents}
             </head>
             <noscript

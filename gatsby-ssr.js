2c3d6af3cedda36cb0556cd290797b55110c19fe
Sean Ho <seanho96@yahoo.com>
2022-03-24 12:51:13 +0800

    refactor: replace html injection with gatsby ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Partytown } from '@builder.io/partytown/react'
 import { WrapPagesWithLocaleContext } from './src/components/localization'
 import './src/components/localization/config'
 import { MediaContextProvider } from './src/themes/media'
 import { DerivProvider } from './src/store'
+
+export const onRenderBody = ({ setHeadComponents }) => {
+    setHeadComponents([
+        <Partytown key="partytown" forward={['dataLayer.push']} />,
+        <script
+            key="gtm"
+            dangerouslySetInnerHTML={{
+                __html: `
+                            if(typeof window !== 'undefined' && window.location.href.includes('no_partytown=1')) {
+                                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
+                                f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.type="text/javascript";j.src=
+                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                                })(window,document,'script','dataLayer','GTM-NF7884S');
+                            } else {
+                                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
+                                f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.type="text/partytown";j.src=
+                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                                })(window,document,'script','dataLayer','GTM-NF7884S');
+                            }
+                        `,
+            }}
+        />,
+    ])
+}
 
 export const wrapRootElement = ({ element }) => {
     return (
--- src/html.js
+++ src/html.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Partytown } from '@builder.io/partytown/react'
 
 export default function HTML(props) {
     return (
@@ -11,24 +10,6 @@ export default function HTML(props) {
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1, shrink-to-fit=no"
-                />
-                <Partytown forward={['dataLayer.push']} />
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            if(typeof window !== 'undefined' && window.location.href.includes('no_partytown=1')) {
-                                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
-                                f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.type="text/javascript";j.src=
-                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                                })(window,document,'script','dataLayer','GTM-NF7884S');
-                            } else {
-                                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
-                                f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.type="text/partytown";j.src=
-                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                                })(window,document,'script','dataLayer','GTM-NF7884S');
-                            }
-                        `,
-                    }}
                 />
                 {props.headComponents}
             </head>

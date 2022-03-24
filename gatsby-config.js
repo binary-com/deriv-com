41d1f4eafd0c03ad8248c7d2f1475dec59bc72c1
Sean Ho <72253841+sean-binary@users.noreply.github.com>
2022-03-24 13:22:05 +0800

    Offload third party scripts partytown (#2801)
    * test: partytown
    
    * chore: replace partytown scripts with latest version
    
    * Redeploy
    
    * Redeploy
    
    * Redeploy
    
    * chore: remove unused code
    
    * script: revert build script
    
    * chore: update partytown to latest version
    
    * chore: update packages patch version
    
    * chore: update packages minor version
    
    * chore: update commander package major version
    
    * chore: update stylelint config standard major version
    
    * chore: update packages patch version
    
    * chore: fix stylelint errors
    
    * chore: remove prettier ignore
    
    * test: reverse proxy
    
    * update resolve url
    
    * update resolve url
    
    * update vercel redirect destination
    
    * update vercel rewrites
    
    * test: reverse proxy
    
    * test: new rewrite
    
    * test: new rewrite
    
    * test: new rewrite
    
    * test: new resolveUrl
    
    * rename proxy path
    
    * change source pattern
    
    * change rewrite to redirect
    
    * change order or vercel config
    
    * update source pattern
    
    * replace redirects with rewrites
    
    * replace rewrites with redirects
    
    * test: load livechat through partytown
    
    * revert: uselivechat config
    
    * feat: don't load gtm through partytown when flag is present
    
    * chore: load livechat normally
    
    * chore: install latest partytown
    
    * chore: use package versions of master branch
    
    * chore: remove gatsby google tag manager
    
    * refactor: replace html injection with gatsby ssr
    
    * refactor: replace html injection with gatsby ssr
    
    Co-authored-by: Sean Ho <seanho96@yahoo.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -202,13 +202,6 @@ module.exports = {
         },
         'gatsby-plugin-anchor-links',
         {
-            resolve: 'gatsby-plugin-google-tagmanager',
-            options: {
-                id: 'GTM-NF7884S',
-                includeInDevelopment: false,
-            },
-        },
-        {
             resolve: 'gatsby-plugin-anchor-links',
             options: {
                 offset: -100,
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Partytown } from '@builder.io/partytown/react'
 import { WrapPagesWithLocaleContext } from './src/components/localization'
 import './src/components/localization/config'
 import { MediaContextProvider } from './src/themes/media'
 import { DerivProvider } from './src/store'
+
+export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
+    setHeadComponents([
+        <Partytown key="partytown" forward={['dataLayer.push']} />,
+        <script
+            key="gtm"
+            dangerouslySetInnerHTML={{
+                __html: `
+                            if(typeof window !== 'undefined' && window.location.href.includes('no_partytown=1')) {
+                                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
+                                f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.type="text/javascript";j.src=
+                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                                })(window,document,'script','dataLayer','GTM-NF7884S');
+                            } else {
+                                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
+                                f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.type="text/partytown";j.src=
+                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                                })(window,document,'script','dataLayer','GTM-NF7884S');
+                            }
+                        `,
+            }}
+        />,
+    ])
+    setPreBodyComponents([
+        <noscript
+            key="noscript-gtm"
+            dangerouslySetInnerHTML={{
+                __html: `
+                            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NF7884S" height="0" width="0"
+                                style="display:none;visibility:hidden"></iframe>
+                        `,
+            }}
+        />,
+    ])
+}
 
 export const wrapRootElement = ({ element }) => {
     return (

6e30533c214fbdf4fe0aedc524c6e9062fb20d5f
Sean Ho <72253841+sean-binary@users.noreply.github.com>
2022-03-24 11:28:06 +0800

    Revert "Revert "mehrzad / add dbot to multiplier (#2722)" (#2743)" (#2744)
    This reverts commit b48196ab27db435f2bb0272cb83bc13af44f1a9a.

--- src/pages/trade-types/multiplier/_synthetic-indices.tsx
+++ src/pages/trade-types/multiplier/_synthetic-indices.tsx
@@ -24,6 +24,7 @@ import {
 import { Text } from 'components/elements'
 import { SectionContainer, Flex, CssGrid, Show } from 'components/containers'
 import { localize, Localize } from 'components/localization'
+import { getCountryRule } from 'components/containers/visibility'
 import device from 'themes/device'
 import { DerivStore } from 'store'
 
@@ -157,6 +158,7 @@ const ContinuousIndicesDetails = () => (
 
 const SyntheticIndices = () => {
     const { is_eu_country } = React.useContext(DerivStore)
+    const { is_row } = getCountryRule()
 
     return (
         <SectionContainer padding="4rem 0 8rem">
@@ -171,7 +173,7 @@ const SyntheticIndices = () => {
                           )}
                 </StyledText>
                 <AvailablePlatformsWrapper mb="40px">
-                    <AvailablePlatforms dtrader />
+                    <AvailablePlatforms m_top="16px" dbot={is_row} dtrader />
                 </AvailablePlatformsWrapper>
                 <Text weight="bold">
                     {localize('Synthetic indices available for multipliers trading')}

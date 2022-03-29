7d96f4f45ecf5b682c7523504c4e17f3d559265d
Niloofar Sadeghi <93518187+niloo-fs@users.noreply.github.com>
2022-03-29 07:10:16 +0430

    Niloofar Sadeghi / Refactor navigation component  (#2655)
    * nav global styles
    
    * nav global methods
    
    * nav-desktop component
    
    * nav-academy component
    
    * nav-mobile and nav component
    
    * nav-sticky component
    
    * nav-static component
    
    * nav-besquare component
    
    * nav-besquare component
    
    * nav-interim component
    
    * nav-jumpIndice component
    
    * type improvement
    
    * nav-careers component
    
    * nav-partner component
    
    * refactor nav-desktop component
    
    * CFDWarnings component
    
    * CFDWarnings component
    
    * refactor nav component
    
    * refactor nav-sticky component
    
    * nav-academy component
    
    * refactor partner-nav
    
    * refactor static-nav
    
    * refactor nav-jump-indices
    
    * refactor nav-career
    
    * refactor nav-interim
    
    * nav-interim height
    
    * remove cfd-component
    
    * refactor nav-security
    
    * .
    
    * final check
    
    * resolve review comments
    
    * move use-auth hook to hook folder
    
    * update home nav
    
    * more improvement
    
    * remove redundant codes
    
    * code improvement
    
    * .
    
    * resolve conflict
    
    Co-authored-by: Sean Ho <seanho96@yahoo.com>

---
+++ src/components/layout/nav/nav-security.tsx
@@ -0,0 +1,65 @@
+// Note: When using layout component for security page, please add type='security' and padding_top='10rem'
+import React from 'react'
+import styled from 'styled-components'
+import MainNav from './components/partner/main-nav'
+import { StyledLogoLink } from './components/partner/nav-partner-desktop'
+import { PartnerWrapper, PartnerNavigationBarWrapper } from './styles/nav-styles'
+import { localize } from 'components/localization'
+import { LinkButton } from 'components/form'
+import { Container } from 'components/containers'
+import device from 'themes/device'
+import LogoBugBounty from 'images/svg/layout/logo-bug-bounty.svg'
+import { CFDWarning } from 'components/layout'
+
+const Wrapper = styled(Container)`
+    height: 7.2rem;
+    padding: 1.2rem 0;
+`
+const SubmissionButton = styled(LinkButton)`
+    color: var(--color-white);
+    font-size: 14px;
+    width: 16rem;
+
+    @media ${device.tabletL} {
+        padding: 8px 16px 7px;
+        font-size: 12px;
+        width: auto;
+    }
+`
+const Logo = styled.img`
+    max-width: 30rem;
+    @media ${device.tabletL} {
+        width: 170px;
+        margin-right: 10px;
+    }
+`
+
+const NavSecurity = () => {
+    return (
+        <>
+            <PartnerWrapper>
+                <MainNav is_security />
+                <PartnerNavigationBarWrapper>
+                    <Wrapper jc="space-between">
+                        <StyledLogoLink to="/" aria-label="Bug bounty">
+                            <Logo src={LogoBugBounty} alt="logo bug bounty" />
+                        </StyledLogoLink>
+
+                        <SubmissionButton
+                            to="mailto:security@deriv.com"
+                            is_mail_link
+                            external="true"
+                            target="_blank"
+                            tertiary
+                        >
+                            {localize('Submit a report')}
+                        </SubmissionButton>
+                    </Wrapper>
+                </PartnerNavigationBarWrapper>
+            </PartnerWrapper>
+            <CFDWarning />
+        </>
+    )
+}
+
+export default NavSecurity

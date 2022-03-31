449b4003ac7f4c01368074b2e4082cbcac56ce33
NikitaK-deriv <nikita.kolesnik@regentmarkets.com>
2022-03-31 15:41:54 +0300

    refactor: replace isMobile on component Mobile and Desktop

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { isMobile } from '../../common/os-detect'
 import { LocationContext } from './location-context'
 import { DefaultFooter, FooterGrid } from './footer/common/style.js'
 import LogoSection from './footer/logo'
 import MainLinksSection from './footer/main-links'
 import DisclaimerSection from './footer/disclaimer'
 import BottomSocialSection from './footer/bottom-social-wrapper'
-import { Container, NonEU } from 'components/containers'
+import { Container, NonEU, Mobile, Desktop } from 'components/containers'
 import { DerivStore } from 'store'
 
 // TODO: (discussion) make footer pure component, and move usage of footer to custom
@@ -25,13 +24,14 @@ const Footer = ({ type, is_ppc, is_ppc_redirect, academy, no_footer_links }) => 
                         <MainLinksSection is_ppc={is_ppc} is_ppc_redirect={is_ppc_redirect} />
                     )}
                     <DisclaimerSection is_academy={academy} />
-                    {isMobile ? (
+                    <Mobile>
                         <BottomSocialSection type={type} />
-                    ) : (
+                    </Mobile>
+                    <Desktop>
                         <NonEU>
                             <BottomSocialSection type={type} />
                         </NonEU>
-                    )}
+                    </Desktop>
                 </FooterGrid>
             </Container>
         </DefaultFooter>

d125c341ed54fbd72fa9d01a14066d0d1a07ee98
NikitaK-deriv <94372063+NikitaK-deriv@users.noreply.github.com>
2022-04-04 11:43:46 +0300

    NikitaK/ Difference in header title and footer content  (#2864)
    * style: change placement and behavior of sharing buttons
    
    * text: change several & | and
    
    * text: change text for cr accounts in footer

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { LocationContext } from './location-context'
-import { DefaultFooter, FooterGrid, EuLogoWrapper } from './footer/common/style.js'
+import { DefaultFooter, FooterGrid } from './footer/common/style.js'
 import LogoSection from './footer/logo'
 import MainLinksSection from './footer/main-links'
 import DisclaimerSection from './footer/disclaimer'
 import BottomSocialSection from './footer/bottom-social-wrapper'
-import { Container, NonEU, UKEU, NonUK, Mobile, Desktop } from 'components/containers'
+import { Container, Mobile } from 'components/containers'
 import { DerivStore } from 'store'
-
 // TODO: (discussion) make footer pure component, and move usage of footer to custom
 
 const Footer = ({ type, is_ppc, is_ppc_redirect, academy, no_footer_links }) => {
@@ -24,27 +23,11 @@ const Footer = ({ type, is_ppc, is_ppc_redirect, academy, no_footer_links }) => 
                         <MainLinksSection is_ppc={is_ppc} is_ppc_redirect={is_ppc_redirect} />
                     )}
                     <DisclaimerSection is_academy={academy} />
-                    <UKEU>
-                        <Mobile>
-                            <BottomSocialSection type={type} />
-                        </Mobile>
-                    </UKEU>
-                    <NonEU>
-                        <EuLogoWrapper>
-                            <Desktop>
-                                <BottomSocialSection type={type} />
-                            </Desktop>
-                        </EuLogoWrapper>
-                    </NonEU>
-                </FooterGrid>
-            </Container>
-            <NonEU>
-                <NonUK>
                     <Mobile>
                         <BottomSocialSection type={type} />
                     </Mobile>
-                </NonUK>
-            </NonEU>
+                </FooterGrid>
+            </Container>
         </DefaultFooter>
     )
 }

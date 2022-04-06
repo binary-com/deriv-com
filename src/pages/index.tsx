a9b92606d96bfcf0ae853526223ed7030ea7603f
ashrafali-v <ashrafalifrk@gmail.com>
2022-04-06 13:06:45 +0800

    perf: remove home page components for testing perf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { OurPlatforms, Signup, WhatOurClientsSay, TradeTypes } from './home/_lazy-load'
-import MarketsFold from './home/_markets_fold'
+//import { OurPlatforms, Signup, WhatOurClientsSay, TradeTypes } from './home/_lazy-load'
+//import MarketsFold from './home/_markets_fold'
 import Hero from './home/_hero'
 import { useOpenLiveChat } from 'components/hooks/use-open-live-chat-redirection'
 import { SEO } from 'components/containers'
 import Layout from 'components/layout/layout'
 import { localize, WithIntl } from 'components/localization'
-import { Appearances } from 'components/custom/signup'
+//import { Appearances } from 'components/custom/signup'
 
 const Home = () => {
     /* redirect livechat for en to open live chat popup */
@@ -24,11 +24,11 @@ const Home = () => {
                 has_organization_schema
             />
             <Hero />
-            <MarketsFold />
+            {/* <MarketsFold />
             <TradeTypes />
             <OurPlatforms />
             <WhatOurClientsSay />
-            <Signup appearance={Appearances.public} />
+            <Signup appearance={Appearances.public} /> */}
         </Layout>
     )
 }

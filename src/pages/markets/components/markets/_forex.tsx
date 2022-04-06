438363d113953fafc517ee40dd3668d88cb96dfa
“pavel-latyshou-deriv” <pavel.latyshou@regentmarkets.com>
2022-04-06 14:51:54 +0300

    fix: footer basket for eu &cfds&multipl

--- src/pages/markets/components/markets/_forex.tsx
+++ src/pages/markets/components/markets/_forex.tsx
@@ -3,7 +3,7 @@ import Loadable from '@loadable/component'
 import { WhyTrade } from '../sections/_why-trade'
 import AvailableTrades from '../helper/_available-trades'
 import { forex_content, forex_content_eu } from '../../static/content/_forex'
-import { forex_cfds } from '../../static/content/_cfds'
+import { forex_cfds, forex_cfds_eu } from '../../static/content/_cfds'
 import { forex_multiplier, forex_multiplier_eu } from '../../static/content/_multipliers'
 import { forex_options } from '../../static/content/_digital-options'
 import CFDs from '../sub-markets/_cfds'
@@ -12,7 +12,7 @@ import DigitalOptions from '../sub-markets/_digital-options'
 import { StyledBox } from '../../static/style/_markets-style'
 import { getCountryRule } from 'components/containers/visibility'
 import { Localize, localize } from 'components/localization'
-import { DerivStore } from 'store'
+// import { DerivStore } from 'store'
 import type { SimpleStepsContent } from 'components/custom/_simple-steps'
 //Lazy-load
 const SimpleSteps = Loadable(() => import('components/custom/_simple-steps'))
@@ -23,7 +23,7 @@ type ForexProps = {
 }
 const Forex = ({ simple_step_content }: ForexProps) => {
     // const { is_eu_country } = React.useContext(DerivStore)
-    const { is_row, is_uk_eu } = getCountryRule()
+    const { is_row, is_uk_eu, is_eu } = getCountryRule()
     return (
         <>
             <WhyTrade
@@ -41,7 +41,7 @@ const Forex = ({ simple_step_content }: ForexProps) => {
                 ))}
             </WhyTrade>
             <AvailableTrades
-                CFDs={<CFDs market_content={forex_cfds} />}
+                CFDs={<CFDs market_content={is_eu ? forex_cfds_eu : forex_cfds} />}
                 DigitalOptions={
                     is_row && (
                         <DigitalOptions

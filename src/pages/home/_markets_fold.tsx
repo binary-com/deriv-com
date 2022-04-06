849725f3ac2f68fe48fdd8fb3510fe2255b4fb73
“pavel-latyshou-deriv” <pavel.latyshou@regentmarkets.com>
2022-04-06 13:16:19 +0300

    fix: uk/eu conditions for navtab

--- src/pages/home/_markets_fold.tsx
+++ src/pages/home/_markets_fold.tsx
@@ -137,6 +137,58 @@ const market_data = [
         gradient_end: '#060C11',
     },
 ]
+const market_data_eu = [
+    {
+        header: <Localize translate_text="Forex" />,
+        description: (
+            <Localize translate_text="Take part in the world’s largest financial market where more than $5 trillion worth of currencies are bought and sold each day." />
+        ),
+        img_name: 'market_forex',
+        to: '/markets/forex/',
+        gradient_start: '#661B20',
+        gradient_end: '#190708',
+    },
+    {
+        header: <Localize translate_text="Synthetic indices" />,
+        description: (
+            <Localize translate_text="Enjoy synthetic markets that emulate the excitement of real-world markets without unpredictable real-world disruptions." />
+        ),
+        img_name: 'market_synthetic_indices',
+        to: '/markets/synthetic/',
+        gradient_start: '#20403A',
+        gradient_end: '#08100E',
+    },
+    {
+        header: <Localize translate_text="Stocks & indices" />,
+        description: (
+            <Localize translate_text="Trade share price movements of big brands and predict broader market trends with indices that measure the overall performance of a market." />
+        ),
+        img_name: 'market_stocks_indices',
+        to: '/markets/stock/',
+        gradient_start: '#2A2040',
+        gradient_end: '#0A0810',
+    },
+    {
+        header: <Localize translate_text="Cryptocurrencies" />,
+        description: (
+            <Localize translate_text="Trade on the rising and falling prices of the most popular cryptocurrencies without the need to own a digital wallet." />
+        ),
+        img_name: 'market_crypto',
+        to: '/markets/cryptocurrencies/',
+        gradient_start: '#664407',
+        gradient_end: '#191102',
+    },
+    {
+        header: <Localize translate_text="Commodities" />,
+        description: (
+            <Localize translate_text="Trade the price movements of natural resources that are central to the world’s economy and make the most of the market action." />
+        ),
+        img_name: 'market_commodities',
+        to: '/markets/commodities/',
+        gradient_start: '#183046',
+        gradient_end: '#060C11',
+    },
+]
 
 const market_data_uk = [
     {
@@ -268,7 +320,7 @@ const CarouselItem = ({
 
 const MarketsFold = () => {
     const data = useStaticQuery(query)
-    const { is_uk, is_non_uk } = getCountryRule()
+    const { is_uk, is_non_uk, is_eu } = getCountryRule()
     const size = useWindowSize()
     const is_not_big_screen = size.width < 1980 && size.width >= 768
     const is_mobile = size.width < 768
@@ -327,23 +379,31 @@ const MarketsFold = () => {
                     autoplay_interval={is_mobile ? 3200 : 4000}
                     {...settings}
                 >
-                    {(is_uk ? market_data_uk : market_data).map((market, index) => {
-                        const { header, description, img_name, gradient_start, gradient_end, to } =
-                            market
+                    {(is_uk ? market_data_uk : is_eu ? market_data_eu : market_data).map(
+                        (market, index) => {
+                            const {
+                                header,
+                                description,
+                                img_name,
+                                gradient_start,
+                                gradient_end,
+                                to,
+                            } = market
 
-                        return (
-                            <CarouselItem
-                                key={index}
-                                header={header}
-                                description={description}
-                                is_mobile={is_mobile}
-                                image={data[img_name]}
-                                gradient_start={gradient_start}
-                                gradient_end={gradient_end}
-                                url={to}
-                            />
-                        )
-                    })}
+                            return (
+                                <CarouselItem
+                                    key={index}
+                                    header={header}
+                                    description={description}
+                                    is_mobile={is_mobile}
+                                    image={data[img_name]}
+                                    gradient_start={gradient_start}
+                                    gradient_end={gradient_end}
+                                    url={to}
+                                />
+                            )
+                        },
+                    )}
                 </Carousel>
             </FoldContainer>
         </FoldWrapper>

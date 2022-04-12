551489a6a53cc69593d85fea09a5bbb047f60a2f
Kevin <70131533+kevinw-binary@users.noreply.github.com>
2022-04-12 14:15:55 +0800

    Kevin/add deriv life url on footer (#2880)
    * chore: deriv life url
    
    * chore: add deriv life on nav
    
    * chore: fix qa raised issues
    
    * chore: remove external
    
    * chore: add external
    
    * chore: remove popup

--- src/components/layout/footer/main-links.js
+++ src/components/layout/footer/main-links.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { LinksWrapper, LinkWrapper, LinksCol, Title, Link } from './common/style.js'
 import { localize } from 'components/localization'
 import { Flex, NonUK, ROW, Desktop } from 'components/containers'
-import { deriv_status_page_url, binary_bot_url } from 'common/constants'
+import { deriv_status_page_url, binary_bot_url, deriv_life_url } from 'common/constants'
 
 const MainLinksSection = ({ is_ppc, is_ppc_redirect }) => {
     return (
@@ -28,6 +28,11 @@ const MainLinksSection = ({ is_ppc, is_ppc_redirect }) => {
                         </LinkWrapper>
                         <LinkWrapper>
                             <Link to="/careers/">{localize('Careers')}</Link>
+                        </LinkWrapper>
+                        <LinkWrapper>
+                            <Link to={deriv_life_url} target="_blank" rel="noopener noreferrer">
+                                {localize('Deriv life')}
+                            </Link>
                         </LinkWrapper>
                     </LinksCol>
                     {!is_ppc && (

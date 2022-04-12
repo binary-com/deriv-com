ca2602cafb26b66d71a5a448c7247f224e7e42b2
iman (shayan) <iman@firstsource.tech>
2022-04-12 11:16:28 +0430

    feat(jest):  test case  added

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,18 @@
+const { defaults } = require('jest-config')
 module.exports = {
     transform: {
         '^.+\\.[jt]sx?$': '<rootDir>/jest-preprocess.js',
+        '^.+\\.js$': 'babel-jest',
+        '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$': 'jest-transform-stub',
     },
     moduleNameMapper: {
         '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-        '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+        '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+        '^components/(.*)$': '<rootDir>/src/components/$1',
+        '^themes/(.*)$': '<rootDir>/src/themes/$1',
+        '^common/(.*)$': '<rootDir>/src/common/$1',
+        '^images/(.*)$': '<rootDir>/src/images/$1',
+        '^store': '<rootDir>/src/store/index.tsx',
         '^gatsby-page-utils/(.*)$': `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
         '^gatsby-core-utils/(.*)$': `gatsby-core-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
         '^gatsby-plugin-utils/(.*)$': [`gatsby-plugin-utils/dist/$1`, `gatsby-plugin-utils/$1`], // Workaround for https://github.com/facebook/jest/issues/9771
@@ -16,4 +24,5 @@ module.exports = {
     },
     testURL: `http://localhost`,
     setupFiles: [`<rootDir>/loadershim.js`],
+    // moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx', 'svg'],
 }

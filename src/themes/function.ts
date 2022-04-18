8a755d01a412a6bcbe0885dc3bcfc8ccc5e0ca60
vijayasree garapati <vijayasree@deriv.com>
2022-04-18 13:58:47 +0400

    refactor: add default values to optional props

--- src/themes/function.ts
+++ src/themes/function.ts
@@ -16,14 +16,14 @@ type PaddingsType = {
     pb?: string
 }
 
-export const Margins = ({ m, mt, ml, mr, mb }: MarginsType) => css`
+export const Margins = ({ m = '', mt = '', ml = '', mr = '', mb = '' }: MarginsType) => css`
     margin: ${m ? m : null};
     margin-top: ${mt ? mt : null};
     margin-right: ${mr ? mr : null};
     margin-bottom: ${mb ? mb : null};
     margin-left: ${ml ? ml : null};
 `
-export const Paddings = ({ p, pt, pl, pr, pb }: PaddingsType) => css`
+export const Paddings = ({ p = '', pt = '', pl = '', pr = '', pb = '' }: PaddingsType) => css`
     padding: ${p ? p : null};
     padding-top: ${pt ? pt : null};
     padding-bottom: ${pb ? pb : null};

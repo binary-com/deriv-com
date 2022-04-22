a02e28c43f4f784029dd0f4debe270300c3dbf22
vijayasree garapati <vijayasree@deriv.com>
2022-04-22 10:19:19 +0400

    refactor: typing maxSize

--- src/components/hooks/use-browser-resize.tsx
+++ src/components/hooks/use-browser-resize.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { isBrowser } from 'common/utility'
 import { size } from 'themes/device'
 
-export const useBrowserResize = (maxSize) => {
+export const useBrowserResize = (maxSize: number) => {
     const [is_mobile, setMobile] = useState(false)
     const [width, setWidth] = useState(isBrowser() ? window.innerWidth : 0)
     const breakpoint = maxSize ? maxSize : size.tablet

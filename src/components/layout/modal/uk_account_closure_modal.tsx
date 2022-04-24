0b4d649b375b8aff001c3f82ff5e5eb50a2b45a6
mitra-fs <mitra@firstsource.tech>
2022-04-24 09:55:57 +0430

    fix: change of content

--- src/components/layout/modal/uk_account_closure_modal.tsx
+++ src/components/layout/modal/uk_account_closure_modal.tsx
@@ -3,22 +3,7 @@ import { ModalRefType } from 'components/elements/modal'
 import { Localize } from 'components/localization'
 import { Modal, Header } from 'components/elements'
 import { getCountryRule } from 'components/containers/visibility'
-import { Flex } from 'components/containers'
 import { CookieStorage } from 'common/storage'
-
-type BulletPropType = {
-    bullet: string
-    children: React.ReactNode
-}
-
-const Bullet = ({ bullet, children }: BulletPropType) => (
-    <Flex>
-        <Header type="paragraph-2" width="unset" mr="0.4rem" mt="0.8rem">
-            <Localize translate_text={bullet} />
-        </Header>
-        <div>{children}</div>
-    </Flex>
-)
 
 const UKAccountClosureModal = () => {
     const modalRef = React.useRef<ModalRefType>()
@@ -48,27 +33,20 @@ const UKAccountClosureModal = () => {
                 <Localize translate_text="What this means for you" />
             </Header>
             <Header type="paragraph-2" weight="regular" mt="0.8rem">
-                <Localize translate_text="From 25 Apr 2022 onwards, you won’t be able to trade on Deriv. Also, you won’t be able to make deposits into your Deriv account. You need to withdraw any funds from your account as soon as possible." />
+                <Localize translate_text="You can no longer trade on Deriv or deposit funds into your account." />
+            </Header>
+            <Header type="paragraph-2" weight="regular" mt="0.8rem">
+                <Localize translate_text="If you had any open positions, we have closed them and refunded you." />
             </Header>
             <Header type="paragraph-2" mt="1.6rem">
                 <Localize translate_text="What you need to do now" />
             </Header>
-            <Bullet bullet="1.">
-                <Header type="paragraph-2" mt="0.8rem">
-                    <Localize translate_text="Close all your positions before 25 Apr, 12:00 am GMT" />
-                </Header>
-                <Header type="paragraph-2" weight="regular">
-                    <Localize translate_text="If you still have open positions on 25 Apr 2022, 12:00 am GMT, we shall close them and refund you." />
-                </Header>
-            </Bullet>
-            <Bullet bullet="2.">
-                <Header type="paragraph-2" mt="0.8rem">
-                    <Localize translate_text="Withdraw your funds from your Deriv account" />
-                </Header>
-                <Header type="paragraph-2" weight="regular">
-                    <Localize translate_text="You will lose access to your account when it gets closed, so be sure to withdraw all your funds." />
-                </Header>
-            </Bullet>
+            <Header type="paragraph-2" weight="regular" mt="0.8rem">
+                <Localize translate_text="Please proceed to withdraw all your funds from your account." />
+            </Header>
+            <Header type="paragraph-2" weight="regular" mt="0.8rem">
+                <Localize translate_text="You will lose access to your account when it gets closed, so be sure to withdraw all your funds." />
+            </Header>
         </Modal>
     )
 }

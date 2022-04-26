728676dae5c986433b90ec5561684fad394c378f
NikitK-deriv <nikita.kolesnik@deriv.com>
2022-04-26 16:14:25 +0300

    chore: try to find correct aspectRatio

--- src/components/hooks/use-academy-data.tsx
+++ src/components/hooks/use-academy-data.tsx
@@ -69,7 +69,7 @@ const query = graphql`
                     description
                     imageFile {
                         childImageSharp {
-                            gatsbyImageData(width: 600, aspectRatio: 1.7)
+                            gatsbyImageData(width: 600, aspectRatio: 1.75)
                         }
                     }
                 }

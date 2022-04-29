1bffae4debe39935de7db2e4f03d42d839717bfa
“pavel-latyshou-deriv” <pavel.latyshou@regentmarkets.com>
2022-04-29 10:10:49 +0300

    fix: remove eu/noneu condition

--- src/pages/regulatory/_document_accordion.tsx
+++ src/pages/regulatory/_document_accordion.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { kid_data, kid_data_eu } from './data/_kid_data'
+import kid_data from './data/_kid_data'
 import { Text, Accordion, AccordionItem, LinkText } from 'components/elements'
 import { Flex } from 'components/containers'
 import { localize } from 'components/localization'
@@ -120,8 +120,6 @@ const DocumentAccordion = (locale: DocumentAccordionProps) => {
 
     const { is_eu_country } = React.useContext(DerivStore)
 
-    const kid = is_eu_country ? kid_data_eu : kid_data
-
     return (
         <Accordion has_single_state id="kid">
             <AccordionItem
@@ -166,7 +164,7 @@ const DocumentAccordion = (locale: DocumentAccordionProps) => {
                 </Text>
                 <Flex>
                     <EdgeFlex mt="1.8rem">
-                        {kid.map((data, idx) => (
+                        {kid_data.map((data, idx) => (
                             <FlexText
                                 key={idx}
                                 color="red"

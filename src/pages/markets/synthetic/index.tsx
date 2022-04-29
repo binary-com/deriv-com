4a2fbbb8b48aa9bab51d20ec4d26337ee94a3358
Kevin <70131533+kevinw-binary@users.noreply.github.com>
2022-04-29 17:09:51 +0800

    chore: hotfix fix (#3003)

--- src/pages/markets/synthetic/index.tsx
+++ src/pages/markets/synthetic/index.tsx
@@ -5,16 +5,23 @@ import NavTab from '../components/sections/_nav-tab'
 import { simple_step_content_synthetic } from '../static/content/_synthetic'
 import Signup, { Appearances } from 'components/custom/signup'
 import Layout from 'components/layout/layout'
-import { localize, WithIntl } from 'components/localization'
+import { Localize, WithIntl } from 'components/localization'
 import { SEO } from 'components/containers'
 
 const Markets = () => (
     <Layout>
         <SEO
+<<<<<<< HEAD
             description={localize(
                 "Deriv's proprietary synthetic indices are free of market and liquidity risks. Enjoy 24/7 synthetic trading on Deriv MT5, DTrader, and our other platforms.",
             )}
             title={localize('Synthetic indices | Volatility indices | Deriv')}
+=======
+            description={
+                <Localize translate_text="Deriv’s proprietary synthetic indices are free of market and liquidity risks. Enjoy 24/7 synthetic trading on Deriv MT5, DTrader, and our other platforms." />
+            }
+            title={<Localize translate_text="Synthetic indices | Volatility indices | Deriv" />}
+>>>>>>> fa126d4131 (chore: hotfix fix (#3003))
         />
         <Hero />
         <NavTab route_from={'synthetic'} route_offset={50} />

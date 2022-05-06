4ce89a79026cbb6304867a0c9bb06c1ba72c2785
Mohammad Hashemi <mohammad.hashemi@re-work.dev>
2022-05-06 11:25:09 +0430

    chore: add types to academy utility

--- src/pages/academy/components/utility.ts
+++ src/pages/academy/components/utility.ts
@@ -3,10 +3,10 @@ import { isBrowser, addScript } from 'common/utility'
 
 export const url = isBrowser() ? window.location.href : ''
 
-export const addScriptForCIO = (is_eu) => {
+export const addScriptForCIO = (is_eu: boolean) => {
     const site_id = process.env.GATSBY_ENV_CIO_SITE_ID
 
-    let cio_url
+    let cio_url: string
 
     if (is_eu) {
         cio_url = 'https://assets.customer.io/assets/track-eu.js'
@@ -30,6 +30,6 @@ export const addScriptForCIO = (is_eu) => {
             s.parentNode.insertBefore(t, s);`,
     })
 }
-export const handleTag = (tag_name) => {
+export const handleTag = (tag_name: string) => {
     navigate(`/academy/search?category=${encodeURI(tag_name)}`)
 }

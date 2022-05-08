32abb17eab9e632285dc52bf3586dd5eff77dbf7
Prince <prince@deriv.com>
2022-05-08 14:33:51 +0400

    chore: add script to create PR to crowdin

--- scripts/texts-to-crowdin.js
+++ scripts/texts-to-crowdin.js
@@ -14,6 +14,8 @@ exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
         let text_file = ''
         const unix = +new Date()
         const crowdin_branch = 'my-crowdin'
+        const translate_branch = `translation-${unix}`
+        const file_path = `src/crowdin/${target_branch}.js`
 
         const steps_messages = [
             'Branch Check',
@@ -55,7 +57,7 @@ exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
                     exec(`npm run translate:generate`, handleProcess)
                     break
                 case 2:
-                    fs.readFile(`src/crowdin/${target_branch}.js`, 'utf8', (err, data) => {
+                    fs.readFile(file_path, 'utf8', (err, data) => {
                         if (err) {
                             console.error(err)
                             return
@@ -73,12 +75,22 @@ exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
                     exec(`git pull origin ${crowdin_branch}`, handleProcess)
                     break
                 case 4:
-                    exec(`git checkout -b translation-${unix}`, handleProcess)
-                    console.log('file copy?')
-                    console.log(text_file)
+                    exec(`git checkout -b ${translate_branch}`, handleProcess)
+
                     // exec(`git checkout ${target_branch}`, handleProcess)
                     break
+                case 5:
+                    // Generate new list for crowdiwn translations
+                    fs.writeFileSync(path.resolve(file_path), text_file, 'utf8', (err) =>
+                        console.log(err),
+                    )
 
+                    exec(
+                        `git add ${file_path} && git commit -m 'chore: added new texts to be translated' && git push origin ${translate_branch}`,
+                        () => {},
+                    )
+
+                    break
                 default:
                     break
             }

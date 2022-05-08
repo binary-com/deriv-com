69d71bcb2713542221149ca4574e2e625214df72
Prince <prince@deriv.com>
2022-05-08 13:10:06 +0400

    chore: add script to create PR to crowdin

--- scripts/texts-to-crowdin.js
+++ scripts/texts-to-crowdin.js
@@ -5,7 +5,10 @@ let current_step = 0
 const crowdin_branch = 'my-crowdin'
 const target_branch = process.argv[2]
 
-const steps_messages = [`[1] Switched to ${crowdin_branch}`]
+const steps_messages = [
+    `[1] Switched to ${crowdin_branch}`,
+    `[2] Fetched latest: ${crowdin_branch}`,
+]
 
 const showMessage = (err) => {
     const status = err ? 'x' : '✓'
@@ -13,17 +16,33 @@ const showMessage = (err) => {
     console.log(`${steps_messages[current_step]} ${status}`)
 }
 
+const handleProcess = (err, stdout, stderr) => {
+    if (err) {
+        showMessage(err)
+        console.log(err)
+    } else {
+        showMessage()
+        current_step++
+        process()
+    }
+}
+
+const process = () => {
+    switch (current_step) {
+        case 0:
+            exec(`git checkout ${crowdin_branch}`, handleProcess)
+            break
+        case 1:
+            exec(`git pull origin ${crowdin_branch}`, handleProcess)
+            break
+
+        default:
+            break
+    }
+}
+
 if (target_branch) {
-    exec(`git checkout ${crowdin_branch}`, (err, stdout, stderr) => {
-        // handle err, stdout & stderr
-        if (err) {
-            showMessage(err)
-            console.log(err)
-        } else {
-            showMessage()
-            current_step++
-        }
-    })
+    process()
 } else {
     console.log('Target branch is required!')
 }

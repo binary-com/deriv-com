bfb584e3373859b8affad856b8ec157e1bbc58ff
Prince <prince@deriv.com>
2022-05-08 13:02:55 +0400

    chore: add script to create PR to crowdin

--- scripts/texts-to-crowdin.js
+++ scripts/texts-to-crowdin.js
@@ -1,12 +1,29 @@
-const { exec } = require('child_process');
+/* eslint-disable  */
+const { exec } = require('child_process')
 
-exec('git checkout my-crowdin', (err, stdout, stderr) => {
- // handle err, stdout & stderr
-    if(err) {
-        console.log(err)
-    }
+let current_step = 0
+const crowdin_branch = 'my-crowdin'
+const target_branch = process.argv[2]
 
-    else {
-        console.log('Switched to Crowdin')
-    }
-});
+const steps_messages = [`[1] Switched to ${crowdin_branch}`]
+
+const showMessage = (err) => {
+    const status = err ? 'x' : '✓'
+
+    console.log(`${steps_messages[current_step]} ${status}`)
+}
+
+if (target_branch) {
+    exec(`git checkout ${crowdin_branch}`, (err, stdout, stderr) => {
+        // handle err, stdout & stderr
+        if (err) {
+            showMessage(err)
+            console.log(err)
+        } else {
+            showMessage()
+            current_step++
+        }
+    })
+} else {
+    console.log('Target branch is required!')
+}

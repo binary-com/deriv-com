9c099c48af8feca5eb80ad4b3c757c24cb228529
vijayasree garapati <vijayasree@deriv.com>
2022-05-12 16:50:19 +0400

    refactor: block_eu condition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { OurPlatforms, Signup, WhatOurClientsSay, TradeTypes } from './home/_laz
 import MarketsFold from './home/_markets_fold'
 import Hero from './home/_hero'
 import { isBrowser } from 'common/utility'
+import { eu_domains } from 'common/constants'
 import { useOpenLiveChat } from 'components/hooks/use-open-live-chat-redirection'
 import { SEO } from 'components/containers'
 import Layout from 'components/layout/layout'
@@ -15,10 +16,7 @@ const Home = () => {
     useOpenLiveChat()
 
     // To block eu.deriv.com domain for search engines
-    const block_eu =
-        isBrowser() &&
-        (window.location.hostname === 'eu.deriv.com' ||
-            window.location.hostname === 'staging.deriv.com') // qa testing
+    const block_eu = isBrowser() && eu_domains.includes(window.location.hostname)
 
     return (
         <Layout type="transparent" margin_top="0">

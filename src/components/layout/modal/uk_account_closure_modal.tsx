f93b56c161027f90e6da748016db2b08fef09685
shayan khaleghparast <iman@firstsource.tech>
2022-05-20 13:08:11 +0430

    feat(jest): uk_account_closure_modal.tsx changes reverted and jest warning fixed

--- src/components/layout/modal/uk_account_closure_modal.tsx
+++ src/components/layout/modal/uk_account_closure_modal.tsx
@@ -26,25 +26,25 @@ const UKAccountClosureModal = () => {
             onPositive={handlePositive}
             is_dismissible={false}
         >
-            <Header as="h1" type="paragraph-2" weight="regular">
+            <Header type="paragraph-2" weight="regular">
                 <Localize translate_text="As part of the changes in our market offerings, we will be closing our UK clients’ accounts." />
             </Header>
-            <Header as="h1" type="paragraph-2" mt="1.6rem">
+            <Header type="paragraph-2" mt="1.6rem">
                 <Localize translate_text="What this means for you" />
             </Header>
-            <Header as="h1" type="paragraph-2" weight="regular" mt="0.8rem">
+            <Header type="paragraph-2" weight="regular" mt="0.8rem">
                 <Localize translate_text="You can no longer trade on Deriv or deposit funds into your account." />
             </Header>
-            <Header as="h1" type="paragraph-2" weight="regular" mt="0.8rem">
+            <Header type="paragraph-2" weight="regular" mt="0.8rem">
                 <Localize translate_text="If you had any open positions, we have closed them and refunded you." />
             </Header>
-            <Header as="h1" type="paragraph-2" mt="1.6rem">
+            <Header type="paragraph-2" mt="1.6rem">
                 <Localize translate_text="What you need to do now" />
             </Header>
-            <Header as="h1" type="paragraph-2" weight="regular" mt="0.8rem">
+            <Header type="paragraph-2" weight="regular" mt="0.8rem">
                 <Localize translate_text="Please proceed to withdraw all your funds from your account." />
             </Header>
-            <Header as="h1" type="paragraph-2" weight="regular" mt="0.8rem">
+            <Header type="paragraph-2" weight="regular" mt="0.8rem">
                 <Localize translate_text="You will lose access to your account when it gets closed, so be sure to withdraw all your funds." />
             </Header>
         </Modal>

bc3d45175a9440e687841e30b7bc735994bc0c1d
Prince <prince@deriv.com>
2022-05-20 17:17:25 +0800

    chore: added pre-commit script

---
+++ scripts/translation-pre-commit.js
@@ -0,0 +1,51 @@
+/* eslint-disable */
+const { exec } = require('child_process')
+
+let branch_name = ''
+
+const logError = (err) => {
+    console.log(`\x1b[31m${err}`)
+}
+
+exec('git rev-parse --abbrev-ref HEAD', (err, stdout) => {
+    if (err) {
+        logError(err)
+    }
+
+    if (typeof stdout === 'string') {
+        branch_name = stdout.trim()
+
+        handleProcess()
+    }
+})
+
+const handleProcess = () => {
+    const branch_name_convention = 'translation-'
+    const strings_path = 'crowdin/messages.json'
+
+    // Detect Auto Translation Process
+    if (!branch_name.startsWith(branch_name_convention)) {
+        console.log(
+            `\x1b[32mInvalid branch name: \x1b[33m${branch_name}\x1b[32m Auto Extract Strings \n `,
+        )
+
+        runProcess({
+            process: 'extract',
+            callback: () => {
+                exec(`git add ${strings_path}`)
+            },
+        })
+    }
+}
+
+const runProcess = (data) => {
+    const { process, callback } = data
+
+    switch (process) {
+        case 'extract':
+            exec('npm run translate:extract', callback)
+            break
+        default:
+            break
+    }
+}

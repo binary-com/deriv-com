6c38bf7bcf1ea37ba0b55f7f9e8c8b586e0c8e5d
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2022-06-02 15:04:06 +0800

    Revert "Mohsen / updated PA page content (#3105)" (#3123)
    This reverts commit ddd27bec660be60eeab68d93852049fe77646c08.

--- src/pages/partners/payment-agent/_who-can-apply.tsx
+++ src/pages/partners/payment-agent/_who-can-apply.tsx
@@ -13,7 +13,7 @@ import {
 } from '../affiliate-ib/_who-can-apply'
 import { Flex } from 'components/containers'
 import { localize, Localize } from 'components/localization'
-import { Header, LinkText, Timeline } from 'components/elements'
+import { Header, Timeline } from 'components/elements'
 import { LinkButton } from 'components/form'
 import TradingExperts from 'images/svg/partners/trading-experts.svg'
 import Affiliates from 'images/svg/partners/affiliates.svg'
@@ -68,11 +68,6 @@ const StyledUl = styled.ul`
     list-style-type: disc;
     font-size: var(--text-size-s);
     padding-left: 16px;
-    margin-bottom: 36px;
-
-    &.get-started--list {
-        margin-bottom: 0;
-    }
 `
 
 const Li = styled.li`
@@ -149,13 +144,13 @@ const WhoCanApply = () => {
                             />
                         ))}
                         <Header as="h4" type="paragraph-1" mt="24px">
-                            {localize('Note:')}
+                            {localize('A couple of things to note:')}
                         </Header>
                         <StyledUl>
                             <Li>
                                 <Header as="h4" type="paragraph-1" weight="normal">
                                     {localize(
-                                        'You must have a minimum balance in your Deriv account while your application is under review. The amount of this balance depends on your country of residence. You only need to maintain the minimum balance until your application is successful.',
+                                        'You must have a minimum balance in your Deriv account, depending on your country of residence.',
                                     )}
                                 </Header>
                             </Li>
@@ -167,20 +162,6 @@ const WhoCanApply = () => {
                                 </Header>
                             </Li>
                         </StyledUl>
-                        <Header as="h4" type="paragraph-1" weight="normal">
-                            <Localize
-                                translate_text="See our <0>terms and conditions</0> for more info."
-                                components={[
-                                    <LinkText
-                                        key={0}
-                                        color="red"
-                                        target="_blank"
-                                        href="/tnc/business-partners-payment-agents.pdf"
-                                        rel="noopener noreferrer"
-                                    />,
-                                ]}
-                            />
-                        </Header>
                     </WhoCanApplyWrapper>
                 </Flex>
                 <Separator />
@@ -201,7 +182,7 @@ const WhoCanApply = () => {
                                         <Header as="h4" type="paragraph-1" weight="normal">
                                             {localize('Send us an email with the following:')}
                                         </Header>
-                                        <StyledUl className="get-started--list">
+                                        <StyledUl>
                                             <Li>
                                                 <Header as="h4" type="paragraph-1" weight="normal">
                                                     {localize(
@@ -276,7 +257,7 @@ const WhoCanApply = () => {
                     to="mailto:partners@deriv.com"
                     is_mail_link
                 >
-                    {localize('Send us email to apply')}
+                    {localize('Sign up')}
                 </LinkButton>
             </ButtonWrapper>
         </SectionWrapper>

2eec8c897e354a802dab51cab8c1b05d10dc7044
meenakshi-deriv <104189801+meenakshi-deriv@users.noreply.github.com>
2022-06-08 16:47:07 +0400

    Meenu/feat: created tabs and organised location (#3107)
    * feat: created tabs and organised location
    
    * feat: sorted tabs in the same series based on figma design
    
    * fix: change all to lowercase
    
    * refactor: added function to format name and changed variable name
    
    * refactor: updated method for changing case
    
    * fix: design fixes in location page
    
    * fix: location page responsive design fix
    
    * text: added explore our location as a header
    
    * fix: space between tags is fixed
    
    Co-authored-by: Meenakshi <meenakshi@Meenakshis-MacBook-Pro.local>

--- src/pages/careers/_model/_locations/_locations.types.ts
+++ src/pages/careers/_model/_locations/_locations.types.ts
@@ -5,6 +5,8 @@ export type LocationsType = {
     img_alt?: string
     map_office_name: string
     country: string
+    continent: string
+    display_continent: string
     flagIcon: string
     thumbnail: string
     first_p: string

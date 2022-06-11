28ccf8ca509e365bd6e6f1d7929496d88abfcfdd
Mohammad Hashemi <mohammad.hashemi@re-work.dev>
2022-06-11 15:17:41 +0430

    chore: set default value for useState

--- src/pages/home/platform-carousel/_platform-carousel.tsx
+++ src/pages/home/platform-carousel/_platform-carousel.tsx
@@ -7,7 +7,11 @@ import { useCountryRule } from 'components/hooks/use-country-rule'
 
 const PlatformCarousel = () => {
     const { is_eu, is_uk, is_row } = useCountryRule()
-    const [carousel_data, setCarouselData] = useState(null)
+    const [carousel_data, setCarouselData] = useState(
+        (is_eu && platform_details_eu) ||
+            (is_uk && platform_details_uk) ||
+            (is_row && platform_details_cr),
+    )
 
     useEffect(() => {
         setCarouselData(

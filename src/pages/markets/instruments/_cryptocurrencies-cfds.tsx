3145740983c3273522d512ea2b223c3bdbf62d27
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2022-06-17 14:13:55 +0200

    style: trying showing with filter

--- src/pages/markets/instruments/_cryptocurrencies-cfds.tsx
+++ src/pages/markets/instruments/_cryptocurrencies-cfds.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Symbol from '../components/helper/_symbol'
-import { cryptocurrencies_cfds, cryptocurrencies_cfds_ROW } from '../static/content/_market-symbols'
+import { cryptocurrencies_cfds } from '../static/content/_market-symbols'
 import { EU, ROW } from 'components/containers/visibility'
 
 const CryptocurrenciesCFDs = () => {
@@ -9,14 +9,14 @@ const CryptocurrenciesCFDs = () => {
             <EU>
                 <>
                     {cryptocurrencies_cfds.map((symbol, index) => (
-                        <Symbol key={index} src={symbol.src} text={symbol.text} />
+                        <Symbol key={index} src={symbol.src} text={symbol.text} row={symbol.row} />
                     ))}
                 </>
             </EU>
             <ROW>
                 <>
-                    {cryptocurrencies_cfds_ROW.map((symbol, index) => (
-                        <Symbol key={index} src={symbol.src} text={symbol.text} />
+                    {cryptocurrencies_cfds.map((symbol, index) => (
+                        <Symbol key={index} src={symbol.src} text={symbol.text} row={symbol.row} />
                     ))}
                 </>
             </ROW>

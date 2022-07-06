7eca0c9182e7cde97a76670d8250ffba2e2d6b23
Mohammad Hashemi <mohammad.hashemi@re-work.dev>
2022-07-06 17:10:31 +0430

    feat: add pagination to all videos page

--- src/pages/academy/videos/_all-videos.tsx
+++ src/pages/academy/videos/_all-videos.tsx
@@ -1,20 +1,39 @@
 import React from 'react'
 import { StyledImg, Container, VideoGrid } from '../common/_styles'
 import { RedirectLink } from '../components/recent-featured-posts/_style'
+import Pagination from '../components/_pagination'
 import VideoCard from './_video-card'
 import { VideoDataType } from './index'
 import { Flex } from 'components/containers'
 import { Text, LocalizedLinkText } from 'components/elements'
 import RightArrow from 'images/svg/tools/black-right-arrow.svg'
+import { useBrowserResize } from 'components/hooks/use-browser-resize'
 
 type AllVideosProps = {
     video_data: VideoDataType
 }
 
 const AllVideos = ({ video_data }: AllVideosProps) => {
+    const [is_mobile] = useBrowserResize()
+    const [current_page, setCurrentPage] = React.useState(1)
+    const desktop_max_posts = 9
+    const mobile_max_posts = 9
+    const posts_per_page = is_mobile ? mobile_max_posts : desktop_max_posts
+
+    const index_of_last_post = current_page * posts_per_page
+    const index_of_first_post = index_of_last_post - posts_per_page
+    const current_videos = video_data.slice(index_of_first_post, index_of_last_post)
+
+    const myRef = React.useRef(null)
+
+    const paginate = (page_number) => {
+        myRef.current.scrollIntoView()
+        setCurrentPage(page_number)
+    }
+
     return (
         <Container m="0 auto" fd="column">
-            <Flex jc="flex-start" ai="center" mt="4rem">
+            <Flex ref={myRef} jc="flex-start" ai="center" mt="4rem">
                 <LocalizedLinkText to="/academy/" color="grey-5">
                     Home
                 </LocalizedLinkText>
@@ -22,7 +41,7 @@ const AllVideos = ({ video_data }: AllVideosProps) => {
                 <Text>All videos</Text>
             </Flex>
             <VideoGrid margin="8rem 0">
-                {video_data.map((item) => {
+                {current_videos.map((item) => {
                     return (
                         <RedirectLink
                             key={item.video_id}
@@ -33,6 +52,14 @@ const AllVideos = ({ video_data }: AllVideosProps) => {
                     )
                 })}
             </VideoGrid>
+            {video_data.length > 8 && (
+                <Pagination
+                    posts_per_page={posts_per_page}
+                    total_posts={video_data.length - 1}
+                    paginate={paginate}
+                    current_page={current_page}
+                />
+            )}
         </Container>
     )
 }

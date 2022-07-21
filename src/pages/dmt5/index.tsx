6f7260adb073657182bb997757c62a2049f18ba0
Meenakshi <meenakshi@Meenakshis-MacBook-Pro.local>
2022-07-21 11:27:52 +0400

    fix: image optimization

--- src/pages/dmt5/index.tsx
+++ src/pages/dmt5/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { graphql, StaticQuery, useStaticQuery } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import {
     WhyTrader,
     StartTrader,

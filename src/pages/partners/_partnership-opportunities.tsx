f25330da3b971379cada153f0bd0d7def8356028
NikitK-deriv <nikita.kolesnik@deriv.com>
2022-07-26 09:52:21 +0300

    refactor: convert partners page to ts

--- src/pages/partners/_partnership-opportunities.tsx
+++ src/pages/partners/_partnership-opportunities.tsx
@@ -3,13 +3,12 @@ import styled, { css } from 'styled-components'
 import { Header, Text, CardStyle } from 'components/elements'
 import { localize, LocalizedLink } from 'components/localization'
 import { Container, SectionContainer, Flex } from 'components/containers'
-// import device from 'themes/device'
 // Icons
 import { ReactComponent as PartnerAffiliate } from 'images/svg/partners/partner-affiliate.svg'
 import { ReactComponent as PartnerPaymentAgent } from 'images/svg/partners/partner-payment-agent.svg'
 import { ReactComponent as DeveloperProgramme } from 'images/svg/partners/developer-programme.svg'
 
-const ClientCard = styled(LocalizedLink)`
+const ClientCard = styled(LocalizedLink)<{ first?: number; second?: number; third?: number }>`
     ${CardStyle}
     text-decoration: none;
     max-width: 38.4rem;

e0eeb28eb577818404500de7874e2f75bd72e9f1
Prince <prince@deriv.com>
2022-07-28 16:31:40 +0400

    chore: update translation pull script

--- scripts/handle-translations.js
+++ scripts/handle-translations.js
@@ -108,7 +108,7 @@ const runProcess = (data) => {
     switch (process) {
         case 'pull':
             exec(
-                `git commit -m 'auto-pull-translation' --allow-empty --no-verify;git push`,
+                `git commit -m 'auto-pull-translation' --allow-empty --no-verify;git push origin ${branch_name} `,
                 callback,
             )
             break

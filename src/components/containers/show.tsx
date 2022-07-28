3d54b735981383edbca5213e0a58597ca4442669
NikitK-deriv <nikita.kolesnik@deriv.com>
2022-07-28 15:31:58 +0300

    chore: change type in show.tsx and make NonEU component the same as Eu

--- src/components/containers/show.tsx
+++ src/components/containers/show.tsx
@@ -2,20 +2,19 @@ import React, { useContext, ReactNode } from 'react'
 import { Media } from 'themes/media'
 import { DerivStore } from 'store'
 
-type ResponseType = {
+type DesktopType = {
     children?: ReactNode
     max_width?: string & number
-    min_width?: string & number
 }
 
-export const Desktop = ({ children, max_width, ...props }: ResponseType) => (
+export const Desktop = ({ children, max_width, ...props }: DesktopType) => (
     <Media greaterThanOrEqual={max_width || 'tabletL'} {...props}>
         {children}
     </Media>
 )
 
 // We can't type it properly due fresnel
-export const Mobile = ({ children, min_width, ...props }: ResponseType) => (
+export const Mobile = ({ children, min_width, ...props }: any) => (
     <Media between={['start', ...(min_width ? [min_width] : ['tabletL'])]} {...props}>
         {children}
     </Media>
@@ -30,12 +29,7 @@ export const Eu = ({ children }: DomainType) => {
     else return null
 }
 
-export const NonEU = ({ children }: DomainType) => {
-    const { is_eu_country } = useContext(DerivStore)
-
-    if (is_eu_country === false) return <>{children}</>
-    else return null
-}
+export const NonEU = Eu
 
 export default {
     Eu,

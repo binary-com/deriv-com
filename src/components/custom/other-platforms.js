4cb863eb1a2d524323ca82e823e376d5998fc1fd
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2022-08-05 11:29:14 +0200

    chore: more wrong design issues fixed

--- src/components/custom/other-platforms.js
+++ src/components/custom/other-platforms.js
@@ -233,7 +233,7 @@ export const OtherPlatform = ({ header, subHeader, exclude, is_nav, is_ppc_redir
             <StyledFlexGridContainer content_width="38.4rem" gap="1rem" grid="3" justify="center">
                 {}
                 {excludetoLowerCase !== 'dtrader' && <TraderCard />}
-                {excludetoLowerCase !== 'dbot' && <BotCard />}
+                <ROW>{excludetoLowerCase !== 'dbot' && <BotCard />}</ROW>
                 {excludetoLowerCase !== 'dmt5' && <DMT5Card is_ppc_redirect={is_ppc_redirect} />}
                 <ROW>{excludetoLowerCase !== 'derivx' && <DerivXCard />}</ROW>
             </StyledFlexGridContainer>

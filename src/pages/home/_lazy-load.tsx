4a6ceacd2f93727241245ce3d65cdaace9c15110
ashrafali-v <ashrafalifrk@gmail.com>
2022-08-08 12:01:51 +0800

    fix: markets lazy loaded section issue fix

--- src/pages/home/_lazy-load.tsx
+++ src/pages/home/_lazy-load.tsx
@@ -1,6 +1,6 @@
 import Loadable from '@loadable/component'
 
-//export const MarketsFold = Loadable(() => import('./_markets_fold'))
+export const Markets = Loadable(() => import('./_markets_fold'))
 export const TradeTypes = Loadable(() => import('./_trade-types'))
 export const OurPlatforms = Loadable(() => import('./_our_platforms'))
 export const WhatOurClientsSay = Loadable(() => import('./_what-our-clients-say'))

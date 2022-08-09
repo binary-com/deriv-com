41ed5ac265103881cb75960a68624efe852f7455
Aswathy <96725617+aswathy-deriv@users.noreply.github.com>
2022-08-09 11:05:47 +0400

    Aswathy/signup affiliate success version-2 and validation (#3439)
    * fix: signup success page validation
    
    * fix: disable the button
    
    * fix: added the email dynamic
    
    * fix: code cleaning
    
    * feat: success page
    
    * refactor: changed the naming convention

--- src/pages/signup-affiliates/index.tsx
+++ src/pages/signup-affiliates/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import ReCAPTCHA from 'react-google-recaptcha'
 import AffiliateSignupLayout, { SignUpWrapper } from './components/_layout'
-import { localize, WithIntl } from 'components/localization'
+import { localize, WithIntl, Localize } from 'components/localization'
 import { Input, Button } from 'components/form'
 import { Header, LinkText, LocalizedLinkText } from 'components/elements'
 import device from 'themes/device'
 import Google from 'images/svg/custom/googlenew.svg'
 import Apple from 'images/svg/custom/applenew.svg'
 import Facebook from 'images/svg/custom/facebooknew-blue.svg'
+import Email from 'images/svg/check-email/new email.svg'
+import validation from 'common/validation'
+import Login from 'common/login'
 
 const StyledNote = styled.div`
     padding: 8px 0;
@@ -65,6 +68,11 @@ const StyledText = styled(LinkText)`
         width: 17rem;
     }
 `
+const EmailText = styled(LinkText)`
+    font-size: 16px;
+    line-height: 24px;
+    padding-top: 24px;
+`
 const LoginText = styled(LinkText)`
     text-align: center;
     align-self: center;
@@ -117,88 +125,168 @@ const FacebookWrapper = styled.div``
 const onChange = (data) => {
     console.log('Captcha value:', data)
 }
-type AffiliateSignupProps = {
-    autofocus: boolean
-    email: string
-    email_error_msg: string
-    is_submitting: boolean
-}
+const EmailWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+const ImageWrapper = styled.div`
+    margin: auto;
+`
 
-const AffiliateSignup = ({ autofocus, email, email_error_msg }: AffiliateSignupProps) => {
+const AffiliateSignup = () => {
+    const [email, setEmail] = useState('')
+    const [is_submitted, setSubmitted] = useState(false)
+    const [email_error_msg, setEmailErrorMsg] = useState('')
+    const [submit_status, setSubmitStatus] = useState('')
+    const [submit_error_msg, setSubmitErrorMsg] = useState('')
+
+    const validateEmail = (error_address) => {
+        const error_message =
+            validation.required(error_address) ||
+            validation.email(error_address) ||
+            submit_error_msg
+        if (submit_error_msg) {
+            setSubmitErrorMsg('')
+        }
+        return error_message
+    }
+    const handleValidation = (param) => {
+        const message = typeof param === 'object' ? param.target.value : param
+        setEmailErrorMsg(validateEmail(message.replace(/\s/g, '')))
+    }
+    const handleInputValidation = (e) => {
+        const { value } = e.target
+        setEmail(value)
+        handleValidation(value)
+    }
+
+    const clearEmail = () => {
+        setEmail('')
+        setEmailErrorMsg('')
+    }
+    const handleLogin = (e) => {
+        e.preventDefault()
+        Login.redirectTologin()
+    }
     return (
         <AffiliateSignupLayout>
             <SignUpWrapper>
-                <Header as="h3" type="heading-3" mb="0.8rem">
-                    {localize('Sign up')}
-                </Header>
-                <Header as="p" type="subtitle-1" weight="normal">
-                    {localize('Enter your email address to begin')}
-                </Header>
+                {submit_status !== 'success' ? (
+                    <>
+                        <Header as="h3" type="heading-3" mb="0.8rem">
+                            {localize('Sign up')}
+                        </Header>
+                        <Header as="p" type="subtitle-1" weight="normal">
+                            {localize('Enter your email address to begin')}
+                        </Header>
 
-                <StyledNote>
-                    <Header as="p" type="paragraph-2" weight="normal" align="center" color="grey-5">
-                        {localize('Want to sign up as a trader?')}
-                        <LocalizedLinkText to="/signup">
-                            <StyledLinkText id="dm-new-login-button" size="14px" color="grey-5">
-                                {localize('Create a Deriv account')}
-                            </StyledLinkText>
-                        </LocalizedLinkText>
-                    </Header>
-                </StyledNote>
+                        <StyledNote>
+                            <Header
+                                as="p"
+                                type="paragraph-2"
+                                weight="normal"
+                                align="center"
+                                color="grey-5"
+                            >
+                                {localize('Want to sign up as a trader?')}
+                                <LocalizedLinkText to="/signup">
+                                    <StyledLinkText
+                                        id="dm-new-login-button"
+                                        size="14px"
+                                        color="grey-5"
+                                    >
+                                        {localize('Create a Deriv account')}
+                                    </StyledLinkText>
+                                </LocalizedLinkText>
+                            </Header>
+                        </StyledNote>
 
-                <InputGroup>
-                    <Input
-                        id="dm-email-input"
-                        name="email"
-                        type="text"
-                        border="solid 1px var(--color-grey-7)"
-                        label_color="grey-5"
-                        label_hover_color="grey-5"
-                        background="white"
-                        error={email_error_msg}
-                        value={email}
-                        label={localize('Email')}
-                        placeholder={'Email'}
-                        autoFocus={autofocus}
-                        autoComplete="off"
-                        required
-                    />
-                </InputGroup>
-                <CaptchaContainer>
-                    <ReCAPTCHA
-                        sitekey="6Ld_EwIhAAAAAI8eRUeCN9RtKzn5oKsHKKwwPaXf"
-                        onChange={onChange}
-                    />
-                </CaptchaContainer>
-                <EmailButton type="submit" secondary="true" id="partner-signup">
-                    {localize('Create partner account')}
-                </EmailButton>
-                <SignupWithContainer>
-                    <Line />
-                    <StyledText color="grey-5" align="center">
-                        {localize('Or sign up with')}
-                    </StyledText>
-                    <Line />
-                </SignupWithContainer>
-                <SocialWrapper>
-                    <GoogleWrapper id="google-signup">
-                        <img src={Google} alt="google" width="40" height="40" />
-                    </GoogleWrapper>
-                    <FacebookWrapper id="facebook-signup">
-                        <img src={Facebook} alt="facebook" width="40" height="40" />
-                    </FacebookWrapper>
-                    <AppleWrapper id="apple-signup">
-                        <img src={Apple} alt="apple" width="40" height="40" />
-                    </AppleWrapper>
-                </SocialWrapper>
-                <LoginContainer>
-                    <LoginText mt="3.75rem">
-                        {localize('Already have a Deriv or Binary.com affiliate account?')}
-                    </LoginText>
-                    <StyledText id="dm-new-login-button" align="center" size="16px" color="red">
-                        {localize('Log in')}
-                    </StyledText>
-                </LoginContainer>
+                        <InputGroup>
+                            <Input
+                                id="dm-email-input"
+                                name="email"
+                                type="text"
+                                border="solid 1px var(--color-grey-7)"
+                                label_color="grey-5"
+                                label_hover_color="grey-5"
+                                background="white"
+                                error={email_error_msg}
+                                value={email}
+                                label={localize('Email')}
+                                placeholder={'Email'}
+                                autoComplete="off"
+                                handleError={clearEmail}
+                                onChange={handleInputValidation}
+                                onBlur={handleValidation}
+                                required
+                            />
+                        </InputGroup>
+                        <CaptchaContainer>
+                            <ReCAPTCHA
+                                sitekey="6Ld_EwIhAAAAAI8eRUeCN9RtKzn5oKsHKKwwPaXf"
+                                onChange={onChange}
+                            />
+                        </CaptchaContainer>
+                        <EmailButton
+                            disabled={is_submitted || !email || email_error_msg}
+                            type="submit"
+                            secondary="true"
+                            id="partner-signup"
+                        >
+                            {localize('Create partner account')}
+                        </EmailButton>
+                        <SignupWithContainer>
+                            <Line />
+                            <StyledText color="grey-5" align="center">
+                                {localize('Or sign up with')}
+                            </StyledText>
+                            <Line />
+                        </SignupWithContainer>
+                        <SocialWrapper>
+                            <GoogleWrapper id="google-signup">
+                                <img src={Google} alt="google" width="40" height="40" />
+                            </GoogleWrapper>
+                            <FacebookWrapper id="facebook-signup">
+                                <img src={Facebook} alt="facebook" width="40" height="40" />
+                            </FacebookWrapper>
+                            <AppleWrapper id="apple-signup">
+                                <img src={Apple} alt="apple" width="40" height="40" />
+                            </AppleWrapper>
+                        </SocialWrapper>
+                        <LoginContainer>
+                            <LoginText mt="3.75rem">
+                                {localize('Already have a Deriv or Binary.com affiliate account?')}
+                            </LoginText>
+                            <StyledText
+                                id="dm-new-login-button"
+                                align="center"
+                                size="16px"
+                                color="red"
+                                onClick={handleLogin}
+                            >
+                                {localize('Log in')}
+                            </StyledText>
+                        </LoginContainer>
+                    </>
+                ) : (
+                    <EmailWrapper>
+                        <ImageWrapper>
+                            <img src={Email} alt="email" width="116" height="100" />
+                        </ImageWrapper>
+                        <Header type="subtitle-1" align="center">
+                            {localize('Check your email')}
+                        </Header>
+                        <Header type="paragraph-1" align="center" weight="400" pt="8px">
+                            <Localize
+                                translate_text="We've sent a message to {{email}} with a link to activate your account."
+                                values={{ email: email }}
+                            />
+                        </Header>
+                        <EmailText id="dm-new-login-button" align="center" color="red">
+                            {localize('Didn’t receive your email?')}
+                        </EmailText>
+                    </EmailWrapper>
+                )}
             </SignUpWrapper>
         </AffiliateSignupLayout>
     )

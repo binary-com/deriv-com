470cad78c7b609148b762636918bc7eac2be7f53
aswathy-deriv <aswathy@deriv.com>
2022-08-12 15:50:22 +0400

    fix: otp ui design

--- src/pages/signup-affiliates/components/_phone_number.tsx
+++ src/pages/signup-affiliates/components/_phone_number.tsx
@@ -6,22 +6,81 @@ import OtpVerification from 'images/svg/signup-affiliates/code 2.svg'
 import { Input, Button } from 'components/form'
 
 const PhoneNumber = () => {
-    const [name, setName] = React.useState('')
-    const [name_error_msg, setNameErrorMsg] = React.useState('')
-
+    const [phone, setPhone] = useState('')
+    const [phonecode, setPhoneCode] = useState('44')
     const handleInputNameChange = (e) => {
         const { value } = e.target
-        setName(value)
+        setPhone(value)
     }
     const ImageContainer = styled.div`
         display: flex;
         justify-content: center;
     `
-    const InputWrapper = styled.div`
-        position: relative;
-        width: 100%;
-        margin: 2.4rem 0;
+
+    const CodeWrapper = styled.div`
+        width: 58px;
+        height: 40px;
+        background: var(--color-grey-8);
+        border: 1px solid var(--color-grey-7);
+        border-radius: 4px 0;
+        align-items: center;
+        display: flex;
+        justify-content: center;
     `
+    const CodeContainer = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        padding-top: 20px;
+    `
+
+    const ButtonCode = styled(Button)`
+        border-radius: 0 4px;
+    `
+    const PhoneNumberInput = styled(Input)`
+        border-radius: 0;
+        width: 240px;
+    `
+    const CountryCode = styled.div`
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 20px;
+        color: var(--color-grey-5);
+    `
+    const CodeText = styled(Header)`
+        padding-right: 95px;
+    `
+    const OtpCard = {
+        height: '40px',
+        width: '40px',
+        margin: 'auto',
+        display: 'flex',
+        textAlign: 'center' as const,
+        color: 'var(--color-grey-7)',
+        border: '1px solid var(--color-grey-7)',
+    }
+    const OtpContainer = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    `
+    const FormValue = {
+        padding: '10px',
+    }
+    const OtpNumbers = [
+        {
+            value: 1,
+        },
+        {
+            value: 5,
+        },
+        {
+            value: 3,
+        },
+        {
+            value: 6,
+        },
+    ]
     return (
         <>
             <Header as="h2" align="center" type="paragraph-1" pb="24px" weight="normal">
@@ -31,21 +90,38 @@ const PhoneNumber = () => {
                 <img src={OtpVerification} width={240} height={192} />
             </ImageContainer>
 
-            <InputWrapper>
-                <Input
+            <CodeContainer>
+                <CodeWrapper>
+                    <CountryCode>{phonecode && <span>{`+${phonecode}`}</span>}</CountryCode>
+                </CodeWrapper>
+
+                <PhoneNumberInput
                     required
-                    id="name"
-                    name="number"
-                    type="text"
-                    error={name_error_msg}
-                    value={name}
+                    id="phone"
+                    value={phone}
                     onChange={handleInputNameChange}
-                    autoComplete="off"
-                    maxLength="70"
                     height="40px"
+                    autoFocus={true}
+                    autoComplete="off"
                     focus_border="var(--color-grey-7)"
                 />
-            </InputWrapper>
+
+                <ButtonCode disabled tertiary="true">
+                    {localize('Send code')}
+                </ButtonCode>
+            </CodeContainer>
+            <CodeText align="center" color="grey" type="paragraph-2" weight="400">
+                {localize('Didn’t receive your code? Try again in 59 seconds')}
+            </CodeText>
+            <OtpContainer>
+                {OtpNumbers.map(({ value }, index) => {
+                    return (
+                        <form key={index} style={FormValue}>
+                            <input value={value} style={OtpCard}></input>
+                        </form>
+                    )
+                })}
+            </OtpContainer>
         </>
     )
 }

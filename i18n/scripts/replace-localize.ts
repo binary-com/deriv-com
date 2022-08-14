6283bc5343033532bba46f1161409b35844e8474
Mohsen Hajibeigloo <mohsen@re-work.dev>
2022-08-14 16:04:58 +0430

    chore: updated prefix/postfix translation

--- i18n/scripts/replace-localize.ts
+++ i18n/scripts/replace-localize.ts
@@ -1,6 +1,6 @@
 import { FindAndReplaceGlobPatterns } from '../utils'
 
 const regex = new RegExp(/(localize)(\()([\s\t\n]*)(['"])(.*)(['"])(,?)([\s\t\n]*)(\))/g)
-const replacePattern = '$1$2$3$4__translate__$5__translate__$6$7$8$9'
+const replacePattern = '$1$2$3$4_t_$5_t_$6$7$8$9'
 
 FindAndReplaceGlobPatterns(regex, replacePattern)
--- i18n/scripts/replace-translate-text.ts
+++ i18n/scripts/replace-translate-text.ts
@@ -1,6 +1,6 @@
 import { FindAndReplaceGlobPatterns } from '../utils'
 
 const regex = new RegExp(/translate_text=(['"])(.*?)(['"])/g)
-const replacePattern = 'translate_text=$1__translate__$2__translate__$3'
+const replacePattern = 'translate_text=$1_t_$2_t_$3'
 
 FindAndReplaceGlobPatterns(regex, replacePattern)

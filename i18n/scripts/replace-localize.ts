d97332fe38f0615eefe78358e455e69e0db72df8
Mohsen Hajibeigloo <mohsen@re-work.dev>
2022-08-16 16:45:48 +0430

    chore: updated the regexs for replace

--- i18n/scripts/replace-localize.ts
+++ i18n/scripts/replace-localize.ts
@@ -1,6 +1,9 @@
 import { FindAndReplaceGlobPatterns } from '../utils'
 
-const regex = new RegExp(/(localize)(\()([\s\t\n]*)(['"])([^)]+)(['"])(,?)([\s\t\n]*)(\))/g)
-const replacePattern = '$1$2$3$4_t_$5_t_$6$7$8$9'
+// const regex = new RegExp(/(localize)(\()([\s\t\n]*)(['"])([^)]+)(['"])(,?)([\s\t\n]*)(\))/g)
+const regex = new RegExp(/(localize)(\()(\s*?)(['"])(\s*)(.*?)(\s*)(\4)(,?)(\s*)(\))/g)
+
+// const replacePattern = '$1$2$3$4_t_$5_t_$6$7$8$9'
+const replacePattern = '$1$2$3$4_t_$5$6$7_t_$8$9$10$11'
 
 FindAndReplaceGlobPatterns(regex, replacePattern)
--- i18n/scripts/replace-translate-text.ts
+++ i18n/scripts/replace-translate-text.ts
@@ -1,6 +1,6 @@
 import { FindAndReplaceGlobPatterns } from '../utils'
 
-const regex = new RegExp(/translate_text=(['"])(.*)(['"])/g)
+const regex = new RegExp(/translate_text=(['"])(.*?)(\1)/gm)
 const replacePattern = 'translate_text=$1_t_$2_t_$3'
 
 FindAndReplaceGlobPatterns(regex, replacePattern)

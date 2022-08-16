47600465e79036d070ed1fce5aa3297c237bb492
aswathy-deriv <aswathy@deriv.com>
2022-08-16 15:33:29 +0400

    fix: review comments

--- src/pages/signup-affiliates/components/_phone_number.tsx
+++ src/pages/signup-affiliates/components/_phone_number.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import OtpVerification from 'images/svg/signup-affiliates/code.svg'
 import { Header } from 'components/elements'
 import { localize } from 'components/localization'
-import OtpVerification from 'images/svg/signup-affiliates/code 2.svg'
 import { Input, Button } from 'components/form'
 
 const PhoneNumber = () => {
     const [phone, setPhone] = useState('')
     const [phonecode, setPhoneCode] = useState('44')
-    const handleInputNameChange = (e) => {
+
+    const handleInputPhoneNumberChange = (e) => {
         const { value } = e.target
         setPhone(value)
     }
@@ -41,6 +42,7 @@ const PhoneNumber = () => {
         border-radius: 0;
         width: 240px;
     `
+
     const CountryCode = styled.div`
         font-weight: 400;
         font-size: 14px;
@@ -50,19 +52,21 @@ const PhoneNumber = () => {
     const CodeText = styled(Header)`
         padding-right: 95px;
     `
-    const OtpCard = {
-        height: '40px',
-        width: '40px',
-        margin: 'auto',
-        display: 'flex',
-        textAlign: 'center' as const,
-        color: 'var(--color-grey-7)',
-        border: '1px solid var(--color-grey-7)',
-    }
-    const OtpContainer = styled.div`
+    const OtpCard = styled.input`
+        height: 40px;
+        width: 40px;
+        display: flex;
+        text-align: center;
+        color: var(--color-grey-7);
+        border: 1px solid var(--color-grey-7);
+        margin-left: 15px;
+    `
+
+    const OtpContainer = styled.form`
         display: flex;
         flex-direction: row;
         justify-content: center;
+        padding-top: 15px;
     `
     const FormValue = {
         padding: '10px',
@@ -99,7 +103,7 @@ const PhoneNumber = () => {
                     required
                     id="phone"
                     value={phone}
-                    onChange={handleInputNameChange}
+                    onChange={handleInputPhoneNumberChange}
                     height="40px"
                     autoFocus={true}
                     autoComplete="off"
@@ -113,13 +117,10 @@ const PhoneNumber = () => {
             <CodeText align="center" color="grey" type="paragraph-2" weight="400">
                 {localize('Didn’t receive your code? Try again in 59 seconds')}
             </CodeText>
+
             <OtpContainer>
                 {OtpNumbers.map(({ value }, index) => {
-                    return (
-                        <form key={index} style={FormValue}>
-                            <input value={value} style={OtpCard}></input>
-                        </form>
-                    )
+                    return <OtpCard key={`option-number-${index}`} value={value}></OtpCard>
                 })}
             </OtpContainer>
         </>

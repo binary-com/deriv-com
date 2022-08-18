5f1fe393f81cc5c4654b659030555851ac4cb208
Mohammad Hashemi <mohammad.hashemi@re-work.dev>
2022-08-18 18:00:46 +0430

    fix: hidden nav while we open each blog page

--- src/components/hooks/use-scroll.tsx
+++ src/components/hooks/use-scroll.tsx
@@ -1,27 +1,23 @@
 import { useState, useEffect } from 'react'
-import { isBrowser } from 'common/utility'
 
 export const useScroll = () => {
-    const offset = isBrowser() && document.body.getBoundingClientRect()
+    const [scroll_y, setScrollY] = useState(0)
     const [last_scroll_top, setLastScrollTop] = useState(0)
-    const [body_offset, setBodyOffset] = useState(offset)
-    const [scroll_y, setScrollY] = useState(body_offset.top)
     const [scroll_direction, setScrollDirection] = useState('')
 
-    const listener = () => {
-        const offset_top = -body_offset.top
-        setBodyOffset(document.body.getBoundingClientRect())
-        setScrollY(offset_top)
-        setScrollDirection(last_scroll_top > offset_top ? 'up' : 'down')
-        setLastScrollTop(offset_top)
-    }
-
     useEffect(() => {
+        const listener = () => {
+            const { scrollY } = window
+            setScrollY(scrollY)
+            setScrollDirection(last_scroll_top > scrollY ? 'up' : 'down')
+            setLastScrollTop(scrollY)
+        }
+
         window.addEventListener('scroll', listener)
         return () => {
             window.removeEventListener('scroll', listener)
         }
-    })
+    }, [last_scroll_top])
 
     return {
         scroll_y,

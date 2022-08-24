3d66ddea8a25dc9726316116e3fb28b026c1764b
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2022-08-24 10:42:26 +0200

    style: fix title

--- src/pages/partners/_about-deriv.js
+++ src/pages/partners/_about-deriv.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { graphql, useStaticQuery } from 'gatsby'
-import { Container, Flex } from 'components/containers'
+import { Container, Flex, Desktop, Mobile } from 'components/containers'
 import { Header, Text, QueryImage } from 'components/elements'
 import { localize } from 'components/localization'
 import device from 'themes/device'
@@ -29,15 +29,22 @@ const AboutDeriv = () => {
     const data = useStaticQuery(query)
     return (
         <Container direction="column">
-            <Header mt="4rem" align="center" mb="4rem" as="h2" type="page-title">
-                {localize('About Deriv')}
-            </Header>
+            <Mobile>
+                <Header mt="4rem" align="center" mb="4rem" as="h2" type="page-title">
+                    {localize('About Deriv')}
+                </Header>
+            </Mobile>
             <WrapContainer mb="8rem">
                 <ImageWrapper>
                     <QueryImage data={data.deriv_platform} alt={'Deriv.com on laptop and mobile'} />
                 </ImageWrapper>
 
                 <Flex direction="column" ml="2.4rem" max_width="69rem">
+                    <Desktop>
+                        <Header mt="4rem" mb="4rem" as="h2" type="page-title">
+                            {localize('About Deriv')}
+                        </Header>
+                    </Desktop>
                     <Text size="var(--text-size-m)" mb="2rem">
                         {localize(
                             'Deriv is the next step in the evolution of Binary.com, the company with a proven record of delivering market-leading products that are trusted around the world.',

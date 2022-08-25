c43bf897510ba57746eb386b2a7ec692d52f9f38
aswathy-deriv <aswathy@deriv.com>
2022-08-25 13:07:26 +0400

    fix: account details validation

--- src/pages/signup-affiliates/components/_account-details.tsx
+++ src/pages/signup-affiliates/components/_account-details.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import validation from '../validations/account-details'
 import { DropdownSearch } from 'components/elements'
 import { Input } from 'components/form'
 import { localize } from 'components/localization'
@@ -41,7 +42,18 @@ type countryType = {
 }
 const AccountDetails = () => {
     const [residence_list, setResidenceList] = useState([])
-    const [value, setValue] = useState('')
+    const [country, setCountry] = useState('')
+    const [state, setState] = useState('')
+    const [city, setCity] = useState('')
+    const [street, setStreet] = useState('')
+    const [postcode, setPostCode] = useState('')
+    const [country_error_msg, setCountryErrorMsg] = React.useState('')
+    const [state_error_msg, setStateErrorMsg] = React.useState('')
+    const [city_error_msg, setCityErrorMsg] = React.useState('')
+    const [street_error_msg, setStreetErrorMsg] = React.useState('')
+    const [postcode_error_msg, setPostCodeErrorMsg] = React.useState('')
+    const [touch, setTouch] = useState()
+    const [name, setName] = useState('')
     const { send } = useDerivWS()
 
     useEffect(() => {
@@ -68,15 +80,19 @@ const AccountDetails = () => {
             label: localize('Country of residence'),
             placeholder: 'Country of residence',
             required: true,
+            error: country_error_msg,
+            value: country,
             list: residence_list,
         },
         {
             id: 'dm-state',
             name: 'state',
             type: 'text',
+            error: state_error_msg,
             label: localize('State/province'),
             placeholder: 'State/province',
             required: true,
+            value: state,
         },
         {
             id: 'dm-town',
@@ -85,6 +101,8 @@ const AccountDetails = () => {
             label: localize('Town/city'),
             placeholder: 'Town/city',
             required: true,
+            error: city_error_msg,
+            value: city,
         },
         {
             id: 'dm-street',
@@ -93,6 +111,8 @@ const AccountDetails = () => {
             label: localize('Street'),
             placeholder: 'Street',
             required: true,
+            error: street_error_msg,
+            value: street,
         },
         {
             id: 'dm-postal-code',
@@ -101,8 +121,55 @@ const AccountDetails = () => {
             label: localize('Postal/Zip code'),
             placeholder: 'Postal/Zip code',
             required: true,
+            error: postcode_error_msg,
+            value: postcode,
         },
     ]
+    const handleValidation = (e) => {
+        const { name, value } = e.target
+        switch (name) {
+            case 'country': {
+                return setCountryErrorMsg(validateCountry(value))
+            }
+            case 'state': {
+                setState(value)
+                return setStateErrorMsg(validateState(value))
+            }
+            case 'town': {
+                setCity(value)
+                return setCityErrorMsg(validateCity(value))
+            }
+            case 'street': {
+                setStreet(value)
+                return setStreetErrorMsg(validateStreet(value))
+            }
+            case 'postal_code': {
+                setPostCode(value)
+                return setPostCodeErrorMsg(validatePostCode(value))
+            }
+        }
+    }
+
+    const validateCountry = (country_str) => {
+        const error_message = validation.country(country_str)
+        return error_message
+    }
+    const validateState = (state_str) => {
+        const error_message = validation.address_state(state_str)
+        return error_message
+    }
+    const validateCity = (state_str) => {
+        const error_message = validation.address_city(state_str)
+        return error_message
+    }
+    const validateStreet = (state_str) => {
+        const error_message = validation.address_street(state_str)
+        return error_message
+    }
+    const validatePostCode = (state_str) => {
+        const error_message = validation.address_postcode(state_str)
+        return error_message
+    }
 
     return (
         <InputGroup>
@@ -115,9 +182,10 @@ const AccountDetails = () => {
                                     id={item.id}
                                     label_position={0.8}
                                     key={index}
-                                    selected_item={value}
-                                    onChange={(value) => setValue(value.name)}
+                                    selected_item={country}
+                                    onChange={(value) => setCountry(value.name)}
                                     default_item={''}
+                                    error={item.error}
                                     items={item.list}
                                     type={item.type}
                                     label={localize('Country of residence')}
@@ -129,12 +197,25 @@ const AccountDetails = () => {
                             <Input
                                 width={500}
                                 id={item.id}
+                                name={item.name}
                                 key={index}
                                 type={item.type}
-                                label_focus_color="black"
+                                value={item.value}
+                                error={item.error}
+                                border="solid 1px var(--color-grey-7)"
+                                label_color="grey-5"
+                                label_hover_color="grey-5"
                                 background="white"
                                 label={localize(item.label)}
                                 placeholder={item.placeholder}
+                                onChange={handleValidation}
+                                autoComplete="off"
+                                handleError={(current_input) => {
+                                    setState('')
+                                    setStreet('')
+                                    console.log('current_input', current_input)
+                                }}
+                                required={item.required}
                             />
                         )
                     }

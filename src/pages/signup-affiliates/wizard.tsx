c581536b302eebdc74877152f85bd422b6ead064
Aswathy <96725617+aswathy-deriv@users.noreply.github.com>
2022-08-26 12:41:17 +0400

    Aswathy/Account type validation (#3510)
    * feat: validation of card type
    
    * fix: removed the unnecessary value
    
    * fix: removed the set value
    
    * fix: validation for the stepper card type
    
    * fix: validation for next button enabling
    
    * fix: removed white spaces

--- src/pages/signup-affiliates/wizard.tsx
+++ src/pages/signup-affiliates/wizard.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AffiliateSignupLayout, { SignUpWrapper } from './components/_layout'
 import AccountType from './components/_account-type'
+import AccountDetails from './components/_account-details'
+import PhoneNumber from './components/_phone_number'
 import { WithIntl, localize } from 'components/localization'
 import { Wizard } from 'components/form'
 
@@ -12,15 +14,42 @@ const AffiliateSignup = () => {
         'Personal details',
         'Terms of use',
     ]
+
+    const [next_btn_enabled, setNextBtnEnabled] = useState(false)
+
+    const [affiliate_account, setAffiliateAccount] = useState({
+        account_type: -1,
+        address_details: null,
+        phone_number: null,
+        personal_details: null,
+        terms_use: null,
+    })
+
+    const updateAffiliateValues = (value) => {
+        affiliate_account.account_type = value
+        setAffiliateAccount({
+            ...affiliate_account,
+            account_type: value,
+        })
+    }
+
     return (
         <AffiliateSignupLayout>
             <SignUpWrapper>
                 <Wizard
-                    title={localize('Create partner account')}
+                    title={localize('Add an affiliate account')}
                     steps_names={Steps}
-                    enable_next_button={true}
+                    enable_next_button={next_btn_enabled}
                 >
-                    <AccountType />
+                    <AccountType
+                        cardSelected={affiliate_account.account_type}
+                        updateData={updateAffiliateValues}
+                        onValidate={(valid) => {
+                            setNextBtnEnabled(valid)
+                        }}
+                    />
+                    <AccountDetails />
+                    <PhoneNumber />
                 </Wizard>
             </SignUpWrapper>
         </AffiliateSignupLayout>

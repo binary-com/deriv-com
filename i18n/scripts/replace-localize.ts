99fc0cf51b8a15eb56e7cbfd0ecee5c17f578c39
Mohsen Hajibeigloo <mohsen@re-work.dev>
2022-08-28 19:22:10 +0430

    chore: code clean up

--- i18n/scripts/replace-localize.ts
+++ i18n/scripts/replace-localize.ts
@@ -1,9 +1,7 @@
 import { FindAndReplaceGlobPatterns } from '../utils'
 
-// const regex = new RegExp(/(localize)(\()([\s\t\n]*)(['"])([^)]+)(['"])(,?)([\s\t\n]*)(\))/g)
 const regex = new RegExp(/(localize)(\()(\s*?)(['`"])(\s*)(.*?)(\s*)(\4)(,?)(\s*)(\))/g)
 
-// const replacePattern = '$1$2$3$4_t_$5_t_$6$7$8$9'
 const replacePattern = '$1$2$3$4_t_$5$6$7_t_$8$9$10$11'
 
 FindAndReplaceGlobPatterns(regex, replacePattern)

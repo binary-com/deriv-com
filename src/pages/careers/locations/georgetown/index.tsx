4663972fb2f17802282ba7644a322283bd0c6f1b
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2022-08-30 09:08:47 +0200

    chore: fix image quality

--- src/pages/careers/locations/georgetown/index.tsx
+++ src/pages/careers/locations/georgetown/index.tsx
@@ -9,7 +9,14 @@ import { localize, WithIntl } from 'components/localization'
 const query = graphql`
     query {
         georgetown: file(relativePath: { eq: "careers/georgetown.png" }) {
-            ...heroImage
+            childImageSharp {
+                gatsbyImageData(
+                    formats: [AUTO, WEBP]
+                    layout: CONSTRAINED
+                    quality: 100
+                    placeholder: NONE
+                )
+            }
         }
         living_in_georgetown: file(relativePath: { eq: "careers/living_in_georgetown.png" }) {
             ...fadeIn

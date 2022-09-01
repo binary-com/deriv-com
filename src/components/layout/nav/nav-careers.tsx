7ae18b6977dbc900fcc40a21a52b57781d0e48ff
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2022-09-01 09:36:12 +0200

    chore: remove message

--- src/components/layout/nav/nav-careers.tsx
+++ src/components/layout/nav/nav-careers.tsx
@@ -176,7 +176,6 @@ const NavCareers = ({ is_besquare }: NavCareersProps) => {
                     <RightSection jc="flex-end" ai="center">
                         {has_mounted && (
                             <StyledLinkButton
-                                external="true"
                                 secondary
                                 rel="noopener noreferrer"
                                 ml="2.4rem"

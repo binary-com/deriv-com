70bbec1d4ad083de4894fed429d78c07f9a58807
Mohsen Hajibeigloo <mohsen@re-work.dev>
2022-09-06 11:49:13 +0430

    chore: updated footer with mixins

--- src/components/layout/footer/common/style.js
+++ src/components/layout/footer/common/style.js
@@ -3,49 +3,54 @@ import { Container, CssGrid, Flex } from 'components/containers'
 import { StyledLink, Text } from 'components/elements'
 import { LocalizedLink } from 'components/localization'
 import device from 'themes/device'
+import { SizeMixin, MarginMixin, PaddingMixin, BorderMixin } from 'themes/mixins'
 
 export const DefaultFooter = styled.footer`
     background-color: var(--color-grey-25);
-    inline-size: 100%;
-    margin-block: 0;
-    margin-inline: auto;
+    ${SizeMixin({ width: '100%' })}
+    ${(props) =>
+        MarginMixin({
+            top: '0',
+            start: 'auto',
+            end: 'auto',
+            bottom: props.is_eu_country && '7.3rem',
+        })}
     -webkit-tap-highlight-color: transparent;
     user-select: none;
-    margin-block-end: ${(props) => props.is_eu_country && '7.3rem'};
-    padding-block-end: 1.6rem;
+    ${PaddingMixin({ bottom: '1.6rem' })}
 
     @media (max-width: 1090px) {
-        margin-block-end: ${(props) => props.is_eu_country && '9rem'};
+        ${(props) => MarginMixin({ bottom: props.is_eu_country && '9rem' })}
     }
     @media (max-width: 991px) {
-        margin-block-end: ${(props) => props.is_eu_country && '11rem'};
+        ${(props) => MarginMixin({ bottom: props.is_eu_country && '11rem' })}
     }
     @media (max-width: 826px) {
-        margin-block-end: ${(props) => props.is_eu_country && '12.2rem'};
+        ${(props) => MarginMixin({ bottom: props.is_eu_country && '12.2rem' })}
     }
     @media (max-width: 710px) {
-        margin-block-end: ${(props) => props.is_eu_country && '10.6rem'};
+        ${(props) => MarginMixin({ bottom: props.is_eu_country && '10.6rem' })}
     }
     @media (max-width: 538px) {
-        margin-block-end: ${(props) => props.is_eu_country && '13.8rem'};
+        ${(props) => MarginMixin({ bottom: props.is_eu_country && '13.8rem' })}
     }
 
     ${Container} {
         @media ${device.tabletL} {
-            inline-size: 100%;
+            ${SizeMixin({ width: '100%' })}
         }
     }
 
     @media ${device.mobileL} {
-        padding-block-end: 6rem;
+        ${PaddingMixin({ bottom: '6rem' })}
 
         @media ${device.tabletL} {
-            padding-block-end: 0;
+            ${PaddingMixin({ bottom: '0' })}
         }
     }
 `
 export const FooterGrid = styled(CssGrid)`
-    inline-size: 100%;
+    ${SizeMixin({ width: '100%' })}
     grid-template-columns: 2fr;
     grid-template-areas:
         'logo logo'
@@ -71,42 +76,38 @@ export const DerivLogoWrapper = styled.div`
     align-items: center;
     grid-area: logo;
     background: var(--color-grey-25);
-    padding-block-start: 4rem;
-    padding-block-end: 2rem;
-    padding-inline: 0;
+    ${PaddingMixin({ top: '4rem', bottom: '2rem', start: '0', end: '0' })}
 
     @media ${device.tabletL} {
-        margin-inline-start: 2rem;
+        ${MarginMixin({ start: '2rem' })}
     }
 `
 export const LinksWrapper = styled.div`
     grid-area: links;
     background: var(--color-grey-25);
-    padding-block-start: 0.8rem;
-    padding-block-end: 2.4rem;
-    padding-inline: 0;
-    border-block-end: 1px solid var(--color-grey-26);
-    border-block-start: 2px solid var(--color-grey-26);
+    ${PaddingMixin({ top: '0.8rem', bottom: '2.4rem', start: '0', end: '0' })}
+    ${BorderMixin({
+        top: '2px solid var(--color-grey-26)',
+        bottom: '1px solid var(--color-grey-26)',
+    })}
 
     @media ${device.tabletL} {
-        padding-inline: 0;
-        padding: 0;
+        ${PaddingMixin({ all: '0' })}
     }
 `
 
 export const LinksCol = styled(Flex)`
     flex-direction: column;
-    inline-size: fit-content;
-    min-inline-size: 100px;
-    margin-inline-end: 40px;
 
     :last-child {
-        margin-inline-end: 0;
+        ${MarginMixin({ end: '0' })}
     }
 
+    ${SizeMixin({ width: 'fit-content', min_width: '100px' })}
+    ${MarginMixin({ end: '40px' })}
+
     @media (min-width: 993px) and (max-width: 1050px) {
-        min-inline-size: 0;
-        max-inline-size: 100px;
+        ${SizeMixin({ min_width: '0', max_width: '100px' })}
     }
 `
 
@@ -123,11 +124,12 @@ export const AcademyWrapper = styled.div`
     color: var(--color-red-1);
     font-size: var(--text-size-xs);
     line-height: 1.5;
-    margin-block-start: 10px;
+    ${MarginMixin({ top: '10px' })}
 `
 
 export const LinkWrapper = styled.div`
     margin-block-start: 1.6rem;
+    ${MarginMixin({ top: '1.6rem' })}
 
     @media ${device.laptopM} {
         ${Title} {
@@ -139,7 +141,7 @@ export const LinkWrapper = styled.div`
     }
 
     :nth-child(2) {
-        margin-block-start: 0.8rem;
+        ${MarginMixin({ top: '0.8rem' })}
     }
 `
 export const DisclaimerWrapper = styled.div`
@@ -148,13 +150,11 @@ export const DisclaimerWrapper = styled.div`
 `
 export const DisclaimerParagraph = styled(Text)`
     font-size: var(--text-size-xs);
-    margin-block-start: ${(props) => (props.no_margin ? '0' : '2rem')};
+    ${(props) => MarginMixin({ top: props.no_margin ? '0' : '2rem' })}
 
     @media ${device.tabletL} {
-        width: 90%;
-        margin-block-start: 16px;
-        margin-block-end: 0;
-        margin-inline: 0;
+        ${SizeMixin({ width: '90%' })}
+        ${MarginMixin({ top: '16px', bottom: '0', start: '0', end: '0' })}
         font-size: 12px;
         line-height: ${(props) => (props.line_height ? '21px' : '18px')};
     }
@@ -180,26 +180,19 @@ export const StaticAssetLink = styled(LocalizedLink)`
 `
 export const RiskWarning = styled.div`
     background-color: var(--color-grey-28);
-    border-inline-start: 4px solid var(--color-grey-27);
-    padding-inline: 1.6rem;
-    padding-block: 1.6rem;
-    margin-block-start: 2rem;
+    ${BorderMixin({ start: '4px solid var(--color-grey-27)' })}
+    ${PaddingMixin({ all: '1.6rem 1.6rem' })}
+    ${MarginMixin({ top: '2rem' })}
 
     @media ${device.tabletL} {
-        border-block-start: 2px solid var(--color-grey-27);
-        border-left: none;
-        inline-size: 90%;
-        margin-block-start: 24px;
-        margin-block-end: 0;
-        margin-inline: 16px;
-        padding-block-start: 0;
-        padding-block-end: 16px;
-        padding-inline: 8px;
+        ${BorderMixin({ top: '2px solid var(--color-grey-27)', left: 'none' })}
+        ${SizeMixin({ width: '90%' })}
+        ${MarginMixin({ top: '24px', bottom: '0', start: '16px', end: '16px' })}
+        ${PaddingMixin({ top: '0', bottom: '16px', start: '8px', end: '8px' })}
 
         p {
-            padding-inline: 0;
-            padding-block: 0;
-            inline-size: 100%;
+            ${PaddingMixin({ all: '0' })}
+            ${SizeMixin({ width: '100%' })}
         }
     }
 `
@@ -221,16 +214,13 @@ export const CopyrightWrapper = styled(Flex)`
     p {
         font-size: var(--text-size-xs);
         line-height: 1.14;
-        padding-block: 2rem;
-        padding-inline: 0;
+        ${PaddingMixin({ all: '2rem 0' })}
     }
 
     @media ${device.tabletL} {
-        inline-size: 90%;
-        margin-block: 0;
-        margin-inline: auto;
-        padding-block: 2rem;
-        padding-inline: 0;
+        ${SizeMixin({ width: '90%' })}
+        ${MarginMixin({ all: '0 auto' })}
+        ${PaddingMixin({ all: '2rem 0' })}
         justify-content: center;
 
         p {
@@ -245,31 +235,28 @@ export const EuLogoWrapper = styled(Flex)`
 export const SocialWrapper = styled.div`
     grid-area: social;
     background: var(--color-grey-25);
-    margin-block: 1.6rem;
-    margin-inline: 0;
+    ${MarginMixin({ all: '1.6rem 0' })}
 
     img {
-        margin-inline-start: 1.6rem;
+        ${MarginMixin({ start: '1.6rem' })}
     }
 
     @media ${device.tabletL} {
         display: flex;
         justify-content: center;
-        margin-block-start: 3rem;
-        margin-block-end: 1rem;
-        margin-inline: 0;
+        ${MarginMixin({ top: '3rem', bottom: '1rem', start: '0', end: '0' })}
 
         a:first-child {
             img {
-                margin-inline-start: 0;
+                ${MarginMixin({ start: '0' })}
             }
         }
     }
 `
 
 export const StyledMgaLogo = styled.img`
-    margin-inline-end: 2.4rem;
+    ${MarginMixin({ end: '2.4rem' })}
 `
 export const StyledLogo = styled.img`
-    inline-size: 18.2rem;
+    ${SizeMixin({ width: '18.2rem' })}
 `

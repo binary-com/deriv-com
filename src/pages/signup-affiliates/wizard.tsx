94e5715cab92254358204f2f12178a1125e9c46c
NikitK-deriv <nikita.kolesnik@deriv.com>
2022-09-09 12:02:34 +0300

    feat: personal details in work

--- src/pages/signup-affiliates/wizard.tsx
+++ src/pages/signup-affiliates/wizard.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import AffiliateSignupLayout, { SignUpWrapper } from './components/_layout'
 import AccountType from './components/_account-type'
-import AccountDetails from './components/_account-details'
+import AccountDetails from './components/_adress-details'
 import PhoneNumber from './components/_phone_number'
 import AccountTerms from './components/_account-terms'
+import PersonalDetails from './components/_account-personal-details'
 import { WithIntl, localize } from 'components/localization'
 import { Wizard } from 'components/form'
 
@@ -27,18 +28,35 @@ const AffiliateSignup = () => {
             street: '',
             postal_code: '',
         },
-        phone_number: null,
-        personal_details: null,
+        phone_number: {
+            phone: null,
+            prefix: '44',
+        },
+        personal_details: {
+            first_name: '',
+            last_name: '',
+            date_birth: '',
+            social_media_url: '',
+            website_url: '',
+            password: '',
+            company_name: '',
+            company_registration_number: '',
+            certificate: '',
+            citizen: null,
+        },
         terms_use: null,
     })
 
     const updateAffiliateValues = (value, type) => {
+        console.log(value)
+
         switch (type) {
             case 'account-type':
+                setNextBtnEnabled(false)
                 setAffiliateAccount({ ...affiliate_account, account_type: value })
                 break
 
-            case 'account-details':
+            case 'address-details':
                 setNextBtnEnabled(false)
                 setAffiliateAccount({
                     ...affiliate_account,
@@ -48,6 +66,35 @@ const AffiliateSignup = () => {
                         city: value.city,
                         street: value.street,
                         postal_code: value.postal_code,
+                    },
+                })
+                break
+            case 'phone-number':
+                setNextBtnEnabled(false)
+                setAffiliateAccount({
+                    ...affiliate_account,
+                    phone_number: {
+                        phone: value.phone,
+                        prefix: value.prefix,
+                    },
+                })
+                break
+
+            case 'personal-details':
+                setNextBtnEnabled(false)
+                setAffiliateAccount({
+                    ...affiliate_account,
+                    personal_details: {
+                        first_name: value.first_name,
+                        last_name: value.last_name,
+                        date_birth: value.date_birth,
+                        social_media_url: value.social_media_url,
+                        website_url: value.website_url,
+                        password: value.password,
+                        company_name: value.company_name,
+                        company_registration_number: value.company_registration_number,
+                        certificate: value.certificate,
+                        citizen: value.citizen,
                     },
                 })
                 break
@@ -61,6 +108,7 @@ const AffiliateSignup = () => {
                     title={localize('Add an affiliate account')}
                     steps_names={Steps}
                     enable_next_button={next_btn_enabled}
+                    show
                 >
                     <AccountType
                         cardSelected={affiliate_account.account_type}
@@ -74,13 +122,31 @@ const AffiliateSignup = () => {
                     <AccountDetails
                         affiliate_address_data={affiliate_account.address_details}
                         updatedData={(value) => {
-                            updateAffiliateValues(value, 'account-details')
+                            updateAffiliateValues(value, 'address-details')
                         }}
                         onValidate={(valid) => {
                             setNextBtnEnabled(valid)
                         }}
                     />
-                    <PhoneNumber />
+                    <PhoneNumber
+                        affiliate_phone_number={affiliate_account.phone_number}
+                        updatedData={(value) => {
+                            updateAffiliateValues(value, 'phone-number')
+                        }}
+                        onValidate={(valid) => {
+                            setNextBtnEnabled(valid)
+                        }}
+                    />
+                    <PersonalDetails
+                        affiliate_personal_data={affiliate_account.personal_details}
+                        is_individual={affiliate_account.account_type === 0 ? true : false}
+                        onValidate={(valid) => {
+                            setNextBtnEnabled(valid)
+                        }}
+                        updatedData={(value) => {
+                            updateAffiliateValues(value, 'personal-details')
+                        }}
+                    />
                     <AccountTerms />
                 </Wizard>
             </SignUpWrapper>

bd145915e80d5e6e391e238b7793e4e6beeda5d7
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2022-09-21 10:47:30 +0200

    chore: fix video

--- src/pages/p2p/components/_dp2p.tsx
+++ src/pages/p2p/components/_dp2p.tsx
@@ -163,7 +163,7 @@ const DP2P = ({ P2P, reverse }: DP2PProps) => {
 
                 <VideoText>{localize('Find out how Deriv P2P works')}</VideoText>
                 <StyledIFrame
-                    src="https://www.youtube.com/embed/sITTfAZAnwU"
+                    src="https://www.youtube.com/watch?v=x8v4Hb-Uw2I"
                     title="YouTube video player"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"

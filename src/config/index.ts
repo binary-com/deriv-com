bebf15e1f190d272fa0a7669baa0a0f0bbd68f18
NikitK-deriv <nikita.kolesnik@deriv.com>
2022-09-22 17:17:35 +0300

    fix: change !

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
-export default !process.env.DIRECTUS_AUTH_TOKEN || false
+export default process.env.GATSBY_SHOW_BRANDING
 // ! need to just to return value to boolean false. If server or someone who try to run app withou DIRECTUS_AUTH_TOKEN will not see our brands at all

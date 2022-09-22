b412255454741fc2998d1f732714bd19ce9fdf38
NikitK-deriv <nikita.kolesnik@deriv.com>
2022-09-22 12:34:47 +0300

    fix: return back alphabet validtaion

--- src/pages/signup-affiliates/validations/_validations.tsx
+++ src/pages/signup-affiliates/validations/_validations.tsx
@@ -3,7 +3,7 @@ import { localize, Localize } from 'components/localization'
 /* eslint-disable */
 
 export const affiliate_validation_regex = {
-    alphabet: /^[A-Za-z]+([a-zA-Z\.' -])*[a-zA-Z\.' -]+$/,
+    alphabet: /[`~!@#$%^&*)(_=+[}{\]\\/";:?><,|\d]+/,
     phone: /^\+?[^\D]((-|\s)*\d)*$/,
     password: /^(?=.*[a-z])(?=.*\d)(?=.*[A-Z])[ -~]*$/,
     city: /[`~!@#%^&*)(_=+[}{\]\\/";:?><,|\d]+/,

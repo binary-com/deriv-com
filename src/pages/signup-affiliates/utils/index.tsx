463213e0e56ec833f81160a96a188d3cd89de8f7
NikitK-deriv <nikita.kolesnik@deriv.com>
2022-09-28 15:05:41 +0300

    chore: remove index which used for export

--- src/pages/signup-affiliates/utils/index.tsx
+++
@@ -1,6 +0,0 @@
-import Currency from './_currency-affiliates'
-import BirthPicker from './_birth-picker'
-import BirthForm from './_birth-form'
-import AffiliateInput from './_affiliate-input'
-
-export { Currency, BirthPicker, BirthForm, AffiliateInput }

1729d2f9d7398edd7db37b06a8dad89b35f829e6
mitra-deriv <mitra@regentmarkets.com>
2022-09-29 19:54:04 +0800

    fix: resolve comment

--- src/components/elements/typography.ts
+++ src/components/elements/typography.ts
@@ -103,7 +103,7 @@ export const BaseElement = css<BaseElementProps>`
     }
 `
 
-/** @deprecated Use Header instead */
+/** @deprecated Use `Header` component instead */
 
 export const Text = styled.p<BaseElementProps & ResponseDeviceProps>`
     ${BaseElement}

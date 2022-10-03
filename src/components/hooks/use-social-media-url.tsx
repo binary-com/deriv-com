4bf0a667e2c3737d1fc314133e630765cfc9d2f8
NikitK-deriv <nikita.kolesnik@deriv.com>
2022-10-03 11:29:20 +0300

    feat: movement inside hook, ts type state and return

--- src/components/hooks/use-social-media-url.tsx
+++ src/components/hooks/use-social-media-url.tsx
@@ -2,45 +2,52 @@ import { useEffect, useState } from 'react'
 import { useCountryRule } from './use-country-rule'
 import { getLanguage, getLocationPathname } from 'common/utility'
 
-export const useSocialMediaUrl = () => {
-    const [social_media, setSocialMedia] = useState({
+const social_media_urls = {
+    twitter: {
+        non_eu_url: 'https://twitter.com/derivdotcom/',
+        eu_url: 'https://www.twitter.com/deriv_eu/',
+        uk_url: 'https://www.twitter.com/deriv_uk/',
+    },
+    instagram: {
+        url_career: 'https://www.instagram.com/derivcareers/',
+        non_eu_url: 'https://www.instagram.com/deriv_official/',
+        eu_url: 'https://www.instagram.com/deriv_eu/',
+        uk_url: 'https://www.instagram.com/deriv_uk/',
+    },
+    facebook: {
+        url_career: 'https://www.facebook.com/derivcareers',
+        non_eu_url: 'https://www.facebook.com/derivdotcom',
+        eu_url: 'https://www.facebook.com/derivEU/',
+        uk_url: 'https://www.facebook.com/derivUK/',
+    },
+    linkedin: {
+        non_eu_url: 'https://www.linkedin.com/company/derivdotcom/',
+        url_career: 'https://www.linkedin.com/company/derivdotcom/life/',
+    },
+}
+const special_language_urls = {
+    es: {
+        instagram: 'https://www.instagram.com/deriv_espanol/',
+        twitter: 'https://twitter.com/DerivEspanol',
+        facebook: 'https://www.facebook.com/derivespanol',
+    },
+}
+const types = ['facebook', 'instagram', 'twitter', 'linkedin']
+
+type TSocialMediaUrl = {
+    fb_url: string
+    instagram_url: string
+    twitter_url: string
+    linkedin_url: string
+}
+
+export const useSocialMediaUrl = (): TSocialMediaUrl => {
+    const [social_media, setSocialMedia] = useState<TSocialMediaUrl>({
         fb_url: '',
         instagram_url: '',
         twitter_url: '',
         linkedin_url: '',
     })
-
-    const social_media_urls = {
-        twitter: {
-            non_eu_url: 'https://twitter.com/derivdotcom/',
-            eu_url: 'https://www.twitter.com/deriv_eu/',
-            uk_url: 'https://www.twitter.com/deriv_uk/',
-        },
-        instagram: {
-            url_career: 'https://www.instagram.com/derivcareers/',
-            non_eu_url: 'https://www.instagram.com/deriv_official/',
-            eu_url: 'https://www.instagram.com/deriv_eu/',
-            uk_url: 'https://www.instagram.com/deriv_uk/',
-        },
-        facebook: {
-            url_career: 'https://www.facebook.com/derivcareers',
-            non_eu_url: 'https://www.facebook.com/derivdotcom',
-            eu_url: 'https://www.facebook.com/derivEU/',
-            uk_url: 'https://www.facebook.com/derivUK/',
-        },
-        linkedin: {
-            non_eu_url: 'https://www.linkedin.com/company/derivdotcom/',
-            url_career: 'https://www.linkedin.com/company/derivdotcom/life/',
-        },
-    }
-    const special_language_urls = {
-        es: {
-            instagram: 'https://www.instagram.com/deriv_espanol/',
-            twitter: 'https://twitter.com/DerivEspanol',
-            facebook: 'https://www.facebook.com/derivespanol',
-        },
-    }
-    const types = ['facebook', 'instagram', 'twitter', 'linkedin']
     const { is_eu, is_uk } = useCountryRule()
     const language = getLanguage()
     const current_path = getLocationPathname()

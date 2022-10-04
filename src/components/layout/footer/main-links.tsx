0bc398522e928a0b2bec34b70d4d7d6dbac010b6
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2022-10-04 12:47:02 +0600

    Revert "chore: add derived url fix (#3699)"
    This reverts commit febff42c097843ea44f6a6da29373f9b3a6ece0d.

--- src/components/layout/footer/main-links.tsx
+++ src/components/layout/footer/main-links.tsx
@@ -94,7 +94,7 @@ const MainLinksSection = ({ is_ppc = false, is_ppc_redirect = false }: MainLinks
                         </LinkWrapper>
                         {is_non_uk && (
                             <LinkWrapper>
-                                <Link to="/markets/synthetic/">
+                                <Link to="/markets/derived-fx/">
                                     {<Localize translate_text="Derived" />}
                                 </Link>
                             </LinkWrapper>

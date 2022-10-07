f21b18d36174e8c7edae0830527e779065f6040a
Prince <prince@deriv.com>
2022-10-07 09:08:11 +0400

    chore: remove unnecessary changes

--- src/common/login.ts
+++ src/common/login.ts
@@ -36,9 +36,7 @@ const Login = (() => {
 
     const initOneAll = (provider) => {
         const social_login_url = `${loginUrl()}&social_signup=${provider}`
-        console.log(social_login_url)
 
-        return false
         window.location.href = social_login_url
     }
 

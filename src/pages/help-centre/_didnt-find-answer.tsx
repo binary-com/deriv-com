863110cdb383acd2cdb8f315dbac32c6f38b3f8e
mitra-deriv <mitra@regentmarkets.com>
2022-10-12 10:10:07 +0800

    fix: design mismatch in didn't find your answer section

--- src/pages/help-centre/_didnt-find-answer.tsx
+++ src/pages/help-centre/_didnt-find-answer.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 import { Text } from 'components/elements'
 import { Button } from 'components/form'
 import { localize } from 'components/localization'
-import { Container, Flex } from 'components/containers'
 import { useLivechat } from 'components/hooks/use-livechat'
 import device from 'themes/device'
 import ContactUsIcon from 'images/svg/help/livechat-red.svg'
@@ -11,22 +10,16 @@ import WhatsAppSVG from 'images/svg/help/whatsapp.svg'
 import { useCountryRule } from 'components/hooks/use-country-rule'
 import { whatsapp_url } from 'common/constants'
 
-const DFYAWrapper = styled.section`
-    background-color: var(--color-black);
-`
-const DFYASection = styled(Container)`
+const DidntFindYourAnswerWrapper = styled.div`
+    background: var(--color-black);
+    display: flex;
+    height: 100%;
+    justify-content: center;
     padding: 3.5rem 0;
+
     @media ${device.tablet} {
-        flex-wrap: wrap;
-
-        ${Button} {
-            font-size: 14px;
-            padding: 10px 16px;
-        }
-    }
-
-    & > * {
-        width: auto;
+        flex-direction: column;
+        align-items: center;
     }
 `
 const StyledIcon = styled.img`
@@ -47,12 +40,13 @@ const WhatsAppButton = styled.button`
     background-color: var(--color-green-3);
     border: 2px solid var(--color-green-3);
     color: var(--color-white);
-    font-size: var(--text-size-xs);
+    font-size: 14px;
     border-radius: 4px;
-    height: auto;
+    height: fit-content;
     padding: 10px 16px;
     font-weight: bold;
     margin-left: 16px;
+    width: inherit;
 
     &:hover {
         background-color: var(--color-red);
@@ -65,41 +59,48 @@ const WhatsAppIcon = styled.img`
     margin: -3px 8px -3px 0;
 `
 
+const ButtonWrapper = styled.div`
+    @media ${device.tablet} {
+        display: flex;
+        width: 100%;
+        padding: 16px;
+    }
+`
+
 export const DidntFindYourAnswerBanner = () => {
     const [is_livechat_interactive, LC_API] = useLivechat()
     const { is_south_africa, is_nigeria } = useCountryRule()
     return (
-        <DFYAWrapper>
-            <DFYASection>
-                <StyledIcon src={ContactUsIcon} alt="contact us icon" />
-                <MiddleText size="var(--text-size-l)" color="white" m="0 2.4rem">
-                    {localize('Didn’t find your answer? We can help.')}
-                </MiddleText>
-                {is_livechat_interactive && (
-                    <Flex>
-                        <Button
-                            secondary
-                            onClick={() => {
-                                LC_API.open_chat_window()
-                            }}
-                        >
-                            {localize('Chat')}
-                        </Button>
-                        {(is_south_africa || is_nigeria) && (
-                            <WhatsAppButton onClick={() => window.open(whatsapp_url, '_blank')}>
-                                <WhatsAppIcon
-                                    src={WhatsAppSVG}
-                                    alt={localize('whatsappicon')}
-                                    height="16"
-                                    width="16"
-                                />
-                                {localize('WhatsApp')}
-                            </WhatsAppButton>
-                        )}
-                    </Flex>
-                )}
-            </DFYASection>
-        </DFYAWrapper>
+        <DidntFindYourAnswerWrapper>
+            <StyledIcon src={ContactUsIcon} alt="contact us icon" />
+            <MiddleText size="var(--text-size-l)" color="white" m="0 2.4rem">
+                {localize('Didn’t find your answer? We can help.')}
+            </MiddleText>
+            {is_livechat_interactive && (
+                <ButtonWrapper>
+                    <Button
+                        width="inherit"
+                        secondary
+                        onClick={() => {
+                            LC_API.open_chat_window()
+                        }}
+                    >
+                        {localize('Chat')}
+                    </Button>
+                    {(is_south_africa || is_nigeria) && (
+                        <WhatsAppButton onClick={() => window.open(whatsapp_url, '_blank')}>
+                            <WhatsAppIcon
+                                src={WhatsAppSVG}
+                                alt={localize('whatsappicon')}
+                                height="16"
+                                width="16"
+                            />
+                            {localize('WhatsApp')}
+                        </WhatsAppButton>
+                    )}
+                </ButtonWrapper>
+            )}
+        </DidntFindYourAnswerWrapper>
     )
 }
 

73c0bd4b4afe464379b57a069cadf8b9036e10c5
NikitK-deriv <nikita.kolesnik@deriv.com>
2022-10-31 17:18:17 +0300

    feat: replace reload page on reconnect after users activity

--- src/common/websocket/api.ts
+++ src/common/websocket/api.ts
@@ -50,26 +50,50 @@ export default class DerivWS {
     }&brand=${brand_name.toLowerCase()}`
     connection: WebSocket
 
-    constructor() {
-        const connection = new WebSocket(this.socket_url)
-        connection.onopen = this.openHandler
-        connection.onmessage = this.messageHandler
-        connection.onclose = this.reloadPage
-
-        this.connection = connection
-    }
-
-    isConnectionClosed() {
-        const closed_state = [2, 3]
-        return closed_state.includes(this.connection.readyState)
-    }
-
     private openHandler = () => {
         if (this.connection.readyState === 1) {
             this.connected.resolve()
         } else {
             setTimeout(this.openHandler, 50)
         }
+    }
+
+    private messageHandler = (msg: { data: string }) => {
+        const response: DerivAPIResponse = JSON.parse(msg.data)
+        const reqId = response.req_id
+
+        if (reqId && reqId in this.pendingRequests) {
+            this.pendingRequests[reqId].resolve(response)
+            delete this.pendingRequests[reqId]
+        }
+    }
+
+    private recursiveConnect = () => {
+        this.connection = new WebSocket(this.socket_url)
+        this.connection.onopen = this.openHandler
+        this.connection.onmessage = this.messageHandler
+        this.connection.onclose = () => {
+            let lastActionTaken = new Date().getTime()
+            const checkLastAction = () => {
+                const now = new Date().getTime()
+                if (now - lastActionTaken > 500) {
+                    this.recursiveConnect()
+                }
+                lastActionTaken = now
+            }
+            window.addEventListener('mousemove', checkLastAction)
+            window.addEventListener('touchstart', checkLastAction)
+            window.addEventListener('keydown', checkLastAction)
+        }
+    }
+
+    constructor() {
+        this.recursiveConnect()
+    }
+
+    isConnectionClosed() {
+        const closed_state = [2, 3]
+        return closed_state.includes(this.connection.readyState)
     }
 
     send(request: DerivAPIRequest) {
@@ -88,27 +112,5 @@ export default class DerivWS {
             .catch((e) => pending.reject(e))
 
         return pending.promise
-    }
-
-    private messageHandler = (msg: { data: string }) => {
-        const response: DerivAPIResponse = JSON.parse(msg.data)
-        const reqId = response.req_id
-
-        if (reqId && reqId in this.pendingRequests) {
-            this.pendingRequests[reqId].resolve(response)
-            delete this.pendingRequests[reqId]
-        }
-    }
-
-    private reloadPage = () => {
-        let lastActionTaken = new Date().getTime()
-        const checkLastAction = () => {
-            const now = new Date().getTime()
-            if (now > lastActionTaken) window.location.reload()
-            else lastActionTaken = now
-        }
-        window.addEventListener('mousemove', checkLastAction)
-        window.addEventListener('touchstart', checkLastAction)
-        window.addEventListener('keydown', checkLastAction)
     }
 }

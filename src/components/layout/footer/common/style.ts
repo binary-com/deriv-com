9142db8491e7e7aa5073c3351dbfde688ebe5b9e
mitra-deriv <mitra@regentmarkets.com>
2022-11-01 11:38:37 +0800

    chore: :recycle: replace `DerivStore` eu rule with `useCountryRule` eu rule

--- src/components/layout/footer/common/style.ts
+++ src/components/layout/footer/common/style.ts
@@ -5,7 +5,7 @@ import { LocalizedLink } from 'components/localization'
 import device from 'themes/device'
 
 type DefaultFooterProps = {
-    is_eu_country?: boolean
+    is_eu?: boolean
     has_banner_cookie?: boolean
 }
 
@@ -20,23 +20,23 @@ export const DefaultFooter = styled.footer<DefaultFooterProps>`
     margin: 0 auto;
     -webkit-tap-highlight-color: transparent;
     user-select: none;
-    margin-bottom: ${(props) => props.is_eu_country && '7.3rem'};
+    margin-bottom: ${(props) => props.is_eu && '7.3rem'};
     padding-bottom: 1.6rem;
 
     @media (max-width: 1090px) {
-        margin-bottom: ${({ is_eu_country }) => is_eu_country && '9rem'};
+        margin-bottom: ${({ is_eu }) => is_eu && '9rem'};
     }
     @media (max-width: 991px) {
-        margin-bottom: ${({ is_eu_country }) => is_eu_country && '11rem'};
+        margin-bottom: ${({ is_eu }) => is_eu && '11rem'};
     }
     @media (max-width: 826px) {
-        margin-bottom: ${({ is_eu_country }) => is_eu_country && '12.2rem'};
+        margin-bottom: ${({ is_eu }) => is_eu && '12.2rem'};
     }
     @media (max-width: 710px) {
-        margin-bottom: ${({ is_eu_country }) => is_eu_country && '10.6rem'};
+        margin-bottom: ${({ is_eu }) => is_eu && '10.6rem'};
     }
     @media (max-width: 538px) {
-        margin-bottom: ${({ is_eu_country }) => is_eu_country && '13.8rem'};
+        margin-bottom: ${({ is_eu }) => is_eu && '13.8rem'};
     }
 
     ${Container} {

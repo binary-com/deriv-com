70b3a168b4d484720e83dbcebb0b84baa9b40554
Mohsen Hajibeigloo <mohsen@re-work.dev>
2022-11-15 10:15:20 +0330

    chore: fixed shortname i18config

--- i18n-config.js
+++ i18n-config.js
@@ -88,7 +88,7 @@ module.exports = {
     ar: {
         path: 'ar',
         display_name: 'العربية',
-        short_name: 'العربية',
+        short_name: 'AR',
     },
     ko: {
         path: 'ko',

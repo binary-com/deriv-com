634dd475a22bf68fb2e1be3a5d7e7f698bf2bff5
Mohsen Hajibeigloo <mohsen@re-work.dev>
2022-11-21 18:01:44 +0330

    chore: added comment on the hook type

--- src/components/hooks/use-visibility.tsx
+++ src/components/hooks/use-visibility.tsx
@@ -2,6 +2,8 @@ import { useContext } from 'react'
 import { TUseBreakpoints } from './use-breakpoints'
 import { DerivStore } from 'store'
 
+// HINT: We will add more values to the hook in the future,
+// so for the type handling for now I'll just alias the type
 export type TVisiblity = TUseBreakpoints
 
 const useVisibility = (): TVisiblity => {

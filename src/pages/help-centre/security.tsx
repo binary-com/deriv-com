b33812ee915d0cbab772b23fbada303efccb533b
meenakshi-deriv <meenakshi@deriv.com>
2022-11-21 12:16:23 +0400

    fix: added telegram faq

--- src/pages/help-centre/security.tsx
+++ src/pages/help-centre/security.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Article, ArticleProps } from './_article'
 import { ArticleWrapper, StyledHeader, ExternalLink } from './_help-centre-style'
 import { usePageLoaded } from 'components/hooks/use-page-loaded'
-import { Text } from 'components/elements'
+import { Header, Text, Ul } from 'components/elements'
 import { localize, Localize, WithIntl } from 'components/localization'
+import device from 'themes/device'
+import TelegranDerivGroupImg from 'images/common/help-centre/help-center-telegram-one.png'
+import TelegranDerivChatImg from 'images/common/help-centre/help-center-telegram-two.png'
 
+const ImageWrapper = styled.section`
+    margin-top: 16px;
+    @media ${device.mobileL} {
+        & > img {
+            width: 110% !important;
+        }
+    }
+`
+const StyledText = styled.header`
+    font-weight: normal;
+    margin-top: 12px;
+`
 const VerifyAccount = ({ text }: ArticleProps) => (
     <ArticleWrapper>
         <StyledHeader as="h4">{text}</StyledHeader>
@@ -37,7 +53,111 @@ const DocumentsDeclined = ({ text }: ArticleProps) => (
         </Text>
     </ArticleWrapper>
 )
-
+const IdentifyImpersonatorsTelegram = ({ text }: ArticleProps) => (
+    <ArticleWrapper>
+        <StyledHeader as="h4">{text}</StyledHeader>
+        <ImageWrapper>
+            <img
+                src={TelegranDerivGroupImg}
+                alt={localize('Telegream deriv group')}
+                style={{ width: '36.4rem' }}
+                loading="lazy"
+            />
+        </ImageWrapper>
+        <Header color="grey" weight="normal">
+            {localize('Examples of groups impersonating Deriv on Telegram')}
+        </Header>
+        <StyledText>
+            {localize(
+                'An impersonator’s main goal is to steal your sensitive information and funds. Here are some ways to identify impersonators of Deriv client support on Telegram:',
+            )}
+        </StyledText>
+        <Ul>
+            <li>
+                <StyledText>
+                    {localize(
+                        'Impersonators ask for your login details or other sensitive information via Telegram or email.',
+                    )}
+                </StyledText>
+            </li>
+            <li>
+                <StyledText>
+                    {localize(
+                        'They provide rewards that sound too good to be true and demand payments via untraceable channels, for example, via cryptocurrency.',
+                    )}
+                </StyledText>
+            </li>
+            <li>
+                <StyledText>
+                    {localize(
+                        'They ask you to download an app that isn’t verified by the Android Play Store and Apple App Store.',
+                    )}
+                </StyledText>
+            </li>
+            <li>
+                <StyledText>{localize('They ask you to download')}</StyledText>
+            </li>
+            <Ul>
+                <li>
+                    <StyledText>
+                        {localize(
+                            'Software that enables your device to be controlled remotely, or',
+                        )}
+                    </StyledText>
+                </li>
+                <li>
+                    <StyledText>
+                        {localize('Files containing malware or virus that can infect your device')}
+                    </StyledText>
+                </li>
+            </Ul>
+        </Ul>
+        <StyledText>
+            {localize(
+                'Of course, this list isn’t exhaustive. Impersonators employ increasingly creative methods to try to steal your information and funds.',
+            )}
+        </StyledText>
+        <ImageWrapper>
+            <img
+                src={TelegranDerivChatImg}
+                alt={localize('Telegream deriv chat')}
+                style={{ width: '26.9rem' }}
+                loading="lazy"
+            />
+        </ImageWrapper>
+        <Header color="grey" weight="normal">
+            {localize('Examples of messages from impersonators')}
+        </Header>
+        <StyledText>
+            <Localize
+                translate_text="Please ensure to <0>join the correct Deriv group</0> on Telegram. If you have been contacted by a potential impersonator, or if you have any questions, talk to our Customer Support team <1>via live chat</1>."
+                components={[
+                    <ExternalLink
+                        to={'https://telegramchannels.me/groups/derivdotcom'}
+                        target="_blank"
+                        external
+                        weight="bold"
+                        rel="noopener noreferrer"
+                        key={0}
+                    />,
+                    <ExternalLink
+                        to={'/contact_us/?is_livechat_open=true'}
+                        target="_blank"
+                        external
+                        weight="normal"
+                        rel="noopener noreferrer"
+                        key={1}
+                    />,
+                ]}
+            />
+        </StyledText>
+        <StyledText>
+            {localize(
+                'The live chat link is available on our website as well as our trading platforms.',
+            )}
+        </StyledText>
+    </ArticleWrapper>
+)
 const DisableTwoFactorAuthentication = ({ text }: ArticleProps) => (
     <ArticleWrapper>
         <StyledHeader as="h4">{text}</StyledHeader>
@@ -99,6 +219,13 @@ const SecurityArticle = () => {
                     label="disable-two-factor-authentication"
                     is_mounted={is_mounted}
                 />
+                <IdentifyImpersonatorsTelegram
+                    text={localize(
+                        'How to identify impersonators of Deriv client support on Telegram?',
+                    )}
+                    label="identify-impersonators-telegram"
+                    is_mounted={is_mounted}
+                ></IdentifyImpersonatorsTelegram>
             </Article>
         </div>
     )

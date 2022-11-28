5184a5f38be592dadb9130d4fc6ddb7782820809
Aswathy <96725617+aswathy-deriv@users.noreply.github.com>
2022-11-28 15:16:56 +0400

    Aswathy/Timeline (#3870)
    * feat: schedule conference
    
    * fix: vercel error
    
    * fix: removed localization
    
    * fix: vercel error fix by renaming to ts file
    
    * fix: removed special character in the data
    
    * fix: rename the data file
    
    * fix: refactor: refactored the component
    
    * refactor: refactored the code
    
    * refactor: refactored header
    
    * fix: nitpicks for header font weight
    
    * fix: added common component for header title and period
    
    * fix: nitpick for changing the camelcase

---
+++ src/pages/conference/timeline/index.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react'
+import styled, { css } from 'styled-components'
+import HeroBanner from './images/calendar.png'
+import ScheduleConference from './_schedule'
+import events_data from './_data-schedule'
+import { Header } from 'components/elements'
+import Layout from 'components/layout/layout'
+import device from 'themes/device'
+import { localize } from 'components/localization'
+
+const HeroWrapper = styled.div`
+    background-image: url(${HeroBanner});
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    min-height: 72rem;
+
+    @media ${device.tabletS} {
+        min-height: 60rem;
+    }
+`
+
+const Tab = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin: 80px 0;
+`
+
+const selected = css`
+    box-shadow: 0 32px 64px rgba(14, 14, 14, 0.14);
+    border-radius: 4px 0;
+
+    ${Header} {
+        font-weight: 700;
+    }
+`
+
+const unselected = css`
+    border: 1px solid var(--color-grey-8);
+    border-radius: 0 4px;
+
+    ${Header} {
+        font-weight: 400;
+        color: var(--color-grey-5);
+    }
+`
+
+const TabItem = styled.div<{ active?: boolean }>`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 24px 40px;
+    height: 84px;
+    background: var(--color-white);
+    cursor: pointer;
+    ${(props) => (props.active ? selected : unselected)}
+`
+
+const TimeLine = () => {
+    const [tab, setTab] = useState('day1')
+    const isDay1 = tab === 'day1'
+    const isDay2 = tab === 'day2'
+    const isDay3 = tab === 'day3'
+
+    return (
+        <Layout type="conference" margin_top="0px">
+            <HeroWrapper>
+                <Header as="h1" type="heading-1" align="center" pt="280px">
+                    {localize('Schedule')}
+                </Header>
+            </HeroWrapper>
+            <Tab>
+                <TabItem active={isDay1} onClick={() => setTab('day1')}>
+                    <Header type="subtitle-1" align="center">
+                        {localize('Day 1')}
+                    </Header>
+                </TabItem>
+                <TabItem active={isDay2} onClick={() => setTab('day2')}>
+                    <Header type="subtitle-1" align="center">
+                        {localize('Day 2')}
+                    </Header>
+                </TabItem>
+                <TabItem active={isDay3} onClick={() => setTab('day3')}>
+                    <Header type="subtitle-1" align="center">
+                        {localize('Day 3')}
+                    </Header>
+                </TabItem>
+            </Tab>
+
+            <div>
+                {events_data.map((item, index) => {
+                    return (
+                        tab === `day${index + 1}` && (
+                            <ScheduleConference key={item.id} item={item} />
+                        )
+                    )
+                })}
+            </div>
+        </Layout>
+    )
+}
+
+export default TimeLine

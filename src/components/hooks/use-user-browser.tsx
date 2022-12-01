ffbadcca37ef4bde2e4ff665fc10ef05a1686d8a
iman <iman@firstsource.tech>
2022-12-01 10:43:58 +0330

    refactor: moved the whole logic inside the hook

--- src/components/hooks/use-user-browser.tsx
+++ src/components/hooks/use-user-browser.tsx
@@ -1,11 +1,32 @@
 import { useEffect, useState } from 'react'
 import { browserName, browserVersion } from 'react-device-detect'
 
+export const minimum_required_versions = {
+    Chrome: 107,
+    Firefox: 116,
+    Opera: 107,
+    Yandex: 108,
+    Safari: 109,
+    'Internet Explorer': 110,
+    Edge: 111,
+    Chromium: 112,
+    IE: 113,
+    'Mobile Safari': 114,
+    'Edge Chromium': 115,
+    'MIUI Browser': 116,
+    'Samsung Browser': 117,
+}
+
 export const useUserBrowser = () => {
     const [browser, setBrowser] = useState({
         browser_name: '',
         browser_version: 0,
     })
+
+    const is_outdated =
+        minimum_required_versions[browser.browser_name] &&
+        browser.browser_version < minimum_required_versions[browser.browser_name]
+
     useEffect(() => {
         setBrowser({
             browser_name: browserName,
@@ -13,5 +34,8 @@ export const useUserBrowser = () => {
         })
     }, [])
 
-    return browser
+    return {
+        ...browser,
+        is_outdated,
+    }
 }
--- src/components/layout/modal/update_browser_alert_modal.tsx
+++ src/components/layout/modal/update_browser_alert_modal.tsx
@@ -3,21 +3,17 @@ import { ModalRefType } from 'components/elements/modal'
 import { Localize } from 'components/localization'
 import { Modal, Header } from 'components/elements'
 import { useUserBrowser } from 'components/hooks/use-user-browser'
-import { minimum_required_versions } from 'common/constants'
 import { CookieStorage } from 'common/storage'
 
 const BrowserUpdateAlertModal = () => {
     const modalRef = React.useRef<ModalRefType>()
-    const { browser_name, browser_version } = useUserBrowser()
+    const { is_outdated } = useUserBrowser()
     const cookie_key = 'update_browser_alert_modal_shown'
     const cookie = new CookieStorage(cookie_key)
 
     useEffect(
-        () =>
-            minimum_required_versions[browser_name] &&
-            browser_version < minimum_required_versions[browser_name] &&
-            modalRef.current.show(),
-        [browser_version],
+        () => is_outdated && !cookie.get(cookie_key) && modalRef.current.show(),
+        [is_outdated],
     )
 
     const handlePositive = () => {
@@ -34,12 +30,6 @@ const BrowserUpdateAlertModal = () => {
         >
             <Header type="paragraph-2" weight="regular">
                 <Localize translate_text="Update your browser to get the best Deriv experience" />
-                <div>your browser info:</div>
-                <ul>
-                    <li>name : {browser_name}</li>
-                    <li>version : {browser_version}</li>
-                    <li>minimum_required_versions : {minimum_required_versions[browser_name]}</li>
-                </ul>
             </Header>
         </Modal>
     )

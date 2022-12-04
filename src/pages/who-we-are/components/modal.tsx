ec9a0e340bf2507c5bd9940dc946b2a1eb136e10
Niloofar Sadeghi <niloofar.sadeghi@firstsource.tech>
2022-12-04 11:53:48 +0330

    fix: ts errors in who we are

--- src/pages/who-we-are/components/modal.tsx
+++ src/pages/who-we-are/components/modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import styled from 'styled-components'
 import { ModalFlex } from './leaders'
 import { LocalizedLink } from 'components/localization'
@@ -7,7 +7,7 @@ import Linkedin from 'images/svg/who-we-are/linkedin.svg'
 
 type ModalPropsType = {
     name: string
-    position: string
+    position: string | ReactElement
     link?: string
 }
 
@@ -27,13 +27,7 @@ const StyledLogo = styled.img<StyledLogoType>`
 
 const Modal = ({ name, position, link }: ModalPropsType) => {
     return (
-        <ModalFlex
-            ai="center"
-            direction="column"
-            width="unset"
-            height="unset"
-            padding="0px 16px 5px"
-        >
+        <ModalFlex ai="center" direction="column" width="unset" height="unset" p="0px 16px 5px">
             <Header type="unset" as="h4" padding="0" align="center" size="14px">
                 {name}
             </Header>

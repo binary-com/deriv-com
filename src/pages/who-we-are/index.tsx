d7cf79df9037f72a6fb36c6881b461dd1a3b930c
iman <iman@firstsource.tech>
2022-12-13 00:22:30 +0330

    chore: re-triggering vercel

--- src/pages/who-we-are/index.tsx
+++ src/pages/who-we-are/index.tsx
@@ -47,6 +47,7 @@ const AboutUs = () => {
 
     const lazy_components: ReactNode = (
         <>
+            {/* test */}
             <OurValues
                 fallback={
                     <div>

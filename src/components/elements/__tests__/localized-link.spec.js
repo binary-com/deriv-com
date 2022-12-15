7ffcf7bebb7d95261dcff8b8a0123b3a7d6fc698
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2022-12-15 08:33:21 +0100

    chore: fix pathname

--- src/components/elements/__tests__/localized-link.spec.js
+++ src/components/elements/__tests__/localized-link.spec.js
@@ -1,13 +1,13 @@
 import '@testing-library/jest-dom'
 import TestRenderer from 'react-test-renderer'
-import { LocaleContextWrapper } from "../../localization/locale-context"
+import { LocaleContextWrapper } from '../../localization/locale-context'
 import { LocalizedLink } from 'components/localization'
 
 describe('LinkText', () => {
     test('should navigate to url1 when link is clicked', () => {
         const url1 = 'https://www.deriv.com'
         const testRenderer = TestRenderer.create(
-            <LocaleContextWrapper pageContext={{ locale: 'en', pathname: '/en/signup' }}>
+            <LocaleContextWrapper pageContext={{ locale: 'en', pathname: 'deriv.com/en/' }}>
                 <LocalizedLink to={url1}>This is a link</LocalizedLink>
             </LocaleContextWrapper>,
         )

56ca47738b8bbb9f27e94f0e5526832a99612737
Mohsen Hajibeigloo <mohsen@re-work.dev>
2022-12-27 11:07:27 +0330

    fix: fix ws connection lagnuage
    - updated use-deriv-api and connected it to i18next conetxt
    - updated api.ts to get the lang for connection creation

--- src/components/hooks/use-deriv-api.tsx
+++ src/components/hooks/use-deriv-api.tsx
@@ -1,4 +1,5 @@
-import { useState, useLayoutEffect, useRef } from 'react'
+import { useState, useLayoutEffect, useRef, useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
 import DerivWS from 'common/websocket/api'
 
 export type DerivApiProps = {
@@ -9,21 +10,28 @@ export const useDerivApi = () => {
     const [is_opened, setOpened] = useState(false)
     const ws = useRef(null)
 
+    const { i18n } = useTranslation()
+    const current_lang = i18n.language ?? 'en'
+
+    useLayoutEffect(() => {
+        setOpened(false)
+    }, [current_lang])
+
     useLayoutEffect(() => {
         if (!is_opened) {
-            const deriv_api = new DerivWS()
+            const deriv_api = new DerivWS(current_lang)
             setOpened(true)
             ws.current = deriv_api
         }
-    }, [is_opened])
+    }, [is_opened, current_lang])
 
-    const send = async (data: object, callback: (e: object) => void) => {
+    const send = useCallback(async (data: object, callback: (e: object) => void) => {
         if (ws) {
             const response = await ws.current.send(data)
 
             callback(response)
         }
-    }
+    }, [])
 
     return { send }
 }

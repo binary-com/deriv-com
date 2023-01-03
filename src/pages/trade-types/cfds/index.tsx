8750cb1905fce0c57285f4836e6f85c45d094a19
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-01-03 17:14:28 +0400

    refactor: lazy loading cfd

--- src/pages/trade-types/cfds/index.tsx
+++ src/pages/trade-types/cfds/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import Loadable from '@loadable/component'
 import { SmallContainer, Hero } from '../components/_style'
-import WhatIsCFD from './_what_is_cfd'
 import { SEO } from 'components/containers'
 import { Header } from 'components/elements'
 import Layout from 'components/layout/layout'
 import { localize, WithIntl } from 'components/localization'
+import TRADE_DUMMY from 'images/common/trade-type-dummy.png'
+import PLATFORM_DUMMY from 'images/common/platforms-dummy.png'
+import WCS_DUMMY from 'images/common/wcs-dummy.png'
+import CFD_DUMMY from 'images/common/cfd_dummy.png'
+import WHY_DUMMY from 'images/common/why-cfd-dummy.png'
+import START_DUMMY from 'images/common/start-cfd-dummy.png'
+import CFD_TRADE_DUMMY from 'images/common/cfd-trading-dummy.png'
+import CFD_MARKETS_DUMMY from 'images/common/cfd-markets-dummy.png'
+import { useHandleLazyLoad } from 'components/hooks/use-handle-lazy-load'
+const WhatIsCFD = Loadable(() => import('./_what_is_cfd'))
 const WhyTradeCFD = Loadable(() => import('./_why-trade-cfd'))
 const TradingCFDIncreases = Loadable(() => import('./_trading-cfd-increases'))
 const StartTrading = Loadable(() => import('./_start-trading'))
@@ -13,6 +22,60 @@ const ThingsToKeep = Loadable(() => import('./_mind-when-trading'))
 const AvailableMarkets = Loadable(() => import('./_available-markets'))
 
 const CFD = () => {
+    const target = '#gatsby-focus-wrapper'
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.7,
+    }
+    const lazy_components: ReactNode = (
+        <>
+            <WhatIsCFD
+                fallback={
+                    <div>
+                        <img src={CFD_DUMMY} style={{ width: '95%' }} alt="trade-type" />
+                    </div>
+                }
+            />
+            <ThingsToKeep
+                fallback={
+                    <div>
+                        <img src={CFD_DUMMY} style={{ width: '95%' }} alt="trade-type" />
+                    </div>
+                }
+            />
+            <TradingCFDIncreases
+                fallback={
+                    <div>
+                        <img src={CFD_TRADE_DUMMY} style={{ width: '95%' }} alt="platform" />
+                    </div>
+                }
+            />
+
+            <WhyTradeCFD
+                fallback={
+                    <div>
+                        <img src={WHY_DUMMY} style={{ width: '95%' }} alt="wcs" />
+                    </div>
+                }
+            />
+            <StartTrading
+                fallback={
+                    <div>
+                        <img src={START_DUMMY} style={{ width: '95%' }} alt="wcs" />
+                    </div>
+                }
+            />
+            <AvailableMarkets
+                fallback={
+                    <div>
+                        <img src={CFD_MARKETS_DUMMY} style={{ width: '95%' }} alt="wcs" />
+                    </div>
+                }
+            />
+        </>
+    )
+    const lazyTemplate = useHandleLazyLoad(lazy_components, target, options)
     return (
         <Layout>
             <SEO
@@ -28,12 +91,7 @@ const CFD = () => {
                     </Header>
                 </SmallContainer>
             </Hero>
-            <WhatIsCFD />
-            <ThingsToKeep />
-            <TradingCFDIncreases />
-            <WhyTradeCFD />
-            <StartTrading />
-            <AvailableMarkets />
+            {lazyTemplate}
         </Layout>
     )
 }

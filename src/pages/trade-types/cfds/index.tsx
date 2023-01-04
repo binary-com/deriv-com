e17b2aa64c401a494a2433135ec13bb5e538422b
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-01-04 14:54:28 +0400

    refactor: lazy loading cfd

--- src/pages/trade-types/cfds/index.tsx
+++ src/pages/trade-types/cfds/index.tsx
@@ -5,9 +5,6 @@ import { SEO } from 'components/containers'
 import { Header } from 'components/elements'
 import Layout from 'components/layout/layout'
 import { localize, WithIntl } from 'components/localization'
-import TRADE_DUMMY from 'images/common/trade-type-dummy.png'
-import PLATFORM_DUMMY from 'images/common/platforms-dummy.png'
-import WCS_DUMMY from 'images/common/wcs-dummy.png'
 import CFD_DUMMY from 'images/common/cfd_dummy.png'
 import WHY_DUMMY from 'images/common/why-cfd-dummy.png'
 import START_DUMMY from 'images/common/start-cfd-dummy.png'
@@ -20,61 +17,60 @@ const TradingCFDIncreases = Loadable(() => import('./_trading-cfd-increases'))
 const StartTrading = Loadable(() => import('./_start-trading'))
 const ThingsToKeep = Loadable(() => import('./_mind-when-trading'))
 const AvailableMarkets = Loadable(() => import('./_available-markets'))
+const target = '#gatsby-focus-wrapper'
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.7,
+}
+const lazy_components = (
+    <>
+        <WhatIsCFD
+            fallback={
+                <div>
+                    <img src={CFD_DUMMY} style={{ width: '95%' }} alt="what-is-cfd" />
+                </div>
+            }
+        />
+        <ThingsToKeep
+            fallback={
+                <div>
+                    <img src={CFD_DUMMY} style={{ width: '95%' }} alt="things-to-keep" />
+                </div>
+            }
+        />
+        <TradingCFDIncreases
+            fallback={
+                <div>
+                    <img src={CFD_TRADE_DUMMY} style={{ width: '95%' }} alt="cfd-trades" />
+                </div>
+            }
+        />
 
+        <WhyTradeCFD
+            fallback={
+                <div>
+                    <img src={WHY_DUMMY} style={{ width: '95%' }} alt="why-cfd" />
+                </div>
+            }
+        />
+        <StartTrading
+            fallback={
+                <div>
+                    <img src={START_DUMMY} style={{ width: '95%' }} alt="start-cfd" />
+                </div>
+            }
+        />
+        <AvailableMarkets
+            fallback={
+                <div>
+                    <img src={CFD_MARKETS_DUMMY} style={{ width: '95%' }} alt="cfd-markets" />
+                </div>
+            }
+        />
+    </>
+)
 const CFD = () => {
-    const target = '#gatsby-focus-wrapper'
-    const options = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.7,
-    }
-    const lazy_components: ReactNode = (
-        <>
-            <WhatIsCFD
-                fallback={
-                    <div>
-                        <img src={CFD_DUMMY} style={{ width: '95%' }} alt="trade-type" />
-                    </div>
-                }
-            />
-            <ThingsToKeep
-                fallback={
-                    <div>
-                        <img src={CFD_DUMMY} style={{ width: '95%' }} alt="trade-type" />
-                    </div>
-                }
-            />
-            <TradingCFDIncreases
-                fallback={
-                    <div>
-                        <img src={CFD_TRADE_DUMMY} style={{ width: '95%' }} alt="platform" />
-                    </div>
-                }
-            />
-
-            <WhyTradeCFD
-                fallback={
-                    <div>
-                        <img src={WHY_DUMMY} style={{ width: '95%' }} alt="wcs" />
-                    </div>
-                }
-            />
-            <StartTrading
-                fallback={
-                    <div>
-                        <img src={START_DUMMY} style={{ width: '95%' }} alt="wcs" />
-                    </div>
-                }
-            />
-            <AvailableMarkets
-                fallback={
-                    <div>
-                        <img src={CFD_MARKETS_DUMMY} style={{ width: '95%' }} alt="wcs" />
-                    </div>
-                }
-            />
-        </>
-    )
     const lazyTemplate = useHandleLazyLoad(lazy_components, target, options)
     return (
         <Layout>

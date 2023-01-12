44863dae637c9b44f86c0d8ac84f6c5d11e00dc5
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-01-12 08:37:36 +0100

    refactor: applied comments suggestions

--- src/pages/contact_us/index.tsx
+++ src/pages/contact_us/index.tsx
@@ -6,7 +6,7 @@ import device from 'themes/device'
 import { useOpenLiveChat } from 'components/hooks/use-open-live-chat-redirection'
 import { Header, Text } from 'components/elements'
 import { TString } from 'types/generics'
-import { localize, WithIntl } from 'components/localization'
+import { localize, Localize, WithIntl } from 'components/localization'
 import Layout from 'components/layout/layout'
 import { SEO, SectionContainer, Container } from 'components/containers'
 const Offices = Loadable(() => import('./_offices'))
@@ -33,7 +33,7 @@ const StyledText = styled(Text)`
 `
 
 const title_text: TString = '_t_Contact Us | Online Trading | Deriv.com_t_'
-const query_text: TString =
+const description_text: TString =
     '_t_Have any query or need assistance, check out the details to call our international help desk and our email ID. Feel free to contact us anytime!_t_'
 const contact_us_text: TString = '_t_Contact us_t_'
 const questions_text: TString = "_t_Got questions? Here's how to get answers._t_"
@@ -47,16 +47,16 @@ const ContactUs = () => {
         <Layout>
             <SEO
                 title={localize(title_text)}
-                description={localize(query_text)}
+                description={localize(description_text)}
                 has_organization_schema
             />
             <SectionContainer tablet={{ p: '40px 0' }}>
                 <HeroWrapper fd="column">
                     <Header align="center" as="h1" type="display-title">
-                        {localize(contact_us_text)}
+                        <Localize translate_text={contact_us_text} />
                     </Header>
                     <StyledText align="center" size="var(--text-size-sm)">
-                        {localize(questions_text)}
+                        <Localize translate_text={questions_text} />
                     </StyledText>
                 </HeroWrapper>
             </SectionContainer>

2d1901a5173026fb9906c052c8b0bf41fb9dc5bd
NikitK-deriv <nikita.kolesnik@deriv.com>
2023-01-16 10:47:12 +0300

    feat: netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,23 @@ module.exports = {
     ],
     trailingSlash: "never",
     plugins: [
+        `gatsby-plugin-netlify`,
+        {
+            resolve: `gatsby-plugin-netlify`,
+            options: {
+                headers: {
+                    "/*": [
+                        "Basic-Auth: nikita.kolesnik@deriv.com:6371f76405498057b30145d825a0c5c3 Darerk11:5743c52edb06be3ac5962cca420d92a4",
+                    ]
+                }, // option to add more headers. `Link` headers are transformed by the below criteria
+                allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+                mergeSecurityHeaders: true, // boolean to turn off the default security headers
+                mergeCachingHeaders: true, // boolean to turn off the default caching headers
+                transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+                generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+                NETLIFY_SKIP_GATSBY_FUNCTIONS: true
+            },
+        },
         'gatsby-plugin-react-helmet',
         {
             resolve: `gatsby-plugin-react-helmet-canonical-urls`,

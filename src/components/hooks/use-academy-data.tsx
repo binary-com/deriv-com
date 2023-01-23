ea9f49ee82b2868a0c9b21d0b7ccb048355ca03f
Prince <prince@deriv.com>
2023-01-23 13:43:42 +0400

    chore: undo unnecessary change

--- src/components/hooks/use-academy-data.tsx
+++ src/components/hooks/use-academy-data.tsx
@@ -4,8 +4,6 @@ import { AcademyContext } from 'store/academy-context'
 const useAcademyData = () => {
     const academy_data = useContext(AcademyContext)
 
-    console.log(academy_data)
-
     if (!academy_data) {
         throw new Error('useAcademyData must be used within AcademyContext')
     }

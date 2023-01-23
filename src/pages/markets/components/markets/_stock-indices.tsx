5e72e2a62def8281e89d2888e1d64e42d99d6fbe
konstantinosG-deriv <104083272+konstantinosG-deriv@users.noreply.github.com>
2023-01-23 15:55:48 +0100

    Konstantinos /  Ubuntu font  (#4088)
    * habib/Add trade types component initial (#4076)
    
    * chore: add component initial
    
    * chore: update full width container for product pages
    
    * Habib/fix vercel issue rebranding (#4084)
    
    * chore: update package-lock
    
    * chore: update package
    
    * Revert "Habib/fix vercel issue rebranding (#4084)" (#4085)
    
    This reverts commit 7648e09aadb6a1ce4f3789c16a43a1ef5bb242b4.
    
    * feat: add ubuntu font
    
    * feat: change weight
    
    * feat: change weight and delete file
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>

--- src/pages/markets/components/markets/_stock-indices.tsx
+++ src/pages/markets/components/markets/_stock-indices.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Loadable from '@loadable/component'
-import { WhyTrade } from '../sections/_why-trade'
 import AvailableTrades from '../helper/_available-trades'
 import stock_content from '../../static/content/_stock'
 import { stock_cfds } from '../../static/content/_cfds'
@@ -10,6 +9,7 @@ import DigitalOptions from '../sub-markets/_digital-options'
 import { StyledBox } from '../../static/style/_markets-style'
 import { SimpleStepContentElement } from '../../static/content/_simple_step_content'
 import { localize, Localize } from 'components/localization'
+import { FullWidthMultiColumn } from 'components/elements/full-width-multicolumn'
 
 //Lazy-load
 const SimpleSteps = Loadable(() => import('components/custom/_simple-steps'))
@@ -26,15 +26,6 @@ const StockIndices = ({ simple_step_content }: StockIndicesProps) => {
 
     return (
         <div>
-            <WhyTrade header={<Localize translate_text="Why trade stocks & indices on Deriv" />}>
-                {stock_content.map((content, index) => (
-                    <StyledBox
-                        key={index}
-                        text={content.text}
-                        icon={<img src={content.src} alt={content.alt} />}
-                    />
-                ))}
-            </WhyTrade>
             <AvailableTrades
                 CFDs={<CFDs market_tab_name={'stock-indices'} market_content={stock_cfds} />}
                 DigitalOptions={
@@ -47,6 +38,17 @@ const StockIndices = ({ simple_step_content }: StockIndicesProps) => {
                     <Localize translate_text="Stocks & indices trades available on Deriv" />
                 }
             />
+            <FullWidthMultiColumn
+                header={<Localize translate_text="Why trade stocks & indices on Deriv" />}
+            >
+                {stock_content.map((content, index) => (
+                    <StyledBox
+                        key={index}
+                        text={content.text}
+                        icon={<img src={content.src} alt={content.alt} />}
+                    />
+                ))}
+            </FullWidthMultiColumn>
             <SimpleSteps
                 header={
                     <Localize translate_text="Start trading stocks & indices on Deriv in 3 simple steps" />

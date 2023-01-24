8b16cd04369ae16e39b1f83688134cef31cb0009
meenakshi-deriv <104189801+meenakshi-deriv@users.noreply.github.com>
2023-01-24 15:42:13 +0400

    Meenu/ live price rebranding (#4093)
    * Habib/live market price (#3977)
    
    * fix: live market pricing in homepage
    
    * fix: implemented trending asset api
    
    * fix: fixed pr failure
    
    * fix: display only trending markets
    
    * fix: package-lock dateformat
    
    * fix: remove markets component
    
    Co-authored-by: meenakshi-deriv <meenakshi@deriv.com>
    
    * fix: update forex as default tab
    
    * fix: fixed daily pervent change
    
    * fix: fixed color change for daily percentage cell
    
    * fix: updated css variables
    
    * fix: comments resolving
    
    * fix: resolving comments
    
    * fix: fixed market description for each market
    
    * fix undefined is not iterable error for Map()
    
    * Add type data
    
    * auto-pull-translation
    
    * fix: fixed trade now button
    
    * fix: updated login button
    
    * fix: fixed space issue in safari
    
    * auto-pull-translation
    
    * fix: redirect button fix
    
    * auto-pull-translation
    
    * fix: updated to 5 rows
    
    * auto-pull-translation
    
    * auto-pull-translation
    
    * translation: 📚 sync translations from crowdin feature branch
    
    * fix: eu condition applied
    
    * fix: updated disclaimer text
    
    * auto-pull-translation
    
    * translation: 📚 sync translations from crowdin feature branch
    
    * auto-pull-translation
    
    * translation: 📚 sync translations from crowdin feature branch
    
    * fix: tab fix
    
    * fix: live price table updates
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: meenakshi-deriv <meenakshi@deriv.com>
    Co-authored-by: DerivFE <80095553+DerivFE@users.noreply.github.com>

---
+++ src/pages/home/live-pricing/_types.ts
@@ -0,0 +1,30 @@
+export type TMarketData = {
+    ask: number
+    bid: number
+    day_percentage_change: number
+    market: string
+    market_ranking: string
+    spread: number
+    symbol: string
+}
+
+export type TMarketDataResponse = {
+    trading_platform_asset_listing: any
+    active_symbols: TMarketData[]
+    req_id: number
+}
+
+export type TAvailableLiveMarkets =
+    | 'forex'
+    | 'derived'
+    | 'indices'
+    | 'cryptocurrency'
+    | 'commodities'
+
+export type TMarketButtons = {
+    id: number
+    src: string
+    button_text: string
+    market_name: TAvailableLiveMarkets
+    market_description: string
+}
---
+++ src/pages/home/live-pricing/_use-live-columns.tsx
@@ -0,0 +1,42 @@
+import { createColumnHelper } from '@tanstack/react-table'
+import React, { useMemo } from 'react'
+import { TableCell, TableHeaderCell, DailyPercentageCell } from './components/_elements'
+import { TMarketData } from './_types'
+import { Localize } from 'components/localization'
+
+const liveMarketColumnHelper = createColumnHelper<TMarketData>()
+
+const useLiveColumns = (marketData: () => void) => {
+    const columns = useMemo(() => {
+        return [
+            liveMarketColumnHelper.accessor('symbol', {
+                header: () => (
+                    <TableHeaderCell text={<Localize translate_text="Instrument name" />} />
+                ),
+                cell: (info) => <TableCell text={info.getValue()} />,
+            }),
+            liveMarketColumnHelper.accessor('bid', {
+                header: () => <TableHeaderCell text={<Localize translate_text="Bid price" />} />,
+                cell: (info) => <TableCell text={info.getValue()} />,
+            }),
+            liveMarketColumnHelper.accessor('ask', {
+                header: () => <TableHeaderCell text={<Localize translate_text="Ask price" />} />,
+                cell: (info) => <TableCell text={info.getValue()} />,
+            }),
+            liveMarketColumnHelper.accessor('spread', {
+                header: () => <TableHeaderCell text={<Localize translate_text="Spread" />} />,
+                cell: (info) => <TableCell text={info.getValue()} />,
+            }),
+            liveMarketColumnHelper.accessor('day_percentage_change', {
+                header: () => (
+                    <TableHeaderCell text={<Localize translate_text="Daily % Change" />} />
+                ),
+                cell: (info) => <DailyPercentageCell value={info.getValue()} />,
+            }),
+        ]
+    }, [marketData])
+
+    return columns
+}
+
+export default useLiveColumns

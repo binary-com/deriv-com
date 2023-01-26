fab49946fbc10945bcd3673552d25a7ac6bf743f
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-01-26 08:06:24 +0100

    chore: fix spacing

--- src/pages/markets/static/content/_details.tsx
+++ src/pages/markets/static/content/_details.tsx
@@ -75,7 +75,7 @@ export const ContinuousIndicesDetails = () => (
         </Text>
         <Text>
             <Localize
-                translate_text="_t_<0>One tick</0> is generated <0>every second</0> for volatility indices <0>10 (1s), 25 (1s), 50 (1s), 75 (1s), 100 (1s), 150 (1s), 200(1s), 250(1s), and 300(1s)</0>._t_"
+                translate_text="_t_<0>One tick</0> is generated <0>every second</0> for volatility indices <0>10 (1s), 25 (1s), 50 (1s), 75 (1s), 100 (1s), 150 (1s), 200 (1s), 250 (1s), and 300 (1s)</0>._t_"
                 components={[<strong key={0} />]}
             />
         </Text>
@@ -301,7 +301,7 @@ export const VolatilityIndicesDetailsEU = () => (
         </Text>
         <Text>
             <Localize
-                translate_text="_t_<0>One tick</0> is generated <0>every second</0> for volatility indices <0>150 (1s), 200(1s), 250(1s), and 300(1s).</0>_t_"
+                translate_text="_t_<0>One tick</0> is generated <0>every second</0> for volatility indices <0>150 (1s), 200 (1s), 250 (1s), and 300 (1s).</0>_t_"
                 components={[<strong key={0} />]}
             />
         </Text>

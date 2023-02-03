148a253407f1265d6b81a6cf8e175ba6f3e2c608
mohammadh <mohammad.hashemi@re-work.dev>
2023-02-03 19:19:40 +0330

    feat: add cdn js to test cookie.js which added to deriv-static-content

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -128,7 +128,7 @@ export const onClientEntry = () => {
     }
 
     addScript({
-        src: 'https://static.deriv.com/scripts/cookie.js',
+        src: 'https://cdn.jsdelivr.net/gh/mohammad-hashemi-deriv/deriv-static-content@utm-affiliate-priority/public/scripts/cookie.js',
         async: true,
         strategy: 'off-main-thread',
     })

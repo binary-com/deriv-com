b1196cad8eaf0d18f1c20fb85ec5ec8394b23995
konstantinosG-deriv <104083272+konstantinosG-deriv@users.noreply.github.com>
2023-02-03 07:59:35 +0100

    Konstantinos / [FE Deriv.com squad] [Rebranding] Trader Type - Options page  (#4118)
    * Konstantinos /  Ubuntu font  (#4088)
    
    * habib/Add trade types component initial (#4076)
    
    * chore: add component initial
    
    * chore: update full width container for product pages
    
    * Habib/fix vercel issue rebranding (#4084)
    
    * chore: update package-lock
    
    * chore: update package
    
    * Revert "Habib/fix vercel issue rebranding (#4084)" (#4085)
    
    This reverts commit 7648e09aadb6a1ce4f3789c16a43a1ef5bb242b4.
    
    * feat: add ubuntu font
    
    * feat: change weight
    
    * feat: change weight and delete file
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
    
    * Konstantinos / Reusable title (#4087)
    
    * habib/Add trade types component initial (#4076)
    
    * chore: add component initial
    
    * chore: update full width container for product pages
    
    * Habib/fix vercel issue rebranding (#4084)
    
    * chore: update package-lock
    
    * chore: update package
    
    * Revert "Habib/fix vercel issue rebranding (#4084)" (#4085)
    
    This reverts commit 7648e09aadb6a1ce4f3789c16a43a1ef5bb242b4.
    
    * feat: add title and subtitle component
    
    * fix: change name
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
    
    * chore: add assets
    
    * Meenu/ live price rebranding (#4093)
    
    * Habib/live market price (#3977)
    
    * fix: live market pricing in homepage
    
    * fix: implemented trending asset api
    
    * fix: fixed pr failure
    
    * fix: display only trending markets
    
    * fix: package-lock dateformat
    
    * fix: remove markets component
    
    Co-authored-by: meenakshi-deriv <meenakshi@deriv.com>
    
    * fix: update forex as default tab
    
    * fix: fixed daily pervent change
    
    * fix: fixed color change for daily percentage cell
    
    * fix: updated css variables
    
    * fix: comments resolving
    
    * fix: resolving comments
    
    * fix: fixed market description for each market
    
    * fix undefined is not iterable error for Map()
    
    * Add type data
    
    * auto-pull-translation
    
    * fix: fixed trade now button
    
    * fix: updated login button
    
    * fix: fixed space issue in safari
    
    * auto-pull-translation
    
    * fix: redirect button fix
    
    * auto-pull-translation
    
    * fix: updated to 5 rows
    
    * auto-pull-translation
    
    * auto-pull-translation
    
    * translation: 📚 sync translations from crowdin feature branch
    
    * fix: eu condition applied
    
    * fix: updated disclaimer text
    
    * auto-pull-translation
    
    * translation: 📚 sync translations from crowdin feature branch
    
    * auto-pull-translation
    
    * translation: 📚 sync translations from crowdin feature branch
    
    * fix: tab fix
    
    * fix: live price table updates
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: meenakshi-deriv <meenakshi@deriv.com>
    Co-authored-by: DerivFE <80095553+DerivFE@users.noreply.github.com>
    
    * Habib/update attribute (#4098)
    
    * Prince/ update workflow permissions (#4080)
    
    * chore: update workflow permissions
    
    * chore: undo unnecessary change
    
    * chore: update attribute
    
    Co-authored-by: prince-deriv <82309725+prince-deriv@users.noreply.github.com>
    
    * chore: fetch latest translations (#4090)
    
    * chore: fix conflicts
    
    * feat: rebrand options page
    
    * chore: fix
    
    * chore: add import
    
    * fix: change location of options content and fix imports and bugs
    
    * chore: move option file
    
    ---------
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
    Co-authored-by: habib-binary <habib@regentmarkets.com>
    Co-authored-by: meenakshi-deriv <104189801+meenakshi-deriv@users.noreply.github.com>
    Co-authored-by: meenakshi-deriv <meenakshi@deriv.com>
    Co-authored-by: DerivFE <80095553+DerivFE@users.noreply.github.com>
    Co-authored-by: prince-deriv <82309725+prince-deriv@users.noreply.github.com>

--- src/pages/trade-types/options/index.tsx
+++ src/pages/trade-types/options/index.tsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import Loadable from '@loadable/component'
-import { SmallContainer, Hero } from '../components/_style'
+import { Hero } from '../components/_style'
 import PageNotFound from '../../404'
 import WhatAreTheOptions from './_what-are-options'
-import { SEO } from 'components/containers'
-import { Header } from 'components/elements'
+import { SEO, SmallContainer } from 'components/containers'
 import Layout from 'components/layout/layout'
+import CommonHeaderSection from 'components/elements/common-header-section'
+import OptionsNavTab from 'pages/markets/components/sections/_options-nav-tab'
 import { localize, WithIntl } from 'components/localization'
+import { StepperView } from 'components/elements'
 import useRegion from 'components/hooks/use-region'
 const HowOptionsWorks = Loadable(() => import('./_how-options-works'))
 const OptionsToTrade = Loadable(() => import('./_options-to-trade'))
-const StartTrading = Loadable(() => import('./_start-trading'))
 const MarketsAvailable = Loadable(() => import('./_markets-available'))
 
 const meta_attributes = {
@@ -38,17 +39,32 @@ const Options = () => {
                     )}
                     meta_attributes={meta_attributes}
                 />
-                <Hero jc="cneter" ai="center">
-                    <SmallContainer>
-                        <Header as="h1" type="display-title" color="white" align="center">
-                            {localize('Options')}
-                        </Header>
-                    </SmallContainer>
+                <Hero jc="center" ai="center">
+                    <CommonHeaderSection
+                        title="_t_Options_t_"
+                        title_text_color="var(--color-red)"
+                        title_font_size="6.4rem"
+                    />
                 </Hero>
+                <OptionsNavTab route_from={'options'} route_offset={650} />
                 <WhatAreTheOptions />
                 <HowOptionsWorks />
                 <OptionsToTrade />
-                <StartTrading />
+                <SmallContainer direction="column" ai="flex-start">
+                    <CommonHeaderSection
+                        title="_t_Start trading options on Deriv_t_"
+                        title_font_size="3.2rem"
+                        margin_title="15rem 0 7rem 0"
+                    />
+                    <StepperView
+                        first_step_title="_t_Practise_t_"
+                        first_step_subtitle="_t_Open a demo account and get unlimited virtual funds to practise on our platforms — Deriv Trader, SmartTrader, and Deriv Bot._t_"
+                        second_step_title="_t_Trade_t_"
+                        second_step_subtitle="_t_Open a real account, make a deposit, and start trading options for real._t_"
+                        third_step_title="_t_Withdraw_t_"
+                        third_step_subtitle="_t_Conveniently withdraw your funds through any of our supported withdrawal methods._t_"
+                    />
+                </SmallContainer>
                 <MarketsAvailable />
             </Layout>
         ) : (

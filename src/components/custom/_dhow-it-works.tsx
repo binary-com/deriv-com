1f98c4a762a01941e65afb0b824f742bb428099b
Niloofar Sadeghi <niloofar.sadeghi@re-work.dev>
2023-02-06 23:23:51 +0330

    refactor: dbot components

--- src/components/custom/_dhow-it-works.tsx
+++ src/components/custom/_dhow-it-works.tsx
@@ -1,14 +1,16 @@
 import React, { ElementType } from 'react'
 import styled from 'styled-components'
-import { localize } from 'components/localization'
+import { Localize } from 'components/localization'
 import { Container, SectionContainer } from 'components/containers'
 import { Header } from 'components/elements'
 import device from 'themes/device'
+import { TString } from 'types/generics'
 
 type DHowItWorksProps = {
-    title?: string
+    title?: TString
     Video?: ElementType
 }
+
 const StyledHeader = styled(Header)`
     @media ${device.tabletL} {
         text-align: center;
@@ -22,10 +24,10 @@ const DHowItWorks = ({ Video, title }: DHowItWorksProps) => {
         <SectionContainer>
             <Container justify="center" direction="column">
                 <StyledHeader as="h4" type="sub-section-title" align="center" weight="500">
-                    {localize('How it works')}
+                    <Localize translate_text="_t_How it works_t_" />
                 </StyledHeader>
                 <StyledHeader as="h2" type="page-title" align="center" mt="0.9rem">
-                    {title}
+                    <Localize translate_text={title} />
                 </StyledHeader>
                 <Video />
             </Container>

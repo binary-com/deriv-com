567386800fc794779c354b31f0639cb546ead1e1
konstantinosG-deriv <104083272+konstantinosG-deriv@users.noreply.github.com>
2023-02-06 13:22:40 +0100

    Konstantinos / Accumulators (#4134)
    * habib/Add trade types component initial (#4076)
    
    * chore: add component initial
    
    * chore: update full width container for product pages
    
    * Habib/fix vercel issue rebranding (#4084)
    
    * chore: update package-lock
    
    * chore: update package
    
    * Revert "Habib/fix vercel issue rebranding (#4084)" (#4085)
    
    This reverts commit 7648e09aadb6a1ce4f3789c16a43a1ef5bb242b4.
    
    * Konstantinos /  Ubuntu font  (#4088)
    
    * habib/Add trade types component initial (#4076)
    
    * chore: add component initial
    
    * chore: update full width container for product pages
    
    * Habib/fix vercel issue rebranding (#4084)
    
    * chore: update package-lock
    
    * chore: update package
    
    * Revert "Habib/fix vercel issue rebranding (#4084)" (#4085)
    
    This reverts commit 7648e09aadb6a1ce4f3789c16a43a1ef5bb242b4.
    
    * feat: add ubuntu font
    
    * feat: change weight
    
    * feat: change weight and delete file
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
    
    * Konstantinos / Reusable title (#4087)
    
    * habib/Add trade types component initial (#4076)
    
    * chore: add component initial
    
    * chore: update full width container for product pages
    
    * Habib/fix vercel issue rebranding (#4084)
    
    * chore: update package-lock
    
    * chore: update package
    
    * Revert "Habib/fix vercel issue rebranding (#4084)" (#4085)
    
    This reverts commit 7648e09aadb6a1ce4f3789c16a43a1ef5bb242b4.
    
    * feat: add title and subtitle component
    
    * fix: change name
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
    
    * chore: add assets
    
    * feat: add accumulators without redirect
    
    * fix: fix font
    
    ---------
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
    Co-authored-by: habib-binary <habib@regentmarkets.com>

---
+++ src/pages/trade-types/accumulators/index.tsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react'
+import Loadable from '@loadable/component'
+import styled from 'styled-components'
+import { Hero } from '../components/_style'
+import PageNotFound from '../../404'
+import WhatAreAccumulators from './_what-are-accumulators'
+import { SEO, SmallContainer } from 'components/containers'
+import Layout from 'components/layout/layout'
+import Button from 'components/custom/_button'
+import CommonHeaderSection from 'components/elements/common-header-section'
+import OptionsNavTab from 'pages/markets/components/sections/_options-nav-tab'
+import { localize, WithIntl } from 'components/localization'
+import { StepperView } from 'components/elements'
+import useRegion from 'components/hooks/use-region'
+const HowAccumulatorsWork = Loadable(() => import('./_how-accumulators-works'))
+const AccumulatorsToTrade = Loadable(() => import('./_accumulators-to-trade'))
+
+const meta_attributes = {
+    og_title: localize('Options trading | Trading types | Deriv'),
+    og_description: localize(
+        'Learn about options trading on Deriv. Earn payouts by correctly predicting price movements without needing to buy the underlying assets.',
+    ),
+}
+
+const ButtonContainer = styled.div`
+    margin-bottom: 16.4rem;
+`
+
+const Accumulators = () => {
+    const { is_row } = useRegion()
+    const [is_loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        setLoaded(true)
+    }, [])
+
+    if (is_loaded) {
+        return is_row ? (
+            <Layout>
+                <SEO
+                    title={localize('Options trading | Trade digital options on Deriv')}
+                    description={localize(
+                        'Explore what are options on Deriv. Learn how to start trading options with forex, synthetics, stocks & indices, and basket indices.',
+                    )}
+                    meta_attributes={meta_attributes}
+                />
+                <Hero jc="center" ai="center">
+                    <CommonHeaderSection
+                        title="_t_Options_t_"
+                        title_text_color="var(--color-red)"
+                        title_font_size="6.4rem"
+                    />
+                </Hero>
+                <OptionsNavTab route_from={'accumulators'} route_offset={650} />
+                <WhatAreAccumulators />
+                <HowAccumulatorsWork />
+                <AccumulatorsToTrade />
+                <SmallContainer direction="column" ai="flex-start">
+                    <CommonHeaderSection
+                        title="_t_Start trading options on Deriv_t_"
+                        title_font_size="3.2rem"
+                        margin_title="15rem 0 7rem 0"
+                    />
+                    <StepperView
+                        first_step_title="_t_Practise_t_"
+                        first_step_subtitle="_t_Open a demo account on Deriv and practise with an unlimited amount of virtual funds._t_"
+                        second_step_title="_t_Trade_t_"
+                        second_step_subtitle="_t_Open a real account, make a deposit, and start trading accumulators for real._t_"
+                        third_step_title="_t_Withdraw_t_"
+                        third_step_subtitle="_t_Conveniently withdraw your funds through any of our supported withdrawal methods._t_"
+                    />
+                    <ButtonContainer>
+                        <Button label="Create free demo account" primary />
+                    </ButtonContainer>
+                </SmallContainer>
+            </Layout>
+        ) : (
+            <PageNotFound />
+        )
+    }
+
+    return (
+        <>
+            <SEO
+                title={localize('Options trading | Trade types | Deriv')}
+                description={localize(
+                    'Learn about options trading on Deriv. Earn payouts by correctly predicting price movements in forex, synthetic indices, and other popular financial markets.',
+                )}
+            />
+        </>
+    )
+}
+
+export default WithIntl()(Accumulators)

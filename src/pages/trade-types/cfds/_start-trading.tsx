81a0705328445af8ad91007ca05d33d77afbce29
konstantinosG-deriv <104083272+konstantinosG-deriv@users.noreply.github.com>
2023-02-07 07:16:16 +0100

    Konstantinos / CFDs rebrand (#4142)
    * habib/Add trade types component initial (#4076)
    
    * chore: add component initial
    
    * chore: update full width container for product pages
    
    * Habib/fix vercel issue rebranding (#4084)
    
    * chore: update package-lock
    
    * chore: update package
    
    * Revert "Habib/fix vercel issue rebranding (#4084)" (#4085)
    
    This reverts commit 7648e09aadb6a1ce4f3789c16a43a1ef5bb242b4.
    
    * Konstantinos /  Ubuntu font  (#4088)
    
    * habib/Add trade types component initial (#4076)
    
    * chore: add component initial
    
    * chore: update full width container for product pages
    
    * Habib/fix vercel issue rebranding (#4084)
    
    * chore: update package-lock
    
    * chore: update package
    
    * Revert "Habib/fix vercel issue rebranding (#4084)" (#4085)
    
    This reverts commit 7648e09aadb6a1ce4f3789c16a43a1ef5bb242b4.
    
    * feat: add ubuntu font
    
    * feat: change weight
    
    * feat: change weight and delete file
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
    
    * Konstantinos / Reusable title (#4087)
    
    * habib/Add trade types component initial (#4076)
    
    * chore: add component initial
    
    * chore: update full width container for product pages
    
    * Habib/fix vercel issue rebranding (#4084)
    
    * chore: update package-lock
    
    * chore: update package
    
    * Revert "Habib/fix vercel issue rebranding (#4084)" (#4085)
    
    This reverts commit 7648e09aadb6a1ce4f3789c16a43a1ef5bb242b4.
    
    * feat: add title and subtitle component
    
    * fix: change name
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
    
    * chore: add assets
    
    * feat: cfds rebranded
    
    * fix: fix package
    
    ---------
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
    Co-authored-by: habib-binary <habib@regentmarkets.com>

--- src/pages/trade-types/cfds/_start-trading.tsx
+++ src/pages/trade-types/cfds/_start-trading.tsx
@@ -1,70 +1,40 @@
 import React from 'react'
-import Timeline from '../components/_timeline'
-import { SmallContainer, StartTradingBg } from '../components/_style'
-import { localize, Localize } from 'components/localization'
-import { LinkButton } from 'components/form'
-import { Header } from 'components/elements'
+import { SmallContainer } from '../components/_style'
+import CommonHeaderSection from 'components/elements/common-header-section'
+import Button from 'components/custom/_button'
+import { StepperView, Header } from 'components/elements'
 import useRegion from 'components/hooks/use-region'
-import useScreenSize from 'components/hooks/use-screen-size'
+import useHandleSignup from 'components/hooks/use-handle-signup'
 
 const StartTrading = () => {
     const { is_eu } = useRegion()
-    const { is_mobile_or_tablet } = useScreenSize()
+    const handleSignup = useHandleSignup()
     return (
         <>
-            <StartTradingBg>
-                <SmallContainer direction="column" ai="flex-start">
-                    {is_mobile_or_tablet ? (
-                        <Header as="h3" type="section-title" mb="4rem">
-                            {localize('Start trading CFDs on Deriv')}
-                        </Header>
-                    ) : (
-                        <Header as="h3" type="section-title" mb="4rem">
-                            <Localize
-                                translate_text="Start trading CFDs on <0/>Deriv<0/>"
-                                components={[<br key={0} />]}
-                            />
-                        </Header>
-                    )}
-                    <Timeline>
-                        <Timeline.Item title={<Localize translate_text="Practise" />}>
-                            {is_eu
-                                ? localize(
-                                      'Open a demo CFDs account and practise with an unlimited amount of virtual funds.',
-                                  )
-                                : localize(
-                                      'Open a demo Deriv MT5 or demo Deriv X account and practise with an unlimited amount of virtual funds.',
-                                  )}
-                        </Timeline.Item>
-                        <Timeline.Item title={<Localize translate_text="Trade" />}>
-                            {is_eu
-                                ? localize(
-                                      'Trade with a real CFDs account. Get access to leverage and trade positions larger than your existing capital.',
-                                  )
-                                : localize(
-                                      'Trade with a real Deriv MT5 or real Deriv X account and get access to high leverage to trade positions larger than your existing capital.',
-                                  )}
-                        </Timeline.Item>
-                        <Timeline.Item title={<Localize translate_text="Withdraw" />}>
-                            {localize(
-                                'Conveniently withdraw your funds through any of our supported withdrawal methods.',
-                            )}
-                        </Timeline.Item>
-                    </Timeline>
-                    <LinkButton
-                        id="dm-dmt5-signup"
-                        mt="4rem"
-                        external
-                        type="mt5"
-                        to="#demo"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        secondary
-                    >
-                        {localize('Create free demo account')}
-                    </LinkButton>
-                </SmallContainer>
-            </StartTradingBg>
+            <SmallContainer direction="column" ai="flex-start">
+                <CommonHeaderSection
+                    title="_t_Start trading CFDs on Deriv_t_"
+                    title_font_size="3.2rem"
+                    margin_title="15rem 0 7rem 0"
+                />
+                <StepperView
+                    first_step_title="_t_Practise_t_"
+                    first_step_subtitle={
+                        is_eu
+                            ? '_t_Open a demo CFDs account and practise with an unlimited amount of virtual funds._t_'
+                            : '_t_Open a demo Deriv MT5 or demo Deriv X account and practise with an unlimited amount of virtual funds._t_'
+                    }
+                    second_step_title="_t_Trade_t_"
+                    second_step_subtitle={
+                        is_eu
+                            ? '_t_Trade with a real CFDs account. Get access to leverage and trade positions larger than your existing capital._t_'
+                            : '_t_Trade with a real Deriv MT5 or real Deriv X account and get access to high leverage to trade positions larger than your existing capital._t_'
+                    }
+                    third_step_title="_t_Withdraw_t_"
+                    third_step_subtitle="_t_Conveniently withdraw your funds through any of our supported withdrawal methods._t_"
+                />
+                <Button onClick={handleSignup} label="Create free demo account" primary />
+            </SmallContainer>
         </>
     )
 }

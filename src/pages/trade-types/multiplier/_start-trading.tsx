6c74cffb53856b73889ba7d794cbef9483ea837d
konstantinosG-deriv <104083272+konstantinosG-deriv@users.noreply.github.com>
2023-02-07 17:01:11 +0100

    Konstantinos / Multipliers Rebrand (#4146)
    * habib/Add trade types component initial (#4076)
    
    * chore: add component initial
    
    * chore: update full width container for product pages
    
    * Habib/fix vercel issue rebranding (#4084)
    
    * chore: update package-lock
    
    * chore: update package
    
    * Revert "Habib/fix vercel issue rebranding (#4084)" (#4085)
    
    This reverts commit 7648e09aadb6a1ce4f3789c16a43a1ef5bb242b4.
    
    * Konstantinos /  Ubuntu font  (#4088)
    
    * habib/Add trade types component initial (#4076)
    
    * chore: add component initial
    
    * chore: update full width container for product pages
    
    * Habib/fix vercel issue rebranding (#4084)
    
    * chore: update package-lock
    
    * chore: update package
    
    * Revert "Habib/fix vercel issue rebranding (#4084)" (#4085)
    
    This reverts commit 7648e09aadb6a1ce4f3789c16a43a1ef5bb242b4.
    
    * feat: add ubuntu font
    
    * feat: change weight
    
    * feat: change weight and delete file
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
    
    * Konstantinos / Reusable title (#4087)
    
    * habib/Add trade types component initial (#4076)
    
    * chore: add component initial
    
    * chore: update full width container for product pages
    
    * Habib/fix vercel issue rebranding (#4084)
    
    * chore: update package-lock
    
    * chore: update package
    
    * Revert "Habib/fix vercel issue rebranding (#4084)" (#4085)
    
    This reverts commit 7648e09aadb6a1ce4f3789c16a43a1ef5bb242b4.
    
    * feat: add title and subtitle component
    
    * fix: change name
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
    
    * chore: add assets
    
    * feat: multipliers page
    
    * fix: update derived description
    
    * chore: vercel
    
    * fix: package update
    
    ---------
    
    Co-authored-by: Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
    Co-authored-by: habib-binary <habib@regentmarkets.com>

--- src/pages/trade-types/multiplier/_start-trading.tsx
+++ src/pages/trade-types/multiplier/_start-trading.tsx
@@ -3,53 +3,36 @@ import styled from 'styled-components'
 import Timeline from '../components/_timeline'
 import { SmallContainer } from '../components/_style'
 import { SectionContainer } from 'components/containers'
-import { Header } from 'components/elements'
-import { Button } from 'components/form'
-import { localize, Localize } from 'components/localization'
+import CommonHeaderSection from 'components/elements/common-header-section'
+import { StepperView } from 'components/elements'
+import Button from 'components/custom/_button'
 import Pattern from 'images/common/trade-types/pattern-section-small.png'
 import useHandleSignup from 'components/hooks/use-handle-signup'
-
-const PatternContainer = styled(SectionContainer)`
-    background: url(${Pattern});
-    background-size: contain;
-`
-
-const FixTimeline = styled(Timeline)`
-    margin-left: 1.2rem;
-    margin-bottom: 5rem;
-`
 
 const StartTrading = () => {
     const handleSignup = useHandleSignup()
 
     return (
-        <PatternContainer background="white" padding="4rem 0">
-            <SmallContainer direction="column" ai="flex-start">
-                <Header as="h3" size="3.2rem" mb="4rem">
-                    {localize('Start trading multipliers on Deriv')}
-                </Header>
-                <FixTimeline>
-                    <FixTimeline.Item title={<Localize translate_text="Practise" />}>
-                        {localize(
-                            'Open a demo account on Deriv and practise with an unlimited amount of virtual funds on our award-winning platforms.',
-                        )}
-                    </FixTimeline.Item>
-                    <FixTimeline.Item title={<Localize translate_text="Trade" />}>
-                        {localize(
-                            'Open a real account, make a deposit, and start trading multipliers for real.',
-                        )}
-                    </FixTimeline.Item>
-                    <FixTimeline.Item title={<Localize translate_text="Withdraw" />}>
-                        {localize(
-                            'Conveniently withdraw your funds through any of our supported withdrawal methods.',
-                        )}
-                    </FixTimeline.Item>
-                </FixTimeline>
-                <Button onClick={handleSignup} id="dm-multipliers-signup-2" secondary>
-                    {localize('Create free demo account')}
-                </Button>
-            </SmallContainer>
-        </PatternContainer>
+        <SmallContainer direction="column" ai="flex-start">
+            <CommonHeaderSection
+                title="_t_Start trading multipliers on Deriv_t_"
+                title_font_size="3.2rem"
+                margin_title="15rem 0 7rem 0"
+            />
+            <StepperView
+                first_step_title="_t_Practise_t_"
+                first_step_subtitle={
+                    '_t_Open a demo account on Deriv and practise with an unlimited amount of virtual funds on our award-winning platforms._t_'
+                }
+                second_step_title="_t_Trade_t_"
+                second_step_subtitle={
+                    '_t_Open a real account, make a deposit, and start trading multipliers for real._t_'
+                }
+                third_step_title="_t_Withdraw_t_"
+                third_step_subtitle="_t_Conveniently withdraw your funds through any of our supported withdrawal methods._t_"
+            />
+            <Button onClick={handleSignup} label="Create free demo account" primary />
+        </SmallContainer>
     )
 }
 

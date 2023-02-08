262bdc474c417488e6a404dbc3b72108d63569c3
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-02-08 10:09:21 +0400

    refactor: adding hard lazy loading in cfd page

--- src/pages/trade-types/cfds/index.tsx
+++ src/pages/trade-types/cfds/index.tsx
@@ -12,7 +12,7 @@ const TradingCFDIncreases = Loadable(() => import('./_trading-cfd-increases'))
 const StartTrading = Loadable(() => import('./_start-trading'))
 const ThingsToKeep = Loadable(() => import('./_mind-when-trading'))
 const AvailableMarkets = Loadable(() => import('./_available-markets'))
-const target = 'div'
+const target = '#hero-section'
 const options = {
     root: null,
     rootMargin: '0px',
@@ -39,7 +39,7 @@ const CFD = () => {
                     'Trade CFDs on multiple markets. Enjoy high leverage, tight spreads, and risk management features to amplify your potential profits and limit losses.',
                 )}
             />
-            <Hero jc="cneter" ai="center">
+            <Hero jc="cneter" ai="center" id="hero-section">
                 <SmallContainer>
                     <Header as="h1" type="display-title" color="white" align="center">
                         {localize('CFD trading')}

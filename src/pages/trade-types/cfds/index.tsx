63c5310078a6c80aaad47ffd6c2597dd040591e5
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-02-16 11:23:47 +0600

    Revert "Yaswanth/87208/pnl margin bugfix (#4154)"
    This reverts commit 4a3edd244225d3402b730a95b9e977641f18f35f.

--- src/pages/trade-types/cfds/index.tsx
+++ src/pages/trade-types/cfds/index.tsx
@@ -1,36 +1,18 @@
 import React from 'react'
 import Loadable from '@loadable/component'
 import { SmallContainer, Hero } from '../components/_style'
-import { DotLoader, Header } from 'components/elements'
+import WhatIsCFD from './_what_is_cfd'
 import { SEO } from 'components/containers'
+import { Header } from 'components/elements'
 import Layout from 'components/layout/layout'
 import { localize, WithIntl } from 'components/localization'
-import { useHandleLazyLoad } from 'components/hooks/use-handle-lazy-load'
-const WhatIsCFD = Loadable(() => import('./_what_is_cfd'))
 const WhyTradeCFD = Loadable(() => import('./_why-trade-cfd'))
 const TradingCFDIncreases = Loadable(() => import('./_trading-cfd-increases'))
 const StartTrading = Loadable(() => import('./_start-trading'))
 const ThingsToKeep = Loadable(() => import('./_mind-when-trading'))
 const AvailableMarkets = Loadable(() => import('./_available-markets'))
-const target = '#hero-section'
-const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.7,
-}
 
-const lazy_components = (
-    <>
-        <WhatIsCFD fallback={<DotLoader />} />
-        <ThingsToKeep />
-        <TradingCFDIncreases />
-        <WhyTradeCFD />
-        <StartTrading />
-        <AvailableMarkets fallback={<DotLoader />} />
-    </>
-)
 const CFD = () => {
-    const lazyTemplate = useHandleLazyLoad(lazy_components, target, options)
     return (
         <Layout>
             <SEO
@@ -39,14 +21,19 @@ const CFD = () => {
                     'Trade CFDs on multiple markets. Enjoy high leverage, tight spreads, and risk management features to amplify your potential profits and limit losses.',
                 )}
             />
-            <Hero jc="cneter" ai="center" id="hero-section">
+            <Hero jc="cneter" ai="center">
                 <SmallContainer>
                     <Header as="h1" type="display-title" color="white" align="center">
                         {localize('CFD trading')}
                     </Header>
                 </SmallContainer>
             </Hero>
-            {lazyTemplate}
+            <WhatIsCFD />
+            <ThingsToKeep />
+            <TradingCFDIncreases />
+            <WhyTradeCFD />
+            <StartTrading />
+            <AvailableMarkets />
         </Layout>
     )
 }

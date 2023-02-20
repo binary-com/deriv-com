0af8b1a1321bfe28f5ba1e7733400468c11ea099
Niloofar Sadeghi <niloofar.sadeghi@re-work.dev>
2023-02-20 11:48:20 +0330

    fix: review comments

--- src/pages/help-centre/components/_questions-template.tsx
+++ src/pages/help-centre/components/_questions-template.tsx
@@ -34,7 +34,10 @@ const QuestionsTemplate = ({ data }: TQuestionsTemplate) => {
                 title={localize(
                     `_t_Help centre | Frequently asked questions | ${untranslate_category} | Deriv_t_`,
                 )}
-                description={localize(`_t_Frequently asked questions - ${untranslate_category}_t_`)}
+                description={localize(
+                    '_t_Frequently asked questions - {{untranslate_category}}_t_',
+                    { search: untranslate_category },
+                )}
             />
             <Container align="start" justify="flex-start" direction="column">
                 <StyledLink

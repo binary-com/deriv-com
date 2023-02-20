870a3528f0b4cf7872a807fea313f5af5ec95462
yashim-deriv <yashim@deriv.com>
2023-02-20 18:24:55 +0800

    fix: add suggestions from code review

--- src/components/hooks/use-client-country.tsx
+++ src/components/hooks/use-client-country.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react'
+import { cloudflare_trace_url } from 'common/constants'
 
 export const useClientCountry = () => {
     const [clients_country, setClientsCountry] = useState('')
 
     useEffect(() => {
         const fetchClientCountry = async () => {
-            const response = await fetch('https://www.cloudflare.com/cdn-cgi/trace', {
+            const response = await fetch(cloudflare_trace_url, {
                 mode: 'cors',
             })
             const data = await response.text()

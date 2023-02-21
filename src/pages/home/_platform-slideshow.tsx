91823b154c68e9e7e8d47890720ca5e518f38795
yashim-deriv <yashim@deriv.com>
2023-02-21 15:58:04 +0800

    feat: compress specific images

--- src/pages/home/_platform-slideshow.tsx
+++ src/pages/home/_platform-slideshow.tsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect, useMemo, useRef } from 'react'
+import React, { useState, useEffect, useMemo, useRef, ReactNode } from 'react'
 import styled from 'styled-components'
-import { graphql, useStaticQuery } from 'gatsby'
-import type { ImageDataLike } from 'gatsby-plugin-image'
+import { StaticImage } from 'gatsby-plugin-image'
 import { Flex } from 'components/containers'
-import QueryImage from 'components/elements/query-image'
 import device from 'themes/device'
 import useRegion from 'components/hooks/use-region'
 
@@ -16,45 +14,10 @@ const ImagePlaceHolder = styled.div`
     }
 `
 
-const query = graphql`
-    query {
-        hero_platform1: file(relativePath: { eq: "home/hero_platform1.png" }) {
-            ...homePageHeroFadeIn
-        }
-        hero_platform1_eu: file(relativePath: { eq: "home/hero_platform1_eu.png" }) {
-            ...homePageHeroFadeIn
-        }
-        hero_platform2: file(relativePath: { eq: "home/hero_platform2.png" }) {
-            ...homePageHeroFadeIn
-        }
-        hero_platform2_eu: file(relativePath: { eq: "home/hero_platform2_eu.png" }) {
-            ...homePageHeroFadeIn
-        }
-        hero_platform3: file(relativePath: { eq: "home/hero_platform3.png" }) {
-            ...homePageHeroFadeIn
-        }
-        hero_platform4: file(relativePath: { eq: "home/hero_platform4.png" }) {
-            ...homePageHeroFadeIn
-        }
-    }
-`
-
-const StyledImage = styled(QueryImage)<{ $is_hidden: boolean }>`
+const ImageWrapper = styled.div<{ $is_hidden: boolean }>`
     opacity: ${({ $is_hidden }) => ($is_hidden ? '0' : '1')};
     display: ${({ $is_hidden }) => ($is_hidden ? 'none' : 'block')};
     animation: fade 1s ease-in-out;
-
-    @media ${device.tabletL} {
-        animation: unset;
-    }
-
-    .gatsby-image-wrapper {
-        div {
-            @media ${device.tabletL} {
-                transition: none;
-            }
-        }
-    }
 
     @keyframes fade {
         0% {
@@ -68,34 +31,83 @@ const StyledImage = styled(QueryImage)<{ $is_hidden: boolean }>`
 
 const PlatformSlideshow = () => {
     const [active_index, setActiveIndex] = useState(0)
-    const data = useStaticQuery(query)
-
     const { is_row, is_eu, is_region_loading } = useRegion()
 
     const slide_images = useMemo(() => {
         if (is_row)
             return [
-                { key: 'hero1', image: data.hero_platform1 },
-                { key: 'hero2', image: data.hero_platform2 },
-                { key: 'hero3', image: data.hero_platform3 },
-                { key: 'hero4', image: data.hero_platform4 },
+                {
+                    key: 'hero1',
+                    image: (
+                        <StaticImage
+                            src="../../images/common/home/hero_platform1.png"
+                            alt="mobile app deriv go"
+                            formats={['avif', 'webp', 'auto']}
+                            quality={36}
+                        />
+                    ),
+                },
+                {
+                    key: 'hero2',
+                    image: (
+                        <StaticImage
+                            src="../../images/common/home/hero_platform2.png"
+                            alt="laptop dtrader"
+                            formats={['avif', 'webp', 'auto']}
+                            quality={40}
+                        />
+                    ),
+                },
+                {
+                    key: 'hero3',
+                    image: (
+                        <StaticImage
+                            src="../../images/common/home/hero_platform3.png"
+                            alt="laptop mt5"
+                            formats={['avif', 'webp', 'auto']}
+                            quality={40}
+                        />
+                    ),
+                },
+                {
+                    key: 'hero4',
+                    image: (
+                        <StaticImage
+                            src="../../images/common/home/hero_platform4.png"
+                            alt="laptop deriv x"
+                            formats={['avif', 'webp', 'auto']}
+                            quality={40}
+                        />
+                    ),
+                },
             ]
 
         if (is_eu)
             return [
-                { key: 'hero1', image: data.hero_platform1_eu },
-                { key: 'hero2', image: data.hero_platform2_eu },
+                {
+                    key: 'hero1_eu',
+                    image: (
+                        <StaticImage
+                            src="../../images/common/home/hero_platform1_eu.png"
+                            alt="laptop dtrader eu"
+                            formats={['avif', 'webp', 'auto']}
+                            quality={40}
+                        />
+                    ),
+                },
+                {
+                    key: 'hero2_eu',
+                    image: (
+                        <StaticImage
+                            src="../../images/common/home/hero_platform2_eu.png"
+                            alt="laptop mt5 eu"
+                            formats={['avif', 'webp', 'auto']}
+                            quality={40}
+                        />
+                    ),
+                },
             ]
-    }, [
-        data.hero_platform1,
-        data.hero_platform1_eu,
-        data.hero_platform2,
-        data.hero_platform2_eu,
-        data.hero_platform3,
-        data.hero_platform4,
-        is_eu,
-        is_row,
-    ])
+    }, [is_eu, is_row])
 
     const intervalRef = useRef(null)
 
@@ -127,7 +139,7 @@ const PlatformSlideshow = () => {
 }
 
 type SlidesProps = {
-    images: Array<{ key: string; image: ImageDataLike }>
+    images: Array<{ key: string; image: ReactNode }>
     active_index: number
 }
 
@@ -137,15 +149,12 @@ const Slides = ({ images, active_index }: SlidesProps) => {
             {images?.map((slide, index) => {
                 const { key, image } = slide
                 return (
-                    <StyledImage
-                        key={key}
-                        data={image}
-                        alt="platform devices"
-                        width="100%"
-                        loading="eager"
+                    <ImageWrapper
+                        key={`platform-slideshow__${key}`}
                         $is_hidden={active_index !== index}
-                        className="gatsby-image-wrapper"
-                    />
+                    >
+                        {image}
+                    </ImageWrapper>
                 )
             })}
         </>

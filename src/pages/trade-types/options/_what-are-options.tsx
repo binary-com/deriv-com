affd5f71d8812d889cd44eb5b657e596220b81b4
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-02-23 08:44:18 +0100

    fix: text

--- src/pages/trade-types/options/_what-are-options.tsx
+++ src/pages/trade-types/options/_what-are-options.tsx
@@ -57,7 +57,7 @@ const WhatAreOptions = () => {
             </SectionContainer>
             <FullWidthMultiColumn
                 header={<Localize translate_text="Why trade options on Deriv" />}
-                button_title={<Localize translate_text="Don't have a Deriv.com account yet?" />}
+                button_title={<Localize translate_text="Don't have a Deriv account yet?" />}
                 button_text={<Localize translate_text="_t_Create free demo account_t_" />}
             >
                 {digital_options.map((content, index) => (

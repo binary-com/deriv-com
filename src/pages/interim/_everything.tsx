de0acb13ffc4121590e700a138d90ae2261107c7
ashraf-deriv <ashraf@regentmarkets.com>
2023-02-24 11:39:25 +0800

    refactor: interim code keyword extraction second commit

--- src/pages/interim/_everything.tsx
+++ src/pages/interim/_everything.tsx
@@ -44,7 +44,7 @@ const Everything = () => {
         <Box p="8rem 0" bg="var(--color-grey-8)" id="markets">
             <Container fd="column" ai="center">
                 <Header as="h3" type="section-title" align="center" mb="2rem">
-                    {localize('Deriv.com is everything you love about Binary.com, and more')}
+                    <Localize translate_text="_t_Deriv.com is everything you love about Binary.com, and more_t_" />
                 </Header>
                 <Ul jc="center" ai="center">
                     <Li>
@@ -53,11 +53,11 @@ const Everything = () => {
                         </div>
                         <ResponsiveBox ml="1.6rem">
                             <Header as="h4" type="sub-section-title">
-                                {localize('No sign up required')}
+                                <Localize translate_text="_t_No sign up required_t_" />
                             </Header>
                             <Text>
                                 <Localize
-                                    translate_text="Just log in with your <0>Binary.com</0> credentials and start trading right away."
+                                    translate_text="_t_Just log in with your <0>Binary.com</0> credentials and start trading right away._t_"
                                     components={[
                                         <LocalizedLinkText
                                             id="dm-binary-login-link-2"
@@ -85,12 +85,10 @@ const Everything = () => {
                         </div>
                         <ResponsiveBox ml="1.6rem">
                             <Header as="h4" type="sub-section-title">
-                                {localize('Over 100+ financial instruments')}
+                                <Localize translate_text="_t_Over 100+ financial instruments_t_" />
                             </Header>
                             <Text>
-                                {localize(
-                                    'Trade what you like - forex, commodities, stock and synthetic indices.',
-                                )}
+                                <Localize translate_text="_t_Trade what you like - forex, commodities, stock and synthetic indices._t_" />
                             </Text>
                         </ResponsiveBox>
                     </Li>
@@ -105,12 +103,10 @@ const Everything = () => {
                         </div>
                         <ResponsiveBox ml="1.6rem">
                             <Header as="h4" type="sub-section-title">
-                                {localize('An enhanced experience')}
+                                <Localize translate_text="_t_An enhanced experience_t_" />
                             </Header>
                             <Text>
-                                {localize(
-                                    'Trade confidently on a service created by the trusted online trading brand with over 20 years of experience.',
-                                )}
+                                <Localize translate_text="_t_Trade confidently on a service created by the trusted online trading brand with over 20 years of experience._t_" />
                             </Text>
                         </ResponsiveBox>
                     </Li>
@@ -125,12 +121,10 @@ const Everything = () => {
                         </div>
                         <ResponsiveBox ml="1.6rem">
                             <Header as="h4" type="sub-section-title">
-                                {localize('4 trading platforms in one place')}
+                                <Localize translate_text="_t_4 trading platforms in one place_t_" />
                             </Header>
                             <Text>
-                                {localize(
-                                    'Trade the way you like on the Deriv MT5, DTrader, DBot — and the classic SmartTrader.',
-                                )}
+                                <Localize translate_text="_t_Trade the way you like on the Deriv MT5, DTrader, DBot — and the classic SmartTrader._t_" />
                             </Text>
                         </ResponsiveBox>
                     </Li>
@@ -140,12 +134,10 @@ const Everything = () => {
                         </div>
                         <ResponsiveBox ml="1.6rem">
                             <Header as="h4" type="sub-section-title">
-                                {localize('User-friendly, modern design')}
+                                <Localize translate_text="_t_User-friendly, modern design_t_" />
                             </Header>
                             <Text>
-                                {localize(
-                                    'Enjoy an intuitive, easy-to-use interface, even for new traders.',
-                                )}
+                                <Localize translate_text="_t_Enjoy an intuitive, easy-to-use interface, even for new traders._t_" />
                             </Text>
                         </ResponsiveBox>
                     </Li>
@@ -155,18 +147,16 @@ const Everything = () => {
                         </div>
                         <ResponsiveBox ml="1.6rem">
                             <Header as="h4" type="sub-section-title">
-                                {localize('Support when you need it')}
+                                <Localize translate_text="_t_Support when you need it_t_" />
                             </Header>
                             <Text>
-                                {localize(
-                                    'Get access to friendly customer support 7 days a week, an enriched Help centre, plus other helpful content.',
-                                )}
+                                <Localize translate_text="_t_Get access to friendly customer support 7 days a week, an enriched Help centre, plus other helpful content._t_" />
                             </Text>
                         </ResponsiveBox>
                     </Li>
                 </Ul>
                 <LinkButton secondary to="/" mt="2rem">
-                    {localize('Explore Deriv.com now')}
+                    <Localize translate_text="_t_Explore Deriv.com now_t_" />
                 </LinkButton>
             </Container>
         </Box>
--- src/pages/interim/affiliate/_what-to-know.tsx
+++ src/pages/interim/affiliate/_what-to-know.tsx
@@ -14,16 +14,14 @@ const WhatToKnow = () => {
         <SectionContainer>
             <Container direction="column" ai="center">
                 <Header as="h3" type="section-title" mb="0.8rem" align="center">
-                    {localize('What you need to know')}
+                    <Localize translate_text="_t_What you need to know_t_" />
                 </Header>
                 <Text size="var(--text-size-m)" align="center">
-                    {localize(
-                        'As part of our rebranding exercise, your affiliate login URL will change.',
-                    )}
+                    <Localize translate_text="_t_As part of our rebranding exercise, your affiliate login URL will change._t_" />
                 </Text>
                 <Text align="center" size="var(--text-size-m)">
                     <Localize
-                        translate_text="The new affiliate login URL will change to <0>{{login_url}}</0> and you will automatically be redirected to this address on <1>3 June 2020 (Wednesday)</1>."
+                        translate_text="_t_The new affiliate login URL will change to <0>{{login_url}}</0> and you will automatically be redirected to this address on <1>3 June 2020 (Wednesday)</1>._t_"
                         values={{ login_url }}
                         components={[
                             <LocalizedLinkText

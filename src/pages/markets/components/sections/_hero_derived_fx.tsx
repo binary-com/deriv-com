b202b1a5c3961742021fd9e16055d0b6b5c1c96f
konstantinosG-deriv <104083272+konstantinosG-deriv@users.noreply.github.com>
2023-02-28 06:08:46 +0100

    fix: subtask (#4250)
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>

--- src/pages/markets/components/sections/_hero_derived_fx.tsx
+++ src/pages/markets/components/sections/_hero_derived_fx.tsx
@@ -134,15 +134,17 @@ const MarketSubHeader = styled.div`
     font-size: 16px;
     width: 25vw;
     color: var(--color-black-9);
-    text-align: center;
+    text-align: left;
     line-height: 24px;
 
     @media ${device.laptopM} {
         min-width: 40rem;
+        text-align: center;
     }
     @media ${device.mobileM} {
         font-size: 14px;
         width: 83vw;
+        text-align: center;
     }
 `
 const StyledHeader = styled(Header)`

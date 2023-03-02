ba0f74f7a061b97c3f0482c7651f8d284a0971c9
mitra-deriv <mitra@regentmarkets.com>
2023-03-02 21:10:59 +0800

    fix: :rewind: revert logic changes

--- src/pages/markets/basket-indices/index.tsx
+++ src/pages/markets/basket-indices/index.tsx
@@ -8,28 +8,26 @@ import Layout from 'components/layout/layout'
 import { localize, WithIntl } from 'components/localization'
 import { SEO } from 'components/containers'
 
-const BasketPage = () => {
-    return (
-        <Layout type="noNav">
-            <SEO
-                description={localize(
-                    'Trade Basket indices online with Deriv. Open a demo account and enjoy a wide range of currency pairs on our responsive and intuitive trading platforms.',
-                )}
-                title={localize(
-                    'Basket indices trading online | Basket indices trading demo account | Deriv',
-                )}
-            />
-            <DerivedFXHero
-                title={localize('Derived')}
-                description={localize(
-                    'Trade on asset prices derived from real-world or simulated markets. Manage your exposure by selecting the volatility level to suit your risk appetite. Choose from our 24/7 synthetics, derived FX indices, and basket indices.',
-                )}
-            />
-            <NavTab route_from={'basket-indices'} route_offset={650} />
-            <BasketIndices simple_step_content={simple_step_content_basket} />
-            <Signup appearance={Appearances.public} />
-        </Layout>
-    )
-}
+const BasketPage = () => (
+    <Layout type="noNav">
+        <SEO
+            description={localize(
+                'Trade Basket indices online with Deriv. Open a demo account and enjoy a wide range of currency pairs on our responsive and intuitive trading platforms.',
+            )}
+            title={localize(
+                'Basket indices trading online | Basket indices trading demo account | Deriv',
+            )}
+        />
+        <DerivedFXHero
+            title={localize('Derived')}
+            description={localize(
+                'Trade on asset prices derived from real-world or simulated markets. Manage your exposure by selecting the volatility level to suit your risk appetite. Choose from our 24/7 synthetics, derived FX indices, and basket indices.',
+            )}
+        />
+        <NavTab route_from={'basket-indices'} route_offset={650} />
+        <BasketIndices simple_step_content={simple_step_content_basket} />
+        <Signup appearance={Appearances.public} />
+    </Layout>
+)
 
 export default WithIntl()(BasketPage)

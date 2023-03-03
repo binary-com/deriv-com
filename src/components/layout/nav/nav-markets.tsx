8b0f94e111962cf6c4edab289b9fb8ebfe514078
mitra-deriv <mitra@regentmarkets.com>
2023-03-03 17:21:52 +0800

    fix: :bug: fix flickering

--- src/components/layout/nav/nav-markets.tsx
+++ src/components/layout/nav/nav-markets.tsx
@@ -1,21 +1,18 @@
 // Note: When using layout component for partners page, please add type='partners' and padding_top='10rem'
 import React from 'react'
+import { useQueryParam, StringParam } from 'use-query-params'
 import NavDesktop from './components/nav-desktop'
 import NavMarketDesktop from './components/markets/nav-market-desktop'
 import NavMobile from './components/nav-mobile'
 import NavTemplate from './components/nav-template'
 import { PartnerWrapper, PartnerNavigationBarWrapper } from './styles/nav-styles'
-import { usePlatformQueryParam } from 'components/hooks/use-platform-query-param'
 
 const NavMarkets = () => {
-    const { is_deriv_go = true } = usePlatformQueryParam()
-
-    // eslint-disable-next-line no-console
-    console.log(is_deriv_go, 'is_deriv_go')
+    const [platform] = useQueryParam('platform', StringParam)
 
     return (
-        <NavTemplate transparent_background={is_deriv_go}>
-            {!is_deriv_go && (
+        <NavTemplate transparent_background={platform === 'derivgo'}>
+            {platform !== 'derivgo' && (
                 <PartnerWrapper>
                     <NavDesktop />
                     <NavMobile />

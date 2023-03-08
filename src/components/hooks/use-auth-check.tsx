612d1a01a961d0459d408799c431c7d59026814e
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-03-08 16:23:20 +0800

    fix: glitch in cta button get trading

--- src/components/hooks/use-auth-check.tsx
+++ src/components/hooks/use-auth-check.tsx
@@ -3,14 +3,16 @@ import { isLoggedIn } from 'common/utility'
 
 const useAuthCheck = () => {
     const [is_logged_in, setLoggedIn] = useState(false)
+    const [is_auth_checked, setISAuthChecked] = useState(false)
 
     useEffect(() => {
         setLoggedIn(isLoggedIn())
         const checkCookieChange = setInterval(() => setLoggedIn(isLoggedIn()), 800)
+        setISAuthChecked(true)
         return () => clearInterval(checkCookieChange)
     }, [])
 
-    return [is_logged_in]
+    return [is_logged_in, is_auth_checked]
 }
 
 export default useAuthCheck

74a5d13c6ce43393089ca2d99e15c8d12dcb5484
Muhammad Fasih <muhammadfasih@Muhammad-Fasihs-MacBook-Pro-C02T73MMGTFL.local>
2023-03-08 13:30:49 +0800

    fix: added check because build is getting failed

--- src/common/utility.ts
+++ src/common/utility.ts
@@ -391,6 +391,7 @@ export const isLocalhost = () => !!(isBrowser() && process.env.NODE_ENV === 'dev
 
 export const isTestlink = () => !!(isBrowser() && window.location.hostname.includes('binary.sx'))
 
-export const matchHashInURL = (hash: string) => location.hash.replace('#', '') === hash
+export const matchHashInURL = (hash: string) =>
+    isBrowser() && location.hash.replace('#', '') === hash
 
-export const setHashInURL = (hash: string) => (location.hash = `#${hash}`)
+export const setHashInURL = (hash: string) => isBrowser() && (location.hash = `#${hash}`)

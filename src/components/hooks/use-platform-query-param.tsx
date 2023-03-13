2f09b6ca2e83216ccd34ee3e1ba7682d9a7f17bf
Farhan Ahmad Nurzi <125247833+farhan-nurzi-deriv@users.noreply.github.com>
2023-03-13 17:23:09 +0800

    refactor: delete comment
    Co-authored-by: Farzin Mirzaie <72082844+farzin-deriv@users.noreply.github.com>

--- src/components/hooks/use-platform-query-param.tsx
+++ src/components/hooks/use-platform-query-param.tsx
@@ -8,7 +8,6 @@ export const usePlatformQueryParam = () => {
     const [is_deriv_p2p, setIsDerivP2P] = useState(true)
     const [has_platform, setHasPlatform] = useState(false)
 
-    // change value whenever platform query changes
     useEffect(() => {
         setIsDerivGo(platform === 'derivgo')
         setIsDerivP2P(platform === 'p2p')

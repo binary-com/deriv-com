a3802a255475ccfee2bce079941f2e1fd671fa8e
mitra-deriv <mitra@regentmarkets.com>
2023-03-16 11:08:56 +0800

    fix: :bug: fix deriv ez icon

--- src/pages/home/platform-carousel/_utils.tsx
+++ src/pages/home/platform-carousel/_utils.tsx
@@ -23,6 +23,7 @@ import DTraderIcon from 'images/svg/dtrader/dtrader-icon.svg'
 import DMT5Icon from 'images/svg/dmt5/dmt5-icon.svg'
 import DerivGOIcon from 'images/svg/deriv-go/deriv-go-logo.svg'
 import DerivXIcon from 'images/svg/deriv-x/derivx-logo.svg'
+import DerivEZIcon from 'images/svg/deriv-ez/derivez-logo-black.svg'
 import DBotIcon from 'images/svg/dbot/dbot-icon.svg'
 import BinaryBotIcon from 'images/svg/binarybot-icon.svg'
 import SmartTraderIcon from 'images/svg/custom/smarttrader.svg'
@@ -75,7 +76,7 @@ export const platform_details_row: TPlatformDetails[] = [
     },
     {
         title: 'Deriv EZ',
-        icon: DerivXIcon,
+        icon: DerivEZIcon,
         image_key: 'platforms_derivez',
         description: <Localize translate_text="Our mobile-first CFD trading platform" />,
         learn_more_link: '/derivez/',

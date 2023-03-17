9e9637ed0927c88a4e3dc8e8c7cf63a59c5c745a
nuzhy-deriv <nuzhy@deriv.com>
2023-03-17 15:55:54 +0800

    fix: start trading CFDs in Deriv to be single normal screen

--- src/pages/trade-types/cfds/_start-trading.tsx
+++ src/pages/trade-types/cfds/_start-trading.tsx
@@ -14,7 +14,7 @@ const StartTrading = () => {
         <>
             <StartTradingBg>
                 <SmallContainer direction="column" ai="flex-start">
-                    {is_mobile_or_tablet ? (
+                    {!is_mobile_or_tablet ? (
                         <Header as="h3" type="section-title" mb="4rem">
                             {localize('Start trading CFDs on Deriv')}
                         </Header>

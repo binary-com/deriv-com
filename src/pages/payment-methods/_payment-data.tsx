c85445078a541b89ae41c02dc4d81994f32db891
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2023-03-20 11:18:41 +0800

    refactor: update row credit/debit cards note

--- src/pages/payment-methods/_payment-data.tsx
+++ src/pages/payment-methods/_payment-data.tsx
@@ -66,7 +66,7 @@ const payment_data = [
     {
         name: <Localize translate_text="Credit/debit cards" />,
         note: (
-            <Localize translate_text="Withdrawals may take up to 15 working days to reflect on your card." />
+            <Localize translate_text="Withdrawals may take up to 15 working days to reflect on your card. Mastercard and Maestro withdrawals are only available for UK clients." />
         ),
         is_row: true,
         data: [

a0127cee477e356a4468f111e2daebae4d04ce20
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-03-20 15:38:57 +0100

    fix: add t string

--- src/pages/trade-types/cfds/_why-trade-cfd.tsx
+++ src/pages/trade-types/cfds/_why-trade-cfd.tsx
@@ -20,76 +20,76 @@ const WhyTradeCFD = () => {
         <SectionContainer background="rgba(245, 247, 250, 0.64)" padding="4rem 0 4rem">
             <SmallContainer direction="column" ai="flex-start">
                 <Header as="h3" type="section-title" mb="4rem">
-                    {localize('Why trade on CFDs with Deriv')}
+                    {localize('_t_Why trade on CFDs with Deriv_t_')}
                 </Header>
                 <Grid>
                     <WhyTradeItem>
                         <img src={HighLeverge} alt="Leverage and spread" />
                         <Text weight="bold" mb="0.8rem" mt="1.6rem">
                             {is_eu
-                                ? localize('Tight spreads')
-                                : localize('High leverage, tight spreads')}
+                                ? localize('_t_Tight spreads_t_')
+                                : localize('_t_High leverage, tight spreads_t_')}
                         </Text>
                         <Text mb="4rem">
                             {is_eu
                                 ? localize(
-                                      'Take advantage of tight spreads on Deriv’s CFD trading platforms.',
+                                      '_t_Take advantage of tight spreads on Deriv’s CFD trading platforms._t_',
                                   )
                                 : localize(
-                                      'Take advantage of high leverage and tight spreads on Deriv’s CFD trading platforms.',
+                                      '_t_Take advantage of high leverage and tight spreads on Deriv’s CFD trading platforms._t_',
                                   )}
                         </Text>
                     </WhyTradeItem>
                     <WhyTradeItem>
                         <img src={FavoriteMarket} alt="Favourite markets" />
                         <Text weight="bold" mb="0.8rem" mt="1.6rem">
-                            {localize('All your favourite markets')}
+                            {localize('_t_All your favourite markets_t_')}
                         </Text>
                         <Text mb="4rem">
                             {is_eu
                                 ? localize(
-                                      'Trade on all popular markets plus our proprietary synthetic indices that are available 24/7.',
+                                      '_t_Trade on all popular markets plus our proprietary synthetic indices that are available 24/7._t_',
                                   )
                                 : localize(
-                                      'Trade on financial markets plus our proprietary synthetic indices that are available 24/7.',
+                                      '_t_Trade on financial markets plus our proprietary synthetic indices that are available 24/7._t_',
                                   )}
                         </Text>
                     </WhyTradeItem>
                     <WhyTradeItem>
                         <img src={GoLongOrShort} alt="Price movements" />
                         <Text weight="bold" mb="0.8rem" mt="1.6rem">
-                            {localize('Go long or short')}
+                            {localize('_t_Go long or short_t_')}
                         </Text>
                         <Text mb="4rem">
                             {localize(
-                                'Open long or short positions based on your predictions of the market price movements.',
+                                '_t_Open long or short positions based on your predictions of the market price movements._t_',
                             )}
                         </Text>
                     </WhyTradeItem>
                     <WhyTradeItem>
                         <img src={FriendlySupport} alt="Support chat" />
                         <Text weight="bold" mb="0.8rem" mt="1.6rem">
-                            {localize('Expert and friendly support')}
+                            {localize('_t_Expert and friendly support_t_')}
                         </Text>
                         <Text mb="4rem">
-                            {localize('Get expert, friendly support when you need it.')}
+                            {localize('_t_Get expert, friendly support when you need it._t_')}
                         </Text>
                     </WhyTradeItem>
                     <WhyTradeItem>
                         <img src={InstantAccess} alt="Immediate access" />
                         <Text weight="bold" mb="0.8rem" mt="1.6rem">
-                            {localize('Instant access')}
+                            {localize('_t_Instant access_t_')}
                         </Text>
                         <Text mb="4rem">
-                            {localize('Open an account and start trading in minutes.')}
+                            {localize('_t_Open an account and start trading in minutes._t_')}
                         </Text>
                     </WhyTradeItem>
                 </Grid>
                 <Text align="start" width="100%" weight="bold">
-                    {localize("Don't have a Deriv account yet?")}
+                    {localize("_t_Don't have a Deriv account yet?_t_")}
                 </Text>
                 <Button onClick={handleSignup} id="dm-cfd-signup" mt="1.6rem" secondary>
-                    {localize('Create free demo account')}
+                    {localize('_t_Create free demo account_t_')}
                 </Button>
             </SmallContainer>
         </SectionContainer>

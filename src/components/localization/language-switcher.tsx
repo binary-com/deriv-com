7cc447c82aaf14b0ae5af5769b1d4d39c33bdc8e
nuzhy-deriv <nuzhy@deriv.com>
2023-03-24 18:05:06 +0800

    fix: store selected language in cookies

--- src/components/localization/language-switcher.tsx
+++ src/components/localization/language-switcher.tsx
@@ -6,7 +6,7 @@ import language_config from '../../../i18n-config'
 import { useClientInformation } from '../hooks/use-client-information'
 import Dropdown from './language-dropdown'
 import { isProduction } from 'common/websocket/config'
-import { nonENLangUrlReplace } from 'common/utility'
+import { getDomain, nonENLangUrlReplace } from 'common/utility'
 
 type LanguageSwitchProps = {
     i18n?: { language: string }
@@ -71,7 +71,8 @@ const LanguageSwitch = ({ i18n, is_high_nav, is_security }: LanguageSwitchProps)
                     ? current_path.replace(/\//u, '')
                     : nonENLangUrlReplace(current_path)
             }${current_hash}`
-            Cookies.set('user_language', id.replace(/\//g, ''))
+            document.cookie =
+                'user_language=' + id.replace(/\//g, '') + ';domain=.' + getDomain() + ';path=/'
 
             if (path === '/ach/') {
                 localStorage.setItem('i18n', 'ach')

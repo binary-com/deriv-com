ea2289900f01c3e3374df6b5431b96ebb290e702
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-03-27 09:36:53 +0200

    fix: translations for derived ticks

--- src/pages/markets/static/content/_details.tsx
+++ src/pages/markets/static/content/_details.tsx
@@ -270,7 +270,7 @@ export const VolatilityIndicesDetails = () => (
     <DetailsContainer>
         <Text>
             {localize(
-                '_t_These indices correspond to simulated markets with constant volatilities of 10%, 25%, 50%, 75%, 100%, 150% and 250%._t_',
+                '_t_These indices correspond to simulated markets with constant volatilities of 10%, 25%, 50%, 75%, 100%, 150%, and 250%._t_',
             )}
         </Text>
         <Text>
@@ -281,7 +281,7 @@ export const VolatilityIndicesDetails = () => (
         </Text>
         <Text>
             <Localize
-                translate_text="_t_<0>One tick</0> is generated <0>every second</0> for volatility indices <0>10 (1s), 25 (1s), 50 (1s), 75 (1s), 100 (1s), 150 (1s) and 250 (1s)</0>._t_"
+                translate_text="_t_<0>One tick</0> is generated <0>every second</0> for volatility indices <0>10 (1s), 25 (1s), 50 (1s), 75 (1s), 100 (1s), 150 (1s), and 250 (1s)</0>._t_"
                 components={[<strong key={0} />]}
             />
         </Text>

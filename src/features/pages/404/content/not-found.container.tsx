cef9d06ea67df4c8e15cbecd1bccb5d8fe7af68a
Nikita Kolesnik <nikitakolesnik@Nikita-Kolesniks-MacBook-Pro-P45VWV2QJ9.local>
2023-03-29 12:16:21 +0300

    refactor: 404 page to atomic design

--- src/features/pages/404/content/not-found.container.tsx
+++ src/features/pages/404/content/not-found.container.tsx
@@ -1,37 +1,47 @@
 import React from 'react'
 import { button_link, info_items, not_available_image } from './data'
-import { button, container, info } from './not-found.module.scss'
+import * as styles from './not-found.module.scss'
 import Typography from 'features/components/atoms/typography'
 import Button from 'features/components/atoms/button'
 import Link from 'features/components/atoms/link'
+import FlexBox from 'features/components/atoms/flex-box'
 import Image from 'features/components/atoms/image'
-import Container from 'features/components/atoms/container'
 import { Localize } from 'components/localization'
 
 const NotFoundContainer = () => {
     return (
-        <Container.Fixed className={container}>
+        <FlexBox className={styles.not_found}>
             <Image
                 src={not_available_image.src}
                 alt={not_available_image.alt}
                 width="96"
                 height="96"
             />
-
-            <Container.Fixed className={info}>
-                <Typography.Heading as="h3" size={'xxl'}>
+            <FlexBox className={styles.not_found_info} margin={'8x'}>
+                <Typography.Heading
+                    as="h3"
+                    size={'small'}
+                    className={styles.not_found_description}
+                    textcolor={'black-3'}
+                >
                     <Localize translate_text={info_items.header} />
                 </Typography.Heading>
-                <Typography.Heading as="h4" size={'xs'}>
+                <Typography.Heading
+                    as="h4"
+                    size={'xxxs'}
+                    className={styles.not_found_description}
+                    weight={'normal'}
+                    textcolor={'black-3'}
+                >
                     <Localize translate_text={info_items.sub_header} />
                 </Typography.Heading>
-                <Button.Primary className={button}>
-                    <Link url={button_link.url}>
+                <Button.Primary type={'button'} className={styles.not_found_button}>
+                    <Link type={'internal'} url={button_link.url} textcolor={'white'}>
                         <Localize translate_text={button_link.text} />
                     </Link>
                 </Button.Primary>
-            </Container.Fixed>
-        </Container.Fixed>
+            </FlexBox>
+        </FlexBox>
     )
 }
 

5ddd924b6213b9637e6fca03a4133135bcf0ce89
niloofar sadeghi <niloofar.sadeghi@re-work.dev>
2023-04-03 09:45:42 +0330

    refactor: career heros

--- src/pages/careers/_dept-layout-components/_hero.tsx
+++ src/pages/careers/_dept-layout-components/_hero.tsx
@@ -5,12 +5,96 @@ import Container from './_career-container'
 import { HeaderProps } from './_dept-layout.types'
 import { Header, BackgroundImage } from 'components/elements'
 import device from 'themes/device'
+import { useBrowserResize } from 'components/hooks/use-browser-resize'
 
-const StyledBackGroundImage = styled(BackgroundImage)`
-    height: 660px;
+type HeaderDataProps = {
+    data: HeaderProps
+}
 
-    @media ${device.tablet} {
-        height: 560px;
+const query = graphql`
+    query {
+        back_end_bg: file(relativePath: { eq: "careers-2/back-end-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        business_excellence_bg: file(relativePath: { eq: "careers-2/business-excellence-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        compliance_bg: file(relativePath: { eq: "careers-2/compliance-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        cs_bg: file(relativePath: { eq: "careers-2/cs-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        security_bg: file(relativePath: { eq: "careers-2/security-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        finance_bg: file(relativePath: { eq: "careers-2/finance-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        front_end_bg: file(relativePath: { eq: "careers-2/front-end-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        internal_audit_bg: file(relativePath: { eq: "careers-2/internal-audit-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        marketing_bg: file(relativePath: { eq: "careers-2/marketing-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        payments_bg: file(relativePath: { eq: "careers-2/payments-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        human_resources_bg: file(relativePath: { eq: "careers-2/human-resources-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        product_bg: file(relativePath: { eq: "careers-2/product-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        pm_bg: file(relativePath: { eq: "careers-2/pm-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        software_testing_bg: file(relativePath: { eq: "careers-2/software-testing-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        trading_operations_bg: file(relativePath: { eq: "careers-2/trading-operations-bg.png" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
+        business_intelligence_bg: file(
+            relativePath: { eq: "careers-2/business-intelligence-bg.png" }
+        ) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+            }
+        }
     }
 `
 
@@ -43,77 +127,17 @@ const StyledContainer = styled(Container)`
     }
 `
 
-const query = graphql`
-    query {
-        back_end_bg: file(relativePath: { eq: "careers-2/back-end-bg.png" }) {
-            ...fadeIn
-        }
-        business_excellence_bg: file(relativePath: { eq: "careers-2/business-excellence-bg.png" }) {
-            ...fadeIn
-        }
-        compliance_bg: file(relativePath: { eq: "careers-2/compliance-bg.png" }) {
-            ...fadeIn
-        }
-        cs_bg: file(relativePath: { eq: "careers-2/cs-bg.png" }) {
-            ...fadeIn
-        }
-        security_bg: file(relativePath: { eq: "careers-2/security-bg.png" }) {
-            ...fadeIn
-        }
-        finance_bg: file(relativePath: { eq: "careers-2/finance-bg.png" }) {
-            ...fadeIn
-        }
-        front_end_bg: file(relativePath: { eq: "careers-2/front-end-bg.png" }) {
-            ...fadeIn
-        }
-        internal_audit_bg: file(relativePath: { eq: "careers-2/internal-audit-bg.png" }) {
-            ...fadeIn
-        }
-        marketing_bg: file(relativePath: { eq: "careers-2/marketing-bg.png" }) {
-            ...fadeIn
-        }
-        payments_bg: file(relativePath: { eq: "careers-2/payments-bg.png" }) {
-            ...fadeIn
-        }
-        human_resources_bg: file(relativePath: { eq: "careers-2/human-resources-bg.png" }) {
-            ...fadeIn
-        }
-        product_bg: file(relativePath: { eq: "careers-2/product-bg.png" }) {
-            ...fadeIn
-        }
-        pm_bg: file(relativePath: { eq: "careers-2/pm-bg.png" }) {
-            ...fadeIn
-        }
-        software_testing_bg: file(relativePath: { eq: "careers-2/software-testing-bg.png" }) {
-            ...fadeIn
-        }
-        trading_operations_bg: file(relativePath: { eq: "careers-2/trading-operations-bg.png" }) {
-            ...fadeIn
-        }
-        business_intelligence_bg: file(
-            relativePath: { eq: "careers-2/business-intelligence-bg.png" }
-        ) {
-            ...fadeIn
-        }
-    }
-`
-
-type HeaderDataProps = {
-    data: HeaderProps
-}
-
 const Hero = (header_data: HeaderDataProps) => {
     const data = useStaticQuery(query)
+    const [is_mobile] = useBrowserResize(768)
+    const image_height = is_mobile ? '560px' : '660px'
+
     return (
-        <StyledBackGroundImage
+        <BackgroundImage
             data={data[header_data.data.background]}
-            alt={'Deriv careers'}
-            style={{
-                backgroundSize: `cover`,
-                backgroundColor: 'var(--color-black)',
-                maxWidth: '100%',
-            }}
-            dark="0.3"
+            alt="Deriv careers"
+            child_style={{ height: image_height }}
+            image_opacity="0.3"
         >
             <StyledContainer>
                 <StyledHeader align="center" as="h1" type="display-title">
@@ -123,7 +147,7 @@ const Hero = (header_data: HeaderDataProps) => {
                     {header_data.data.subtitle}
                 </Subheadline>
             </StyledContainer>
-        </StyledBackGroundImage>
+        </BackgroundImage>
     )
 }
 

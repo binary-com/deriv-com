22f8020e5cfe7c7eb7b328fab058d3ac2b544573
ashraf-deriv <ashraf@regentmarkets.com>
2023-04-04 11:08:09 +0800

    refactor: query param fetch from query string plugin

--- src/components/hooks/use-platform-query-param.tsx
+++ src/components/hooks/use-platform-query-param.tsx
@@ -1,18 +1,22 @@
 import { useEffect, useState } from 'react'
-import { useQueryParamString } from 'react-use-query-param-string'
+import queryString from 'query-string'
 
 export const usePlatformQueryParam = () => {
-    const [platform] = useQueryParamString('platform', '')
+    type MyQuery = {
+        platform: string
+    }
+    const qparam = location.search && (queryString.parse(location.search) as MyQuery)
+    const { platform } = qparam
     const platforms = ['derivgo', 'p2p']
     const [is_deriv_go, setIsDerivGo] = useState(true)
     const [is_deriv_p2p, setIsDerivP2P] = useState(true)
     const [has_platform, setHasPlatform] = useState(false)
 
     useEffect(() => {
-        setIsDerivGo(platform === 'derivgo')
-        setIsDerivP2P(platform === 'p2p')
-        setHasPlatform(platforms.includes(platform))
-    }, [platform])
+        setIsDerivGo(qparam.platform === 'derivgo')
+        setIsDerivP2P(qparam.platform === 'p2p')
+        setHasPlatform(platforms.includes(qparam.platform))
+    }, [qparam])
 
     return { platform, has_platform, is_deriv_p2p, is_deriv_go }
 }

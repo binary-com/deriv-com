10b00d0cb9c1833c9d7fe7117a907b1b8bb6e6ae
Michio <mohsen@re-work.dev>
2023-04-05 23:02:36 +0330

    fix: sign up title alignment

--- src/features/components/templates/signup/with-banner/banner/index.tsx
+++ src/features/components/templates/signup/with-banner/banner/index.tsx
@@ -14,9 +14,6 @@ const PublicSignupBanner = () => {
             grow="1"
             margin_inline="20x"
             className={'public_signup_banner_container'}
-            md={{
-                padding_inline: '40x',
-            }}
         >
             <div className={'public_signup_banner'}>
                 <StaticImage
@@ -28,7 +25,11 @@ const PublicSignupBanner = () => {
             </div>
             <Link url={{ type: 'company', url_name: 'deriv_app' }} no_hover>
                 <Flex.Box justify="around" align="center" gap="4x">
-                    <Typography.Heading size="medium" textcolor="white">
+                    <Typography.Heading
+                        size="medium"
+                        textcolor="white"
+                        className={'public_signup_title'}
+                    >
                         <Localize translate_text={'_t_Get a taste of the Deriv experience_t_'} />
                     </Typography.Heading>
                     <Typography.Heading size="large" textcolor="white">

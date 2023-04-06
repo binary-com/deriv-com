ce48e7c215d5e420482133694dfede6a7dc1a9ce
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2023-04-06 14:50:44 +0800

    ci: change sampleRate to 100% to test the env

--- gatsby-config.js
+++ gatsby-config.js
@@ -328,7 +328,7 @@ module.exports = {
             resolve: 'gatsby-plugin-datadog',
             options: {
                 site: 'datadoghq.com',
-                sampleRate: 1,
+                sampleRate: 100,
                 replaySampleRate: 20,
                 enabled: true,
                 env: 'production',

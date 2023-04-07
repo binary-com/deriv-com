68546ef7f86d5556e8b9e2364660c0cc9a00fa17
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2023-04-07 12:16:28 +0800

    ci: disable datadog to unblock release

--- gatsby-config.js
+++ gatsby-config.js
@@ -323,25 +323,25 @@ module.exports = {
                 analyzerMode: 'disabled',
                 generateStatsFile: process.env.GENERATE_JSON_STATS === 'true',
             },
-        },
-        {
-            resolve: 'gatsby-plugin-datadog',
-            options: {
-                site: 'datadoghq.com',
-                sessionSampleRate: 1,
-                sessionReplaySampleRate: 1,
-                enabled: true,
-                env: 'production',
-                service:'deriv.com',
-                trackUserInteractions: true,
-                trackResources: true,
-                trackLongTasks: true,
-                defaultPrivacyLevel:'mask-user-input',
-                rum: {
-                    applicationId: process.env.DATADOG_APPLICATION_ID,
-                    clientToken: process.env.DATADOG_CLIENT_TOKEN,
-                },
-            }
+        // },
+        // {
+        //     resolve: 'gatsby-plugin-datadog',
+        //     options: {
+        //         site: 'datadoghq.com',
+        //         sessionSampleRate: 1,
+        //         sessionReplaySampleRate: 1,
+        //         enabled: true,
+        //         env: 'production',
+        //         service:'deriv.com',
+        //         trackUserInteractions: true,
+        //         trackResources: true,
+        //         trackLongTasks: true,
+        //         defaultPrivacyLevel:'mask-user-input',
+        //         rum: {
+        //             applicationId: process.env.DATADOG_APPLICATION_ID,
+        //             clientToken: process.env.DATADOG_CLIENT_TOKEN,
+        //         },
+        //     }
         }
     ],
 }

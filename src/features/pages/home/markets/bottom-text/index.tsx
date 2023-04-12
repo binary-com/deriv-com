27e9e76f8cabe618b97e519a75a9e6d9cab5226f
Hasan Mobarak <126637868+hasan-deriv@users.noreply.github.com>
2023-04-12 17:19:07 +0800

    feat: added bottom text to markets section (#4505)
    * feat: added bottom text to markets section
    
    * fix: box alignment
    
    * fix: box alignment

---
+++ src/features/pages/home/markets/bottom-text/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Typography from 'features/components/atoms/typography'
+import { Localize } from 'components/localization'
+import Button from 'features/components/atoms/button'
+import Flex from 'features/components/atoms/flex-box'
+
+const BottomText = () => {
+    return (
+        <Flex.Box
+            direction="col"
+            container="fluid"
+            justify="center"
+            align="center"
+            pt="10x"
+            md={{ pt: '19x' }}
+        >
+            <Typography.Paragraph size="large" mb="10x" textcolor="black">
+                <Localize translate_text="_t_Want to know more about CFD trading conditions for the instruments we offer?_t_" />
+            </Typography.Paragraph>
+            <Button.Primary>
+                <Localize translate_text="_t_Check trading specs_t_" />
+            </Button.Primary>
+        </Flex.Box>
+    )
+}
+
+export default BottomText

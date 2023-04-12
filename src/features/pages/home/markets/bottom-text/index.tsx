41dc9a0cb35d2a833b2551c0bac700b84f4667f3
hasan-deriv <abuhasan@deriv.com>
2023-04-12 17:06:49 +0800

    fix: box alignment

--- src/features/pages/home/markets/bottom-text/index.tsx
+++ src/features/pages/home/markets/bottom-text/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import Typography from 'features/components/atoms/typography'
 import { Localize } from 'components/localization'
-import Container from 'features/components/atoms/container'
 import Button from 'features/components/atoms/button'
+import Flex from 'features/components/atoms/flex-box'
 
 const BottomText = () => {
     return (
-        <Container.Fluid align="center" pt="10x" md={{ pt: '19x' }}>
+        <Flex.Box
+            direction="col"
+            container="fluid"
+            justify="center"
+            align="center"
+            pt="10x"
+            md={{ pt: '19x' }}
+        >
             <Typography.Paragraph size="large" mb="10x" textcolor="black">
                 <Localize translate_text="_t_Want to know more about CFD trading conditions for the instruments we offer?_t_" />
             </Typography.Paragraph>
             <Button.Primary>
                 <Localize translate_text="_t_Check trading specs_t_" />
             </Button.Primary>
-        </Container.Fluid>
+        </Flex.Box>
     )
 }
 

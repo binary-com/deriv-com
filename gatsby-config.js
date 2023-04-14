152bb3c8a7672eefcb58ef169e7e8fb576445448
Nikita Kolesnik <nikitakolesnik@Nikita-Kolesniks-MacBook-Pro-P45VWV2QJ9.local>
2023-04-14 14:32:33 +0300

    feat: add plugin back

--- gatsby-config.js
+++ gatsby-config.js
@@ -341,21 +341,30 @@ module.exports = {
             options: {
                 site: 'datadoghq.com',
                 sessionSampleRate: parseInt(process.env.DATADOG_SESSION_SAMPLE_RATE) || 10,
-                sessionReplaySampleRate: parseInt(process.env.DATADOG_SESSION_REPLAY_SAMPLE_RATE) || 1,
+                sessionReplaySampleRate:
+                    parseInt(process.env.DATADOG_SESSION_REPLAY_SAMPLE_RATE) || 1,
                 enabled: true,
                 env: 'production',
-                service:'deriv.com',
+                service: 'deriv.com',
                 trackUserInteractions: true,
                 trackFrustrations: true,
                 trackResources: true,
                 trackLongTasks: true,
                 enableExperimentalFeatures: ['clickmap'],
-                defaultPrivacyLevel:'mask-user-input',
+                defaultPrivacyLevel: 'mask-user-input',
                 rum: {
                     applicationId: process.env.DATADOG_APPLICATION_ID,
                     clientToken: process.env.DATADOG_CLIENT_TOKEN,
                 },
-            }
-        }
+            },
+        },
+        {
+            resolve: 'gatsby-source-strapi',
+            options: {
+                apiURL: process.env.STRAPI_URL,
+                accessToken: process.env.STRAPI_TOKEN,
+                collectionTypes: strapi_config,
+            },
+        },
     ],
 }

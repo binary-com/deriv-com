e14fd741915e1143005a16467d02f30afaa8bc8f
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-04-19 08:09:47 +0200

    fix: eu condition

--- src/pages/trade-types/multiplier/_how-options-works.tsx
+++ src/pages/trade-types/multiplier/_how-options-works.tsx
@@ -141,7 +141,15 @@ const HowOptionsWorks = () => {
                     </SideTab.Panel>
                     <SideTab.Panel
                         label={<Localize translate_text="2. Trade type" />}
-                        description={<Localize translate_text="_t_Choose 'Multipliers'_t_" />}
+                        description={
+                            <Localize
+                                translate_text={
+                                    is_eu
+                                        ? "_t_Choose 'Multipliers'_t_"
+                                        : 'Choose ‘Multipliers’ from the list of trade types.'
+                                }
+                            />
+                        }
                     >
                         {is_eu ? (
                             <StaticImage

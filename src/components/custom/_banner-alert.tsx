4c56a1008894eb6a872f723fcf733d47b1c62102
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-04-24 11:50:01 +0400

    fix: translation issue fix

--- src/components/custom/_banner-alert.tsx
+++ src/components/custom/_banner-alert.tsx
@@ -170,7 +170,7 @@ const BannerAlert: FC<TProps> = ({ bannerType }) => {
                         minHeight={cookieBannerProps.minHeight}
                     >
                         <StyledText>
-                            <Localize translate_text="_t_Cookies help us to give you a better experience and personalised content on our site._t_" />
+                            <Localize translate_text="_t_Cookies help us to give you a better experience and personalised content on our site. _t_" />
                             <Localize
                                 translate_text="_t_If you agree to our use of cookies, click on Accept. For more information, <0>see our policy</0>._t_"
                                 components={[

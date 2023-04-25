ad1e8931b6e6f2cc02850e73add31a16574d4ce2
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-04-26 00:40:14 +0400

    fix: code smell removed

--- src/components/custom/_banner-alert.tsx
+++ src/components/custom/_banner-alert.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useMemo, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { Flex } from 'components/containers'
 import { Button } from 'components/form'

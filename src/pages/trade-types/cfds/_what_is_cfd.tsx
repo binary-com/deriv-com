7d331c6eb65f95a605fd5d9f3d4b17fc8ad0e023
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-04-26 08:53:28 +0200

    fix: margin

--- src/pages/trade-types/cfds/_what_is_cfd.tsx
+++ src/pages/trade-types/cfds/_what_is_cfd.tsx
@@ -27,14 +27,14 @@ const WhatIsCFD = () => {
                     </Text>
                 ) : (
                     <>
-                        <Text mb="2.4rem">{localize('On Deriv, you can trade CFDs with:')}</Text>
-                        <Text mb="2.4rem">
+                        <Text mb="1.8rem">{localize('On Deriv, you can trade CFDs with:')}</Text>
+                        <Text mb="1.8rem">
                             <Localize
                                 translate_text="<0>High leverage</0> — Leverage allows you to open larger positions with a smaller balance in your trading account. The higher the leverage, the less money you need."
                                 components={[<strong key={0} />]}
                             />
                         </Text>
-                        <Text mb="2.4rem">
+                        <Text mb="1.8rem">
                             <Localize
                                 translate_text="<0>Tight spreads</0> — The spread is the difference between the buy price and sell price. The tighter the spread, the lower the cost to enter the market."
                                 components={[<strong key={0} />]}

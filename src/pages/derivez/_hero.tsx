945ff66008fd62bcd28ac233260b28a445d78f1c
Nikita Kolesnik <nikitakolesnik@Nikita-Kolesniks-MacBook-Pro-P45VWV2QJ9.local>
2023-04-26 08:45:21 +0300

    refactor: rebranding

--- src/pages/derivez/_hero.tsx
+++ src/pages/derivez/_hero.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
 import styled from 'styled-components'
+import * as style from './hero-content.module.scss'
+import HeroPhone from 'images/common/deriv-ez/hero-phone.png'
+import HeroPhoneMobile from 'images/common/deriv-ez/hero-phone-mobile.png'
 import HeroImageMobile from 'images/common/deriv-ez/background-image-mobile.png'
-import { Header, QueryImage } from 'components/elements'
 import HeroImage from 'images/common/deriv-ez/background-derivez.png'
 import DerivEZLogo from 'images/svg/deriv-ez/derivez-logo.svg'
-import { TString } from 'types/generics'
-import { localize, Localize } from 'components/localization'
+import FlexBox from 'features/components/atoms/flex-box'
+import Image from 'features/components/atoms/image'
+import Typography from 'features/components/atoms/typography'
+import TradeHeroContainer from 'features/components/templates/hero-banners/trade'
+import useBreakpoints from 'components/hooks/use-breakpoints'
+import { Localize } from 'components/localization'
 import device from 'themes/device'
 
-const query = graphql`
-    query {
-        deriv_ez: file(relativePath: { eq: "deriv-ez/hero-phone.png" }) {
-            ...bannerImage
-        }
-    }
-`
 const Wrapper = styled.div`
     display: flex;
     background-image: url(${HeroImage});
@@ -36,90 +34,53 @@ const Wrapper = styled.div`
     }
 `
 const ContentWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 16px;
     flex: 2;
 
     @media ${device.tablet} {
         margin-top: 68px;
     }
 `
-const LogoWrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-`
-
-const DLogo = styled.img`
-    margin-right: 1.6rem;
-    margin-bottom: 6px;
-`
-const StyledHeader = styled(Header)`
-    color: var(--color-white);
-    display: flex;
-    align-items: center;
-    margin-top: 0;
-    font-size: 2.4rem;
-
-    @media ${device.laptopM} {
-        font-size: 24px;
-    }
-    @media ${device.tablet} {
-        font-size: 20px;
-        font-weight: normal;
-    }
-`
-const StyledHeaderTitle = styled.div`
-    color: var(--color-white);
-    display: flex;
-    margin-top: 0;
-    font-size: 64px;
-    font-weight: 700;
-    line-height: 80px;
-    max-width: 80%;
-
-    @media ${device.laptopM} {
-        font-size: 32px;
-        line-height: 48px;
-    }
-`
-const ImageWrapper = styled.div`
-    display: flex;
-    flex: 1;
-    justify-content: center;
-    @media ${device.tablet} {
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        max-width: 70%;
-    }
-`
-const hero_logo_title: TString = '_t_Deriv EZ_t_'
-const hero_header: TString = '_t_An intuitive, easy-to-get-started CFDs trading platform_t_'
-const hero_background_alt: TString = '_t_Deriv EZ online trading platform_t_'
 
 const DerivEZHero = () => {
-    const data = useStaticQuery(query)
+    const { is_mobile } = useBreakpoints()
+
     return (
-        <Wrapper>
-            <ContentWrapper>
-                <LogoWrapper>
-                    <DLogo src={DerivEZLogo} alt="logo" width="32" height="32" />
-                    <StyledHeader as="h4" type="sub-section-title" weight="500">
-                        <Localize translate_text={hero_logo_title} />
-                    </StyledHeader>
-                </LogoWrapper>
-                <StyledHeaderTitle>
-                    <Localize translate_text={hero_header} />
-                </StyledHeaderTitle>
-            </ContentWrapper>
-            <ImageWrapper>
-                <QueryImage data={data['deriv_ez']} alt={localize(hero_background_alt)} />
-            </ImageWrapper>
-        </Wrapper>
+        <TradeHeroContainer
+            container={'fixed'}
+            justify={'center'}
+            direction={'col-reverse'}
+            md={{ direction: 'row' }}
+        >
+            <FlexBox.Box
+                className={style.hero_spacing}
+                direction={'col'}
+                justify={'center'}
+                gap={'8x'}
+                ml={'8x'}
+            >
+                <FlexBox.Box direction={'row'} justify={'start'} align={'center'}>
+                    <Image src={DerivEZLogo} alt="logo" width="32" height="32" />
+                    <Typography.Paragraph
+                        size={'xlarge'}
+                        color={'secondary'}
+                        ml={'8x'}
+                        md={{ weight: 'normal' }}
+                        lg={{ weight: 'bold' }}
+                    >
+                        <Localize translate_text="_t_Deriv EZ_t_" />
+                    </Typography.Paragraph>
+                </FlexBox.Box>
+                <Typography.Heading size={'xlarge'} color={'secondary'} weight={'bold'}>
+                    <Localize translate_text="_t_An intuitive, easy-to-get-started CFDs trading platform_t_" />
+                </Typography.Heading>
+            </FlexBox.Box>
+            <FlexBox.Item align_self={'center'} pt={'40x'} style={{ zIndex: '1' }}>
+                <Image
+                    src={is_mobile ? HeroPhoneMobile : HeroPhone}
+                    alt="Deriv EZ online trading platform"
+                />
+            </FlexBox.Item>
+        </TradeHeroContainer>
     )
 }
 export default DerivEZHero

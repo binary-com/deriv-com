5fb6a179adf43986314b7ff65942dd1f84e0e8c3
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-05-01 17:23:04 +0600

    Beta signup page (#4620)
    * fix: updated sign up page
    
    * fix: updated header
    
    * auto-pull-translation
    
    * chore: fix conflict and spacing issue
    
    * chore: update font size
    
    ---------
    
    Co-authored-by: meenu-deriv <meenakshi@deriv.com>

--- src/components/custom/_signup-new.tsx
+++ src/components/custom/_signup-new.tsx
@@ -1,13 +1,13 @@
 import React, { useState, CSSProperties } from 'react'
 import styled from 'styled-components'
 import AgreementLabel from './_agreement-label'
+import Apple from 'images/svg/custom/apple-signup.svg'
+import Facebook from 'images/svg/custom/fb-signup.svg'
 import { Input, Button } from 'components/form'
-import { Header, LinkText, LocalizedLinkText, Text } from 'components/elements'
+import { Header, LinkText, LocalizedLinkText } from 'components/elements'
 import { localize, Localize } from 'components/localization'
 import device from 'themes/device'
 // SVG
-import Apple from 'images/svg/custom/apple.svg'
-import Facebook from 'images/svg/custom/facebook-blue.svg'
 import Google from 'images/svg/custom/google.svg'
 import useRegion from 'components/hooks/use-region'
 
@@ -25,11 +25,7 @@ type SignupNewProps = {
 }
 
 type SocialButtonProps = {
-    bgColor?: string
-}
-
-type StyledTextProps = {
-    tabletFontSize?: string
+    bg_color?: string
 }
 
 const SignupContent = styled.div`
@@ -39,65 +35,18 @@ const SignupContent = styled.div`
     flex-direction: column;
     padding: 4rem;
     background-color: var(--color-white);
-    border-radius: 0.6rem;
-    box-shadow: 0 1.6rem 2rem 0 rgba(0, 0, 0, 0.1);
+    box-shadow: 0 0 24px rgba(0, 0, 0, 0.05);
+    border-radius: 8px;
 
     @media ${device.tablet} {
-        width: 53rem;
-    }
-    @media ${device.mobileL} {
-        width: 100%;
-        padding: 6rem 2rem;
+        width: auto;
+        box-shadow: none;
+        padding: 2rem;
     }
 `
 const StyledHeader = styled(Header)`
     @media ${device.tabletL} {
         font-size: 3rem;
-        margin-bottom: 3rem;
-    }
-`
-
-const SubTitle = styled(Text)`
-    @media ${device.tabletL} {
-        font-size: 2rem;
-        margin-bottom: 1rem;
-    }
-`
-const Line = styled.div`
-    width: 130px;
-    height: 1px;
-    background-color: var(--color-grey-7);
-`
-const StyledText = styled(Text)<StyledTextProps>`
-    @media (max-width: 340px) {
-        width: 17rem;
-    }
-    @media ${device.tabletL} {
-        font-size: ${(props) => props.tabletFontSize || 'var(--text-size-xxs)'};
-    }
-`
-
-const InputGroup = styled.div`
-    position: relative;
-    width: 100%;
-    margin: 2.5rem 0 0.6rem;
-
-    @media ${device.mobileL} {
-        margin: 25px 0 16px 0;
-    }
-`
-const EmailButton = styled(Button)<{ isChecked?: boolean }>`
-    width: 100%;
-    font-size: 1.4rem;
-    margin-bottom: 0.4rem;
-    margin-top: 3.2rem;
-
-    @media ${device.tabletL} {
-        margin-top: 24px;
-    }
-
-    @media ${device.mobileL} {
-        font-size: 1.75rem;
     }
 `
 const SignupWithContainer = styled.div`
@@ -106,75 +55,89 @@ const SignupWithContainer = styled.div`
     flex-direction: row;
     align-items: center;
     margin-top: 2.4rem;
-
     @media ${device.tabletL} {
         margin-top: 4rem;
     }
 `
+const Line = styled.div`
+    width: 30rem;
+    height: 1px;
+    background-color: var(--color-grey-7);
+`
+const SubTitle = styled(Header)`
+    margin-top: 8px;
 
+    @media ${device.mobileL} {
+        font-size: 18px;
+    }
+`
+const InputGroup = styled.div`
+    position: relative;
+    width: 100%;
+    margin: 2.4rem 0 0.6rem;
+
+    @media ${device.tabletL} {
+        margin: 25px 0 16px 0;
+    }
+`
+const EmailButton = styled(Button)<{ isChecked?: boolean }>`
+    width: 100%;
+    font-size: 1.4rem;
+    margin-bottom: 0.4rem;
+    margin-top: 3.2rem;
+    border-radius: 16px;
+
+    @media ${device.tabletL} {
+        margin-top: 24px;
+    }
+`
 const SocialButton = styled(Button)<SocialButtonProps>`
     display: inline-flex;
     justify-content: center;
     align-items: center;
     box-shadow: none;
-    background-color: ${(props) => props.bgColor || 'var(--color-white)'};
+    height: 4rem;
+    background-color: ${(props) => props.bg_color || 'var(--color-white)'};
     border: solid 1px var(--color-grey-21);
-    width: 12.5rem;
-    height: 3.8rem;
     padding: 0.5rem 0;
+    border-radius: 16px;
 
     &:hover {
-        background: ${(props) => {
-            if (props.provider === 'facebook') return 'var(--color-grey-4)'
-        }};
+        background-color: ${(props) => props.bg_color || 'var(--color-white)'};
     }
-
     @media ${device.tabletL} {
-        width: 100%;
-        height: 6rem;
-        margin-top: 1rem;
+        height: 4.8rem;
     }
-    @media ${device.mobileM} {
-        &:first-child {
-            margin-right: 1.2rem;
-        }
+`
+const SocialText = styled(Header)`
+    width: auto;
+    margin-right: 1.4rem;
+    margin-left: 0.7rem;
+    color: ${({ color }) => color || 'var(--color-black-9)'};
+    @media ${device.tabletL} {
+        margin-left: 2.7rem;
     }
 `
 const SocialWrapper = styled.div<CSSProperties>`
     width: 100%;
     margin-top: 2.4rem;
-    display: flex;
     justify-content: space-between;
-
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
     @media ${device.tabletL} {
-        flex-direction: column;
-        margin-top: 1rem;
-    }
-    @media ${device.mobileS} {
-        justify-content: space-between;
+        gap: 8px;
     }
 `
-const LoginText = styled(Text)`
+const LoginText = styled(Header)`
     text-align: center;
     align-self: center;
-    margin-top: 1.6rem;
+    margin-top: 2.6rem;
 
     @media ${device.tabletL} {
         margin-top: ${(props) => props.mt};
         margin-bottom: ${(props) => props.mb};
         font-size: 2rem;
-    }
-`
-const SocialText = styled(Text)`
-    margin-right: 1.4rem;
-    margin-left: 0.7rem;
-    font-weight: 500;
-    font-size: 1.2rem;
-    color: var(--color-grey-16);
-
-    @media ${device.tabletL} {
-        margin-left: 2.7rem;
-        font-size: 14px;
     }
 `
 const StyledLinkText = styled(LinkText)`
@@ -186,19 +149,6 @@ const StyledLocalizedLink = styled(LocalizedLinkText)`
         font-size: 10px;
     }
 `
-
-const StyledBinaryLogo = styled.img`
-    height: 42px;
-    width: 43px;
-    margin-right: 8px;
-    margin-top: 1rem;
-
-    @media ${device.mobileL} {
-        margin-top: 8px;
-        height: 40px;
-        width: 40px;
-    }
-`
 const SignupNew = ({
     autofocus,
     clearEmail,
@@ -208,7 +158,6 @@ const SignupNew = ({
     handleLogin,
     handleSocialSignup,
     handleValidation,
-    is_ppc,
     is_submitting,
 }: SignupNewProps) => {
     const [is_checked, setChecked] = useState(false)
@@ -223,7 +172,9 @@ const SignupNew = ({
             <StyledHeader as="h4" type="sub-section-title" mb="0.8rem">
                 {localize('Sign up')}
             </StyledHeader>
-            <SubTitle>{localize('Enter your email address to begin')}</SubTitle>
+            <SubTitle type="paragraph-1" as="p" weight="normal">
+                {localize('Enter your email address to begin.')}
+            </SubTitle>
 
             <InputGroup>
                 <Input
@@ -257,13 +208,26 @@ const SignupNew = ({
                 {localize('Create demo account')}
             </EmailButton>
             <Header as="p" type="small" weight="400" color="grey-5" mt="0.8rem">
-                <Localize translate_text="By signing up for a Deriv account, you agree to receive occasional updates about our products, services, and events. You can unsubscribe at any time in your account settings." />
+                <Localize
+                    translate_text="By pressing “Create demo account”, you confirm that you are 18 or older. You understand that we may use your email address to send you information about Deriv products and services as well as market news. You can always unsubscribe from these emails in your account settings. For more information, please take a look at Deriv’s <0>Security and privacy.</0>"
+                    components={[
+                        <StyledLocalizedLink
+                            key={0}
+                            to={`/tnc${is_eu ? '/eu' : ''}/security-and-privacy.pdf`}
+                            size="1.2rem"
+                            color="red"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                            external
+                        />,
+                    ]}
+                />
             </Header>
             <SignupWithContainer>
                 <Line />
-                <StyledText color="grey-5" align="center" tabletFontSize="12px">
+                <Header color="grey-5" weight="normal" align="center" as="p" type="paragraph-2">
                     {localize('Or sign up with')}
-                </StyledText>
+                </Header>
                 <Line />
             </SignupWithContainer>
 
@@ -277,7 +241,9 @@ const SignupNew = ({
                     social
                 >
                     <img src={Google} alt="google" width="24" height="24" />
-                    <SocialText>Google</SocialText>
+                    <SocialText type="paragraph-2" as="p" align="center">
+                        Google
+                    </SocialText>
                 </SocialButton>
                 <SocialButton
                     onClick={handleSocialSignup}
@@ -286,9 +252,12 @@ const SignupNew = ({
                     id="dm-signup-facebook"
                     type="button"
                     social
+                    bg_color="var(--color-blue-15)"
                 >
                     <img src={Facebook} alt="facebook" width="24" height="24" />
-                    <SocialText>Facebook</SocialText>
+                    <SocialText type="paragraph-2" color="var(--color-white)" as="p" align="center">
+                        Facebook
+                    </SocialText>
                 </SocialButton>
                 <SocialButton
                     onClick={handleSocialSignup}
@@ -297,12 +266,15 @@ const SignupNew = ({
                     id="dm-signup-apple"
                     type="button"
                     social
+                    bg_color="var(--color-black)"
                 >
                     <img src={Apple} alt="apple" width="24" height="24" />
-                    <SocialText>Apple</SocialText>
+                    <SocialText type="paragraph-2" color="var(--color-white)" as="p" align="center">
+                        Apple
+                    </SocialText>
                 </SocialButton>
             </SocialWrapper>
-            <LoginText mt="3.75rem" mb={is_eu ? '100px' : '0'}>
+            <LoginText weight="normal" type="paragraph-1">
                 {localize('Already have an account?')}
                 <StyledLinkText
                     id="dm-new-login-button"

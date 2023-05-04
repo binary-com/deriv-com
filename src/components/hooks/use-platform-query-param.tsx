be7852772b17c0c5a22c2770c948d2d82bf159fc
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-05-04 10:11:52 +0400

    fix: name changed

--- src/components/hooks/use-platform-query-param.tsx
+++ src/components/hooks/use-platform-query-param.tsx
@@ -7,13 +7,13 @@ export const usePlatformQueryParam = () => {
     const [is_deriv_go, setIsDerivGo] = useState(true)
     const [is_deriv_p2p, setIsDerivP2P] = useState(true)
     const [has_platform, setHasPlatform] = useState(false)
-    const [is_loading, setInprogress] = useState(true)
+    const [is_loading, setLoading] = useState(true)
 
     useEffect(() => {
         setIsDerivGo(platform === 'derivgo')
         setIsDerivP2P(platform === 'p2p')
         setHasPlatform(platforms.includes(platform))
-        setInprogress(false)
+        setLoading(false)
     }, [platform])
 
     return { platform, has_platform, is_deriv_p2p, is_deriv_go, is_loading }

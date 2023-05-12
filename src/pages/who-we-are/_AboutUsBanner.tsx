db4f585daba67f2886346abe767858e902796420
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-05-12 14:50:54 +0600

    Revert "Bff strapi cloud (#4699)" (#4705)
    This reverts commit f1090b79410bacca263f0ac114f8039d6cccc19c.

--- src/pages/who-we-are/_AboutUsBanner.tsx
+++ src/pages/who-we-are/_AboutUsBanner.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
-import { TWhoWeAreBanner } from '../_types'
+import BgMobile from 'images/common/who-we-are/about-us-banner-mobile.jpg'
+import BgMobileRTL from 'images/common/who-we-are/about-us-banner-mobile_rtl.jpg'
+import Bg from 'images/common/who-we-are/about-us-banner.jpg'
+import BgRTL from 'images/common/who-we-are/about-us-banner_rtl.jpg'
 import { Localize } from 'components/localization'
-import { Flex, SectionContainer } from 'components/containers'
+import { SectionContainer, Flex } from 'components/containers'
 import device from 'themes/device'
-import useBreakpoints from 'components/hooks/use-breakpoints'
 import { Header } from 'components/elements'
 import { LinkButton } from 'components/form'
 import { useIsRtl } from 'components/hooks/use-isrtl'
@@ -15,7 +17,6 @@ const StyledSectionContainer = styled(SectionContainer)`
     align-items: center;
     padding: 0 40px 120px;
     flex-wrap: nowrap;
-
     @media ${device.tablet} {
         padding: 0 40px 40px;
     }
@@ -23,27 +24,33 @@ const StyledSectionContainer = styled(SectionContainer)`
         padding: 0 16px 40px;
     }
 `
-type TStyledFlex = { is_rtl: boolean; bg_image: string }
-const StyledFlex = styled(Flex)<TStyledFlex>`
+
+const StyledFlex = styled(Flex)<{ is_rtl: boolean }>`
     border-radius: 10px;
-    background-image: url(${({ bg_image }) => bg_image});
+    ${({ is_rtl }) =>
+        is_rtl
+            ? css`
+                  background-image: url(${BgRTL});
+              `
+            : css`
+                  background-image: url(${Bg});
+              `};
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
     overflow: hidden;
-
     @media ${device.laptopM} {
         width: 100%;
     }
     @media (max-width: 610px) {
-        ${({ is_rtl, bg_image }) =>
+        ${({ is_rtl }) =>
             is_rtl
                 ? css`
-                      background-image: url(${bg_image});
+                      background-image: url(${BgMobileRTL});
                       background-position-x: right;
                   `
                 : css`
-                      background-image: url(${bg_image});
+                      background-image: url(${BgMobile});
                       background-position-x: left;
                   `};
         height: 546px;
@@ -52,10 +59,10 @@ const StyledFlex = styled(Flex)<TStyledFlex>`
         background-position-x: -40px;
     }
 `
+
 const PictureFlex = styled(Flex)`
     width: 373px;
     margin: 32px 0 32px 100px;
-
     @media ${device.laptop} {
         margin: 32px 0 32px 50px;
     }
@@ -67,9 +74,9 @@ const PictureFlex = styled(Flex)`
         margin: 10px 10px 0 29px;
     }
 `
+
 const StyledHeader = styled(Header)`
     line-height: 40px;
-
     @media ${device.laptop} {
         line-height: 34px;
     }
@@ -80,10 +87,10 @@ const StyledHeader = styled(Header)`
         font-size: 28px;
     }
 `
+
 const StyledHeader2 = styled(Header)`
     color: var(--color-white);
     margin: 12px 0 8px;
-
     @media ${device.tablet} {
         margin: 6px 0 10px;
         font-size: 11px;
@@ -92,26 +99,19 @@ const StyledHeader2 = styled(Header)`
         letter-spacing: 0;
     }
 `
+
 const StyledLinkButton = styled(LinkButton)`
     @media ${device.tablet} {
         padding: 8px 16px;
     }
 `
 
-const WhoWeAreBanner = ({ banner }: TWhoWeAreBanner) => {
-    const { is_mobile_or_tablet } = useBreakpoints()
-    const bg_image = is_mobile_or_tablet
-        ? banner?.bg_mobile.localFile?.publicURL
-        : banner?.bg_desktop.localFile?.publicURL
-    const bg_image_rtl = is_mobile_or_tablet
-        ? banner?.bg_mobile_rtl.localFile?.publicURL
-        : banner?.bg_desktop_rtl.localFile?.publicURL
+const AboutUsBanner = () => {
     const is_rtl = useIsRtl()
     return (
         <StyledSectionContainer>
             <StyledFlex
                 is_rtl={is_rtl}
-                bg_image={is_rtl ? bg_image_rtl : bg_image}
                 direction="column"
                 height="auto"
                 min_height="288px"
@@ -119,7 +119,7 @@ const WhoWeAreBanner = ({ banner }: TWhoWeAreBanner) => {
             >
                 <PictureFlex jc="start" ai="start" direction="column">
                     <StyledHeader as="h3" width="100%" type="unset" size="32px" color="white">
-                        <Localize translate_text={banner?.header} />
+                        <Localize translate_text="_t_We have a huge mission, an incredible team, and rapid growth._t_" />
                     </StyledHeader>
                     <StyledHeader2
                         width="100%"
@@ -128,10 +128,10 @@ const WhoWeAreBanner = ({ banner }: TWhoWeAreBanner) => {
                         size="16px"
                         weight="400px"
                     >
-                        <Localize translate_text={banner?.sub_header} />
+                        <Localize translate_text="_t_Join and grow with us._t_" />
                     </StyledHeader2>
-                    <StyledLinkButton secondary to={banner?.link_url}>
-                        <Localize translate_text={banner?.link_name} />
+                    <StyledLinkButton secondary to="/careers/">
+                        <Localize translate_text="_t_See our open positions_t_" />
                     </StyledLinkButton>
                 </PictureFlex>
             </StyledFlex>
@@ -139,4 +139,4 @@ const WhoWeAreBanner = ({ banner }: TWhoWeAreBanner) => {
     )
 }
 
-export default WhoWeAreBanner
+export default AboutUsBanner

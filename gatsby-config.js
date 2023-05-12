51475e658aadb823f3591926b7156c057d3f5be1
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-05-12 11:26:58 +0600

    Revert "Bff strapi cloud (#4686)"
    This reverts commit 02b403d61a1003f4a0143761dc28b4f329d3435c.

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,21 +10,6 @@ const href = isBrowser && window.location.href
 const site_url =
     origin === 'https://deriv.com' || origin === 'https://eu.deriv.com' ? href : 'https://deriv.com'
 
-const strapi_preview_param = {
-    publicationState:
-        process.env.STRAPI_PREVIEW === 'true' ? 'preview&filters[publishedAt][$null]=true' : 'live',
-}
-const strapi_config = process.env.STRAPI_BUILD == 'true' && [
-    {
-        singularName: 'who-we-are-page',
-        queryParams: strapi_preview_param,
-    },
-    {
-        singularName: 'cfd-warning-banner',
-        queryParams: strapi_preview_param,
-    },
-]
-
 module.exports = {
     // pathPrefix: process.env.PATH_PREFIX || '/deriv-com/', // For non CNAME GH-pages deployment
     flags: {
@@ -335,14 +320,6 @@ module.exports = {
             options: {
                 analyzerMode: 'disabled',
                 generateStatsFile: process.env.GENERATE_JSON_STATS === 'true',
-            },
-        },
-        {
-            resolve: 'gatsby-source-strapi',
-            options: {
-                apiURL: process.env.STRAPI_URL,
-                accessToken: process.env.STRAPI_TOKEN,
-                collectionTypes: strapi_config,
             },
         },
     ],
--- src/components/layout/cfd-warning-banner.tsx
+++ src/components/layout/cfd-warning-banner.tsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
-import { graphql, useStaticQuery } from 'gatsby'
 import { Container } from 'components/containers'
+import { loss_percent } from 'common/constants'
 import device from 'themes/device'
 import { Localize } from 'components/localization'
 import { Text } from 'components/elements'
 import useRegion from 'components/hooks/use-region'
-import { fallback_loss_percent } from 'common/constants'
 
 type TProps = {
     is_ppc: boolean
-    data: {
-        strapiCfdWarningBanner: {
-            loss_percent: number
-        }
-    }
 }
 
 const CFDWrapper = styled.section`
@@ -68,16 +62,9 @@ const CFDText = styled(Text)`
         font-size: 10px;
     }
 `
+
 const CFDWarningBanner = ({ is_ppc }: TProps) => {
     const { is_eu, is_cpa_plan } = useRegion()
-    const data = useStaticQuery(graphql`
-        query {
-            strapiCfdWarningBanner {
-                loss_percent
-            }
-        }
-    `)
-    const loss_percent = data?.strapiCfdWarningBanner?.loss_percent ?? fallback_loss_percent
 
     if (is_ppc || is_eu || is_cpa_plan) {
         return (

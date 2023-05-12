e541ab6ff6fe26aef9531223693842c7b0f376a1
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-05-12 11:27:12 +0600

    Revert "Bff strapi cloud (#4686)" (#4698)
    This reverts commit 02b403d61a1003f4a0143761dc28b4f329d3435c.

--- src/pages/who-we-are/components/modal.tsx
+++ src/pages/who-we-are/components/modal.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Flex } from 'components/containers'
+import { ModalFlex } from './leaders'
 import { Localize, LocalizedLink } from 'components/localization'
 import { Header } from 'components/elements'
 import Linkedin from 'images/svg/who-we-are/linkedin.svg'
-import device from 'themes/device'
+import { TString } from 'types/generics'
 
 type ModalPropsType = {
     name: string
-    role: string
+    position: TString
     link?: string
 }
+
 type StyledLogoType = {
     link: string
 }
@@ -21,39 +22,18 @@ const StyledLogo = styled.img<StyledLogoType>`
     filter: grayscale(100%);
 
     &:hover {
-        filter: ${({ link }) => (link ? 'unset' : 'grayscale(100%)')};
-    }
-`
-export const ModalFlex = styled(Flex)`
-    position: absolute;
-    top: 130px;
-    background-color: white;
-    padding: 8px 16px 6px;
-    z-index: 1;
-    border-radius: 5px;
-    box-shadow: rgba(0, 0, 0, 0.35) 0 4px 15px;
-    white-space: pre;
-    width: fit-content;
-
-    @media ${device.tablet} {
-        top: 124px;
-    }
-    @media ${device.tabletS} {
-        top: 108px;
-    }
-    @media ${device.mobileL} {
-        top: 93px;
+        filter: ${(props) => (props.link ? 'unset' : 'grayscale(100%)')};
     }
 `
 
-const Modal = ({ name, role, link }: ModalPropsType) => {
+const Modal = ({ name, position, link }: ModalPropsType) => {
     return (
         <ModalFlex ai="center" direction="column" width="unset" height="unset" p="0px 16px 5px">
             <Header type="unset" as="h4" padding="0" align="center" size="14px">
                 {name}
             </Header>
             <Header as="h4" padding="0" type="sub-paragraph" weight="normal" align="center">
-                <Localize translate_text={role} />
+                {position && <Localize translate_text={position} />}
             </Header>
             {link && (
                 <LocalizedLink external to={link} target="_blank" rel="noopener noreferrer">

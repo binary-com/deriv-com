de95d39dc93d63c33fc2a31eca676890ed8e9e9e
Jim Daniels Wasswa <jim@deriv.com>
2023-05-15 11:28:34 +0800

    refactor: one more bug

--- src/components/custom/_agreement-label.tsx
+++ src/components/custom/_agreement-label.tsx
@@ -24,7 +24,7 @@ const AgreementLabel = ({
     handleChangeCheckbox,
     isChecked,
     color,
-    link_text = localize('I agree to the <0>terms and conditions</0>'),
+    link_text = localize('<0>I</0> agree to the terms and conditions'),
 }: AgreementLabelProps) => {
     // the is mounted check is used for making sure the localized link text
     // properly renders the correct domain url

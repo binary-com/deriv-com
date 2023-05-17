2e39bcd4bedc136a5643a178cb85484029b0cac5
mitra-deriv <mitra@regentmarkets.com>
2023-05-17 20:04:49 +0800

    fix: :bug: market slider eu changes

--- src/features/pages/home/markets/market-slider/index.tsx
+++ src/features/pages/home/markets/market-slider/index.tsx
@@ -34,7 +34,7 @@ const MarketSlider = () => {
                 {slider_items.map(({ id, data }) => {
                     return (
                         <SwiperSlide key={id} className={'market_slide'}>
-                            <MarketSliderItem item={data} />
+                            <MarketSliderItem item={data} is_eu={is_eu} />
                         </SwiperSlide>
                     )
                 })}

a144d3db6e91ca7d7169496abe77fc2b1ca74536
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-05-18 20:02:33 +0400

    feat: acuity page

---
+++ src/features/pages/acuity/acuity-feature-guide/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { contentData } from './data'
+import ImageWithParagraph from 'features/components/molecules/image-text-boxes'
+
+const AcuityTradeTools = () => {
+    return <ImageWithParagraph contentData={contentData} />
+}
+
+export default AcuityTradeTools

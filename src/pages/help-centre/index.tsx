849584dd459bb79acb4bfb9b033354197ac0104c
Nuzhy-Deriv <125249108+Nuzhy-Deriv@users.noreply.github.com>
2023-05-18 20:34:24 +0800

    fix: p2p load issue in help center page (#4749)

--- src/pages/help-centre/index.tsx
+++ src/pages/help-centre/index.tsx
@@ -9,11 +9,15 @@ import { SEO, Desktop, Container } from 'components/containers'
 import Layout from 'components/layout/layout'
 import { localize, WithIntl } from 'components/localization'
 import { usePlatformQueryParam } from 'components/hooks/use-platform-query-param'
+import useWebsiteStatus from 'components/hooks/use-website-status'
+import InitialLoader from 'components/elements/dot-loader'
 
 const HelpCentre = () => {
     const { is_deriv_go } = usePlatformQueryParam()
     const general_questions = getQuestionsBySection(GENERAL)
     const platforms_questions = getQuestionsBySection(PLATFORMS)
+    const { website_status } = useWebsiteStatus()
+    const clients_country = website_status?.clients_country
 
     return (
         <Layout>
@@ -28,7 +32,14 @@ const HelpCentre = () => {
 
             <Container align="start" justify="flex-start" direction="column">
                 <QuestionsSection data={general_questions} section_name={`_t_${GENERAL}_t_`} />
-                <QuestionsSection data={platforms_questions} section_name={`_t_${PLATFORMS}_t_`} />
+                {clients_country !== undefined ? (
+                    <QuestionsSection
+                        data={platforms_questions}
+                        section_name={`_t_${PLATFORMS}_t_`}
+                    />
+                ) : (
+                    <InitialLoader />
+                )}
             </Container>
 
             <Desktop breakpoint="tabletL">

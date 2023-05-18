a7f2383b41d2721f97be60589cd3b1b0079df262
mitra-deriv <64970259+mitra-deriv@users.noreply.github.com>
2023-05-18 16:56:02 +0800

    fix: :bug: add payment agent to partner in mobile (#4741)

--- src/features/components/templates/navigation/main-nav/content/partner/index.ts
+++ src/features/components/templates/navigation/main-nav/content/partner/index.ts
@@ -11,12 +11,22 @@ export const partnerNavSectionContent: SmartNavContent[] = [
     {
         id: 1,
         data: {
+            title: '_t_Payment agents_t_',
+            url: { type: 'internal', to: '/partners/payment-agent/' },
+        },
+        visibility: {
+            is_row: true,
+        },
+    },
+    {
+        id: 2,
+        data: {
             title: '_t_API_t_',
             url: { type: 'company', url_name: 'api', target: '_blank' },
         },
     },
     {
-        id: 2,
+        id: 3,
         data: {
             title: '_t_Bug bounty_t_',
             url: { type: 'internal', to: '/bug-bounty/' },

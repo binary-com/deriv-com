0f0d2fc825a0d6a4fa3dbea35512f02632c51c60
Shayan Khaleghparast <iman@regentmarkets.com>
2023-05-21 23:35:13 +0330

    feat: added prettier-config-deriv package

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    ...require('prettier-config-deriv'),
+    overrides: [
+        {
+            files: ['crowdin/messages.json', 'src/translations/*.json'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};

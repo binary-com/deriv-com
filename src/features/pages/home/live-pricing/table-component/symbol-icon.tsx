c5f1ada4376571c74303978da4f6684bb09fea83
meenu-deriv <meenakshi@deriv.com>
2023-05-26 08:54:26 +0400

    fix: refactor

--- src/features/pages/home/live-pricing/table-component/symbol-icon.tsx
+++ src/features/pages/home/live-pricing/table-component/symbol-icon.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 import * as icons from 'components/elements/symbols-shortcode'
 import Image from 'features/components/atoms/image'
 
-type TTableCellIcon = {
-    icon_src: string
-}
-
-const SymbolIcon = ({ icon_src }: TTableCellIcon) => {
+const SymbolIcon = ({ icon_src }: { icon_src: string }) => {
     if (icon_src === '1HZ150V')
         return <Image src={icons['Vol_1HZ150V']} width="24px" height="24px" />
     if (icon_src === '1HZ250V')

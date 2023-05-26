77f9f0e7914338fc817bc920b8ee70fb42a90982
aswathy-deriv <aswathy@deriv.com>
2023-05-26 16:15:21 +0400

    fix: css issue for banner

--- src/features/components/organisms/markets/home-page-banner/home-page-wrapper.tsx
+++ src/features/components/organisms/markets/home-page-banner/home-page-wrapper.tsx
@@ -3,6 +3,7 @@ import BannerText from './banner-text'
 import { HomepageBannerType } from './types'
 import { flex_container } from './styles.module.scss'
 import Flex from 'features/components/atoms/flex-box'
+import Container from 'features/components/atoms/container'
 
 interface HomepageBannerProps {
     item: HomepageBannerType
@@ -10,14 +11,17 @@ interface HomepageBannerProps {
 
 const HomePageWrapper = ({ item }: HomepageBannerProps) => {
     return (
-        <Flex.Box
-            className={flex_container}
-            direction="col-reverse"
-            md={{ direction: 'row', justify: 'end', align: 'start' }}
-        >
-            <BannerText item={item} />
-            {item.image}
-        </Flex.Box>
+        <Container.Fixed>
+            <Flex.Box
+                container="fluid"
+                className={flex_container}
+                direction="col-reverse"
+                md={{ direction: 'row', justify: 'end', align: 'start' }}
+            >
+                <BannerText item={item} />
+                {item.image}
+            </Flex.Box>
+        </Container.Fixed>
     )
 }
 

0f783cea7838cddd4e7ba052cc12058f97b10221
aswathy-deriv <aswathy@deriv.com>
2023-06-01 15:39:53 +0400

    fix: using usememo for banner

--- src/features/components/organisms/markets/market-banner/market-wrapper.tsx
+++ src/features/components/organisms/markets/market-banner/market-wrapper.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import BannerText from './banner-text'
 import { HomepageBannerType } from './types'
 import { flex_container, image_container, image_phone } from './styles.module.scss'
@@ -8,22 +8,25 @@ import useBreakpoints from 'components/hooks/use-breakpoints'
 
 const MarketWrapper = ({ item }: { item: HomepageBannerType }) => {
     const { is_mobile_or_tablet } = useBreakpoints()
-    return (
-        <Container.Fixed className={flex_container}>
-            {is_mobile_or_tablet ? (
-                <Flex.Box direction="col-reverse" justify="center">
-                    <BannerText item={item} />
+    const renderBanner = useMemo(() => {
+        return (
+            <>
+                {is_mobile_or_tablet ? (
+                    <Flex.Box direction="col-reverse" justify="center">
+                        <BannerText item={item} />
 
-                    <div className={image_phone}>{item.image_phone}</div>
-                </Flex.Box>
-            ) : (
-                <Flex.Box container="fluid" direction="row" justify="start" align="start">
-                    <BannerText item={item} />
-                    <div className={image_container}>{item.image_tablet}</div>
-                </Flex.Box>
-            )}
-        </Container.Fixed>
-    )
+                        <div className={image_phone}>{item.image_phone}</div>
+                    </Flex.Box>
+                ) : (
+                    <Flex.Box container="fluid" direction="row" justify="start" align="start">
+                        <BannerText item={item} />
+                        <div className={image_container}>{item.image_tablet}</div>
+                    </Flex.Box>
+                )}
+            </>
+        )
+    }, [is_mobile_or_tablet])
+    return <Container.Fixed className={flex_container}>{renderBanner}</Container.Fixed>
 }
 
 export default MarketWrapper

6cb72a884bb08c2b05135daa7891d6ae10c217aa
aswathy-deriv <aswathy@deriv.com>
2023-06-01 17:30:31 +0400

    fix: removed usememo for banner

--- src/features/components/organisms/markets/market-banner/market-wrapper.tsx
+++ src/features/components/organisms/markets/market-banner/market-wrapper.tsx
@@ -8,25 +8,22 @@ import useBreakpoints from 'components/hooks/use-breakpoints'
 
 const MarketWrapper = ({ item }: { item: HomepageBannerType }) => {
     const { is_mobile_or_tablet } = useBreakpoints()
-    const renderBanner = useMemo(() => {
-        return (
-            <>
-                {is_mobile_or_tablet ? (
-                    <Flex.Box direction="col-reverse" justify="center">
-                        <BannerText item={item} />
+    return (
+        <Container.Fixed className={flex_container}>
+            {is_mobile_or_tablet ? (
+                <Flex.Box direction="col-reverse" justify="center">
+                    <BannerText item={item} />
 
-                        <div className={image_phone}>{item.image_phone}</div>
-                    </Flex.Box>
-                ) : (
-                    <Flex.Box container="fluid" direction="row" justify="start" align="start">
-                        <BannerText item={item} />
-                        <div className={image_container}>{item.image_tablet}</div>
-                    </Flex.Box>
-                )}
-            </>
-        )
-    }, [is_mobile_or_tablet])
-    return <Container.Fixed className={flex_container}>{renderBanner}</Container.Fixed>
+                    <div className={image_phone}>{item.image_phone}</div>
+                </Flex.Box>
+            ) : (
+                <Flex.Box container="fluid" direction="row" justify="start" align="start">
+                    <BannerText item={item} />
+                    <div className={image_container}>{item.image_tablet}</div>
+                </Flex.Box>
+            )}
+        </Container.Fixed>
+    )
 }
 
 export default MarketWrapper

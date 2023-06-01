5feac9cf212248b3d6dfbc2589f9f1c2b7dd72c2
mitra-deriv <mitra@regentmarkets.com>
2023-06-01 12:34:02 +0800

    fix: :bug: fix issue to show linkedin for eu

--- src/features/components/templates/footer/social-icons/data.ts
+++ src/features/components/templates/footer/social-icons/data.ts
@@ -310,7 +310,7 @@ export const socialMediaAccounts: SmartSocialAccount[] = [
             image_alt: '_t_Linkedin_t_',
         },
         visibility: {
-            is_career_page: false,
+            is_career_page: true,
             is_eu: true,
         },
     },

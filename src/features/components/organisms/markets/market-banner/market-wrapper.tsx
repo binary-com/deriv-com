8c874b9d431906856421a562680295f48a6cb9af
aswathy-deriv <aswathy@deriv.com>
2023-06-01 14:30:27 +0400

    fix: changes removed

--- src/features/components/organisms/markets/market-banner/market-wrapper.tsx
+++ src/features/components/organisms/markets/market-banner/market-wrapper.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import BannerText from './banner-text'
 import { HomepageBannerType } from './types'
-import { flex_container } from './styles.module.scss'
-import MobileBanner from './mobile-banner'
-import DesktopWrapper from './desktop-banner'
+import { flex_container, image_container, image_phone } from './styles.module.scss'
+import Flex from 'features/components/atoms/flex-box'
 import Container from 'features/components/atoms/container'
 import useBreakpoints from 'components/hooks/use-breakpoints'
 
@@ -11,7 +10,18 @@ const MarketWrapper = ({ item }: { item: HomepageBannerType }) => {
     const { is_mobile_or_tablet } = useBreakpoints()
     return (
         <Container.Fixed className={flex_container}>
-            {is_mobile_or_tablet ? <MobileBanner item={item} /> : <DesktopWrapper item={item} />}
+            {is_mobile_or_tablet ? (
+                <Flex.Box direction="col-reverse" justify="center">
+                    <BannerText item={item} />
+
+                    <div className={image_phone}>{item.image_phone}</div>
+                </Flex.Box>
+            ) : (
+                <Flex.Box container="fluid" direction="row" justify="start" align="start">
+                    <BannerText item={item} />
+                    <div className={image_container}>{item.image_tablet}</div>
+                </Flex.Box>
+            )}
         </Container.Fixed>
     )
 }

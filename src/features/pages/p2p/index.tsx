24167379612a1274f96ee2a95c55f30bd7ea3641
hasan-deriv <abuhasan@deriv.com>
2023-06-06 14:05:40 +0800

    fix: qa issues

--- src/features/pages/p2p/index.tsx
+++ src/features/pages/p2p/index.tsx
@@ -12,30 +12,55 @@ import Layout from 'features/components/templates/layout'
 import { SEO } from 'components/containers'
 import { localize } from 'components/localization'
 import MainNav from 'features/components/templates/navigation/main-nav'
+import { useOpenLiveChat } from 'components/hooks/use-open-live-chat-redirection'
+import { usePageLoaded } from 'components/hooks/use-page-loaded'
+import useRegion from 'components/hooks/use-region'
+import useWebsiteStatus from 'components/hooks/use-website-status'
+import InitialLoader from 'components/elements/dot-loader'
+import PageNotFound from 'features/pages/404'
 
 const Footer = loadable(() => import('features/components/templates/footer'))
 
 const DP2PPage = () => {
+    useOpenLiveChat(true)
+    const [is_mounted] = usePageLoaded() // needed to fix the second Hero-component during page's loadin
+    const { is_p2p_allowed_country } = useRegion()
+    const { website_status } = useWebsiteStatus()
+    const clients_country = website_status?.clients_country
+    if (clients_country !== undefined) {
+        if (is_p2p_allowed_country) {
+            return (
+                <Layout>
+                    <SEO
+                        title={localize(
+                            '_t_Online trading platform | Forex, commodities, synthetic indices, stocks, and stock indices | Deriv_t_',
+                        )}
+                        description={localize(
+                            '_t_Deriv: an online trading platform available 24/7 with a wide selection of derivatives._t_',
+                        )}
+                        has_organization_schema
+                    />
+                    <MainNav />
+                    {is_mounted && (
+                        <>
+                            <DP2Hero />
+                            <DP2Numbers />
+                            <DP2P />
+                            <DP2Steps />
+                            <DP2Availability />
+                            <DP2Banner data={banner_data} />
+                            <Roadmap data={portalData} />
+                        </>
+                    )}
+                    <Footer />
+                </Layout>
+            )
+        }
+        return <PageNotFound />
+    }
     return (
         <Layout>
-            <SEO
-                title={localize(
-                    '_t_Online trading platform | Forex, commodities, synthetic indices, stocks, and stock indices | Deriv_t_',
-                )}
-                description={localize(
-                    '_t_Deriv: an online trading platform available 24/7 with a wide selection of derivatives._t_',
-                )}
-                has_organization_schema
-            />
-            <MainNav />
-            <DP2Hero />
-            <DP2Numbers />
-            <DP2P />
-            <DP2Steps />
-            <DP2Availability />
-            <DP2Banner data={banner_data} />
-            <Roadmap data={portalData} />
-            <Footer />
+            <InitialLoader />
         </Layout>
     )
 }
--- src/pages/p2p.tsx
+++ src/pages/p2p.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
 import { WithIntl } from 'components/localization'
 import DP2PPage from 'features/pages/p2p'
-import PageNotFound from 'features/pages/404'
-import useRegion from 'components/hooks/use-region'
-import { useOpenLiveChat } from 'components/hooks/use-open-live-chat-redirection'
 
 const DP2P = () => {
-    useOpenLiveChat(true)
-    const { is_p2p_allowed_country } = useRegion()
-
-    if (is_p2p_allowed_country) {
-        return <DP2PPage />
-    }
-    return <PageNotFound />
+    return <DP2PPage />
 }
 
 export default WithIntl()(DP2P)

b5dbd49c909661035cea035c519b3b68f97de3a8
NikitK-deriv <103182473+NikitK-deriv@users.noreply.github.com>
2023-06-06 18:11:35 +0300

    fix: strapi build variable and cfd banner (#4948)

--- src/pages/who-we-are/index.tsx
+++ src/pages/who-we-are/index.tsx
@@ -16,7 +16,7 @@ import ImageMarquee from './components/_image-marquee'
 import device from 'themes/device'
 import { SEO } from 'components/containers'
 import Layout from 'components/layout/layout'
-import { WithIntl, localize } from 'components/localization'
+import { localize, WithIntl } from 'components/localization'
 
 const StartSeparator = styled.div`
     width: 0;
@@ -74,162 +74,160 @@ const WhoWeAre = ({ data }: TWhoWeAre) => {
     )
 }
 
-export const query =
-    process.env.STRAPI_BUILD == 'true' &&
-    graphql`
-        query {
-            strapiWhoWeArePage {
-                hero {
+export const query = graphql`
+    query {
+        strapiWhoWeArePage {
+            hero {
+                header
+                sub_header
+                first_paragraph
+                second_paragraph
+                third_paragraph
+                hero_image {
+                    localFile {
+                        publicURL
+                    }
+                }
+                bg_desktop {
+                    localFile {
+                        publicURL
+                    }
+                }
+                bg_mobile {
+                    localFile {
+                        publicURL
+                    }
+                }
+            }
+            our_values {
+                header
+                values {
                     header
                     sub_header
-                    first_paragraph
-                    second_paragraph
-                    third_paragraph
-                    hero_image {
-                        localFile {
-                            publicURL
-                        }
-                    }
-                    bg_desktop {
-                        localFile {
-                            publicURL
-                        }
-                    }
-                    bg_mobile {
-                        localFile {
-                            publicURL
-                        }
-                    }
-                }
-                our_values {
-                    header
-                    values {
-                        header
-                        sub_header
-                        image {
-                            localFile {
-                                publicURL
-                            }
-                        }
-                    }
-                }
-                our_principles {
-                    header
-                    principles {
-                        header
-                        sub_header
-                    }
-                    button {
-                        link_name
-                        link_url
-                    }
-                }
-                our_leadership {
-                    header
-                    leaders {
-                        name
-                        role
-                        link_url
-                        photo {
-                            url
-                            localFile {
-                                publicURL
-                            }
-                        }
-                    }
-                }
-                deriv_in_numbers {
-                    header
-                    sub_header
-                    numbers {
-                        description
-                        number
-                    }
-                }
-                our_locations {
-                    header
-                    numbers {
-                        description
-                        number
-                    }
-                    locations {
-                        country_city
-                        link_url
-                    }
-                    earth {
-                        localFile {
-                            publicURL
-                        }
-                    }
-                    earth_mobile {
-                        localFile {
-                            publicURL
-                        }
-                    }
-                }
-                banner {
-                    header
-                    sub_header
-                    link_name
-                    link_url
-                    bg_desktop {
-                        localFile {
-                            publicURL
-                        }
-                    }
-                    bg_desktop_rtl {
-                        localFile {
-                            url
-                            publicURL
-                        }
-                    }
-                    bg_mobile {
-                        localFile {
-                            url
-                            publicURL
-                        }
-                    }
-                    bg_mobile_rtl {
-                        localFile {
-                            url
-                            publicURL
-                        }
-                    }
-                }
-                slider_medias {
-                    slider_1 {
-                        localFile {
-                            publicURL
-                        }
-                    }
-                    slider_2 {
-                        localFile {
-                            publicURL
-                        }
-                    }
-                    slider_3 {
-                        localFile {
-                            publicURL
-                        }
-                    }
-                    slider_4 {
-                        localFile {
-                            publicURL
-                        }
-                    }
-                    slider_5 {
-                        localFile {
-                            publicURL
-                        }
-                    }
-                    slider_6 {
+                    image {
                         localFile {
                             publicURL
                         }
                     }
                 }
             }
+            our_principles {
+                header
+                principles {
+                    header
+                    sub_header
+                }
+                button {
+                    link_name
+                    link_url
+                }
+            }
+            our_leadership {
+                header
+                leaders {
+                    name
+                    role
+                    link_url
+                    photo {
+                        url
+                        localFile {
+                            publicURL
+                        }
+                    }
+                }
+            }
+            deriv_in_numbers {
+                header
+                sub_header
+                numbers {
+                    description
+                    number
+                }
+            }
+            our_locations {
+                header
+                numbers {
+                    description
+                    number
+                }
+                locations {
+                    country_city
+                    link_url
+                }
+                earth {
+                    localFile {
+                        publicURL
+                    }
+                }
+                earth_mobile {
+                    localFile {
+                        publicURL
+                    }
+                }
+            }
+            banner {
+                header
+                sub_header
+                link_name
+                link_url
+                bg_desktop {
+                    localFile {
+                        publicURL
+                    }
+                }
+                bg_desktop_rtl {
+                    localFile {
+                        url
+                        publicURL
+                    }
+                }
+                bg_mobile {
+                    localFile {
+                        url
+                        publicURL
+                    }
+                }
+                bg_mobile_rtl {
+                    localFile {
+                        url
+                        publicURL
+                    }
+                }
+            }
+            slider_medias {
+                slider_1 {
+                    localFile {
+                        publicURL
+                    }
+                }
+                slider_2 {
+                    localFile {
+                        publicURL
+                    }
+                }
+                slider_3 {
+                    localFile {
+                        publicURL
+                    }
+                }
+                slider_4 {
+                    localFile {
+                        publicURL
+                    }
+                }
+                slider_5 {
+                    localFile {
+                        publicURL
+                    }
+                }
+                slider_6 {
+                    localFile {
+                        publicURL
+                    }
+                }
+            }
         }
-    `
+    }
+`
 
 export default WithIntl()(WhoWeAre)

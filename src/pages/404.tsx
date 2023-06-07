279f74364b5fbccd0281576a4db889576d204f3e
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-06-07 13:34:08 +0600

    Revert "fix: strapi build variable and cfd banner (#4948)" (#4950)
    This reverts commit b5dbd49c909661035cea035c519b3b68f97de3a8.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,59 +8,61 @@ const NotFound = () => {
 }
 
 // this query exist here to get data only which need to be translated in crowdin
-export const query = graphql`
-    query {
-        strapiWhoWeArePage {
-            hero {
-                header
-                sub_header
-                first_paragraph
-                second_paragraph
-                third_paragraph
-            }
-            our_values {
-                header
-                values {
+export const query =
+    process.env.STRAPI_BUILD == 'true' &&
+    graphql`
+        query {
+            strapiWhoWeArePage {
+                hero {
                     header
                     sub_header
+                    first_paragraph
+                    second_paragraph
+                    third_paragraph
                 }
-            }
-            our_principles {
-                header
-                principles {
+                our_values {
+                    header
+                    values {
+                        header
+                        sub_header
+                    }
+                }
+                our_principles {
+                    header
+                    principles {
+                        header
+                        sub_header
+                    }
+                    button {
+                        link_name
+                    }
+                }
+                our_leadership {
+                    header
+                    leaders {
+                        role
+                    }
+                }
+                deriv_in_numbers {
                     header
                     sub_header
+                    numbers {
+                        description
+                    }
                 }
-                button {
+                our_locations {
+                    header
+                    numbers {
+                        description
+                    }
+                }
+                banner {
+                    header
+                    sub_header
                     link_name
                 }
             }
-            our_leadership {
-                header
-                leaders {
-                    role
-                }
-            }
-            deriv_in_numbers {
-                header
-                sub_header
-                numbers {
-                    description
-                }
-            }
-            our_locations {
-                header
-                numbers {
-                    description
-                }
-            }
-            banner {
-                header
-                sub_header
-                link_name
-            }
         }
-    }
-`
+    `
 
 export default WithIntl()(NotFound)

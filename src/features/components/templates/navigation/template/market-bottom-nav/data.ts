2acfc0aea0c40ba7020a5383886e22f53d913387
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-06-08 10:26:42 +0200

    fix: revert

--- src/features/components/templates/navigation/template/market-bottom-nav/data.ts
+++ src/features/components/templates/navigation/template/market-bottom-nav/data.ts
@@ -24,13 +24,6 @@ export const marketItems: TMarketNavItem[] = [
         },
     },
     {
-        title: '_t_ETFs_t_',
-        link: {
-            type: 'internal',
-            to: '/markets/exchange-traded-funds/',
-        },
-    },
-    {
         title: '_t_Cryptocurrencies_t_',
         link: {
             type: 'internal',
--- src/features/components/templates/navigation/template/market-bottom-nav/index.tsx
+++ src/features/components/templates/navigation/template/market-bottom-nav/index.tsx
@@ -2,18 +2,13 @@ import React from 'react'
 import { marketItems } from './data'
 import BottomNavItem from './bottom-nav.item'
 import { container } from './styles.module.scss'
-import useRegion from 'components/hooks/use-region'
 
 const MarketBottomNav = () => {
-    const { is_eu } = useRegion()
-
     return (
         <div className={container}>
-            {marketItems
-                .filter((item) => !(is_eu && item.title === '_t_ETFs_t_'))
-                .map((item) => (
-                    <BottomNavItem key={item.title} item={item} />
-                ))}
+            {marketItems.map((item) => (
+                <BottomNavItem key={item.title} item={item} />
+            ))}
         </div>
     )
 }

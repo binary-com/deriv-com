5ca530c03158af6c036ce1378c2cf0acc5ee14aa
NikitK-deriv <103182473+NikitK-deriv@users.noreply.github.com>
2023-06-08 08:06:38 +0300

    [Rebranding + Refactoring + Translation] Trading Platforms > Deriv EZ (#4786)
    * refactor: + rebrand deriv-ez page
    
    * chore: cleanup
    
    * fix: ui bugs
    
    * refactor: app banner to atomic design with reusable template
    
    * fix: change shadow for other platforms
    
    * fix: return tab content back
    
    * fix: replace localizedlink to atomic link
    
    * fix: resolution of small images in cards
    
    * fix: update the bugs issues
    
    * refactor: replace reusable components on sutiable
    
    * fix: ui and bold text
    
    * auto-pull-translation
    
    * auto-pull-translation
    
    * fix: bugs with tab, spaces and bold text when no selected tab
    
    * auto-pull-translation
    
    * fix: bold for real account as well
    
    * auto-pull-translation
    
    * fix: size of card, number
    
    * auto-pull-translation
    
    * auto-pull-translation
    
    * fix: ui issues
    
    * auto-pull-translation
    
    * fix: ui bugs
    
    * auto-pull-translation
    
    * fix: shape-banner fluid
    
    * auto-pull-translation
    
    * translation: 📚 sync translations from crowdin feature branch
    
    ---------
    
    Co-authored-by: Nikita Kolesnik <nikitakolesnik@Nikita-Kolesniks-MacBook-Pro-P45VWV2QJ9.local>
    Co-authored-by: DerivFE <80095553+DerivFE@users.noreply.github.com>

--- src/features/components/templates/footer/data.ts
+++ src/features/components/templates/footer/data.ts
@@ -143,7 +143,7 @@ const platformsLinks: SmartFooterLink[] = [
         id: 2,
         data: {
             text: '_t_deriv EZ_t_',
-            url: { type: 'internal', to: '/derivez/' },
+            url: { type: 'internal', to: '/deriv-ez/' },
         },
         visibility: {
             is_row: true,

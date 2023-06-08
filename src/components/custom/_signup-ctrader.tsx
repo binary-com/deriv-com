736e731b67d83c8308cc8e53cc8dfe460ac32f40
mohammad-hashemi-deriv <mohammad.hashemi@re-work.dev>
2023-06-08 15:16:04 +0330

    feat: remove social login links from signup page

--- src/components/custom/_signup-ctrader.tsx
+++ src/components/custom/_signup-ctrader.tsx
@@ -5,10 +5,6 @@ import { Input, Button } from 'components/form'
 import { Header, LinkText, Text } from 'components/elements'
 import { localize, Localize } from 'components/localization'
 import device from 'themes/device'
-// SVG
-import Apple from 'images/svg/custom/apple-white.svg'
-import Facebook from 'images/svg/custom/facebook-social-button.svg'
-import Google from 'images/svg/custom/google.svg'
 import useRegion from 'components/hooks/use-region'
 import Logo from 'images/svg/ctrader/logo.svg'
 import Watermark from 'images/svg/ctrader/watermark.svg'
@@ -25,11 +21,6 @@ type SignupCtraderProps = {
     is_ppc?: boolean
     is_submitting?: boolean
 }
-
-type SocialButtonProps = {
-    bgColor?: string
-}
-
 const SignupContent = styled.div`
     margin-top: 8rem;
     width: 48.4rem;
@@ -88,24 +79,6 @@ const EmailButton = styled(Button)<{ isChecked?: boolean }>`
         font-size: 1.75rem;
     }
 `
-
-const SocialButton = styled(Button)<SocialButtonProps>`
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    box-shadow: none;
-    background-color: ${(props) => props.bgColor || 'var(--color-white)'};
-    border: solid 1px var(--color-grey-21);
-    border-radius: 16px;
-    width: 100%;
-    height: 40px;
-    margin: 3px 0;
-
-    &:hover {
-        background-color: ${(props) => props.bgColor || 'var(--color-white)'};
-    }
-`
-
 const LoginText = styled(Text)`
     text-align: center;
     align-self: center;
@@ -115,18 +88,6 @@ const LoginText = styled(Text)`
         margin-top: ${(props) => props.mt};
         margin-bottom: ${(props) => props.mb};
         font-size: 2rem;
-    }
-`
-const SocialText = styled(Text)`
-    margin-right: 1.4rem;
-    margin-left: 0.7rem;
-    font-weight: 700;
-    font-size: 1.2rem;
-    color: ${(props) => props.color || '#414652'};
-
-    @media ${device.tabletL} {
-        margin-left: 2.7rem;
-        font-size: 14px;
     }
 `
 const StyledLinkText = styled(LinkText)`
@@ -140,7 +101,6 @@ const SignupCtrader = ({
     email_error_msg,
     handleInputChange,
     handleLogin,
-    handleSocialSignup,
     handleValidation,
     is_submitting,
 }: SignupCtraderProps) => {
@@ -194,47 +154,6 @@ const SignupCtrader = ({
             <Header as="p" type="small" weight="400" color="grey-5" mt="0.8rem" mb="2rem">
                 <Localize translate_text="_t_By pressing “Create demo account”, you confirm that you are 18 or older. You understand that we may use your email address to send you information about Deriv products and services as well as market news. You can always unsubscribe from these emails in your account settings. For more information, please take a look at Deriv’s Security and privacy._t_" />
             </Header>
-            <SocialButton
-                onClick={handleSocialSignup}
-                provider="google"
-                data-provider="google"
-                id="dm-signup-google"
-                type="button"
-                social
-            >
-                <img src={Google} alt="google" width="24" height="24" />
-                <SocialText>
-                    <Localize translate_text="_t_Sign up with Google_t_" />
-                </SocialText>
-            </SocialButton>
-            <SocialButton
-                onClick={handleSocialSignup}
-                provider="facebook"
-                data-provider="facebook"
-                id="dm-signup-facebook"
-                type="button"
-                social
-                bgColor="#1877F2"
-            >
-                <img src={Facebook} alt="facebook" width="24" height="24" />
-                <SocialText color="#ffffff">
-                    <Localize translate_text="_t_Sign up with Facebook_t_" />
-                </SocialText>
-            </SocialButton>
-            <SocialButton
-                onClick={handleSocialSignup}
-                provider="apple"
-                data-provider="apple"
-                id="dm-signup-apple"
-                type="button"
-                social
-                bgColor="#000000"
-            >
-                <img src={Apple} alt="apple" width="24" height="24" />
-                <SocialText color="#ffffff">
-                    <Localize translate_text="_t_Sign up with Apple_t_" />
-                </SocialText>
-            </SocialButton>
             <LoginText mt="3.75rem" mb={is_eu ? '100px' : '0'}>
                 <Localize translate_text="_t_Already have an account?_t_" />
                 <StyledLinkText

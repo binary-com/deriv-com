3ee8c6ee7960e88bdd50602b8182db5e171a5b23
Eduard Hrachou <eduardhrachou@Eduard-Hrachous-MacBook-Pro-Y24T3WTX5M.local>
2023-06-12 16:13:40 +0300

    test

--- src/features/pages/home/hero/content/index.tsx
+++ src/features/pages/home/hero/content/index.tsx
@@ -1,22 +1,10 @@
 import React from 'react'
-import loadable from '@loadable/component'
-import pMinDelay from 'p-min-delay'
 import HeroCtaButton from './hero-cta.button'
-import { hero_typewriter, hero_content_title } from './hero-content.module.scss'
+import { hero_content_title } from './hero-content.module.scss'
 import HeroHeaderItems from './hero-header.items'
 import Flex from 'features/components/atoms/flex-box'
 import Typography from 'features/components/atoms/typography'
 import { get_lang_direction, Localize } from 'components/localization'
-
-const HeroFeaturesCarousel = loadable(() => pMinDelay(import('./hero-features.carousel'), 3000), {
-    fallback: (
-        <div className={hero_typewriter}>
-            <Typography.Heading as="h3" size="large" weight="bold" color="primary">
-                <Localize translate_text="_t_Forex_t_" />
-            </Typography.Heading>
-        </div>
-    ),
-})
 
 const HomeHeroContent = () => {
     return (
@@ -25,7 +13,6 @@ const HomeHeroContent = () => {
             <Typography.Heading as="h2" size={'xs'} weight="normal" className={hero_content_title}>
                 <Localize translate_text="_t_Get the widest range of markets, trades and platforms_t_" />
             </Typography.Heading>
-            <HeroFeaturesCarousel />
             <HeroCtaButton />
         </Flex.Box>
     )

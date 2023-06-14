b81691c40e84c412622f25ded652709608d2a3d1
Michio <mohsen@re-work.dev>
2023-06-14 14:11:10 +0330

    chore: dom access test on swiper

--- src/features/pages/home/index.tsx
+++ src/features/pages/home/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import loadable from '@loadable/component'
-import HomeHero from './hero'
 import LivePricing from './live-pricing'
 import P2PBanner from './p2p-banner'
 import ClientTestimonial from './client-testimonial'
@@ -13,7 +12,6 @@ import useRegion from 'components/hooks/use-region'
 import MainNav from 'features/components/templates/navigation/main-nav'
 import SignupPublic from 'features/components/templates/signup/with-banner'
 
-const OurPlatforms = loadable(() => import('./our-platforms'))
 const Footer = loadable(() => import('features/components/templates/footer'))
 
 const HomePage = () => {
@@ -32,10 +30,8 @@ const HomePage = () => {
                 has_organization_schema
             />
             <MainNav />
-            <HomeHero />
             <LivePricing />
             <TradeTypes />
-            <OurPlatforms />
             <ClientTestimonial />
             {is_p2p_allowed_country && <P2PBanner />}
             <SignupPublic />

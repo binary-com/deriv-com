2c592719db589399909608ea0cad4a3fc4b9059a
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-06-14 13:39:36 +0200

    fix: navbar condition remove

--- src/features/components/templates/navigation/market-nav/index.tsx
+++ src/features/components/templates/navigation/market-nav/index.tsx
@@ -3,7 +3,9 @@ import NavTemplate from '../template'
 import { mainItems } from '../main-nav/content'
 import MainNavButtons from '../main-nav/main-nav-buttons'
 import MarketBottomNav from '../template/market-bottom-nav'
+import * as styles from './market-nav.module.scss'
 import { market_nav_logo } from './market-nav.module.scss'
+import Flex from 'features/components/atoms/flex-box'
 import Image from 'features/components/atoms/image'
 import LogoImage from 'images/common/rebranding_logo.svg'
 import Link from 'features/components/atoms/link'
@@ -12,7 +14,7 @@ import { usePlatformQueryParam } from 'components/hooks/use-platform-query-param
 const MarketNav = () => {
     const { is_deriv_go } = usePlatformQueryParam()
 
-    return (
+    return is_deriv_go ? null : (
         <NavTemplate
             render_bottom_nav={() => <MarketBottomNav />}
             renderLogo={() => (
@@ -21,7 +23,6 @@ const MarketNav = () => {
                 </Link>
             )}
             items={mainItems}
-            has_deriv_go={is_deriv_go}
         >
             <MainNavButtons />
         </NavTemplate>

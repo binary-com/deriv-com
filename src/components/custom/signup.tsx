bad9f46e197727a92b325a91682a9f16559288d8
mohammad-hashemi-deriv <mohammad.hashemi@re-work.dev>
2023-06-17 01:11:32 +0330

    chore: restore main signup component to default

--- src/components/custom/signup.tsx
+++ src/components/custom/signup.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { graphql } from 'gatsby'
+import { graphql, useStaticQuery, navigate } from 'gatsby'
 import styled from 'styled-components'
 import Cookies from 'js-cookie'
 import { getLanguage } from '../../common/utility'
@@ -11,6 +11,8 @@ import SignupDefault from 'components/custom/_signup-default'
 import SignupFlat from 'components/custom/_signup-flat'
 import SignupNew from 'components/custom/_signup-new'
 import SignupPublic from 'components/custom/_signup-public'
+import { Header, QueryImage, StyledLink } from 'components/elements'
+import { localize, Localize } from 'components/localization'
 import device from 'themes/device'
 import apiManager from 'common/websocket'
 
@@ -23,17 +25,37 @@ type SignupProps = {
     onSubmit?: (submit_status, email) => void
     submit_state?: string
 }
-
 type FormProps = {
     bgColor?: string
 }
 
+const EmailLink = styled(StyledLink)`
+    display: table;
+    font-size: 14px;
+    margin-top: 1.8rem;
+    text-decoration: underline;
+    width: 100%;
+    text-align: center;
+`
+
 const Form = styled.form<FormProps>`
     height: 100%;
     background-color: ${(props) => props.bgColor || 'var(--color-white)'};
-
     @media ${device.mobileL} {
         width: 100%;
+        margin-top: 70px;
+    }
+`
+const ResponseWrapper = styled(Flex)`
+    justify-content: center;
+    max-width: 24rem;
+    margin: 0 auto;
+    flex-direction: column;
+    padding: 2rem 1rem;
+    gap: 12px;
+    margin-top: -100px;
+    @media ${device.mobileL} {
+        max-width: 40rem;
     }
 `
 
@@ -45,46 +67,46 @@ export const Appearances = {
     public: 'public',
     newSignup: 'newSignup',
 }
+const query = graphql`
+    query {
+        view_email: file(relativePath: { eq: "sign-up/response-email.png" }) {
+            ...fadeIn
+        }
+    }
+`
 
 const Signup = (props: SignupProps) => {
+    const data = useStaticQuery(query)
     const [email, setEmail] = useState('')
     const [is_submitting, setSubmitting] = useState(false)
     const [email_error_msg, setEmailErrorMsg] = useState('')
     const [submit_status, setSubmitStatus] = useState('')
     const [submit_error_msg, setSubmitErrorMsg] = useState('')
-
     const validateEmail = (email_address) => {
         const error_message =
             validation.required(email_address) ||
             validation.email(email_address) ||
             submit_error_msg
-
         if (submit_error_msg) {
             setSubmitErrorMsg('')
             setSubmitStatus('')
         }
-
         return error_message
     }
-
     const handleValidation = (param) => {
         const message = typeof param === 'object' ? param.target.value : param
         setEmailErrorMsg(validateEmail(message.replace(/\s/g, '')))
     }
-
     const handleInputChange = (e) => {
         const { value } = e.target
         setEmail(value)
         handleValidation(value)
     }
-
     const getVerifyEmailRequest = (formatted_email) => {
         const affiliate_token = Cookies.getJSON('affiliate_tracking')
-
         const cookies = getCookiesFields()
         const cookies_objects = getCookiesObject(cookies)
         const cookies_value = getDataObjFromCookies(cookies_objects, cookies)
-
         return {
             verify_email: formatted_email,
             url_parameters: {
@@ -93,7 +115,6 @@ const Signup = (props: SignupProps) => {
             },
         }
     }
-
     const handleEmailSignup = (e) => {
         e.preventDefault()
         setSubmitting(true)
@@ -105,7 +126,6 @@ const Signup = (props: SignupProps) => {
         }
 
         const verify_email_req = getVerifyEmailRequest(formatted_email)
-
         apiManager
             .augmentedSend('verify_email', { ...verify_email_req, type: 'account_opening' })
             .then((response) => {
@@ -121,7 +141,6 @@ const Signup = (props: SignupProps) => {
                     }
                 }
             })
-
         if (props.appearance === 'public') {
             const success_default_link = `signup-success?email=${email}`
             const link_with_language = `${getLanguage()}/${success_default_link}`
@@ -131,23 +150,19 @@ const Signup = (props: SignupProps) => {
             navigate(success_link, { replace: true })
         }
     }
-
     const clearEmail = () => {
         setEmail('')
         setEmailErrorMsg('')
     }
     const handleSocialSignup = (e) => {
         e.preventDefault()
-
         const data_provider: TSocialProvider = e.currentTarget.getAttribute('data-provider')
         Login.initOneAll(data_provider)
     }
-
     const handleLogin = (e) => {
         e.preventDefault()
         Login.redirectToLogin()
     }
-
     const renderSwitch = (param) => {
         const parameters = {
             autofocus: props.autofocus,
@@ -161,7 +176,6 @@ const Signup = (props: SignupProps) => {
             is_ppc: props.is_ppc,
             is_submitting: is_submitting,
         }
-
         switch (param) {
             case Appearances.newSignup:
                 return <SignupNew {...parameters}></SignupNew>
@@ -180,11 +194,28 @@ const Signup = (props: SignupProps) => {
         }
     }
 
-    return (
+    return props.submit_state === 'success' ? (
+        <ResponseWrapper>
+            <Header as="p" type="subtitle-1" align="center" weight="700">
+                {localize('Check your email')}
+            </Header>
+            <Flex jc="center" height="128px">
+                <QueryImage data={data.view_email} alt="Email image" height="128px" width="128px" />
+            </Flex>
+            <Header type="paragraph-1" weight="normal" align="center">
+                <Localize
+                    translate_text="We've sent a message to {{email}} with a link to activate your account."
+                    values={{ email: props.email }}
+                />
+            </Header>
+            <EmailLink to="/check-email/" align="center">
+                {localize("Didn't receive your email?")}
+            </EmailLink>
+        </ResponseWrapper>
+    ) : (
         <Form onSubmit={handleEmailSignup} noValidate bgColor={props.bgColor}>
             {renderSwitch(props.appearance)}
         </Form>
     )
 }
-
 export default Signup

da56bc97075eea1eb7041cae593c6b3f1ea51402
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-06-16 11:24:21 +0400

    fix: deriv ez revert

--- src/features/pages/deriv-ez/index.tsx
+++ src/features/pages/deriv-ez/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Hero from '../../components/molecules/deriv-cfd-hero'
+import Hero from './hero'
 import OurPlatforms from './deprecated-other-platforms'
 import Numbers from './numbers'
 import WhatIsDerivEZ from './what-have'
 import DerivEZAppBanner from './app-banner'
-import { hero_content_data } from './hero/data'
 import WhyTradeEZ from 'features/pages/deriv-ez/why-trade'
 import StartDerivEZ from 'features/pages/deriv-ez/how-to-start'
 import MainNav from 'features/components/templates/navigation/main-nav'
@@ -23,7 +22,7 @@ const DerivEZ = () => {
                 )}
                 description={localize('_t_Deriv EZ – a CFD trading platform | Deriv_t_')}
             />
-            <Hero contentData={hero_content_data} />
+            <Hero />
             <Numbers />
             <WhyTradeEZ />
             <WhatIsDerivEZ />

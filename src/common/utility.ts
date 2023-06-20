4f3e96fdd4502849ad5e82ccf3312d653565331a
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-06-20 15:33:50 +0400

    Update src/common/utility.ts
    Co-authored-by: Jim Daniels Wasswa <104334373+jim-deriv@users.noreply.github.com>

--- src/common/utility.ts
+++ src/common/utility.ts
@@ -17,24 +17,22 @@ import { localize } from 'components/localization'
 
 export const epochToMoment = (epoch: number) => moment.unix(epoch).utc()
 export const toMoment = (value?: moment.MomentInput): moment.Moment => {
-    if (!value) return moment().utc() // returns 'now' moment object
-    if (
-        value instanceof moment &&
-        (value as moment.Moment).isValid() &&
-        (value as moment.Moment).isUTC()
-    )
-        return value as moment.Moment // returns if already a moment object
-    if (typeof value === 'number') return epochToMoment(value) // returns epochToMoment() if not a date
+   if (!value) return moment().utc(); // returns 'now' moment object
+    if (moment.isMoment(value) && value.isValid() && value.isUTC())
+        return value; // returns if already a moment object
+    if (typeof value === 'number') return epochToMoment(value); // returns epochToMoment() if not a date
 
     if (/invalid/i.test(moment(value).toString())) {
-        const today_moment = moment()
-        const days_in_month = today_moment.utc().daysInMonth()
-        const value_as_number = moment.utc(value, 'DD MMM YYYY').valueOf() / (1000 * 60 * 60 * 24)
-        return value_as_number > days_in_month
-            ? moment.utc(today_moment.add(value as string | number, 'd'), 'DD MMM YYYY')
-            : moment.utc(value, 'DD MMM YYYY') // returns target date
+        const today_moment = moment();
+        const days_in_month = today_moment.utc().daysInMonth();
+        const value_as_number = moment.utc(value, 'DD MMM YYYY').valueOf() / (1000 * 60 * 60 * 24);
+        if (typeof value === 'string' || typeof value === 'number') {
+            return value_as_number > days_in_month
+                ? moment.utc(today_moment.add(value, 'd'), 'DD MMM YYYY')
+                : moment.utc(value, 'DD MMM YYYY'); // returns target date
+        }
     }
-    return moment.utc(value)
+    return moment.utc(value);
 }
 export const formatDate = (date?: moment.MomentInput, date_format = 'YYYY-MM-DD') =>
     toMoment(date).format(date_format)

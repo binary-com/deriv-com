3a66c23b5938c90814843063b01e129554ecd113
Michio <mohsen@re-work.dev>
2023-06-21 10:58:28 +0330

    chore: updated the props model for app banner

--- src/features/components/templates/trade-platform/app-banner/index.tsx
+++ src/features/components/templates/trade-platform/app-banner/index.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import { BannerType } from 'features/components/templates/banners/blade-shape-banner/types'
 import BladeShapeBanner from 'features/components/templates/banners/blade-shape-banner'
 
-const TradingPlatformBanner = ({
-    trading_platform_banner,
-}: {
-    trading_platform_banner: BannerType
-}) => {
-    return <BladeShapeBanner data={trading_platform_banner} />
+export type TradingPlatformBannerProps = BannerType
+
+const TradingPlatformBanner = (props: TradingPlatformBannerProps) => {
+    return <BladeShapeBanner data={props} />
 }
 
 export default TradingPlatformBanner

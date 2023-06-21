c89ec0ceecef7b7461a579b2a471096de244c10a
meenu-deriv <meenakshi@deriv.com>
2023-06-21 10:20:50 +0400

    fix: number component refactor

--- src/features/components/templates/trade-platform/numbers/index.tsx
+++ src/features/components/templates/trade-platform/numbers/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { TradingPlatformNumbersProps } from '../types'
-import { number_box, number_description } from './numbers.module.scss'
 import { Localize } from 'components/localization'
 import FlexBox from 'features/components/atoms/flex-box'
 import Typography from 'features/components/atoms/typography'
@@ -11,12 +10,7 @@ const TradingPlatformNumbers = ({
     trading_platform_numbers: TradingPlatformNumbersProps
 }) => {
     return (
-        <FlexBox.Box
-            container={'fluid'}
-            direction={'col'}
-            className={number_box}
-            margin_block={'20x'}
-        >
+        <FlexBox.Box container={'fluid'} direction={'col'} margin_block={'20x'}>
             <FlexBox.Box
                 direction={'col'}
                 align={'center'}
@@ -32,27 +26,6 @@ const TradingPlatformNumbers = ({
                         </Typography.Paragraph>
                     </FlexBox.Item>
                 ))}
-            </FlexBox.Box>
-            <FlexBox.Box
-                justify={'center'}
-                margin_block={'20x'}
-                margin_inline={'8x'}
-                md={{ margin_block: '40x' }}
-            >
-                <FlexBox.Box direction={'col'} className={number_description}>
-                    <Typography.Heading align={'center'}>
-                        <Localize
-                            translate_text={trading_platform_numbers['what_is_description'].title}
-                        />
-                    </Typography.Heading>
-                    <Typography.Paragraph align={'center'}>
-                        <Localize
-                            translate_text={
-                                trading_platform_numbers['what_is_description'].subtitle
-                            }
-                        />
-                    </Typography.Paragraph>
-                </FlexBox.Box>
             </FlexBox.Box>
         </FlexBox.Box>
     )

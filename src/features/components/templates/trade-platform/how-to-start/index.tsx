45a946344e6aede11d66ee585322e8666a7f1596
Michio <mohsen@re-work.dev>
2023-06-21 11:27:21 +0330

    chore: updated the how to start trading platfrom

--- src/features/components/templates/trade-platform/how-to-start/index.tsx
+++ src/features/components/templates/trade-platform/how-to-start/index.tsx
@@ -1,31 +1,49 @@
-import React, { useState } from 'react'
-import { TradingPlatformStartProps } from '../types'
+import React, { useCallback, useState } from 'react'
+import { TradingPlatformStepperViewType } from '../types'
 import TabStepper from 'features/components/templates/tabs/stepper'
 import useBreakpoints from 'components/hooks/use-breakpoints'
+import { TabItemTypes } from 'features/components/atoms/tab/types'
+
+export type TradingPlatformStartProps = {
+    demo: TradingPlatformStepperViewType
+    demo_mobile?: TradingPlatformStepperViewType
+    real: TradingPlatformStepperViewType
+    real_mobile?: TradingPlatformStepperViewType
+}
+
+type TabType = 'demo' | 'real'
 
 const TradingPlatformHowToStart = ({
-    trading_platform_start,
-}: {
-    trading_platform_start: TradingPlatformStartProps
-}) => {
-    const [tab, setTab] = useState('demo')
+    demo,
+    demo_mobile,
+    real,
+    real_mobile,
+}: TradingPlatformStartProps) => {
+    const [tab, setTab] = useState<TabType>('demo')
     const { is_mobile_or_tablet } = useBreakpoints()
+    const [items, setItems] = useState<TabItemTypes[]>([])
 
-    const onTabClick = (chosen_tab: 'demo' | 'real') => {
-        setTab(chosen_tab)
-    }
-
-    return (
-        <TabStepper
-            tab={tab}
-            onTabClick={onTabClick}
-            items={
-                is_mobile_or_tablet
-                    ? trading_platform_start[`${tab}_mobile`]
-                    : trading_platform_start[tab]
+    const onTabClick = useCallback(
+        (chosen_tab: TabType) => {
+            setTab(chosen_tab)
+            if (chosen_tab === 'real') {
+                if (is_mobile_or_tablet) {
+                    setItems(real_mobile)
+                } else {
+                    setItems(real)
+                }
+            } else {
+                if (is_mobile_or_tablet) {
+                    setItems(demo_mobile)
+                } else {
+                    setItems(demo)
+                }
             }
-        />
+        },
+        [demo, demo_mobile, is_mobile_or_tablet, real, real_mobile],
     )
+
+    return <TabStepper tab={tab} onTabClick={onTabClick} items={items} />
 }
 
 export default TradingPlatformHowToStart

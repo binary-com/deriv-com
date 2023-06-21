5248da640f6c0dd1feba95298ac80bd174036a84
Nikita Kolesnik <nikita.kolesnik@deriv.com>
2023-06-21 11:57:47 +0300

    feat: update text

--- src/features/pages/deriv-ez/why-trade/data.tsx
+++ src/features/pages/deriv-ez/why-trade/data.tsx
@@ -9,19 +9,18 @@ export const card_data: DerivEzCardTypes[] = [
     {
         title: '_t_Easy to start_t_',
         subtitle:
-            '_t_Start trading CFDs in two easy steps — click to get an account, start trading. No need to remember another login ID and password._t_',
+            '_t_Start trading CFDs in two easy steps — just add an account and start trading. No need to remember another login ID and password._t_',
         icon: <Image src={EasyStart} height={48} alt={'_t_Easy start_t_'} />,
     },
     {
         title: '_t_24/7 trading_t_',
         subtitle:
-            '_t_Trade anytime you want, including weekends and holidays, on cryptocurrencies and our proprietary derived indices._t_',
+            '_t_Trade CFDs on cryptocurrencies and derived indices anytime, including weekends and holidays._t_',
         icon: <Image src={AnyTime} height={48} alt={'_t_24/7 chat_t_'} />,
     },
     {
         title: '_t_All your favourite assets, one platform_t_',
-        subtitle:
-            '_t_Access over 190 assets in multiple markets simultaneously on one platform._t_',
+        subtitle: '_t_Access over 190 assets in multiple markets on desktop and mobile._t_',
         icon: <Image src={FavouriteAssets} height={48} alt={'_t_Favourite assets_t_'} />,
     },
 ]

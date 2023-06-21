b19ecad6e679813821c64b8c095411bda7ab2df7
meenu-deriv <meenakshi@deriv.com>
2023-06-21 17:16:39 +0400

    fix: product page

--- src/features/pages/deriv-ctrader/index.tsx
+++ src/features/pages/deriv-ctrader/index.tsx
@@ -6,6 +6,7 @@ import CTraderWhyTrade from './why-trade'
 import CTraderTradeFeatures from './platform-features'
 import HowToStartCTrader from './how-to-start'
 import CTraderPlatformBanner from './platform-banner'
+import OtherPlatforms from './other-platforms'
 import MainNav from 'features/components/templates/navigation/main-nav'
 import Footer from 'features/components/templates/footer/'
 import Layout from 'features/components/templates/layout'
@@ -29,6 +30,7 @@ const DerivCTrader = () => {
             <CTraderTradeFeatures />
             <HowToStartCTrader />
             <CTraderPlatformBanner />
+            <OtherPlatforms />
             <Footer />
         </Layout>
     )

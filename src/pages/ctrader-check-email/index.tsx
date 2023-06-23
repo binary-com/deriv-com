908f80c126e12cef2a965d47f5612fea5edccb3d
mohammad-hashemi-deriv <mohammad.hashemi@re-work.dev>
2023-06-23 17:06:35 +0330

    feat: add didnt receive email page for ctrader signup

---
+++ src/pages/ctrader-check-email/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { WithIntl } from 'components/localization'
+import CtraderCheckEmailContainer from 'features/pages/ctrader-check-email'
+
+const CtraderCheckEmail = () => {
+    return <CtraderCheckEmailContainer />
+}
+export default WithIntl()(CtraderCheckEmail)

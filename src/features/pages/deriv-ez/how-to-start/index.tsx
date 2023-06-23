fa03a9aadf5f077f1f30f52cb67bd9e4060a234e
Nikita Kolesnik <nikita.kolesnik@deriv.com>
2023-06-23 11:13:53 +0300

    refactor: use div instead of condition

--- src/features/pages/deriv-ez/how-to-start/index.tsx
+++ src/features/pages/deriv-ez/how-to-start/index.tsx
@@ -1,21 +1,28 @@
 import React, { useState } from 'react'
 import { trading_platform_start } from './data'
 import TabStepper from 'features/components/templates/tabs/stepper'
-import useBreakpoints from 'components/hooks/use-breakpoints'
 
 const StartDerivEZ = () => {
     const [tab, setTab] = useState('demo')
-    const { is_mobile_or_tablet } = useBreakpoints()
 
     const onTabClick = (chosenTab: 'demo' | 'real') => {
         setTab(chosenTab)
     }
-    console.log(is_mobile_or_tablet)
-    const tab_items = is_mobile_or_tablet
-        ? trading_platform_start[`${tab}_mobile`]
-        : trading_platform_start[tab]
 
-    return <TabStepper tab={tab} onTabClick={onTabClick} items={tab_items} />
+    return (
+        <>
+            <div className="visible-phone-and-tablet">
+                <TabStepper
+                    tab={tab}
+                    onTabClick={onTabClick}
+                    items={trading_platform_start[`${tab}_mobile`]}
+                />
+            </div>
+            <div className="visible-larger-than-tablet">
+                <TabStepper tab={tab} onTabClick={onTabClick} items={trading_platform_start[tab]} />
+            </div>
+        </>
+    )
 }
 
 export default StartDerivEZ

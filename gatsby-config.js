c3bde5dbf746c58cd4672900ba0910a1de6ca00b
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-06-26 20:54:29 +0600

    Habib/move sw (#5089)
    * chore: add service worker
    
    * chore: add service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -345,7 +345,7 @@ module.exports = {
         {
             resolve: `gatsby-plugin-offline`,
             options: {
-                appendScript: require.resolve(`./static/pushwoosh-service-worker.js`),
+                appendScript: require.resolve(`./src/pushwoosh-service-worker.js`),
                 precachePages: [`/`],
                 workboxConfig: {
                     runtimeCaching: [

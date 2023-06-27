0262f0d35eeb2f35725386fd7d8e5914ffea9d3c
habib-binary <habib@regentmarkets.com>
2023-06-27 17:51:59 +0600

    chore: update logic

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,5 +1,7 @@
 // Check if Pushwoosh service worker is already active
-if (!self.registration.active || !self.registration.active.scriptURL.includes('/sw.js')) {
-    // eslint-disable-next-line no-undef
-    importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js')
+if (!self.registration.active) {
+    if (self.registration.active.state === 'redundant') {
+        // eslint-disable-next-line no-undef
+        importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js')
+    }
 }

f7129e9afa398759121cc0a4c92a31d1c8b0074b
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-06-27 11:05:44 +0600

    Revert "Habib/cache clean mode sw (#5092)" (#5093)
    This reverts commit bd424b5e8b39d7e350a83b84a394a8d807248d8e.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -67,7 +67,7 @@ const pushwooshInit = (push_woosh) => {
             safariWebsitePushID: 'web.com.deriv',
             defaultNotificationTitle: 'Deriv.com',
             defaultNotificationImage: 'https://deriv.com/favicons/favicon-192x192.png',
-            serviceWorkerUrl: '/sw.js?cache_clean=' + new Date().getTime(),
+            serviceWorkerUrl: '/sw.js'
         },
     ])
 

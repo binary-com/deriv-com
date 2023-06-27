b9a7634cfde6e6d5cc48311831e326aa8f831a28
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-06-27 17:22:04 +0600

    Revert "Habib/investigate sw (#5101)" (#5102)
    This reverts commit 3e811ff8996ead977d1919a641b007c22d92a62c.

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,6 +1,4 @@
-// Check if Pushwoosh service worker is already active
-if (self.registration.active) console.log(self.registration, 13)
-if (!self.registration.active || !self.registration.scriptURL.includes('cache_clean')) {
-    // eslint-disable-next-line no-undef
-    importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js')
-}
+// eslint-disable-next-line no-undef
+importScripts(
+    'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,
+)

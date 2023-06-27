c315095c1c3b3b18b35efa9e25ed7a6b0442e6af
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-06-27 11:05:28 +0600

    Revert "Habib/cache clean mode sw (#5092)"
    This reverts commit bd424b5e8b39d7e350a83b84a394a8d807248d8e.

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,10 +1,4 @@
-// Check if Pushwoosh service worker is already active
-if (
-    !self.registration.active ||
-    !self.registration.active.scriptURL.includes('pushwoosh-service-worker.js')
-) {
-    // eslint-disable-next-line no-undef
-    importScripts(
-        'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,
-    )
-}
+// eslint-disable-next-line no-undef
+importScripts(
+    'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,
+)

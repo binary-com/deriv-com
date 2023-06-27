df03bfab6a079b23fbe15a07fe0659ba89b12dcb
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-06-27 15:44:11 +0600

    Revert "chore: add logic to pw sw (#5094)"
    This reverts commit 159e0b9b25a3b87e0095f46b8701b86def87efa0.

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,7 +1,4 @@
-// Check if Pushwoosh service worker is already active
-if (!self.registration.active.scriptURL.includes('?cache_clean')) {
-    // eslint-disable-next-line no-undef
-    importScripts(
-        'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,
-    )
-}
+// eslint-disable-next-line no-undef
+importScripts(
+    'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,
+)

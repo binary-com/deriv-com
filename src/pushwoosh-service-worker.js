4635d2f4ad243bab1b1c7205a85996a7f0f8f56a
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-06-27 16:44:08 +0600

    Revert "Habib/check pw cache clean (#5099)" (#5100)
    This reverts commit d786f0bed6eb2f17e33e32bdef383c2fdc858bfb.

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,5 +1,4 @@
-// Check if Pushwoosh service worker is already active
-if (!self.registration.active || !self.registration.active.scriptURL.includes('cache_clean')) {
-    // eslint-disable-next-line no-undef
-    importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js')
-}
+// eslint-disable-next-line no-undef
+importScripts(
+    'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,
+)

b75b92af35e5e2a07ca202af342f4ff350528c20
habib-binary <habib@regentmarkets.com>
2023-06-27 15:44:59 +0600

    chore: add logic to pw

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,5 +1,5 @@
 // Check if Pushwoosh service worker is already active
-if (!self.registration.active.scriptURL.includes('?cache_clean')) {
+if (!self.registration.active || !self.registration.active.scriptURL.includes('pushwoosh')) {
     // eslint-disable-next-line no-undef
     importScripts(
         'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,

d786f0bed6eb2f17e33e32bdef383c2fdc858bfb
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-06-27 16:11:51 +0600

    Habib/check pw cache clean (#5099)
    * chore: add logic to pw sw
    
    * chore: add logic to pw
    
    * chore: add logic to pw

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,4 +1,5 @@
-// eslint-disable-next-line no-undef
-importScripts(
-    'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,
-)
+// Check if Pushwoosh service worker is already active
+if (!self.registration.active || !self.registration.active.scriptURL.includes('cache_clean')) {
+    // eslint-disable-next-line no-undef
+    importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js')
+}

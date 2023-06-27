1e3f7eb6a17e3eca7cf218bfe27da58ca8becd1f
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-06-27 19:43:23 +0600

    Revert "Habib/check activated worker (#5111)"
    This reverts commit b1a43d76178ef9806d220cf2dc900f28e9031ac1.

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,13 +1,4 @@
-// Check if Pushwoosh service worker is already active
-if (self.registration && self.registration.active) {
-    console.log(self.registration, 13)
-    if (self.registration.active.state === 'redundant') {
-        self.registration.unregister().then(() => {
-            self.registration.update()
-        })
-    }
-}
-
-// Import the Pushwoosh service worker
 // eslint-disable-next-line no-undef
-importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js')
+importScripts(
+    'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,
+)

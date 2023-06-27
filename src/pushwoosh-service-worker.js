159e0b9b25a3b87e0095f46b8701b86def87efa0
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-06-27 15:20:40 +0600

    chore: add logic to pw sw (#5094)

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,4 +1,7 @@
-// eslint-disable-next-line no-undef
-importScripts(
-    'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,
-)
+// Check if Pushwoosh service worker is already active
+if (!self.registration.active.scriptURL.includes('?cache_clean')) {
+    // eslint-disable-next-line no-undef
+    importScripts(
+        'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,
+    )
+}

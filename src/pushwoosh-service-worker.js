37119e7a97017582272ccc8d5709fcb406d00d9c
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-06-27 18:19:18 +0600

    Habib/sw based on state redundant (#5107)
    * chore: add logic to pw sw
    
    * chore: add logic to pw
    
    * chore: add logic to pw
    
    * chore: update logic
    
    * chore: update logic
    
    * chore: update logic
    
    * chore: add redundant check

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,4 +1,8 @@
-// eslint-disable-next-line no-undef
-importScripts(
-    'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,
-)
+// Check if Pushwoosh service worker is already active
+if (self.registration && self.registration.active) {
+    console.log(self.registration, 13)
+    if (self.registration.active.state === 'redundant') {
+        // eslint-disable-next-line no-undef
+        importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js')
+    }
+}

768336dcaec6570b6ca6974b336be24c5d97f3f3
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-06-27 17:52:37 +0600

    Habib/sw based on state (#5105)
    * chore: update logic

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,4 +1,7 @@
-// eslint-disable-next-line no-undef
-importScripts(
-    'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,
-)
+// Check if Pushwoosh service worker is already active
+if (!self.registration.active) {
+    if (self.registration.active.state === 'redundant') {
+        // eslint-disable-next-line no-undef
+        importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js')
+    }
+}

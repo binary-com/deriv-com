07a0d661effb965f915969382130d8cfabcd9ea9
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-06-27 18:54:52 +0600

    Revert "Habib/sw based on state redundant (#5107)" (#5108)
    This reverts commit 37119e7a97017582272ccc8d5709fcb406d00d9c.

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,8 +1,4 @@
-// Check if Pushwoosh service worker is already active
-if (self.registration && self.registration.active) {
-    console.log(self.registration, 13)
-    if (self.registration.active.state === 'redundant') {
-        // eslint-disable-next-line no-undef
-        importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js')
-    }
-}
+// eslint-disable-next-line no-undef
+importScripts(
+    'https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search,
+)

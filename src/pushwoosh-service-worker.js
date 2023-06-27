d41c5ecaddb320e722314e6a196330c667a4190c
habib-binary <habib@regentmarkets.com>
2023-06-27 18:16:20 +0600

    chore: add redundant check

--- src/pushwoosh-service-worker.js
+++ src/pushwoosh-service-worker.js
@@ -1,5 +1,6 @@
 // Check if Pushwoosh service worker is already active
-if (!self.registration.active) {
+if (self.registration && self.registration.active) {
+    console.log(self.registration, 13)
     if (self.registration.active.state === 'redundant') {
         // eslint-disable-next-line no-undef
         importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js')

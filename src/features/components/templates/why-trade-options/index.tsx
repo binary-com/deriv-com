ef0591190d65c01076da87ed0ddb05f4400dca48
hasan-deriv <abuhasan@deriv.com>
2023-06-30 12:08:53 +0800

    feat: added heading

--- src/features/components/templates/why-trade-options/index.tsx
+++ src/features/components/templates/why-trade-options/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import Container from 'features/components/atoms/container'
+import Typography from 'features/components/atoms/typography'
+import { Localize } from 'components/localization'
 
 const WhyTradeOptions = () => {
-    return <div>WhyTradeOptions</div>
+    return (
+        <Container.Fixed as="section" bgcolor="black" padding_block="40x">
+            <Container.Fluid>
+                <Typography.Heading
+                    as="h2"
+                    textcolor="inverted"
+                    align="center"
+                    size="large"
+                    mb="20x"
+                >
+                    <Localize translate_text="_t_Why trade options on Deriv_t_" />
+                </Typography.Heading>
+            </Container.Fluid>
+        </Container.Fixed>
+    )
 }
 
 export default WhyTradeOptions

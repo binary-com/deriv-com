890916cc6ddccc4e42eef0b1498f553534a807c0
Nuzhy-Deriv <nuzhy@deriv.com>
2023-07-03 12:53:37 +0800

    feat: refactor and images

--- src/features/components/templates/banners/trade-type-banner/data.tsx
+++ src/features/components/templates/banners/trade-type-banner/data.tsx
@@ -4,19 +4,21 @@ import { StaticImage } from 'gatsby-plugin-image'
 export const banner_images = {
     banner: (
         <StaticImage
-            src="../../../../../images/common/trade-types/trade-patterns.png"
+            src="../../../../../images/common/trade-types/trade-patterns-new.png"
             alt="Trade type banner"
+            loading="eager"
             formats={['avif', 'webp', 'auto']}
-            placeholder="none"
+            placeholder="blurred"
             style={{ height: '45rem' }}
         />
     ),
     banner_mobile: (
         <StaticImage
-            src="../../../../../images/common/trade-types/trade-patterns-mobile.png"
+            src="../../../../../images/common/trade-types/trade-patterns-mobile-new.png"
             alt="Trade type banner"
+            loading="eager"
             formats={['avif', 'webp', 'auto']}
-            placeholder="none"
+            placeholder="blurred"
             style={{ height: '57rem' }}
         />
     ),
--- src/features/components/templates/banners/trade-type-banner/index.tsx
+++ src/features/components/templates/banners/trade-type-banner/index.tsx
@@ -6,13 +6,17 @@ import Typography from 'features/components/atoms/typography'
 import { Localize } from 'components/localization'
 import useBreakpoints from 'components/hooks/use-breakpoints'
 import { TString } from 'types/generics'
+import dclsx from 'features/utils/dclsx'
 
 const TradeTypeBanner = ({ title }: { title: TString }) => {
     const { is_mobile } = useBreakpoints()
     return (
         <FlexBox.Box as="section" className={trade_type_hero} align="center" justify="center">
-            <div className={trade_type_hero_img}>
-                {is_mobile ? banner_images['banner_mobile'] : banner_images['banner']}
+            <div className={dclsx(trade_type_hero_img, 'visible-phone-only')}>
+                {banner_images['banner_mobile']}
+            </div>
+            <div className={dclsx(trade_type_hero_img, 'visible-larger-than-phone')}>
+                {banner_images['banner']}
             </div>
             <Typography.Heading textcolor="brand" size={is_mobile ? 'large' : 'xlarge'}>
                 <Localize translate_text={title} />

f0106a6e130c2f0ae49c8e885b10fbc32a838379
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-07-03 10:19:49 +0200

    fix: navigation scroll

--- src/features/components/templates/navigation/template/desktop-menu/desktop.items.tsx
+++ src/features/components/templates/navigation/template/desktop-menu/desktop.items.tsx
@@ -4,17 +4,21 @@ import { SmartNavDropItems } from '../../types'
 import NavDropItems from '../nav-drop-items'
 import { Localize } from 'components/localization'
 import Typography from 'features/components/atoms/typography'
+import dclsx from 'features/utils/dclsx'
 
 interface DesktopItemsProps {
     item: SmartNavDropItems
+    active: string
 }
 
-const NavDesktopItem = ({ item }: DesktopItemsProps) => {
+const NavDesktopItem = ({ item, active }: DesktopItemsProps) => {
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         if (!window.matchMedia('(hover: none)').matches) {
             e.preventDefault()
         }
     }
+
+    const has_scroll_menu = active === '_t_Trade_t_' || active === '_t_Markets_t_'
 
     return (
         <NavigationMenu.Item value={item.data.title}>
@@ -23,7 +27,11 @@ const NavDesktopItem = ({ item }: DesktopItemsProps) => {
                     <Localize translate_text={item.data.title} />
                 </Typography.Paragraph>
             </NavigationMenu.Trigger>
-            <NavigationMenu.Content className="navigation_content">
+            <NavigationMenu.Content
+                className={dclsx('navigation_content', {
+                    navigation_scrollable_menu: has_scroll_menu,
+                })}
+            >
                 <NavDropItems items={item.data} />
             </NavigationMenu.Content>
         </NavigationMenu.Item>

cd7c7152c5a5806c470f7498650eed0a9107e84f
Nuzhy-Deriv <nuzhy@deriv.com>
2023-07-03 10:24:27 +0800

    feat: refactor

--- src/features/components/templates/banners/trade-type-banner/index.tsx
+++ src/features/components/templates/banners/trade-type-banner/index.tsx
@@ -1,31 +1,23 @@
 import React from 'react'
 import { trade_type_hero, trade_type_hero_img } from './trade-type.module.scss'
 import { banner_images } from './data'
-import Container from 'features/components/atoms/container'
+import FlexBox from 'features/components/atoms/flex-box'
 import Typography from 'features/components/atoms/typography'
 import { Localize } from 'components/localization'
-import dclsx from 'features/utils/dclsx'
 import useBreakpoints from 'components/hooks/use-breakpoints'
 import { TString } from 'types/generics'
 
-type BannerType = {
-    title: TString
-}
-
-const TradeTypeBanner = (data: BannerType) => {
+const TradeTypeBanner = ({ title }: { title: TString }) => {
     const { is_mobile } = useBreakpoints()
     return (
-        <Container.Fixed
-            as="section"
-            className={dclsx(trade_type_hero, 'flex', 'align-items-center', 'justify-center')}
-        >
+        <FlexBox.Box as="section" className={trade_type_hero} align="center" justify="center">
             <div className={trade_type_hero_img}>
                 {is_mobile ? banner_images['banner_mobile'] : banner_images['banner']}
             </div>
             <Typography.Heading textcolor="brand" size={is_mobile ? 'large' : 'xlarge'}>
-                <Localize translate_text={data.title} />
+                <Localize translate_text={title} />
             </Typography.Heading>
-        </Container.Fixed>
+        </FlexBox.Box>
     )
 }
 

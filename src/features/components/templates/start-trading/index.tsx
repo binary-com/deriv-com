c839b61b85c8ce54ebab93920b3d7c704aa0a373
hasan-deriv <abuhasan@deriv.com>
2023-07-04 13:12:18 +0800

    feat: added type to strat trading template

--- src/features/components/templates/start-trading/index.tsx
+++ src/features/components/templates/start-trading/index.tsx
@@ -2,20 +2,33 @@ import React from 'react'
 import Container from 'features/components/atoms/container'
 import Typography from 'features/components/atoms/typography'
 import { Localize } from 'components/localization'
-import TimelineItem from 'features/components/molecules/timeline-item'
+import TimelineItem, { TimelineItemType } from 'features/components/molecules/timeline-item'
 import LinkButton from 'features/components/atoms/link-button'
 import Flex from 'features/components/atoms/flex-box'
+import { TString } from 'types/generics'
+import { LinkUrlType } from 'features/types'
 
-const StartTrading = () => {
+export type StartTradingType = {
+    heading: TString
+    button: {
+        text: TString
+        url: LinkUrlType
+    }
+    items: TimelineItemType[]
+}
+
+const StartTrading = ({ data }: { data: StartTradingType }) => {
     return (
         <Container.Fixed as="section" padding_block="40x">
             <Flex.Box direction="col" container="fluid" align="start">
                 <Typography.Heading as="h2" textcolor="black" size="small" mb="20x">
-                    <Localize translate_text="_t_Start trading options on Deriv_t_" />
+                    <Localize translate_text={data.heading} />
                 </Typography.Heading>
-
-                <LinkButton.Primary url={{ type: 'internal', to: '/' }}>
-                    <Localize translate_text="_t_Create free demo account_t_" />
+                {data.items.map((item) => (
+                    <TimelineItem key={item.heading} data={item} />
+                ))}
+                <LinkButton.Primary url={data.button.url}>
+                    <Localize translate_text={data.button.text} />
                 </LinkButton.Primary>
             </Flex.Box>
         </Container.Fixed>

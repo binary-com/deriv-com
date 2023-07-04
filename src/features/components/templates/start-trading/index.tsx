29fc89ab917cd7f3842789945b1d0fa1070cc6c4
hasan-deriv <abuhasan@deriv.com>
2023-07-04 11:42:03 +0800

    feat: created strat trading template

---
+++ src/features/components/templates/start-trading/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from 'features/components/atoms/container'
+
+const StartTrading = () => {
+    return (
+        <Container.Fixed as="section" padding_block="40x">
+            <Container.Fluid>StartTrading</Container.Fluid>
+        </Container.Fixed>
+    )
+}
+
+export default StartTrading

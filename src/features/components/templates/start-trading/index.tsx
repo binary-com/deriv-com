0bb6aaad3ce1ebe3f1c315c41e27d3169cd84084
hasan-deriv <abuhasan@deriv.com>
2023-07-04 11:55:03 +0800

    feat: added heading

--- src/features/components/templates/start-trading/index.tsx
+++ src/features/components/templates/start-trading/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Container from 'features/components/atoms/container'
+import Typography from 'features/components/atoms/typography'
+import { Localize } from 'components/localization'
 
 const StartTrading = () => {
     return (
         <Container.Fixed as="section" padding_block="40x">
-            <Container.Fluid>StartTrading</Container.Fluid>
+            <Container.Fluid>
+                <Typography.Heading as="h2" textcolor="black" align="center" size="small" mb="20x">
+                    <Localize translate_text="_t_Start trading options on Deriv_t_" />
+                </Typography.Heading>
+            </Container.Fluid>
         </Container.Fixed>
     )
 }

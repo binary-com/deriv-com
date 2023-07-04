2444e493b6eb88b3626f4737d392d637ed5260ac
hasan-deriv <abuhasan@deriv.com>
2023-07-04 12:47:05 +0800

    feat: added timeline's type

--- src/features/components/molecules/timeline-item/index.tsx
+++ src/features/components/molecules/timeline-item/index.tsx
@@ -3,15 +3,26 @@ import { timeline_item } from './style.module.scss'
 import { Localize } from 'components/localization'
 import Typography from 'features/components/atoms/typography'
 import Flex from 'features/components/atoms/flex-box'
-import checkCircle from 'images/svg/check-circle.svg'
 import Image from 'features/components/atoms/image'
 import dclsx from 'features/utils/dclsx'
+import { TString } from 'types/generics'
+import { FlexBoxProps } from 'features/components/atoms/flex-box/box'
 
-const TimelineItem = () => {
+export type TimelineItemType = {
+    icon_src: string
+    heading: TString
+    description: TString
+}
+
+interface TimelineItemProps extends FlexBoxProps<'div'> {
+    data: TimelineItemType
+}
+
+const TimelineItem = ({ data, ...rest }: TimelineItemProps) => {
     return (
         <Flex.Box gap="12x" pb="20x" className={timeline_item}>
             <Image
-                src={checkCircle}
+                src={data.icon_src}
                 alt="check icon"
                 width={24}
                 height={24}
@@ -19,10 +30,10 @@ const TimelineItem = () => {
             />
             <div>
                 <Typography.Heading as="h3" size="xs" mb="4x">
-                    <Localize translate_text="_t_Practise_t_" />
+                    <Localize translate_text={data.heading} />
                 </Typography.Heading>
                 <Typography.Paragraph>
-                    <Localize translate_text="_t_Open a demo account and get unlimited virtual funds to practise on our platforms — Deriv Trader, SmartTrader, and Deriv Bot._t_" />
+                    <Localize translate_text={data.description} />
                 </Typography.Paragraph>
             </div>
         </Flex.Box>

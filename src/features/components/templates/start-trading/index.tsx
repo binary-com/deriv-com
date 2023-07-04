25c809d0d0e2eb9a157d6657e0a3958fc27e4774
hasan-deriv <abuhasan@deriv.com>
2023-07-04 13:09:26 +0800

    feat: added button

--- src/features/components/templates/start-trading/index.tsx
+++ src/features/components/templates/start-trading/index.tsx
@@ -2,15 +2,22 @@ import React from 'react'
 import Container from 'features/components/atoms/container'
 import Typography from 'features/components/atoms/typography'
 import { Localize } from 'components/localization'
+import TimelineItem from 'features/components/molecules/timeline-item'
+import LinkButton from 'features/components/atoms/link-button'
+import Flex from 'features/components/atoms/flex-box'
 
 const StartTrading = () => {
     return (
         <Container.Fixed as="section" padding_block="40x">
-            <Container.Fluid>
-                <Typography.Heading as="h2" textcolor="black" align="center" size="small" mb="20x">
+            <Flex.Box direction="col" container="fluid" align="start">
+                <Typography.Heading as="h2" textcolor="black" size="small" mb="20x">
                     <Localize translate_text="_t_Start trading options on Deriv_t_" />
                 </Typography.Heading>
-            </Container.Fluid>
+
+                <LinkButton.Primary url={{ type: 'internal', to: '/' }}>
+                    <Localize translate_text="_t_Create free demo account_t_" />
+                </LinkButton.Primary>
+            </Flex.Box>
         </Container.Fixed>
     )
 }

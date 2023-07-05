baa85ce621af9bae05879a63f895e2539fca9e70
hasan-deriv <abuhasan@deriv.com>
2023-07-05 11:32:39 +0800

    feat: removed old options page and created new one

---
+++ src/features/pages/trade-types/options/digital-options/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DigitalOptions = () => {
+    return <div>DigitalOptions</div>
+}
+
+export default DigitalOptions
---
+++ src/pages/trade-types/options/digital-options/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { WithIntl } from 'components/localization'
+import DigitalOptionsPage from 'features/pages/trade-types/options/digital-options'
+
+const DigitalOptions = () => {
+    return <DigitalOptionsPage />
+}
+
+export default WithIntl()(DigitalOptions)

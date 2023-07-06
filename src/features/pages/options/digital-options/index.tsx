ac8745e11a984935bb8955239b04d6c8110f0e95
hasan-deriv <abuhasan@deriv.com>
2023-07-06 15:20:37 +0800

    feat: created digital options page

---
+++ src/features/pages/options/digital-options/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DigitalOptionsPage = () => {
+    return <div>DigitalOptionsPage</div>
+}
+
+export default DigitalOptionsPage
---
+++ src/pages/trade-types/options/digital-options/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { WithIntl } from 'components/localization'
+import DigitalOptionsPage from 'features/pages/options/digital-options'
+
+const DigitalOptions = () => {
+    return <DigitalOptionsPage />
+}
+
+export default WithIntl()(DigitalOptions)

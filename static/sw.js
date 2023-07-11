4c9896caa1b5f14193ad965a58abdba1ae75c652
habib-binary <habib@regentmarkets.com>
2023-07-11 11:07:43 +0600

    chore: update sw

--- static/sw.js
+++ static/sw.js
@@ -1,13 +1,19 @@
-self.addEventListener('activate', function(e) {
-    self.registration.unregister().then(function() {
-      return self.clients.matchAll();
-    }).then(function(clients) {
-      clients.forEach(client => client.navigate(client.url))
-    }).then(function() {
-      // Register the new service worker
-      // eslint-disable-next-line no-undef
-      importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search);
-    }).catch(function(error) {
-        return error
+// Unregister all previous service workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
+// Register the new service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search)
+    .then(function(registration) {
+      console.log('Pushwoosh service worker registered:', registration);
+    })
+    .catch(function(error) {
+      console.error('Error registering Pushwoosh service worker:', error);
     });
-});
+}

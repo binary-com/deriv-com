64b80a9aea587f5e6787b72d7781ee5a2c2a1cfc
hasan-deriv <abuhasan@deriv.com>
2023-07-11 11:50:41 +0800

    fix: remove trade types

--- src/pages/home/_lazy-load.tsx
+++ src/pages/home/_lazy-load.tsx
@@ -1,9 +1,6 @@
 import Loadable from '@loadable/component'
 
 export const Markets = Loadable(() => import(/* webpackChunkName: "markets" */ './_markets'))
-export const TradeTypes = Loadable(
-    () => import(/* webpackChunkName: "trade-types" */ './_trade-types'),
-)
 export const OurPlatforms = Loadable(
     () => import(/* webpackChunkName: "our-platforms" */ './_our_platforms'),
 )

e6752478271538f3a9a169ae487935405b40d595
hasan-deriv <abuhasan@deriv.com>
2023-07-13 12:53:48 +0800

    fix: added fix width to layout

--- src/features/components/templates/layout/index.tsx
+++ src/features/components/templates/layout/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import loadable from '@loadable/component'
 import pMinDelay from 'p-min-delay'
+import { main_wrapper } from './style.module.scss'
 import { usePlatformQueryParam } from 'components/hooks/use-platform-query-param'
 import PpcProvider from 'features/contexts/ppc-campaign/ppc.provider'
 import { getLanguage, isBrowser } from 'common/utility'
@@ -33,7 +34,7 @@ const Layout = ({ children, is_ppc = false, is_ppc_redirect = false }: LayoutPro
 
     return (
         <PpcProvider is_ppc={is_ppc} is_ppc_redirect={is_ppc_redirect}>
-            <main>{children}</main>
+            <main className={main_wrapper}>{children}</main>
             <BrowserUpdateAlert />
             <LayoutOverlay />
         </PpcProvider>

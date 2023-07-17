7cac9440ad4790a5854b284eee8c57d25faf6345
hasan-deriv <abuhasan@deriv.com>
2023-07-17 11:03:50 +0800

    fix: added rtl dir to start trading

--- src/features/components/templates/start-trading/index.tsx
+++ src/features/components/templates/start-trading/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Container from 'features/components/atoms/container'
 import Typography from 'features/components/atoms/typography'
-import { Localize } from 'components/localization'
+import { Localize, get_lang_direction } from 'components/localization'
 import TimelineItem, { TimelineItemType } from 'features/components/molecules/timeline-item'
 import LinkButton from 'features/components/atoms/link-button'
 import Flex from 'features/components/atoms/flex-box'
@@ -20,13 +20,14 @@ export type StartTradingType = {
 const StartTrading = ({ data }: { data: StartTradingType }) => {
     return (
         <Container.Fixed as="section" padding_block="20x" md={{ padding_block: '40x' }}>
-            <Flex.Box direction="col" container="fluid" align="start">
+            <Flex.Box direction="col" container="fluid" align="start" dir={get_lang_direction()}>
                 <Typography.Heading
                     as="h2"
                     textcolor="black"
                     size="small"
                     mb="12x"
                     md={{ mb: '20x' }}
+                    align="left"
                 >
                     <Localize translate_text={data.heading} />
                 </Typography.Heading>

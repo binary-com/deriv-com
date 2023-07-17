8f835499d2344f9815c210ff9005c55e7803fbe5
hasan-deriv <abuhasan@deriv.com>
2023-07-17 12:35:25 +0800

    feat: created asians page

---
+++ src/pages/trade-types/options/digital-options/asians/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { WithIntl } from 'components/localization'
+import UpAndDownDigitalOptionsPage from 'features/pages/options/digital/asians'
+
+const DigitalOptionsAsians = () => {
+    return <UpAndDownDigitalOptionsPage />
+}
+
+export default WithIntl()(DigitalOptionsAsians)

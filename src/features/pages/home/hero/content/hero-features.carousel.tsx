d16f12e19861d3542bd144d45c379a96880ec056
Eduard Hrachou <eduardhrachou@Eduard-Hrachous-MacBook-Pro-Y24T3WTX5M.local>
2023-07-17 10:05:48 +0300

    replace js animations to css

--- src/features/pages/home/hero/content/hero-features.carousel.tsx
+++ src/features/pages/home/hero/content/hero-features.carousel.tsx
@@ -1,32 +1,25 @@
 import React from 'react'
-import Typewriter from 'typewriter-effect'
-import { hero_typewriter } from './hero-content.module.scss'
-import dclsx from 'features/utils/dclsx'
+import { hero_typewriter, text, wrapper } from './hero-content.module.scss'
 import { localize } from 'components/localization'
+
+const strings = [
+    localize('_t_Forex_t_'),
+    localize('_t_Cryptocurrencies_t_'),
+    localize('_t_Stocks & indices_t_'),
+    localize('_t_Commodities_t_'),
+    localize('_t_Derived Indices_t_'),
+];
 
 const HeroFeaturesCarousel = () => {
     return (
         <div className={hero_typewriter}>
-            <Typewriter
-                options={{
-                    strings: [
-                        localize('_t_Forex_t_'),
-                        localize('_t_Cryptocurrencies_t_'),
-                        localize('_t_Stocks & indices_t_'),
-                        localize('_t_Commodities_t_'),
-                        localize('_t_Derived Indices_t_'),
-                    ],
-                    autoStart: true,
-                    loop: true,
-                    wrapperClassName: dclsx(
-                        'heading',
-                        'heading-large',
-                        'typography-weight-bold',
-                        'typography-color-primary',
-                    ),
-                    cursorClassName: dclsx('heading', 'heading-large', 'typography-color-primary'),
-                }}
-            />
+            <div className={text}>
+                <div className={wrapper}>
+                    {strings.map((string, i) => (
+                        <p key={i}>{string}</p>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }

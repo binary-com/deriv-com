9242c5fe4fbdc11714b8c4b7e83e0bdb92772b04
hasan-deriv <abuhasan@deriv.com>
2023-07-17 16:33:09 +0800

    fix: added asians faq content

---
+++ src/features/pages/options/digital/asians/data.ts
@@ -0,0 +1,81 @@
+import { OptionsFAQDataItem } from 'features/components/templates/options-content/type'
+
+export const asiansFAQ: OptionsFAQDataItem[] = [
+    {
+        id: 1,
+        question: '_t_Where can I trade Asian options?_t_',
+        answers: [
+            {
+                id: 1,
+                type: 'text',
+                text: '_t_Asian options are available on the SmartTrader and Deriv Bot trading platforms. You can trade Asian options on synthetics._t_',
+            },
+        ],
+    },
+    {
+        id: 2,
+        question: '_t_How can I open an Asians trade?_t_',
+        answers: [
+            {
+                id: 1,
+                type: 'list',
+                list_title: '_t_You can open an Asians trade by:_t_',
+                list_items: [
+                    '_t_- Choosing your preferred instrument._t_',
+                    '_t_- Setting the duration of your trade._t_',
+                    '_t_- Entering your preferred stake amount or desired potential payout._t_',
+                    '_t_- Making your market prediction._t_',
+                ],
+            },
+            {
+                id: 2,
+                type: 'text',
+                text: '_t_Once you have determined the above parameters, you can open an Asians trade on SmartTrader and Deriv Bot._t_',
+            },
+        ],
+    },
+    {
+        id: 3,
+        question: '_t_How do Asians contracts work?_t_',
+        answers: [
+            {
+                id: 1,
+                type: 'text',
+                text: "_t_Asians contracts calculate the average price of an underlying asset over a predetermined trade duration of 5 to 10 ticks. When you open an Asians trade, you'll predict whether the final price tick (the last tick) will be higher or lower than the average price throughout the contract period._t_",
+            },
+        ],
+    },
+    {
+        id: 4,
+        question: '_t_Can I close an Asians contract before the contract expires?_t_',
+        answers: [
+            {
+                id: 1,
+                type: 'text',
+                text: '_t_No, you cannot close an Asians contract before it expires._t_',
+            },
+        ],
+    },
+    {
+        id: 5,
+        question: '_t_Can I select the duration for an Asians contract?_t_',
+        answers: [
+            {
+                id: 1,
+                type: 'text',
+                text: '_t_Yes, you can choose the duration of your Asians contract anywhere between 5 and 10 ticks._t_',
+            },
+        ],
+    },
+    {
+        id: 5,
+        question: '_t_Can I open multiple Asians contracts simultaneously?_t_',
+        answers: [
+            {
+                id: 1,
+                type: 'text',
+                text: '_t_Yes, you can. You can open contracts with different parameters to take advantage of more market opportunities._t_',
+            },
+        ],
+    },
+]
--- src/features/pages/options/digital/asians/index.tsx
+++ src/features/pages/options/digital/asians/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
-// import { upAndDownContentItems, upAndDownFAQ, upAndDownMarkets, upAndDownPlatforms } from './data'
+import {
+    /* upAndDownContentItems, */ asiansFAQ /* upAndDownMarkets, upAndDownPlatforms */,
+} from './data'
 // import AvailableMarketPlatforms from 'features/components/organisms/available-markets-platforms'
 import DigitalOptionsLayout from 'features/components/templates/digital-options-layout'
 // import OptionsContent from 'features/components/templates/options-content'
-// import OptionsFaq from 'features/components/templates/options-faq'
+import OptionsFaq from 'features/components/templates/options-faq'
 
 const AsiansDigitalOptionsPage = () => {
     return (
         <DigitalOptionsLayout>
-            Asians Content
             {/* <OptionsContent items={upAndDownContentItems} />
-            <AvailableMarketPlatforms markets={upAndDownMarkets} platforms={upAndDownPlatforms} />
-            <OptionsFaq faqs={upAndDownFAQ} /> */}
+            <AvailableMarketPlatforms markets={upAndDownMarkets} platforms={upAndDownPlatforms} /> */}
+            <OptionsFaq faqs={asiansFAQ} />
         </DigitalOptionsLayout>
     )
 }

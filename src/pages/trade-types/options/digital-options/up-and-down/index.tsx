99185af44a56b86c32778576aa34745bbe5c7171
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-07-17 09:14:17 +0400

    fix: remove up and down contents

--- src/pages/trade-types/options/digital-options/up-and-down/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { WithIntl } from 'components/localization'
-import UpAndDownDigitalOptionsPage from 'features/pages/options/digital/up-down'
-
-const DigitalOptions = () => {
-    return <UpAndDownDigitalOptionsPage />
-}
-
-export default WithIntl()(DigitalOptions)

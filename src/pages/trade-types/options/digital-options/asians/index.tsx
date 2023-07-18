477a261790a9cbd9c059ad178e597dcf2a10b63b
hasan-deriv <abuhasan@deriv.com>
2023-07-18 13:25:53 +0800

    fix: page component rename

--- src/pages/trade-types/options/digital-options/asians/index.tsx
+++ src/pages/trade-types/options/digital-options/asians/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { WithIntl } from 'components/localization'
-import UpAndDownDigitalOptionsPage from 'features/pages/options/digital/asians'
+import AsiansDigitalOptionsPage from 'features/pages/options/digital/asians'
 
 const DigitalOptionsAsians = () => {
-    return <UpAndDownDigitalOptionsPage />
+    return <AsiansDigitalOptionsPage />
 }
 
 export default WithIntl()(DigitalOptionsAsians)

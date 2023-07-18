455e6231f607801892596c58189355190ff3f71b
Nuzhy-Deriv <nuzhy@deriv.com>
2023-07-18 18:35:26 +0800

    fix: phrase correction

--- src/features/pages/options/digital/lookbacks/index.tsx
+++ src/features/pages/options/digital/lookbacks/index.tsx
@@ -6,13 +6,12 @@ import OptionsContent from 'features/components/templates/options-content'
 import OptionsFaq from 'features/components/templates/options-faq'
 import Typography from 'features/components/atoms/typography'
 import { Localize } from 'components/localization'
-import Flex from 'features/components/atoms/flex-box'
 
 const UpAndDownDigitalOptionsPage = () => {
     return (
         <DigitalOptionsLayout>
             <Typography.Paragraph pb="20x">
-                <Localize translate_text="_t_Lookbacks that allow you to earn a payout depending on the optimum high or low achieved by the market during the duration of a contract._t_" />
+                <Localize translate_text="_t_Lookbacks allow you to earn a payout depending on the optimum high or low achieved by the market during the duration of a contract._t_" />
             </Typography.Paragraph>
             <OptionsContent items={lookbacks_content_items} />
             <AvailableMarketPlatforms markets={lookbacksMarkets} platforms={lookbacksPlatforms} />

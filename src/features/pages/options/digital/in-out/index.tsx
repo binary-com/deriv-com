05b1fc046377adc50aa4e8d5269e7962124313ee
hasan-deriv <abuhasan@deriv.com>
2023-07-18 11:16:29 +0800

    feat: created options page

---
+++ src/features/pages/options/digital/in-out/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import { asiansContentItems,  /* asiansFAQ, asiansMarkets, asiansPlatforms */ } from './data'
+// import AvailableMarketPlatforms from 'features/components/organisms/available-markets-platforms'
+import DigitalOptionsLayout from 'features/components/templates/digital-options-layout'
+import OptionsContent from 'features/components/templates/options-content'
+// import OptionsFaq from 'features/components/templates/options-faq'
+
+const InOutDigitalOptionsPage = () => {
+    return (
+        <DigitalOptionsLayout>
+            In Out Content
+            {/* <OptionsContent items={asiansContentItems} /> */}
+            {/* <AvailableMarketPlatforms markets={asiansMarkets} platforms={asiansPlatforms} />
+            <OptionsFaq faqs={asiansFAQ} /> */}
+        </DigitalOptionsLayout>
+    )
+}
+
+export default InOutDigitalOptionsPage

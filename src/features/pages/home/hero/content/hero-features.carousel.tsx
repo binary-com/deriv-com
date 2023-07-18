d9c1923d451829c9a2f4d37b47f751f15ee12435
Eduard Hrachou <eduardhrachou@Eduard-Hrachous-MacBook-Pro-Y24T3WTX5M.local>
2023-07-18 16:49:50 +0300

    remove package and update package.log

--- src/features/pages/home/hero/content/hero-features.carousel.tsx
+++ src/features/pages/home/hero/content/hero-features.carousel.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { hero_typewriter, wrapper, text } from './hero-content.module.scss'
-import { localize } from 'components/localization'
+import { Localize } from 'components/localization'
 import Typography from 'features/components/atoms/typography'
 import { TString } from 'types/generics'
 
@@ -19,7 +19,7 @@ const HeroFeaturesCarousel = () => {
                 <div className={wrapper}>
                     {strings.map((string) => (
                         <p key={string}>
-                            {localize(string)}
+                            <Localize translate_text={string} />
                         </p>
                     ))}
                 </div>

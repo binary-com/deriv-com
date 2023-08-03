0fe96c51886247af4db3e5a4890fdc13c9e223d3
aswathy-deriv <aswathy@deriv.com>
2023-08-03 12:31:52 +0400

    fix: prime page changes in atomic approach

---
+++ src/features/pages/partners/deriv-prime/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DerivPrime = () => {
+    return <></>
+}
+
+export default DerivPrime
--- src/pages/partners/deriv-prime/index.tsx
+++ src/pages/partners/deriv-prime/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { WithIntl } from 'components/localization/withIntl'
 import Layout from 'components/layout/layout'
+import { WithIntl } from 'components/localization'
+import { SEO } from 'components/containers'
+import DerivPrime from 'features/pages/partners/deriv-prime'
+import PaymentAgentAffiliateNav from 'features/components/templates/navigation/payment-agent-nav'
 
-const DerivPrime = () => {
+const DerivPrimePage = () => {
     return (
-        <Layout type="partners" margin_top={10}>
-            <></>
+        <Layout type="noNav">
+            <SEO
+                description="_t_With Deriv Prime's liquidity solutions for online trading, you get unbeatable spreads on the commodities, cryptocurrencies, and forex markets._t_"
+                title="_t_Liquidity solutions provider | Deriv Prime_t_"
+            />
+            <PaymentAgentAffiliateNav />
+            <DerivPrime />
         </Layout>
     )
 }
 
-export default WithIntl()(DerivPrime)
+export default WithIntl()(DerivPrimePage)

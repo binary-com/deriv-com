ece5518ad9969f166ca326d5870a5e11623f57b5
aswathy-deriv <aswathy@deriv.com>
2023-08-03 13:28:04 +0400

    fix: added the new layout

--- src/features/pages/partners/deriv-prime/index.tsx
+++ src/features/pages/partners/deriv-prime/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Layout from 'features/components/templates/layout'
+import { SEO } from 'components/containers'
+import PaymentAgentAffiliateNav from 'features/components/templates/navigation/payment-agent-nav'
+import Footer from 'features/components/templates/footer'
 
 const DerivPrime = () => {
-    return <></>
+    return (
+        <Layout>
+            <SEO
+                description="_t_With Deriv Prime's liquidity solutions for online trading, you get unbeatable spreads on the commodities, cryptocurrencies, and forex markets._t_"
+                title="_t_Liquidity solutions provider | Deriv Prime_t_"
+            />
+            <PaymentAgentAffiliateNav />
+            <Footer />
+        </Layout>
+    )
 }
 
 export default DerivPrime
--- src/pages/partners/deriv-prime/index.tsx
+++ src/pages/partners/deriv-prime/index.tsx
@@ -1,21 +1,9 @@
 import React from 'react'
-import Layout from 'components/layout/layout'
 import { WithIntl } from 'components/localization'
-import { SEO } from 'components/containers'
 import DerivPrime from 'features/pages/partners/deriv-prime'
-import PaymentAgentAffiliateNav from 'features/components/templates/navigation/payment-agent-nav'
 
 const DerivPrimePage = () => {
-    return (
-        <Layout type="noNav">
-            <SEO
-                description="_t_With Deriv Prime's liquidity solutions for online trading, you get unbeatable spreads on the commodities, cryptocurrencies, and forex markets._t_"
-                title="_t_Liquidity solutions provider | Deriv Prime_t_"
-            />
-            <PaymentAgentAffiliateNav />
-            <DerivPrime />
-        </Layout>
-    )
+    return <DerivPrime />
 }
 
 export default WithIntl()(DerivPrimePage)

d4c45baf5b7f1091f4699d37aca1c7d500240785
Jim Daniels Wasswa <jim@deriv.com>
2023-08-03 12:04:08 +0800

    chore: add and integrate function to request for detailed data with user-agent client hints

--- src/common/os-detect.ts
+++ src/common/os-detect.ts
@@ -1,5 +1,30 @@
-export const isMobile = () =>
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+/**
+ * Requests detailed data with User-Agent Client Hints
+ *
+ * @see https://wicg.github.io/ua-client-hints/
+ * @returns {Promise<UADataValues | null>} Promise with detailed data or null if not supported
+ * **/
+const getUserAgentData = async () => {
+    if ('userAgentData' in navigator) {
+        const user_agent_data = await navigator.userAgentData.getHighEntropyValues([
+            'architecture',
+            'bitness',
+            'model',
+            'platform',
+            'platformVersion',
+            'fullVersionList',
+            'wow64',
+        ])
+        return user_agent_data
+    }
+    return null
+}
+export const isMobile = async () => {
+    const user_agent_data = await getUserAgentData()
+    return user_agent_data
+        ? user_agent_data.mobile
+        : /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+}
 
 export const mobileOSDetect = () => {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera

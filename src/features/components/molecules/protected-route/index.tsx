44cf17bf49bd3ca3ed31a061d07aec4fff24d1fb
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-08-03 15:23:29 +0400

    fix: 404 duplicate code and issue in

---
+++ src/features/components/molecules/protected-route/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PageNotFound from 'features/pages/404'
+import InitialLoader from 'components/elements/dot-loader'
+
+interface ProtectedRouteProps {
+    condition: boolean
+    params?: any
+    component: React.ComponentType<any>
+    is_loading: boolean
+}
+
+const ProtectedRoute = ({
+    condition,
+    params,
+    component: Component,
+    is_loading = false,
+}: ProtectedRouteProps) => {
+    return is_loading ? <InitialLoader /> : condition ? <Component {...params} /> : <PageNotFound />
+}
+
+export default ProtectedRoute
--- src/pages/trade-types/options/digital-options/asians/index.tsx
+++ src/pages/trade-types/options/digital-options/asians/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import ProtectedRoute from 'features/components/molecules/protected-route'
 import { WithIntl } from 'components/localization'
 import AsiansDigitalOptionsPage from 'features/pages/options/digital/asians'
+import useRegion from 'components/hooks/use-region'
 
 const DigitalOptionsAsians = () => {
-    return <AsiansDigitalOptionsPage />
+    const { is_row, is_region_loading } = useRegion()
+    return (
+        <ProtectedRoute
+            is_loading={is_region_loading}
+            component={AsiansDigitalOptionsPage}
+            condition={is_row}
+        />
+    )
 }
 
 export default WithIntl()(DigitalOptionsAsians)

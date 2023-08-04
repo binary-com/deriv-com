74ab8bdcd1a964e0e1aa64f07e912bddbcc46dc9
mitra-deriv <mitra@regentmarkets.com>
2023-08-04 17:32:39 +0800

    feat: :sparkles: add canonical element

--- src/pages/trade-types/options/digital-options/up-and-down/index.tsx
+++ src/pages/trade-types/options/digital-options/up-and-down/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import { WithIntl } from 'components/localization'
 import UpAndDownDigitalOptionsPage from 'features/pages/options/digital/up-down'
 
 const DigitalOptions = () => {
-    return <UpAndDownDigitalOptionsPage />
+    const canonicalUrl = '/trade-types/options/digital-options/up-and-down'
+
+    ;<>
+        <Helmet>
+            <link rel="canonical" href={canonicalUrl} />
+        </Helmet>
+        <UpAndDownDigitalOptionsPage />
+    </>
 }
 
 export default WithIntl()(DigitalOptions)

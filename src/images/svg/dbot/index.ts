abcab3f8f1365e1fe3ae40f26cfb506c26630736
Nuzhy-Deriv <nuzhy@deriv.com>
2023-08-07 15:21:32 +0800

    fix: update dbot-image

--- src/images/svg/dbot/index.ts
+++ src/images/svg/dbot/index.ts
@@ -1,23 +1,10 @@
-import DbotIcon from './dbot-icon.svg'
-import BotIcon from './bot-icon.svg'
-import DbotBgMobile from './dbot-bg-mobile.svg'
-import DbotBg from './dbot-bg.svg'
-import BotBannerLogo from './deriv-bot-banner-logo.svg'
-import DtIcon from './dt-icon.svg'
-import DxIcon from './dx-icon.svg'
-import EzIcon from './ez-icon.svg'
-import GoIcon from './go-icon.svg'
-import Mt5Icon from './mt5.svg'
-
-export {
-    DbotIcon,
-    BotIcon,
-    DbotBgMobile,
-    DbotBg,
-    BotBannerLogo,
-    DtIcon,
-    DxIcon,
-    EzIcon,
-    GoIcon,
-    Mt5Icon,
-}
+export { default as DbotIcon } from './dbot-icon.svg'
+export { default as BotIcon } from './bot-icon.svg'
+export { default as DbotBgMobile } from './dbot-bg-mobile.svg'
+export { default as DbotBg } from './dbot-bg.svg'
+export { default as BotBannerLogo } from './deriv-bot-banner-logo.svg'
+export { default as DtIcon } from './dt-icon.svg'
+export { default as DxIcon } from './dx-icon.svg'
+export { default as EzIcon } from './ez-icon.svg'
+export { default as GoIcon } from './go-icon.svg'
+export { default as Mt5Icon } from './mt5.svg'

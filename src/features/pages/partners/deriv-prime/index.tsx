1c4655d6e892d8ca5e41376c9666f2c63dfe88fa
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-08-14 12:59:32 +0200

    fix: why deriv prime section

--- src/features/pages/partners/deriv-prime/index.tsx
+++ src/features/pages/partners/deriv-prime/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import WhyDerivPrime from './why-deriv-prime'
 import Layout from 'features/components/templates/layout'
 import { SEO } from 'components/containers'
 import PaymentAgentAffiliateNav from 'features/components/templates/navigation/payment-agent-nav'
@@ -12,6 +13,7 @@ const DerivPrime = () => {
                 title="_t_Liquidity solutions provider | Deriv Prime_t_"
             />
             <PaymentAgentAffiliateNav />
+            <WhyDerivPrime />
             <Footer />
         </Layout>
     )

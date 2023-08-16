2ea668f402d1df6df8fc03cf77ed0210423fec76
hasan-deriv <abuhasan@deriv.com>
2023-08-16 11:16:20 +0800

    feat: added container and typhography

--- src/features/pages/partners/deriv-prime/commercial-plan/index.tsx
+++ src/features/pages/partners/deriv-prime/commercial-plan/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import Container from 'features/components/atoms/container'
+import Typography from 'features/components/atoms/typography'
+import { Localize } from 'components/localization'
 
 const CommercialPlan = () => {
-    return <div>CommercialPlan</div>
+    return (
+        <Container.Fixed as="section" padding_block="40x" bgcolor="secondary">
+            <Container.Fluid padding_block="20x">
+                <Typography.Heading as="h2" align="center">
+                    <Localize translate_text="_t_Scalable commercial plan_t_" />
+                </Typography.Heading>
+                <Typography.Paragraph align="center" size="large" mt="8x">
+                    <Localize translate_text="_t_We’ll help you maximise your earnings with a plan that suits your business._t_" />
+                </Typography.Paragraph>
+            </Container.Fluid>
+        </Container.Fixed>
+    )
 }
 
 export default CommercialPlan

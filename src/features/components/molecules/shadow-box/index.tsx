1c5e76a12174ce95e53dd48d407d59442c806fd1
Fasih Ali <fasihali@Fasih-Alis-MacBook-Pro-C02DJ87UMD6M.local>
2023-08-16 14:19:48 +0800

    chore: updated boxes

--- src/features/components/molecules/shadow-box/index.tsx
+++ src/features/components/molecules/shadow-box/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { box_shadow, styled_heading } from './style.module.scss'
 import Flex from 'features/components/atoms/flex-box'
 import Icon from 'features/components/atoms/icon'
 import Typography from 'features/components/atoms/typography'
 import { TString } from 'types/generics'
 import { Localize, localize } from 'components/localization'
 import { FlexBoxProps } from 'features/components/atoms/flex-box/box'
+import dclsx from 'features/utils/dclsx'
 
 export type BoxData = {
     heading: TString
@@ -16,9 +18,17 @@ type ShadowBoxProps = BoxData & FlexBoxProps<'div'>
 
 const ShadowBox = ({ icon_URL, heading, icon_alt, ...rest }: ShadowBoxProps) => {
     return (
-        <Flex.Box padding="4x" className="box_shadow" radius="2x" direction="column" {...rest}>
+        <Flex.Box
+            grow="1"
+            radius="8x"
+            padding="12x"
+            mb="8x"
+            className={dclsx(box_shadow)}
+            direction="col"
+            {...rest}
+        >
             <Icon size="large" src={icon_URL} alt={localize(icon_alt)} />
-            <Typography.Heading>
+            <Typography.Heading size="xs" className={dclsx(styled_heading)} mt="12x">
                 <Localize translate_text={heading} />
             </Typography.Heading>
         </Flex.Box>
--- src/features/pages/partners/deriv-prime/potential/index.tsx
+++ src/features/pages/partners/deriv-prime/potential/index.tsx
@@ -3,6 +3,9 @@ import { boxData } from './data'
 import Flex from 'features/components/atoms/flex-box'
 import Container from 'features/components/atoms/container'
 import ShadowBox from 'features/components/molecules/shadow-box'
+import Typography from 'features/components/atoms/typography'
+import { Localize } from 'components/localization'
+import dclsx from 'features/utils/dclsx'
 
 const Potential = () => (
     <Container.Fixed as="section" pt="40x" md={{ pt: '40x' }}>
@@ -29,29 +32,25 @@ const Potential = () => (
         <br />
         <br />
         <br />
-        <Flex.Box container="fluid">
-            <Flex.Item basis="1-2">
+        <Flex.Box container="fluid" wrap="wrap" gap="8x">
+            <Flex.Box basis="2-5" wrap="wrap" gap="8x" grow="1">
                 {boxData.map((item) => {
                     return (
                         <ShadowBox
                             key={item.icon_URL}
-                            basis="1-2"
+                            basis="2-5"
                             icon_URL={item.icon_URL}
                             heading={item.heading}
                             icon_alt={item.icon_alt}
                         />
                     )
                 })}
-            </Flex.Item>
-            <Flex.Item basis="1-2">
-                234567jhgf234567jhgf234567jhgf234567jhgf234567jhgf23456 7jhgf23456 7jhgf234567jh
-                gf234567jhgf234567jhgf23456 7jhgf234567jhgf2 34567 jhgf234567jhgf23 567jhgf23456
-                7jhgf234567jhgf234567 jhgf234567j hgf234567jh gf23456 7jhgf 23456 hgf234 56
-                7jhgf234567j hgf234567j hgf234567jhgf234567j hgf234567jhgf234567jhgf234567jhgf
-                234567jhgf234567jhgf234567jhgf234567jhgf2 34567jhgf234567j hgf234567jhgf234567jhgf
-                234567jhgf234567jhgf234567jhgf234567jhgf23456 7jhgf234567jh
-                gf234567jhgf234567jhgf234567jhgf
-                234567jhgf234567jhgf234567jhgf234567jhgf234567jhgf23456 7jhgf
+            </Flex.Box>
+            <Flex.Item basis="2-5" grow="1">
+                {/* className={dclsx(styled_heading)} */}
+                <Typography.Heading>
+                    <Localize translate_text="_t_Your potential. Our technology._t_" />
+                </Typography.Heading>
             </Flex.Item>
         </Flex.Box>
     </Container.Fixed>

a621c56b79c3c907008f1b0cc1cd07e639ef4409
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-08-17 12:04:22 +0200

    fix: test vercel

--- src/features/pages/partners/deriv-prime/index.tsx
+++ src/features/pages/partners/deriv-prime/index.tsx
@@ -8,7 +8,7 @@ const DerivPrime = () => {
     return (
         <Layout>
             <SEO
-                description="_t_With Deriv Prime's liquidity solutions for online trading, you get unbeatable spreads on the commodities, cryptocurrencies, and forex markets._t_"
+                description="_t_W1ith Deriv Prime's liquidity solutions for online trading, you get unbeatable spreads on the commodities, cryptocurrencies, and forex markets._t_"
                 title="_t_Liquidity solutions provider | Deriv Prime_t_"
             />
             <PaymentAgentAffiliateNav />

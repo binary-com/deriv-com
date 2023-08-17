5bfd71a64d4d84111186ee5c5d55841a6542fc96
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-08-17 12:32:57 +0200

    fix: meet us there section

---
+++ src/features/pages/partners/deriv-prime/meet-us-there/index.tsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import * as styles from './meet-us-there.module.scss'
+import { expo_cards } from './data'
+import Typography from 'features/components/atoms/typography'
+import FlexBox from 'features/components/atoms/flex-box'
+import { Localize , WithIntl } from 'components/localization'
+import Link from 'features/components/atoms/link'
+
+const MeetUsThere = () => {
+    return (
+        <FlexBox.Box
+            container={'fluid'}
+            direction={'col'}
+            padding_inline={'4x'}
+            pt={'20x'}
+            md={{ padding_inline: '0x', padding_block: '40x' }}
+        >
+            <FlexBox.Box
+                direction={'col'}
+                margin_block={'40x'}
+                md={{ justify: 'center', margin_block: '40x' }}
+            >
+                <Typography.Heading align={'center'}>
+                    <Localize translate_text={'_t_Meet us there_t_'} />
+                </Typography.Heading>
+                <FlexBox.Box
+                    direction={'col'}
+                    align={'center'}
+                    wrap="nowrap"
+                    basis="1-3"
+                    md={{ direction: 'row', gap: '12x', justify: 'center', mt: '20x' }}
+                >
+                    {expo_cards.map((card) => {
+                        return (
+                            <>
+                                <Link
+                                    style={{ textDecoration: 'none' }}
+                                    url={{
+                                        type: 'non-company',
+                                        href: `${card.link}`,
+                                        target: '_blank',
+                                    }}
+                                >
+                                    <FlexBox.Box
+                                        key={card.title}
+                                        className={styles.styled_card}
+                                        direction={'col'}
+                                        justify={'start'}
+                                        align={'start'}
+                                        radius={'8x'}
+                                        mt={'12x'}
+                                        md={{ mt: '0x' }}
+                                    >
+                                        {card.image}
+                                        <FlexBox.Box
+                                            padding="8x"
+                                            direction={'col'}
+                                            md={{ padding: '12x' }}
+                                        >
+                                            <Typography.Paragraph
+                                                size={'xlarge'}
+                                                weight={'bold'}
+                                                mb={'4x'}
+                                            >
+                                                <Localize translate_text={card.title} />
+                                            </Typography.Paragraph>
+                                            <Typography.Paragraph size="small">
+                                                <Localize translate_text={card.date} />
+                                            </Typography.Paragraph>
+                                        </FlexBox.Box>
+                                    </FlexBox.Box>
+                                </Link>
+                            </>
+                        )
+                    })}
+                </FlexBox.Box>
+            </FlexBox.Box>
+        </FlexBox.Box>
+    )
+}
+
+export default WithIntl()(MeetUsThere)

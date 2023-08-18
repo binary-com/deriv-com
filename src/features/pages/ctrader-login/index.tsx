ad9b0fc58a1f35524da267b71baeaf6d1f7b3bb9
mohammad-hashemi-deriv <mohammad.hashemi@re-work.dev>
2023-08-19 00:02:58 +0330

    feat: add return button to error login page

--- src/features/pages/ctrader-login/index.tsx
+++ src/features/pages/ctrader-login/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { navigate } from 'gatsby'
 import { useDerivAuth } from 'features/hooks/use-deriv-auth'
 import Loading from 'features/components/atoms/loading'
 import CtraderWrapper from 'features/components/templates/ctrader/ctrader-wrapper'
 import Typography from 'features/components/atoms/typography'
 import Layout from 'features/components/templates/layout'
 import { isBrowser } from 'common/utility'
+import Button from 'features/components/atoms/button'
+import { Localize } from 'components/localization'
+import Flex from 'features/components/atoms/flex-box'
 
 const CtraderLogin = () => {
     const url_params = new URLSearchParams((isBrowser() && window.location.search) || '')
@@ -15,9 +19,14 @@ const CtraderLogin = () => {
         <Layout hide_layout_overlay>
             <CtraderWrapper>
                 {account_error || create_account_error ? (
-                    <Typography.Heading size="small" pt="40x" align="center">
-                        {account_error || create_account_error.message}
-                    </Typography.Heading>
+                    <Flex.Box direction="col" gap="10x">
+                        <Typography.Heading size="small" pt="40x" align="center">
+                            {account_error || create_account_error.message}
+                        </Typography.Heading>
+                        <Button.Primary onClick={() => navigate(-1)}>
+                            <Localize translate_text="_t_Return_t_" />
+                        </Button.Primary>
+                    </Flex.Box>
                 ) : (
                     <Loading />
                 )}

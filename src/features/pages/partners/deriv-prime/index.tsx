0dbb48facdd4562ae6bb0621b962a1a04dd7aa78
mitra-deriv <mitra@regentmarkets.com>
2023-08-18 21:43:58 +0800

    fix: :bug: add margin top

--- src/features/pages/partners/deriv-prime/index.tsx
+++ src/features/pages/partners/deriv-prime/index.tsx
@@ -4,6 +4,7 @@ import Layout from 'features/components/templates/layout'
 import { SEO } from 'components/containers'
 import PaymentAgentAffiliateNav from 'features/components/templates/navigation/payment-agent-nav'
 import Footer from 'features/components/templates/footer'
+import Flex from 'features/components/atoms/flex-box'
 
 const DerivPrime = () => {
     return (
@@ -13,7 +14,10 @@ const DerivPrime = () => {
                 title="_t_Liquidity solutions provider | Deriv Prime_t_"
             />
             <PaymentAgentAffiliateNav />
-            <PrimeNumbers />
+            */remove the flex box before merging its only for test/*
+            <Flex.Box pt="40x" mt="40x">
+                <PrimeNumbers />
+            </Flex.Box>
             <Footer />
         </Layout>
     )

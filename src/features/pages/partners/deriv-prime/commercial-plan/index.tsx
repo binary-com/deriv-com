5bfe5e66029008714697aeaa17065ae5a73c93cb
hasan-deriv <abuhasan@deriv.com>
2023-08-18 12:36:06 +0800

    feat: added data

--- src/features/pages/partners/deriv-prime/commercial-plan/index.tsx
+++ src/features/pages/partners/deriv-prime/commercial-plan/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { commercial_table } from './styles.module.scss'
 import Container from 'features/components/atoms/container'
 import Typography from 'features/components/atoms/typography'
 import { Localize } from 'components/localization'
@@ -15,12 +16,16 @@ const tableData = [
     },
     {
         '_t_Monthly volume requirement_t_': <Localize translate_text="_t_> 5 - 10 billion_t_" />,
+        '_t_Fee per million_t_': '$ 6.00',
+    },
+    {
+        '_t_Monthly volume requirement_t_': <Localize translate_text="_t_> 10 billion_t_" />,
         '_t_Fee per million_t_': '$ 5.00',
     },
     {
         '_t_Monthly volume requirement_t_': (
             <Localize
-                translate_text="_t_0 - 15 billion <0>(Crypto)</0>_t_"
+                translate_text="_t_0 - 15 million <0>(Crypto)</0>_t_"
                 components={[<span key={0} />]}
             />
         ),
@@ -29,7 +34,7 @@ const tableData = [
     {
         '_t_Monthly volume requirement_t_': (
             <Localize
-                translate_text="_t_0 - 15 billion <0>(Crypto)</0>_t_"
+                translate_text="_t_> 15 million <0>(Crypto)</0>_t_"
                 components={[<span key={0} />]}
             />
         ),
@@ -47,7 +52,7 @@ const CommercialPlan = () => {
                 <Typography.Paragraph align="center" size="large" mt="8x" mb="20x">
                     <Localize translate_text="_t_We’ll help you maximise your earnings with a plan that suits your business._t_" />
                 </Typography.Paragraph>
-                <Table.Shadow data={tableData} striped />
+                <Table.Shadow data={tableData} striped className={commercial_table} />
             </Container.Fluid>
         </Container.Fixed>
     )

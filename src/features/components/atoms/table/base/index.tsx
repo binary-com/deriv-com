d573f4f7d0601773e0f96f3f2440f2af3e656d0a
hasan-deriv <abuhasan@deriv.com>
2023-08-18 16:40:03 +0800

    fix: modified table base component

--- src/features/components/atoms/table/base/index.tsx
+++ src/features/components/atoms/table/base/index.tsx
@@ -1,65 +1,32 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
+import { TableColumnType } from '../types'
 import Box, { BoxProps } from '../../box'
-import { Localize } from 'components/localization'
-import { TString } from 'types/generics'
+import TableHeader from './table-header'
+import TableRow from './table-row'
 import dclsx from 'features/utils/dclsx'
 import { ClassProps } from 'features/types'
 
-type Data = Record<string, string | ReactNode>
-
-export interface TableProps extends BoxProps, ClassProps {
+export interface TableProps<T, K extends keyof T> extends BoxProps, ClassProps {
     striped?: boolean
-    data: Data[]
+    data: T[]
+    columns: Array<TableColumnType<T, K>>
 }
 
-const BaseTable = ({ data, striped, className, ...rest }: TableProps) => {
+function BaseTable<T, K extends keyof T>({
+    data,
+    columns,
+    striped,
+    className,
+    ...rest
+}: TableProps<T, K>) {
     return (
         <Box
             className={dclsx('table-responsive', { ['table-striped']: striped }, className)}
             {...rest}
         >
             <table className={dclsx('table')}>
-                <thead>
-                    <tr>
-                        {Object.keys(data[0]).map((key) => (
-                            <th
-                                key={key}
-                                className={dclsx(
-                                    'typography-color-black',
-                                    'typography-align-center',
-                                    'typography-font-UBUNTU',
-                                    'typography-weight-bold',
-                                    'text-small',
-                                    'padding-block-6x',
-                                )}
-                            >
-                                <Localize translate_text={key as TString} />
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((row, i) => (
-                        <tr key={i}>
-                            {Object.keys(row).map((key) => {
-                                return (
-                                    <td
-                                        key={key}
-                                        className={dclsx(
-                                            'typography-color-black',
-                                            'typography-align-center',
-                                            'typography-font-UBUNTU',
-                                            'text-small',
-                                            'padding-block-6x',
-                                        )}
-                                    >
-                                        {row[key]}
-                                    </td>
-                                )
-                            })}
-                        </tr>
-                    ))}
-                </tbody>
+                <TableHeader columns={columns} />
+                <TableRow data={data} columns={columns} />
             </table>
         </Box>
     )

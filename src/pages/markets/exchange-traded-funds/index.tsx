c57492d7af7b09b98a21f68cfbb5c608c4aa9bb2
mitra-deriv <mitra@regentmarkets.com>
2023-08-21 17:55:11 +0800

    fix: :bug: fix banner position

--- src/pages/markets/exchange-traded-funds/index.tsx
+++ src/pages/markets/exchange-traded-funds/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Layout from 'components/layout/layout'
 import { WithIntl } from 'components/localization'
 import { SEO } from 'components/containers'
 import MarketNav from 'features/components/templates/navigation/market-nav'
@@ -7,12 +6,13 @@ import ETFMarket from 'features/pages/markets/etf'
 import SignupPublic from 'features/components/templates/signup/with-banner'
 import useRegion from 'components/hooks/use-region'
 import PageNotFound from 'features/pages/404'
+import Layout from 'features/components/templates/layout'
 
 const StocksAndIndicesPage = () => {
     const { is_row } = useRegion()
 
     return is_row ? (
-        <Layout type="noNav">
+        <Layout>
             <SEO
                 description="_t_Trade ETFs on Deriv and diversify your portfolio with assets that track bonds, commodities, and indices, without needing to own the underlying assets._t_"
                 title="_t_Exchange-traded funds | ETF trading | Deriv_t_"

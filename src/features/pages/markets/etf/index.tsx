2f844ed4465359564606213b198125fc36838f75
hasan-deriv <abuhasan@deriv.com>
2023-08-22 16:30:59 +0800

    fix: moved row logic to features

--- src/features/pages/markets/etf/index.tsx
+++ src/features/pages/markets/etf/index.tsx
@@ -12,34 +12,42 @@ import { SEO } from 'components/containers'
 import Layout from 'features/components/templates/layout'
 import MarketNav from 'features/components/templates/navigation/market-nav'
 import Footer from 'features/components/templates/footer/'
+import useRegion from 'components/hooks/use-region'
+import PageNotFound from 'features/pages/404'
 
 const ETFMarket = () => {
-    return (
-        <Layout>
-            <SEO
-                description="_t_Trade ETFs on Deriv and diversify your portfolio with assets that track bonds, commodities, and indices, without needing to own the underlying assets._t_"
-                title="_t_Exchange-traded funds | ETF trading | Deriv_t_"
-            />
-            <MarketNav />
-            <Banner />
-            <TradeTypes />
-            <Flex.Box
-                justify="center"
-                align="center"
-                direction="col"
-                as="section"
-                padding_inline="8x"
-            >
-                <InstrumentsTable />
-                <InstrumentsContent />
-            </Flex.Box>
-            <WhyTrade />
-            <Steps />
-            <OtherMarketsSlider current_market="etfs" />
-            <SignupPublic />
-            <Footer />
-        </Layout>
-    )
+    const { is_row } = useRegion()
+
+    if (is_row) {
+        return (
+            <Layout>
+                <SEO
+                    description="_t_Trade ETFs on Deriv and diversify your portfolio with assets that track bonds, commodities, and indices, without needing to own the underlying assets._t_"
+                    title="_t_Exchange-traded funds | ETF trading | Deriv_t_"
+                />
+                <MarketNav />
+                <Banner />
+                <TradeTypes />
+                <Flex.Box
+                    justify="center"
+                    align="center"
+                    direction="col"
+                    as="section"
+                    padding_inline="8x"
+                >
+                    <InstrumentsTable />
+                    <InstrumentsContent />
+                </Flex.Box>
+                <WhyTrade />
+                <Steps />
+                <OtherMarketsSlider current_market="etfs" />
+                <SignupPublic />
+                <Footer />
+            </Layout>
+        )
+    }
+
+    return <PageNotFound />
 }
 
 export default ETFMarket
--- src/pages/markets/exchange-traded-funds/index.tsx
+++ src/pages/markets/exchange-traded-funds/index.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import { WithIntl } from 'components/localization'
 import ETFMarket from 'features/pages/markets/etf'
-import useRegion from 'components/hooks/use-region'
-import PageNotFound from 'features/pages/404'
 
 const ETFMarketPage = () => {
-    const { is_row } = useRegion()
-    if (is_row) {
-        return <ETFMarket />
-    }
-
-    return <PageNotFound />
+    return <ETFMarket />
 }
 
 export default WithIntl()(ETFMarketPage)

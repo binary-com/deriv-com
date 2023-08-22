3acd2ee08cf21af16f240c5748ad7c7141a78675
habib-binary <habib@regentmarkets.com>
2023-08-23 01:29:42 +0600

    chore: add env variable support

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,6 @@ require('dotenv').config()
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.GATSBY_BASE_URL
+    baseUrl: 'https://staging.deriv.com',
   },
 })
--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -1,6 +1,7 @@
 describe('Signup page is functional for staging', () => {
   it('allows user to signup', () => {
-    cy.visit(`https://staging.deriv.com/signup`)
+    let url = Cypress.env('base_url') || 'https://staging.deriv.com';
+    cy.visit(`${url}/signup`)
     cy.get('input#email_address').type('test@example.com')
     cy.get('input#terms_and_conditions').click()
     cy.contains('button', 'Create demo account').click()

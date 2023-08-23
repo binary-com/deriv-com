75c9187c8d55b20bfbf0f9a37716dbd419f3a2f9
habib-binary <habib@regentmarkets.com>
2023-08-23 11:20:47 +0600

    chore: update email from env

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'https://staging.deriv.com',
   },
+  env: {
+    email: 'test@example.com',
+  },
 })
--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -1,10 +1,21 @@
 describe('Signup page is functional for staging', () => {
-  it('allows user to signup', () => {
-    let url = Cypress.env('base_url') || 'https://staging.deriv.com';
+  it('allows user to signup in deriv.com', () => {
+    const url = Cypress.env('base_url') || 'https://staging.deriv.com';
+    const email = Cypress.env('email')
     cy.visit(`${url}/signup`)
-    cy.get('input#email_address').type('test@example.com')
+    cy.get('input#email_address').type(email)
     cy.get('input#terms_and_conditions').click()
     cy.contains('button', 'Create demo account').click()
-    cy.url().should('include', '/signup-success?email=test@example.com') 
+    cy.url().should('include', `/signup-success?email=${email}`) 
+  })
+
+  it('allows user to signup in eu.deriv.com', () => {
+    const url = Cypress.env('base_url_eu') || 'https://staging-eu.deriv.com';
+    const email = Cypress.env('email')
+    cy.visit(`${url}/signup`)
+    cy.get('input#email_address').type(email)
+    cy.get('input#terms_and_conditions').click()
+    cy.contains('button', 'Create demo account').click()
+    cy.url().should('include', `/signup-success?email=${email}`) 
   })
 })

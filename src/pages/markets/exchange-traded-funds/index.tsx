3464189d42a17a9948d12c41a6dcde146c74c52d
hasan-deriv <abuhasan@deriv.com>
2023-08-23 09:35:24 +0800

    fix: added etf to protected route

--- src/pages/markets/exchange-traded-funds/index.tsx
+++ src/pages/markets/exchange-traded-funds/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { WithIntl } from 'components/localization'
 import ETFMarket from 'features/pages/markets/etf'
+import ProtectedRoute from 'features/components/molecules/protected-route'
+import useRegion from 'components/hooks/use-region'
 
 const ETFMarketPage = () => {
-    return <ETFMarket />
+    const { is_row, is_region_loading } = useRegion()
+    return (
+        <ProtectedRoute
+            is_page_visible={is_row}
+            component={<ETFMarket />}
+            is_loading={is_region_loading}
+        />
+    )
 }
 
 export default WithIntl()(ETFMarketPage)

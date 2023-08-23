023d2eb3b7bb4900f4b72a1a11121eb6cc1a70b0
habib-binary <habib@regentmarkets.com>
2023-08-23 11:10:19 +0600

    chore: update cypress test for footer

--- cypress/e2e/footer.cy.ts
+++ cypress/e2e/footer.cy.ts
@@ -1,11 +1,11 @@
 describe('Footer', () => {
-    it('has expected content on deriv.com', () => {
+    it('footer exists for deriv.com', () => {
         let url = Cypress.env('base_url') || 'https://staging.deriv.com'
         cy.visit(url)
         cy.get('footer').should('exist')
     })
 
-    it('has expected content on eu.deriv.com', () => {
+    it('footer exists for eu.deriv.com', () => {
         let url = Cypress.env('base_url_eu') || 'https://staging-eu.deriv.com'
         cy.visit(url)
         cy.get('footer').should('exist')

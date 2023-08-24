d25c2356e48c801972ff2623e3179c84bc4c2aee
Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
2023-08-24 09:56:08 +0200

    feat: remove belarus

--- src/pages/contact_us/_offices.tsx
+++ src/pages/contact_us/_offices.tsx
@@ -14,7 +14,6 @@ import Malaysia from 'images/svg/contact/malaysia.svg'
 import Cayman from 'images/svg/contact/cayman.svg'
 import Cyprus from 'images/svg/contact/cyprus.svg'
 import Rwanda from 'images/svg/contact/rwanda.svg'
-import Belarus from 'images/svg/contact/belarus.svg'
 import Singapore from 'images/svg/contact/singapore.svg'
 import Jordan from 'images/svg/contact/jordan.svg'
 import Vanuatu from 'images/svg/contact/vanuatu.svg'
@@ -74,9 +73,6 @@ const query = graphql`
             ...fadeIn
         }
         map_rwanda: file(relativePath: { eq: "maps/map-rwanda.png" }) {
-            ...fadeIn
-        }
-        map_belarus_contact: file(relativePath: { eq: "maps/map-belarus-contact.png" }) {
             ...fadeIn
         }
         map_uk: file(relativePath: { eq: "maps/map-uk.png" }) {
@@ -792,55 +788,6 @@ export const Offices = () => {
                             </BorderBox>
                         </Flex>
                     </GridLayout>
-                    <Flex mt="30px" fd="column" id="belarus">
-                        <ContinentHeader>
-                            <Localize translate_text="_t_Eastern Europe_t_" />
-                        </ContinentHeader>
-                        <OfficeHeader>
-                            <div>
-                                <img src={Belarus} alt="Belarus - National Library" />
-                            </div>
-                            <Header as="h4" type="sub-section-title" mt="0.8rem" mb="1.6rem">
-                                <Localize translate_text="_t_Belarus_t_" />
-                            </Header>
-                        </OfficeHeader>
-                    </Flex>
-                    <Flex mt="30px" fd="column" max_width="48.6rem">
-                        <BorderBox>
-                            <MapContainer>
-                                <LocalizedLink
-                                    to="https://goo.gl/maps/7wUCJ6zy7P8CsmkD7"
-                                    external
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    <QueryImage
-                                        alt="Deriv's office location map in Belarus"
-                                        data={data['map_belarus_contact']}
-                                        height="100%"
-                                    />
-                                </LocalizedLink>
-                            </MapContainer>
-                            <LocalizedLinkText
-                                to="https://goo.gl/maps/7wUCJ6zy7P8CsmkD7"
-                                external
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                mt="0.8rem"
-                                ml="1.6rem"
-                            >
-                                <Desktop>
-                                    <p>Level 2, 25/1-3 Vera Khoruzhey</p>
-                                    <p>Street, Minsk 220123</p>
-                                </Desktop>
-                                <Mobile>
-                                    <StyledMobileAddress>
-                                        Level 2, 25/1-3 Vera Khoruzhey Street, Minsk 220123
-                                    </StyledMobileAddress>
-                                </Mobile>
-                            </LocalizedLinkText>
-                        </BorderBox>
-                    </Flex>
                     <Flex mt="30px" fd="column" id="rwanda">
                         <ContinentHeader>
                             <Localize translate_text="_t_Africa_t_" />

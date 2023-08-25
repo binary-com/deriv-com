eac3fcdec1c658af131520e992f98a3923d1a5f0
Hasan Mobarak <126637868+hasan-deriv@users.noreply.github.com>
2023-08-25 15:46:51 +0800

    Hasan/Translation 00 prime commercial table content fix (#5471)
    * fix: commercial plan table heading content typo
    
    * fix: accessor typo

--- src/features/pages/partners/deriv-prime/commercial-plan/data.tsx
+++ src/features/pages/partners/deriv-prime/commercial-plan/data.tsx
@@ -5,25 +5,25 @@ import { Localize } from 'components/localization'
 
 type Data = {
     monthly_requirement: ReactNode
-    free_per_million: ReactNode
+    fee_per_million: ReactNode
 }
 
 export const data: Data[] = [
     {
         monthly_requirement: <Localize translate_text="_t_0 - 3 billion_t_" />,
-        free_per_million: <Localize translate_text="_t_$ 8.00_t_" />,
+        fee_per_million: <Localize translate_text="_t_$ 8.00_t_" />,
     },
     {
         monthly_requirement: <Localize translate_text="_t_> 3 - 5 billion_t_" />,
-        free_per_million: <Localize translate_text="_t_$ 7.00_t_" />,
+        fee_per_million: <Localize translate_text="_t_$ 7.00_t_" />,
     },
     {
         monthly_requirement: <Localize translate_text="_t_> 5 - 10 billion_t_" />,
-        free_per_million: <Localize translate_text="_t_$ 6.00_t_" />,
+        fee_per_million: <Localize translate_text="_t_$ 6.00_t_" />,
     },
     {
         monthly_requirement: <Localize translate_text="_t_> 10 billion_t_" />,
-        free_per_million: <Localize translate_text="_t_$ 5.00_t_" />,
+        fee_per_million: <Localize translate_text="_t_$ 5.00_t_" />,
     },
     {
         monthly_requirement: (
@@ -32,7 +32,7 @@ export const data: Data[] = [
                 components={[<span key={0} className={dclsx('typography-color-light')} />]}
             />
         ),
-        free_per_million: <Localize translate_text="_t_5.5 bps / 0.055%_t_" />,
+        fee_per_million: <Localize translate_text="_t_5.5 bps / 0.055%_t_" />,
     },
     {
         monthly_requirement: (
@@ -41,7 +41,7 @@ export const data: Data[] = [
                 components={[<span key={0} className={dclsx('typography-color-light')} />]}
             />
         ),
-        free_per_million: <Localize translate_text="_t_5 bps / 0.05%_t_" />,
+        fee_per_million: <Localize translate_text="_t_5 bps / 0.05%_t_" />,
     },
 ]
 
@@ -51,7 +51,7 @@ export const column_data: TableColumnType<Data, keyof Data>[] = [
         header: <Localize translate_text="_t_Monthly volume requirement_t_" />,
     },
     {
-        accessor: 'free_per_million',
-        header: <Localize translate_text="_t_Free Per Million_t_" />,
+        accessor: 'fee_per_million',
+        header: <Localize translate_text="_t_Fee per million_t_" />,
     },
 ]

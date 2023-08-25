8b1b68362c984038df66d39144c03a50108357a8
Nuzhy-Deriv <nuzhy@deriv.com>
2023-08-25 11:02:12 +0800

    fix: reduce spacing of note

--- src/pages/payment-methods/_expanded-list.tsx
+++ src/pages/payment-methods/_expanded-list.tsx
@@ -80,7 +80,7 @@ const Description = styled.div<ExpandListType>`
         props.is_expanded &&
         css`
             max-height: 40rem;
-            padding: 24px 14px;
+            padding: 8px 14px 24px 14px;
             border-bottom: 1px solid var(--color-grey-8);
         `}
 `

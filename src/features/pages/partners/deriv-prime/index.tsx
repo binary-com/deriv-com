4abf9b856507378d49c50264944d7c8ef5111a1a
konstantinosG-deriv <104083272+konstantinosG-deriv@users.noreply.github.com>
2023-08-28 15:40:13 +0200

    Konstantinos / FAQ SEO (#5481)
    * feat: faq seo
    
    * fix: add table for seo too
    
    ---------
    
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>

--- src/features/pages/partners/deriv-prime/index.tsx
+++ src/features/pages/partners/deriv-prime/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
+import CommercialPlan from './commercial-plan'
 import GetInTouchBanner from './get-in-touch'
 import BrowseOurFaq from './browse-our-faq'
 import Potential from './potential'
@@ -7,8 +9,8 @@ import PrimeNumbers from './numbers'
 import MeetUsThere from './meet-us-there'
 import ConnectivityReliabilityType from './connectivity-reliability'
 import AvailableInstruments from './available-instruments'
-import CommercialPlan from './commercial-plan'
 import Hero from './hero'
+import { faq_schema } from './browse-our-faq/_faq-schema'
 import Layout from 'features/components/templates/layout'
 import { SEO } from 'components/containers'
 import PaymentAgentAffiliateNav from 'features/components/templates/navigation/payment-agent-nav'
@@ -21,6 +23,9 @@ const DerivPrime = () => {
                 description="_t_With Deriv Prime's liquidity solutions for online trading, you get unbeatable spreads on the commodities, cryptocurrencies, and forex markets._t_"
                 title="_t_Liquidity solutions provider | Deriv Prime_t_"
             />
+            <Helmet>
+                <script type="application/ld+json">{JSON.stringify(faq_schema)}</script>
+            </Helmet>
             <PaymentAgentAffiliateNav />
             <Hero />
             <Potential />

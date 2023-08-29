c460dc5a3dcdf5972fac9f85cc20d06de2b1b8ae
hasan-deriv <abuhasan@deriv.com>
2023-08-29 12:02:16 +0800

    feat: added high low seo component to page

--- src/features/pages/options/digital/high-low/index.tsx
+++ src/features/pages/options/digital/high-low/index.tsx
@@ -1,28 +1,20 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
-import { faq_schema } from './_faq-schema'
 import {
     high_and_low_content_items,
     high_and_low_faq,
     high_and_low_markets,
     high_and_low_Platforms,
 } from './data'
+import HighLowSEO from './seo'
 import AvailableMarketPlatforms from 'features/components/organisms/available-markets-platforms'
 import DigitalOptionsLayout from 'features/components/templates/digital-options-layout'
 import OptionsContent from 'features/components/templates/options-content'
 import OptionsFaq from 'features/components/templates/options-faq'
-import { SEO } from 'components/containers'
 
 const HighAndLowDigitalOptionsPage = () => {
     return (
         <DigitalOptionsLayout>
-            <SEO
-                title="_t_High/Low ticks | Digital options contract | Deriv_t_"
-                description="_t_Open a digital options contract with a High/Low ticks trade type on Deriv’s trading platforms and earn payouts with accurate market predictions._t_"
-            />
-            <Helmet>
-                <script type="application/ld+json">{JSON.stringify(faq_schema)}</script>
-            </Helmet>
+            <HighLowSEO />
             <OptionsContent items={high_and_low_content_items} />
             <AvailableMarketPlatforms
                 markets={high_and_low_markets}

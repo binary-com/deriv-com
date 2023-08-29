424597ef175e6f5270af3f12b0c36642e37dfd7d
hasan-deriv <abuhasan@deriv.com>
2023-08-29 11:59:33 +0800

    feat: added digits seo component to page

--- src/features/pages/options/digital/digits/index.tsx
+++ src/features/pages/options/digital/digits/index.tsx
@@ -1,28 +1,20 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
+import DigitsSEO from './seo'
 import {
     digits_content_items,
     digit_options_faq,
     digit_options_markets,
     digit_options_platforms,
 } from './data'
-import { faq_schema } from './_faq-schema'
 import AvailableMarketPlatforms from 'features/components/organisms/available-markets-platforms'
 import DigitalOptionsLayout from 'features/components/templates/digital-options-layout'
 import OptionsContent from 'features/components/templates/options-content'
 import OptionsFaq from 'features/components/templates/options-faq'
-import { SEO } from 'components/containers'
 
 const DigitDigitalOptionsPage = () => {
     return (
         <DigitalOptionsLayout>
-            <SEO
-                title="_t_Digits | Digital options contract | Deriv_t_"
-                description="_t_Open a digital options contract with a Digits trade type on Deriv’s trading platforms and earn payouts with accurate market predictions._t_"
-            />
-            <Helmet>
-                <script type="application/ld+json">{JSON.stringify(faq_schema)}</script>
-            </Helmet>
+            <DigitsSEO />
             <OptionsContent items={digits_content_items} />
             <AvailableMarketPlatforms
                 markets={digit_options_markets}
--- src/pages/trade-types/options/digital-options/digits/index.tsx
+++ src/pages/trade-types/options/digital-options/digits/index.tsx
@@ -3,6 +3,7 @@ import { WithIntl } from 'components/localization'
 import DigitDigitalOptionsPage from 'features/pages/options/digital/digits'
 import ProtectedRoute from 'features/components/molecules/protected-route'
 import useRegion from 'components/hooks/use-region'
+import DigitsSEO from 'features/pages/options/digital/digits/seo'
 
 const DigitDigitalOptions = () => {
     const { is_row, is_region_loading } = useRegion()
@@ -11,6 +12,7 @@ const DigitDigitalOptions = () => {
             is_page_visible={is_row}
             component={<DigitDigitalOptionsPage />}
             is_loading={is_region_loading}
+            seo={<DigitsSEO />}
         />
     )
 }

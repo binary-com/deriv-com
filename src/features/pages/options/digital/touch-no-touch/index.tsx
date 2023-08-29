1737b6f5e720efdd8cba4b9f228779ffbe5e2cb9
hasan-deriv <abuhasan@deriv.com>
2023-08-29 12:22:30 +0800

    feat: added touch no touch seo component to page

--- src/features/pages/options/digital/touch-no-touch/index.tsx
+++ src/features/pages/options/digital/touch-no-touch/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
 import {
     touch_and_no_touch_content_items,
     touch_and_no_touch_faq,
     touch_no_touch_markets,
     touch_and_no_touch_platforms,
 } from './data'
-import { faq_schema } from './_faq-schema'
-import { SEO } from 'components/containers'
+import TouchNoTouchSEO from './seo'
 import AvailableMarketPlatforms from 'features/components/organisms/available-markets-platforms'
 import DigitalOptionsLayout from 'features/components/templates/digital-options-layout'
 import OptionsContent from 'features/components/templates/options-content'
@@ -16,13 +14,7 @@ import OptionsFaq from 'features/components/templates/options-faq'
 const TouchAndNoTouchDigitalOptionsPage = () => {
     return (
         <DigitalOptionsLayout>
-            <SEO
-                title="_t_Touch/No touch | Digital options contract | Deriv_t_"
-                description="_t_Open a digital options contract with a Touch/No touch trade type on Deriv’s trading platforms and earn payouts with accurate market predictions._t_"
-            />
-            <Helmet>
-                <script type="application/ld+json">{JSON.stringify(faq_schema)}</script>
-            </Helmet>
+            <TouchNoTouchSEO />
             <OptionsContent items={touch_and_no_touch_content_items} />
             <AvailableMarketPlatforms
                 markets={touch_no_touch_markets}

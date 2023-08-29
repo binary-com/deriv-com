b7c12abe7571d9e9244ef6b5416bdd6af64f6bd8
amir ali <amir.ali@regentmarkets.com>
2023-08-29 16:40:54 +0500

    chore: clean up unwanted logs

--- src/store/global-provider.tsx
+++ src/store/global-provider.tsx
@@ -1,21 +1,9 @@
 import React from 'react'
-import { RudderStack } from '@deriv/analytics'
 import { BreakpointsProvider } from './breakpoints-context'
 import { PopupProvider } from './popup-context'
 import { RegionProvider } from './region-context'
 import { WebsiteStatusProvider } from './website-status-context'
 import { MediaContextProvider } from 'themes/media'
-
-// TODO: Remove this temporary debug logs
-// eslint-disable-next-line
-// @ts-ignore
-typeof window !== 'undefined' && (window.RudderStack = RudderStack)
-// eslint-disable-next-line
-console.log(
-    process.env.RUDDERSTACK_URL,
-    process.env.RUDDERSTACK_STAGING_KEY,
-    process.env.RUDDERSTACK_PRODUCTION_KEY,
-)
 
 type GlobalProviderProps = { children: React.ReactNode }
 

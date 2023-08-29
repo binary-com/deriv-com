bf3d50ea56d650b86387f1bdde0497c024b4ad23
hasan-deriv <abuhasan@deriv.com>
2023-08-29 12:01:48 +0800

    feat: created high low seo component

---
+++ src/features/pages/options/digital/high-low/seo.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import { faq_schema } from './_faq-schema'
+import { SEO } from 'components/containers'
+
+const HighLowSEO = () => {
+    return (
+        <>
+            <SEO
+                title="_t_High/Low ticks | Digital options contract | Deriv_t_"
+                description="_t_Open a digital options contract with a High/Low ticks trade type on Deriv’s trading platforms and earn payouts with accurate market predictions._t_"
+            />
+            <Helmet>
+                <script type="application/ld+json">{JSON.stringify(faq_schema)}</script>
+            </Helmet>
+        </>
+    )
+}
+
+export default HighLowSEO

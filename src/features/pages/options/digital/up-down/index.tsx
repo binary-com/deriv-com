c6bb16ecf9c821c10d0c12284f3eccd6877bba93
hasan-deriv <abuhasan@deriv.com>
2023-08-29 12:26:05 +0800

    feat: added up down seo component to page

--- src/features/pages/options/digital/up-down/index.tsx
+++ src/features/pages/options/digital/up-down/index.tsx
@@ -1,23 +1,15 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
 import { upAndDownContentItems, upAndDownFAQ, upAndDownMarkets, upAndDownPlatforms } from './data'
-import { faq_schema } from './_faq-schema'
+import UpDownSEO from './seo'
 import AvailableMarketPlatforms from 'features/components/organisms/available-markets-platforms'
 import DigitalOptionsLayout from 'features/components/templates/digital-options-layout'
 import OptionsContent from 'features/components/templates/options-content'
 import OptionsFaq from 'features/components/templates/options-faq'
-import { SEO } from 'components/containers'
 
 const UpAndDownDigitalOptionsPage = () => {
     return (
         <DigitalOptionsLayout>
-            <SEO
-                title="_t_Up/Down | Digital options contract | Deriv_t_"
-                description="_t_Open a digital options contract with an Up/Down trade type on Deriv’s trading platforms and earn payouts with accurate market predictions._t_"
-            />
-            <Helmet>
-                <script type="application/ld+json">{JSON.stringify(faq_schema)}</script>
-            </Helmet>
+            <UpDownSEO />
             <OptionsContent items={upAndDownContentItems} />
             <AvailableMarketPlatforms markets={upAndDownMarkets} platforms={upAndDownPlatforms} />
             <OptionsFaq faqs={upAndDownFAQ} />
--- src/pages/trade-types/options/digital-options/up-and-down/index.tsx
+++ src/pages/trade-types/options/digital-options/up-and-down/index.tsx
@@ -5,6 +5,7 @@ import { WithIntl } from 'components/localization'
 import UpAndDownDigitalOptionsPage from 'features/pages/options/digital/up-down'
 import ProtectedRoute from 'features/components/molecules/protected-route'
 import useRegion from 'components/hooks/use-region'
+import UpDownSEO from 'features/pages/options/digital/up-down/seo'
 
 const DigitalOptions = () => {
     const { is_row, is_region_loading } = useRegion()
@@ -21,6 +22,7 @@ const DigitalOptions = () => {
                 is_page_visible={is_row}
                 component={<UpAndDownDigitalOptionsPage />}
                 is_loading={is_region_loading}
+                seo={<UpDownSEO />}
             />
         </>
     )

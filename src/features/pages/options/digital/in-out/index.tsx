0885293d5665dad034b34c1b202574640a3f93d1
hasan-deriv <abuhasan@deriv.com>
2023-08-29 12:04:43 +0800

    feat: added in out seo component to page

--- src/features/pages/options/digital/in-out/index.tsx
+++ src/features/pages/options/digital/in-out/index.tsx
@@ -1,23 +1,15 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
 import { inOutContentItems, inOutFAQ, inOutMarkets, inOutPlatforms } from './data'
-import { faq_schema } from './_faq-schema'
+import InOutSEO from './seo'
 import AvailableMarketPlatforms from 'features/components/organisms/available-markets-platforms'
 import DigitalOptionsLayout from 'features/components/templates/digital-options-layout'
 import OptionsContent from 'features/components/templates/options-content'
 import OptionsFaq from 'features/components/templates/options-faq'
-import { SEO } from 'components/containers'
 
 const InOutDigitalOptionsPage = () => {
     return (
         <DigitalOptionsLayout>
-            <SEO
-                title="_t_In/Out | Digital options contract | Deriv_t_"
-                description="_t_Open a digital options contract with an In/Out trade type on Deriv’s trading platforms and earn payouts with accurate market predictions._t_"
-            />
-            <Helmet>
-                <script type="application/ld+json">{JSON.stringify(faq_schema)}</script>
-            </Helmet>
+            <InOutSEO />
             <OptionsContent items={inOutContentItems} />
             <AvailableMarketPlatforms markets={inOutMarkets} platforms={inOutPlatforms} />
             <OptionsFaq faqs={inOutFAQ} />
--- src/pages/trade-types/options/digital-options/in-out/index.tsx
+++ src/pages/trade-types/options/digital-options/in-out/index.tsx
@@ -3,6 +3,7 @@ import { WithIntl } from 'components/localization'
 import InOutDigitalOptionsPage from 'features/pages/options/digital/in-out'
 import ProtectedRoute from 'features/components/molecules/protected-route'
 import useRegion from 'components/hooks/use-region'
+import InOutSEO from 'features/pages/options/digital/in-out/seo'
 
 const DigitalOptionsInOut = () => {
     const { is_row, is_region_loading } = useRegion()
@@ -11,6 +12,7 @@ const DigitalOptionsInOut = () => {
             is_page_visible={is_row}
             component={<InOutDigitalOptionsPage />}
             is_loading={is_region_loading}
+            seo={<InOutSEO />}
         />
     )
 }

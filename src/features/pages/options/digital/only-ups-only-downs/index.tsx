442b378485242a8bf426cdc17b67a3767df67aa4
hasan-deriv <abuhasan@deriv.com>
2023-08-29 12:17:04 +0800

    feat: added only ups and downs seo component to page

--- src/features/pages/options/digital/only-ups-only-downs/index.tsx
+++ src/features/pages/options/digital/only-ups-only-downs/index.tsx
@@ -1,28 +1,20 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
 import {
     only_ups_downs_content_items,
     only_ups_downs_markets,
     only_ups_downs_platforms,
     only_ups_downs_FAQ,
 } from './data'
-import { faq_schema } from './_faq-schema'
+import OnlyUpAndOnlyDownSEO from './seo'
 import AvailableMarketPlatforms from 'features/components/organisms/available-markets-platforms'
 import DigitalOptionsLayout from 'features/components/templates/digital-options-layout'
 import OptionsContent from 'features/components/templates/options-content'
 import OptionsFaq from 'features/components/templates/options-faq'
-import { SEO } from 'components/containers'
 
 const OnlyUpAndOnlyDownDigitalOptionsPage = () => {
     return (
         <DigitalOptionsLayout>
-            <SEO
-                title="_t_Only ups/Only downs | Digital options contract | Deriv_t_"
-                description="_t_Open a digital options contract with an Only ups/Only downs trade type on Deriv’s trading platforms and earn payouts with accurate market predictions._t_"
-            />
-            <Helmet>
-                <script type="application/ld+json">{JSON.stringify(faq_schema)}</script>
-            </Helmet>
+            <OnlyUpAndOnlyDownSEO />
             <OptionsContent items={only_ups_downs_content_items} />
             <AvailableMarketPlatforms
                 markets={only_ups_downs_markets}
--- src/pages/trade-types/options/digital-options/only-ups-only-downs/index.tsx
+++ src/pages/trade-types/options/digital-options/only-ups-only-downs/index.tsx
@@ -3,6 +3,7 @@ import { WithIntl } from 'components/localization'
 import OnlyUpsOnlyDowns from 'features/pages/options/digital/only-ups-only-downs'
 import useRegion from 'components/hooks/use-region'
 import ProtectedRoute from 'features/components/molecules/protected-route'
+import OnlyUpAndOnlyDownSEO from 'features/pages/options/digital/only-ups-only-downs/seo'
 
 const OnlyUpsAndOnlyDowns = () => {
     const { is_row, is_region_loading } = useRegion()
@@ -11,6 +12,7 @@ const OnlyUpsAndOnlyDowns = () => {
             is_page_visible={is_row}
             component={<OnlyUpsOnlyDowns />}
             is_loading={is_region_loading}
+            seo={<OnlyUpAndOnlyDownSEO />}
         />
     )
 }

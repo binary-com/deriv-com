0c06addbf6c3c8b39a37e0e2f3d038f0532e0e2f
hasan-deriv <abuhasan@deriv.com>
2023-08-29 11:42:36 +0800

    feat: added vanilla seo component to page

--- src/pages/trade-types/options/vanilla-options/index.tsx
+++ src/pages/trade-types/options/vanilla-options/index.tsx
@@ -3,6 +3,7 @@ import VanillaOptions from 'features/pages/options/vanilla'
 import { WithIntl } from 'components/localization'
 import useRegion from 'components/hooks/use-region'
 import ProtectedRoute from 'features/components/molecules/protected-route'
+import VanillaSEO from 'features/pages/options/vanilla/seo'
 
 const VanillaOptionsPage = () => {
     const { is_row, is_region_loading } = useRegion()
@@ -11,6 +12,7 @@ const VanillaOptionsPage = () => {
             is_page_visible={is_row}
             component={<VanillaOptions />}
             is_loading={is_region_loading}
+            seo={<VanillaSEO />}
         />
     )
 }

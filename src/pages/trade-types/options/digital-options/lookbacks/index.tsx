41e415c7c0580a34864afbcd62939a8a8ce66121
hasan-deriv <abuhasan@deriv.com>
2023-08-29 12:07:56 +0800

    feat: added lookbacks seo component to page

--- src/pages/trade-types/options/digital-options/lookbacks/index.tsx
+++ src/pages/trade-types/options/digital-options/lookbacks/index.tsx
@@ -3,6 +3,7 @@ import { WithIntl } from 'components/localization'
 import LookbacksDigitalOptionsPage from 'features/pages/options/digital/lookbacks'
 import ProtectedRoute from 'features/components/molecules/protected-route'
 import useRegion from 'components/hooks/use-region'
+import LookbacksSEO from 'features/pages/options/digital/lookbacks/seo'
 
 const LookBacksOptions = () => {
     const { is_row, is_region_loading } = useRegion()
@@ -11,6 +12,7 @@ const LookBacksOptions = () => {
             is_page_visible={is_row}
             component={<LookbacksDigitalOptionsPage />}
             is_loading={is_region_loading}
+            seo={<LookbacksSEO />}
         />
     )
 }

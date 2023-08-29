b7492b522e379da3b3cfd67184a0b4f27f1cbe25
hasan-deriv <abuhasan@deriv.com>
2023-08-29 12:02:16 +0800

    feat: added high low seo component to page

--- src/pages/trade-types/options/digital-options/high-low-ticks/index.tsx
+++ src/pages/trade-types/options/digital-options/high-low-ticks/index.tsx
@@ -3,6 +3,7 @@ import HighLowDigitalOptionsPage from 'features/pages/options/digital/high-low'
 import { WithIntl } from 'components/localization'
 import ProtectedRoute from 'features/components/molecules/protected-route'
 import useRegion from 'components/hooks/use-region'
+import HighLowSEO from 'features/pages/options/digital/high-low/seo'
 
 const HighLowDigitalOptions = () => {
     const { is_row, is_region_loading } = useRegion()
@@ -11,6 +12,7 @@ const HighLowDigitalOptions = () => {
             is_page_visible={is_row}
             component={<HighLowDigitalOptionsPage />}
             is_loading={is_region_loading}
+            seo={<HighLowSEO />}
         />
     )
 }

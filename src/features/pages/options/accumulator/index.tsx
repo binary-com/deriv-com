a10c8e1a77f8edd6bf57c3c8b4febd4df31cd694
hasan-deriv <abuhasan@deriv.com>
2023-08-29 11:53:38 +0800

    feat: added accumulator seo component to page

--- src/features/pages/options/accumulator/index.tsx
+++ src/features/pages/options/accumulator/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
 import {
     accumulatorFAQ,
     accumulatorMarkets,
     accumulatorOptionsContentItems,
     accumulatorPlatforms,
 } from './data'
-import { faq_schema } from './_faq-schema'
+import AccumulatorSEO from './seo'
 import { Localize } from 'components/localization'
-import { SEO } from 'components/containers'
 import Typography from 'features/components/atoms/typography'
 import OptionsLayout from 'features/components/templates/options-layout'
 import OptionsContent from 'features/components/templates/options-content'
@@ -22,13 +20,7 @@ const AccumulatorsOptions = () => {
             heading="_t_What are accumulator options?_t_"
             description="_t_Accumulator options allow you to predict whether the current spot price remains within a predefined range from the previous spot price._t_"
         >
-            <SEO
-                title="_t_Accumulator options | Trade options | Deriv_t_"
-                description="_t_Grow your potential payout at every tick with accumulator options. Accumulator trading is available on volatility indices under derived indices._t_"
-            />
-            <Helmet>
-                <script type="application/ld+json">{JSON.stringify(faq_schema)}</script>
-            </Helmet>
+            <AccumulatorSEO />
             <Typography.Heading
                 as="h2"
                 align="center"
--- src/pages/trade-types/options/accumulator-options/index.tsx
+++ src/pages/trade-types/options/accumulator-options/index.tsx
@@ -3,6 +3,7 @@ import AccumulatorsOptions from 'features/pages/options/accumulator'
 import { WithIntl } from 'components/localization'
 import useRegion from 'components/hooks/use-region'
 import ProtectedRoute from 'features/components/molecules/protected-route'
+import AccumulatorSEO from 'features/pages/options/accumulator/seo'
 
 const AccumulatorsOptionsPage = () => {
     const { is_row, is_region_loading } = useRegion()
@@ -11,6 +12,7 @@ const AccumulatorsOptionsPage = () => {
             is_page_visible={is_row}
             component={<AccumulatorsOptions />}
             is_loading={is_region_loading}
+            seo={<AccumulatorSEO />}
         />
     )
 }

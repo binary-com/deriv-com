7a9bd82450fdab8180e7870d5c855afeeb757fce
hasan-deriv <abuhasan@deriv.com>
2023-08-29 11:56:45 +0800

    feat: added asians seo component to page

--- src/features/pages/options/digital/asians/index.tsx
+++ src/features/pages/options/digital/asians/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
+import AsiansSEO from './seo'
 import { asiansContentItems, asiansFAQ, asiansMarkets, asiansPlatforms } from './data'
-import { faq_schema } from './_faq-schema'
-import { SEO } from 'components/containers'
 import AvailableMarketPlatforms from 'features/components/organisms/available-markets-platforms'
 import DigitalOptionsLayout from 'features/components/templates/digital-options-layout'
 import OptionsContent from 'features/components/templates/options-content'
@@ -11,13 +9,7 @@ import OptionsFaq from 'features/components/templates/options-faq'
 const AsiansDigitalOptionsPage = () => {
     return (
         <DigitalOptionsLayout>
-            <SEO
-                title="_t_Asians | Digital options contract | Deriv_t_"
-                description="_t_Open a digital options contract with an Asians trade type on Deriv’s trading platforms and earn payouts with accurate market predictions._t_"
-            />
-            <Helmet>
-                <script type="application/ld+json">{JSON.stringify(faq_schema)}</script>
-            </Helmet>
+            <AsiansSEO />
             <OptionsContent items={asiansContentItems} />
             <AvailableMarketPlatforms markets={asiansMarkets} platforms={asiansPlatforms} />
             <OptionsFaq faqs={asiansFAQ} />
--- src/pages/trade-types/options/digital-options/asians/index.tsx
+++ src/pages/trade-types/options/digital-options/asians/index.tsx
@@ -3,6 +3,7 @@ import ProtectedRoute from 'features/components/molecules/protected-route'
 import { WithIntl } from 'components/localization'
 import AsiansDigitalOptionsPage from 'features/pages/options/digital/asians'
 import useRegion from 'components/hooks/use-region'
+import AsiansSEO from 'features/pages/options/digital/asians/seo'
 
 const DigitalOptionsAsians = () => {
     const { is_row, is_region_loading } = useRegion()
@@ -11,6 +12,7 @@ const DigitalOptionsAsians = () => {
             is_page_visible={is_row}
             component={<AsiansDigitalOptionsPage />}
             is_loading={is_region_loading}
+            seo={<AsiansSEO />}
         />
     )
 }

5fe41cd67996f93ea971186db0475376d338e83d
hasan-deriv <abuhasan@deriv.com>
2023-08-29 12:19:47 +0800

    feat: added reset call seo component to page

--- src/features/pages/options/digital/reset-call/index.tsx
+++ src/features/pages/options/digital/reset-call/index.tsx
@@ -1,28 +1,20 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
-import { faq_schema } from './_faq-schema'
 import {
     reset_and_recall_content_items,
     reset_call_faq,
     reset_call_markets,
     reset_call_Platforms,
 } from './data'
+import ResetCallSEO from './seo'
 import AvailableMarketPlatforms from 'features/components/organisms/available-markets-platforms'
 import DigitalOptionsLayout from 'features/components/templates/digital-options-layout'
 import OptionsContent from 'features/components/templates/options-content'
 import OptionsFaq from 'features/components/templates/options-faq'
-import { SEO } from 'components/containers'
 
 const ResetCallDigitalOptionsPage = () => {
     return (
         <DigitalOptionsLayout>
-            <SEO
-                title="_t_Reset call/reset put | Digital options contract | Deriv_t_"
-                description="_t_Open a digital options contract with a Reset call/Reset put trade type on Deriv’s trading platforms and earn payouts with accurate market predictions._t_"
-            />
-            <Helmet>
-                <script type="application/ld+json">{JSON.stringify(faq_schema)}</script>
-            </Helmet>
+            <ResetCallSEO />
             <OptionsContent items={reset_and_recall_content_items} />
             <AvailableMarketPlatforms
                 markets={reset_call_markets}
--- src/pages/trade-types/options/digital-options/reset-call-reset-put/index.tsx
+++ src/pages/trade-types/options/digital-options/reset-call-reset-put/index.tsx
@@ -3,6 +3,7 @@ import ResetCallOptionsPage from 'features/pages/options/digital/reset-call'
 import { WithIntl } from 'components/localization'
 import useRegion from 'components/hooks/use-region'
 import ProtectedRoute from 'features/components/molecules/protected-route'
+import ResetCallSEO from 'features/pages/options/digital/reset-call/seo'
 
 const ResetCallDigitalOptions = () => {
     const { is_row, is_region_loading } = useRegion()
@@ -11,6 +12,7 @@ const ResetCallDigitalOptions = () => {
             is_page_visible={is_row}
             component={<ResetCallOptionsPage />}
             is_loading={is_region_loading}
+            seo={<ResetCallSEO />}
         />
     )
 }

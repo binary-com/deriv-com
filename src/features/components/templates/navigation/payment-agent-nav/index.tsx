593600c4a34ca7361cf9f7a1e7e6ac634ab6cde1
aswathy-deriv <aswathy@deriv.com>
2023-08-30 14:58:31 +0400

    fix: check server side rendering for contact us button

--- src/features/components/templates/navigation/payment-agent-nav/index.tsx
+++ src/features/components/templates/navigation/payment-agent-nav/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import NavTemplate from '../template'
 import affiliateNavItems from './data'
 import { partners_nav_logo, partners_buttons } from './payment-agent-nav.module.scss'
@@ -13,8 +13,52 @@ import { getLocationPathname } from 'common/utility'
 import useScrollToElement from 'features/hooks/use-scroll-to-element'
 
 const PaymentAgentAffiliateNav = () => {
-    const is_deriv_prime_url = getLocationPathname().includes('deriv-prime')
+    const path_name = getLocationPathname()
     const clickToScrollHandler = useScrollToElement('getintouch')
+
+    const generate_buttons = useMemo(() => {
+        return !path_name ? null : path_name.includes('deriv-prime') ? (
+            <Button.Primary
+                id="dm-nav-deriv-prime-contact"
+                onClick={clickToScrollHandler}
+                className={partners_buttons}
+            >
+                <Localize translate_text="_t_Contact us_t_" />
+            </Button.Primary>
+        ) : (
+            <>
+                <Button.Primary
+                    id="dm-nav-affiliate-login-button"
+                    onClick={() =>
+                        window.open('https://login.deriv.com/signin.php?lang=0', '_blank')
+                    }
+                    outlined
+                    visible={'larger-than-tablet'}
+                    className={partners_buttons}
+                >
+                    <Localize translate_text="_t_Affiliate & IB Log in_t_" />
+                </Button.Primary>
+                <Button.Primary
+                    id="dm-nav-affiliate-login-button"
+                    onClick={() =>
+                        window.open('https://login.deriv.com/signin.php?lang=0', '_blank')
+                    }
+                    outlined
+                    visible={'phone-and-tablet'}
+                >
+                    <Localize translate_text="_t_Log in_t_" />
+                </Button.Primary>
+                <Button.Primary
+                    id="dm-nav-affiliate-signup-button"
+                    onClick={() => window.open('https://login.deriv.com/signup.php', '_blank')}
+                    visible={'larger-than-tablet'}
+                    className={partners_buttons}
+                >
+                    <Localize translate_text="_t_Affiliate & IB sign up_t_" />
+                </Button.Primary>
+            </>
+        )
+    }, [path_name])
 
     return (
         <NavTemplate
@@ -34,50 +78,7 @@ const PaymentAgentAffiliateNav = () => {
                 align="center"
                 gap="8x"
             >
-                {is_deriv_prime_url ? (
-                    <Button.Primary
-                        id="dm-nav-deriv-prime-contact"
-                        onClick={clickToScrollHandler}
-                        className={partners_buttons}
-                    >
-                        <Localize translate_text="_t_Contact us_t_" />
-                    </Button.Primary>
-                ) : (
-                    <>
-                        <Button.Primary
-                            id="dm-nav-affiliate-login-button"
-                            onClick={() =>
-                                window.open('https://login.deriv.com/signin.php?lang=0', '_blank')
-                            }
-                            outlined
-                            visible={'larger-than-tablet'}
-                            className={partners_buttons}
-                        >
-                            <Localize translate_text="_t_Affiliate & IB Log in_t_" />
-                        </Button.Primary>
-                        <Button.Primary
-                            id="dm-nav-affiliate-login-button"
-                            onClick={() =>
-                                window.open('https://login.deriv.com/signin.php?lang=0', '_blank')
-                            }
-                            outlined
-                            visible={'phone-and-tablet'}
-                        >
-                            <Localize translate_text="_t_Log in_t_" />
-                        </Button.Primary>
-                        <Button.Primary
-                            id="dm-nav-affiliate-signup-button"
-                            onClick={() =>
-                                window.open('https://login.deriv.com/signup.php', '_blank')
-                            }
-                            visible={'larger-than-tablet'}
-                            className={partners_buttons}
-                        >
-                            <Localize translate_text="_t_Affiliate & IB sign up_t_" />
-                        </Button.Primary>
-                    </>
-                )}
-
+                {generate_buttons}
                 <LanguageSwitcher />
             </Flex.Box>
         </NavTemplate>

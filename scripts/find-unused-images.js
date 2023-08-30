632b95767718a40a125ea224d72d238f244da591
Nuzhy-Deriv <nuzhy@deriv.com>
2023-08-30 17:10:46 +0800

    refactor: variables and output

--- scripts/find-unused-images.js
+++ scripts/find-unused-images.js
@@ -7,7 +7,7 @@ const IMAGE_DIRECTORY = path.join(__dirname, '../src/images');
 const SRC_DIRECTORY = path.join(__dirname, '../src');
 
 function findUsedImages() {
-    const usedImages = new Set();
+    const used_images = new Set();
 
     // Search for image references in your source code
     const files = glob.sync(`${SRC_DIRECTORY}/**/*.{js,jsx,ts,tsx}`,{
@@ -17,42 +17,43 @@ function findUsedImages() {
     files.forEach(file => {
         const content = fs.readFileSync(file, 'utf8')
         IMAGE_EXTENSIONS.forEach(ext => {
-        const pattern = new RegExp(`[\'\"]([^\'\"]+${ext})[\'\"]`, 'g');
-        const matches = content.match(pattern);
-        if (matches) {
-            matches.forEach(match => {
-                usedImages.add(match.split("/").slice(-1).join("/").replace(/['"]+/g, ''))
-            });
-        }});
+            const pattern = new RegExp(`[\'\"]([^\'\"]+${ext})[\'\"]`, 'g');
+            const matches = content.match(pattern);
+            if (matches) {
+                matches.forEach(match => {
+                    used_images.add(match.split("/").slice(-1).join("/").replace(/['"]+/g, ''))
+                });
+            }
+        });
     });
 
-    return usedImages;
+    return used_images;
 }
 
-function findUnusedImages(usedImages) {
-    const allImages = glob.sync(`${IMAGE_DIRECTORY}/**/*.{png,jpeg,jpg,svg}`);
-    const unusedImages = allImages.filter(image => 
-        !usedImages.has(image.split("/").slice(-1).join("/").replace(/['"]+/g, ''))
+function findUnusedImages(used_images) {
+    const all_images = glob.sync(`${IMAGE_DIRECTORY}/**/*.{png,jpeg,jpg,svg}`);
+    const unused_images = all_images.filter(image => 
+        !used_images.has(image.split("/").slice(-1).join("/").replace(/['"]+/g, ''))
     );
-    return unusedImages;
+    return unused_images;
 }
 
 function main() {
     let total_size = 0
-    const usedImages = findUsedImages();
-    const unusedImages = findUnusedImages(usedImages);
+    const used_images = findUsedImages();
+    const unused_images = findUnusedImages(used_images);
 
     // console.log('Used images:');
     // usedImages.forEach(image => console.log(image));
 
-    if(unusedImages.length > 0) {
-        console.log('\nUnused images:');
-        unusedImages.forEach(image => {
-            let size = fs.statSync(image).size / (1024*1024);
-            console.log(image, ', size(MB):', size)
-            total_size += size
+    if(unused_images.length > 0) {
+        console.log('\nUnused images ([size] - path) :');
+        unused_images.forEach(image => {
+            let size_in_kb = fs.statSync(image).size / (1024);
+            console.log(`[${size_in_kb.toFixed(2)}kb] - ${image}`)
+            total_size += size_in_kb
         });
-        console.log('\nTotal Unused Image Memory(MB)', total_size)
+        console.log(`\nTotal Unused Image Memory: ${total_size.toFixed(2)}kb\n`)
         
         process.exit(1);
     }

05bb5075adcc94e95d4cd9992da749e15d498103
NikitK-deriv <103182473+NikitK-deriv@users.noreply.github.com>
2023-09-01 13:16:32 +0300

    feat: rudderstack + growthbook without deriv package (#5512)
    * feat: rudderstack + growthbook without deriv package
    
    * feat: add deriv/analytics package
    
    * chore: update import and package-lock

--- gatsby-config.js
+++ gatsby-config.js
@@ -346,5 +346,15 @@ module.exports = {
                 generateStatsFile: process.env.GENERATE_JSON_STATS === 'true',
             },
         },
+        {
+            resolve: 'gatsby-plugin-env-variables',
+            options: {
+                allowList: [
+                    'RUDDERSTACK_URL',
+                    'RUDDERSTACK_STAGING_KEY',
+                    'RUDDERSTACK_PRODUCTION_KEY',
+                ],
+            },
+        },
     ],
 }
--- src/features/pages/home/hero/content/index.tsx
+++ src/features/pages/home/hero/content/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import loadable from '@loadable/component'
 import pMinDelay from 'p-min-delay'
+import { useFeatureValue } from '@growthbook/growthbook-react'
 import HeroCtaButton from './hero-cta.button'
-import { hero_typewriter, hero_content_title } from './hero-content.module.scss'
+import { hero_content_title, hero_typewriter } from './hero-content.module.scss'
 import HeroHeaderItems from './hero-header.items'
 import Flex from 'features/components/atoms/flex-box'
 import Typography from 'features/components/atoms/typography'
@@ -19,11 +20,20 @@ const HeroFeaturesCarousel = loadable(() => pMinDelay(import('./hero-features.ca
 })
 
 const HomeHeroContent = () => {
+    const ebookStocksHeadingTest = useFeatureValue('homepage', 'control')
+
+    const headings = {
+        control: '_t_Get the widest range of markets, trades and platforms_t_',
+        'new-title': '_t_Get the widest range of markets, trades and platforms_t_',
+    }
+
+    const heading = headings[ebookStocksHeadingTest] || headings.control
+
     return (
         <Flex.Box justify="start" direction="col" align="start" gap="4x" dir={get_lang_direction()}>
             <HeroHeaderItems />
             <Typography.Heading as="h2" size={'xs'} weight="normal" className={hero_content_title}>
-                <Localize translate_text="_t_Get the widest range of markets, trades and platforms_t_" />
+                <Localize translate_text={heading} />
             </Typography.Heading>
             <HeroFeaturesCarousel />
             <HeroCtaButton />

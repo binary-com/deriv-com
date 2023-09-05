95adb256d0dcc9560fe69299f3e9e9c7c8c97b08
deriv <nikita.kolesnik@regentmarkets.com>
2023-09-05 10:56:27 +0300

    chore: update the package to sync with env variables

--- src/features/hooks/analytic/use-analytic-events/index.ts
+++ src/features/hooks/analytic/use-analytic-events/index.ts
@@ -1,19 +1,15 @@
 import { useCallback } from 'react'
-import { RudderStack } from '@deriv/analytics'
+import { RudderStack, SignupProvider, TEvents } from '@deriv/analytics'
 import { useAnalyticData } from 'features/hooks/analytic/use-analytic-data'
 
-type AnalyticsTypes = 'ce_virtual_signup_form' | 'ce_real_account_signup_form'
+type ExtractAction<T> = T extends { action: infer A } ? A : never
+type ActionForEvent<E extends keyof TEvents> = ExtractAction<TEvents[E]>
 
-type AnalyticsEventsTypes = 'open' | 'started' | 'email_confirmation_sent'
-
-type SignupProvider = 'email' | 'phone' | 'google' | 'facebook' | 'apple'
-
-export const useAnalyticsEvents = (event: AnalyticsTypes) => {
+export const useAnalyticsEvents = <T extends keyof TEvents>(event: keyof TEvents) => {
     const { form_source, form_name } = useAnalyticData()
-    // console.log(RudderStack)
 
     const analytic_events = {
-        [event]: useCallback((action: AnalyticsEventsTypes, signup_provider?: SignupProvider) => {
+        [event]: useCallback((action: ActionForEvent<T>, signup_provider?: SignupProvider) => {
             RudderStack.track(
                 event,
                 { action, signup_provider, form_source, form_name },

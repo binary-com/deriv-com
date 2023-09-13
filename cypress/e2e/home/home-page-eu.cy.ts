20fcc227b63766685a06e23bb92a2ac5080767e1
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-09-13 16:11:21 +0400

    fix: comments and features

--- cypress/e2e/home/home-page-eu.cy.ts
+++ cypress/e2e/home/home-page-eu.cy.ts
@@ -1,21 +1,22 @@
 describe('EU Language pop up in the navigation', () => {
+    //before every check visit base url
+    beforeEach(() => {
+        cy.visit(`${Cypress.env('base_url_eu') || 'https://eu.deriv.com'}`)
+    })
     it('language popup opens perfectly in eu', () => {
-        const url = Cypress.env('base_url_eu') || 'https://eu.deriv.com'
-        cy.visit(`${url}/`)
         cy.get('#radix-4').click()
     })
 
     it('EU Language popup Should contain all language items in the dropdown', () => {
         // Visit the webpage or navigate to the relevant page
-        const url = Cypress.env('base_url_eu') || 'https://eu.deriv.com'
-        cy.visit(`${url}/`)
+
         cy.get('#radix-4').click()
 
         const languagesArray = Cypress.env('languages')
         const languageArrayLength = languagesArray.length
 
         // Get all the options within the dropdown
-        cy.get('#radix-6 div').should('have.length', languageArrayLength)
+        cy.get('#radix-6 [role=menuitem]').should('have.length', languageArrayLength)
     })
 
     it('EU - Should visit eu.deriv.com on each language', () => {
@@ -23,7 +24,6 @@ describe('EU Language pop up in the navigation', () => {
         Cypress.env('languages').forEach((language) => {
             const url = `${baseUrl}/${language}/`
             cy.visit(url)
-            // cy.get('#dm-hero-signup').should('exist')
         })
     })
 })
--- cypress/e2e/home/home-page.cy.ts
+++ cypress/e2e/home/home-page.cy.ts
@@ -1,22 +1,22 @@
 describe('ROW Language pop up in the navigation', () => {
+    //before every check visit base url
+    beforeEach(() => {
+        cy.visit(`${Cypress.env('base_url') || 'https://deriv.com'}`)
+    })
     it('language popup opens perfectly in row', () => {
-        const url = Cypress.env('base_url') || 'https://deriv.com'
         // You can only use cy commands inside the it block
-        cy.visit(`${url}/`)
         cy.get('#radix-4').click()
     })
 
     it('ROW Language popup Should contain all language items in the dropdown', () => {
         // Visit the webpage or navigate to the relevant page
-        const url = Cypress.env('base_url') || 'https://deriv.com'
-        cy.visit(`${url}/`)
         cy.get('#radix-4').click()
 
         const languagesArray = Cypress.env('languages')
         const languageArrayLength = languagesArray.length
 
         // Get all the options within the dropdown
-        cy.get('#radix-6 div').should('have.length', languageArrayLength)
+        cy.get('#radix-6 [role=menuitem]').should('have.length', languageArrayLength)
     })
 
     it('ROW - Should visit deriv.com on each language', () => {
@@ -24,7 +24,6 @@ describe('ROW Language pop up in the navigation', () => {
         Cypress.env('languages').forEach((language) => {
             const url = `${baseUrl}/${language}/`
             cy.visit(url)
-            // cy.get('#dm-hero-signup').should('exist')
         })
     })
 })

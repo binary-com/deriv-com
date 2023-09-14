68488be9026381818a00ca621d3695f22ce9acbb
mitra-deriv <mitra@regentmarkets.com>
2023-09-14 20:03:56 +0800

    chore: :bug: fix naming convention

--- cypress/e2e/live-pricing.cy.ts
+++ cypress/e2e/live-pricing.cy.ts
@@ -41,31 +41,31 @@ describe('Live pricing spec', () => {
   // 2nd approach of view all button testing
   it.only("should have correct view all link (2nd approach)", () => {
     cy.get('[data-cy="market"]').eq(0).click();
-    cy.get('[data-cy="view_all"]').click();
+    cy.get('[data-cy="view-all"]').click();
     cy.url().should("contain", "markets/forex/")
     cy.visit("http://localhost:8000/")
 
 
     cy.get('[data-cy="market"]').eq(1).click();
-    cy.get('[data-cy="view_all"]').click();
+    cy.get('[data-cy="view-all"]').click();
     cy.url().should("contain", "markets/synthetic/")
     cy.visit("http://localhost:8000/")
 
 
     cy.get('[data-cy="market"]').eq(2).click();
-    cy.get('[data-cy="view_all"]').click();
+    cy.get('[data-cy="view-all"]').click();
     cy.url().should("contain", "markets/stock/")
     cy.visit("http://localhost:8000/")
 
 
     cy.get('[data-cy="market"]').eq(3).click();
-    cy.get('[data-cy="view_all"]').click();
+    cy.get('[data-cy="view-all"]').click();
     cy.url().should("contain", "markets/cryptocurrencies/")
     cy.visit("http://localhost:8000/")
 
 
     cy.get('[data-cy="market"]').eq(4).click();
-    cy.get('[data-cy="view_all"]').click();
+    cy.get('[data-cy="view-all"]').click();
     cy.url().should("contain", "markets/commodities/")
     cy.visit("http://localhost:8000/")
   })

bc5eb612ced6090b32c8215f9a816d677c149930
hasan-deriv <abuhasan@deriv.com>
2023-09-14 18:23:03 +0800

    chore: view all button test

--- cypress/e2e/live-pricing.cy.ts
+++ cypress/e2e/live-pricing.cy.ts
@@ -27,7 +27,46 @@ describe('Live pricing spec', () => {
     })
   })
 
-  it("should have correct view all link", () => {
-    
+  // 1st approach of view all button testing
+  it.only("should have correct view all link (1st approach)", () => {
+
+    const items = ["markets/forex/", "markets/synthetic/", "markets/stock/", "markets/cryptocurrencies/", "markets/commodities/"]
+
+    cy.get('[data-cy="market"]').each(($item, $index) => {
+      cy.wrap($item).click();
+      cy.get('[data-cy="view_all"] a').invoke('attr', 'href').should("include", items[$index]);
+    })
+  })
+
+  // 2nd approach of view all button testing
+  it.only("should have correct view all link (2nd approach)", () => {
+    cy.get('[data-cy="market"]').eq(0).click();
+    cy.get('[data-cy="view_all"]').click();
+    cy.url().should("contain", "markets/forex/")
+    cy.visit("http://localhost:8000/")
+
+
+    cy.get('[data-cy="market"]').eq(1).click();
+    cy.get('[data-cy="view_all"]').click();
+    cy.url().should("contain", "markets/synthetic/")
+    cy.visit("http://localhost:8000/")
+
+
+    cy.get('[data-cy="market"]').eq(2).click();
+    cy.get('[data-cy="view_all"]').click();
+    cy.url().should("contain", "markets/stock/")
+    cy.visit("http://localhost:8000/")
+
+
+    cy.get('[data-cy="market"]').eq(3).click();
+    cy.get('[data-cy="view_all"]').click();
+    cy.url().should("contain", "markets/cryptocurrencies/")
+    cy.visit("http://localhost:8000/")
+
+
+    cy.get('[data-cy="market"]').eq(4).click();
+    cy.get('[data-cy="view_all"]').click();
+    cy.url().should("contain", "markets/commodities/")
+    cy.visit("http://localhost:8000/")
   })
 })

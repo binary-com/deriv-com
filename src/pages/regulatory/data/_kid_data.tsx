0f3e0aa8cc733e5a9353e1971d4223648544cf17
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-09-14 20:13:25 +0400

    fix: etfs regulatory

--- src/pages/regulatory/data/_kid_data.tsx
+++ src/pages/regulatory/data/_kid_data.tsx
@@ -7,17 +7,41 @@ type kidType = {
 
 const kid_data: kidType[] = [
     {
-        title: '_t_Commodities_t_',
-        ref: 'kid_deriv_commodities.pdf',
-    },
-    {
-        title: '_t_Forex_t_',
+        title: '_t_CFDs - Forex_t_',
         ref: 'kid_deriv_forex.pdf',
     },
     {
-        title: '_t_Cryptocurrencies_t_',
+        title: '_t_CFDs - Stocks_t_',
+        ref: 'kid_deriv_Stocks.pdf',
+    },
+
+    {
+        title: '_t_CFDs - Stock indices_t_',
+        ref: 'kid_deriv_Stock Indices.pdf',
+    },
+    {
+        title: '_t_CFDs - Cryptocurrencies_t_',
         ref: 'kid_deriv_crypto.pdf',
     },
+    {
+        title: '_t_CFDs - Commodities_t_',
+        ref: 'kid_deriv_commodities.pdf',
+    },
+    {
+        title: '_t_CFDs - synthetics: Volatility 250 (1s) Index_t_',
+        ref: 'kid_deriv_CFD_synthetic_vol_250.pdf',
+    },
+
+    {
+        title: '_t_CFDs - synthetics: Crash 300 Index_t_',
+        ref: 'kid_deriv_CFD_synthetic_crash_300.pdf',
+    },
+    {
+        title: '_t_CFDs - ETFs_t_',
+        ref: 'kid_deriv_cfds_etfs.pdf',
+    },
+]
+const kid_data_multiplier = [
     {
         title: '_t_Multipliers - forex_t_',
         ref: 'kid_deriv_multipliers_forex.pdf',
@@ -34,22 +58,6 @@ const kid_data: kidType[] = [
         title: '_t_Multipliers - synthetics: Crash 300 Index_t_',
         ref: 'kid_deriv_multipliers_synthetics_crash_300.pdf',
     },
-    {
-        title: '_t_Stocks_t_',
-        ref: 'kid_deriv_Stocks.pdf',
-    },
-    {
-        title: '_t_Stock indices_t_',
-        ref: 'kid_deriv_Stock Indices.pdf',
-    },
-    {
-        title: '_t_CFDs - synthetics: Volatility 250 (1s) Index_t_',
-        ref: 'kid_deriv_CFD_synthetic_vol_250.pdf',
-    },
-    {
-        title: '_t_CFDs - synthetics: Crash 300 Index_t_',
-        ref: 'kid_deriv_CFD_synthetic_crash_300.pdf',
-    },
 ]
 
-export default kid_data
+export { kid_data, kid_data_multiplier }

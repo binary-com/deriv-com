df844952d15ab150ae7e723bf5ef9890c24646d9
Jia Wei <jiawei@deriv.com>
2023-09-18 18:10:49 +0800

    feat: add the check trading specs button on the etfs market page

---
+++ src/features/pages/markets/etf/check-trading/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Flex from 'features/components/atoms/flex-box'
+import { Localize } from 'components/localization'
+import LinkButton from 'features/components/atoms/link-button'
+import Typography from 'features/components/atoms/typography'
+
+const CheckTrading = () => {
+    return (
+        <Flex.Box
+            direction="col"
+            container="fluid"
+            justify="center"
+            align="center"
+            pb="10x"
+            md={{ pb: '40x', mb: '20x' }}
+        >
+            <Typography.Paragraph mb="10x" textcolor="black" align="center">
+                <Localize translate_text="_t_Want to know more about CFD trading conditions for the instruments we offer?_t_" />
+            </Typography.Paragraph>
+            <LinkButton.Primary
+                font_family="UBUNTU"
+                size="medium"
+                aria-label="check trading specs"
+                url={{
+                    type: 'internal',
+                    to: '/trading-specification',
+                }}
+            >
+                <Localize translate_text="_t_Check trading specs_t_" />
+            </LinkButton.Primary>
+        </Flex.Box>
+    )
+}
+
+export default CheckTrading

d843c4a577b13c80040090e72f0f730cb2c8d051
Niloofar Sadeghi <93518187+niloofar-deriv@users.noreply.github.com>
2023-09-19 13:03:02 +0330

    Niloofar Sadeghi / Node 18, React 18, Gatsby 5 migration (#5381)
    * refactor: node18, react18 and gatsby5 migration
    
    * fix: robot.txt file is changed when compared from production
    
    * refactor: replace react helmet with gatsby head api
    
    * fix: page direction issue
    
    * fix: glitches in market page
    
    * build: triger build
    
    * refactor: replace helmet with gatsby head in options pages
    
    * build: regenerate package-lock
    
    * docs: added react 18 development guide
    
    * docs: update documentation
    
    * fix: options page error
    
    * fix: fix issue where gatsby 5 bundles all files into one chunk
    
    * build: trigger build
    
    * fix: build error
    
    * fix: createroot not found issue
    
    * fix: scroll issue in signup and check email
    
    * fix: seo issue in some of the routes
    
    * fix: scroll issue in deriv prime and markets
    
    * fix: seo issue
    
    * fix: seo issue in 404 and some other pages
    
    * fix: seo script issues
    
    * fix: seo issues
    
    * fix: eu site has extra credit & debit cards section
    
    * fix: search input auto focus issue
    
    * fix: search input auto focus issue
    
    * build: trigger build
    
    ---------
    
    Co-authored-by: niloofar sadeghi <niloofar.sadeghi@re-work.dev>
    Co-authored-by: adrienne-rio <adrienne@deriv.com>

--- src/pages/help-centre/dbot.tsx
+++ src/pages/help-centre/dbot.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 import QuestionsTemplate from './components/_questions-template'
 import dbot from './data/_dbot'
 import { WithIntl } from 'components/localization'
+import { SEO } from 'components/containers'
+import { TGatsbyHead } from 'features/types'
 
 const DBot = () => <QuestionsTemplate data={dbot} />
 
 export default WithIntl()(DBot)
+
+export const Head = ({ pageContext }: TGatsbyHead) => (
+    <SEO
+        title="_t_Help centre | Frequently asked questions | DBot | Deriv_t_"
+        description="_t_Frequently asked questions - DBot_t_"
+        pageContext={pageContext}
+    />
+)

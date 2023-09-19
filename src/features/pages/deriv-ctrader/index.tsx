7b7c35a6234bba67c876fea063b4ae31822a0595
aswathy-deriv <aswathy@deriv.com>
2023-09-19 15:52:32 +0400

    fix: changed seo to the main page and added pageContext

--- src/features/pages/deriv-ctrader/index.tsx
+++ src/features/pages/deriv-ctrader/index.tsx
@@ -10,8 +10,7 @@ import OtherPlatforms from './other-platforms'
 import MainNav from 'features/components/templates/navigation/main-nav'
 import Footer from 'features/components/templates/footer/'
 import Layout from 'features/components/templates/layout'
-import { SEO } from 'components/containers'
-import { localize, WithIntl } from 'components/localization'
+import { WithIntl } from 'components/localization'
 
 const DerivCTrader = () => {
     const is_ctrader = false
@@ -19,12 +18,6 @@ const DerivCTrader = () => {
     return (
         <Layout>
             <MainNav />
-            <SEO
-                title={localize('_t_cTrader – a CFD trading platform | Deriv_t_')}
-                description={localize(
-                    '_t_cTrader is our intuitive CFD trading platform where you can trade forex, commodities, stocks & indices, cryptocurrencies, and derived indices._t_',
-                )}
-            />
             <CTraderHero />
             <CTraderNumbers />
             <CTraderTradeDescription />
--- src/pages/deriv-ctrader/index.tsx
+++ src/pages/deriv-ctrader/index.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import PageNotFound from '../404'
 import DerivCTrader from 'features/pages/deriv-ctrader'
 import useRegion from 'components/hooks/use-region'
-import { WithIntl } from 'components/localization'
+import { WithIntl, localize } from 'components/localization'
+import SEO from 'components/containers/seo'
+import { TGatsbyHead } from 'features/types'
 
 const CTraderPage = () => {
     const { is_row } = useRegion()
@@ -13,3 +15,13 @@ const CTraderPage = () => {
 }
 
 export default WithIntl()(CTraderPage)
+
+export const Head = ({ pageContext }: TGatsbyHead) => (
+    <SEO
+        title={localize('_t_cTrader – a CFD trading platform | Deriv_t_')}
+        description={localize(
+            '_t_cTrader is our intuitive CFD trading platform where you can trade forex, commodities, stocks & indices, cryptocurrencies, and derived indices._t_',
+        )}
+        pageContext={pageContext}
+    />
+)

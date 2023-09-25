28573abccd82793828b965a0b7c1605dec31e191
deriv <nikita.kolesnik@regentmarkets.com>
2023-09-25 15:07:42 +0300

    fix: resolve build issue

--- src/features/hooks/analytic/use-growthbook/index.ts
+++ src/features/hooks/analytic/use-growthbook/index.ts
@@ -1,16 +1,17 @@
 import { useRef } from 'react'
 import { GrowthBook } from '@growthbook/growthbook-react'
+import { isBrowser } from 'common/utility'
 
 export const useGrowthBook = () => {
     const growthbook = useRef<GrowthBook>()
-    growthbook.current = window._growthbook
+    growthbook.current = isBrowser() && window._growthbook
 
     // calls tracking feature trackingCallback()
-    growthbook.current.isOn('test-toggle-aa-test')
+    isBrowser() && growthbook.current?.isOn('test-toggle-aa-test')
 
     // fetch feature flag from growthbook by name
     const test_toggle_aa_test =
-        growthbook.current.context.features['test-toggle-aa-test'].defaultValue
+        growthbook.current?.context?.features?.['test-toggle-aa-test']?.defaultValue
 
     return {
         test_toggle_aa_test,

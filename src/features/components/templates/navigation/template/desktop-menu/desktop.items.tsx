0d2abd04110868bc87171880ada3c34e5bcc21f1
meenakshi-deriv <104189801+meenakshi-deriv@users.noreply.github.com>
2023-09-28 17:56:30 +0400

    Meenu/Translation 92 page ctrader trade platform (#5071)
    * auto-pull-translation
    
    * auto-pull-translation
    
    * fix: test
    
    * fix: initial commit
    
    * fix: product page
    
    * fix: banner fixes
    
    * fix: products available on added
    
    * fix: comments fixes
    
    * fix: bug fixes
    
    * fix: render issue in mobile in tab
    
    * fix: removed ternory and added switch case
    
    * auto-pull-translation
    
    * fix: added hoc
    
    * fix: updated dependency
    
    * auto-pull-translation
    
    * translation: 📚 sync translations from crowdin feature branch
    
    * fix: banner issue arab
    
    * fix: scss change
    
    * fix: empty line removed
    
    * feat: created carousel file
    
    * feat: added icons
    
    * feat: added platform component
    
    * Update src/features/components/templates/trade-platform/other-platforms/styles.module.scss
    
    Co-authored-by: konstantinosG-deriv <104083272+konstantinosG-deriv@users.noreply.github.com>
    
    * Update src/features/components/templates/trade-platform/other-platforms/index.tsx
    
    Co-authored-by: mayuran-deriv <129507167+mayuran-deriv@users.noreply.github.com>
    
    * fix: pr suggestions
    
    * fix: swiper btn's css
    
    * fix: radius issue fix
    
    * fix: pr comments
    
    * fix: integrated other platform section
    
    * fix: log mobile or tab
    
    * fix: responsive tab fixes
    
    * fix: step 2 real image
    
    * fix: decrease other platform carousel's speed
    
    * fix: align issue in markets
    
    * fix: added tab radius and updated urls
    
    * fix: added ctrader in available platform
    
    * fix: tab button fix
    
    * fix: mobile spacing issue
    
    * fix: fixed padding
    
    * fix: padding fix
    
    * fix: ctrader on eu
    
    * fix: hidden some ctrader sections and added coming soon tag
    
    * fix: review comments
    
    * auto-pull-translation
    
    * translation: 📚 sync translations from crowdin feature branch
    
    * fix: added the container fluid
    
    * empty: :rocket: to redeploy and trigger build
    
    * fix: changes in ctrader page
    
    * empty: :rocket: to redeploy and trigger build
    
    * empty: :rocket: to redeploy and trigger build
    
    * feat: added ctrader in the navigation and footer
    
    * fix: changed seo to the main page and added pageContext
    
    * fix: peer comments
    
    ---------
    
    Co-authored-by: meenu-deriv <meenakshi@deriv.com>
    Co-authored-by: mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
    Co-authored-by: DerivFE <80095553+DerivFE@users.noreply.github.com>
    Co-authored-by: hasan-deriv <abuhasan@deriv.com>
    Co-authored-by: Hasan Mobarak <126637868+hasan-deriv@users.noreply.github.com>
    Co-authored-by: konstantinosG-deriv <104083272+konstantinosG-deriv@users.noreply.github.com>
    Co-authored-by: mayuran-deriv <129507167+mayuran-deriv@users.noreply.github.com>
    Co-authored-by: aswathy-deriv <aswathy@deriv.com>
    Co-authored-by: Spare <spare@Deriv-Laptops-MacBook-Pro-C02DXPUNMD6M-Standard.local>

--- src/features/components/templates/navigation/template/desktop-menu/desktop.items.tsx
+++ src/features/components/templates/navigation/template/desktop-menu/desktop.items.tsx
@@ -4,17 +4,23 @@ import { SmartNavDropItems } from '../../types'
 import NavDropItems from '../nav-drop-items'
 import { Localize } from 'components/localization'
 import Typography from 'features/components/atoms/typography'
+import useRegion from 'components/hooks/use-region'
+import dclsx from 'features/utils/dclsx'
 
 interface DesktopItemsProps {
     item: SmartNavDropItems
+    active: string
 }
 
-const NavDesktopItem = ({ item }: DesktopItemsProps) => {
+const NavDesktopItem = ({ item, active }: DesktopItemsProps) => {
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         if (!window.matchMedia('(hover: none)').matches) {
             e.preventDefault()
         }
     }
+
+    const { is_row } = useRegion()
+    const has_scroll_menu = active === '_t_Trade_t_' && is_row
 
     return (
         <NavigationMenu.Item value={item.data.title}>
@@ -23,7 +29,11 @@ const NavDesktopItem = ({ item }: DesktopItemsProps) => {
                     <Localize translate_text={item.data.title} />
                 </Typography.Paragraph>
             </NavigationMenu.Trigger>
-            <NavigationMenu.Content className="navigation_content">
+            <NavigationMenu.Content
+                className={dclsx('navigation_content', {
+                    navigation_scrollable_menu: has_scroll_menu,
+                })}
+            >
                 <NavDropItems items={item.data} />
             </NavigationMenu.Content>
         </NavigationMenu.Item>

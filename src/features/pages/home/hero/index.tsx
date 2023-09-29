3f1d7ee46513e8c15ecf4ccd5fd0395844883ec5
hasan-deriv <abuhasan@deriv.com>
2023-09-29 11:37:15 +0800

    fix: update package

--- src/features/pages/home/hero/index.tsx
+++ src/features/pages/home/hero/index.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react'
+import React from 'react'
 import loadable from '@loadable/component'
 import { StaticImage } from 'gatsby-plugin-image'
 import pMinDelay from 'p-min-delay'
@@ -8,40 +8,37 @@ import Flex from 'features/components/atoms/flex-box'
 import ProductHeroContainer from 'features/components/templates/hero-banners/product'
 import useRegion from 'components/hooks/use-region'
 
-const HomeHeroSlider = lazy(() => pMinDelay(import('./slider'), 4000))
-const HomeHeroSliderEu = lazy(() => pMinDelay(import('./slider'), 4000))
+const HomeHeroSlider = loadable(() => pMinDelay(import('./slider'), 4000), {
+    fallback: (
+        <SliderWrapper>
+            <StaticImage
+                objectFit="fill"
+                src="../../../../images/common/home/hero_1.png"
+                alt="person-hero-1"
+                formats={['avif', 'webp', 'auto']}
+                loading="eager"
+                quality={50}
+                placeholder="blurred"
+            />
+        </SliderWrapper>
+    ),
+})
 
-// const HomeHeroSlider = loadable(() => pMinDelay(import('./slider'), 4000), {
-//     fallback: (
-//         <SliderWrapper>
-//             <StaticImage
-//                 objectFit="contain"
-//                 src="../../../../images/common/home/hero_1.png"
-//                 alt="person-hero-1"
-//                 formats={['avif', 'webp', 'auto']}
-//                 loading="eager"
-//                 quality={100}
-//                 placeholder="none"
-//             />
-//         </SliderWrapper>
-//     ),
-// })
-
-// const HomeHeroSliderEu = loadable(() => pMinDelay(import('./slider'), 4000), {
-//     fallback: (
-//         <SliderWrapper>
-//             <StaticImage
-//                 objectFit="contain"
-//                 src="../../../../images/common/home/eu_hero_person_5.png"
-//                 alt="person-hero-1"
-//                 formats={['avif', 'webp', 'auto']}
-//                 loading="eager"
-//                 quality={100}
-//                 placeholder="none"
-//             />
-//         </SliderWrapper>
-//     ),
-// })
+const HomeHeroSliderEu = loadable(() => pMinDelay(import('./slider'), 4000), {
+    fallback: (
+        <SliderWrapper>
+            <StaticImage
+                objectFit="fill"
+                src="../../../../images/common/home/eu_hero_person_5.png"
+                alt="person-hero-1"
+                formats={['avif', 'webp', 'auto']}
+                loading="eager"
+                quality={50}
+                placeholder="blurred"
+            />
+        </SliderWrapper>
+    ),
+})
 
 const HomeHero = () => {
     const { is_eu } = useRegion()
@@ -67,43 +64,7 @@ const HomeHero = () => {
                 }}
             >
                 <HomeHeroContent />
-                {is_eu ? (
-                    <Suspense
-                        fallback={
-                            <SliderWrapper>
-                                <StaticImage
-                                    objectFit="fill"
-                                    src="../../../../images/common/home/eu_hero_person_5.png"
-                                    alt="person-hero-1"
-                                    formats={['avif', 'webp', 'auto']}
-                                    loading="eager"
-                                    quality={50}
-                                    placeholder="blurred"
-                                />
-                            </SliderWrapper>
-                        }
-                    >
-                        <HomeHeroSliderEu />
-                    </Suspense>
-                ) : (
-                    <Suspense
-                        fallback={
-                            <SliderWrapper>
-                                <StaticImage
-                                    objectFit="fill"
-                                    src="../../../../images/common/home/hero_1.png"
-                                    alt="person-hero-1"
-                                    formats={['avif', 'webp', 'auto']}
-                                    loading="eager"
-                                    quality={50}
-                                    placeholder="blurred"
-                                />
-                            </SliderWrapper>
-                        }
-                    >
-                        <HomeHeroSlider />
-                    </Suspense>
-                )}
+                {is_eu ? <HomeHeroSliderEu /> : <HomeHeroSlider />}
             </Flex.Box>
         </ProductHeroContainer>
     )

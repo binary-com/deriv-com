0200c906d8f0118e8559e7603fd9c25e238e9071
hasan-deriv <abuhasan@deriv.com>
2023-09-29 14:15:16 +0800

    fix: remove replace hydration method

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
 import { WrapPagesWithLocaleContext } from './src/components/localization'
 import { isProduction } from './src/common/websocket/config'
 import { LocalStore } from './src/common/storage'
@@ -142,11 +142,11 @@ export const onRouteUpdate = () => {
     }, 1500)
 }
 
-export const replaceHydrateFunction = () => {
-    return (element, container) => {
-        const root = createRoot(container)
-        root.render(element)
-    }
-}
+// export const replaceHydrateFunction = () => {
+//     return (element, container) => {
+//         const root = createRoot(container)
+//         root.render(element)
+//     }
+// }
 
 export const wrapPageElement = WrapPagesWithLocaleContext

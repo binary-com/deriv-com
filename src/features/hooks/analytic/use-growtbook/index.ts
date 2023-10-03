3524a1b799418548b3bc42528a46a3046c64f283
deriv <nikita.kolesnik@regentmarkets.com>
2023-10-03 10:43:13 +0300

    chore: cleanup

--- src/features/hooks/analytic/use-growtbook/index.ts
+++ src/features/hooks/analytic/use-growtbook/index.ts
@@ -1,6 +1,5 @@
 import { useRef } from 'react'
 import { GrowthBook } from '@growthbook/growthbook-react'
-import { RudderStack } from '@deriv/analytics'
 import { isBrowser } from 'common/utility'
 
 export const useGrowthBook = () => {
@@ -10,9 +9,6 @@ export const useGrowthBook = () => {
 
     // calls tracking feature trackingCallback()
     const test_toggle_aa_test = isBrowser() && growthbook?.evalFeature('test-toggle-aa-test')
-
-    console.log('Rudderstack', RudderStack)
-    console.log('test_toggle_aa_test', test_toggle_aa_test)
 
     return {
         test_toggle_aa_test: test_toggle_aa_test?.experimentResult?.name,

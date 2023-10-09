e05ca7fa6b3039ce77788ae0b3a3ddc04587efa9
hasan-deriv <abuhasan@deriv.com>
2023-10-09 11:35:12 +0800

    fix: cleanup

--- src/pages/partners/affiliate-ib/_deriv-affiliate-programme.tsx
+++ src/pages/partners/affiliate-ib/_deriv-affiliate-programme.tsx
@@ -1,24 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
-import RevenueShare from './_revenue-share'
-import Turnover from './_turnover'
+import RevenueShareCard from './_revenue-share-card'
+import TurnoverCard from './_turnover-card'
 import CpaEuOnlyCard from './_cpa-eu-only-card'
-import { Card, CardWrapper } from './_partner-card'
-import { Table, TRAP, TRAPREVERSE, TC } from './_table'
+import { CardWrapper } from './_partner-card'
 import { SectionContainer, Container } from 'components/containers'
-import { Header, Text } from 'components/elements/typography'
+import { Header } from 'components/elements/typography'
 import { Localize } from 'components/localization'
-import { LinkButton, Button } from 'components/form'
+import { LinkButton } from 'components/form'
 import { affiliate_signup_url } from 'common/constants'
 import device from 'themes/device'
-import { TString } from 'types/generics'
 import useRegion from 'components/hooks/use-region'
-import { Li, Ul } from 'components/elements'
-
-type AffiliateType = {
-    title: TString
-    data: TString[]
-}[]
 
 type ApplyNowProps = {
     mt_mobile?: string
@@ -74,15 +66,6 @@ const ComissionPlanContainer = styled(SectionContainer)`
     }
 `
 
-const StyledText = styled(Text)`
-    font-size: 1.4rem;
-    line-height: 1.5;
-    text-align: center;
-    @media ${device.tabletL} {
-        font-size: 14px;
-    }
-`
-
 const StyledHeader = styled(Header)`
     @media (max-width: 1524px) {
         text-align: start;
@@ -109,77 +92,10 @@ const ApplyNow = styled(LinkButton)<ApplyNowProps>`
     }
 `
 
-const CPAContent = styled.div`
-    ${Text} {
-        margin-top: 2.4rem;
-    }
-`
-
-const StyledCard = styled(Card)`
-    min-width: calc((100% - 4.8rem) / 3);
-    width: calc((100% - 4.8rem) / 3);
-    border-radius: 8px;
-
-    :nth-child(2) {
-        margin: 1.6rem 2.8rem 0 2rem;
-
-        @media ${device.tabletL} {
-            margin: 20px 0 11px;
-        }
-    }
-
-    @media (max-width: 1313px) {
-        min-height: 63rem;
-    }
-
-    @media ${device.laptopM} {
-        min-width: 38.4rem;
-        width: 38.4rem;
-    }
-
-    @media ${device.tabletL} {
-        min-width: unset;
-        padding: 24px 16px;
-        min-height: auto;
-        ${(props) => props.tabletHeight && 'height:' + props.tabletHeight};
-
-        :first-child {
-            margin-top: 16px;
-        }
-        :last-child {
-            margin-top: 0;
-        }
-    }
-
-    @media ${device.mobileM} {
-        min-width: unset;
-        width: 100%;
-    }
-`
-const StyledTrap = styled(TRAP)`
-    background-color: var(--color-grey-39);
-    padding: 1rem 0.8rem;
-    height: 40px;
-    border-bottom: none;
-`
-
 const StyledSignupBtnWrap = styled.div`
     display: flex;
     justify-content: center;
     text-align: center;
-`
-
-const StyledCalculatedButton = styled(Button)`
-    display: block;
-    width: 100%;
-    border-radius: 4px;
-    margin-block-start: 24px;
-    padding-block: 7px;
-
-    @media ${device.tabletL} {
-        height: 40px;
-        margin-top: 40px;
-    }
 `
 
 const DerivAffiliateProgramme = () => {
@@ -208,8 +124,8 @@ const DerivAffiliateProgramme = () => {
                     <CardWrapper>
                         {!is_eu && (
                             <>
-                                <RevenueShare />
-                                <Turnover />
+                                <RevenueShareCard />
+                                <TurnoverCard />
                             </>
                         )}
                         <CpaEuOnlyCard />

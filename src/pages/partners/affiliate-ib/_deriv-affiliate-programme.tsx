7a0172d011a7ba2479581a5709efed386cb23070
hasan-deriv <abuhasan@deriv.com>
2023-10-09 11:24:06 +0800

    feat: created a different component for turnover card

--- src/pages/partners/affiliate-ib/_deriv-affiliate-programme.tsx
+++ src/pages/partners/affiliate-ib/_deriv-affiliate-programme.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import RevenueShare from './_revenue-share'
+import Turnover from './_turnover'
 import { Card, CardWrapper } from './_partner-card'
 import { Table, TRAP, TRAPREVERSE, TC } from './_table'
 import { SectionContainer, Container } from 'components/containers'
@@ -180,23 +181,6 @@ const StyledCalculatedButton = styled(Button)`
     }
 `
 
-const Turnover: AffiliateType = [
-    {
-        title: '_t_Probability of return_t_',
-        data: [
-            '_t_0–19.999%_t_',
-            '_t_20–39.999%_t_',
-            '_t_40–59.999%_t_',
-            '_t_60–79.999%_t_',
-            '_t_80–94.999%_t_',
-            '_t_95% and above_t_',
-        ],
-    },
-    {
-        title: '_t_Commission_t_',
-        data: ['_t_1.5%_t_', '_t_1%_t_', '_t_0.75%_t_', '_t_0.5%_t_', '_t_0.4%_t_', '_t_0%_t_'],
-    },
-]
 const DerivAffiliateProgramme = () => {
     const { is_eu } = useRegion()
     return (
@@ -224,81 +208,7 @@ const DerivAffiliateProgramme = () => {
                         {!is_eu && (
                             <>
                                 <RevenueShare />
-                                <StyledCard height="auto" tabletHeight="auto" padding="2.4rem">
-                                    <div>
-                                        <Header as="h4" type="sub-section-title" mb="0.8rem">
-                                            <Localize translate_text="_t_Turnover_t_" />
-                                        </Header>
-                                        <Text>
-                                            <Localize
-                                                translate_text="_t_<0>Options:</0> Earn based on each contract’s payout probability._t_"
-                                                components={[<strong key={0} />]}
-                                            />
-                                        </Text>
-                                        <Table grid_col_number={2}>
-                                            {Turnover.map(({ title, data }, index) => (
-                                                <TC grid_area={'area' + index} key={index}>
-                                                    <StyledTrap isTitle="true">
-                                                        <StyledText weight="bold">
-                                                            <Localize translate_text={title} />
-                                                        </StyledText>
-                                                    </StyledTrap>
-                                                    {data.map((data, id) => (
-                                                        <TRAPREVERSE
-                                                            even={id % 2 ? 'true' : ''}
-                                                            key={id}
-                                                        >
-                                                            <StyledText>
-                                                                <Localize translate_text={data} />
-                                                            </StyledText>
-                                                        </TRAPREVERSE>
-                                                    ))}
-                                                </TC>
-                                            ))}
-                                        </Table>
-                                        <Text mt="2.4rem">
-                                            <Localize
-                                                translate_text="_t_<0>Multipliers:</0> Earn 40% of the commissions generated from your clients’ trades._t_"
-                                                components={[<strong key={0} />]}
-                                            />
-                                        </Text>
-                                        <Text mt="2.4rem">
-                                            <Localize
-                                                translate_text="_t_<0>Lookbacks:</0> Earn 0.8% on the stake of each lookbacks trade on SmartTrader._t_"
-                                                components={[<strong key={0} />]}
-                                            />
-                                        </Text>
-                                        <Text mb="0.8rem" mt="2.4rem">
-                                            <Localize
-                                                translate_text="_t_<0>Disclaimer:</0>_t_"
-                                                components={[<strong key={0} />]}
-                                            />
-                                        </Text>
-                                        <Ul>
-                                            <Li>
-                                                <Text>
-                                                    <Localize
-                                                        translate_text="_t_<0>This plan is not available for affiliates who promote to clients residing in the EU.</0>_t_"
-                                                        components={[<strong key={0} />]}
-                                                    />
-                                                </Text>
-                                            </Li>
-                                            <Li>
-                                                <Text>
-                                                    <Localize
-                                                        translate_text="_t_<0>Affiliates residing in the EU may sign up for the Turnover plan. However, only clients residing outside of the EU can be referred under this plan.</0>_t_"
-                                                        components={[<strong key={0} />]}
-                                                    />
-                                                </Text>
-                                            </Li>
-                                        </Ul>
-                                    </div>
-                                    <div>
-                                        <StyledCalculatedButton secondary>
-                                            <Localize translate_text="_t_How it’s calculated_t_" />
-                                        </StyledCalculatedButton>
-                                    </div>
-                                </StyledCard>
+                                <Turnover />
                             </>
                         )}
                         <StyledCard height="auto" tabletHeight="auto" padding="2.4rem">

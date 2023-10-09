3bd3b20697e60494ce98f1e9369e52256f489639
hasan-deriv <abuhasan@deriv.com>
2023-10-09 18:35:35 +0800

    feat: cpa calculate data added

--- src/pages/partners/affiliate-ib/cpa-eu-only/_calculated-card.tsx
+++ src/pages/partners/affiliate-ib/cpa-eu-only/_calculated-card.tsx
@@ -15,18 +15,28 @@ const CalculatedCard = ({ toggleCalculated }: { toggleCalculated: () => void }) 
                     <Localize translate_text="_t_This plan is not available for affiliates who promote to clients residing in the EU._t_" />
                 </Header>
                 <Header as="h2" type="paragraph-2" mt="1.6rem" mb="0.8rem">
+                    <Localize translate_text="_t_Earn based on each successful referral._t_" />
+                </Header>
+                <Ul>
+                    <Li>
+                        <Header as="p" weight="normal">
+                            <Localize translate_text="_t_You earn USD 100 when a referred client successfully deposits a one-time or cumulative total of USD 100 into their Deriv account._t_" />
+                        </Header>
+                        <Header as="p" weight="normal" mt="2.4rem">
+                            <Localize translate_text="_t_This plan is available exclusively for EU-based clients. Please note that according to regulations, you cannot have clients who reside in Portugal or Spain._t_" />
+                        </Header>
+                    </Li>
+                </Ul>
+                <Header as="h2" type="paragraph-2" mt="1.6rem" mb="0.8rem">
                     <Localize translate_text="_t_Example 1:_t_" />
                 </Header>
                 <Ul>
                     <Li>
                         <Header as="p" weight="normal">
-                            <Localize translate_text="_t_A sub-affiliate generates USD 300 worth of commission. Here is how we calculate their total commissions:_t_" />
+                            <Localize translate_text="_t_A referred client based in the EU deposits USD 100 in one transaction_t_" />
                         </Header>
                         <Header as="p" weight="normal" mt="2.4rem">
-                            <Localize translate_text="_t_Sub affiliate’s total commission: USD 300_t_" />
-                        </Header>
-                        <Header as="p" weight="normal" mt="2.4rem">
-                            <Localize translate_text="_t_Master affiliate’s total commission: USD 300 x 20% = USD 60_t_" />
+                            <Localize translate_text="_t_Total commission earned: USD 100_t_" />
                         </Header>
                     </Li>
                 </Ul>
@@ -36,31 +46,35 @@ const CalculatedCard = ({ toggleCalculated }: { toggleCalculated: () => void }) 
                 <Ul>
                     <Li>
                         <Header as="p" weight="normal">
-                            <Localize translate_text="_t_A sub-affiliate earns USD 200 from their Revenue Share plan. The same sub-affiliate earns USD 100 from their IB programme. Here is how we calculate their total commissions:_t_" />
+                            <Localize translate_text="_t_A referred client based in the EU deposits USD 50 in one transaction. Commission = USD 0_t_" />
                         </Header>
                         <Header as="p" weight="normal" mt="2.4rem">
-                            <Localize translate_text="_t_Sub-affiliate’s total commission: 200 + 100 = USD 300 (including IB commission)_t_" />
+                            <Localize translate_text="_t_The same EU client deposits USD 50 in another transaction_t_" />
                         </Header>
                         <Header as="p" weight="normal" mt="2.4rem">
-                            <Localize translate_text="_t_Master affiliate’s total commission: USD 200 * 20% = USD 40_t_" />
+                            <Localize translate_text="_t_Total commission earned: USD 100_t_" />
                         </Header>
                     </Li>
                 </Ul>
                 <Header as="h2" type="paragraph-2" mt="1.6rem" mb="0.8rem">
-                    <Localize translate_text="_t_Disclaimer:_t_" />
+                    <Localize translate_text="_t_Example 3:_t_" />
                 </Header>
                 <Ul>
                     <Li>
-                        <Header as="p">
-                            <Localize translate_text="_t_Master affiliates will not earn based on the sub-affiliates’ IB programme_t_" />
+                        <Header as="p" weight="normal">
+                            <Localize translate_text="_t_A referred client based in the EU deposits USD 200 in one transaction._t_" />
                         </Header>
-                    </Li>
-                    <Li>
-                        <Header as="p" mt="0.8">
-                            <Localize translate_text="_t_You need to be a registered affiliate with Deriv before signing up as a master affiliate. (To sign up as a master affiliate, contact your respective account manager.)_t_" />
+                        <Header as="p" weight="normal" mt="2.4rem">
+                            <Localize translate_text="_t_Total commission earned: USD 100_t_" />
                         </Header>
                     </Li>
                 </Ul>
+                <Header as="p" type="paragraph-2" weight="normal" mt="1.6rem">
+                    <Localize translate_text="_t_(The CPA commission is a one-time payment.)_t_" />
+                </Header>
+                <Header as="h2" type="paragraph-2" mt="1.6rem" mb="0.8rem">
+                    <Localize translate_text="_t_Disclaimer: Affiliates not residing in the EU may sign up for the CPA plan._t_" />
+                </Header>
             </div>
             <div>
                 <StyledBackButton tertiary onClick={toggleCalculated} mt="2.4rem">

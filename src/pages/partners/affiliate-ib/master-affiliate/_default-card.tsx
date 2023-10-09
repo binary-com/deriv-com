23b74080197b8f42489c3a11756fe26745e76a1f
hasan-deriv <abuhasan@deriv.com>
2023-10-09 18:39:40 +0800

    fix: cleanup

--- src/pages/partners/affiliate-ib/master-affiliate/_default-card.tsx
+++ src/pages/partners/affiliate-ib/master-affiliate/_default-card.tsx
@@ -1,26 +1,7 @@
 import React from 'react'
-import { StyledCard, StyledCalculatedButton, StyledTrap, StyledText } from '../_style'
-import { Table, TRAPREVERSE, TC } from '../_table'
+import { StyledCard, StyledCalculatedButton } from '../_style'
 import { Localize } from 'components/localization'
-import { Header, Text } from 'components/elements/typography'
-import { Li, Ul } from 'components/elements'
-import { TString } from 'types/generics'
-
-type AffiliateType = {
-    title: TString
-    data: TString[]
-}[]
-
-const revenue_data: AffiliateType = [
-    {
-        title: '_t_Net revenue_t_',
-        data: ['_t_USD ≤ 20,000 per month_t_', '_t_USD > 20,000 per month_t_'],
-    },
-    {
-        title: '_t_Commission_t_',
-        data: ['_t_30%_t_', '_t_45%_t_'],
-    },
-]
+import { Header } from 'components/elements/typography'
 
 const DefaultCard = ({ toggleCalculated }: { toggleCalculated: () => void }) => {
     return (
@@ -35,12 +16,9 @@ const DefaultCard = ({ toggleCalculated }: { toggleCalculated: () => void }) => 
                 <Header as="p" weight="normal" type="paragraph-1" mt="2.4rem">
                     <Localize translate_text="_t_You will earn 20% flat rate of what your sub-affiliate generates from their commission._t_" />
                 </Header>
-                <Text mb="0.8rem" mt="2.4rem">
-                    <Localize
-                        translate_text="_t_<0>Disclaimer:</0>_t_"
-                        components={[<strong key={0} />]}
-                    />
-                </Text>
+                <Header as="p" type="paragraph-1" mb="0.8rem" mt="2.4rem">
+                    <Localize translate_text="_t_Disclaimer:_t_" />
+                </Header>
                 <Header as="p" type="paragraph-1">
                     <Localize translate_text="_t_Only affiliates with Revenue share and/or Turnover plans are eligible to apply to become Master Affiliate._t_" />
                 </Header>

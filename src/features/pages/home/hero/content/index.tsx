b7e046c7797e7c629bacab36e8140f9dd8b82883
deriv <nikita.kolesnik@regentmarkets.com>
2023-10-10 10:37:49 +0300

    chore: vercel issue

--- src/features/pages/home/hero/content/index.tsx
+++ src/features/pages/home/hero/content/index.tsx
@@ -20,7 +20,7 @@ const HeroFeaturesCarousel = loadable(() => pMinDelay(import('./hero-features.ca
 })
 
 const HomeHeroContent = () => {
-    const { ab } = Analytics.getInstances()
+    const { ab } = Analytics?.getInstances()
     const test_toggle_aa_test = Analytics?.getFeatureIsOn('test-toggle-aa-test')
     console.log(ab)
     console.log('test_toggle_aa_test:', test_toggle_aa_test)

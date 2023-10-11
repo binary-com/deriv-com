771f90e85115cd201da14d1dace86ac8d3705b9c
Fasih Ali <fasihali@Fasih-Alis-MacBook-Pro-C02DJ87UMD6M.local>
2023-10-11 14:02:05 +0800

    fix: fixing by removing cookie banner

--- src/features/components/molecules/layout-overlay/index.tsx
+++ src/features/components/molecules/layout-overlay/index.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import pMinDelay from 'p-min-delay'
 import loadable from '@loadable/component'
 import { overlay_container } from './layout-overlay.module.scss'
+import CfdWarningBanner from './cfd-warning-banner'
 import Flex from 'features/components/atoms/flex-box'
 import { useIsRtl } from 'components/hooks/use-isrtl'
 import { getLocationPathname, getLanguage } from 'common/utility'
 
 const LiveChatButton = loadable(() => pMinDelay(import('./live-chat-button'), 5000))
 const WhatsappButton = loadable(() => pMinDelay(import('./whats-app-button'), 5000))
-const CookieBanner = loadable(() => pMinDelay(import('./cookie-banner'), 5000))
+// const CookieBanner = loadable(() => pMinDelay(import('./cookie-banner'), 5000))
 
 const LayoutOverlay = () => {
     const is_rtl = useIsRtl()
@@ -36,13 +37,14 @@ const LayoutOverlay = () => {
                 align="end"
             >
                 <Flex.Box justify={is_rtl ? 'end' : 'start'} basis="6-12" grow={'1'}>
-                    <CookieBanner />
+                    {/* <CookieBanner /> */}
                 </Flex.Box>
                 <Flex.Box direction="col">
                     <LiveChatButton />
                     <WhatsappButton />
                 </Flex.Box>
             </Flex.Box>
+            {isHomePage() && <CfdWarningBanner />}
         </Flex.Box>
     )
 }

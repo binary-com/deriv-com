77a694a64f920f23e03357cf1a42bb77666d10d8
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-10-13 07:27:19 +0000

    Revert "Hasan/fix: hero image flickering (#5716)"
    This reverts commit f07b2191ec734212159b7c85a1b4388dc0b3aa1c.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { createRoot } from 'react-dom/client'
 import { WrapPagesWithLocaleContext } from './src/components/localization'
 import { isProduction } from './src/common/websocket/config'
 import { LocalStore } from './src/common/storage'
@@ -139,6 +140,13 @@ export const onRouteUpdate = () => {
             }),
         })
     }, 1500)
+}
+
+export const replaceHydrateFunction = () => {
+    return (element, container) => {
+        const root = createRoot(container)
+        root.render(element)
+    }
 }
 
 export const wrapPageElement = WrapPagesWithLocaleContext

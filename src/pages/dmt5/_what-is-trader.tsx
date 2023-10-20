4639889a2dd561dac31261191871946817554366
konstantinosG-deriv <104083272+konstantinosG-deriv@users.noreply.github.com>
2023-10-20 14:07:28 +0200

    Konstantinos / etfs on eu (#5695)
    * feat: etfs on eu
    
    * chore: redeploy
    
    ---------
    
    Co-authored-by: Konstantinos-Gk <konstantinos.gkagiannis@deriv.com>
    Co-authored-by: “konstantinosG-deriv” <“konstantinos.gkagiannis@regentmarkets.com”>

--- src/pages/dmt5/_what-is-trader.tsx
+++ src/pages/dmt5/_what-is-trader.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 import { Header } from 'components/elements'
 import { SectionContainer } from 'components/containers'
 import { Localize } from 'components/localization'
-import useRegion from 'components/hooks/use-region'
 import device from 'themes/device'
 
 const Section = styled(SectionContainer)`
@@ -36,15 +35,17 @@ const StyledText = styled(Header)`
 `
 
 const WhatIsTrader = () => {
-    const { is_eu } = useRegion()
-
     return (
         <Section>
             <StyledHeader align="center" mb="1.2rem" as="h2" type="page-title">
                 <Localize translate_text="_t_What is Deriv MT5_t_" />
             </StyledHeader>
             <StyledText max_width="80.2rem" align="center" as="p" type="paragraph-1">
-                <Localize translate_text="_t_Deriv MT5 gives you access to multiple asset classes – forex, stocks & indices, cryptocurrencies, commodities, exchange-traded funds, and derived indices — on a single platform. With exclusive access to innovative trade types, Deriv brings the MT5 experience to a superior level for both new and experienced traders._t_" />
+                <Localize
+                    translate_text={
+                        '_t_Deriv MT5 gives you access to multiple asset classes – forex, stocks & indices, cryptocurrencies, commodities, exchange-traded funds, and derived indices — on a single platform. With exclusive access to innovative trade types, Deriv brings the MT5 experience to a superior level for both new and experienced traders._t_'
+                    }
+                />
             </StyledText>
         </Section>
     )

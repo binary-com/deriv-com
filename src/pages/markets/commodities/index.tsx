714c032929e1551c95366b5baed99a481c2c460d
hasan-deriv <abuhasan@deriv.com>
2023-10-24 20:12:45 +0800

    fix: minified error in market pages

--- src/pages/markets/commodities/index.tsx
+++ src/pages/markets/commodities/index.tsx
@@ -8,12 +8,16 @@ import { WithIntl } from 'components/localization'
 import { SEO } from 'components/containers'
 import useRegion from 'components/hooks/use-region'
 import { TGatsbyHead } from 'features/types'
+import { usePageLoaded } from 'components/hooks/use-page-loaded'
 
 const CommoditiesPage = () => {
     const { is_row } = useRegion()
+    const [is_mounted] = usePageLoaded()
     const hero_description = is_row
         ? '_t_Trade popular assets in the commodities market such as silver, gold, oil, and more without owning the underlying asset. Speculate on the price movements and benefit from our high leverage and competitive spreads._t_'
         : '_t_Trade popular assets in the commodities market such as silver, gold, oil, and more without owning the underlying asset. Speculate on the price movements and benefit from our competitive spreads._t_'
+
+    if (!is_mounted) return null
 
     return (
         <Layout type="noNav">

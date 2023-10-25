4e852ed2b8edfb21bd7e72f9a05230b5596f281f
“konstantinosG-deriv” <konstantinos.gkagiannis@regentmarkets.com>
2023-10-25 12:45:09 +0200

    fix: seo updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,8 @@ export default WithIntl()(Home)
 
 export const Head = ({ pageContext }: TGatsbyHead) => (
     <SEO
-        title="_t_Online trading platform | Forex, commodities, synthetic indices, stocks, and stock indices | Deriv_t_"
-        description="_t_Deriv: an online trading platform available 24/7 with a wide selection of derivatives._t_"
+        title="_t_Online trading platform | Forex, cryptos, stocks | Deriv_t_"
+        description="_t_A global online trading broker for forex, commodities & other financial markets. Sign up for a demo account and join over 2.5 million traders._t_"
         has_organization_schema
         pageContext={pageContext}
     />

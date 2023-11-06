983b6b949fa270816e2291e5cca12b26a818d7b0
aswathy-deriv <aswathy@deriv.com>
2023-11-06 10:30:20 +0400

    feat: changed the redirection of traders hub

--- src/features/components/templates/navigation/main-nav/main-nav-buttons/index.tsx
+++ src/features/components/templates/navigation/main-nav/main-nav-buttons/index.tsx
@@ -7,13 +7,11 @@ import useHandleSignup from 'components/hooks/use-handle-signup'
 import usePpc from 'features/hooks/use-ppc'
 import LanguageSwitcher from 'features/components/molecules/language-switcher'
 import Flex from 'features/components/atoms/flex-box'
-import { handleGetTrading, handleRedirectToTradersHub } from 'components/custom/utils'
-import { getLocationPathname } from 'common/utility'
+import { handleRedirectToTradersHub } from 'components/custom/utils'
 
 const MainNavButtons = () => {
     const [is_logged_in] = useAuthCheck()
     const { is_ppc_redirect } = usePpc()
-    const is_ctrader = getLocationPathname().includes('deriv-ctrader')
 
     const handleLogin = useHandleLogin()
     const handleSignup = useHandleSignup(is_ppc_redirect)
@@ -27,10 +25,7 @@ const MainNavButtons = () => {
             gap="8x"
         >
             {is_logged_in ? (
-                <Button.Primary
-                    onClick={is_ctrader ? handleRedirectToTradersHub : handleGetTrading}
-                    outlined
-                >
+                <Button.Primary onClick={handleRedirectToTradersHub} outlined>
                     <Localize translate_text="_t_Get trading_t_" />
                 </Button.Primary>
             ) : (

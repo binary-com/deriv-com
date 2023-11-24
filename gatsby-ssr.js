55987b9bf675089ef86e3cdf86c3167526e346a6
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2023-11-25 01:32:28 +0600

    Add gtm script in partytown

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -46,11 +46,9 @@ export const onRenderBody = ({ setHeadComponents }) => {
         ),
         gtmTrackingId && (
             <script
-                key="gtm-config"
-                type="text/partytown"
+                key="gtm-script"
                 dangerouslySetInnerHTML={{
-                    __html: `window.dataLayer = window.dataLayer || [];
-                            window.dataLayer.push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });`,
+                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl+'';f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer', '${gtmTrackingId}');`,
                 }}
             />
         ),

d52bbfbec7c68da93477e007f57d2673d05d3217
Michio <mohsen@regentmarkets.com>
2023-11-26 00:09:10 +0800

    chore: added more section and block usage

--- src/features/pages/home/index.tsx
+++ src/features/pages/home/index.tsx
@@ -1,47 +1,30 @@
 import React from 'react'
-import loadable from '@loadable/component'
-import { StatBlock } from '@deriv-com/blocks'
+import { PageLayout } from '@deriv-com/components'
 import Layout from 'features/components/templates/layout'
-import HomeHero from './hero'
-import LivePricing from './live-pricing'
-import P2PBanner from './p2p-banner'
-import ClientTestimonial from './client-testimonial'
-import TradeTypes from './trade-types'
+import ROWFooter from './footer'
+import LiveMarketSection from './live-pricing-migration'
+import TwentyYearsStrong from './years'
+import StatSection from './stats'
+import UserFriendlyPlatforms from './user-platfroms'
+import TradeTypeSection from './trade-type'
 import { useOpenLiveChat } from 'components/hooks/use-open-live-chat-redirection'
-import AwardBanner from 'features/components/templates/banners/award-banners'
-import useRegion from 'components/hooks/use-region'
 import MainNav from 'features/components/templates/navigation/main-nav'
-import SignupPublic from 'features/components/templates/signup/with-banner'
-import { localize } from 'components/localization'
-
-const OurPlatforms = loadable(() => import('./our-platforms'))
-const Footer = loadable(() => import('features/components/templates/footer'))
 
 const HomePage = () => {
     useOpenLiveChat(true)
-    const { is_p2p_allowed_country, is_eu } = useRegion()
 
     return (
         <Layout>
             <MainNav />
-            <HomeHero />
-            <LivePricing />
-            <TradeTypes />
-            <StatBlock.NumbersWithTitle
-                title={localize('_t_Why choose Deriv?_t_')}
-                cards={[
-                    { header: 'hassan', description: 'is cool', color: 'coral' },
-                    { header: 'nuzhy', description: 'is cool', color: 'dark' },
-                    { header: 'fasih', description: 'is NOT cool', color: 'dark' },
-                    { header: 'michio', description: 'is cool', color: 'coral' },
-                ]}
-            />
-            <OurPlatforms />
-            {is_eu ? null : <AwardBanner title="_t_Awards_t_" />}
-            <ClientTestimonial />
-            {is_p2p_allowed_country && <P2PBanner />}
-            <SignupPublic />
-            <Footer />
+            <PageLayout>
+                <StatSection />
+                <TwentyYearsStrong />
+
+                <LiveMarketSection />
+                <UserFriendlyPlatforms />
+                <TradeTypeSection />
+                <ROWFooter />
+            </PageLayout>
         </Layout>
     )
 }

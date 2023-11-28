4577b65524672624e6081450ec38ce604eae9db2
jia-deriv <jiawei@regentmarkets.com>
2023-11-28 11:59:43 +0800

    chore: remove mobile version footer cta bg

--- src/features/pages/home/footer-cta/index.tsx
+++ src/features/pages/home/footer-cta/index.tsx
@@ -5,29 +5,19 @@ import { StaticImage } from 'gatsby-plugin-image'
 import { localize } from 'components/localization'
 
 const image = (
-    <>
-        <StaticImage
-            src="../../../../images/migration/home/desktop_cta_bg.jpeg"
-            alt={localize('_t_call to action background_t_')}
-            loading="eager"
-            objectPosition="top"
-            className="hidden w-full h-full md:block"
-            formats={['webp', 'auto']}
-        />
-        <StaticImage
-            src="../../../../images/migration/home/mobile_cta_bg.png"
-            alt={localize('_t_call to action background_t_')}
-            loading="eager"
-            className="w-full h-full md:hidden"
-            objectPosition="top"
-            formats={['webp', 'auto']}
-        />
-    </>
+    <StaticImage
+        src="../../../../images/migration/home/cta_bg.png"
+        alt={localize('_t_call to action background_t_')}
+        loading="eager"
+        objectPosition="78% 0"
+        className="w-full h-full"
+        formats={['webp', 'auto']}
+    />
 )
 
 const FooterCTA = () => {
     return (
-        <Footer.CTABlock padding image={image} className="">
+        <Footer.CTABlock padding image={image}>
             <div className="flex h-full flex-col items-start justify-center gap-gap-3xl max-w-[328px] lg:max-w-none">
                 <Heading.H2 className="text-solid-slate-50">
                     Join over 2.5 million online traders

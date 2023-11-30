cf795f4edfee200c0cfa6646117203ea7ce15098
jia-deriv <jiawei@regentmarkets.com>
2023-11-30 17:46:51 +0800

    chore: nothing

--- src/features/pages/home/fast-payment/index.tsx
+++ src/features/pages/home/fast-payment/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const FastPaymentSection: React.FC = () => {
     return (
         <FastPayment
-            title="Fast, hassle-free deposits & withdrawals"
+            title="Fast, hassle-free deposits & withdrawal"
             description="60+ global payment methods. Deposit instantly starting from just USD 5. Withdraw in minutes.*"
             link={{
                 content: 'Learn More',

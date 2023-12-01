8e475fd9d1821de9a6043ccc1612a6e85e205335
hasan-deriv <abuhasan@deriv.com>
2023-12-01 19:17:38 +0800

    chore: platform section

--- src/features/pages/home/user-platfroms/index.tsx
+++ src/features/pages/home/user-platfroms/index.tsx
@@ -1,30 +1,30 @@
 import React from 'react'
-import { Features } from '@deriv-com/blocks'
-import { Section, FluidContainer, Heading, Tab } from '@deriv/quill-design'
+import { PlatformBlock } from '@deriv-com/blocks'
+import { CardsContainer } from '@deriv-com/components'
 import { cards, platformTabs } from './data'
 import { localize } from 'components/localization'
 
 const getTabContent = (tabId) => {
     switch (tabId) {
-        case 'all':
+        case 'All':
             return cards
-        case 'cfd':
+        case 'CFDs':
             return cards.filter(
                 (card) =>
                     card.header.includes('Deriv MT5') ||
                     card.header.includes('Deriv cTrader') ||
                     card.header.includes('Deriv X'),
             )
-        case 'options':
+        case 'Options':
             return cards.filter(
                 (card) =>
                     card.header.includes('Deriv GO') ||
                     card.header.includes('Deriv Trader') ||
                     card.header.includes('Deriv Bot'),
             )
-        case 'bots':
+        case 'Bots':
             return cards.filter((card) => card.header.includes('Deriv Bot'))
-        case 'social':
+        case 'Social':
             return cards.filter((card) => card.header.includes('Deriv cTrader'))
         default:
             return []
@@ -32,32 +32,21 @@ const getTabContent = (tabId) => {
 }
 const UserFriendlyPlatforms = () => {
     return (
-        <Section className="py-general-4xl bg-background-primary-base">
-            <FluidContainer className="flex flex-col items-center justify-center gap-gap-3xl">
-                <Heading.H2>
-                    {localize('_t_User-friendly trading platforms, on any device_t_')}
-                </Heading.H2>
-
-                <Tab.Container id="test" size="md">
-                    <Tab.List className="justify-center">
-                        {platformTabs.map((item) => (
-                            <Tab.Trigger key={item.tabId}>{item.name}</Tab.Trigger>
-                        ))}
-                    </Tab.List>
-                    <Tab.Content>
-                        {platformTabs.map((item) => (
-                            <Tab.Panel key={item.tabId}>
-                                <Features.Card
-                                    className="!bg-background-primary-base"
-                                    cards={getTabContent(item.tabId)}
-                                    cols="three"
-                                />
-                            </Tab.Panel>
-                        ))}
-                    </Tab.Content>
-                </Tab.Container>
-            </FluidContainer>
-        </Section>
+        <PlatformBlock.Tab
+            className="bg-background-primary-base"
+            header={localize('_t_User-friendly trading platforms, on any device_t_')}
+            tabs={platformTabs}
+        >
+            {platformTabs.map((item) => (
+                <>
+                    <CardsContainer
+                        variant="ContentBottom"
+                        cards={getTabContent(item.children)}
+                        cols="three"
+                    />
+                </>
+            ))}
+        </PlatformBlock.Tab>
     )
 }
 

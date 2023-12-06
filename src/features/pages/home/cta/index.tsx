6fc8bf5b634a18224074b69644b3b31d905246c0
jia-deriv <jiawei@regentmarkets.com>
2023-12-06 13:46:19 +0800

    fix: image blurry on mobile

--- src/features/pages/home/cta/index.tsx
+++ src/features/pages/home/cta/index.tsx
@@ -6,15 +6,26 @@ import { CustomLink } from '@deriv-com/components'
 import { localize } from 'components/localization'
 
 const image = (
-    <StaticImage
-        src="../../../../images/migration/home/cta_bg.png"
-        alt={localize('_t_call to action background_t_')}
-        loading="eager"
-        objectPosition="78% 0"
-        className="w-full h-full"
-        formats={['webp', 'auto']}
-        placeholder="none"
-    />
+    <>
+        <StaticImage
+            src="../../../../images/migration/home/cta_bg.png"
+            alt={localize('_t_call to action background_t_')}
+            loading="eager"
+            objectPosition="78% 0"
+            className="w-full h-full hidden md:block"
+            formats={['webp', 'auto']}
+            placeholder="none"
+        />
+        <StaticImage
+            src="../../../../images/migration/home/mobile_cta_bg.png"
+            alt={localize('_t_call to action background_t_')}
+            loading="eager"
+            className="w-full h-full md:hidden"
+            objectPosition="top"
+            formats={['webp', 'auto']}
+            placeholder="none"
+        />
+    </>
 )
 
 const CTA = () => {

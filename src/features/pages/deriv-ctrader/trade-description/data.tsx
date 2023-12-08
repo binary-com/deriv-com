3e38f5f9a78ac82829d1e3f37f4e41137f871632
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-12-08 14:24:19 +0400

    feat: c trader update

--- src/features/pages/deriv-ctrader/trade-description/data.tsx
+++ src/features/pages/deriv-ctrader/trade-description/data.tsx
@@ -3,5 +3,5 @@ import { TradingPlatformTextType } from 'features/components/templates/trade-pla
 export const what_is_trade_description: TradingPlatformTextType = {
     title: '_t_What is Deriv cTrader_t_',
     subtitle:
-        '_t_Deriv cTrader is an easy-to-use, multi-asset CFD trading platform packed with a wide range of features on a user-friendly interface. Enhance your trading experience further with features such as limit order and stop order, chart trading, and custom indicators. With Deriv cTrader, take advantage of our deep liquidity to execute trades quickly and efficiently._t_',
+        '_t_Deriv cTrader is an easy-to-use, multi-asset CFD trading platform packed with a wide range of features on a user-friendly interface. Enhance your trading experience further with features such as copy trading, limit order and stop order, chart trading, and custom indicators._t_',
 }

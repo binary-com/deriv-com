0c081cdaaef52430c58c345ba2e3fbabed225e07
hasan-deriv <abuhasan@deriv.com>
2023-12-11 12:34:57 +0800

    chore: update traders hub cta button

--- src/features/pages/home/navigation/nav.buttons.tsx
+++ src/features/pages/home/navigation/nav.buttons.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { MobileNavToggle } from '@deriv-com/blocks'
 import { Button } from '@deriv/quill-design'
 // eslint-disable-next-line import/no-unresolved
-import { StandalonePlaceholderRegularIcon } from '@deriv/quill-icons/Standalone'
+import { LabelPairedGrid2RegularIcon } from '@deriv/quill-icons/LabelPaired'
 import { Localize } from 'components/localization'
 import { handleRedirectToTradersHub } from 'components/custom/utils'
 import useAuthCheck from 'components/hooks/use-auth-check'
@@ -25,7 +25,7 @@ export const MainNavigationButtons = () => {
                     colorStyle="coral"
                     size="lg"
                     onClick={handleRedirectToTradersHub}
-                    icon={StandalonePlaceholderRegularIcon}
+                    icon={LabelPairedGrid2RegularIcon}
                     iconPosition="start"
                 >
                     <Localize translate_text="_t_Traders Hub_t_" />

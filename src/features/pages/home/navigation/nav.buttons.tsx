28295b9cdab7929801bc74e9e484c96406d254a3
hasan-deriv <abuhasan@deriv.com>
2023-12-11 12:29:23 +0800

    chore: updated header navigation cta buttons

--- src/features/pages/home/navigation/nav.buttons.tsx
+++ src/features/pages/home/navigation/nav.buttons.tsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import { MobileNavToggle } from '@deriv-com/blocks'
-import { useNavigation } from '@deriv-com/hooks'
 import { Button } from '@deriv/quill-design'
 // eslint-disable-next-line import/no-unresolved
 import { StandalonePlaceholderRegularIcon } from '@deriv/quill-icons/Standalone'
 import { Localize } from 'components/localization'
+import { handleRedirectToTradersHub } from 'components/custom/utils'
+import useAuthCheck from 'components/hooks/use-auth-check'
+import useHandleLogin from 'components/hooks/use-handle-login'
+import useHandleSignup from 'components/hooks/use-handle-signup'
+import usePpc from 'features/hooks/use-ppc'
 
 export const MainNavigationButtons = () => {
-    const { isLoggedIn, setIsLoggedIn } = useNavigation()
+    const [is_logged_in] = useAuthCheck()
+    const { is_ppc_redirect } = usePpc()
 
-    const onLoginClick = () => {
-        setIsLoggedIn?.(true)
-    }
-
-    const onTradersHubClick = () => {
-        setIsLoggedIn?.(false)
-    }
+    const handleLogin = useHandleLogin()
+    const handleSignup = useHandleSignup(is_ppc_redirect)
 
     return (
         <div className="flex flex-row items-center gap-gap-sm">
-            {isLoggedIn ? (
+            {is_logged_in ? (
                 <Button
                     variant="secondary"
                     colorStyle="coral"
                     size="lg"
-                    onClick={onTradersHubClick}
+                    onClick={handleRedirectToTradersHub}
                     icon={StandalonePlaceholderRegularIcon}
                     iconPosition="start"
                 >
@@ -32,10 +32,10 @@ export const MainNavigationButtons = () => {
                 </Button>
             ) : (
                 <>
-                    <Button variant="secondary" colorStyle="black" size="lg" onClick={onLoginClick}>
+                    <Button variant="secondary" colorStyle="black" size="lg" onClick={handleLogin}>
                         <Localize translate_text="_t_Log in_t_" />
                     </Button>
-                    <Button size="lg" className="hidden lg:block">
+                    <Button size="lg" className="hidden lg:block" onClick={handleSignup}>
                         <Localize translate_text="_t_Open demo account_t_" />
                     </Button>
                 </>

8848fbd02f4f55a44242a043ef9c06c8a1afb022
Michio <mohsen@regentmarkets.com>
2023-12-11 16:49:32 +0800

    chore: added live pricing logic

---
+++ src/features/pages/home/live-pricing-migration/cards/index.tsx
@@ -0,0 +1,58 @@
+import { CardSlider, LiveMarketContent } from '@deriv-com/components'
+import {
+    MarketForexAudusdIcon,
+    MarketForexEurusdIcon,
+    MarketForexGbpjpyIcon,
+    MarketForexGbpusdIcon,
+    MarketForexUsdcadIcon,
+} from '@deriv/quill-icons'
+import React, { ReactNode, useMemo } from 'react'
+import useLiveData from '../data-provider/useLiveData'
+import { MarketName } from '../data-provider/types'
+import { percentToDecimal, swiperOption } from '../utils'
+
+const IconsMapper = {
+    AUDUSD: <MarketForexAudusdIcon />,
+    EURUSD: <MarketForexEurusdIcon />,
+    GBPJPY: <MarketForexGbpjpyIcon />,
+    GBPUSD: <MarketForexGbpusdIcon />,
+    USDCAD: <MarketForexUsdcadIcon />,
+}
+
+const LiveMarketCard = <T extends MarketName>({
+    market,
+    children,
+}: {
+    market: T
+    children: ReactNode
+}) => {
+    const { data } = useLiveData(market)
+
+    const livePriceData: LiveMarketContent[] = useMemo(() => {
+        if (!data) return []
+        return Object.keys(data).map((key, index) => ({
+            id: index,
+            instrumentIcon: IconsMapper[key] ?? <MarketForexAudusdIcon />,
+            instrument: data[key].sym,
+            changePercentage: `${data[key].chng}`,
+            status: percentToDecimal(data[key].chng) >= 0 ? 'up' : 'down',
+            bidPrice: `${data[key].bid}`,
+            askPrice: `${data[key].ask}`,
+            spread: `${data[key].sprd}`,
+        }))
+    }, [data])
+
+    return (
+        <>
+            <CardSlider
+                variant="LiveMarketCard"
+                swiperData={swiperOption}
+                slideClasses="max-w-[286px]"
+                cards={livePriceData}
+            />
+            {children}
+        </>
+    )
+}
+
+export default LiveMarketCard

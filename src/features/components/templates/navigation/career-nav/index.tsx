b74bfbdb3479355242f75bd3e8e70252e6145bc7
Nuzhy-Deriv <nuzhy@deriv.com>
2023-12-12 19:38:48 +0800

    chore: careers nav updated

--- src/features/components/templates/navigation/career-nav/index.tsx
+++ src/features/components/templates/navigation/career-nav/index.tsx
@@ -1,48 +1,15 @@
 import React from 'react'
-import NavTemplate from '../template'
-import career_nav_items from './data'
-import { careers_nav_logo } from './career-nav.module.scss'
-import Image from 'features/components/atoms/image'
-import CareerNavLogo from 'images/svg/career-nav-icon.svg'
-import Button from 'features/components/atoms/button'
-import { Localize } from 'components/localization/localize'
-import LanguageSwitcher from 'features/components/molecules/language-switcher'
-import Link from 'features/components/atoms/link'
-import Flex from 'features/components/atoms/flex-box'
-import { localize } from 'components/localization'
+import { NavigationBlock } from '@deriv-com/blocks'
+// eslint-disable-next-line import/no-unresolved
+import { SubBrandDerivCareersBrandLightIcon } from '@deriv/quill-icons/Logo'
+import { career_nav_items } from './data'
 
 const CareerNav = () => {
     return (
-        <NavTemplate
-            renderLogo={() => (
-                <Link url={{ type: 'internal', to: '/' }} aria-label="deriv careers link">
-                    <Image
-                        src={CareerNavLogo}
-                        className={careers_nav_logo}
-                        alt={localize('_t_Deriv Careers_t_')}
-                    />
-                </Link>
-            )}
+        <NavigationBlock
+            renderLogo={() => <SubBrandDerivCareersBrandLightIcon width={133} height={24} />}
             items={career_nav_items}
-            has_centered_items
-        >
-            <Flex.Box
-                direction="row-reverse"
-                md={{ direction: 'row' }}
-                justify="center"
-                align="center"
-                gap="8x"
-            >
-                <Button.Primary
-                    id="dm-nav-explore-job-button"
-                    onClick={() =>
-                        window.open('https://deriv.zohorecruit.eu/jobs/Careers', '_blank')
-                    }
-                >
-                    <Localize translate_text="_t_Explore jobs_t_" />
-                </Button.Primary>
-            </Flex.Box>
-        </NavTemplate>
+        />
     )
 }
 

dd3945caa6e65c1b9bcf073e8bd2ccdaa2b51a4b
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-12-18 16:53:52 +0400

    fix: delay 5 seconds on hot jar append

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -59,13 +59,17 @@ export const onRenderBody = ({ setHeadComponents }) => {
                 key="hotjar-script"
                 dangerouslySetInnerHTML={{
                     __html: `(function(h,o,t,j,a,r){
-                        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                        h._hjSettings={hjid:'${hotjarId}',hjsv:'6'};
-                        a=o.getElementsByTagName('head')[0];
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:'${hotjarId}',hjsv:'7'};
+                    a=o.getElementsByTagName('head')[0];
+                    
+                    // Delay script execution by 5 seconds
+                    setTimeout(function(){
                         r=o.createElement('script');r.async=1;
                         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                         a.appendChild(r);
-                    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+                    }, 5000);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
                 }}
             />
         ),

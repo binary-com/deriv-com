5975047187644beda238e193074da65d84584279
Nuzhy-Deriv <125249108+Nuzhy-Deriv@users.noreply.github.com>
2023-12-19 17:07:01 +0800

    chore: update footer social links for EU, careers (#6272)
    * chore: update footer social links for EU, careers
    
    * fix: refactor
    
    * fix: remove unused telegram icon
    
    * fix: bannere overlap issue for eu

--- src/features/components/templates/footer/index.tsx
+++ src/features/components/templates/footer/index.tsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Footer } from '@deriv-com/blocks'
 import { qtMerge } from '@deriv/quill-design'
-import { EuFooterNavData, RowFooterNavData, socialButtons, warnText } from './data'
+import {
+    EuFooterNavData,
+    RowFooterNavData,
+    socialButtonsCareers,
+    socialButtonsEU,
+    socialButtonsROW,
+    warnText,
+} from './data'
 import { DerivGoBannerAndAwards } from './banner-and-awards'
 import { DescriptionContent, DescriptionContentEU } from './description'
 import useRegion from 'components/hooks/use-region'
+import { getLocationPathname } from 'common/utility'
 
 export const MainFooter = () => {
+    const [is_career, setIsCareer] = useState(false)
     const { is_eu } = useRegion()
+
+    useEffect(() => {
+        const current_path = getLocationPathname()
+        const splitted_path = current_path.split('/')
+        const is_career_page = splitted_path.includes('careers')
+        setIsCareer(is_career_page)
+    }, [])
+
+    const socialButtons = is_career
+        ? socialButtonsCareers
+        : is_eu
+        ? socialButtonsEU
+        : socialButtonsROW
+
     return (
         <Footer.FooterBlock
             warningText={!is_eu ? warnText : null}
             socialButtons={socialButtons}
             bannerAndAwards={DerivGoBannerAndAwards}
             descriptionContent={is_eu ? DescriptionContentEU : DescriptionContent}
-            className={qtMerge(is_eu && 'mb-[80px]')}
+            className={qtMerge(is_eu && 'mb-[120px] lg:mb-[80px]')}
         >
             <Footer.MainNavContent items={is_eu ? EuFooterNavData : RowFooterNavData} cols="six" />
         </Footer.FooterBlock>

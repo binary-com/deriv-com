59bca44950c0f215dd8716104f661775dc14decb
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-12-20 15:24:44 +0400

    fix: ctrader update

--- src/pages/markets/instruments/_dex-indices.tsx
+++ src/pages/markets/instruments/_dex-indices.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Symbol from '../components/helper/_symbol'
 import { dex_indices } from '../static/content/_market-symbols'
+import { Localize } from 'components/localization'
+import Typography from 'features/components/atoms/typography'
+import dclsx from 'features/utils/dclsx'
 
 const DexIndices = () => {
     return (
         <>
+            <Typography.Paragraph
+                mb="4x"
+                weight="bold"
+                size="small"
+                className={dclsx('visible-larger-than-phone')}
+            >
+                <Localize translate_text={'_t_Deriv MT5 and Deriv X:_t_'} />
+            </Typography.Paragraph>
+            <Typography.Paragraph
+                mb="4x"
+                weight="bold"
+                size="xs"
+                className={dclsx('visible-phone-only')}
+            >
+                <Localize translate_text={'_t_Deriv MT5 and Deriv X:_t_'} />
+            </Typography.Paragraph>
             {dex_indices.map((symbol, index) => (
                 <Symbol key={index} src={symbol.src} text={symbol.text} />
             ))}

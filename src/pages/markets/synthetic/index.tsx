8a1cdf7ee2e411d83419d3f4f9d09d62633d3ef3
Nuzhy-Deriv <nuzhy@deriv.com>
2023-12-21 16:54:54 +0800

    fix: padding top to market pages with old layout

--- src/pages/markets/synthetic/index.tsx
+++ src/pages/markets/synthetic/index.tsx
@@ -21,7 +21,7 @@ const Markets = () => {
         '_t_Trade on asset prices derived from real-world or simulated markets. Manage your exposure by selecting the volatility level to suit your risk appetite. Choose from our 24/7 synthetics, derived FX, and baskets._t_'
 
     return (
-        <Layout type="noNav">
+        <Layout type="noNav" padding_top="0">
             <DerivedFXHero
                 title="_t_Derived indices_t_"
                 description={is_eu ? description_eu : description_row}

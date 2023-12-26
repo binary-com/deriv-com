9f93cff21ebdfcc07f5a137548af305200300579
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-12-26 18:27:58 +0400

    fix: changes

--- src/features/pages/home/index.tsx
+++ src/features/pages/home/index.tsx
@@ -1,38 +1,9 @@
 import React from 'react'
-import loadable from '@loadable/component'
-import Layout from 'features/components/templates/layout'
-import HomeHero from './hero'
-import LivePricing from './live-pricing'
-import P2PBanner from './p2p-banner'
-import ClientTestimonial from './client-testimonial'
-import TradeTypes from './trade-types'
-import { useOpenLiveChat } from 'components/hooks/use-open-live-chat-redirection'
-import AwardBanner from 'features/components/templates/banners/award-banners'
-import useRegion from 'components/hooks/use-region'
-import SignupPublic from 'features/components/templates/signup/with-banner'
-import MainRowNavigation from 'features/components/templates/navigation/main-nav'
-
-const OurPlatforms = loadable(() => import('./our-platforms'))
-const MainFooter = loadable(() => import('features/components/templates/footer'))
+// eslint-disable-next-line import/no-unresolved
+import { BrandDerivWordmarkCoralIcon } from '@deriv/quill-icons/Logo'
 
 const HomePage = () => {
-    useOpenLiveChat(true)
-    const { is_p2p_allowed_country, is_eu } = useRegion()
-
-    return (
-        <Layout>
-            <MainRowNavigation />
-            <HomeHero />
-            <LivePricing />
-            <TradeTypes />
-            <OurPlatforms />
-            {is_eu ? null : <AwardBanner title="_t_Awards_t_" />}
-            <ClientTestimonial />
-            {is_p2p_allowed_country && <P2PBanner />}
-            <SignupPublic />
-            <MainFooter />
-        </Layout>
-    )
+    return <BrandDerivWordmarkCoralIcon width={73} height={24} />
 }
 
 export default HomePage

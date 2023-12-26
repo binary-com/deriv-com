dad6c8ec4d331d1ba9ecdfe16d4d08f1167cbbd3
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2023-12-26 18:42:54 +0400

    fix: changes

--- src/features/pages/home/index.tsx
+++ src/features/pages/home/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react'
-// eslint-disable-next-line import/no-unresolved
+import loadable from '@loadable/component'
+import Layout from 'features/components/templates/layout'
 import { BrandDerivWordmarkCoralIcon } from '@deriv/quill-icons/Logo'
+// eslint-disable-next-line import/no-unresolved
+import HomeHero from './hero'
+import LivePricing from './live-pricing'
+import P2PBanner from './p2p-banner'
+import ClientTestimonial from './client-testimonial'
+import TradeTypes from './trade-types'
+import { useOpenLiveChat } from 'components/hooks/use-open-live-chat-redirection'
+import AwardBanner from 'features/components/templates/banners/award-banners'
+import useRegion from 'components/hooks/use-region'
+import SignupPublic from 'features/components/templates/signup/with-banner'
+import MainRowNavigation from 'features/components/templates/navigation/main-nav'
+
+const OurPlatforms = loadable(() => import('./our-platforms'))
+const MainFooter = loadable(() => import('features/components/templates/footer'))
 
 const HomePage = () => {
-    return <BrandDerivWordmarkCoralIcon width={73} height={24} />
-}
+    useOpenLiveChat(true)
+    const { is_p2p_allowed_country, is_eu } = useRegion()
 
+    return (
+        <Layout>
+            <MainRowNavigation />
+            <HomeHero />
+            <BrandDerivWordmarkCoralIcon width={100} height={100} />
+            <LivePricing />
+            <TradeTypes />
+            <OurPlatforms />
+            {is_eu ? null : <AwardBanner title="_t_Awards_t_" />}
+            <ClientTestimonial />
+            {is_p2p_allowed_country && <P2PBanner />}
+            <SignupPublic />
+            <MainFooter />
+        </Layout>
+    )
+}
 export default HomePage

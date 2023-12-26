e523fa6ef167f2d01b887430d58019ca6e6f2757
prince-deriv <82309725+prince-deriv@users.noreply.github.com>
2023-12-26 12:44:19 +0400

    Prince/New homepage master update (#6356)
    * chore: merge master to new-homepage-translation
    
    * chore: quill-icon stable version usage

--- src/features/pages/markets/etf/index.tsx
+++ src/features/pages/markets/etf/index.tsx
@@ -10,13 +10,15 @@ import CheckTrading from './check-trading'
 import SignupPublic from 'features/components/templates/signup/with-banner'
 import Flex from 'features/components/atoms/flex-box'
 import OtherMarketsSlider from 'features/components/molecules/other-markets-slider'
-import MarketNav from 'features/components/templates/navigation/market-nav'
-import Footer from 'features/components/templates/footer/'
+import MainFooter from 'features/components/templates/footer/'
+import MainRowNavigation from 'features/components/templates/navigation/main-nav'
+import { usePlatformQueryParam } from 'components/hooks/use-platform-query-param'
 
 const ETFMarket = () => {
+    const { is_deriv_go } = usePlatformQueryParam()
     return (
         <Layout>
-            <MarketNav />
+            {!is_deriv_go && <MainRowNavigation />}
             <Banner />
             <TradeTypes />
             <Flex.Box
@@ -35,7 +37,7 @@ const ETFMarket = () => {
             <Steps />
             <OtherMarketsSlider current_market="etfs" />
             <SignupPublic />
-            <Footer />
+            <MainFooter />
         </Layout>
     )
 }

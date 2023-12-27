45dffc18d43847f1229510afb4d3fad5ef927af6
hasan-deriv <abuhasan@deriv.com>
2023-12-27 16:49:26 +0800

    chore: updated live market icons

--- src/features/pages/home/live-pricing-migration/cards/index.tsx
+++ src/features/pages/home/live-pricing-migration/cards/index.tsx
@@ -14,6 +14,13 @@ import {
     MarketCommoditySilverusdIcon,
     MarketCommodityGoldusdIcon,
     MarketCommodityCopperusdIcon,
+    MarketStocksAppleIcon,
+    MarketStocksWaltDisneyIcon,
+    MarketStocksTeslaIcon,
+    MarketDerivedCrash1000Icon,
+    MarketDerivedVolatility25Icon,
+    MarketDerivedJump25Icon,
+    MarketDerivedJump200Icon,
     // eslint-disable-next-line import/no-unresolved
 } from '@deriv/quill-icons/Markets'
 import useLiveData from '../data-provider/useLiveData'
@@ -37,6 +44,13 @@ const IconsMapper = {
     XAGUSD: <MarketCommoditySilverusdIcon />,
     XAUUSD: <MarketCommodityGoldusdIcon />,
     XCUUSD: <MarketCommodityCopperusdIcon />,
+    AAPL: <MarketStocksAppleIcon />,
+    DIS: <MarketStocksWaltDisneyIcon />,
+    TSLA: <MarketStocksTeslaIcon />,
+    Crash_1000_Index: <MarketDerivedCrash1000Icon />,
+    EURUSD_DFX_10_Index: <MarketDerivedVolatility25Icon />,
+    Jump_50_Index: <MarketDerivedJump25Icon />,
+    Range_Break_200_Index: <MarketDerivedJump200Icon />,
 }
 
 const LiveMarketCard = <T extends MarketName>({

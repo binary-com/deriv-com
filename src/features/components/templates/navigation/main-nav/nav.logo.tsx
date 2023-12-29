3e170829642187796ae514c30e521c4f4f92ecfb
hasan-deriv <abuhasan@deriv.com>
2023-12-29 12:51:24 +0800

    chore: added cy id for logo and hamburger

--- src/features/components/templates/navigation/main-nav/nav.logo.tsx
+++ src/features/components/templates/navigation/main-nav/nav.logo.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import { CustomLink } from '@deriv-com/components'
 // eslint-disable-next-line import/no-unresolved
 import { BrandDerivWordmarkCoralIcon } from '@deriv/quill-icons/Logo'
-import { useBrowserResize } from 'components/hooks/use-browser-resize'
 
 export const MainNavigationLogo = () => {
-    const [is_mobile] = useBrowserResize(1024)
     return (
-        <CustomLink href={'/'} data-cy={is_mobile ? 'deriv-logo-mobile' : 'deriv-logo-desktop'}>
+        <CustomLink href={'/'} data-cy="deriv-logo">
             <BrandDerivWordmarkCoralIcon width={73} height={24} />
         </CustomLink>
     )

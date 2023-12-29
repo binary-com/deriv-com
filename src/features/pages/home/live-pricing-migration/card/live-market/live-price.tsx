9657d7e16db59cd2099db5af85c92bcc51eff0cc
hasan-deriv <abuhasan@deriv.com>
2023-12-29 11:29:34 +0800

    chore: added 4 decimal condition for price

--- src/features/pages/home/live-pricing-migration/card/live-market/live-price.tsx
+++ src/features/pages/home/live-pricing-migration/card/live-market/live-price.tsx
@@ -39,20 +39,64 @@ const PriceContent = (price: string, status: MarketStatus) => {
     )
 }
 
+type PriceComponentType = {
+    beforeDecimal: string
+    afterDecimal: string
+    status: MarketStatus
+}
+
+const FourDecimalPoint = ({ beforeDecimal, afterDecimal, status }: PriceComponentType) => {
+    return (
+        <div className="flex flex-row items-center gap-[2px]">
+            <Text
+                size="md"
+                className={clsx('flex h-full items-end pb-[3px]', colorVariant[status])}
+            >
+                {beforeDecimal}
+            </Text>
+            <Text className={clsx('text-400 leading-700', colorVariant[status])}>
+                {afterDecimal?.substring(0, 2)}
+            </Text>
+            <Text
+                size="md"
+                className={clsx('flex h-full items-start pt-[3px]', colorVariant[status])}
+            >
+                {afterDecimal?.substring(2)}
+            </Text>
+        </div>
+    )
+}
+
 export const LivePrice = ({ status, bidPrice, askPrice, textClass }: LivePriceProps) => {
+    const splittedBidPrice = bidPrice.split('.')
+    const splittedAskPrice = askPrice.split('.')
+
     return (
         <div className="flex flex-row gap-gap-md">
             <div className="flex flex-1 flex-col">
                 <Text size="sm" className={textClass}>
                     Bid
                 </Text>
-                {PriceContent(bidPrice, status)}
+                {/* {PriceContent(bidPrice, status)} */}
+                {splittedBidPrice[0].length <= 4 && splittedBidPrice[0].length > 1 && (
+                    <FourDecimalPoint
+                        beforeDecimal={splittedBidPrice[0]}
+                        afterDecimal={splittedBidPrice[1]}
+                        status={status}
+                    />
+                )}
             </div>
             <div className="flex flex-1 flex-col">
                 <Text size="sm" className={textClass}>
                     Ask
                 </Text>
-                {PriceContent(askPrice, status)}
+                {splittedAskPrice[0].length <= 4 && splittedAskPrice[0].length > 1 && (
+                    <FourDecimalPoint
+                        beforeDecimal={splittedAskPrice[0]}
+                        afterDecimal={splittedAskPrice[1]}
+                        status={status}
+                    />
+                )}
             </div>
         </div>
     )

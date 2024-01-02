b321284bfed3a9217e7143df3be9262cc7b65a4c
hasan-deriv <abuhasan@deriv.com>
2024-01-02 13:18:09 +0800

    chore: updated floating cta root margin

--- src/features/hooks/use-floating-cta/index.tsx
+++ src/features/hooks/use-floating-cta/index.tsx
@@ -9,17 +9,15 @@ const useFloatingCta = () => {
     const { width } = useWindowSize()
     const entry = useIntersectionObserver(targetRef, {
         threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
-        rootMargin: '-80px',
+        rootMargin: '-80px 0px 0px 0px',
     })
 
     useEffect(() => {
         if (width < 601 && !is_logged_in) {
-            const targetHeight = entry?.boundingClientRect?.height
-            const intersectionHeight = entry?.intersectionRect?.height
-            const percentage = (intersectionHeight / targetHeight) * 100
+            const percentage = entry?.intersectionRatio * 100
             setVisibilityPercentage(percentage)
         }
-    }, [entry?.boundingClientRect?.height, entry?.intersectionRect?.height, width, is_logged_in])
+    }, [entry?.intersectionRatio, width, is_logged_in])
 
     return { visibilityPercentage, targetRef }
 }

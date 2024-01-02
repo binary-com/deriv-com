0edfb91e0e03851df9629480b571cb08630e7307
hasan-deriv <abuhasan@deriv.com>
2024-01-02 10:55:58 +0800

    chore: updated quill layout

--- src/features/components/quill/quill-layout/index.tsx
+++ src/features/components/quill/quill-layout/index.tsx
@@ -1,10 +1,17 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useCallback } from 'react'
+import { LanguageProvider, SharedLinkProvider } from '@deriv-com/providers'
+import { BreakpointProvider, ThemeProvider } from '@deriv/quill-design'
 import BrowserUpdateAlert from '../browser-update-alert'
 import LayoutOverlay from '../layout-overlay'
+import GatsbySharedLink from '../shared-link'
 import { usePlatformQueryParam } from 'components/hooks/use-platform-query-param'
 import PpcProvider from 'features/contexts/ppc-campaign/ppc.provider'
 import { getLanguage, isBrowser } from 'common/utility'
 import apiManager from 'common/websocket'
+import { useLangDirection } from 'components/hooks/use-lang-direction'
+import { LocaleContext } from 'components/localization'
+import useLangSwitcher from 'features/components/molecules/language-switcher/useLangSwitcher'
+import { langItemsROW } from 'features/pages/home/data'
 
 interface LayoutProps {
     is_ppc?: boolean
@@ -26,17 +33,50 @@ const QuillLayout = ({
 }: LayoutProps) => {
     const { has_platform } = usePlatformQueryParam()
 
+    const lang_direction = useLangDirection()
+    const { locale } = React.useContext(LocaleContext)
+    const formatted_lang = locale.replace('_', '-')
+
+    React.useEffect(() => {
+        document.body.dir = lang_direction
+        document.documentElement.lang = formatted_lang
+    }, [lang_direction, formatted_lang])
+
+    //need to update the language data and type
+    //here using langauge data from `i18n-config.js`
+    const { onSwitchLanguage, currentLang } = useLangSwitcher()
+    const activeLang = langItemsROW[currentLang.path.replace('-', '')]
+
+    const onLanguageChange = useCallback(
+        (event) => {
+            onSwitchLanguage(`/${event.path}/`)
+        },
+        [onSwitchLanguage],
+    )
+
     //Handle page layout when redirection from mobile app.
     if (has_platform) {
         return <>{children}</>
     }
 
     return (
-        <PpcProvider is_ppc={is_ppc} is_ppc_redirect={is_ppc_redirect}>
-            <main className="mx-auto relative max-w-[256rem]">{children}</main>
-            <BrowserUpdateAlert />
-            {!hide_layout_overlay && <LayoutOverlay />}
-        </PpcProvider>
+        <SharedLinkProvider DerivLink={GatsbySharedLink}>
+            <PpcProvider is_ppc={is_ppc} is_ppc_redirect={is_ppc_redirect}>
+                <LanguageProvider
+                    langItems={langItemsROW}
+                    onLangSelect={onLanguageChange}
+                    activeLanguage={activeLang}
+                >
+                    <BreakpointProvider>
+                        <ThemeProvider theme="light">
+                            <main className="relative max-w-[256rem] mx-auto">{children}</main>
+                            <BrowserUpdateAlert />
+                            {!hide_layout_overlay && <LayoutOverlay />}
+                        </ThemeProvider>
+                    </BreakpointProvider>
+                </LanguageProvider>
+            </PpcProvider>
+        </SharedLinkProvider>
     )
 }
 

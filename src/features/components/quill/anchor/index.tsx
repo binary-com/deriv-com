852b4c694f555ec3ad2cd7f9288e1e188383176b
hasan-deriv <abuhasan@deriv.com>
2024-01-02 13:54:44 +0800

    chore: updated banner font size

--- src/features/components/quill/anchor/index.tsx
+++ src/features/components/quill/anchor/index.tsx
@@ -1,13 +1,13 @@
 import React, { ComponentProps } from 'react'
-import clsx from 'clsx'
+import { qtMerge } from '@deriv/quill-design'
 
 type AnchorProps = ComponentProps<'a'>
 
 const Anchor = ({ className, ...restProps }: AnchorProps) => {
     return (
         <a
-            className={clsx(
-                'font-bold inline text-75 transition-colors lg:text-100 hover:underline hover:text-solid-coral-700',
+            className={qtMerge(
+                'font-bold inline text-75 transition-colors hover:underline hover:text-solid-coral-700',
                 className,
             )}
             {...restProps}

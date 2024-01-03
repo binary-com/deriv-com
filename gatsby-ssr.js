cf7ed6ccc03bde3505e4cee817330a5e4645ab81
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2024-01-03 21:42:02 +0400

    fix: 3 seconds with part town

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -53,22 +53,23 @@ export const onRenderBody = ({ setHeadComponents }) => {
             />
         ),
 
-        hotjarId && (
+        // Hotjar setup
+        <Partytown key="partytown-hotjar">
             <script
                 key="hotjar-script"
                 dangerouslySetInnerHTML={{
                     __html: `(function(h,o,t,j,a,r){
-                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:'${hotjarId}',hjsv:'7'};
-                    a=o.getElementsByTagName('head')[0];
-                    setTimeout(function(){
-                        r=o.createElement('script');r.async=1;
-                        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                        a.appendChild(r);
-                    }, 5000);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:'${hotjarId}',hjsv:'7'};
+            a=o.getElementsByTagName('head')[0];
+            setTimeout(function(){
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            }, 3000);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
                 }}
             />
-        ),
+        </Partytown>,
     ])
 }

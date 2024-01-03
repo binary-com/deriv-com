0bfa838d9c6a4db3dd0067e0c1031c5d1489353c
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2024-01-03 18:00:39 +0400

    fix: hero banner

--- src/features/pages/principles/index.tsx
+++ src/features/pages/principles/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { BreakpointProvider, ThemeProvider } from '@deriv/quill-design'
-import { PageLayout } from '@deriv-com/components'
+import { BreakpointProvider, ThemeProvider, FluidContainer, Section } from '@deriv/quill-design'
+import { PageLayout, Breadcrumbs } from '@deriv-com/components'
 import Layout from 'features/components/templates/layout'
 import HeroBanner from './hero-banner'
 import MainRowNavigation from 'features/components/templates/navigation/main-nav'
@@ -13,6 +13,24 @@ const OurPrincipalsComponent = () => {
                 <Layout>
                     <MainRowNavigation />
                     <PageLayout>
+                        <Section className="!bg-background-secondary-container">
+                            <FluidContainer>
+                                <Breadcrumbs
+                                    className="py-general-md"
+                                    links={[
+                                        {
+                                            content: 'Home',
+                                            href: '/',
+                                        },
+                                        {
+                                            content: 'Our principles',
+                                            href: '/our-principles/',
+                                        },
+                                    ]}
+                                />
+                            </FluidContainer>
+                        </Section>
+
                         <HeroBanner />
                     </PageLayout>
                     <MainFooter />

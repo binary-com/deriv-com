3a0bfac00817c4f636b45ba4c8c4dac68a0b1e33
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2024-01-05 17:54:31 +0400

    fix: unwanted words

--- src/features/pages/principles/data.tsx
+++ src/features/pages/principles/data.tsx
@@ -319,8 +319,3 @@ export const ContentImage: ContentImageType[] = [
         ),
     },
 ]
-
-//need to remove later
-const messages = [
-    '_t_Our principles_t_,_t_We are honoured to serve everyone who relies on our products and services, and we only want to offer the best. That’s why our principles and values are so important in defining who we are, why we do what we do, and how we treat our clients and each other. Across our international offices, we are committed to the following principles in everything that we do._t_',
-]

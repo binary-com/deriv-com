1b92ed1a1dfc81191ab8dc7acce6ec9408b80777
mayuran-deriv <129507167+mayuran-deriv@users.noreply.github.com>
2024-01-05 12:07:02 +0400

    Mayuran/hotjar beta (#6445)
    * fix: hot jar changes
    
    * fix: delay 5 seconds on hot jar append
    
    * fix: console removed
    
    * fix: revert things
    
    * fix: test party town and 3 seconds
    
    * fix: added partytown mandatory

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -53,22 +53,26 @@ export const onRenderBody = ({ setHeadComponents }) => {
             />
         ),
 
-        // Hotjar setup
+        // Hotjar setup after Partytown
         hotjarId && (
             <script
                 key="hotjar-script"
-                type="text/partytown"
                 dangerouslySetInnerHTML={{
                     __html: `(function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:'${hotjarId}',hjsv:'7'};
-                a=o.getElementsByTagName('head')[0];
-                setTimeout(function(){
-                    r=o.createElement('script');r.async=1;
-                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                    a.appendChild(r);
-                }, 3000);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:'${hotjarId}',hjsv:'7'};
+                    a=o.getElementsByTagName('head')[0];
+                    function loadHotjarScript() {
+                        r=o.createElement('script');r.async=1;
+                        r.src=t+j+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                        a.appendChild(r);
+                    }
+                    setTimeout(function() {
+                        if (window.partytown) {
+                            window.partytown.load('hotjar', loadHotjarScript);
+                        }
+                    }, 3000);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
                 }}
             />
         ),

3122af71dce4e18b2bfd0c058b0dba90d998ff6d
hasan-deriv <abuhasan@deriv.com>
2024-01-09 11:07:58 +0800

    chore: fixed floating button bg color

--- src/features/components/quill/layout-overlay/floating-button/index.tsx
+++ src/features/components/quill/layout-overlay/floating-button/index.tsx
@@ -12,7 +12,7 @@ const FloatingButton = ({ children, className, onClick }: TProps) => {
         <button
             className={clsx(
                 className,
-                'tap-highlight-transparent shadow-[0_16px_20px_0_rgba(0,0,0,0.05),0_0_20px_0_rgba(0,0,0,0.05)] bg-background-primary-base p-400 flex cursor-pointer border-[transparent] rounded-[50%] m-800 mt-50 pointer-events-auto transition-all select-none sm:p-700 hover:scale-100',
+                'tap-highlight-transparent shadow-[0_16px_20px_0_rgba(0,0,0,0.05),0_0_20px_0_rgba(0,0,0,0.05)] bg-background-primary-container p-400 flex cursor-pointer border-[transparent] rounded-[50%] m-800 mt-50 pointer-events-auto transition-all select-none sm:p-700 hover:scale-100',
             )}
             onMouseEnter={() => setHasHover(true)}
             onMouseLeave={() => setHasHover(false)}

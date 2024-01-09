f1d34c610d1730230bd299fce9a4b21a966c6302
hasan-deriv <abuhasan@deriv.com>
2024-01-09 17:13:28 +0800

    chore: updated different ref conditon for mobile and tablet

--- src/features/components/quill/layout-overlay/cfd-warning-banner/desktop-text.tsx
+++ src/features/components/quill/layout-overlay/cfd-warning-banner/desktop-text.tsx
@@ -9,27 +9,26 @@ type TextProps = {
     loss_percent: number
 }
 
-const UnexpandedText = ({ loss_percent }: TextProps) => {
+const DesktopText = ({ loss_percent }: TextProps) => {
     const { setCtaBottom } = useFloatingCtaContext()
-    const { width } = useWindowSize()
     const ref = useRef<HTMLParagraphElement>(null)
+    const { width } = useWindowSize()
     useEffect(() => {
-        if (width < 640) {
+        if (width >= 640) {
             setCtaBottom(ref.current?.clientHeight + 40) // 40 is the padding of top and bottom of it's container
         }
     }, [setCtaBottom, width])
-
     return (
-        <Text ref={ref} className="w-[95%] mx-auto">
+        <Text className="text-75 xl:text-100" ref={ref}>
             <Localize
-                translate_text="_t_<0> {{loss_percent}}% of retail investor accounts lose money when trading CFDs with Deriv, read our full Risk disclosure here.</0>_t_"
+                translate_text="_t_CFDs and other products offered on this website are complex instruments with a high risk of losing money rapidly owing to leverage.<0> {{loss_percent}}% of retail investor accounts lose money when trading CFDs with Deriv, read our full Risk disclosure here.</0> You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money._t_"
                 values={{ loss_percent }}
                 components={[
                     <Anchor
                         key={0}
+                        className="lg:text-100"
                         target="_blank"
                         href="/tnc/eu/risk-disclosure.pdf"
-                        className="text-100"
                     />,
                 ]}
             />
@@ -37,4 +36,4 @@ const UnexpandedText = ({ loss_percent }: TextProps) => {
     )
 }
 
-export default UnexpandedText
+export default DesktopText

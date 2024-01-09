1cecf74c7034e7eada570c6d662fb73908d2f530
hasan-deriv <abuhasan@deriv.com>
2024-01-09 16:15:47 +0800

    chore: updated gatsby browser and global css file

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -18,8 +18,8 @@ import {
 import './static/css/noto-sans-arabic.css'
 import 'swiper/swiper-bundle.min.css'
 import 'features/styles/app.scss'
-import './static/css/global.css'
 import './static/css/ubuntu.css'
+import './static/css/global.css'
 import '@deriv-com/blocks/style.css';
 import '@deriv-com/components/style.css';
 

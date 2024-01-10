b5e8e7e6d0137b0dab5542b4095d1a2bbc92596b
mitra-deriv <mitra@regentmarkets.com>
2024-01-10 18:26:35 +0800

    fix: :bug: hide app gallery and app store if it is not supported in footer

--- src/features/components/templates/footer/download.tsx
+++ src/features/components/templates/footer/download.tsx
@@ -9,27 +9,31 @@ import clsx from 'clsx'
 import { StaticImage } from 'gatsby-plugin-image'
 import { CustomLink } from '@deriv-com/components'
 import { localize } from 'components/localization'
+import useRegion from 'components/hooks/use-region'
 
 const sharedClasses =
     'flex items-center justify-center gap-gap-md p-general-sm rounded-[4px] border-solid border-xs border-opacity-black-100'
 
 const DownloadBadges = () => {
+    const { is_appgallery_supported, is_ios_supported } = useRegion()
     return (
         <div className="flex flex-col gap-gap-md max-lg:flex-1">
-            <CustomLink
-                href="https://appgallery.huawei.com/app/C103801913"
-                target="_blank"
-                className={clsx(sharedClasses, 'order-last')}
-            >
-                <LabelPairedHuaweiAppGalleryLgIcon fill="#000000b8" />
-                <StaticImage
-                    src="../../../../images/common/migration/footer/explore-appgallery.png"
-                    alt={localize('_t_explore it on appgallery_t_')}
-                    formats={['webp', 'auto']}
-                    width={90}
-                    placeholder="none"
-                />
-            </CustomLink>
+            {is_appgallery_supported && (
+                <CustomLink
+                    href="https://appgallery.huawei.com/app/C103801913"
+                    target="_blank"
+                    className={clsx(sharedClasses, 'order-last')}
+                >
+                    <LabelPairedHuaweiAppGalleryLgIcon fill="#000000b8" />
+                    <StaticImage
+                        src="../../../../images/common/migration/footer/explore-appgallery.png"
+                        alt={localize('_t_explore it on appgallery_t_')}
+                        formats={['webp', 'auto']}
+                        width={90}
+                        placeholder="none"
+                    />
+                </CustomLink>
+            )}
             <CustomLink
                 href="https://play.google.com/store/apps/details?id=com.deriv.app"
                 target="_blank"
@@ -44,20 +48,22 @@ const DownloadBadges = () => {
                     placeholder="none"
                 />
             </CustomLink>
-            <CustomLink
-                href="https://apps.apple.com/my/app/deriv-go-online-trading-app/id1550561298"
-                target="_blank"
-                className={clsx(sharedClasses)}
-            >
-                <LabelPairedAppleLgIcon fill="#000000b8" />
-                <StaticImage
-                    src="../../../../images/common/migration/footer/download-appstore.png"
-                    alt={localize('_t_download on the app store_t_')}
-                    formats={['webp', 'auto']}
-                    width={90}
-                    placeholder="none"
-                />
-            </CustomLink>
+            {is_ios_supported && (
+                <CustomLink
+                    href="https://apps.apple.com/my/app/deriv-go-online-trading-app/id1550561298"
+                    target="_blank"
+                    className={clsx(sharedClasses)}
+                >
+                    <LabelPairedAppleLgIcon fill="#000000b8" />
+                    <StaticImage
+                        src="../../../../images/common/migration/footer/download-appstore.png"
+                        alt={localize('_t_download on the app store_t_')}
+                        formats={['webp', 'auto']}
+                        width={90}
+                        placeholder="none"
+                    />
+                </CustomLink>
+            )}
         </div>
     )
 }

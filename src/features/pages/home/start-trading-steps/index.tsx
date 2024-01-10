bfa614ef893d6d73c60c9951759dbfc9cdc6dede
meenu-deriv <meenakshi@regentmarkets.com>
2024-01-10 13:15:49 +0400

    fix: fixed cta button fixes and updated icons

--- src/features/pages/home/start-trading-steps/index.tsx
+++ src/features/pages/home/start-trading-steps/index.tsx
@@ -12,8 +12,8 @@ const StartTradingSteps = () => {
             className="xl:[&>*:nth-child(1)]:!px-50"
             title={<Localize translate_text="_t_Start trading in 3 simple steps_t_" />}
             cta={
-                <div className="flex w-full justify-center">
-                    <TradersHubCtaButton />
+                <div className="hidden lg:block">
+                    <TradersHubCtaButton className="flex" />
                 </div>
             }
             cols="three"

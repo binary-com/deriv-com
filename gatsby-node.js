6f5f864ef7229c42866824fa86d7ac48d9e5cc2b
eduardhrachou <eduard@regentmarkets.com>
2024-01-12 15:08:41 +0300

    test

--- gatsby-node.js
+++ gatsby-node.js
@@ -397,27 +397,49 @@ const style_lint_options = {
     lintDirtyModulesOnly: true,
 }
 
-exports.onCreateWebpackConfig = ({ stage, actions, loaders, getConfig }, { ...options }) => {
+exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }, { ...options }) => {
     const config = getConfig()
-    if (config.optimization) {
-        config.optimization.minimizer = [new TerserPlugin()]
-    }
-    if (stage === 'build-html' || stage === 'develop-html') {
-        actions.setWebpackConfig({
-            module: {
-                rules: [
-                    {
-                        test: /analytics/,
-                        use: loaders.null(),
-                    },
-                ],
-            },
-        })
-    }
+    const isProduction = config.mode === 'production'
+
     actions.setWebpackConfig({
+        devtool: false,
+        mode: isProduction ? 'production' : 'development',
+        optimization: {
+            minimize: isProduction,
+            minimizer: [new TerserPlugin()],
+            splitChunks: {
+                chunks: 'all',
+                minSize: 30000,
+                maxSize: 0,
+                minChunks: 1,
+                maxAsyncRequests: 5,
+                maxInitialRequests: 3,
+                automaticNameDelimiter: '~',
+                name: true,
+                cacheGroups: {
+                    vendors: {
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: -10,
+                    },
+                    default: {
+                        minChunks: 2,
+                        priority: -20,
+                        reuseExistingChunk: true,
+                    },
+                },
+            },
+        },
         plugins: [new StylelintPlugin({ ...style_lint_options, ...options })],
         resolve: {
             modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /analytics/,
+                    use: loaders.null(),
+                },
+            ],
         },
     })
 }

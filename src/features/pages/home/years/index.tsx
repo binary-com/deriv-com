a69e5ca3aab3fa0353360e5e36e04828729fdaae
hasan-deriv <abuhasan@deriv.com>
2024-01-17 18:27:11 +0800

    chore: added mr to swiper slide

--- src/features/pages/home/years/index.tsx
+++ src/features/pages/home/years/index.tsx
@@ -8,7 +8,7 @@ const TwentyYearsStrong = () => {
         <Features.ContentSlider
             title={<Localize translate_text="_t_We're 24+ years strong_t_" />}
             cardSliderProps={{
-                slideClasses: 'max-w-[296px] !h-auto',
+                slideClasses: 'max-w-[296px] !h-auto mr-800',
                 className: 'w-screen !mr-[calc((-100vw+100%)/2)] lg:w-full lg:!mr-auto',
                 variant: 'ContentBottom',
                 cards: cards,

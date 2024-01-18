a745bf1998290267e7b83578e66f5c046a203cd3
eduardhrachou <eduard@regentmarkets.com>
2024-01-18 14:03:36 +0300

    html css and js optimizations

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,18 +58,47 @@ module.exports = {
         // },
         'gatsby-plugin-postcss',
         {
+            resolve: 'gatsby-plugin-html-minifier',
+            options: {
+                caseSensitive: true,
+                collapseBooleanAttributes: true,
+                collapseWhitespace: true,
+                conservativeCollapse: true,
+                decodeEntities: true,
+                html5: true,
+                minifyCSS: true,
+                minifyJS: true,
+                removeComments: true,
+                removeEmptyAttributes: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true,
+            },
+        },
+        {
             resolve: 'gatsby-plugin-sass',
             options: {
                 postCssPlugins: [
                     require('postcss-discard-duplicates'),
                     plugin({
                         dest: 'src/classnames.d.ts',
-                        // Set isModule if you want to import ClassNames from another file
-                        // isModule: true,
-                        exportAsDefault: true, // to use in combination with isModule
+                        exportAsDefault: true,
                     }),
                     require('cssnano')({
-                        preset: 'default',
+                        preset: [
+                            'default',
+                            {
+                                discardComments: { removeAll: true },
+                                discardUnused: true,
+                                mergeIdents: true,
+                                reduceIdents: true,
+                                mergeRules: true,
+                                minifySelectors: true,
+                                discardEmpty: true,
+                                minifyFontValues: true,
+                            },
+                        ],
                     }),
                 ],
             },

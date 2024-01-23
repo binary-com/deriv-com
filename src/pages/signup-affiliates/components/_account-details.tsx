8a2d48502b10768e007e55582ef660ab56c8140c
behnaz-deriv <behnaz@regentmarkets.com>
2024-01-23 12:46:19 +0800

    fix: trim value in details

--- src/pages/signup-affiliates/components/_account-details.tsx
+++ src/pages/signup-affiliates/components/_account-details.tsx
@@ -140,7 +140,7 @@ const AccountDetails = ({
         setFormData((prev) => ({ ...prev, [name]: value }))
 
         if (affiliate_validation[name]) {
-            const error_msg = affiliate_validation[name](value)
+            const error_msg = affiliate_validation[name](value.trim())
             setFormErrors((errors) => ({
                 ...errors,
                 [`${name}_error_msg`]: error_msg,

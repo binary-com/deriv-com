c393c1d491db0700900ecbda5a96d6a4ba42d9c1
eduardhrachou <eduard@regentmarkets.com>
2024-01-24 14:43:39 +0300

    source-maps behaviour

--- gatsby-node.js
+++ gatsby-node.js
@@ -392,9 +392,10 @@ const style_lint_options = {
 exports.onCreateWebpackConfig = ({ stage, actions, loaders, getConfig }, { ...options }) => {
     const config = getConfig()
     const isProduction = config.mode === 'production'
+    // const isDevtool = isProduction ? false :'source-map'
 
     actions.setWebpackConfig({
-        devtool: isProduction ? false : 'eval-source-map', // enable/disable source-maps
+        devtool: isProduction ? 'source-map' : 'inline-source-map', // enable/disable source-maps
         mode: isProduction ? 'production' : 'development',
         optimization: {
             minimize: isProduction,

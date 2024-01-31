56702a34f1c653d9aa5753df972102f86d9eb2f5
hasan-deriv <abuhasan@deriv.com>
2024-01-31 14:19:57 +0800

    chore: fixed html minification issue

--- sites/eu/gatsby-config.js
+++ sites/eu/gatsby-config.js
@@ -1,7 +1,14 @@
 const path = require("path")
 
 module.exports = {
-    plugins: [{ resolve: `gatsby-theme-deriv`, options: {isEUPage: true, 
-      buildDirPath: path.resolve(__dirname, "..", "..", "sites", "row", 'public/index.html')} }],
+    plugins: [
+      {
+        resolve: `gatsby-theme-deriv`,
+        options: {
+          isEUPage: true,
+          buildDirPath: path.resolve(__dirname, "..", "..", "sites", "eu", 'public/index.html')
+        }
+      }
+    ],
   }
   

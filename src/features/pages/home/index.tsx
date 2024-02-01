c943597aad5b166110e42f58176b26c1cc458b89
fasihali-deriv <muhammad.fasih@deriv.com>
2024-02-01 19:06:01 +0800

    chore: added lazy loading

--- src/features/pages/home/index.tsx
+++ src/features/pages/home/index.tsx
@@ -1,20 +1,20 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { PageLayout } from '@deriv-com/components'
 import QuillLayout from 'features/components/quill/quill-layout'
-import LiveMarketSection from './live-pricing-migration'
 import TwentyYearsStrong from './years'
 import StatSection from './stats'
 import UserFriendlyPlatforms from './user-platfroms'
-import TradeTypeSection from './trade-type'
 import HomeHero from './hero'
-import FastPaymentSection from './fast-payment'
 import CTA from './cta'
 import StartTradingSteps from './start-trading-steps'
 import TrustpilotSection from './trustpilot'
 import { useOpenLiveChat } from 'components/hooks/use-open-live-chat-redirection'
 import MainRowNavigation from 'features/components/templates/navigation/main-nav'
 import MainFooter from 'features/components/templates/footer'
+const FastPaymentSection = lazy(() => import('./fast-payment'))
+const LiveMarketSection = lazy(() => import('./live-pricing-migration'))
+const TradeTypeSection = lazy(() => import('./trade-type'))
 
 const HomePage = () => {
     useOpenLiveChat(true)
@@ -27,11 +27,17 @@ const HomePage = () => {
                 <TrustpilotSection />
                 <StatSection />
                 <TwentyYearsStrong />
-                <LiveMarketSection />
+                <Suspense fallback={<div className="h-[695px] md:h-[638.5px] lg:h-[811.78px]" />}>
+                    <LiveMarketSection />
+                </Suspense>
                 <UserFriendlyPlatforms />
-                <TradeTypeSection />
+                <Suspense fallback={<div className="h-[1086px] md:h-[750.5px] lg:h-[624.24px]" />}>
+                    <TradeTypeSection />
+                </Suspense>
                 <StartTradingSteps />
-                <FastPaymentSection />
+                <Suspense fallback={<div className="h-[493px] md:h-[436.5px] lg:h-[579.96px]" />}>
+                    <FastPaymentSection />
+                </Suspense>
                 <CTA />
             </PageLayout>
             <MainFooter />

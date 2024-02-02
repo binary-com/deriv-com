6e7b03f474d8e7b7605e0b8552f8d37254dc9be1
fasihali-deriv <muhammad.fasih@deriv.com>
2024-02-02 14:40:41 +0800

    feat: removed lazy loading attribute

--- src/components/hooks/use-is-in-viewport.tsx
+++ src/components/hooks/use-is-in-viewport.tsx
@@ -4,20 +4,15 @@ function useIsInViewport(ref: MutableRefObject<HTMLDivElement>) {
     const [isIntersecting, setIsIntersecting] = useState(false)
     const observer = useRef<IntersectionObserver>()
 
-    // do this only once, on mount
     useEffect(() => {
         observer.current = new IntersectionObserver(([entry]) =>
             setIsIntersecting(entry.isIntersecting),
         )
-    }, [])
 
-    useEffect(() => {
         observer.current.observe(ref.current)
 
-        return () => {
-            observer.current.disconnect()
-        }
-    }, [ref, observer])
+        return () => observer.current.disconnect()
+    }, [ref.current, observer.current])
 
     return isIntersecting
 }

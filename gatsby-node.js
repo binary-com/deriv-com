d500f9bc5f3abb9e7e85f2db20940e3b2df83148
fasihali-deriv <121229483+fasihali-deriv@users.noreply.github.com>
2024-02-05 19:05:47 +0800

    Fasih/DPROD-3364/New Homepage optimization WITH font priority (#6994)
    * feat: preload fonts and updated optimised banner
    
    * feat: improved with image tag and added chunked
    
    * feat: new links added for fonts
    
    * feat: import google fonts and added lazy on images
    
    * feat: preload fonts
    
    ---------
    
    Co-authored-by: fasihali-deriv <muhammad.fasih@deriv.com>

--- gatsby-node.js
+++ gatsby-node.js
@@ -427,11 +427,19 @@ exports.onCreateWebpackConfig = ({ stage, actions, loaders, getConfig }, { ...op
         optimization: {
             minimize: isProduction,
             minimizer: [new TerserPlugin()],
-            // splitChunks: {
-            //     chunks: 'all',
-            //     name: "deriv-com",
-            // },
-
+            splitChunks: {
+                chunks: 'all',
+                cacheGroups: {
+                    default: false,
+                    vendors: false,
+                    // Merge all js, ts, and tsx files  into one bundle
+                    all: {
+                        test: /\.(js|ts|tsx)$/,
+                        name: 'bundle',
+                        chunks: 'all',
+                    },
+                },
+            },
             mangleExports: 'size',
             mangleWasmImports: true,
 
@@ -452,7 +460,7 @@ exports.onCreateWebpackConfig = ({ stage, actions, loaders, getConfig }, { ...op
         },
         plugins: [
             new StylelintPlugin({ ...style_lint_options, ...options }),
-            new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
+            // new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
         ],
         resolve: {
             modules: [path.resolve(__dirname, 'src'), 'node_modules'],

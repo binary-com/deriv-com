d96ef5e3b38d0da9189575368bc6add64bca621f
“konstantinosG-deriv” <konstantinos.gkagiannis@regentmarkets.com>
2024-02-05 10:05:15 +0100

    feat: regulatory page revamp

--- src/pages/regulatory/_eu-grid.tsx
+++ src/pages/regulatory/_eu-grid.tsx
@@ -1,164 +1,251 @@
 import React from 'react'
-import styled from 'styled-components'
-import { CssGrid, CssGridColumn } from 'components/containers'
+import {
+    FlagAustriaIcon,
+    FlagBulgariaIcon,
+    FlagCroatiaIcon,
+    FlagCyprusIcon,
+    FlagCzechRepublicIcon,
+    FlagDenmarkIcon,
+    FlagEstoniaIcon,
+    FlagFinlandIcon,
+    FlagFranceIcon,
+    FlagGermanyIcon,
+    FlagGreeceIcon,
+    FlagHungaryIcon,
+    FlagItalyIcon,
+    FlagIrelandIcon,
+    FlagLatviaIcon,
+    FlagLithuaniaIcon,
+    FlagLuxembourgIcon,
+    FlagNetherlandIcon,
+    FlagPolandIcon,
+    FlagPortugalIcon,
+    FlagRomaniaIcon,
+    FlagSlovakiaIcon,
+    FlagSloveniaIcon,
+    FlagSpainIcon,
+    FlagSwedenIcon, // eslint-disable-next-line import/no-unresolved
+} from '@deriv/quill-icons/Flags'
+import { Text } from '@deriv/quill-design'
+import { CustomLink } from '@deriv-com/components'
 import { WithIntl, Localize } from 'components/localization'
-import { ReactComponent as ViewLicense } from 'images/svg/regulatory/view-license.svg'
-import { TString } from 'types/generics'
-
-type GridItemProps = {
-    name: TString
-    order: string
-    url: string
-}
-
-const Country = styled.div`
-    margin-bottom: 2.4rem;
-    text-align: start;
-    display: grid;
-    grid-template-columns: 1.9rem 1fr;
-    grid-column-gap: 0.8rem;
-    font-size: var(--text-size-s);
-    color: var(--color-grey-3);
-    align-items: center;
-    inline-size: 20rem;
-
-    & a {
-        display: inline;
-        font-size: 1rem;
-        line-height: 1rem;
-        color: var(--color-grey-3);
-    }
-
-    @media (max-width: 425px) {
-        font-size: var(--text-size-xs);
-        inline-size: 16rem;
-    }
-`
-const Order = styled.div`
-    display: inline-block;
-    font-size: 1rem;
-    line-height: 1.9rem;
-    font-weight: 500;
-    border: 0.1rem solid var(--color-grey-3);
-    border-radius: 100%;
-    width: 2rem;
-    height: 2rem;
-    text-align: center;
-`
-const License = styled.div`
-    margin: 0;
-    display: flex;
-    align-items: center;
-
-    & a {
-        margin-left: 0.3rem;
-        font-size: 1rem;
-        line-height: 1rem;
-        color: var(--color-grey-3);
-        text-decoration: none;
-
-        :hover {
-            color: var(--color-red);
-            text-decoration: underline;
-        }
-    }
-`
-
-const AttachmentIcon = styled(ViewLicense)`
-    margin-left: 1rem;
-
-    path {
-        transition: 0.2s fill;
-    }
-    &:hover {
-        path {
-            fill: red;
-        }
-    }
-`
-
-const GridItem = ({ name, order, url }: GridItemProps) => (
-    <Country>
-        <Order>{order}</Order>
-        <License>
-            <Localize translate_text={name} />
-            <a href={url} target="_blank" rel="noopener noreferrer">
-                <AttachmentIcon />
-            </a>
-        </License>
-    </Country>
-)
 
 const EUgrid = () => {
     return (
-        <CssGrid columns="repeat(2, 1fr)">
-            <CssGridColumn>
-                <GridItem name="_t_Austria_t_" url="/regulatory/countries/Austria.pdf" order="1" />
-                <GridItem
-                    name="_t_Bulgaria_t_"
-                    url="/regulatory/countries/Bulgaria.pdf"
-                    order="2"
-                />
-                <GridItem name="_t_Croatia_t_" url="/regulatory/countries/Croatia.pdf" order="3" />
-                <GridItem name="_t_Cyprus_t_" url="/regulatory/countries/Cyprus.pdf" order="4" />
-                <GridItem
-                    name="_t_Czech Republic_t_"
-                    url="/regulatory/countries/CzechRepublic.pdf"
-                    order="5"
-                />
-                <GridItem name="_t_Denmark_t_" url="/regulatory/countries/Denmark.pdf" order="6" />
-                <GridItem name="_t_Estonia_t_" url="/regulatory/countries/Estonia.pdf" order="7" />
-                <GridItem name="_t_Finland_t_" url="/regulatory/countries/Finland.pdf" order="8" />
-                <GridItem name="_t_France_t_" url="/regulatory/countries/France.pdf" order="9" />
-                <GridItem name="_t_Germany_t_" url="/regulatory/countries/Germany.pdf" order="10" />
-                <GridItem name="_t_Greece_t_" url="/regulatory/countries/Greece.pdf" order="11" />
-                <GridItem name="_t_Hungary_t_" url="/regulatory/countries/Hungary.pdf" order="12" />
-                <GridItem name="_t_Italy_t_" url="/regulatory/countries/Italy.pdf" order="13" />
-            </CssGridColumn>
-            <CssGridColumn>
-                <Country>
-                    <Order>14</Order>
-                    <License>
+        <div className="grid grid-cols-2 md:grid-cols-8">
+            <CustomLink href="/regulatory/countries/Austria.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagAustriaIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Austria_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Bulgaria.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagBulgariaIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Bulgaria_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Croatia.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagCroatiaIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Croatia_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Cyprus.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagCyprusIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Cyprus_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink
+                href="/regulatory/countries/CzechRepublic.pdf"
+                className=" inline pr-800 pb-800"
+            >
+                <div className="flex flex-row items-center">
+                    <FlagCzechRepublicIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Czech Republic_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Denmark.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagDenmarkIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Denmark_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Estonia.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagEstoniaIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Estonia_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Finland.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagFinlandIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Finland_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/France.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagFranceIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_France_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Germany.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagGermanyIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Germany_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Greece.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagGreeceIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Greece_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Hungary.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagHungaryIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Hungary_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Italy.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagItalyIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Italy_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Ireland.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagIrelandIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
                         <Localize translate_text="_t_Ireland_t_" />
-                    </License>
-                </Country>
-                <GridItem name="_t_Latvia_t_" url="/regulatory/countries/Latvia.pdf" order="15" />
-                <GridItem
-                    name="_t_Lithuania_t_"
-                    url="/regulatory/countries/Lithuania.pdf"
-                    order="16"
-                />
-                <GridItem
-                    name="_t_Luxembourg_t_"
-                    url="/regulatory/countries/Luxembourg.pdf"
-                    order="17"
-                />
-                <GridItem
-                    name="_t_Netherlands_t_"
-                    url="/regulatory/countries/Netherlands.pdf"
-                    order="18"
-                />
-                <GridItem name="_t_Poland_t_" url="/regulatory/countries/Poland.pdf" order="19" />
-                <GridItem
-                    name="_t_Portugal_t_"
-                    url="/regulatory/countries/Portugal.pdf"
-                    order="20"
-                />
-                <GridItem name="_t_Romania_t_" url="/regulatory/countries/Romania.pdf" order="21" />
-                <GridItem
-                    name="_t_Slovakia_t_"
-                    url="/regulatory/countries/Slovakia.pdf"
-                    order="22"
-                />
-                <GridItem
-                    name="_t_Slovenia_t_"
-                    url="/regulatory/countries/Slovenia.pdf"
-                    order="23"
-                />
-                <GridItem name="_t_Spain_t_" url="/regulatory/countries/Spain.pdf" order="24" />
-                <GridItem name="_t_Sweden_t_" url="/regulatory/countries/Sweden.pdf" order="25" />
-            </CssGridColumn>
-        </CssGrid>
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Latvia.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagLatviaIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Latvia_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink
+                href="/regulatory/countries/Lithuania.pdf"
+                className=" inline pr-800 pb-800"
+            >
+                <div className="flex flex-row items-center">
+                    <FlagLithuaniaIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Lithuania_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink
+                href="/regulatory/countries/Luxembourg.pdf"
+                className=" inline pr-800 pb-800"
+            >
+                <div className="flex flex-row items-center">
+                    <FlagLuxembourgIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Luxembourg_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink
+                href="/regulatory/countries/Netherlands.pdf"
+                className=" inline pr-800 pb-800"
+            >
+                <div className="flex flex-row items-center">
+                    <FlagNetherlandIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Netherlands_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Poland.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagPolandIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Poland_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Portugal.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagPortugalIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Portugal_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Romania.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagRomaniaIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Romania_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Slovakia.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagSlovakiaIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Slovakia_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Slovenia.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagSloveniaIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Slovenia_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Spain.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagSpainIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Spain_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+            <CustomLink href="/regulatory/countries/Sweden.pdf" className=" inline pr-800 pb-800">
+                <div className="flex flex-row items-center">
+                    <FlagSwedenIcon className="mr-400" iconSize="md" fill="#000000b8" />
+                    <Text className="text-base">
+                        <Localize translate_text="_t_Sweden_t_" />
+                    </Text>
+                </div>
+            </CustomLink>
+        </div>
     )
 }
 

d4a4cb72f814b7b6218e6129b19149c95077853a
fasihali-deriv <121229483+fasihali-deriv@users.noreply.github.com>
2024-02-06 12:55:56 +0800

    Fasih/DPROD-3364/New Homepage optimization WITH font priority (#7005)
    * feat: preload fonts and updated optimised banner
    
    * feat: improved with image tag and added chunked
    
    * feat: new links added for fonts
    
    * feat: import google fonts and added lazy on images
    
    * feat: preload fonts
    
    ---------
    
    Co-authored-by: fasihali-deriv <muhammad.fasih@deriv.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -15,11 +15,10 @@ import {
     getLanguage,
     updateURLAsPerUserLanguage,
 } from 'common/utility'
-import './static/css/noto-sans-arabic.css'
 import 'swiper/swiper-bundle.min.css'
 import 'features/styles/app.scss'
-import './static/css/ubuntu.css'
 import './static/css/global.css'
+import './static/css/google-fonts.css'
 import '@deriv-com/blocks/style.css';
 
 const is_browser = typeof window !== 'undefined'

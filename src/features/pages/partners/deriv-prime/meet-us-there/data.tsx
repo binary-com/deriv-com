1065e11f7078f734c5c750fae37ed5b2b0f9f3f8
behnaz-deriv <behnaz@regentmarkets.com>
2024-02-07 10:54:08 +0800

    chore: dubai expo

--- src/features/pages/partners/deriv-prime/meet-us-there/data.tsx
+++ src/features/pages/partners/deriv-prime/meet-us-there/data.tsx
@@ -6,20 +6,6 @@ import { localize } from 'components/localization'
 
 export const expo_cards: ExpoCardTypes[] = [
     {
-        title: '_t_iFX Expo Dubai_t_',
-        date: '_t_16-18 January 2024_t_',
-        image: (
-            <StaticImage
-                src="../../../../../images/common/deriv-prime/ifx_expo_dubai.png"
-                alt={localize('_t_iFX Expo Dubai Exhibitor_t_')}
-                className={styles.image}
-                formats={['avif', 'webp', 'auto']}
-                placeholder="none"
-            />
-        ),
-        link: 'https://dubai2024.ifxexpo.com/discover/',
-    },
-    {
         title: '_t_iFX Expo Latam_t_',
         date: '_t_9-11 April 2024_t_',
         image: (

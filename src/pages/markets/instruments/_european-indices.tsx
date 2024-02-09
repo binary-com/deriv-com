6266901c964f431f36dd69219d740c725bf336d9
hasan-deriv <abuhasan@deriv.com>
2024-02-09 11:25:09 +0800

    chore: fixed stock page

--- src/pages/markets/instruments/_european-indices.tsx
+++ src/pages/markets/instruments/_european-indices.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Symbol from '../components/helper/_symbol'
 import { stocks_european_indices } from '../static/content/_market-symbols'
 import useRegion from 'components/hooks/use-region'
@@ -7,23 +7,18 @@ const stocks_european_indices_eu_endpoint = stocks_european_indices.filter((cfds
     return cfds?.eu === true
 })
 const EuropeanIndices = () => {
-    const { is_eu, is_row } = useRegion()
+    const { is_eu } = useRegion()
+    const [content, seetContent] = useState(stocks_european_indices)
+
+    useEffect(() => {
+        if (is_eu) seetContent(stocks_european_indices_eu_endpoint)
+    }, [is_eu])
+
     return (
         <>
-            {is_row && (
-                <>
-                    {stocks_european_indices.map((symbol, index) => (
-                        <Symbol key={symbol.text && index} src={symbol.src} text={symbol.text} />
-                    ))}
-                </>
-            )}
-            {is_eu && (
-                <>
-                    {stocks_european_indices_eu_endpoint.map((symbol, index) => (
-                        <Symbol key={symbol.text && index} src={symbol.src} text={symbol.text} />
-                    ))}
-                </>
-            )}
+            {content.map((symbol, index) => (
+                <Symbol key={symbol.text && index} src={symbol.src} text={symbol.text} />
+            ))}
         </>
     )
 }

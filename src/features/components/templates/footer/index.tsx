32838580a0e17fb7232c90076992228165499290
eduardhrachou <eduard@regentmarkets.com>
2024-02-12 14:37:47 +0300

    social media changer based on lang without config

--- src/features/components/templates/footer/index.tsx
+++ src/features/components/templates/footer/index.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
+import Cookies from 'js-cookie'
 import { Footer } from '@deriv-com/blocks'
 import { qtMerge } from '@deriv/quill-design'
 import {
@@ -7,6 +8,7 @@ import {
     socialButtonsCareers,
     socialButtonsEU,
     socialButtonsROW,
+    specialLanguageUrls,
     warnText,
 } from './data'
 import { DerivGoBanner } from './deriv-go-banner'
@@ -18,6 +20,7 @@ import { getLocationPathname } from 'common/utility'
 export const MainFooter = () => {
     const [is_career, setIsCareer] = useState(false)
     const { is_eu, is_cpa_plan } = useRegion()
+    const lang = Cookies.get('user_language') || 'en'
 
     useEffect(() => {
         const current_path = getLocationPathname()
@@ -26,11 +29,9 @@ export const MainFooter = () => {
         setIsCareer(is_career_page)
     }, [])
 
-    const socialButtons = is_career
-        ? socialButtonsCareers
-        : is_eu
-        ? socialButtonsEU
-        : socialButtonsROW
+    const socialButtons = useMemo(() => {
+        return getSocialButtons(lang, is_eu, is_career)
+    }, [is_eu, lang, is_career])
 
     return (
         <Footer.FooterBlock
@@ -44,6 +45,22 @@ export const MainFooter = () => {
             <Footer.MainNavContent items={is_eu ? EuFooterNavData : RowFooterNavData} cols="six" />
         </Footer.FooterBlock>
     )
+}
+
+const getSocialButtons = (lang: string, is_eu: boolean, is_career: boolean) => {
+    const overrideWithLang = (arr: any) =>
+        arr.map((button: any) =>
+            lang in specialLanguageUrls
+                ? button['aria-label'] in specialLanguageUrls[lang]
+                    ? { ...button, href: specialLanguageUrls[lang][button['aria-label']] }
+                    : button
+                : button,
+        )
+
+    let buttons = is_career ? socialButtonsCareers : is_eu ? socialButtonsEU : socialButtonsROW
+    buttons = overrideWithLang(buttons)
+
+    return buttons
 }
 
 export default MainFooter

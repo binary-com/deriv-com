ddc8aeea3a3bca6d9f8c7ebc7e61617f81e48e9f
fasihali-deriv <121229483+fasihali-deriv@users.noreply.github.com>
2024-02-19 14:18:10 +0800

    Revert "wojtek/DPROD-3020/Fix rendering quirk with login button (#6826)"
    This reverts commit 3bf8a82362ba768a4c4e4206e2e8ae16d0ef2cb3.

--- src/components/hooks/use-auth-check.tsx
+++ src/components/hooks/use-auth-check.tsx
@@ -2,11 +2,12 @@ import { useEffect, useRef, useState } from 'react'
 import { isLoggedIn } from 'common/utility'
 
 const useAuthCheck = () => {
-    const [is_logged_in, setLoggedIn] = useState(isLoggedIn())
+    const [is_logged_in, setLoggedIn] = useState(false)
     const [is_auth_checked, setIsAuthChecked] = useState(false)
     const checkCookieInterval = useRef<NodeJS.Timer>()
 
     useEffect(() => {
+        setLoggedIn(isLoggedIn())
         setIsAuthChecked(true)
         checkCookieInterval.current = setInterval(() => setLoggedIn(isLoggedIn()), 800)
         return () => clearInterval(checkCookieInterval.current)

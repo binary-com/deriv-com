52e4047a0f62f7dceecb3803a32725f699653232
behnaz-deriv <behnaz@regentmarkets.com>
2024-02-21 11:57:23 +0800

    chore: temp change

--- src/features/pages/partners/deriv-prime/meet-us-there/data.tsx
+++ src/features/pages/partners/deriv-prime/meet-us-there/data.tsx
@@ -6,20 +6,6 @@ import { localize } from 'components/localization'
 
 export const expo_cards: ExpoCardTypes[] = [
     {
-        title: '_t_iFX Expo Latam_t_',
-        date: '_t_9-11 April 2024_t_',
-        image: (
-            <StaticImage
-                src="../../../../../images/common/deriv-prime/ifx_expo_latam.png"
-                alt={localize('_t_iFX Expo Latam Exhibitor_t_')}
-                className={styles.image}
-                formats={['avif', 'webp', 'auto']}
-                placeholder="none"
-            />
-        ),
-        link: 'https://latam2024.ifxexpo.com/',
-    },
-    {
         title: '_t_iFX Expo International Cyprus_t_',
         date: '_t_18-20 June 2024_t_',
         image: (
@@ -32,6 +18,20 @@ export const expo_cards: ExpoCardTypes[] = [
             />
         ),
         link: 'https://dubai2024.ifxexpo.com/discover/',
+    },
+    {
+        title: '_t_iFX Expo Latam_t_',
+        date: '_t_9-11 April 2024_t_',
+        image: (
+            <StaticImage
+                src="../../../../../images/common/deriv-prime/ifx_expo_latam.png"
+                alt={localize('_t_iFX Expo Latam Exhibitor_t_')}
+                className={styles.image}
+                formats={['avif', 'webp', 'auto']}
+                placeholder="none"
+            />
+        ),
+        link: 'https://latam2024.ifxexpo.com/',
     },
     {
         title: '_t_Forex Expo Dubai_t_',

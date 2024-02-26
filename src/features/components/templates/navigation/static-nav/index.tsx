034c0b2143aea158eca2a4fe37c4902acdcfc75b
aswathy-deriv <aswathy@deriv.com>
2024-02-26 15:23:51 +0400

    fix: flickering logo

--- src/features/components/templates/navigation/static-nav/index.tsx
+++ src/features/components/templates/navigation/static-nav/index.tsx
@@ -1,33 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import NavTemplate from '../template'
-import { static_nav_logo, static_nav_logo_academy } from './static-nav.module.scss'
+import { static_nav_logo } from './static-nav.module.scss'
 import Link from 'features/components/atoms/link'
 import Image from 'features/components/atoms/image'
 import LogoImage from 'images/common/rebranding_logo.svg'
-import AcademyLogoImage from 'images/common/derivacademy.svg'
-import { isBrowser } from 'common/utility'
 
 const StaticNav = () => {
-    const url = isBrowser() && window.location.href
-    const academy = isBrowser() && url.includes('academy')
-    const [is_academy, setIsAcademy] = useState(false)
-    useEffect(() => {
-        // setIsAcademy(true)
-        if (academy) {
-            setIsAcademy(true)
-        }
-    }, [])
-
     return (
         <NavTemplate
             has_centered_items
             has_centered_logo
             renderLogo={() => (
                 <Link url={{ type: 'internal', to: '/' }}>
-                    <Image
-                        src={is_academy ? AcademyLogoImage : LogoImage}
-                        className={is_academy ? static_nav_logo_academy : static_nav_logo}
-                    />
+                    <Image src={LogoImage} className={static_nav_logo} />
                 </Link>
             )}
         />
--- src/features/pages/signup-academy/index.tsx
+++ src/features/pages/signup-academy/index.tsx
@@ -4,9 +4,12 @@ import Layout from 'features/components/templates/layout'
 import { signup_wrapper, static_nav_logo } from './signup.module.scss'
 import SignUpContent from './signup.content'
 import SignUpFormContainer from './form-container'
+import NavTemplate from 'features/components/templates/navigation/template'
 import Flex from 'features/components/atoms/flex-box'
 import { isBrowser } from 'common/utility'
-import StaticNav from 'features/components/templates/navigation/static-nav'
+import Link from 'features/components/atoms/link'
+import Image from 'features/components/atoms/image'
+import LogoImage from 'images/common/derivacademy.svg'
 
 const SignUpAcademy = () => {
     const analyticsData: Parameters<typeof Analytics.trackEvent>[1] = {
@@ -20,7 +23,16 @@ const SignUpAcademy = () => {
 
     return (
         <Layout>
-            <StaticNav />
+            <NavTemplate
+                has_centered_items
+                has_centered_logo
+                renderLogo={() => (
+                    <Link url={{ type: 'internal', to: '/' }}>
+                        <Image src={LogoImage} className={static_nav_logo} />
+                    </Link>
+                )}
+            />
+
             <Flex.Box
                 container="fluid"
                 align="start"

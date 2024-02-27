2e0253327910ad5576eef8193b5e4395add85fd6
agrim-deriv <agrim@regentmarkets.com>
2024-02-27 12:50:26 +0400

    fix: improve resolution of image

--- src/features/pages/home/hero/hero-image.tsx
+++ src/features/pages/home/hero/hero-image.tsx
@@ -5,9 +5,9 @@ import { hero_img } from './styles.module.scss'
 
 const HeroImage = () => {
     return (
-        <div className={clsx('absolute inset-50 flex items-end -z-10', hero_img)}>
+        <div className={clsx('absolute inset-50 flex items-end -z-100', hero_img)}>
             <StaticImage
-                src="../../../../images/migration/home/home_hero_new.webp"
+                src="../../../../images/migration/home/home_hero.webp"
                 alt="hero image"
                 placeholder="none"
                 formats={['auto', 'webp']}

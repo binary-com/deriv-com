3d6ddcaff45961fcbbf72a777d4e7b9afe47e936
Agrim Jain <agrimjain@Agrim-Jains-Mac-FQ22G9XXP9-Standard.local>
2024-02-28 12:03:22 +0400

    fix: webp to png fix

--- src/features/pages/home/hero/hero-image.tsx
+++ src/features/pages/home/hero/hero-image.tsx
@@ -7,10 +7,10 @@ const HeroImage = () => {
     return (
         <div className={clsx('absolute inset-50 flex items-end -z-100', hero_img)}>
             <StaticImage
-                src="../../../../images/migration/home/home_hero.png"
+                src="../../../../images/migration/home/person_hero.png"
                 alt="hero image"
                 placeholder="none"
-                formats={['auto', 'webp']}
+                formats={['auto', 'png']}
                 loading="eager"
             />
         </div>

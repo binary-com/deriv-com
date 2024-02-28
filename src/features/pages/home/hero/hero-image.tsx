e59d32b7f967838986e8bd8cbb4f03272dc83be4
Agrim Jain <agrimjain@Agrim-Jains-Mac-FQ22G9XXP9-Standard.local>
2024-02-28 10:32:22 +0400

    fix: fix the photo resolution

--- src/features/pages/home/hero/hero-image.tsx
+++ src/features/pages/home/hero/hero-image.tsx
@@ -7,7 +7,7 @@ const HeroImage = () => {
     return (
         <div className={clsx('absolute inset-50 flex items-end -z-100', hero_img)}>
             <StaticImage
-                src="../../../../images/migration/home/home_hero.webp"
+                src="../../../../images/migration/home/home_hero.png"
                 alt="hero image"
                 placeholder="none"
                 formats={['auto', 'webp']}

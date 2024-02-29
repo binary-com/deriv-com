4ffa7946f743db4c6860c6e27c85da914876a8df
habib-deriv <habib@regentmarkets.com>
2024-02-29 11:57:18 +0800

    chore: from local everything true

--- static/appConfig.js
+++ static/appConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     "live_chat": true,
     "whatsapp_chat": true,
-    "google_signup": false,
+    "google_signup": true,
     "apple_signup": true,
     "facebook_signup": true,
 }

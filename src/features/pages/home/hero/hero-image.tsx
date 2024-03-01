74111c04e5e11c27bc5c98927703787aa97e2d6c
Agrim Jain <agrimjain@Agrim-Jains-Mac-FQ22G9XXP9-Standard.local>
2024-03-01 14:45:13 +0400

    fix: fixing the styling

--- src/features/pages/home/hero/hero-image.tsx
+++ src/features/pages/home/hero/hero-image.tsx
@@ -5,7 +5,7 @@ import { hero_img } from './styles.module.scss'
 
 const HeroImage = () => {
     return (
-        <div className={clsx('absolute inset-50 flex items-end -z-1', hero_img)}>
+        <div className={clsx('absolute inset-50 flex items-end -z-10', hero_img)}>
             <StaticImage
                 src="../../../../images/migration/home/feb-human.png"
                 alt="hero image"

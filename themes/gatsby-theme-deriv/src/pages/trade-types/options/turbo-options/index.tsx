0638dbd82aaf448876a19e64c2de60138bbf6ec3
hasan-deriv <abuhasan@deriv.com>
2024-03-04 14:01:18 +0800

    chore: updated turbo options page

--- themes/gatsby-theme-deriv/src/pages/trade-types/options/turbo-options/index.tsx
+++ themes/gatsby-theme-deriv/src/pages/trade-types/options/turbo-options/index.tsx
@@ -2,18 +2,17 @@ import React from 'react'
 import TurboOptions from 'features/pages/options/turbo'
 import { faq_schema } from 'features/pages/options/turbo/_faq-schema'
 import { WithIntl } from 'components/localization'
-import useRegion from 'components/hooks/use-region'
 import ProtectedRoute from 'features/components/molecules/protected-route'
 import { SEO } from 'components/containers'
 import { TGatsbyHead } from 'features/types'
 
-const TurboOptionsPage = () => {
-    const { is_row, is_region_loading } = useRegion()
+const TurboOptionsPage = ({ pageContext }: TGatsbyHead) => {
+    const { region } = pageContext
     return (
         <ProtectedRoute
-            is_page_visible={is_row}
-            component={<TurboOptions />}
-            is_loading={is_region_loading}
+            region={region}
+            is_page_visible={region === "row"}
+            component={<TurboOptions region={region}/>}
         />
     )
 }

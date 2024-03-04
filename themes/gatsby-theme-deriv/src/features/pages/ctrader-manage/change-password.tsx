c607dc179dfda3500eba09d986adb618d9c0c9b9
hasan-deriv <abuhasan@deriv.com>
2024-03-04 14:26:19 +0800

    chore: updated ctrader manage pages

--- themes/gatsby-theme-deriv/src/features/pages/ctrader-manage/change-password.tsx
+++ themes/gatsby-theme-deriv/src/features/pages/ctrader-manage/change-password.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { ctrader_manage_data } from './ctrader-manage-data'
 import CtraderManage from './ctrader-manage'
+import { BuildVariantType } from 'features/types'
 
-const ChangePassword = () => {
+const ChangePassword = ({ region }: BuildVariantType) => {
     const { title, button_url, paragraph, button_text } = ctrader_manage_data.change_password
     return (
         <CtraderManage
@@ -10,6 +11,7 @@ const ChangePassword = () => {
             button_url={button_url}
             paragraph={paragraph}
             button_text={button_text}
+            region={region}
         />
     )
 }

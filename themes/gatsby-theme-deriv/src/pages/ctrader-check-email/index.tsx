3a1d8eaa42c405635dcd538a6cabbceddca0f3ab
hasan-deriv <abuhasan@deriv.com>
2024-03-04 14:19:51 +0800

    chore: updated ctrader check email page

--- themes/gatsby-theme-deriv/src/pages/ctrader-check-email/index.tsx
+++ themes/gatsby-theme-deriv/src/pages/ctrader-check-email/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { WithIntl } from 'components/localization'
 import CtraderCheckEmail from 'features/pages/ctrader-check-email'
+import { TGatsbyHead } from 'features/types'
 
-const CtraderCheckEmailPage = () => <CtraderCheckEmail />
+const CtraderCheckEmailPage = ({ pageContext }: TGatsbyHead) => {
+    const { region } = pageContext
+    return <CtraderCheckEmail region={region} />
+}
 
 export default WithIntl()(CtraderCheckEmailPage)

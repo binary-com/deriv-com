91ced565391759ce688dec138f32079787f2cba2
hasan-deriv <abuhasan@deriv.com>
2024-03-04 14:18:54 +0800

    chore: updated ctrader login page

--- themes/gatsby-theme-deriv/src/pages/ctrader-login/index.tsx
+++ themes/gatsby-theme-deriv/src/pages/ctrader-login/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { WithIntl } from 'components/localization'
 import CtraderLogin from 'features/pages/ctrader-login'
+import { TGatsbyHead } from 'features/types'
 
-const CtraderLoginPage = () => <CtraderLogin />
+const CtraderLoginPage = ({ pageContext }: TGatsbyHead) => {
+    const { region } = pageContext
+
+    return <CtraderLogin region={region}/>
+}
 
 export default WithIntl()(CtraderLoginPage)

95af0e0b8c15bcb9aedc994c28e52fd6257417a6
hasan-deriv <abuhasan@deriv.com>
2024-03-04 14:03:00 +0800

    chore: updated vanilla options page

--- themes/gatsby-theme-deriv/src/pages/trade-types/options/vanilla-options/index.tsx
+++ themes/gatsby-theme-deriv/src/pages/trade-types/options/vanilla-options/index.tsx
@@ -2,18 +2,17 @@ import React from 'react'
 import VanillaOptions from 'features/pages/options/vanilla'
 import { faq_schema } from 'features/pages/options/vanilla/_faq-schema'
 import { WithIntl } from 'components/localization'
-import useRegion from 'components/hooks/use-region'
 import ProtectedRoute from 'features/components/molecules/protected-route'
 import { SEO } from 'components/containers'
 import { TGatsbyHead } from 'features/types'
 
-const VanillaOptionsPage = () => {
-    const { is_row, is_region_loading } = useRegion()
+const VanillaOptionsPage = ({ pageContext }: TGatsbyHead) => {
+    const { region } = pageContext
     return (
         <ProtectedRoute
-            is_page_visible={is_row}
-            component={<VanillaOptions />}
-            is_loading={is_region_loading}
+            region={region}
+            is_page_visible={region === "row"}
+            component={<VanillaOptions region={region}/>}
         />
     )
 }

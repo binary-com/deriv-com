6fb519fb04c295b3bdc44aac214e92fb05d47159
Agrim Jain <agrimjain@Agrim-Jains-Mac-FQ22G9XXP9-Standard.local>
2024-03-06 13:06:13 +0400

    fix: fixes in the text

--- src/features/components/templates/footer/description.tsx
+++ src/features/components/templates/footer/description.tsx
@@ -32,7 +32,7 @@ export const DescriptionContentROW = () => {
 
             <Text size="sm" className="pt-general-lg">
                 <Localize
-                    translate_text="_t_Make sure to read our <0>Terms and conditions</0>, <1>Risk disclosure</1>, and <2>Secure and responsible trading</2> to fully understand the risks involved before using our services. Please note that the information on this website does not constitute investment advice._t_"
+                    translate_text="_t_Make sure to read our <0>Terms and Conditions</0>, <1>Risk Disclosure</1>, and <2>Secure and Responsible Trading</2> to fully understand the risks involved before using our services. Please also note that the information on this website does not constitute investment advice._t_"
                     components={[
                         <a
                             className={clsx(sharedClasses)}
@@ -71,7 +71,7 @@ export const DescriptionContentEU = () => {
             </Text>
             <Text size="sm" className="pt-general-lg">
                 <Localize
-                    translate_text="_t_Make sure to read our <0>Terms and conditions</0>, <1>Risk disclosure</1>, and <2>Secure and responsible trading</2> to fully understand the risks involved before using our services. Please also note that the information on this website does not constitute investment advice. The value of investments may go down as well as up._t_"
+                    translate_text="_t_Make sure to read our <0>Terms and Conditions</0>, <1>Risk Disclosure</1>, and <2>Secure and Responsible Trading</2> to fully understand the risks involved before using our services. Please also note that the information on this website does not constitute investment advice. The value of investments may go down as well as up._t_"
                     components={[
                         <a
                             className={clsx(sharedClasses)}
@@ -121,7 +121,7 @@ export const DescriptionContentCPA = () => {
 
             <Text size="sm" className="pt-general-lg">
                 <Localize
-                    translate_text="_t_Make sure to read our <0>Terms and conditions</0>, <1>Risk disclosure</1>, and <2>Secure and responsible trading</2> to fully understand the risks involved before using our services. Please also note that the information on this website does not constitute investment advice._t_"
+                    translate_text="_t_Make sure to read our <0>Terms and Conditions</0>, <1>Risk Disclosure</1>, and <2>Secure and Responsible Trading</2> to fully understand the risks involved before using our services. Please also note that the information on this website does not constitute investment advice._t_"
                     components={[
                         <a
                             className={clsx(sharedClasses)}
@@ -130,7 +130,7 @@ export const DescriptionContentCPA = () => {
                         />,
                         <a
                             className={clsx(sharedClasses)}
-                            href="/tnc/eu/risk-disclosure.pdf"
+                            href="/tnc/risk-disclosure.pdf"
                             key={1}
                             target="_blank"
                         />,

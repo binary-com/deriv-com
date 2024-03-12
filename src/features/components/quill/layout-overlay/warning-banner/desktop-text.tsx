e9de56f7dd09dddb70458f4352116f2b0889fd30
hasan-deriv <abuhasan@deriv.com>
2024-03-12 16:58:13 +0800

    chore: fixed tablet size

--- src/features/components/quill/layout-overlay/warning-banner/desktop-text.tsx
+++ src/features/components/quill/layout-overlay/warning-banner/desktop-text.tsx
@@ -5,14 +5,14 @@ import { Localize } from 'components/localization'
 const DesktopText = () => {
     return (
         <div className="hidden sm:!block">
-            <Section className="bg-solid-slate-700 py-general-sm fixed left-general-none top-general-none w-full z-10">
+            <Section className="bg-solid-slate-700 py-general-sm fixed left-general-none top-general-none w-full z-[60]">
                 <FluidContainer>
                     <Text className="text-solid-slate-50" size="sm">
                         <Localize translate_text="_t_The products offered on our website are complex derivative products that carry a significant risk of potential loss. CFDs are complex instruments with a high risk of losing money rapidly due to leverage. 70.1% of retail investor accounts lose money when trading CFDs with this provider. You should consider whether you understand how these products work and whether you can afford to take the high risk of losing your money._t_" />
                     </Text>
                 </FluidContainer>
             </Section>
-            <div className="w-full h-[82px]" />
+            <div className="w-full sm:h-[104px] min-[952px]:h-[82px]" />
         </div>
     )
 }

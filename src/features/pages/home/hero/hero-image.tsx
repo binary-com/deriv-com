ee6e77ba846ee2801d633ccb0ce31cb0636b9da7
hasan-deriv <abuhasan@deriv.com>
2024-03-12 17:58:57 +0800

    chore: updated hero

--- src/features/pages/home/hero/hero-image.tsx
+++ src/features/pages/home/hero/hero-image.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
 import clsx from 'clsx'
-import { hero_img } from './styles.module.scss'
+import { hero_img, hero_img_eu } from './styles.module.scss'
+import { useShowCfdBanner } from 'components/hooks/use-show-cfd-banner'
 
 const HeroImage = () => {
+    const show_banner = useShowCfdBanner()
     return (
-        <div className={clsx('absolute lg:inset-50 flex items-end -z-10', hero_img)}>
+        <div
+            className={clsx(
+                'absolute flex items-end -z-10',
+                show_banner ? 'lg:inset-50' : 'inset-50',
+                show_banner ? hero_img_eu : hero_img,
+            )}
+        >
             <StaticImage
                 src="../../../../images/migration/home/home_hero_new.png"
                 alt="hero image"
--- src/features/pages/home/hero/index.tsx
+++ src/features/pages/home/hero/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react'
-import { FluidContainer, Heading, Section } from '@deriv/quill-design'
+import { FluidContainer, Heading, Section, qtJoin } from '@deriv/quill-design'
 import clsx from 'clsx'
 import { StaticImage } from 'gatsby-plugin-image'
 import {
@@ -17,6 +17,7 @@ import { Localize, get_lang_direction } from 'components/localization'
 import TradersHubCtaButton from 'features/components/molecules/traders-hub-cta-button'
 import { useFloatingCtaContext } from 'features/contexts/floating-cta/cta.provider'
 import { usePageLoaded } from 'components/hooks/use-page-loaded'
+import { useShowCfdBanner } from 'components/hooks/use-show-cfd-banner'
 
 export interface HomeHeroProps {
     children?: ReactNode
@@ -26,10 +27,18 @@ export interface HomeHeroProps {
 const HomeHero: React.FC<HomeHeroProps> = () => {
     const [is_mounted] = usePageLoaded()
     const { ctaBottom, visibilityPercentage, entryRef } = useFloatingCtaContext()
+    const show_banner = useShowCfdBanner()
 
     return (
         <>
-            <Section className="h-[calc(100vh-230px)] sm:h-[calc(100vh-218px)] min-h-[587px] relative isolate overflow-hidden">
+            <Section
+                className={qtJoin(
+                    'min-h-[587px] relative isolate overflow-hidden',
+                    show_banner
+                        ? 'h-[calc(100vh-230px)] sm:h-[calc(100vh-218px)]'
+                        : 'h-[calc(100vh-136px)]',
+                )}
+            >
                 <StaticImage
                     src="../../../../images/migration/home/home_hero_bg.jpg"
                     alt="hero background image"

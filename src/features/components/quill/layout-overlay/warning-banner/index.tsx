dd7868704c8d0501cbb002c9dab95ae589d4520e
hasan-deriv <abuhasan@deriv.com>
2024-03-12 13:31:56 +0800

    chore: splitted mobile and desktop banner

--- src/features/components/quill/layout-overlay/warning-banner/index.tsx
+++ src/features/components/quill/layout-overlay/warning-banner/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import { FluidContainer, Section, Text } from '@deriv/quill-design'
-import { Localize } from 'components/localization'
+import DesktopText from './desktop-text'
+import MobileText from './mobile-text'
 
 const WarningBanner = () => {
     return (
         <>
-            <Section className="bg-solid-slate-700 py-general-sm fixed left-general-none top-general-none w-full z-10">
-                <FluidContainer>
-                    <Text className="text-solid-slate-50" size="sm">
-                        <Localize translate_text="_t_The products offered on our website are complex derivative products that carry a significant risk of potential loss. CFDs are complex instruments with a high risk of losing money rapidly due to leverage. 70.1% of retail investor accounts lose money when trading CFDs with this provider. You should consider whether you understand how these products work and whether you can afford to take the high risk of losing your money._t_" />
-                    </Text>
-                </FluidContainer>
-            </Section>
-            <div className="w-full h-[82px]" />
+            <DesktopText />
+            <MobileText />
         </>
     )
 }
---
+++ src/features/components/quill/layout-overlay/warning-banner/mobile-text.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MobileText = () => {
+    return <div>MobileText</div>
+}
+
+export default MobileText

1f30edf994cbcd65e4b382b70c36326e389b8cdf
mayuran-deriv <129507167+mayuran-deriv@users.noreply.github.com>
2024-04-04 15:40:10 +0400

    mayuran/DPROD-3499/web_site_pop_up (#7423)
    * fix: web status pop up
    
    * fix: path issue
    
    * fix: comments for use call back
    
    * fix: layout
    
    * fix: added console
    
    * fix: cookie banner
    
    * fix: cookie set state
    
    * fix: half show
    
    * fix: bugs fixed
    
    * fix: icon middle
    
    * fix: ltr on arabic
    
    * fix: remove animatin
    
    * fix: stylings and bugs
    
    * fix: env variable added
    
    * chore: trigger vercel
    
    * chore: trigger vercel
    
    * fix: data id added
    
    * fix: data id added
    
    * chore: trigger vercel
    
    ---------
    
    Co-authored-by: prince-deriv <prince@deriv.com>

--- themes/gatsby-theme-deriv/src/features/components/molecules/layout-overlay/index.tsx
+++ themes/gatsby-theme-deriv/src/features/components/molecules/layout-overlay/index.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import pMinDelay from 'p-min-delay'
 import loadable from '@loadable/component'
 import { overlay_container } from './layout-overlay.module.scss'
 import Flex from 'features/components/atoms/flex-box'
 import { useIsRtl } from 'components/hooks/use-isrtl'
 import useThirdPartyFlags from 'components/hooks/use-third-party-flags'
+import { useCookieBanner } from 'components/hooks/use-cookie-banner'
 
 const LiveChatButton = loadable(() => pMinDelay(import('./live-chat-button'), 5000))
 const WhatsappButton = loadable(() => pMinDelay(import('./whats-app-button'), 5000))
 const CookieBanner = loadable(() => pMinDelay(import('./cookie-banner'), 5000))
+const WarningBanner = loadable(() =>
+    pMinDelay(import('features/components/quill/layout-overlay/warnings-alerts'), 5000),
+)
 
 const LayoutOverlay = () => {
+    const cookie = useCookieBanner()
     const is_rtl = useIsRtl()
     const isLiveChat = useThirdPartyFlags('chat.live_chat')
     const isWhatsappChat = useThirdPartyFlags('chat.whatsapp_chat')
+    const [trigger_warning_popuop, setTriggerWarningPopup] = useState(false)
+
+    useEffect(() => setTriggerWarningPopup(!cookie?.should_show), [cookie.should_show])
 
     return (
         <Flex.Box
@@ -31,9 +39,16 @@ const LayoutOverlay = () => {
                 justify="between"
                 align="end"
             >
-                <Flex.Box justify={is_rtl ? 'end' : 'start'} basis="6-12" grow={'1'}>
-                    <CookieBanner />
+                {cookie.should_show && !trigger_warning_popuop && (
+                    <Flex.Box justify={is_rtl ? 'end' : 'start'} basis="6-12" grow={'1'}>
+                        <CookieBanner onCookieBannerClose={() => setTriggerWarningPopup(true)} />
+                    </Flex.Box>
+                )}
+
+                <Flex.Box justify={'center'} grow={'1'}>
+                    <WarningBanner trigger_warning_popuop={trigger_warning_popuop} />
                 </Flex.Box>
+
                 <Flex.Box direction="col">
                     {isLiveChat && <LiveChatButton />}
                     {isWhatsappChat && <WhatsappButton />}

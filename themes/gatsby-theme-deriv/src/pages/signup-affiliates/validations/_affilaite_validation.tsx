a366dae13189c586599497c6602a154655be1e40
behnaz-deriv <behnaz@regentmarkets.com>
2024-06-03 16:34:54 +0800

    chore: 1 lenght show err validation

--- themes/gatsby-theme-deriv/src/pages/signup-affiliates/validations/_affilaite_validation.tsx
+++ themes/gatsby-theme-deriv/src/pages/signup-affiliates/validations/_affilaite_validation.tsx
@@ -144,7 +144,7 @@ const urlValidation = (input: string, website_number: number ) => {
     } else if ((website_number == 1) && (input.length < 2 || input.length > 50)) {
         return localize('_t_You should enter 2-50 characters._t_')
     } else  if ((website_number == 2) && 
-         (!affiliate_validation_regex.url(input) || (input.length > 50)))
+         (!affiliate_validation_regex.url(input) || (input.length > 50) || (input.length == 1)))
          return localize('_t_You should enter 2-50 characters._t_');
 }
 

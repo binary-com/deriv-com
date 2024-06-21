3cab1f3ba665ab06de49aaf6fde8b29dc779d849
yashim-deriv <yashim@regentmarkets.com>
2024-06-21 14:55:03 +0800

    feat: fix feature

--- themes/gatsby-theme-deriv/src/components/hooks/use-third-party-flags.tsx
+++ themes/gatsby-theme-deriv/src/components/hooks/use-third-party-flags.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react'
 import featuresConfig from '../../../static/appConfig'
 
 const useThirdPartyFlags = (featurePath) => {
-    return featurePath ? featuresConfig : featuresConfig
-}
+    const [data] = useState(featuresConfig)
+    const [feature, setFeature] = useState(null)
 
+    useEffect(() => {
+        const pathParts = featurePath.split('.')
+        let currentFeature = data
+        for (const part of pathParts) {
+            if (currentFeature[part] === undefined) {
+                return // Feature path does not exist
+            }
+            currentFeature = currentFeature[part]
+        }
+        if (typeof currentFeature === 'object') {
+            setFeature(currentFeature)
+        } else {
+            if (featurePath === 'maintenance_mode_content') {
+                setFeature(currentFeature)
+            } else {
+                setFeature(Boolean(currentFeature))
+            }
+        }
+    }, [data, featurePath])
+
+    return feature
+}
 export default useThirdPartyFlags

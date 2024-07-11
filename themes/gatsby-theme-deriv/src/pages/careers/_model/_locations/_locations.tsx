6dee7a537fc40125fc4c022c15214648da5556b1
Agrim Jain <agrim@regentmarkets.com>
2024-07-11 12:02:23 +0400

    fix: georgetown

--- themes/gatsby-theme-deriv/src/pages/careers/_model/_locations/_locations.tsx
+++ themes/gatsby-theme-deriv/src/pages/careers/_model/_locations/_locations.tsx
@@ -720,7 +720,7 @@ export const georgetown: LocationsType = {
     flagIcon: CaymanFlagIcon,
     thumbnail: 'thumbnail_georgetown',
     first_p:
-        ' Deriv Group SEZC is in the Cayman Commodities and Derivatives City of the Cayman Enterprise City. This office serves as a centre for compliance, corporate governance, and operational risk management for the Deriv Group. It also assists with trading support for the North American time zone.',
+        ' Deriv Group SEZC is in the Cayman Commodities and Derivatives City of the Cayman Enterprise City. This office serves as a centre for compliance, corporate governance, and operational risk management for the Deriv Group.',
     first_img: 'living_in_georgetown',
     first_img_alt: 'Deriv’s office in the Cayman Islands',
     grid_images: ['guernsey_grid_1', 'guernsey_grid_2', 'cayman_map', 'guernsey_grid_4'],
